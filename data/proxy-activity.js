bb7a9aa2506c9e082e9f0afeaf94c2ccfa3a2324
Steve Rumizen <steverumizen@gmail.com>
2019-05-07 14:45:34 -0600

    Add proxy data files for testing > > > Co-authored-by: Katie Williams <kawilliams8@gmail.com> > Co-authored-by: Steve Rumizen <steverumizen@gmail.com>

---
+++ data/proxy-activity.js
@@ -0,0 +1,73 @@
+const proxyActivityData = [
+  {
+    "userID": 1,
+    "activityData": [
+      {
+        "date": "06/05/2019",
+        "numSteps": 7368,
+        "minutesActive": 204,
+        "flightsOfStairs": 46
+      },
+      {
+        "date": "07/05/2019",
+        "numSteps": 3079,
+        "minutesActive": 115,
+        "flightsOfStairs": 4
+      },
+      {
+        "date": "08/05/2019",
+        "numSteps": 2387,
+        "minutesActive": 179,
+        "flightsOfStairs": 4
+      }
+      ]
+    },
+  {
+    "userID": 2,
+    "activityData": [
+      {
+        "date": "06/05/2019",
+        "numSteps": 9101,
+        "minutesActive": 202,
+        "flightsOfStairs": 1
+      },
+      {
+        "date": "07/05/2019",
+        "numSteps": 11825,
+        "minutesActive": 77,
+        "flightsOfStairs": 28
+      },
+      {
+        "date": "08/05/2019",
+        "numSteps": 4423,
+        "minutesActive": 266,
+        "flightsOfStairs": 31
+      }
+      ]
+    },
+  {
+    "userID": 3,
+    "activityData": [
+      {
+        "date": "06/05/2019",
+        "numSteps": 2420,
+        "minutesActive": 121,
+        "flightsOfStairs": 22
+      },
+      {
+        "date": "07/05/2019",
+        "numSteps": 2280,
+        "minutesActive": 235,
+        "flightsOfStairs": 36
+      },
+      {
+        "date": "08/05/2019",
+        "numSteps": 4483,
+        "minutesActive": 75,
+        "flightsOfStairs": 29
+      }
+      ]
+    }
+  ]
+
+  module.exports = proxyActivityData;

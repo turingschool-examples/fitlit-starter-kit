f3b3744f2efe2ada2707cfbccf9a175a08e9417b
Taylor Jordan <taylornoel418@icloud.com>
2019-05-07 06:01:16 -0600

    Initialize testing files

---
+++ test/activity-test.js
@@ -0,0 +1,7 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Activity = require('/data/activity');
+
+describe('Activity', function() {
+
+})
---
+++ test/sleep-test.js
@@ -0,0 +1,7 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Sleep = require('../data/sleep');
+
+describe('Sleep', function() {
+	
+})

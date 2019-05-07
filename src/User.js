a73de9309ec87f2a218038dbbb39b63201938c23
Antonio Fry <antoniofry2000@gmail.com>
2019-05-07 15:34:47 -0600

    add test and User class

---
+++ src/User.js
@@ -0,0 +1,7 @@
+class User {
+  constructor(name) {
+    this.name = name
+  }
+}
+
+module.exports = User;
---
+++ test/User-test.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const expect = chai.expect;
+const User = require('../src/User')
+
+describe('User', function() {
+  it('should be a function', function() {
+    const user = new User();
+    expect(User).to.be.a('function');
+  })
+})

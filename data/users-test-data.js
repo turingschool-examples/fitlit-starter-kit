9bf490a0318efb7c3a10d6b5db7eea9ae044fbde
Alek <alekaker@outlook.com>
2019-05-07 14:41:25 -0600

    adding user repository test and class
    Co-authored-by: Jev Forsberg <jev.forsberg@gmail.com>

--- data/users-test-data.js
+++ data/users-test-data.js
@@ -2,7 +2,7 @@ const usersTestData = [
 {
   "id": 1,
   "name": "Nyasia Weber",
-  "address": "270 August Meadows, Maribelside SD 36129",
+  "address": "270 August Meadows, Maribelside AR 36129",
   "email": "Jordane_Schultz@yahoo.com",
   "strideLength": 4.7,
   "dailyStepGoal": 8000
--- test/User-repository-test.js
+++ test/User-repository-test.js
@@ -1,2 +1,19 @@
 // it should have a parameter that takes in a string
-// it 
+// it should have a method to take in a user id and show the user object
+// it should have a method that shows average daily step goal of all users.
+// it should have a method to show the state that the most users are from.
+
+const chai = require('chai');
+const expect = chai.expect;
+const UserRepository = require('../src/User-repository');
+
+describe('UserRepository', function() {
+
+  it('should be a function', function() {
+    const userRepository = new UserRepository();
+    expect(UserRepository).to.be.a('function');
+  });
+
+
+
+});

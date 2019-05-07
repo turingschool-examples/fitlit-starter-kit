5d21493e2f91c0c2afdc13c8e91729f31c6de00d
Alek <alekaker@outlook.com>
2019-05-07 14:20:37 -0600

    add userTestData file content
    Co-authored-by: Jev Forsberg <jev.forsberg@gmail.com>

---
+++ data/users-test-data.js
@@ -0,0 +1,44 @@
+const usersTestData = [
+{
+  "id": 1,
+  "name": "Nyasia Weber",
+  "address": "270 August Meadows, Maribelside SD 36129",
+  "email": "Jordane_Schultz@yahoo.com",
+  "strideLength": 4.7,
+  "dailyStepGoal": 8000
+},
+{
+  "id": 2,
+  "name": "Shayne Swift",
+  "address": "747 Dickinson Gardens, South Helga AR 88484-2240",
+  "email": "Lawson74@yahoo.com",
+  "strideLength": 4.5,
+  "dailyStepGoal": 11000
+},
+{
+  "id": 3,
+  "name": "Cleo Lindgren",
+  "address": "744 Josephine Parkway, Hellerside OH 17625",
+  "email": "Zachery.Von49@gmail.com",
+  "strideLength": 5.1,
+  "dailyStepGoal": 4000
+},
+{
+  "id": 4,
+  "name": "Elaina Rau",
+  "address": "73488 Cole Ways, Jakubowskimouth AK 48498",
+  "email": "Adah_Friesen@hotmail.com",
+  "strideLength": 5.4,
+  "dailyStepGoal": 8000
+},
+{
+  "id": 5,
+  "name": "Grady Wolff",
+  "address": "45657 Lindgren Ramp, New Marysechester MT 41312",
+  "email": "Isadore42@gmail.com",
+  "strideLength": 5.5,
+  "dailyStepGoal": 11000
+}
+]
+
+module.exports = usersTestData;

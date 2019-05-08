b83b9aede2d4ef4f44dabc61d4aa910258deaa88
Patrick Goulding <patrickgoulding@gmail.com>
2019-05-07 20:07:11 -0600

    refactor for performance
    co-authored-by: Colby Allen <colbyallen012@gmail.com>

--- src/UserRepo.js
+++ src/UserRepo.js
@@ -17,14 +17,9 @@ class UserRepo {
       .split(' ')
       .filter(strings => strings.length === 2)
 
-    const uniqueStates = Array.from(new Set(justStates))
-
-    let counts = uniqueStates.map(a => justStates
-    .filter(b => b == a).length);
-    let averageState = counts.map((a, b) => Math.max(...counts) === a ? b : 0) 
-    .filter(b => b !== 0)
-    .map(a => uniqueStates[a]);
-    return averageState.toString();
+      return justStates.sort((a, b) =>
+      justStates.filter(state => state === a).length - justStates.filter(state => state === b).length)
+      .pop()
   }
   // compareID() {
   //   console.log(this.returnUserData(1).id)

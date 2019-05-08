285788fbfbc0d550fdbb42a896d375a063b30b83
Antonio Fry <antoniofry2000@gmail.com>
2019-05-08 13:05:49 -0600

    add section to header in html

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -12,11 +12,9 @@ describe('UserRepository', function() {
     expect(userRepository.getUserData(5)[0]).to.be.a('object');
   });
 
-  it('should return the average of steps based on goals across all users', function() {
+  it('should return an average of step all step goals', function() {
     const userRepository = new UserRepository(userFakeData);
 
     expect(userRepository.averageSteps()).to.be.a('number');
-
   });
-
-})
+});

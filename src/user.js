5414b2fa4401871192cfe3f32f1457bdf01ba8ad
Anneke McGrady <annekemcgrady@gmail.com>
2019-05-09 17:26:29 -0600

    Create headline with custom name and create card with user info on DOM

--- src/user.js
+++ src/user.js
@@ -1,14 +1,23 @@
+
 class User {
-  constructor(userData){
+  constructor(userData) {
   this.userData = userData;
+  
   }
+
+  // findFilepath(dataFilePath) {
+  //   if(typeof module !== 'undefined') {
+  //     return require(dataFilePath)
+  //   } else {
+  //     return userData;
+  //   }
+  // }
 
   returnFirstName() {
   	return this.userData.name.split(' ')[0];
   }
 
 }
-
 
 
 if(typeof module !== 'undefined') {

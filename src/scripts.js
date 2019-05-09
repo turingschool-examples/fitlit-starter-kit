456dd964206d8ba77639a2de17a76b2faca0aee7
Colby Allen <colbyallen012@gmail.com>
2019-05-09 11:38:13 -0600

    dom manipulation for user

--- src/scripts.js
+++ src/scripts.js
@@ -1,16 +1,24 @@
 // Variables
 
-let userRepo = new UserRepo()
+let userRepo = new UserRepo();
 console.log('userRepo: ', userRepo)
+let userStep = userRepo.averageStepGoal();
 let user = new User(1)
 console.log('user: ', user)
-let hydration = new Hydration(1)
+let hydration = new Hydration(1);
+hydration.findHydrationData();
 console.log('hydration: ', hydration)
 let sleep = new Sleep(1)
 console.log(sleep)
 
 // Event Listeners
-
+document.getElementById('userName').innerText = `Welcome ${user.userData.name}!`;
+document.getElementById('userAddress').innerText = user.userData.address;
+document.getElementById('userEmail').innerText = user.userData.email;
+document.getElementById('userStepGoal').innerText = `Daily Step Goal: ${user.userData.dailyStepGoal}`;
+document.getElementById('userStrideLength').innerText = `Stride Length ${user.userData.strideLength}`;
+document.getElementById('compStepGoal').innerText = `You: ${user.userData.dailyStepGoal}`;
+document.getElementById('compTheirStepGoal').innerText = `Them: ${userStep}`;
 // Functions
 
 

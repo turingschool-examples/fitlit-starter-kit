1427805ad08ab3cdb178da0122e0bbc9a8dfc21c
Patrick Goulding <patrickgoulding@gmail.com>
2019-05-09 17:29:21 -0600

    sleep test through 'allUsersSleepQuality' done
    co-authored-by: Colby Allen <colbyallen012@gmail.com>

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -26,7 +26,44 @@ describe('Sleep Test', function () {
   })
 
   it('should find the speciifc sleep data per user', function () {
-
+    expect(sleep.findSleepData().userID).to.be.equal(sleep.userID)
+    expect(sleep.findSleepData()).to.be.equal(data[0])
   })
+
+  it('should average their hours slept per day', function () {
+    expect(sleep.averageSleepPerDay()).to.be.a('number')
+    expect(sleep.averageSleepPerDay()).to.be.equal(7.06)
+  })
+
+  it('should return their average sleep quality per day over all time', function () {
+    expect(sleep.averageSleepAllTime()).to.be.a('number')
+    expect(sleep.averageSleepAllTime()).to.equal(2.48)
+  })
+
+  it('should return how many hours they slept for a specific day(identified by a date)', function () {
+    expect(sleep.hoursSleptOnDay('06/05/2019')).to.equal(8)
+  })
+
+  it('should return how many hours slept each day over the course of a given week(7 days)', function () {
+    expect(sleep.hoursSleptGivenWeek('06/05/2019')).to.have.length(7)
+    expect(sleep.hoursSleptGivenWeek('06/05/2019')).to.deep.include({ date: '08/05/2019', hoursSlept: 8.1 })
+  })
+
+  it('should return their sleep quality each day over the course of a given week(7 days)', function () {
+    expect(sleep.sleepQualityGivenWeek('06/05/2019')).to.have.length(7)
+    expect(sleep.sleepQualityGivenWeek('06/05/2019')).to.deep.include({ date: '11/05/2019', quality: 3.5 })
+  })
+
+  it('should return for all users, the average sleep quality', function () {
+    expect(sleep.allUsersSleepQuality()).to.be.a('number')
+    expect(sleep.allUsersSleepQuality()).to.equal(2.57)
+  })
+
+  it('should', function () {
+    sleep.allUsersGoodSleepGivenWeek()
+    
+    // expect(sleep.allUsersGoodSleepGivenWeek())
+  })
+
 
 })

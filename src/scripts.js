4739d31cfb1e7e73a6b9375ae192b9251a3e3f7f
Anneke McGrady <annekemcgrady@gmail.com>
2019-05-10 11:10:17 -0600

    Update color and add labels for user card

--- src/scripts.js
+++ src/scripts.js
@@ -1,15 +1,12 @@
-console.log("Hello World");
-
-
-
-
-let user = new User(userData[0])
-console.log(userData[0])
+let user = new User(userData[15])
+// let hydration = new Hydration(userData[15])
 
 $('span.user-name').html(user.returnFirstName());
 $('article.user').append("<div class='user-card'></div>")
-$('div.user-card').append("<h6 class='name-h6'></h6><h6 class ='address-h6'></h6><h6 class ='email-h6'></h6><h6 class ='goal-h6'></h6>")
-$('h6.name-h6').text(`${user.userData.name}`)
-$('h6.address-h6').text(`${user.userData.address}`)
-$('h6.email-h6').text(`${user.userData.email}`)
-$('h6.goal-h6').text(`${user.userData.dailyStepGoal}`)
+$('div.user-card').append("<h6> Name: <span class='name-span'></span></h6><h6>Address: <span class='address-span'></span></h6><h6> Email: <span class='email-span'></span></h6><h6>Daily Step Goal: <span class ='goal-span'></span></h6>")
+$('span.name-span').text(`${user.userData.name}`)
+$('span.address-span').text(`${user.userData.address}`)
+$('span.email-span').text(`${user.userData.email}`)
+$('span.goal-span').text(`${user.userData.dailyStepGoal}`)
+
+

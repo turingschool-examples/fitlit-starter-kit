3f71c256a1fe6c6396a6e0798d893a21bd3727c4
Patrick Goulding <patrickgoulding@gmail.com>
2019-05-10 20:07:03 -0600

    styled some graphs and widgets

--- src/scripts.js
+++ src/scripts.js
@@ -1,17 +1,27 @@
 // Variables
 let dynamicUser = Math.floor(Math.random() * (50 - 1 + 1)) + 1;
 
+// instantiations
+
 let userRepo = new UserRepo();
-console.log('userRepo: ', userRepo)
 let user = new User(dynamicUser)
-let userStep = userRepo.averageStepGoal();
-console.log('user: ', user)
 let hydration = new Hydration()
-hydration.findHydrationData();
-console.log('hydration: ', hydration)
 let sleep = new Sleep()
-console.log(sleep)
-var dt = new Date(); 
+var dt = new Date();
+
+// methods called
+
+hydration.findHydrationData();
+let userStep = userRepo.averageStepGoal();
+
+
+console.log('user: ', user)
+console.log('userRepo: ', userRepo)
+console.log('hydration: ', hydration)
+console.log('sleep: ', sleep)
+
+
+const sleepWeek = sleep.hoursSleptGivenWeek('06/05/2019')
 
 document.getElementById("datetime").innerHTML = dt.toLocaleDateString();
 
@@ -24,20 +34,61 @@ document.getElementById('userStepGoal').innerText = `Daily Step Goal: ${user.use
 document.getElementById('userStrideLength').innerText = `Stride Length ${user.user.strideLength}`;
 document.getElementById('compStepGoal').innerText = `You: ${user.user.dailyStepGoal}`;
 document.getElementById('compTheirStepGoal').innerText = `Them: ${userStep}`;
+
 // Hydration Info
+
 
 // Functions
 
 
 
-$(document).ready(() => {
-  displayUserName()
-  // startClock()
-  // $btnUpdateRange.attr('disabled', 'disbaled')
-  // $('.tagline').fadeIn(1000)
-})
+// $(document).ready(() => {
+//   displayUserName()
+//   // startClock()
+//   // $btnUpdateRange.attr('disabled', 'disbaled')
+//   // $('.tagline').fadeIn(1000)
+// })
 
-var ctx = document.getElementById('myChart').getContext('2d');
+let ctx = document.getElementById('myChart').getContext('2d');
+console.log(sleepWeek[0].hoursSlept)
+var sleepHours = document.getElementById('hours-slept-chart').getContext('2d');
+
+let hoursSleptChart = new Chart(sleepHours, {
+  type: 'bar',
+  data: {
+    labels: [`${(sleepWeek[0].date)}`, `${(sleepWeek[1].date)}`, `${(sleepWeek[2].date)}`, `${(sleepWeek[3].date)}`, `${(sleepWeek[4].date)}`, `${(sleepWeek[5].date)}`, `${(sleepWeek[6].date)}`],
+    datasets: [{
+      label: '#hours slept',
+      data: [(sleepWeek[0].hoursSlept), (sleepWeek[1].hoursSlept), (sleepWeek[2].hoursSlept), (sleepWeek[3].hoursSlept), (sleepWeek[4].hoursSlept), (sleepWeek[5].hoursSlept), (sleepWeek[6].hoursSlept)],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
 
 var myChart = new Chart(ctx, {
   type: 'bar',
@@ -77,7 +128,7 @@ var myChart = new Chart(ctx, {
 });
 
 
-const displayUserName = () => {
+// const displayUserName = () => {
   
-  console.log(user.returnUserFirstName())
-}
+//   console.log(user.returnUserFirstName())
+// }

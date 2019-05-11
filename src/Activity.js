f84d9157c895239d7c871660b90941b3be51d046
Patrick Goulding <patrickgoulding@gmail.com>
2019-05-11 00:38:25 -0600

    add tests and methods for single users activity
    co-authored-by: Colby Allen <colbyallen012@gmail.com>

--- src/Activity.js
+++ src/Activity.js
@@ -17,23 +17,50 @@ class Activity {
 //For a specific day(specified by a date), return the miles a user has walked based on their number of steps(use their strideLength to help calculate this)
 
   milesWalkedToday(day) {
-    let todaysData = this.data.find(today => today.date === day)
-    let walkedToday = Math.floor(todaysData.numSteps * user.user.strideLength) / 5280;
-    return walkedToday
+    let todaysData = this.activeData.activityData.find(today => today.date === day)
+    let walkedToday = (todaysData.numSteps * user.user.strideLength) / 5280;
+    return parseFloat(walkedToday.toFixed(2))
   }
 
 //For a user, (identified by their userID) how many minutes were they active for a given day(specified by a date) ?
+  activeMinutesPerDay(day) {
+    let todaysData = this.activeData.activityData.find(today => today.date === day)
+    return todaysData.minutesActive
+  }
   
 //For a user, how many minutes active did they average for a given week(7 days) ?
-    
-
+  activeMinutesPerWeek(weekStart) {
+    let firstDayIndex = this.activeData.activityData.findIndex(ele => ele.date === weekStart)
+    let week = this.activeData.activityData.slice(firstDayIndex, firstDayIndex + 7)
+      .map(minutes => minutes.minutesActive)
+    let weekTotal = week.reduce((acc, minute) =>
+      acc += minute, 0) / 7
+    return parseFloat(weekTotal.toFixed(2))
+  }
 //For a user, did they reach their step goal for a given day(specified by a date) ?
-      
-//For a user, find all the days where they exceeded their step goal
+  stepGoalReached(day) {
+    let activityDataForDay = this.activeData.activityData.find(ele => ele.date === day)
+    let stepsForDay = activityDataForDay.numSteps
+    let userStepGoal = user.user.dailyStepGoal
+    return userStepGoal <= stepsForDay
+  }
 
-//For a user, find their all - time stair climbing record
+  //For a user, find all the days where they exceeded their step goal
+  daysExceededStepGoal() {
+    let dailyStepGoal = user.user.dailyStepGoal
+    let filteredDays = this.activeData.activityData
+      .filter(steps =>  steps.numSteps > dailyStepGoal)
 
-//For all users, what is the average number of:
+    return filteredDays.map(date => date.date)
+  }
+
+  //For a user, find their all - time stair climbing record
+
+  allTimeStairRecord() {
+    return this.activeData.activityData.sort((a, b)=> b.flightsOfStairs - a.flightsOfStairs).map(stair => stair.flightsOfStairs).shift()
+  }
+
+  //For *ALL* users, what is the average number of:
 
 //stairs climbed for a specified date
 
@@ -42,9 +69,9 @@ class Activity {
 //minutes active for a specific date
 
 //Make a metric of your own! Document it, calculate it, and display it.
-
-
 }
+
+
 
 if (typeof module !== 'undefined') {
   module.exports = Activity;
--- test/Activity-test.js
+++ test/Activity-test.js
@@ -20,10 +20,40 @@ describe('Activity Test', function () {
     expect(activity).to.be.a.instanceOf(Activity)
   })
 
-  it('should calculate how many miles walked today in miles, based on their stride length', function () {
-    console.log(activity.milesWalkedToday('06/05/2019'))
-    expect(activity.milesWalkedToday('06/05/2019'))
-
+  it('should return the actuivity data based on the user ID', function () {
+    expect(activity.findActivityData(1)).to.be.equal(data[0])
   })
+
+  it('should calculate how many miles walked today in miles, based on their stride length', function () {
+    expect(activity.milesWalkedToday('06/05/2019')).to.be.a('number')
+    expect(activity.milesWalkedToday('06/05/2019')).to.be.equal(6.56)
+  })
+
+  it('should return how many minutes the user was active for a given day(specified by a date)', function () {
+    expect(activity.activeMinutesPerDay('06/05/2019')).to.be.a('number')
+    expect(activity.activeMinutesPerDay('06/05/2019')).to.equal(204)
+  })
+
+  it('should return how many minutes active did a user average for a given week', function () {
+    expect(activity.activeMinutesPerWeek('06/05/2019')).to.equal(176.43)
+  })
+
+  it('should return true or false for if they reached their step goal for a given day(specified by a date)', function () {
+    expect(user.user.dailyStepGoal).to.equal(8000)
+    expect(activity.activeData.activityData[22].numSteps).to.equal(11328)
+    expect(user.user.dailyStepGoal).to.be.lessThan(activity.activeData.activityData[22].numSteps)
+    expect(activity.stepGoalReached('28/05/2019')).to.equal(true)
+  })
+
+  it('should find all the days where the user exceede their step goal', function () {
+    expect(activity.daysExceededStepGoal()).to.be.a('array')
+    expect(activity.daysExceededStepGoal()).to.not.include('06/05/2019')
+  })
+
+  it('should find their all time user record of flights of stairs', function () {
+    expect(activity.allTimeStairRecord()).to.equal(50)
+  })
+
+  
 
 })

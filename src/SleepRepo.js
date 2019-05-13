461920bee97b9bf713b2448751948478030a5748
Antonio Fry <antoniofry2000@gmail.com>
2019-05-13 00:08:28 -0600

    finish SleepRepo

--- src/SleepRepo.js
+++ src/SleepRepo.js
@@ -9,40 +9,62 @@ class SleepRepo {
     return joined
   }
 
-  getWeek(date) {
-    let allSleepData = this.dataFilePath.map(el => el.sleepData.reverse());
+  getDateIndex(date) {
+    let allSleepData = this.dataFilePath.map(el => {
+      return el.sleepData.slice().reverse();
+    });
     let dates = allSleepData[0].map(el => el.date);
     return dates.indexOf(date);
   }
 
   avgSleepQual() {
     let allSleepQuality = this.getSleepData().map(el => el.sleepQuality);
-    let average = allSleepQuality.reduce((acc, curr) => acc + curr / allSleepQuality.length);
+    let average = allSleepQuality.reduce((acc, curr) => {
+      return acc + curr / allSleepQuality.length
+    });
     return Math.round(average);
   }
   
-  // I need to get the index of the element where the date is included.
-  // I need to 
   goodSleepQual(date) {
-    let index = this.getWeek(date);
-    let allSleepData = this.dataFilePath.map(el => el.sleepData);
-    let week = allSleepData.map(array => {
+    let index = this.getDateIndex(date);
+    let sleepData = this.dataFilePath.map(el => el.sleepData.slice().reverse());
+    let week = sleepData.map(array => {
       return array.slice(index, index + 7);
     });
-    let sleepQualityAverage = week.map(array => {
+    let qualityAverage = week.map(array => {
       let quality = array.map(el => el.sleepQuality);
       let average = quality.reduce((acc, curr) => acc + curr / quality.length);
       return Math.floor(average);
     });
-    console.log(sleepQualityAverage)
+    let users = this.dataFilePath.filter((el, i) => qualityAverage[i] >= 3);
+    return users.map(el => el.userID);
   }
 
   mostSleep(date) {
-
+    let sleepData = this.dataFilePath.map(el => el.sleepData.slice().reverse());
+    let dataByDate = sleepData.map(array => {
+      return array.find(el => el.date.includes(date));
+    });
+    let sleepHours = dataByDate.map(el => el.hoursSlept);
+    let greatestHour = Math.max(...sleepHours);
+    let sleepiestUser = sleepHours.indexOf(greatestHour);
+    return sleepiestUser;
   }
 
-  worstSleepQual() {
-    
+  worstSleepQual(date) {
+    let index = this.getDateIndex(date);
+    let sleepData = this.dataFilePath.map(el => el.sleepData.slice().reverse());
+    let week = sleepData.map(array => {
+      return array.slice(index, index + 7);
+    });
+    let qualityAverage = week.map(array => {
+      let quality = array.map(el => el.sleepQuality);
+      let average = quality.reduce((acc, curr) => acc + curr / quality.length);
+      return Math.floor(average);
+    });
+    let users = this.dataFilePath.filter((el, i) => qualityAverage[i] <= 3);
+    console.log(users.map(el => el.userID))
+    return users.map(el => el.userID);
   }
 }
 

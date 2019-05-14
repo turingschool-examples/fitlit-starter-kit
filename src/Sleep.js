f2da770d55d45aed80cbc0fa07c923b3fcb403bd
DeMarcus Kirby <tkd024@gmail.com>
2019-05-14 12:26:02 -0600

    add method for sleep class

--- src/Sleep.js
+++ src/Sleep.js
@@ -5,27 +5,48 @@ class Sleep {
   }
 
   avgHoursSlept(){
-    let sum = sleepData[0].sleepData.map(date => date.hoursSlept)
-    
+    let sum = info.sleepData.map(date => date.hoursSlept);
+    let avg = sum.reduce((acc, curr) => {
+      return acc + curr
+    }, 0)/sum.length
+    return Math.round(avg)  
   }
 
   avgSleepQuality(){
-
+    let sum = this.info.sleepData.map(date => date.sleepQuality);
+    let avg = sum.reduce((acc, curr) => {
+      return acc + curr
+    }, 0)/sum.length
+    return Math.round(avg) 
   }
 
-  hoursSlept(date){
-
+  hoursSlept(day){
+    let data = this.info.sleepData.filter(dates => {
+      return dates.date === day    
+     })
+    return data.map(stuff => stuff.hoursSlept)[0]
   }
 
-  sleepQuality(date){
-
+  sleepQuality(day){
+    let data = this.info.sleepData.filter(dates => {
+      return dates.date === day    
+     })
+    return data.map(stuff => stuff.sleepQuality)[0]
   }
 
-  weekSleep(startDate){
-
+  weekSleep(day){
+  let sleepInfo = this.info.sleepData
+  let workArray = sleepInfo.map(el => el.date)
+  let index = workArray.indexOf(day)
+  let data = sleepInfo.slice(index, index+2)
+  return data.map(stuff => `Date: ${stuff.date}, Hours Slept: ${stuff.hoursSlept}`)
   }
 
-  weekQuality(StartDate){
-
+  weekQuality(day){
+    let sleepInfo = this.info.sleepData
+    let workArray = sleepInfo.map(el => el.date)
+    let index = workArray.indexOf(day)
+    let data = sleepInfo.slice(index, index+2)
+    return data.map(stuff => `Date: ${stuff.date}, Hours Slept: ${stuff.sleepQuality}`)
   }
 }

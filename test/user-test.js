ed0d4d9c11d76fd974e66452c9f5a2360e705415
Jessie Le-Ho <jessiele-ho@Jessies-MacBook-Pro.local>
2019-07-01 15:30:49 -0600

    Create user test

---
+++ test/user-test.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const User = require("../src/singleUser");
+
+describe('User', function(){
+  it('should be a function', function() {
+    expect(User).to.be.a('function');
+  })
+  it('should be an instance of User', function() {
+    let user = new User();
+    expect(user).to.be.an.instanceof(User);
+  })
+  it('should represent a single user', function() {
+    let user = new User();
+    expect(user.length).to.equal(1);
+  })
+  it('should hold properties from sample data file', function() {
+    let user = new User();
+    expect(user.name).to.equal();
+  })
+});

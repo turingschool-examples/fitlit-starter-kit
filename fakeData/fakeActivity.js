43e94c167c8e8e1e75a4291678f19adff982599b
Julian Enochs <julianenochs@yahoo.com>
2019-07-01 16:23:57 -0600

    Add fake user data files
    Co-authored-by: Chris Lane <pankokrumps@gmail.com>

---
+++ fakeData/fakeActivity.js
@@ -0,0 +1,50 @@
+const activityData = [{
+            "userID": 1,
+            "date": "2019/06/15",
+            "numSteps": 3577,
+            "minutesActive": 140,
+            "flightsOfStairs": 16
+        },
+        {
+            "userID": 3,
+            "date": "2019/06/15",
+            "numSteps": 7402,
+            "minutesActive": 116,
+            "flightsOfStairs": 33
+        },
+        {
+            "userID": 4,
+            "date": "2019/06/15",
+            "numSteps": 3486,
+            "minutesActive": 114,
+            "flightsOfStairs": 32
+        },
+        {
+            "userID": 8,
+            "date": "2019/06/15",
+            "numSteps": 10333,
+            "minutesActive": 114,
+            "flightsOfStairs": 31
+        },
+        {
+            "userID": 16,
+            "date": "2019/06/15",
+            "numSteps": 6188,
+            "minutesActive": 292,
+            "flightsOfStairs": 32
+        },
+        {
+            "userID": 25,
+            "date": "2019/06/15",
+            "numSteps": 5144,
+            "minutesActive": 282,
+            "flightsOfStairs": 16
+        },
+        {
+            "userID": 31,
+            "date": "2019/06/15",
+            "numSteps": 13890,
+            "minutesActive": 280,
+            "flightsOfStairs": 41
+        }
+]
---
+++ fakeData/fakeHydration.js
@@ -0,0 +1,36 @@
+const hydrationData = [{
+            "userID": 1,
+            "date": "2019/06/15",
+            "numOunces": 37
+        },
+        {
+            "userID": 3,
+            "date": "2019/06/15",
+            "numOunces": 47
+        }, 
+        {
+            "userID": 4,
+            "date": "2019/06/15",
+            "numOunces": 85
+        },
+        {
+            "userID": 8,
+            "date": "2019/06/15",
+            "numOunces": 84
+        },
+        {
+            "userID": 16,
+            "date": "2019/06/15",
+            "numOunces": 40
+        },
+        {
+            "userID": 25,
+            "date": "2019/06/15",
+            "numOunces": 40
+        },
+        {
+            "userID": 31,
+            "date": "2019/06/15",
+            "numOunces": 97
+        }
+    ]

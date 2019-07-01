d098edd3ebcb14b33ecb9856bdd2327d83ccc081
Amy Rippeto <amyrippeto@gmail.com>
2019-07-01 16:40:35 -0600

    Add sample data
    Co-authored-by: Katherine Williams <kawilliams8@gmail.com>

---
+++ data/sample-hydration.js
@@ -0,0 +1,27 @@
+const sampleHydrationData = [
+  {
+    "userID": 1,
+    "date": "2019/06/15",
+    "numOunces": 37
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "numOunces": 75
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "numOunces": 47
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/15",
+    "numOunces": 85
+  },
+  {
+    "userID": 5,
+    "date": "2019/06/15",
+    "numOunces": 42
+  }
+]

ded2ff08e0fdff337043cf24d8368fda340afb4c
Alyssa Lundgren <lundgrea@gmail.com>
2019-07-01 17:04:45 -0600

    add function and test for inputting id and getting back user object

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,6 +1,16 @@
+// const data = require('../test-data/users-fixtures');
+
+
 class UserRepository{
-  constructor(){
-    
+  constructor(data){
+    this.data = data || {}
+  }
+  getUserData(specificId) {
+    let userData = this.data.find(function(object){
+      return object.id === specificId
+    })
+    console.log(userData)
+    return userData
   }
 }
 

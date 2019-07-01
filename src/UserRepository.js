e822359b6edbb21592bfd19f3c38be1d25219c39
Alyssa Lundgren <lundgrea@gmail.com>
2019-07-01 17:34:06 -0600

    finish user repository methods and tests

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -9,10 +9,22 @@ class UserRepository{
     let userData = this.data.find(function(object){
       return object.id === specificId
     })
-    console.log(userData)
     return userData
   }
-}
+  calculateAverageStepGoal(){
+    let stepGoals = this.data.map(function(object){
+      return object.dailyStepGoal
+    })
+    let totalStepGoal = stepGoals.reduce(function(acc, curVal){
+      return acc = acc + curVal
+    }, 0)
+    let averageStepGoal = totalStepGoal / this.data.length;
+    return averageStepGoal
+  }
+  }
+
+
+
 
 
 module.exports = UserRepository;

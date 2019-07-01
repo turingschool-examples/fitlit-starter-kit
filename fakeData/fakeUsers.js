636c3fbb63abf51e0d4941180f909e944884941b
Chris Lane <pankokrumbs@gmail.com>
2019-07-01 17:31:18 -0600

    Add functional testing
    Co-authored-by: Julian Enochs <JulianEnochs@yahoo.com>

--- fakeData/fakeUsers.js
+++ fakeData/fakeUsers.js
@@ -1,97 +1,97 @@
 const userData = [
-    {
-            "id": 1,
-            "name": "Luisa Hane",
-            "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
-            "email": "Diana.Hayes1@hotmail.com",
-            "strideLength": 4.3,
-            "dailyStepGoal": 10000,
-            "friends": [
-                16,
-                4,
-                8
-            ]
-        },
-        {
-            "id": 16,
-            "name": "Garnett Cruickshank",
-            "address": "992 Zita Mall, North Tremainemouth MA 19312-3532",
-            "email": "Laverna47@hotmail.com",
-            "strideLength": 3.9,
-            "dailyStepGoal": 10000,
-            "friends": [
-                25,
-                31,
-                3,
-                16
-            ]
-        },
-        {
-            "id": 4,
-            "name": "Mae Connelly",
-            "address": "28926 Schinner Islands, Turnermouth NE 23720-3230",
-            "email": "Marcos_Pollich@hotmail.com",
-            "strideLength": 3.1,
-            "dailyStepGoal": 4000,
-            "friends": [
-                48,
-                7,
-                44,
-                8
-            ]
-        },
-        {
-            "id": 8,
-            "name": "Laney Abshire",
-            "address": "86416 Koch Inlet, North Kaciefurt MA 80635",
-            "email": "Janice_Nitzsche2@yahoo.com",
-            "strideLength": 2.8,
-            "dailyStepGoal": 2000,
-            "friends": [
-                11,
-                41,
-                23,
-                49
-            ]
-        },
-        {
-            "id": 25,
-            "name": "Isobel Schmeler",
-            "address": "5592 Donnelly Trail, Scottieport SC 35511",
-            "email": "Name79@yahoo.com",
-            "strideLength": 3.3,
-            "dailyStepGoal": 4000,
-            "friends": [
-                37,
-                31,
-                43
-            ]
-        },
-        {
-            "id": 31,
-            "name": "Bertrand Yundt",
-            "address": "0032 Claudia Plain, Delfinaland RI 22298-3685",
-            "email": "Sibyl.Schmidt39@yahoo.com",
-            "strideLength": 3.4,
-            "dailyStepGoal": 7000,
-            "friends": [
-                16,
-                41,
-                9
-            ]
-        },
-        {
-            "id": 3,
-            "name": "Herminia Witting",
-            "address": "85823 Bosco Fork, East Oscarstad MI 85126-5660",
-            "email": "Elwin.Tromp@yahoo.com",
-            "strideLength": 4.4,
-            "dailyStepGoal": 5000,
-            "friends": [
-                19,
-                11,
-                42,
-                33
-            ]
-        }
-    ];
+  {
+    "id": 1,
+    "name": "Luisa Hane",
+    "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+    "email": "Diana.Hayes1@hotmail.com",
+    "strideLength": 4.3,
+    "dailyStepGoal": 10000,
+    "friends": [
+      16,
+      4,
+      8
+    ]
+  },
+  {
+    "id": 16,
+    "name": "Garnett Cruickshank",
+    "address": "992 Zita Mall, North Tremainemouth MA 19312-3532",
+    "email": "Laverna47@hotmail.com",
+    "strideLength": 3.9,
+    "dailyStepGoal": 10000,
+    "friends": [
+      25,
+      31,
+      3,
+      16
+    ]
+  },
+  {
+    "id": 4,
+    "name": "Mae Connelly",
+    "address": "28926 Schinner Islands, Turnermouth NE 23720-3230",
+    "email": "Marcos_Pollich@hotmail.com",
+    "strideLength": 3.1,
+    "dailyStepGoal": 4000,
+    "friends": [
+      48,
+      7,
+      44,
+      8
+    ]
+  },
+  {
+    "id": 8,
+    "name": "Laney Abshire",
+    "address": "86416 Koch Inlet, North Kaciefurt MA 80635",
+    "email": "Janice_Nitzsche2@yahoo.com",
+    "strideLength": 2.8,
+    "dailyStepGoal": 2000,
+    "friends": [
+      11,
+      41,
+      23,
+      49
+    ]
+  },
+  {
+    "id": 25,
+    "name": "Isobel Schmeler",
+    "address": "5592 Donnelly Trail, Scottieport SC 35511",
+    "email": "Name79@yahoo.com",
+    "strideLength": 3.3,
+    "dailyStepGoal": 4000,
+    "friends": [
+      37,
+      31,
+      43
+    ]
+  },
+  {
+    "id": 31,
+    "name": "Bertrand Yundt",
+    "address": "0032 Claudia Plain, Delfinaland RI 22298-3685",
+    "email": "Sibyl.Schmidt39@yahoo.com",
+    "strideLength": 3.4,
+    "dailyStepGoal": 7000,
+    "friends": [
+      16,
+      41,
+      9
+    ]
+  },
+  {
+    "id": 3,
+    "name": "Herminia Witting",
+    "address": "85823 Bosco Fork, East Oscarstad MI 85126-5660",
+    "email": "Elwin.Tromp@yahoo.com",
+    "strideLength": 4.4,
+    "dailyStepGoal": 5000,
+    "friends": [
+      19,
+      11,
+      42,
+      33
+    ]
+  }
+];
---
+++ test/userRepository-test.js
@@ -0,0 +1,21 @@
+const chai = require('chai')
+const expect = chai.expect;
+
+// var userRepository = new UserRepository;
+
+const UserRepository = require('../src/userRepository');
+const fakeUsers = require('../fakeData/fakeUsers')
+
+describe('UserRepository', function() {
+
+  it('should be a function', function() {
+    
+    expect(UserRepository).to.be.a('function');
+  }),
+
+  it('should take an object as an arguement', function() {
+    const userRepository = new UserRepository(fakeUsers)
+
+    expect(userRepository.data).to.be.an('object')
+  })
+});

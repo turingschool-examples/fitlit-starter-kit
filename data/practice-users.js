40cb1ad93bf25b0234a0bbe96bb01d2b50798535
Brandy Mello <brandyamello@gmail.com>
2019-07-01 15:42:44 -0600

    Add practice user dataset Co-authored by: Amanda Sierra <asierralopez526@gmail.com>

---
+++ data/practice-users.js
@@ -0,0 +1,72 @@
+const practiceUserData = [
+  {
+    "id": 3,
+    "name": "Herminia Witting",
+    "address": "85823 Bosco Fork, East Oscarstad MI 85126-5660",
+    "email": "Elwin.Tromp@yahoo.com",
+    "strideLength": 4.4,
+    "dailyStepGoal": 5000,
+    "friends": [
+      19,
+      11,
+      42,
+      33
+    ]
+  },
+  {
+    "id": 4,
+    "name": "Mae Connelly",
+    "address": "28926 Schinner Islands, Turnermouth NE 23720-3230",
+    "email": "Marcos_Pollich@hotmail.com",
+    "strideLength": 3.1,
+    "dailyStepGoal": 4000,
+    "friends": [
+      48,
+      7,
+      44,
+      8
+    ]
+  },
+  {
+    "id": 5,
+    "name": "Erick Schaden",
+    "address": "514 Mayert Walk, Jordaneside SC 55023-6523",
+    "email": "Vanessa_Gerhold@gmail.com",
+    "strideLength": 3.1,
+    "dailyStepGoal": 8000,
+    "friends": [
+      13,
+      44,
+      49,
+      33,
+      10
+    ]
+  },
+  {
+    "id": 6,
+    "name": "Jerrold Bogisich",
+    "address": "8283 Carroll Harbor, Borerfort CT 69020-3448",
+    "email": "Mercedes_Zboncak53@yahoo.com",
+    "strideLength": 3.7,
+    "dailyStepGoal": 11000,
+    "friends": [
+      11,
+      48,
+      15
+    ]
+  },
+  {
+    "id": 7,
+    "name": "Breanne Fay",
+    "address": "834 Retta Knoll, Stantonland MA 71627-4121",
+    "email": "Dashawn28@gmail.com",
+    "strideLength": 2.9,
+    "dailyStepGoal": 8000,
+    "friends": [
+      12,
+      27,
+      22,
+      30
+    ]
+  }
+]

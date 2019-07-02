bd6774f662da8565730c0c1c36b56d8d0fb76aac
Ann Cerveny <cervann11@gmail.com>
2019-07-02 12:02:00 -0600

    Update tests to generate random user for testing

--- src/singleUser.js
+++ src/singleUser.js
@@ -1,6 +1,7 @@
 class User {
-  constructor(userData) {
-    this.userData = userData;
+  constructor(randomUser) {
+    this.randomUser = randomUser;
+    console.log(randomUser)
   }
 }
 
--- test/singleUser-test.js
+++ test/singleUser-test.js
@@ -3,21 +3,27 @@ const expect = chai.expect;
 
 const User = require("../src/singleUser");
 const userData = require("../data/sampleUsers");
+const randomIndex = Math.floor(Math.random() * (userData.length - 1) + 1)
+const randomUser = userData.filter(user => user.id === randomIndex)
 
-describe('User', function(){
+describe('User', function() {
+
   it('should be a function', function() {
     expect(User).to.be.a('function');
   })
+
   it('should be an instance of User', function() {
-    let user = new User();
+    let user = new User(randomUser);
     expect(user).to.be.an.instanceof(User);
   })
+
   it('should represent a single user', function() {
-    let user = new User(userData);
-    expect(user.length).to.equal();
+    let user = new User(randomUser);
+    expect(randomUser.length).to.equal(1);
   })
+
   it('should hold properties from sample data file', function() {
-    let user = new User();
-    expect(user.name).to.equal();
+    let user = new User(randomUser);
+    expect(user.name).to.equal(randomUser.name);
   })
 });

862f6ceae16e597e7f41e72d3c3c336e55b33116
Julian Enochs <julianenochs@yahoo.com>
2019-07-01 19:32:53 -0600

    Add getUserData method

--- src/userRepository.js
+++ src/userRepository.js
@@ -1,11 +1,18 @@
-// require fakeData
-
+const fakeUsers = require('../fakeData/fakeUsers');
 class UserRepository {
-    constructor() {
+  constructor(data) {
+    this.data = data;
+  }
 
-    }
-}
+  getUserData(id) {
+      return this.data.find(function(user){
+          if (user.id === id) {
+              return user
+          }
+      })
+      }
+  };
 
 if (typeof module !== 'undefined') {
-    module.exports = UserRepository;
+  module.exports = UserRepository;
 }
--- test/userRepository-test.js
+++ test/userRepository-test.js
@@ -1,12 +1,21 @@
-const chai = require('chai');
+const chai = require('chai')
 const expect = chai.expect;
 
+// var userRepository = new UserRepository;
+
 const UserRepository = require('../src/userRepository');
+const fakeUsers = require('../fakeData/fakeUsers');
 
 describe('UserRepository', function() {
 
-    it('should be a function', function() {
-        const userRepository = new UserRepository();
-        expect(UserRepository).to.be.a('function');
-    })
+  it('should be a function', function() {
+    
+    expect(UserRepository).to.be.a('function');
+  }),
+
+  it('should find the users information using their ID', function() {
+    const userRepository = new UserRepository(fakeUsers);
+    
+    expect(userRepository.getUserData(1)).to.be.eql(fakeUsers[0])
+  })
 });

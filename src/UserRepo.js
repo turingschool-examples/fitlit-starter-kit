2d107860458c3c8d30c1873f7b1cca1e9ed15129
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-07-02 15:30:31 -0600

    Complete UserRepo test and JS Co-authored-by: Greg Anderson <anderson.gregory.mail@gmail.com>

---
+++ src/UserRepo.js
@@ -0,0 +1,24 @@
+if (typeof module !== 'undefined') {
+	userData = require('../data/users-test-data')
+	user = require('./User')
+}
+
+class UserRepo{
+	constructor(){
+		this.users = userData;
+	};
+
+	findUserData(id){
+		this.users.filter(user => user.id === id); 
+	};
+
+	findAverageStepGoal(){
+		return this.users.reduce((totalSteps, user) => {
+			totalSteps += user.dailyStepGoal;
+			return totalSteps;
+		}, 0) / this.users.length;
+	};
+};
+
+
+module.exports = UserRepo;

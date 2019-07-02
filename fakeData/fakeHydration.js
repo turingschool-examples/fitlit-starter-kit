c552e632435e1aaf14687b833f50a3ce847f07f8
Chris Lane <pankokrumbs@gmail.com>
2019-07-02 14:47:50 -0600

    Add fake dataset for hydration tests
    Co-authored-by: Julian Enochs <JulianEnochs@yahoo.com>

--- fakeData/fakeHydration.js
+++ fakeData/fakeHydration.js
@@ -1,36 +1,77 @@
 const hydrationData = [{
-            "userID": 1,
-            "date": "2019/06/15",
-            "numOunces": 37
-        },
-        {
-            "userID": 3,
-            "date": "2019/06/15",
-            "numOunces": 47
-        }, 
-        {
-            "userID": 4,
-            "date": "2019/06/15",
-            "numOunces": 85
-        },
-        {
-            "userID": 8,
-            "date": "2019/06/15",
-            "numOunces": 84
-        },
-        {
-            "userID": 16,
-            "date": "2019/06/15",
-            "numOunces": 40
-        },
-        {
-            "userID": 25,
-            "date": "2019/06/15",
-            "numOunces": 40
-        },
-        {
-            "userID": 31,
-            "date": "2019/06/15",
-            "numOunces": 97
-        }
-    ]
+  "userID": 1,
+  "date": "2019/06/15",
+  "numOunces": 37
+},
+{
+  "userID": 1,
+  "date": "2019/06/16",
+  "numOunces": 69
+},
+{
+  "userID": 1,
+  "date": "2019/06/17",
+  "numOunces": 96
+},
+{
+  "userID": 1,
+  "date": "2019/06/18",
+  "numOunces": 61
+},
+{
+  "userID": 1,
+  "date": "2019/06/19",
+  "numOunces": 91
+},
+{
+  "userID": 1,
+  "date": "2019/06/20",
+  "numOunces": 50
+},
+{
+  "userID": 1,
+  "date": "2019/06/21",
+  "numOunces": 50
+},
+{
+  "userID": 1,
+  "date": "2019/06/22",
+  "numOunces": 43
+},
+{
+  "userID": 1,
+  "date": "2019/06/23",
+  "numOunces": 39
+},
+    
+{
+  "userID": 3,
+  "date": "2019/06/15",
+  "numOunces": 47
+}, 
+{
+  "userID": 4,
+  "date": "2019/06/15",
+  "numOunces": 85
+},
+{
+  "userID": 8,
+  "date": "2019/06/15",
+  "numOunces": 84
+},
+{
+  "userID": 16,
+  "date": "2019/06/15",
+  "numOunces": 40
+},
+{
+  "userID": 25,
+  "date": "2019/06/15",
+  "numOunces": 40
+},
+{
+  "userID": 31,
+  "date": "2019/06/15",
+  "numOunces": 97
+}
+]

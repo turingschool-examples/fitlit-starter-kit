22024b091305cf8096498753b98bf3999d8794a3
Julian Enochs <julianenochs@yahoo.com>
2019-07-01 20:53:53 -0600

    Update getUserData

--- src/userRepository.js
+++ src/userRepository.js
@@ -2,24 +2,28 @@ const fakeUsers = require('../fakeData/fakeUsers');
 class UserRepository {
   constructor(data) {
     this.data = data;
+    this.user = {};
   }
 
   getUserData(id) {
       return this.data.find(function(user){
-          if (user.id === id) {
-              console.log(user)
-              return user
-          }
+          return user.id === id
       })
       }
 
   compareStepGoal(steps) {
       let userCounter = 0
-      return this.data.reduce(function (totalSteps, user) {
+      let sumOfUserSteps = this.data.reduce(function (totalSteps, user) {
           userCounter++
-          console.log(userCounter)
           return totalSteps += user.dailyStepGoal
       }, 0)
+      let totalStepAverage = sumOfUserSteps / userCounter
+      if (this.user.dailyStepGoal < totalStepAverage) {
+          return `Your goal is ${totalStepAverage - this.user.dailyStepGoal} steps less than your friends!`
+      }
+      if (user.dailyStepGoal > totalStepAverage) {
+          return `Your goal is ${this.user.dailyStepGoal - totalStepAverage} steps more than your friends!`
+      }
   };
   };
 

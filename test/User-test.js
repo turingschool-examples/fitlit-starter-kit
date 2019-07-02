ed1f0d45c7afb0e0f2bed9144c67ad2351db2995
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-07-02 16:43:04 -0600

    Add test and functionality for method to return user name in user file > Co-author by: Brandy Mello <brandyamello@gmail.com>

--- test/User-test.js
+++ test/User-test.js
@@ -1,18 +1,28 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const data = require('../data/practice-users');
+const userData = require('../data/practice-users');
+
 const User = require('../src/User');
 
 describe('User', function() {
 
+  let user;
+
+  beforeEach(() => {
+
+    user = new User(userData[0]);
+  });
+
   it('should be a function', function() {
-    const user = new User(data);
     expect(User).to.be.a('function');
   });
     
   it('should be an instance of UserRepository', function() {
-    const user = new User(data);
     expect(user).to.be.an.instanceof(User);
+  });
+
+  it('should return user name', function() {
+    expect(user.returnUserName()).to.equal('Herminia');
   });
 })

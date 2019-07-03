40f7ae59a74d3ec618050b31dd04082230fe25b6
Alek <alekaker@outlook.com>
2019-07-02 19:39:54 -0600

    Add sleep methods for weekly data

--- Test/sleep-test.js
+++ Test/sleep-test.js
@@ -1,11 +1,13 @@
 const chai = require('chai');
 const expect = chai.expect;
 const sleepData = require('../data/sleep-test-data');
+const sleepData2 = require('../data/sleep-test-data-2');
 const Sleep = require('../src/sleep');
 const SleepRepository = require('../src/sleep-repository')
 
 describe("Sleep", () => {
-
+  let sleepRepository
+  let sleep1
   beforeEach(function() {
     sleepRepository = new SleepRepository(sleepData);
     sleep1 = new Sleep(sleepRepository.returnUserSleepData(1));
@@ -19,24 +21,28 @@ describe("Sleep", () => {
     expect(sleep1).to.be.an.instanceof(Sleep)
   });
 
-  it.skip("should return total average sleep quality for a user", () => {
+  it("should return total average sleep quality for a user", () => {
     expect(sleep1.userSleepAve()).to.eql(3)
   });
 
-  it.skip("should return for a user how many hours slept for a specific date", () => {
+  it("should return for a user how many hours slept for a specific date", () => {
     expect(sleep1.userHoursSleptForDay("2019/06/15")).to.eql(6.1)
   });
 
-  it.skip("should return for a user sleep quality for a specific date", () => {
+  it("should return for a user sleep quality for a specific date", () => {
     expect(sleep1.userQualitySleepForDay("2019/06/15")).to.eql(2.2)
   });
 
-  it.skip("should return hours slept for each day over the course of a week", () => {
-    expect(sleep1.userWeeklySleep("2019/06/15")).to.eql(8.1)
+  it("should return hours slept for each day over the course of a week", () => {
+    sleepRepository2 = new SleepRepository(sleepData2);
+    sleep2 = new Sleep(sleepRepository2.returnUserSleepData(1));
+    expect(sleep2.userWeeklySleep("2019/06/15")).to.eql([ 6.1, 4.1, 8, 10.4, 10.7, 9.3, 7.8 ])
   });
 
-  it.skip("should sleep quality for each day over the course of a week", () => {
-    expect(sleep1.userWeeklyQualitySleep("2019/06/15")).to.eql(2.6)
+  it("should sleep quality for each day over the course of a week", () => {
+    sleepRepository2 = new SleepRepository(sleepData2);
+    sleep2 = new Sleep(sleepRepository2.returnUserSleepData(1));
+    expect(sleep2.userWeeklyQualitySleep("2019/06/15")).to.eql([ 2.2, 3.8, 2.6, 3.1, 1.2, 1.2, 4.2 ])
   });
 
 
--- src/sleep.js
+++ src/sleep.js
@@ -2,7 +2,34 @@ class Sleep {
   constructor (currentUser) {
     this.currentUser = currentUser
   }
-  
+
+  userSleepAve() {
+    return this.currentUser.reduce((acc, total) => acc + total.sleepQuality, 0)/this.currentUser.length;
+  }
+
+  userHoursSleptForDay(specificDate) {
+    return this.currentUser.find(el => el.date === specificDate).hoursSlept;
+  }
+
+  userQualitySleepForDay(specificDate) {
+    return this.currentUser.find(el => el.date === specificDate).sleepQuality;
+  }
+
+  returnAWeek(firstDate) {
+    let data = [...this.currentUser]
+    let index = data.findIndex(el => el.date === firstDate);
+    return data.splice(index, 7);
+  }
+
+    userWeeklySleep(firstDate) {
+    let week = this.returnAWeek(firstDate);
+    return week.map(el => el.hoursSlept)
+  }
+
+  userWeeklyQualitySleep(firstDate) {
+  let week = this.returnAWeek(firstDate);
+  return week.map(el => el.sleepQuality)
+  }
 }
 
 module.exports = Sleep

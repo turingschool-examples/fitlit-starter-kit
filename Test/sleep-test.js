759ef67dcc0bc59298594792c43449250be46682
Alek <alekaker@outlook.com>
2019-07-02 19:10:04 -0600

    Add tests for methods in sleep class

--- Test/sleep-test.js
+++ Test/sleep-test.js
@@ -20,18 +20,24 @@ describe("Sleep", () => {
   });
 
   it.skip("should return total average sleep quality for a user", () => {
-    expect(sleep1.UserSleepAve()).to.be.an.instanceof(Sleep)
+    expect(sleep1.userSleepAve()).to.eql(3)
   });
 
-  it.skip("should return total average sleep quality for a user", () => {
-    expect(sleep1).to.be.an.instanceof(Sleep)
+  it.skip("should return for a user how many hours slept for a specific date", () => {
+    expect(sleep1.userHoursSleptForDay("2019/06/15")).to.eql(6.1)
   });
 
+  it.skip("should return for a user sleep quality for a specific date", () => {
+    expect(sleep1.userQualitySleepForDay("2019/06/15")).to.eql(2.2)
+  });
 
+  it.skip("should return hours slept for each day over the course of a week", () => {
+    expect(sleep1.userWeeklySleep("2019/06/15")).to.eql(8.1)
+  });
 
-
-
-
+  it.skip("should sleep quality for each day over the course of a week", () => {
+    expect(sleep1.userWeeklyQualitySleep("2019/06/15")).to.eql(2.6)
+  });
 
 
 });

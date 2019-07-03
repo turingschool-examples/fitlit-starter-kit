3ff5dd9603ada1664d9f48c429ddb1fa9b8c1402
Eric O'Neill <oneill.eric23@gmail.com>
2019-07-03 17:12:18 -0600

    Complete first method on activity
    Co-authored-by: Jon O'Drobinak <jodrobinak@gmail.com>

---
+++ data/activitySub.js
@@ -0,0 +1,76 @@
+const activityData = [
+    {
+      "userID": 1,
+      "date": "2019/06/15",
+      "numSteps": 3577,
+      "minutesActive": 140,
+      "flightsOfStairs": 16
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/16",
+      "numSteps": 4294,
+      "minutesActive": 138,
+      "flightsOfStairs": 10
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/17",
+      "numSteps": 7402,
+      "minutesActive": 116,
+      "flightsOfStairs": 33
+    },
+    {
+      "userID": 4,
+      "date": "2019/06/15",
+      "numSteps": 3486,
+      "minutesActive": 114,
+      "flightsOfStairs": 32
+    },
+    {
+      "userID": 5,
+      "date": "2019/06/15",
+      "numSteps": 11374,
+      "minutesActive": 213,
+      "flightsOfStairs": 13
+    },
+    {
+      "userID": 6,
+      "date": "2019/06/15",
+      "numSteps": 14810,
+      "minutesActive": 287,
+      "flightsOfStairs": 18
+    },
+    {
+      "userID": 7,
+      "date": "2019/06/15",
+      "numSteps": 2634,
+      "minutesActive": 107,
+      "flightsOfStairs": 5
+    },
+    {
+      "userID": 8,
+      "date": "2019/06/15",
+      "numSteps": 10333,
+      "minutesActive": 114,
+      "flightsOfStairs": 31
+    },
+    {
+      "userID": 9,
+      "date": "2019/06/15",
+      "numSteps": 6389,
+      "minutesActive": 41,
+      "flightsOfStairs": 33
+    },
+    {
+      "userID": 10,
+      "date": "2019/06/15",
+      "numSteps": 8015,
+      "minutesActive": 106,
+      "flightsOfStairs": 37
+    }
+]
+
+if (typeof module !== 'undefined') {
+    module.exports = activityData;
+  }

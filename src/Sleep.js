452ba9345907f63a1d74f0b59360c7bb7ea36c9f
Alyssa Lundgren <lundgrea@gmail.com>
2019-07-02 18:43:08 -0600

    add sleep file to git

---
+++ src/Sleep.js
@@ -0,0 +1,74 @@
+class Sleep {
+  constructor(sleepData) {
+    this.sleepData = sleepData
+  }
+
+   findIdHelper(id) {
+    let specificUserIntake = this.sleepData.filter(function(object) {
+      return object.userID === id
+    })
+    return specificUserIntake;
+  }
+  calculateAverageDailySleepHours(id) {
+    let userSleepData = this.findIdHelper(id).map(function(obj){
+      return obj.hoursSlept
+    }) 
+    let totalHoursSlept = userSleepData.reduce(function(acc, curVal){
+      return acc = acc + curVal
+    }, 0 )
+    let averageHourSlept = totalHoursSlept / userSleepData.length
+    return averageHourSlept 
+    }
+
+    calculateAverageSleepQuality(id) {
+    let userSleepData = this.findIdHelper(id).map(function(obj){
+      return obj.sleepQuality
+    }) 
+    let totalSleepQuality = userSleepData.reduce(function(acc, curVal){
+      return acc = acc + curVal
+    }, 0 )
+    let averageSleepQuality = totalSleepQuality / userSleepData.length
+    return averageSleepQuality 
+    }
+
+    displayHoursSlept(id, date){
+      let dateOfUserIntake = this.findIdHelper(id).find(function(obj) {
+      return obj.date === date
+      });
+      return dateOfUserIntake.hoursSlept;
+    }
+
+    displaySleepQuality(id, date) {
+      let dateOfUserIntake = this.findIdHelper(id).find(function(obj) {
+      return obj.date === date
+      });
+      return dateOfUserIntake.sleepQuality;
+    }
+
+    displayWeeklySleep(id) {
+      let user = this.findIdHelper(id)
+      let userWeeklySleep = user.map(function(obj){
+        return obj.hoursSlept
+      })
+      if (userWeeklySleep.length >= 7) {
+      userWeeklySleep.shift();
+      }
+      return userWeeklySleep;
+    }
+
+    displayWeeklySleepQuality(id) {
+      let user = this.findIdHelper(id)
+      let userWeeklySleepQuality = user.map(function(obj){
+        return obj.sleepQuality
+      })
+      if (userWeeklySleepQuality.length >= 7) {
+      userWeeklySleepQuality.shift();
+      }
+      return userWeeklySleepQuality;
+    }
+
+
+}
+
+
+module.exports = Sleep;

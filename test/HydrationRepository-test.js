0e3cf3ce83a634774c4adb5d019c5ba433ff93dc
dawnlunacy <alanturing@example.com>
2019-07-04 14:57:20 -0600

    Add tests for HydrationUser-test.js

--- test/HydrationRepository-test.js
+++ test/HydrationRepository-test.js
@@ -25,25 +25,55 @@ describe('HydrationRepository', function() {
 
     it('should store the id correlated with the user trying to be accessed', function() {
         expect(newHydrationUserRepo.id).to.equal(2);
-      });
+    });
       
      describe('findHydrationUser', () =>
         it('should be able to find all instances of the user based on the id it stored', function() {
             expect(newHydrationUserRepo.findHydrationUser()).to.eql(
-            [ {
+            [  {
+                "userID": 2,
+                "date": "2019/06/14",
+                "numOunces": 39
+              },
+              {
                 "userID": 2,
                 "date": "2019/06/15",
                 "numOunces": 75
             },
             {
                 "userID": 2,
-                "date": "2019/06/15",
+                "date": "2019/06/16",
                 "numOunces": 47
-            }])
+            },
+            {
+                "userID": 2,
+                "date": "2019/06/17",
+                "numOunces": 96
+              },
+              {
+                "userID": 2,
+                "date": "2019/06/18",
+                "numOunces": 61
+              },
+              {
+                "userID": 2,
+                "date": "2019/06/19",
+                "numOunces": 91
+              },
+               {
+                "userID": 2,
+                "date": "2019/06/20",
+                "numOunces": 50
+              },
+              {
+                "userID": 2,
+                "date": "2019/06/21",
+                "numOunces": 50
+              }])
         }));
 
     describe('findHydrationAverage', () =>
         it('should find the average fluid ounces a user consumed per day for all time', function () {
-            expect(newHydrationUserRepo.findHydrationAverage()).to.equal(61)
+            expect(newHydrationUserRepo.findHydrationAverage()).to.equal(63)
         }));
 });

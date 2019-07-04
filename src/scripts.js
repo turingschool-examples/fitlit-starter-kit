fcca3c5f8e8d0a4e8fb3b3ba42430d72c5ae9f60
Alan Turing <freemasl.sf@gmail.com>
2019-07-03 22:18:52 -0600

    Add test for an additional functionality on the DOM

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
   const userRepo = new UserRepository(userData);
-  const specificUser = userRepo.returnUserData(1);
+  let userID = Math.floor((Math.random() * 50) + 1);
+  const specificUser = userRepo.returnUserData(userID);
   const user = new User(specificUser);
+  const hydrationRepo = new HydrationRepository(hydrationData);
   
-  $('#js-first-name').html(user.returnFirstName())
-  $('#js-full-name').html(user.name)
+  console.log(userID);
   
-  //
+  $('#js-first-name').html(user.returnFirstName());
+  $('#js-full-name').html(user.name);
+
+  let foundUser = hydrationRepo.returnUserHydrationData(userID);
+  const hydration = new Hydration(foundUser);
+  $('#js-water-consumed-today').html(hydration.returnFluidOunces("2019/06/15"));
+
 
 
 

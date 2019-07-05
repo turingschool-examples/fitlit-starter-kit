54904e7a85ad450fa4f0bf120bfe42f4f2be56b7
Alan Turing <freemasl.sf@gmail.com>
2019-07-05 12:12:04 -0600

    Add button to change user

--- src/scripts.js
+++ src/scripts.js
@@ -1,22 +1,27 @@
 $(document).ready(function() {
-  const userRepo = new UserRepository(userData);
-  let userID = Math.floor((Math.random() * 50) + 1);
-  const specificUser = userRepo.returnUserData(userID);
-  const user = new User(specificUser);
-  const hydrationRepo = new HydrationRepository(hydrationData);
+  // const hydrationRepo = new HydrationRepository(hydrationData);
   
-  console.log(userID);
   
-  $('#js-first-name').html(user.returnFirstName());
-  $('#js-full-name').html(user.name);
 
-  let foundUser = hydrationRepo.returnUserHydrationData(userID);
-  const hydration = new Hydration(foundUser);
-  $('#js-water-consumed-today').html(hydration.returnFluidOunces("2019/06/15"));
+  $('#js-change-user').click(function() {
+    const userRepo = new UserRepository(userData);
+    let userID = Math.floor((Math.random() * 50) + 1);
+    const specificUser = userRepo.returnUserData(userID);
+    const user = new User(specificUser);
+    $('#js-first-name').html(user.returnFirstName());
+    $('#js-full-name').html(user.name);
+    console.log(userID);
+  });
 
-  $('.nav-list').on('click', () => {
-    $('.list-item').addClass('.list-item-active');
-  })
+  
+
+  // let foundUser = hydrationRepo.returnUserHydrationData(userID);
+  // const hydration = new Hydration(foundUser);
+  // $('#js-water-consumed-today').html(hydration.returnFluidOunces("2019/06/15"));
+
+  // $('.nav-list').on('click', () => {
+  //   $('.list-item').addClass('.list-item-active');
+  // })
 
 
 

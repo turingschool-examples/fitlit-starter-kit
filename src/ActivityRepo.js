63e4058d7ece2f31e328392dae0f54e9b41f7ba3
Amy Rippeto <amyrippeto@gmail.com>
2019-07-05 14:27:25 -0600

    Add methods to activityRepo class
    Co-authored-by: Katherine Williams <kawilliams8@gmail.com>

--- src/ActivityRepo.js
+++ src/ActivityRepo.js
@@ -5,7 +5,35 @@ class ActivityRepo {
 
   returnUserActivityData(id) {
     return this.userData.filter(user => user.userID === id);
+  }
+
+  findAllRecordsForGivenDate(date) {
+    return this.userData.filter(record => record.date === date);
   } 
+
+  avgNumStairsClimbedGivenDate(date) {
+    let records = this.findAllRecordsForGivenDate(date);
+    return +(records.reduce((acc, record) => {
+      acc += record.flightsOfStairs; 
+        return acc;
+    }, 0) / records.length).toFixed(2);
+  }
+
+  avgNumStepsTakenGivenDate(date) {
+    let records = this.findAllRecordsForGivenDate(date);
+    return +(records.reduce((acc, record) => {
+      acc += record.numSteps; 
+        return acc;
+    }, 0) / records.length).toFixed(2);
+  }
+
+  avgNumMinActiveGivenDate(date) {
+  let records = this.findAllRecordsForGivenDate(date);
+    return +(records.reduce((acc, record) => {
+      acc += record.minutesActive; 
+        return acc;
+    }, 0) / records.length).toFixed(2);
+  }
 
 }
 

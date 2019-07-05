94f08fe344b85082c07056b6da1bcb8c647d10eb
Katie Williams <kawilliams8@gmail.com>
2019-07-05 09:37:13 -0600

    Add second round of Sleep and SleepRepo tests and methods
    Co-authored-by: Amy Rippeto <amyrippeto@gmail.com>

--- src/Sleep.js
+++ src/Sleep.js
@@ -13,6 +13,34 @@ class Sleep {
   returnUserSleepHoursDate(date) {
     return this.userData.find(record => record.date === date).hoursSlept;
   }
+
+  returnUserSleepQualityDate(date) {
+    return this.userData.find(record => record.date === date).sleepQuality;
+  }
+
+  returnUserSleepHoursWeek(date) {
+    let daySeven = this.userData.findIndex(record => record.date === date);
+    let dayOne = daySeven - 6;
+
+    return this.userData.reduce((acc, record, index) => {
+      if (index <= daySeven && index >= dayOne) {
+        acc.push({ ['x']: record.date, ['y']: record.hoursSlept })
+      }
+        return acc;
+    }, []);
+  }
+
+  returnUserSleepQualityWeek(date) {
+    let daySeven = this.userData.findIndex(record => record.date === date);
+    let dayOne = daySeven - 6;
+
+    return this.userData.reduce((acc, record, index) => {
+      if (index <= daySeven && index >= dayOne) {
+        acc.push({ ['x']: record.date, ['y']: record.sleepQuality })
+      }
+      return acc;
+    }, []);
+  }
   
 }
 
--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -12,7 +12,7 @@ describe('Sleep', () => {
   beforeEach(() => {
     sleepRepo = new SleepRepo(sampleSleepData);
     sleep = new Sleep(sleepRepo.returnUserData(2));
-    givenDate = "2019/06/19";
+    givenDate = "2019/06/23";
   });
 
   it('should be a function', () => {
@@ -31,6 +31,21 @@ describe('Sleep', () => {
   it('should return a given user\'s hours slept for a given date', () => {
     let hours = sleep.returnUserSleepHoursDate(givenDate);
     expect(hours).to.equal(7);
+  });
+
+  it('should return a given user\'s sleep quality for a given date', () => {
+    let quality = sleep.returnUserSleepQualityDate(givenDate);
+    expect(quality).to.equal(2);
+  });
+
+  it('should return the hours slept for a user in a given week', () => {
+    let hours = sleep.returnUserSleepHoursWeek(givenDate);
+    expect(hours[0]['y']).to.equal(7);
+  });
+
+  it('should return the sleep quality for a user in a given week', () => {
+    let hours = sleep.returnUserSleepQualityWeek(givenDate);
+    expect(hours[0]['y']).to.equal(2);
   });
 
 })

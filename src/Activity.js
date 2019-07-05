75fdbf2c50cc9116594a8f1e8729417bf44da966
Eric O'Neill <oneill.eric23@gmail.com>
2019-07-05 12:34:18 -0600

    Finalize returnMinutesActive Activity method and test

--- src/Activity.js
+++ src/Activity.js
@@ -8,13 +8,22 @@ class Activity {
     this.data = filePath;
   }
 
-  returnMilesWalked(date) {
-    let users = userData.map((user) => {
+  instantiateUsers() {
+    return userData.map((user) => {
       return new User(user);
     });
-    let correctUser = users.find((user) => {
+  }
+
+  findCorrectUser() {
+    let users = this.instantiateUsers();
+    return users.find((user) => {
       return user.id === this.userID;
-    })
+    });
+  }
+
+  returnMilesWalked(date) {
+    let users = this.instantiateUsers();
+    let correctUser =  this.findCorrectUser();
     let correctUserDate = this.data.filter((user) => {
       return user.date === date;
     });
@@ -24,6 +33,19 @@ class Activity {
     let stepsPerMile = 5280 / correctUser.strideLength;
     return Number(((user.numSteps / stepsPerMile).toFixed(2)))
   }
+
+  returnMinutesActive(date) {
+    let users = this.instantiateUsers();
+    let correctUser =  this.findCorrectUser();
+    let correctUserDate = this.data.filter((user) => {
+        return user.date === date;
+      });
+    let user = correctUserDate.find((user) =>{
+        return user.userID === correctUser.id
+    });
+    return user.minutesActive;
+  }
 }
+
 
 module.exports = Activity
--- test/Activity-test.js
+++ test/Activity-test.js
@@ -14,8 +14,18 @@ describe("Activity", function() {
       expect(activity).to.be.an.instanceof(Activity);
     });
 
-    it("return the miles a user has walked", function() {
+    it("should return the miles a user has walked", function() {
         const activity = new Activity(1);
         expect(activity.returnMilesWalked("2019/06/15")).to.equal(2.91);
       });
+
+    it("should return the minutes active on a given day", function() {
+        const activity = new Activity(1);
+        expect(activity.returnMinutesActive("2019/06/15")).to.equal(140);
+    });
+
+    it("should return the active minutes for a give week", function() {
+        const activity = new Activity(1);
+        expect(activity.returnMinutesActive("2019/06/15")).to.equal(140);
+    });
 });

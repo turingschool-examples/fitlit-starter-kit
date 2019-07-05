6f2e82a5d7d77708313db1c26b8d75179dd75c63
Brianna DelValle <briannadelvalle@gmail.com>
2019-07-05 11:27:47 -0600

    Complete writing activity repo tests and functionality

--- src/activity-repository.js
+++ src/activity-repository.js
@@ -2,8 +2,45 @@ class ActivityRepository {
     constructor(data) {
         this.data = data;
     }
-}
 
+    returnAvgStairs(date) {
+        let dateArray = this.data.filter((user) => user.date === date);
+        let avgStairs = dateArray.reduce((acc, day) => {
+            return acc += day.flightsOfStairs;
+        }, 0) / dateArray.length;
+        return avgStairs;
+    }
+
+    returnAvgSteps(date) {
+        let dateArray = this.data.filter((user) => user.date === date);
+        let avgSteps = dateArray.reduce((acc, day) => {
+            return acc += day.numSteps;
+        }, 0) / dateArray.length;
+        return avgSteps;
+    }
+
+    returnAvgMins(date) {
+        let dateArray = this.data.filter((user) => user.date === date);
+        let avgMins = dateArray.reduce((acc, day) => {
+            return acc += day.minutesActive;
+        }, 0) / dateArray.length;
+        return avgMins;
+    }
+
+    returnTopStepper(date) {
+        let currentDayUsers = this.data.filter(day => day.date === date)
+        let currentTopStepper = currentDayUsers.reduce((acc, user) => {
+        if(!acc.numSteps) {
+            acc.numSteps = 0;
+        }
+        if(user.numSteps > acc.numSteps) {
+            acc = user;
+        }
+        return acc;
+        }, {})
+        return currentTopStepper
+    }
+}
 
 if (typeof module !== 'undefined') {
     module.exports = ActivityRepository;

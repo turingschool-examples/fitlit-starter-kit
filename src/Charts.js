eb8a00c796c2c27d10cf3f22181aaf2a5d088d1d
Eric O'Neill <oneill.eric23@gmail.com>
2019-07-06 13:36:18 -0600

    Begin building out both charts in Activity section
    Co-authored-by: Jon O'Drobinak <jodrobinak@gmail.com>

--- src/Charts.js
+++ src/Charts.js
@@ -1,20 +1,53 @@
 let stepGoalChart = new Chart($('.activity__chart-day-allUsers'), {
     type: 'bar',
     data: {
-      labels: ['Personal Step Goal', 'The Average'],
-      datasets: [{
-        label: 'Step Goal',
-        data: [],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-        ],
-        borderWidth: 1
-        }]
-      },
-        beginAtZero: true
+      labels: ["Number of steps", "Minutes active", "Flights of stairs climbed"],
+      datasets: [
+        {
+          label: "Your stats",
+          backgroundColor: "#3e95cd",
+          data: []
+        }, {
+          label: "The average stats of all users",
+          backgroundColor: "#8e5ea2",
+          data: []
+        }
+      ]
+    },
+    options: {
+    title: {
+        display: true,
+        text: 'Your stats compared to all users'
+      }
+    }
+  });
+
+  new Chart($(".activity__chart-weeklyView-oneUser"), {
+    type: 'line',
+    data: {
+      labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+      datasets: [{ 
+          data: [],
+          label: "Step count",
+          borderColor: "#3e95cd",
+          fill: false
+        }, { 
+          data: [],
+          label: "Flights of stairs climbed",
+          borderColor: "#8e5ea2",
+          fill: false
+        }, { 
+          data: [],
+          label: "Minutes active",
+          borderColor: "#3cba9f",
+          fill: false
+        }   
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Weekly overview'
+      }
+    }
   });

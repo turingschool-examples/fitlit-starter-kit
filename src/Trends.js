0369ecd009bd0baad243b16591c011a594aaa02e
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-07-08 17:01:28 -0600

    All activity tests passing again

---
+++ src/Trends.js
@@ -0,0 +1,31 @@
+if (typeof module !== 'undefined') {
+    sleepData = require('../data/sleep-test-data');
+	userData = require('../data/users-test-data')
+	user = require('./User')
+  } 
+
+
+class Trends {
+	constructor(){
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  if (typeof module !== 'undefined') {
+    module.exports = Trends;
+  }

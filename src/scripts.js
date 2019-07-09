0314f2b28c9e25280212fdbee9454dffa44edadb
Brianna DelValle <briannadelvalle@gmail.com>
2019-07-09 16:01:17 -0600

    Make user card and header to have only app name

--- src/scripts.js
+++ src/scripts.js
@@ -37,10 +37,7 @@ $(document).ready(function() {
 
   $(".user-name").text(currentUser.returnFirstName())
   $(".user-steps").text(currentUser.dailyStepGoal)
-  $(".avg-step-goal").text(globalRepo.returnAvgStepGoal())
-  $("li").eq(0).text(currentUser.email)
-  $("li").eq(1).text(currentUser.dailyStepGoal)
-  $("li").eq(2).text(currentUser.strideLength)
+  $(".user-email").eq(0).text(currentUser.email)
   $(".main__section--friends--challenge").html(friendsStepChallenge())
   $('.main__section--daily-intake').text(currentHydration.returnIntakeByDay("2019/06/23"))
   $('.main__section--average-intake').text(currentHydration.returnDailyAverage())

748774b8e7e24b137bd7bef74bff605ac9402c0b
Brandy Mello <brandyamello@gmail.com>
2019-07-09 15:13:11 -0600

    Complete functionality for wklyStepsChallenge Co-authored by: Amanda Sierra <asierralopez526@gmail.com>

--- src/Activity.js
+++ src/Activity.js
@@ -102,32 +102,19 @@ class Activity {
   }
 
   wklyStepsChallenge(id, day) {
-    console.log(userData.find((obj) => id === obj.id));
     let friendsFound = userData.find((obj) => id === obj.id).friends;
     friendsFound.unshift(id);
-    friendsFound.map((obj) => {
-      userData.find((user) => obj === user.id);
-    });
-
-    console.log(friendsFound)
-
-    // friendsFound.map(friend => {
-    //   let wklyStepComp = {};
-    //   let names = [];
-    //   friendsFound.forEach(id => {
-    //     user.returnUserName();
-    //    friend.forEach((id) => {
-    //       wklyStepComp['name'] = 
-    //       // console.log(id);
-    //    })
-
+    let friendObjs = userData.filter(user => friendsFound.includes(user.id));
+    return friendObjs.map(person => {
+        let personWklySteps = {};
+        personWklySteps.id = person.id;
+        personWklySteps.name = person.name.split(' ')[0];
+        personWklySteps.wklyStepTotal = this.dailyStepsPerWeek(person.id, day).reduce((acc, steps) => {
+          return acc + steps;
+        }, 0);
+        return personWklySteps;
+      });
     }
-    // let currentDay = this.activityData.find((date) => date.date === day);
-    // let user = userData.find((x) => x.id === currentDay.userID);
-    // let stepsPerWeek = this.dailyStepsPerWeek(id, day).reduce((acc, steps) => {
-    //   return acc + steps;
-    // }, 0);
-  // }
 
   averageActivity(day, el) {
     let findDay = this.activityData.filter((obj) => obj.date === day);
@@ -137,9 +124,6 @@ class Activity {
     }, 0);
     return Math.round(sumTotal / findElementArr.length);
   }
-
-
-
 } 
 
 if (typeof module !== 'undefined') {

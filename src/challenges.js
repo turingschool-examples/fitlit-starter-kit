069a2cfbc430e2bdab5393c1bbb4991b696529b1
Chris Lane <pankokrumbs@gmail.com>
2019-07-08 21:50:37 -0600

    Add a template for Challenger class

--- src/challenges.js
+++ src/challenges.js
@@ -12,12 +12,35 @@ class Challenges {
     let friendIds = userObject.friends
     return this.data.reduce((acc, user) => {
       friendIds.forEach(id => {
-        if( id === user.id) {
+        if ( id === user.id) {
           acc.push(user)
         }
       })
       return acc
     }, [])
+  }
+  getWeeklySteps(dataSet, date, idArr) {
+  //  console.log(dataSet);
+  //  console.log(date);
+  //  console.log(idArr);
+   
+    let test = idArr.map(friend => {
+      let arr = dataSet.filter(user => 
+        user.userID === friend.id
+      )
+      return arr
+    })
+    let testMap = test.map((array) => {
+       return array.find(obj => {
+         return obj.date === date
+      })
+    })
+    return testMap.forEach(el => {
+      let index = dataSet.findIndex(obj => {
+        el.date === obj.date && el.userID === obj.userID})
+        console.log(index);
+      var week = this.object.slice(index - 6, index + 1).reverse()
+    })
   }
 
 

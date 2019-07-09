e37c54dec8b695cd197545852a1741e374b2b889
Ann Cerveny <cervann11@gmail.com>
2019-07-08 20:56:30 -0600

    Structure basic css and img in header

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,14 @@
-// $(document).ready(function() {
-//     $('.greeting').text(${user.randomUser.returnFirstName()})
-// })
+$(document).ready(function() {
+    const randomIndex = Math.floor(Math.random() * (userData.length - 1) + 1)       
+    const randomUser = userData.find(user => user.id === randomIndex)
+    const user = new User(randomUser);
+    const users = new UserRepo(userData);
+    const hydro = new Hydration(hydrationData);
+    const sleep = new Sleep(sleepData);
+    const activity = new Activity(activityData);
+    $('.user-name').text(`${user.returnFirstName(randomUser)}`)
+})
+
+$(document).ready(function() {
+
+})

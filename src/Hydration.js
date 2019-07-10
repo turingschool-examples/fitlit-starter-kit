c28ea20f952ec19f45062d6ecba01da9de742176
Eric O'Neill <oneill.eric23@gmail.com>
2019-07-10 10:18:33 -0600

    Lint Hydration.js

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,69 +1,69 @@
 if (typeof module !== "undefined") {
-hydrationFilePath = require("../data/HydrationSub");
+  hydrationFilePath = require("../data/HydrationSub");
 } else {
   hydrationFilePath = hydrationData;
 }
 
-  class Hydration {
-    constructor(userID) {
-      this.userID = userID,
-      this.data = hydrationFilePath;
-    }
-  
-    returnAllTimeAvg(userID) {
-      let users = this.data.filter(function(user) {
-          return user.userID === userID;
-      });
-      return Math.floor(Number(users.reduce(function(totalOunces, user) {
-          return totalOunces += user.numOunces;
-      }, 0) / users.length));
-    }
-
-    returnSpecificDayOz(date) {
-      let users = this.data.filter(user => {
-        return user.userID === this.userID;
-      });
-      return users.reduce(function(acc, user) {
-        if (user.date === date) {
-          acc += user.numOunces;
-        }
-        return Number(acc);
-      }, 0);
-    }
-
-    returnWaterEachDay(userID) {
-      return this.data.reduce(function(totalDays, user) {
-        if (totalDays.length < 8 && user.userID === userID) {
-          totalDays.push(user.numOunces);
-        };
-        return totalDays;
-      }, []);
-    }
-
-    returnWeekInfo(startDate, endDate) {
-      let firstIndex = this.data.findIndex(el => {
-        return el.date === startDate;
-      });
-      let allDates = this.data.reduce((dates, user) => {
-        dates.push(user.date)
-        return dates
-      }, [])
-      let lastIndex = allDates.lastIndexOf(endDate);
-      return this.data.slice(firstIndex, lastIndex)
-    }
-
-    returnWeeklyHydration(startDate, endDate) {
-      let correctWeek = this.returnWeekInfo(startDate, endDate);
-      let correctUserData = correctWeek.filter(user => {
-        return user.userID === this.userID
-      })
-      return correctUserData.map(user => {
-        return user.numOunces;
-      })
-    }
-  
-  }  
-  
-  if (typeof module !== 'undefined') {
-    module.exports = Hydration;
+class Hydration {
+  constructor(userID) {
+    this.userID = userID,
+    this.data = hydrationFilePath;
   }
+  
+  returnAllTimeAvg(userID) {
+    let users = this.data.filter(function(user) {
+      return user.userID === userID;
+    });
+    return Math.floor(Number(users.reduce(function(totalOunces, user) {
+      return totalOunces += user.numOunces;
+    }, 0) / users.length));
+  }
+
+  returnSpecificDayOz(date) {
+    let users = this.data.filter(user => {
+      return user.userID === this.userID;
+    });
+    return users.reduce(function(acc, user) {
+      if (user.date === date) {
+        acc += user.numOunces;
+      }
+      return Number(acc);
+    }, 0);
+  }
+
+  returnWaterEachDay(userID) {
+    return this.data.reduce(function(totalDays, user) {
+      if (totalDays.length < 8 && user.userID === userID) {
+        totalDays.push(user.numOunces);
+      }
+      return totalDays;
+    }, []);
+  }
+
+  returnWeekInfo(startDate, endDate) {
+    let firstIndex = this.data.findIndex(el => {
+      return el.date === startDate;
+    });
+    let allDates = this.data.reduce((dates, user) => {
+      dates.push(user.date)
+      return dates
+    }, [])
+    let lastIndex = allDates.lastIndexOf(endDate);
+    return this.data.slice(firstIndex, lastIndex)
+  }
+
+  returnWeeklyHydration(startDate, endDate) {
+    let correctWeek = this.returnWeekInfo(startDate, endDate);
+    let correctUserData = correctWeek.filter(user => {
+      return user.userID === this.userID
+    })
+    return correctUserData.map(user => {
+      return user.numOunces;
+    })
+  }
+  
+}  
+  
+if (typeof module !== 'undefined') {
+  module.exports = Hydration;
+}

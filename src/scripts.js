8df3c547bd108033b18e6e35b1f21cc4560f2965
Jessie Le-Ho <jessiele-ho@Jessies-MacBook-Pro.local>
2019-07-10 19:21:50 -0600

    Finish CSS

--- src/scripts.js
+++ src/scripts.js
@@ -49,7 +49,7 @@ cardFour.addEventListener( 'click', function() {
 const userVsAvg = new Chart($('#user-vs-avg-steps'), {
     type: 'bar',
     data: {
-      labels: ['Your Steps', 'Average User Steps'],
+      labels:['Your Steps', 'Avg User Steps'],
       datasets: [{
         label: 'Steps',
         data: [user.randomUser.dailyStepGoal, users.averageGoalSteps()],
@@ -58,26 +58,41 @@ const userVsAvg = new Chart($('#user-vs-avg-steps'), {
           'rgba(255, 159, 64, 0.2)'
         ],
         borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(255, 159, 64, 1)'
+          '#ef1de7',
+          '#afff14'
         ],
         borderWidth: 1
       }]
     },
     options: {
-      legend: {display: false},
-      title: {
-        display: true,
-        text: 'You VS Everyone (steps)'
-      },
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      }
-    }
+     legend: { display: false,
+       labels: {
+         fontColor: "black",
+         fontSize: 20
+       }
+     },
+     title: {
+       display: true,
+       fontColor: 'black',
+       text: 'You VS Everyone (steps)'
+     },
+     scales: {
+       yAxes: [{
+         ticks: {
+          beginAtZero: true,
+           fontColor: 'black',
+           fontSize: 16
+         }
+       }],
+       xAxes: [{
+         ticks: {
+          beginAtZero: true,
+           fontColor: 'black',
+           fontSize: 16
+         }
+       }],
+     }
+   }
 });
 
 const weeklyWaterIntake = new Chart($('#weekly-water-intake'), {
@@ -88,34 +103,35 @@ const weeklyWaterIntake = new Chart($('#weekly-water-intake'), {
             label: 'Your Weekly Water Intake (fl Oz)',
             data: Object.values(hydro.userFluidsPerWeek(randomIndex, today)),
             backgroundColor: [                
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(90, 173, 226, 0.2)'
-                
+            'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(90, 173, 226, 1)'
+                '#afff14'
             ],
             borderWidth: 1
         }]
     },
     options: {
+      legend: { display: true,
+       labels: {
+         fontColor: "black",
+         fontSize: 14
+       }
+     },
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero: true
+                    beginAtZero: true,
+                    fontColor: 'black',
                 }
-            }]
+            }],
+            xAxes: [{
+         ticks: {
+          beginAtZero: true,
+           fontColor: 'black',
+           fontSize: 16
+         }
+       }],
         }
     }
 });

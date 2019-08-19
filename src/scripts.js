4e7b4c03568296fb00c7fc28595c982bac9cf4f1
Roger Hughes <Rogerallenhughes@hotmail.com>
2019-08-19 17:44:22 -0600

    Add dom manipulation for user info
    co-authored-by: Sam Coleman <smcoleman19@gmail.com>

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,32 @@
 console.log("Hello World");
+// const chai = require('chai');
+// const expect = chai.expect;
+// const UserRepository = require('./userRepository.js');
+// const data = require('../data/users.js');
+// const User = require('../src/user.js')
+
+/***************** Variables *****************/
+let name = document.querySelector('.main_user_name');
+let address = document.querySelector('.main_user_address');
+let email = document.querySelector('.main_user_email');
+let stride = document.querySelector('.main_user_strideLength');
+let goal = document.querySelector('.main_user_dailyStepGoal');
+let friends = document.querySelector('.main_user_friends');
+
+/*************** Event Listeners *************/
+window.addEventListener('load', initializePage(userData))
+/***************** Functions *****************/
+
+function initializePage(data) {
+	const userRepository = new UserRepository(data)
+	userRepository.findUser(10)
+	const user = new User(userRepository.currentUser)
+	name.innerHTML = user.name
+	address.innerHTML = user.address
+	email.innerHTML = user.email
+	stride.innerHTML = user.strideLength
+	// console.log(user.dailyStepGoal)
+	goal.innerHTML = user.dailyStepGoal
+	friends.innerHTML = user.friends
+
+}
--- src/user.js
+++ src/user.js
@@ -5,6 +5,7 @@ class User {
 		this.address = userData.address
 		this.email = userData.email
 		this.strideLength = userData.strideLength
+		this.dailyStepGoal = userData.dailyStepGoal
 		this.friends = userData.friends
 	}
 	firstName() {

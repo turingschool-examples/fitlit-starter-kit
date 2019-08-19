fdee97c2723d997dfb535051ca1a6ccf126223c2
Chris Basham <chrisdbasham0317@gmail.com>
2019-08-19 16:53:30 -0600

    Class and testing for user-repo

--- test/user-repository-test.js
+++ test/user-repository-test.js
@@ -1,15 +1,25 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const users = require('../user)
-const UserRepository = require('../src/UserRepository');
+const data = require('../test/User-data-subset');
+const UserRepository = require('../src/User-repository');
+
 
 describe('UserRepository', () => {
   it('should load data for all users', function () {
-    const
+    const userRepository = new UserRepository(data);
+    expect(userRepository.users.length).to.equal(5);
+  });
+
+  it('should return a user based on user id', () => {
+    const userRepository = new UserRepository(data);
+    expect
+    (userRepository.returnUser(2)).to.deep.equal(data[1])
   })
 
-
-
-})
+  it('should return the average of all users step goal', () => {
+    const userRepository = new UserRepository(data);
+    expect(userRepository.calculateStepGoal()).to.equal(6400)
+  })
+});
 

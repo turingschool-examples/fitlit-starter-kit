f7c768068a0f0a9533974e311e02c90324fd7ac2
Chris Basham <chrisdbasham0317@gmail.com>
2019-08-19 16:52:21 -0600

    Class and testing for user-repo

--- src/user-repository.js
+++ src/user-repository.js
@@ -2,6 +2,16 @@ class UserRepository {
   constructor(data) {
     this.users = data;
   }
+  returnUser(userId) {
+    let foundUser = this.users.find(user => user.id === userId)
+    return foundUser
+  }
+
+  calculateStepGoal() {
+    let goal = 0;
+    let stepGoal = this.users.forEach(user => goal += user.dailyStepGoal)
+    return goal / this.users.length
+  }
 
 }
 

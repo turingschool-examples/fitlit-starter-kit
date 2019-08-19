f54837209618bcf8e5ccc270d471338c4372bf48
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-08-19 17:51:51 -0600

    Add User class and tesing
    Co-Authored by: Vanessa Randall @vrandall66

--- src/User.js
+++ src/User.js
@@ -1,6 +1,15 @@
 class User {
-  constructor(data) {
-      
+  constructor(userData) {
+    this.id = userData.id;
+    this.name = userData.name;
+    this.address = userData.address;
+    this.email = userData.email;
+    this.strideLength = userData.strideLength;
+    this.friends = userData.friends;
+  }
+
+  getFirstName() {
+    return this.name.split(' ')[0];
   }
 }
 

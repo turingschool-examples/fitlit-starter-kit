a327de9208e1d5e1632c7d6baaa8c98521cb08eb
Matthew Malone <m.malone@me.com>
2019-08-19 17:29:58 -0600

    Create user class and test
    Co-authored-by: Scott Schipke <swschipke@gmail.com>

---
+++ src/User.js
@@ -0,0 +1,22 @@
+const UserRepository = require('../src/UserRepository');
+
+class User {
+  constructor(userObj) {
+    this.id = userObj.id;    
+    this.name = userObj.name;
+    this.address = userObj.address;
+    this.email = userObj.email;
+    this.strideLength = userObj.strideLength;
+    this.dailyStepGoal = userObj.dailyStepGoal;
+    this.friends = userObj.friends;    
+  }
+
+  getUserFirstName() {
+    return this.name.split(' ')[0];
+  }
+
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = User;
+}

c4932f9fa5388c12fd6258b4ab4f9eead1700e20
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-08-19 14:58:45 -0600

    add test-data samples

---
+++ data/users-test-data.js
@@ -0,0 +1,168 @@
+const userData = [
+  {
+    "id": 1,
+    "name": "Luisa Hane",
+    "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+    "email": "Diana.Hayes1@hotmail.com",
+    "strideLength": 4.3,
+    "dailyStepGoal": 10000,
+    "friends": [
+      2,
+      3,
+      4
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Jarvis Considine",
+    "address": "30086 Kathryn Port, Ciceroland NE 07273",
+    "email": "Dimitri.Bechtelar11@gmail.com",
+    "strideLength": 4.5,
+    "dailyStepGoal": 5000,
+    "friends": [
+      5,
+      1,
+      3,
+      4
+    ]
+  },
+  {
+    "id": 3,
+    "name": "Herminia Witting",
+    "address": "85823 Bosco Fork, East Oscarstad MI 85126-5660",
+    "email": "Elwin.Tromp@yahoo.com",
+    "strideLength": 4.4,
+    "dailyStepGoal": 5000,
+    "friends": [
+      19,
+      11,
+      42,
+      33
+    ]
+  },
+  {
+    "id": 4,
+    "name": "Mae Connelly",
+    "address": "28926 Schinner Islands, Turnermouth NE 23720-3230",
+    "email": "Marcos_Pollich@hotmail.com",
+    "strideLength": 3.1,
+    "dailyStepGoal": 4000,
+    "friends": [
+      48,
+      7,
+      44,
+      8
+    ]
+  },
+  {
+    "id": 5,
+    "name": "Erick Schaden",
+    "address": "514 Mayert Walk, Jordaneside SC 55023-6523",
+    "email": "Vanessa_Gerhold@gmail.com",
+    "strideLength": 3.1,
+    "dailyStepGoal": 8000,
+    "friends": [
+      24,
+      26,
+      2,
+      27,
+    ]
+  },
+  {
+    "id": 24,
+    "name": "Kristin Cruickshank",
+    "address": "806 Raynor Port, Kilbackbury OK 11077",
+    "email": "Waylon_Zulauf@hotmail.com",
+    "strideLength": 3.3,
+    "dailyStepGoal": 4000,
+    "friends": [
+      1,
+      2,
+      4,
+      5
+    ]
+  },
+  {
+    "id": 25,
+    "name": "Isobel Schmeler",
+    "address": "5592 Donnelly Trail, Scottieport SC 35511",
+    "email": "Name79@yahoo.com",
+    "strideLength": 3.3,
+    "dailyStepGoal": 4000,
+    "friends": [
+      3,
+      1,
+      24
+    ]
+  },
+  {
+    "id": 26,
+    "name": "Greta Corkery",
+    "address": "99020 Botsford Knoll, Lake Cecil ID 44141",
+    "email": "Virgil28@hotmail.com",
+    "strideLength": 3,
+    "dailyStepGoal": 12000,
+    "friends": [
+      1,
+      3,
+      2,
+      25
+    ]
+  },
+  {
+    "id": 27,
+    "name": "Johnathan Schulist",
+    "address": "868 Kathryn Pike, Gibsonport ME 79500-6839",
+    "email": "Mayra49@yahoo.com",
+    "strideLength": 3,
+    "dailyStepGoal": 10000,
+    "friends": [
+      3,
+      1,
+      26
+    ]
+  },
+  {
+    "id": 28,
+    "name": "Noemi Huels",
+    "address": "5437 Barton Oval, Caesarview RI 88637",
+    "email": "Geovany.Jaskolski@hotmail.com",
+    "strideLength": 3.3,
+    "dailyStepGoal": 2000,
+    "friends": [
+      5,
+      29,
+      30
+    ]
+  },
+  {
+    "id": 29,
+    "name": "Colten Trantow",
+    "address": "2625 Waino Skyway, Kaceybury ME 18723",
+    "email": "Demetris67@hotmail.com",
+    "strideLength": 4.2,
+    "dailyStepGoal": 9000,
+    "friends": [
+      4,
+      3,
+      24
+    ]
+  },
+  {
+    "id": 30,
+    "name": "Deborah Keebler",
+    "address": "039 Jerde Brook, South Helen TN 08907-6883",
+    "email": "Colt_Hermann59@gmail.com",
+    "strideLength": 3.7,
+    "dailyStepGoal": 9000,
+    "friends": [
+      29,
+      3,
+      2,
+    ]
+  },
+]
+
+if(typeof module !== 'undefined'){
+  module.exports = userData;
+}

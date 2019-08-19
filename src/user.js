f6864840d69c7fbdbf8dda3ae6871a0da3ef7e2e
Julian Enochs <julianenochs@yahoo.com>
2019-08-19 17:48:32 -0600

    Add functionality to userRepository
    Co-authored by: Garrett Iannuzzi <garrettiannuzzi@gmail.com>

--- src/user.js
+++ src/user.js
@@ -1,5 +1,5 @@
 class User {
-    constructor() {
+    constructor(user) {
 
     }
 }
--- test/userRepository-test.js
+++ test/userRepository-test.js
@@ -4,8 +4,139 @@ const expect = chai.expect;
 const UserRepository = require('../src/userRepository');
 
 describe('User Repository', () => {
-
+    let userRepository;
+beforeEach(() => {
+    userRepository = new UserRepository([{
+        "id": 1,
+        "name": "Luisa Hane",
+        "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+        "email": "Diana.Hayes1@hotmail.com",
+        "strideLength": 4.3,
+        "dailyStepGoal": 10000,
+        "friends": [
+            16,
+            4,
+            8
+        ]
+    }, {
+        "id": 4,
+        "name": "Mae Connelly",
+        "address": "28926 Schinner Islands, Turnermouth NE 23720-3230",
+        "email": "Marcos_Pollich@hotmail.com",
+        "strideLength": 3.1,
+        "dailyStepGoal": 4000,
+        "friends": [
+            48,
+            7,
+            44,
+            8
+        ]
+    }, {
+        "id": 8,
+        "name": "Laney Abshire",
+        "address": "86416 Koch Inlet, North Kaciefurt MA 80635",
+        "email": "Janice_Nitzsche2@yahoo.com",
+        "strideLength": 2.8,
+        "dailyStepGoal": 2000,
+        "friends": [
+            11,
+            41,
+            23,
+            49
+        ]
+    }, {
+        "id": 16,
+        "name": "Garnett Cruickshank",
+        "address": "992 Zita Mall, North Tremainemouth MA 19312-3532",
+        "email": "Laverna47@hotmail.com",
+        "strideLength": 3.9,
+        "dailyStepGoal": 10000,
+        "friends": [
+            25,
+            31,
+            3,
+            16
+        ]
+    }])
+});
     it('should be a function', () => {
         expect(UserRepository).to.be.a('function');
+    });
+
+    it('should have access to all user information', () => {
+        expect(userRepository.data).to.eql([{
+            "id": 1,
+            "name": "Luisa Hane",
+            "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+            "email": "Diana.Hayes1@hotmail.com",
+            "strideLength": 4.3,
+            "dailyStepGoal": 10000,
+            "friends": [
+                16,
+                4,
+                8
+            ]
+        }, {
+            "id": 4,
+            "name": "Mae Connelly",
+            "address": "28926 Schinner Islands, Turnermouth NE 23720-3230",
+            "email": "Marcos_Pollich@hotmail.com",
+            "strideLength": 3.1,
+            "dailyStepGoal": 4000,
+            "friends": [
+                48,
+                7,
+                44,
+                8
+            ]
+        }, {
+            "id": 8,
+            "name": "Laney Abshire",
+            "address": "86416 Koch Inlet, North Kaciefurt MA 80635",
+            "email": "Janice_Nitzsche2@yahoo.com",
+            "strideLength": 2.8,
+            "dailyStepGoal": 2000,
+            "friends": [
+                11,
+                41,
+                23,
+                49
+            ]
+        }, {
+            "id": 16,
+            "name": "Garnett Cruickshank",
+            "address": "992 Zita Mall, North Tremainemouth MA 19312-3532",
+            "email": "Laverna47@hotmail.com",
+            "strideLength": 3.9,
+            "dailyStepGoal": 10000,
+            "friends": [
+                25,
+                31,
+                3,
+                16
+            ]
+        }])
+    });
+
+    it('should return a users information based on their ID', () => {
+
+        expect(userRepository.getUserData(1)).to.eql({
+            "id": 1,
+            "name": "Luisa Hane",
+            "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+            "email": "Diana.Hayes1@hotmail.com",
+            "strideLength": 4.3,
+            "dailyStepGoal": 10000,
+            "friends": [
+                16,
+                4,
+                8
+            ]
+        });
+    });
+
+    it('should get all users step goals average', () => {
+
+        expect(userRepository.getAllUserStepGoalAverage()).to.equal(6500)
     });
 });

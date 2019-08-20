09db83258428745c67f4f91cfe029859e3894856
Eduardo <jeduardorjx@gmail.com>
2019-08-20 15:10:11 -0600

    Add our user name to DOM

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+console.log(userData[16])
+
+const dailySection = document.querySelector('.daily_section')
+
+dailySection.insertAdjacentHTML('afterbegin', `<article class="user_info">
+  <h2>${userData[16].name}</h2>
+  <p>Address</p>
+  <p>Email</p>
+  <p>Stride Length</p>
+  <p>Daily Step Goal</p>
+</article>`)
+
+//insertAdjecentHTML?
+  //Interpolate inside of here for user's info
+  //
+//
+

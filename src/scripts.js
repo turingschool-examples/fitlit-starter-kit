a7fb15c09a218b05eb19f84efddba2f561b4df2a
Zoe <zoekharvey@gmail.com>
2019-08-20 15:20:59 -0600

    Change jquery method to only display first name

--- src/scripts.js
+++ src/scripts.js
@@ -2,5 +2,5 @@ const userRepository = new UserRepository(userData);
 const userNameDisplay = $('.header__h1--name');
 const user = new User(userRepository.findUserData(1))
 $(document).ready(() => {
-  $('.header__h1--name').text(user.name)
+  $('strong').text(user.returnFirstName())
 });

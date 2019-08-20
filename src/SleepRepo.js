158649c48d0a4895437510845568ed0891b54bb6
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-08-20 16:19:40 -0600

    Almost all sleep repo functionality created

--- src/SleepRepo.js
+++ src/SleepRepo.js
@@ -1,11 +1,34 @@
-if (typeof module !== "undefined") {
-    sleepData = require("../data/sleep-test-data");
+class SleepRepo {
+  constructor(sleepData) {
+    this.sleepData = sleepData;
   }
 
-class SleepRepo {
-    constructor() {
-        
-    }
+  returnAllSleepQuality() {
+    return Number((this.sleepData.reduce((totalQuality, eachPerson) => {
+      totalQuality += eachPerson.sleepQuality;
+      return totalQuality;
+    }, 0) / this.sleepData.length).toFixed(1));
+  }
+  
+  // returnAboveAverageSleepers() {
+  //   var weekOfData = this.sleepData.splice(-7)
+  //   console.log(weekOfData.reduce((obj, user) => {
+  //     if (!obj[user.userID]) {
+  //       obj[user.userID] = weekOfData.filter(el => el.userID === user.userID).reduce((totalQuality, eachDay) => {
+  //         totalQuality += eachDay.sleepQuality
+  //         return totalQuality
+  //       },0)
+  //     } else {
+  //       obj[user.userID] += user.sleepQuality
+  //     }
+  //     return obj
+  //   }, {}))
+  // }
+  returnLongestSleepers(date) {
+    var dateData = this.sleepData.filter(day => day.date === date)
+    var sortedSleepers = dateData.sort((a,b) => b.hoursSlept - a.hoursSlept)
+    return sortedSleepers.filter(day => day.hoursSlept === sortedSleepers[0].hoursSlept).map(user => user.userID)
+  }
 }
 
 
--- test/SleepRepo-test.js
+++ test/SleepRepo-test.js
@@ -6,14 +6,26 @@ const sleepData = require('../data/sleep-test-data');
 const SleepRepo = require('../src/SleepRepo');
 
 describe('SleepRepo', () => {
+  let sleepRepo;
   
   beforeEach(() => {
-    let sleepRepo = new SleepRepo(sleepData)
-  })
+    sleepRepo = new SleepRepo(sleepData)
+
+  });
 
   it('should be a function', () => {
     expect(SleepRepo).to.be.a('function');
-  })
+  });
 
+  it('should return the average sleep quality amongst all users', () => {
+    expect(sleepRepo.returnAllSleepQuality()).to.equal(2.9);
+  });
 
+  // it('should return all users who average a sleep quality greater than 3 for a given week', () => {
+  //   expect(sleepRepo.returnAboveAverageSleepers()).to.equal();
+  // });
+
+  it('should return the users that slept the most hours for a given date', () => {
+    expect(sleepRepo.returnLongestSleepers("2019/06/25")).to.eql([2]);
+  });
 });

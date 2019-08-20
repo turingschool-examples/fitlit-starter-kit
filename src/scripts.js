16f7fe3295e3b410bdaeeb25c51970d562f1c997
Julian Enochs <julianenochs@yahoo.com>
2019-08-19 20:00:42 -0600

    Add styling

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+$(document).ready(() => {
+    $('#splash-page-js').show();
+    $('#main-page-js').hide();
+    $('#header-js').hide();
+    $('#enter__button-js').click(hideSplash)
+    function hideSplash() {
+        $('#splash-page-js').hide();
+        $('#main-page-js').show();
+        $('#header-js').show();
+    }
+});

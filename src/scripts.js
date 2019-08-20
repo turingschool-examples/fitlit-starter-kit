5cbf575ef0680add09888a0f84a56184a1efc371
Zoe <zoekharvey@gmail.com>
2019-08-19 23:01:06 -0600

    Edit jquery function to start dom manipulation

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,6 @@
-let userRepository = new UserRepository(userData);
-const userName = $('.header__h1--title');
-
+// const userRepository = new UserRepository(data, id);
+const userNameDisplay = $('.header__h1--title');
+const user = new User(userRepository.findUserData())
 $(document).ready(() => {
-  (() => userName.text(userRepository.getUserData(19).name))();
+  $(userNameDisplay).text(user.name)
 });

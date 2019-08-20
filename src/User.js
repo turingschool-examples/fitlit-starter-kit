d4ffabb54f30934c73488668aa2da308a92df0bf
Zoe <zoekharvey@gmail.com>
2019-08-19 18:09:04 -0600

    Add user class and set up user-test.js file.

---
+++ src/User.js
@@ -0,0 +1,21 @@
+class User {
+  constructor(userData) {
+    this.id = userData.id;
+    this.name = userData.name;
+    this.address = userData.address;
+    this.email = userData.email;
+    this.strideLength = userData.strideLength;
+    this.dailyStepGoal = userData.dailyStepGoal;
+    this.friends = userData.friends
+  }
+  
+  returnFirstName() {
+  
+  }
+  
+  
+}
+  
+if (typeof module !== 'undefined') {
+  module.exports = User;
+}
---
+++ src/UserRepository.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+
+
+if (typeof module !== 'undefined') {
+    module.exports = UserRepository;
+  }

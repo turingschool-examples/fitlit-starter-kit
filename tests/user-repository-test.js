3d8bc7691fd1a6ea930d429322dad948033505c5
Colin Turing <colin.koga@gmail.com>
2019-08-19 21:19:15 -0600

    Add sample user data file to the user repository test file successful console log of userData

--- tests/user-repository-test.js
+++ tests/user-repository-test.js
@@ -1,22 +1,24 @@
 const chai = require("chai");
 const expect = chai.expect;
 
-const UserRepository = require('../src/users-repository')
+const UserRepository = require('../src/users-repository');
+const { userData } = require('../data/test-data')
+
 
 describe('UserRepository', function() {
 
-
   it('should be a function', () => {
 
-    let obj = {
-      id: 1,
-      name: "Luisa Hane",
-      address: "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
-      email: "Diana.Hayes1@hotmail.com",
-      strideLength: 4.3,
-      dailyStepGoal: 10000,
-      friends: [16, 4, 8]
-    };
+    // let obj = {
+    //   id: 1,
+    //   name: "Luisa Hane",
+    //   address: "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+    //   email: "Diana.Hayes1@hotmail.com",
+    //   strideLength: 4.3,
+    //   dailyStepGoal: 10000,
+    //   friends: [16, 4, 8]
+    // };
+    // console.log(userData);
 
     const userRepo = new UserRepository(obj);
 

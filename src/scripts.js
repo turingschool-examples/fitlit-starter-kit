95b6fc8e10c5197c5e724701d6600c20a2590f5f
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-08-19 19:49:25 -0600

    Add HTML structure and update exports links
    Co-Authored By: Vanessa Randall @vrandall66

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+// eslint-disable-next-line no-undef
+let userRepository = new UserRepository(userData);
+const h1 = $('h1');
+
+$(document).ready(() => {
+  (() => h1.text(userRepository.getUserData(33).name))();
+});

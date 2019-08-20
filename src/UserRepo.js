ad95f3f98fdea3b019e4f0c9fdb27713d3d63318
Pol Sieira <polsieira@Pols-MacBook-Pro.local>
2019-08-20 10:41:49 -0600

    Set up userRepo class and begin with two easy test

--- src/UserRepo.js
+++ src/UserRepo.js
@@ -1,14 +1,9 @@
-if (typeof module !== "undefined") {
-    userData = require("../data/users-test-data");
-  }
-
 class UserRepo {
-    constructor() {
-        
-    }
+  constructor() {
+
+  }
 }
 
-
 if (typeof module !== 'undefined') {
-    module.exports = UserRepo;
-  }
+  module.exports = UserRepo;
+}
--- test/UserRepo-test.js
+++ test/UserRepo-test.js
@@ -6,13 +6,18 @@ const userData = require('../data/users-test-data');
 const UserRepo = require('../src/UserRepo');
 
 describe('UserRepo', () => {
-  
+
+  let userRepo;
   beforeEach(() => {
-    let userRepo = new UserRepo(userData)
+    userRepo = new UserRepo(userData)
   });
 
   it('should be a function', () => {
     expect(UserRepo).to.be.a('function');
+  });
+
+  it('should be an instance of the class UserRepo', () => {
+    expect(userRepo).to.be.an.instanceOf(UserRepo);
   });
 
 

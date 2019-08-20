bfb8670534aafebe60aefaabff9633222636e21a
Allison Wagner <allisonjw01@gmail.com>
2019-08-19 18:03:05 -0600

    Add the user class and test

---
+++ src/User.js
@@ -0,0 +1,19 @@
+// const userData = require('./data-subsets/users-subset')
+
+class User {
+    constructor(userData) {
+     this.userData = userData;
+    }
+    getFirstName() {
+        let name = this.userData.name.split(' ')[0];
+        return name;
+    }
+}
+
+
+
+
+
+if (typeof module !== 'undefined') {
+    module.exports = User;
+  }

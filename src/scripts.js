250940ec688d1f92ad0cead9bf67fecab848bbd5
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-08-20 15:12:17 -0600

    Fix jquery inital function and updated parameters in user repo

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,6 @@
-// const userRepository = new UserRepository(data, id);
-const userNameDisplay = $('.header__h1--title');
-const user = new User(userRepository.findUserData())
+const userRepository = new UserRepository(userData);
+const userNameDisplay = $('.header__h1--name');
+const user = new User(userRepository.findUserData(1))
 $(document).ready(() => {
-  $(userNameDisplay).text(user.name)
+  $('.header__h1--name').text(user.name)
 });

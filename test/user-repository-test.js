51690a1f58aec8b99e8cf38f81f8057b677ed274
Kate Malone <katerosemalone@gmail.com>
2019-08-19 19:03:09 -0600

    Adds test to user repository

--- test/user-repository-test.js
+++ test/user-repository-test.js
@@ -1,11 +1,18 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const sampleData = require('../test/sample-data')
 const UserRepository = require('../src/user-repository');
 
 describe('UserRepository', function() {
-    it('should be a function', function() {
-        expect(UserRepository).to.be.a('function');
-    })
-    })
+
+it('should be a function', function() {
+    expect(UserRepository).to.be.a('function');
+})
+
+it.only('should return user data',function() {
+    const user1 = new UserRepository(sampleData)
+    expect(user1.returnUserData(1)).to.equal(sampleData[0])
+})
+})
 

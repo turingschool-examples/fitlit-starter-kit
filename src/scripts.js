d2b4772b308dab0079c458f2883bf435a86f1a98
Scott Schipke <swschipke@gmail.com>
2019-08-20 15:56:31 -0600

    Add DOM manipulation for iteration 1

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+$(document).ready(() => {
+  const repo = new UserRepository(sampleData);
+  user = new User(repo.getUserData(getRandomNumber()));
+  $('.header__div--h2').text(`Hi, ${user.getUserFirstName()}!`);
+
+  $('.header__div__user-stepgoal').text(`Your step goal: ${user.dailyStepGoal} steps`);
+
+  $('.header__div__allusers-stepgoal').text(`Average User Goal: ${repo.getAllUsersAvgStepGoal()} steps`)
+
+
+})
+      
+
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * sampleData.length)
+}

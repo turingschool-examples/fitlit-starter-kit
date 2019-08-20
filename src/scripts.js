92495d19bc0fecfbc53a5a725d2ab4f733fcc8a0
Vanessa Randall <vanessalrandall@gmail.com>
2019-08-20 17:29:35 -0600

    Generate User info on homepage and add generic styling
    Co-authored-by: Edwin Del Bosque @edwindelbosque

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,19 @@
 // eslint-disable-next-line no-undef
 let userRepository = new UserRepository(userData);
-const h1 = $('h1');
+const name = $('#name');
+const address = $('#address');
+const email = $('#email');
+const strideLength = $('#strideLength');
+const dailyStepGoal = $('#stepGoal');
 
 $(document).ready(() => {
-  (() => h1.text(userRepository.getUserData(33).name))();
+  updateUserDataDOM();
 });
+
+function updateUserDataDOM () {
+  name.text(userRepository.getUserData(33).name);
+  address.text(userRepository.getUserData(33).address);
+  email.text(userRepository.getUserData(33).email);
+  strideLength.text(userRepository.getUserData(33).strideLength);
+  dailyStepGoal.text(userRepository.getUserData(33).dailyStepGoal);
+}

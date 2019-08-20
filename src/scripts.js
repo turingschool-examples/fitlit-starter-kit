cc92d943564cc9d798e43a5fbbea34a4dccb96bc
Eduardo <jeduardorjx@gmail.com>
2019-08-20 15:44:07 -0600

    Caculate the average step goals of all users

--- src/scripts.js
+++ src/scripts.js
@@ -1,17 +1,17 @@
-console.log(userData[16])
+
+// console.log(userRepository)
+var users = new UserRepository(userData)
+console.log(users)
 
 const dailySection = document.querySelector('.daily_section')
 
+const header = document.querySelector('header')
+
+header.insertAdjacentHTML('beforeend', `<h2>Welcome, ${userData[16].name}</h2>`)
+
 dailySection.insertAdjacentHTML('afterbegin', `<article class="user_info">
-  <h2>${userData[16].name}</h2>
-  <p>Address</p>
-  <p>Email</p>
-  <p>Stride Length</p>
-  <p>Daily Step Goal</p>
+  <p>Address: ${userData[16].address}</p>
+  <p>E-mail: ${userData[16].email}</p>
+  <p>Stride Length: ${userData[16].strideLength}</p>
+  <p>Daily Step Goal: ${userData[16].dailyStepGoal} / ${users.calculateAverageStepGoals()}</p>
 </article>`)
-
-//insertAdjecentHTML?
-  //Interpolate inside of here for user's info
-  //
-//
-

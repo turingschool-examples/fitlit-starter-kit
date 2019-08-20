bff18ba32825a640c68cf7cec62e68d9f73f8e30
Sara Karsh <sara@Saras-MacBook-Pro.local>
2019-08-20 15:51:39 -0600

    Complete iteration 1 functionality co-authored by Kate Malone, <katerosemalone@gmail.com>

--- test/user-repository-test.js
+++ test/user-repository-test.js
@@ -10,9 +10,14 @@ it('should be a function', function() {
     expect(UserRepository).to.be.a('function');
 })
 
-it.only('should return user data',function() {
+it('should return user data',function() {
     const user1 = new UserRepository(sampleData)
     expect(user1.returnUserData(1)).to.equal(sampleData[0])
+})
+
+it('should return the average step goal among all users', function() {
+    const allUsers = new UserRepository(sampleData);
+    expect(allUsers.averageStepGoal()).to.equal(7166);
 })
 })
 

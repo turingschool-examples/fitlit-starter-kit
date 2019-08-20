92aee7436217ff50a7134c6f730b8ab99a57429e
Zoe <zoekharvey@gmail.com>
2019-08-20 16:10:09 -0600

    Add user card info to dom

--- src/scripts.js
+++ src/scripts.js
@@ -2,5 +2,11 @@ const userRepository = new UserRepository(userData);
 const userNameDisplay = $('.header__h1--name');
 const user = new User(userRepository.findUserData(1))
 $(document).ready(() => {
-  $('strong').text(user.returnFirstName())
+  $('strong').text(user.returnFirstName());
+  $('.article__strong--name').text(user.name);
+  $('.article__strong--address').text(user.address);
+  $('.article__strong--email').text(user.email);
+  $('.article__strong--stride').text(user.strideLength)
+  
 });
+

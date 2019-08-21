776bb9622d11975f33bb8499bac4520d9f4b273e
Eduardo <jeduardorjx@gmail.com>
2019-08-21 16:29:37 -0600

    Add test for returning weekly water intake and method to return weekly water intake

--- src/hydrationClass.js
+++ src/hydrationClass.js
@@ -13,19 +13,27 @@ class Hydration {
     return Math.round(sumWaterIntake / this.hydrationData.length);
   }
 
-  //method to return how many fluid ounces of water they consumed for a specific day
   waterConsumedThatDay(date) {
-    var numOunces = this.hydrationData.find(element => {
+    let dayOunces = this.hydrationData.find(element => {
       if (this.userID === element.userID && date === element.date) {
         return element.numOunces;
      }
     });
-    return numOunces.numOunces;
+    return dayOunces.numOunces;
   }
-  //method to return how many fluid ounces of water they consumed over a course of a week (7 days) - return amount for each day
+  
+  waterConsumedThatWeek() {
+    let currentDay = this.hydrationData[this.hydrationData.length - 1]
+    let currentDayIndex = this.hydrationData.indexOf(currentDay)
+    let currentWeek = this.hydrationData.slice(-currentDayIndex)
+    let currentWeekOunces = currentWeek.map(element => {
+      return element.numOunces;
+    })
+    return currentWeekOunces;
+  }
 }
 
-
+//
 
 if (typeof module !== 'undefined') {
   module.exports = Hydration;
--- test/hydration-test.js
+++ test/hydration-test.js
@@ -48,7 +48,13 @@ describe('Hydration', () => {
       "numOunces": 99
     };
 
-    hydrationData = new Hydration([day1, day2, day3, day4, day5, day6, day7], 17);
+    day8 = {
+      "userID": 17,
+      "date": "2019/06/22",
+      "numOunces": 54
+    };
+
+    hydrationData = new Hydration([day1, day2, day3, day4, day5, day6, day7, day8], 17);
   });
 
   it('should be a function', () => {
@@ -69,8 +75,12 @@ describe('Hydration', () => {
 
   it('should return number of fluid ounces consumed for a specific day', () => {
     assert.equal(hydrationData.hydrationData[0].date, "2019/06/15");
-    var date = hydrationData.hydrationData[0].date
+    let date = hydrationData.hydrationData[0].date
     assert.equal(hydrationData.waterConsumedThatDay(date), 60)
   })
+
+  it('should return number of fluid ounces consumed each day for a week', () => {
+    assert.deepEqual(hydrationData.waterConsumedThatWeek(), [43, 59, 52, 50, 24, 99, 54])
+  });
 
 });

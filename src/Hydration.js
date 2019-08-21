2d78dc3e82c04de53143c7eeae90a844156286c9
Scott Schipke <swschipke@gmail.com>
2019-08-21 14:59:38 -0600

    Add get weekly average function to Hydration class
    Co-authored-by: Matthew Malone <m.malone@me.com>

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,7 +1,8 @@
 class Hydration {
   constructor(userData) {
     this.userData = userData;
-    this.newUserData = [...userData]
+    this.newUserData = [...userData];
+    this.weeklyHydroArr;
   }
 
   userTotalAvgOz() {
@@ -9,16 +10,24 @@ class Hydration {
     return Math.round(total/this.userData.length)
   }
 
-  userAvgByDate(date) {
+  userHydrationByDate(date) {
     return this.userData.find(day => day.date === date).numOunces;
   }
 
   getHydroArray(dateToday) {
   const arrayOfDates = this.userData.map(day => day.date)
     const index = (((arrayOfDates.length - arrayOfDates.findIndex(date => dateToday === date)) * -1) - 6)
-    return this.newUserData.splice(index, 7).reverse();
+    this.weeklyHydroArr = this.newUserData.splice(index, 7).reverse();
+    return this.weeklyHydroArr;
   }
-
+  
+  getWeeklyHydroAvg() {
+    const weekTotal = this.weeklyHydroArr.reduce((total, day) => {
+      total += day.numOunces;
+      return total
+    }, 0)
+    return Math.round(weekTotal/this.weeklyHydroArr.length)
+  }
 };
 
 

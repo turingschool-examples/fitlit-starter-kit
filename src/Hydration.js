9b414e2e36ee37670749b66e6fde65e8883bcff7
dawnlunacy <rudd.lacy@gmail.com>
2019-08-21 13:24:04 -0600

    Set up initial Hydration test, mockdata, and class file

---
+++ src/Hydration.js
@@ -0,0 +1,21 @@
+class Hydration {
+    constructor() {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Hydration;
+
+if (typeof module !== 'undefined') {
+    module.exports = Hydration;
+  };
---
+++ test/Hydration-test.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Hydration = require('../src/Hydration');
+const mockHydrationData = require('../mock-data/mock-hydration');
+
+let hydration;
+
+describe('Hydration', () => {
+
+  beforeEach( () => {
+    hydration = new Hydration(mockHydrationData[1]); 
+  });
+
+  it('should be a function', () => {
+    expect(Hydration).to.be.a('function');
+  });
+
+  it('should be an instance of Hydration', () => {
+    expect(hydration).to.be.an.instanceof(Hydration);
+  });
+
+});

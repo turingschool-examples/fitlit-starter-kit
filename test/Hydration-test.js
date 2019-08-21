90bbaa582715c90357df17358395e7fd1acc2ecb
dawnlunacy <rudd.lacy@gmail.com>
2019-08-21 14:17:26 -0600

    Add first to method test to hydration-test

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -8,7 +8,7 @@ let hydration;
 describe('Hydration', () => {
 
   beforeEach( () => {
-    hydration = new Hydration(mockHydrationData[1]); 
+    hydration = new Hydration(mockHydrationData); 
   });
 
   it('should be a function', () => {
@@ -19,6 +19,81 @@ describe('Hydration', () => {
     expect(hydration).to.be.an.instanceof(Hydration);
   });
 
+  it('should have a parameter to take in all of the hydration data', () => {
+    expect(hydration.allHydrationData).to.eql(mockHydrationData)
+  })
+
+  it('should have a parameter to take in an id for the current user', () => {
+    expect(hydration.idCurrentUser).to.eql(1);
+  })
+
+  describe('findCurrentUserHydrationData', () => {
+    it('should return a list of the current user/s hydration information', () => {
+      hydration.findCurrentUserData()
+      expect(hydration.currentUserData).to.eql([{
+        "userID": 1,
+        "date": "2019/06/15",
+        "numOunces": 37
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/16",
+        "numOunces": 69
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/17",
+        "numOunces": 96
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/18",
+        "numOunces": 61
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/19",
+        "numOunces": 91
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/20",
+        "numOunces": 50
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/21",
+        "numOunces": 50
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/22",
+        "numOunces": 43
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/23",
+        "numOunces": 39
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/24",
+        "numOunces": 61
+      }
+      ])
+    });
+  });
+
+  describe('findAverageFluidOzConsumedPerDayForAllTime', () => {
+    it('should find the average fluid oz consumed per dat for all time rounding to the nearest whole number', () => {
+      expect(hydration.findAverageFluidOzConsumedPerDayForAllTime()).to.equal(58);
+    });
+    
+
+  })
+
+
+
 });
 
-    
+

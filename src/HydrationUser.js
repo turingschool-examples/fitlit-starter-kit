094d99001d66d52a967670d9e156e4c9240e0888
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-08-21 14:52:34 -0600

    Add functionality to find and test hydro by date

--- src/HydrationUser.js
+++ src/HydrationUser.js
@@ -1,6 +1,11 @@
 class HydrationUser {
-  constructor() {
+  constructor(hydrationTestData) {
+    this.hydrationTestData = hydrationTestData;
+  }
 
+  findDailyHydration(date, id) {
+    var day = this.hydrationTestData.find(user => user.date === date && id)
+  console.log(day)
   }
 }
 

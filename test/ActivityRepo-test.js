df87ec1dcf947c729b42229cceb602d05813f10f
posi7790 <posi7790@colorado.edu>
2019-08-21 15:13:41 -0600

    Finish ActivityRepo class and pass all tests

--- test/ActivityRepo-test.js
+++ test/ActivityRepo-test.js
@@ -2,18 +2,35 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const activityData = require('../data/activity-test-data');
+const userData = require('../data/users-test-data');
 
 const ActivityRepo = require('../src/ActivityRepo');
 
 describe('ActivityRepo', () => {
-  
+
+  let activityRepo;
   beforeEach(() => {
-    let activityRepo = new ActivityRepo(activityData)
+    activityRepo = new ActivityRepo(activityData, userData)
   })
 
   it('should be a function', () => {
     expect(ActivityRepo).to.be.a('function');
-  })
+  });
 
+  it('should return the average number of stair climbed for all users for a specific date', () => {
+    expect(activityRepo.returnAverageDailyStairsClimbed("2019/06/15")).to.equal(20.8);
+  });
+
+  it('should return the average number of steps for all users taken for a specific date', () => {
+    expect(activityRepo.returnAverageNumberofSteps("2019/06/15")).to.equal(6026.6);
+  });
+
+  it('should return the average minutes active for a specific date for all users', () => {
+    expect(activityRepo.returnAverageMinutesActive("2019/06/15")).to.equal(144.2);
+  });
+
+  it('should return the highest minutes active of all time', () => {
+    expect(activityRepo.returnMostActive()).to.equal('Luisa Hane');
+  });
 
 })

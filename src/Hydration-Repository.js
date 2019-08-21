a5151f889aa45ffa3a01a64769e31906480531b4
“Elyse <elysemyers1@gmail.com>
2019-08-21 13:31:36 -0600

    Updates hydration repository file name and require statement in test file

---
+++ src/Hydration-Repository.js
@@ -0,0 +1,27 @@
+class Hydration {
+  constructor(hydrationData) {
+    this.hydrationData = hydrationData;
+  }
+
+  findCurrentUserData(userId) {
+    return this.hydrationData.filter((hydrationObj) => hydrationObj.userID === userId);
+  }
+
+  returnAvgFluidOzPerDayAllTime(userId) {
+    return this.findCurrentUserData(userId).reduce((totalOunces, hydrationObj) => {
+      return totalOunces += hydrationObj.numOunces;
+    }, 0);
+  }
+
+  returnFluidOzByDate(userId, date) {
+    return this.findCurrentUserData(userId).find((hydrationObj) => hydrationObj.date === date).numOunces;
+  }
+
+  returnFluidOzByWeek(userId, date) {
+    let index = this.findCurrentUserData(userId).findIndex((hydrationObj) => hydrationObj.date === date);
+    return this.findCurrentUserData(userId).map(hydrationObj => hydrationObj.numOunces).splice(index, 7);
+  }
+}
+
+
+module.exports = Hydration;

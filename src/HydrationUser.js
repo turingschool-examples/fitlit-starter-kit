ff6568ce9dc0fd42b2287858573a9c1fba587a32
Zoe <zoekharvey@gmail.com>
2019-08-21 15:52:30 -0600

    Add functionality and tests to find weekly hydration info based on id

--- src/HydrationUser.js
+++ src/HydrationUser.js
@@ -1,16 +1,33 @@
+// const HydrationRepository = require('./HydrationRepository')
+
 class HydrationUser {
   constructor(hydrationTestData) {
     this.hydrationTestData = hydrationTestData;
   }
 
   findDailyHydration(date, id) {
-    var day = this.hydrationTestData.find(user => user.date === date && id)
+    let day = this.hydrationTestData.find(user => user.date === date && id)
     return day.numOunces
   }
+
+  findWeeklyHydration(id) {
+    let userHydroData = this.hydrationTestData.filter(element => element.userID === id);
+    let weeklyData = userHydroData.slice((userHydroData.length - 7), userHydroData.length)
+    console.log(weeklyData)
+   return weeklyData.map((element) => {
+      return element.numOunces
+  })
+  }
+
 }
+
+
 
 
 
 if (typeof module !== 'undefined') {
   module.exports = HydrationUser;
 }
+
+
+

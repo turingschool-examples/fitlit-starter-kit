eead25749dad56ad43d78902e0d906f727e3d1c1
Matthew Malone <m.malone@me.com>
2019-08-21 14:30:53 -0600

    Created last method for hydration iteration
    Co-authored-by: Scott Schipke <swschipke@gmail.com>

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,6 +1,7 @@
 class Hydration {
   constructor(userData) {
     this.userData = userData;
+    this.newUserData = [...userData]
   }
 
   userTotalAvgOz() {
@@ -11,8 +12,14 @@ class Hydration {
   userAvgByDate(date) {
     return this.userData.find(day => day.date === date).numOunces;
   }
-};
 
+  getHydroArray(dateToday) {
+  const arrayOfDates = this.userData.map(day => day.date)
+    const index = (((arrayOfDates.length - arrayOfDates.findIndex(date => dateToday === date)) * -1) - 6)
+    return this.newUserData.splice(index, 7).reverse();
+  }
+
+};
 
 
 if (typeof module !== 'undefined') {

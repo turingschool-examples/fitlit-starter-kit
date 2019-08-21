1c7cdb41304a4d6e0aefe2b7c94006c551ff2014
“Elyse <elysemyers1@gmail.com>
2019-08-21 15:01:34 -0600

    Updates export statement at bottom of file

--- src/Activity-Repository.js
+++ src/Activity-Repository.js
@@ -6,8 +6,44 @@ class Activity {
   findCurrentUserData(userId) {
     return this.activityData.filter((activityObj) => activityObj.userID === userId);
   }
-  
+
+  returnStairsClimbedAllUsersByDate(date)  {
+    //return average num of stairs climbed for given date (all users)
+  }
+
+  returnStepsTakenAllUsersByDate(date) {
+    //return average num of steps take for given date (all users)
+  } 
+
+  returnActiveMinutesAllUsersByDate(date) {
+    //return average mins active for given date (all users)
+  } 
+
+  returnMilesWalkedByDate(userId, date) {
+    //return miles walked on date for user (use stride length)
+  } 
+
+  returnActiveMinutesByDate(userId, startDate) {
+    //return active minutes on date for week
+  } 
+
+  returnAvgActiveMinutesByWeek(userId, startDate) {
+    //return average active minutes for a given week 
+  } 
+
+  returnStepGoalMetByDate(userId, date) {
+    //return true or false if user achieved their step goal
+  } 
+
+  returnAllDaysStepGoalExceeded(userId) {
+    //return all days user exceeded step goal
+  } 
+
+  returnStairClimbingRecord(userId) {
+    //return all time stair climbing record
+  }
 }
 
-
-module.exports = Activity;
+if (typeof module !== 'undefined') {
+  module.exports = Activity;
+}

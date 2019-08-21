48111d05a03e90d9259415315e7d74d7d6132412
“Elyse <elysemyers1@gmail.com>
2019-08-20 21:20:20 -0600

    Updates sample data set to include 9 days of data to test method that requires 7 days worth

--- test-data/hydration-test-data.js
+++ test-data/hydration-test-data.js
@@ -27,6 +27,112 @@ const hydrationTestData = [{
   "userID": 3,
   "date": "2019/06/16",
   "numOunces": 99
-}];
+},
+{
+  "userID": 1,
+  "date": "2019/06/17",
+  "numOunces": 96
+},
+{
+  "userID": 2,
+  "date": "2019/06/17",
+  "numOunces": 96
+},
+{
+  "userID": 3,
+  "date": "2019/06/17",
+  "numOunces": 28
+},
+{
+  "userID": 1,
+  "date": "2019/06/18",
+  "numOunces": 61
+},
+{
+  "userID": 2,
+  "date": "2019/06/18",
+  "numOunces": 70
+},
+{
+  "userID": 3,
+  "date": "2019/06/18",
+  "numOunces": 40
+},
+{
+  "userID": 1,
+  "date": "2019/06/19",
+  "numOunces": 91
+},
+{
+  "userID": 2,
+  "date": "2019/06/19",
+  "numOunces": 76
+},
+{
+  "userID": 3,
+  "date": "2019/06/19",
+  "numOunces": 85
+},
+{
+  "userID": 1,
+  "date": "2019/06/20",
+  "numOunces": 50
+},
+{
+  "userID": 2,
+  "date": "2019/06/20",
+  "numOunces": 71
+},
+{
+  "userID": 3,
+  "date": "2019/06/20",
+  "numOunces": 51
+},
+{
+  "userID": 1,
+  "date": "2019/06/21",
+  "numOunces": 50
+},
+{
+  "userID": 2,
+  "date": "2019/06/21",
+  "numOunces": 27
+},
+{
+  "userID": 3,
+  "date": "2019/06/21",
+  "numOunces": 41
+},
+{
+  "userID": 1,
+  "date": "2019/06/22",
+  "numOunces": 43
+},
+{
+  "userID": 2,
+  "date": "2019/06/22",
+  "numOunces": 58
+},
+{
+  "userID": 3,
+  "date": "2019/06/22",
+  "numOunces": 78
+},
+{
+  "userID": 1,
+  "date": "2019/06/23",
+  "numOunces": 39
+},
+{
+  "userID": 2,
+  "date": "2019/06/23",
+  "numOunces": 44
+},
+{
+  "userID": 3,
+  "date": "2019/06/23",
+  "numOunces": 35
+},
+];
 
 module.exports = hydrationTestData;

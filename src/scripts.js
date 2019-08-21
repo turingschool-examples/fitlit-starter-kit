3ce40024a0224abcf1cf2a4396f9dcf81f750429
Scott Schipke <swschipke@gmail.com>
2019-08-21 15:43:24 -0600

    Start hydration DOM manipulation and update HTML structure
    Co-authored-by: Matthew Malone <m.malone@me.com>

--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,23 @@
 $(document).ready(() => {
   const repo = new UserRepository(sampleData);
-  const user = new User(repo.getUserData(getRandomNumber()));
-  // hydroRepo = new HydrationRep(sampleHydration);
-  //const uderHydro = new Hydration(user.id)
+  const user = new User(repo.getUserData(getRandomNumber()))
+  ;
+  const hydroRepo = new HydrationRepository(sampleHydration);
+  const userHydro = new Hydration(hydroRepo.getUserData(user.id));
+
   $('.header__div--h2').text(`Hi, ${user.getUserFirstName()}!`);
 
   $('.header__div__user-stepgoal').text(`Your step goal: ${user.dailyStepGoal} steps`);
 
-  $('.header__div__allusers-stepgoal').text(`Average User Goal: ${repo.getAllUsersAvgStepGoal()} steps`)
+  $('.header__div__allusers-stepgoal').text(`Average User Goal: ${repo.getAllUsersAvgStepGoal()} steps`);
+
+  $('.hydration__container--consumed--today').text(`Consumed today: ${userHydro.userHydrationByDate('2019/06/25')}oz`);
+
+  userHydro.getHydroArray('2019/06/25');
+
+  $('.hydration__container--consumed--this--week').text(`Daily average consumed this week: ${userHydro.getWeeklyHydroAvg()}oz`);
+
+
 
 
 })
@@ -17,3 +27,4 @@ $(document).ready(() => {
 function getRandomNumber() {
   return Math.floor(Math.random() * sampleData.length)
 }
+

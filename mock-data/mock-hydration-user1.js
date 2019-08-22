89ff5ea3c30c90f093870075c1708f31d5b7b493
dawnlunacy <rudd.lacy@gmail.com>
2019-08-22 14:24:13 -0600

    Refactor mockHydrationUser1 and tests to make tests more streamline

--- mock-data/mock-hydration-user1.js
+++ mock-data/mock-hydration-user1.js
@@ -1,4 +1,4 @@
-const mockHydrationDataUser1 = [
+const mockHydrationDataUser1 = [[
   {
     "userID": 1,
     "date": "2019/06/15",
@@ -49,6 +49,15 @@ const mockHydrationDataUser1 = [
     "date": "2019/06/24",
     "numOunces": 61
   }
-];
+], [
+  {date: "2019/06/15", numOunces: 37}, 
+  {date: "2019/06/16", numOunces: 69}, 
+  {date: "2019/06/17", numOunces: 96},
+  {date: "2019/06/18", numOunces: 61},
+  {date: "2019/06/19", numOunces: 91},
+  {date: "2019/06/20", numOunces: 50}, 
+  {date: "2019/06/21", numOunces: 50}
+]];
 
 module.exports = mockHydrationDataUser1;
+

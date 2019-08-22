14371e41e93b11b8b5de8624e2319f3d9b2c35a1
dawnlunacy <rudd.lacy@gmail.com>
2019-08-21 18:26:18 -0600

    Add files for activity class, tests, and mock-data

---
+++ test/Activity-test.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Activity = require('../src/Activity');
+const mockActivityData = require('../mock-data/mock-hydration');
+
+let activity;
+
+describe('Activity', () => {
+
+  beforeEach( () => {
+    activity = new Activity(mockActivityData, 2); 
+    activity.findCurrentUserData();
+  });
+
+  it('should be a function', () => {
+    expect(Activity).to.be.a('function');
+  });
+
+  it('should be an instance of Hydration', () => {
+    expect(activity).to.be.an.instanceof(Activity);
+  });
+
+  it('should have a parameter to take in all of the hydration data', () => {
+    expect(activity.allActivityData).to.eql(mockActivityData)
+  });
+
+  it('should have a parameter to take in an id for the current user', () => {
+    expect(activity.currentUserId).to.eql(2);
+  });

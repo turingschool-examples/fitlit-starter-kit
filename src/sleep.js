2e508ea11cc0ec8af049b62febe4a7953e2132a5
Quinne Farenwald <q.farenwald@gmail.com>
2019-08-21 19:09:12 -0600

    Edit getAvgSleepHours in sleep to use reduce

--- src/sleep.js
+++ src/sleep.js
@@ -6,13 +6,10 @@ class Sleep {
   getAvgSleepHours(id) {
     let person = this.data;
 
-    let hours = person.map(day => {
-      return day.hoursSlept;
-    })
-    let avgHours = hours.reduce((acc, hour) => {
-      return acc += hour;
-    })
-    //can make two functions above one reduce
+    let avgHours = person.reduce((acc, day) => {
+      return acc += day.hoursSlept;
+    }, 0)
+
     return parseFloat((avgHours / 11).toFixed(1));
   }
 

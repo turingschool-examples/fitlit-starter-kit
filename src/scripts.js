b7a3c3269c99f684a9b7b9296973aa70e654fb83
Zoe <zoekharvey@gmail.com>
2019-08-22 14:30:19 -0600

    Append avg amount of sleep all time to dom

--- src/scripts.js
+++ src/scripts.js
@@ -8,8 +8,9 @@ $(document).ready(() => {
   $('.article__strong--address').text(user.address);
   $('.article__strong--email').text(user.email);
   $('.article__strong--stride').text(user.strideLength);
-  $('.article__p--daily-water').text(hydrationUser.findDailyHydration('2019/06/15', 1));
-  $('.article__p--weekly').text(hydrationUser.findWeeklyHydration(1))
+  $('.article__p--daily-water').text(hydrationUser.findDailyHydration('2019/06/15', 2));
+  $('.article__p--weekly').text(hydrationUser.findWeeklyHydration(2));
+  $('.artile__p--average-sleep').text(sleepRepository.returnSleepAvg(2))
 
   
 });

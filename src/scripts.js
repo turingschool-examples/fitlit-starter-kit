0055b2a8fba558acad02ced1d01bb11258c12008
Matthew Malone <m.malone@me.com>
2019-08-22 18:20:25 -0600

    Completed sleep class and tests and put info onto the DOM
    Co-authored-by: Scott Schipke <swschipke@gmail.com>

--- src/scripts.js
+++ src/scripts.js
@@ -5,6 +5,9 @@ $(document).ready(() => {
   const hydroRepo = new HydrationRepository(sampleHydration);
   const userHydro = new Hydration(hydroRepo.getUserData(user.id));
 
+  const sleepRepo = new SleepRepository(sampleSleep);
+  const sleepyPerson = new Sleep(sleepRepo.getUserData(user.id));
+
   $('.header__div--h2').text(`Hi, ${user.getUserFirstName()}!`);
 
   $('.header__div__user-stepgoal').text(`Your step goal: ${user.dailyStepGoal} steps`);
@@ -19,20 +22,37 @@ $(document).ready(() => {
 
   $('.main__hydration--thisWeek').after(`${buildWeeklyHTML('oz')}`)
 
+  $('.main__sleep--thisWeek').after(`${buildWeeklyHTMLSleep()}`)
+
+
+  function buildWeeklyHTMLSleep() {
+    const weeklyMap = sleepyPerson.getWeek('2019/06/25').map(function (day) {
+      return insertWeeklySleepStats(day)
+    })
+    return weeklyMap.join(' ');
+  }
+
+  function insertWeeklySleepStats(obj) {
+    return `<p class="inserted--p"> ${milisecondsToDate(obj.date)}: ${obj.hoursSlept} hours slept with a sleep quality of ${obj.sleepQuality}  </p>`
+  }
+
+  function milisecondsToDate(miliseconds) {
+    return new Date(miliseconds).toString().slice(0, 10)
+  }
 
   function buildWeeklyHTML(unit) {
-  const weeklyMap = userHydro.weeklyArr.map(function(day) {
-    return insertWeeklyStats(day, unit)
-  })
-return weeklyMap.join(' ');
-}
+    const weeklyMap = userHydro.weeklyArr.map(function(day) {
+      return insertWeeklyStats(day, unit)
+    })
+    return weeklyMap.join(' ');
+  }
 
   function getRandomNumber() {
     return Math.floor(Math.random() * sampleData.length)
   }
   
   function insertWeeklyStats(obj, unit) {
-  return `<p class="inserted--p"> ${obj.date}: ${obj.numOunces} ${unit} </p>`
+    return `<p class="inserted--p"> ${milisecondsToDate(obj.date)}: ${obj.numOunces} ${unit} </p>`
   }
 
 })
--- test/test-Sleep.js
+++ test/test-Sleep.js
@@ -5,14 +5,140 @@ const sampleHydration = require('./sampleHydratation');
 const User = require('../src/User');
 
 const SleepRepository = require('../src/SleepRepository');
+const Sleep = require('../src/Sleep');
 const sampleSleep = require('./sampleSleep');
 
 describe('Sleep', () => {
 
   it('should be an instance of class Sleep', () => {
-    const sleepyPerson = new SleepRepository(sampleSleep);
-    expect(sleepyPerson).to.be.an.instanceOf(SleepRepository)
+    const sleepyPerson = new Sleep(sampleSleep);
+    expect(sleepyPerson).to.be.an.instanceOf(Sleep)
   })
 
-  
+  it('should find average number of hours slept per day', () => {
+    const sleepRepo = new SleepRepository(sampleSleep);
+    const sleepyPerson = new Sleep(sleepRepo.getUserData(6));
+    expect(sleepyPerson.userTotalAvg('hoursSlept')).to.equal(8);
+  })
+
+  it('should find average sleep quality per day over time', () => {
+    const sleepRepo = new SleepRepository(sampleSleep);
+    const sleepyPerson = new Sleep(sleepRepo.getUserData(6));
+    expect(sleepyPerson.userTotalAvg('sleepQuality')).to.equal(3);
+  })
+
+  it('should find how many hours a user slept on a specific day', () => {
+    const sleepRepo = new SleepRepository(sampleSleep);
+    const sleepyPerson = new Sleep(sleepRepo.getUserData(6));
+    expect(sleepyPerson.getStatsFromDay('2019/06/23', 'hoursSlept')).to.equal(8.5);
+  })
+
+  it('should find sleep quality of a useer on a specific day', () => {
+    const sleepRepo = new SleepRepository(sampleSleep);
+    const sleepyPerson = new Sleep(sleepRepo.getUserData(1));
+    expect(sleepyPerson.getStatsFromDay('2019/06/24', 'sleepQuality')).to.equal(1.3);
+  })
+
+  it('should show how average hours a user slept each day over the course of a given week (any week given)', () => {
+    const sleepRepo = new SleepRepository(sampleSleep);
+    const sleepyPerson = new Sleep(sleepRepo.getUserData(1));
+    expect(sleepyPerson.getWeek('2019/06/24')).to.eql([{
+      userID: 1,
+      date: 1561356000000,
+      hoursSlept: 8,
+      sleepQuality: 1.3
+    },
+    {
+      userID: 1,
+      date: 1561269600000,
+      hoursSlept: 7.8,
+      sleepQuality: 1.5
+    },
+    { userID: 1, 
+      date: 1561183200000, 
+      hoursSlept: 7, 
+      sleepQuality: 3 
+    },
+    {
+      userID: 1,
+      date: 1561096800000,
+      hoursSlept: 7.8,
+      sleepQuality: 4.2
+    },
+    {
+      userID: 1,
+      date: 1561010400000,
+      hoursSlept: 9.3,
+      sleepQuality: 1.2
+    },
+    {
+      userID: 1,
+      date: 1560924000000,
+      hoursSlept: 10.7,
+      sleepQuality: 1.2
+    },
+    {
+      userID: 1,
+      date: 1560837600000,
+      hoursSlept: 10.4,
+      sleepQuality: 3.1
+    }])
+    
+    expect(sleepyPerson.getWeeklyAvg('2019/06/24', 'hoursSlept')).to.equal(8.7)
+
+  })
+
+
+  it('should find average sleep quality of a user each day over the course of a given week (any week given)', () => {
+    const sleepRepo = new SleepRepository(sampleSleep);
+    const sleepyPerson = new Sleep(sleepRepo.getUserData(1));
+    expect(sleepyPerson.getWeeklyAvg('2019/06/24', 'sleepQuality')).to.equal(2.2)
+  })
+
+  it('should show pull user data for a given week (any week given)', () => {
+    const sleepRepo = new SleepRepository(sampleSleep);
+    const sleepyPerson = new Sleep(sleepRepo.getUserData(1));
+    expect(sleepyPerson.getWeek('2019/06/24')).to.eql([{
+      userID: 1,
+      date: 1561356000000,
+      hoursSlept: 8,
+      sleepQuality: 1.3
+    },
+    {
+      userID: 1,
+      date: 1561269600000,
+      hoursSlept: 7.8,
+      sleepQuality: 1.5
+    },
+    {
+      userID: 1,
+      date: 1561183200000,
+      hoursSlept: 7,
+      sleepQuality: 3
+    },
+    {
+      userID: 1,
+      date: 1561096800000,
+      hoursSlept: 7.8,
+      sleepQuality: 4.2
+    },
+    {
+      userID: 1,
+      date: 1561010400000,
+      hoursSlept: 9.3,
+      sleepQuality: 1.2
+    },
+    {
+      userID: 1,
+      date: 1560924000000,
+      hoursSlept: 10.7,
+      sleepQuality: 1.2
+    },
+    {
+      userID: 1,
+      date: 1560837600000,
+      hoursSlept: 10.4,
+      sleepQuality: 3.1
+    }])
+  })
 })

cff23694f0c16d207bb2242dc245b4aaba88832c
VPAbraham <vpa456@gmail.com>
2019-08-22 22:28:09 -0600

    Continue to build out SleepRepository and create SleepRepository testing

---
+++ test/sleepRepository-test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const userData = require('../data-subsets/users-subset');
+const SleepRepository = require('../src/SleepRepository');
+
+describe('SleepRepository', () => {
+
+  it('should be a function', () => {
+    expect(SleepRepository).to.be.a('function');
+  });
+
+  it('should be an instance of the sleep repository', () => {
+    const sleepRepository = new SleepRepository(userData);
+    expect(sleepRepository).to.be.an.instanceof(SleepRepository);
+  });
+
+  it('should be able to calculate the sleep quality of all users', () => {
+    const sleepRepository = new SleepRepository(userId);
+    expect(sleepRepository).to.be.an.instanceof(SleepRepository);
+  });
+
+
+
+});

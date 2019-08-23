322b0e9ec53863695a623da4f9a7efdf3055b06a
Eduardo <jeduardorjx@gmail.com>
2019-08-22 20:08:05 -0600

    Add water consumed in a week to display on the DOM

--- src/scripts.js
+++ src/scripts.js
@@ -2,6 +2,7 @@ let randomNum = Math.floor(Math.random() * 50 +1);
 let randomUser = userData.find(user => {
   return user.id === randomNum;
 })
+
 const hydration = new Hydration(hydrationData, randomUser.id);
 const users = new UserRepository(userData);
 
@@ -19,14 +20,22 @@ dailySection.insertAdjacentHTML('afterbegin', `<article class="user_info">
   <p>Daily Step Goal: ${randomUser.dailyStepGoal} / ${users.calculateAverageStepGoals()}</p>
 </article>`)
 
-hydration.extractSingleUser()
+hydration.extractSingleUser();
+let waterConsumedToday = hydration.calculateDailyWaterIntake();
 let singleUserIntakeData = hydration.singleUserData;
 let weeklyOunces = hydration.calculateWeeklyWaterIntake();
-console.log("it worked", weeklyOunces)
-// let latestWaterIntake = singleUserIntakeData[singleUserIntakeData.length - 1]
 
-// hydration.calculateWeeklyWaterIntake()
+
 
 dailySection.insertAdjacentHTML('beforeend', `<article>
- <p>Daily Water Intake: ${latestWaterIntake.numOunces}</p>
+ <p>Daily Water Intake: ${waterConsumedToday}</p>
+ <ul>Water consumed this week:
+  <li>${weeklyOunces[0].date} : ${weeklyOunces[0].numOunces} ounces</li>
+  <li>${weeklyOunces[1].date} : ${weeklyOunces[1].numOunces} ounces</li>
+  <li>${weeklyOunces[2].date} : ${weeklyOunces[2].numOunces} ounces</li>
+  <li>${weeklyOunces[3].date} : ${weeklyOunces[3].numOunces} ounces</li>
+  <li>${weeklyOunces[4].date} : ${weeklyOunces[4].numOunces} ounces</li>
+  <li>${weeklyOunces[5].date} : ${weeklyOunces[5].numOunces} ounces</li>
+  <li>${weeklyOunces[6].date} : ${weeklyOunces[6].numOunces} ounces</li>
+ </ul>
  </article>`)

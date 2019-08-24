83047aea3e604957a6d566482d33bb442e847f24
dawnlunacy <rudd.lacy@gmail.com>
2019-08-23 18:27:58 -0600

    Add updates to dom in scripts and files for images

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,37 @@
 console.log("Hello World");
 
+<<<<<<< Updated upstream
 const idRandom = Math.ceil(Math.random() * 50 - 1);
 const userRepository = new UserRepository(userData);
 const userDataArray = userRepository.fetchUserData(idRandom);
 const currentUser = new User(userDataArray);
 
 
+=======
+$(document).ready(() => {
+ 
+  const idRandom = Math.ceil(Math.random() * 50 - 1);
+  const userRepository = new UserRepository(userData);
+  const userDataArray = userRepository.fetchUserData(idRandom);
+  const currentUser = new User(userDataArray);
+
+  const $profileInfoSection = $('.profile-info');
+  const $sleepinfo = $('.sleep-info');
+  const $hydrationInfo = $('.hydration-info');
+  const $activityInfo = $('.activity-info');
+
+
+  $('.profile-name').text(currentUser.name);
+  $('.profile-address').text(currentUser.address);
+  $('.profile-email').text(currentUser.email);
+  $('.profile-stride-length').text(currentUser.strideLength);
+  $('.profile-daily-step-goal').text(currentUser.dailyStepGoal);
+  
+  
+
+
+
+});
+>>>>>>> Stashed changes
 
 
---
+++ test/Hydration-test.js
@@ -0,0 +1,70 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Hydration = require('../src/Hydration');
+const mockHydrationData = require('../mock-data/mock-hydration');
+const mockHydrationUser1 = require('../mock-data/mock-hydration-user1')[0];
+const hydrationUser1ForAWeek = require('../mock-data/mock-hydration-user1')[1];
+
+
+let hydration;
+
+describe('Hydration', () => {
+
+  beforeEach( () => {
+    hydration = new Hydration(mockHydrationData, 1); 
+    hydration.findCurrentUserData();
+  });
+
+  it('should be a function', () => {
+    expect(Hydration).to.be.a('function');
+  });
+
+  it('should be an instance of Hydration', () => {
+    expect(hydration).to.be.an.instanceof(Hydration);
+  });
+
+  it('should have a parameter to take in all of the hydration data', () => {
+    expect(hydration.allHydrationData).to.eql(mockHydrationData)
+  })
+
+  it('should have a parameter to take in an id for the current user', () => {
+    expect(hydration.currentUserId).to.eql(1);
+  })
+
+  describe('findCurrentUserHydrationData', () => {
+    it('should return a list of the current user/s hydration information', () => {
+      expect(hydration.currentUserData).to.eql(mockHydrationUser1)
+    });
+  });
+
+  describe('findAverageFluidOzConsumedPerDayForAllTime', () => {
+    it('should find the average fluid oz consumed per day for all time rounding to the nearest whole number', () => {
+      expect(hydration.findAverageFluidOzConsumedPerDayForAllTime()).to.equal(60);
+    });
+  });
+    
+  describe('findAverageFluidOzConsumedforSpecificDay', () => {
+    it('should find the fluid oz consumed for a specific day', () => {
+      expect(hydration.findAverageFluidOzConsumedforSpecificDay("2019/06/15")).to.equal(37);
+      expect(hydration.findAverageFluidOzConsumedforSpecificDay("2019/06/16")).to.equal(69);
+      expect(hydration.findAverageFluidOzConsumedforSpecificDay("2019/06/24")).to.not.equal(62);
+      expect(hydration.findAverageFluidOzConsumedforSpecificDay("2019/06/24")).to.equal(61);
+    });
+  });
+
+  describe('findFluidOzConsumedEveryDayOverSpecificWeek', () => {
+    it('should find the fluid oz consumed for each day over the course of a specified week', () => {
+      expect(hydration.findFluidOzConsumedEveryDayOverSpecificWeek("2019/06/15", "2019/06/21")).to.eql(hydrationUser1ForAWeek);
+    });
+
+    describe('findTodaysDate', () => {
+      it('should return the last date from the user/s data to represent today', () => {
+        hydration.findTodaysDate();
+        expect(hydration.today).to.equal("2019/06/24")
+      });
+    });
+  });
+
+});
+
+

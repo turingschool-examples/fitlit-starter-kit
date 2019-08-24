e29990d5f92796ae0b7120322298532207850a83
dawnlunacy <rudd.lacy@gmail.com>
2019-08-23 19:34:37 -0600

    Add today's date

--- src/scripts.js
+++ src/scripts.js
@@ -11,11 +11,32 @@ $(document).ready(() => {
   const $hydrationInfo = $('.hydration-info');
   const $activityInfo = $('.activity-info');
 
+  $('.profile-first-name').text(currentUser.findUserFirstName());
   $('.profile-name').text(currentUser.name);
   $('.profile-address').text(currentUser.address);
   $('.profile-email').text(currentUser.email);
   $('.profile-stride-length').text(currentUser.strideLength);
   $('.profile-daily-step-goal').text(currentUser.dailyStepGoal);
   $('.avg-step-goal-of-all-users').text(userRepository.findAverageStepGoalOfAllUsers());
+  $('.today').text(dateToday());
 
 })
+
+
+const dateToday = () => {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1; 
+    // console.log(new Intl.DateTimeFormat('en-US', mm).format(today));
+    const yyyy = today.getFullYear();
+    if (dd<10) {
+        dd=`0${dd}`;
+    } 
+    if (mm<10) {
+        mm=`0${mm}`;
+    } 
+// today = `${yyyy}/${mm}/${dd}`;
+today = `${mm}/${dd}/${yyyy}`;
+// console.log(today);
+return today;
+}

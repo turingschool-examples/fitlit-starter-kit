2de342e582f43b37121de1291eb29223d8b98b14
Scott Schipke <swschipke@gmail.com>
2019-08-24 12:30:44 -0600

    Addweekly averages for all users to DOM and refactor
    Co-authored-by: Matthew Malone <m.malone@me.com>

--- src/scripts.js
+++ src/scripts.js
@@ -8,25 +8,38 @@ $(document).ready(() => {
   const sleepRepo = new SleepRepository(sampleSleep);
   const sleepyPerson = new Sleep(sleepRepo.getUserData(user.id));
 
+  const sampleDate = '2019/06/25'
+
   $('.header__div--h2').text(`Hi, ${user.getUserFirstName()}!`);
 
   $('.header__div__user-stepgoal').text(`Your step goal: ${user.dailyStepGoal} steps`);
 
   $('.header__div__allusers-stepgoal').text(`Average User Goal: ${repo.getAllUsersAvgStepGoal()} steps`);
 
-  $('.hydration__container--consumed--today').text(`Consumed today: ${userHydro.userHydrationByDate('2019/06/25')}oz`);
+  $('.hydration__container--consumed--today').text(`Consumed today: ${userHydro.userHydrationByDate(sampleDate)}oz`);
 
-  userHydro.getHydroArray('2019/06/25');
+  userHydro.getHydroArray(sampleDate);
 
   $('.hydration__container--consumed--this--week').text(`Daily average consumed this week: ${userHydro.getWeeklyHydroAvg()}oz`);
 
   $('.main__hydration--thisWeek').after(`${buildWeeklyHTML('oz')}`)
 
-  $('.main__sleep--thisWeek').after(`${buildWeeklyHTMLSleep()}`)
+  $('.sleep--week-byDay').after(buildWeeklyHTMLSleep())
+
+  $('.sleep__container--hours--today').text(`Hours slept today: ${sleepyPerson.getSleepHoursByDate(sampleDate)} hours`)
+
+  $('.sleep__container--hours--this--week').text(`Average hours slept this week: ${sleepyPerson.getWeeklyAvg(sampleDate, 'hoursSlept')} hours`)
+
+  $('.sleep--week--avg').after(`<p class="main__sleep__average--all-users">
+              Hours slept this week: ${sleepRepo.getAvgSleepStatsAllUsers(sleepRepo.getWeekOfUsers(sampleDate), 'hoursSlept')} hours
+            </p>
+            <p class="main__sleep__average--all-users">
+              Sleep quality this week: ${sleepRepo.getAvgSleepStatsAllUsers(sleepRepo.getWeekOfUsers(sampleDate), 'sleepQuality')}
+            </p>`)
 
 
   function buildWeeklyHTMLSleep() {
-    const weeklyMap = sleepyPerson.getWeek('2019/06/25').map(function (day) {
+    const weeklyMap = sleepyPerson.getWeek(sampleDate).map(function (day) {
       return insertWeeklySleepStats(day)
     })
     return weeklyMap.join(' ');

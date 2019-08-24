72426de2e86ee2c59d2207bfade5b05048d4cd41
dawnlunacy <rudd.lacy@gmail.com>
2019-08-24 16:49:03 -0600

    Add miles walked on specific day for a user  test and method for activity-test

--- src/Activity.js
+++ src/Activity.js
@@ -1,15 +1,27 @@
 class Activity {
-  constructor(allActivityData, id) {
+  constructor(allActivityData, id, allUsers) {
     this.allActivityData = allActivityData;
     this.currentUserId = id;
     this.currentUserData;
+    this.allUsers = allUsers;
+    // console.log("line7", this.allUsers)
+    
   }
 
   findCurrentUserData() {
     this.currentUserData = this.allActivityData.filter((data) => data.userID === this.currentUserId);
     return this.currentUserData;
   }
+
+  findMilesWalkedForSpecificDayOfUser(dateString) {
+    let dayToCalculateSteps = this.currentUserData.filter(day => day.date === dateString)[0].numSteps;
+    let strideLength = this.allUsers.filter(users => users.id === this.currentUserId)[0].strideLength;
+    let totalFeet = dayToCalculateSteps / strideLength
+    let milesWalked = 5280 / totalFeet;
+    return parseFloat(milesWalked.toFixed(2));
+  }
 }
+
 
 
 
--- test/Activity-test.js
+++ test/Activity-test.js
@@ -2,13 +2,15 @@ const chai = require('chai');
 const expect = chai.expect;
 const Activity = require('../src/Activity');
 const mockActivityData = require('../mock-data/mock-activity');
+const mockUsers = require('../mock-data/mock-users');
+const mockActivityUser2Data = require('../mock-data/mock-activity-user2');
 
 let activity;
 
 describe('Activity', () => {
 
   beforeEach( () => {
-    activity = new Activity(mockActivityData, 2); 
+    activity = new Activity(mockActivityData, 2, mockUsers); 
     activity.findCurrentUserData();
   });
 
@@ -30,83 +32,13 @@ describe('Activity', () => {
 
   describe('findCurrentUserData', () => {
     it('should return a list of the current user/s activity information', () => {
-      expect(activity.currentUserData).to.eql([{
-        "userID": 2,
-        "date": "2019/06/15",
-        "numSteps": 4294,
-        "minutesActive": 138,
-        "flightsOfStairs": 10
-      },
-      {
-        "userID": 2,
-        "date": "2019/06/16",
-        "numSteps": 4112,
-        "minutesActive": 220,
-        "flightsOfStairs": 37
-      },
-      {
-        "userID": 2,
-        "date": "2019/06/17",
-        "numSteps": 13750,
-        "minutesActive": 65,
-        "flightsOfStairs": 4
-      },
-      {
-        "userID": 2,
-        "date": "2019/06/18",
-        "numSteps": 4662,
-        "minutesActive": 181,
-        "flightsOfStairs": 31
-      },
-      {
-        "userID": 2,
-        "date": "2019/06/19",
-        "numSteps": 9858,
-        "minutesActive": 243,
-        "flightsOfStairs": 44
-      },
-      {
-        "userID": 2,
-        "date": "2019/06/20",
-        "numSteps": 8153,
-        "minutesActive": 74,
-        "flightsOfStairs": 10
-      },
-      {
-        "userID": 2,
-        "date": "2019/06/21",
-        "numSteps": 10225,
-        "minutesActive": 174,
-        "flightsOfStairs": 26
-      },
-      {
-        "userID": 2,
-        "date": "2019/06/22",
-        "numSteps": 3605,
-        "minutesActive": 124,
-        "flightsOfStairs": 31
-      },
-      {
-        "userID": 2,
-        "date": "2019/06/23",
-        "numSteps": 4148,
-        "minutesActive": 142,
-        "flightsOfStairs": 0
-      },
-      {
-        "userID": 2,
-        "date": "2019/06/24",
-        "numSteps": 8568,
-        "minutesActive": 81,
-        "flightsOfStairs": 31
-      }
-      ])
+      expect(activity.currentUserData).to.eql(mockActivityUser2Data)
     });
   });
 
   describe('findMilesWalkedForSpecificDayOfUser', () => {
     it('should return the miles walked for a specific day', () => {
-      expect(activity.findMilesWalkedForSpecificDayOfUser()).to.equal()
+      expect(activity.findMilesWalkedForSpecificDayOfUser("2019/06/17")).to.equal(1.73)
     })
   })
 

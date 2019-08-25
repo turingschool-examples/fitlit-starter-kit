3218b9c0ff2b5946b08acd69d8e6f346fbac46ed
posi7790 <posi7790@colorado.edu>
2019-08-25 13:50:39 -0600

    Commit to save and merge master

--- src/scripts.js
+++ src/scripts.js
@@ -1,14 +1,44 @@
+//Generate random user 
+const uniqueUserIndex = Math.floor(Math.random() * (50 - 1 + 1)) + 1;
+
 //Repo variables
-const UserRepo = new UserRepo();
-const ActivityRepo = new ActivityRepo();
-const HydrationRepo = new HydrationRepo();
-const SleepRepo = new SleepRepo();
+const userRepo = new UserRepo(userData);
+const sleepRepo = new SleepRepo(sleepData);
+const activityRepo = new ActivityRepo(activityData);
 
 //Individual Class Repos
-const User = new User();
-const Activity = new Activity();
-const Hydration = new Hydration();
-const Sleep = new Sleep();
+const user = new User(userData[uniqueUserIndex]);
+const hydration = new Hydration(hydrationData, user.id);
+const sleep = new Sleep(sleepData, user.id);
+const activity = new Activity(activityData, user.id);
 
+//Date
+const date = activityData[0].date;
 
+//User Section
+$('.username').text(`${user.returnUserName()}`)
 
+//Date Section
+function reformatDate(date) {
+  let arr = date.split('/');
+  return [arr[1], arr[2], arr[0]].join('/');
+}
+
+$('.date').text(`${reformatDate(date)}`);
+
+//Hydration
+$('.water-consumed').text(`Daily Water Consumption: ${hydration.returnDailyFluidOunces(date)}`);
+$('.water-consumed-week').text(`Weekly Water Consumption: ${hydration.returnWeeklyNumOunces()}`);
+
+//Sleep
+$('.hours-slept-day').text(`Hours Slept Last Night: ${sleep.returnSleepHours(date)}`);
+$('.hours-slept-week').text(`Hours Slept Last Week: ${sleep.returnWeekOfSleepHours(1)}`);
+$('.hours-slept-all-time').text(`Hours Slept On Average: ${sleep.returnAvgSleepHours()}`);
+$('.quality-sleep-day').text(`Quality of Sleep Last Night: ${sleep.returnSleepQuality(date)}`);
+$('.quality-sleep-week').text(`Quality of Sleep Last Week: ${sleep.returnWeekOfSleepQuality(1)}`);
+$('.quality-sleep-all-time').text(`Quality of Sleep On Average: ${sleep.returnAvgSleepQuality()}`);
+
+//Activity Section
+$('.user-step-goal').text(`Daily Step Goal: ${user.dailyStepGoal}`);
+$('.average-step-goal').text(`Average Step Goal: ${userRepo.returnAverageStepGoal()}`);
+$('.number-of-steps-day').text(`Daily Steps: ${userRepo.returnNumStepsDay(date)}`);

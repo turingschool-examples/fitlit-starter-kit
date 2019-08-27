ecbce75c5d00ebd7f8e55b0518a4ca6e793bd92b
“Elyse <elysemyers1@gmail.com>
2019-08-27 16:26:51 -0600

    Adds chart for displaying user weekly hydration data

--- src/scripts.js
+++ src/scripts.js
@@ -1,51 +1,99 @@
-$(document).ready(() => {
-  const currentDate = '2019/06/30';
+const currentDate = '2019/06/30';
+const userRepo = new UserRepository(userData);
+let user = userRepo.returnUserData(23);
+let newUser = new User(user);
+let hydration = new Hydration(hydrationData);
+let sleep = new Sleep(sleepData);
+let activity = new Activity(activityData)
+let friendObjs = populateFriends(user.friends);
 
-  const userRepo = new UserRepository(userData);
-  let user = userRepo.returnUserData(15);
-  let newUser = new User(user);
-  let hydration = new Hydration(hydrationData);
-  let sleep = new Sleep(sleepData);
-  let activity = new Activity(activityData)
+$('#user-name').text(newUser.returnUserFirstName());
+$('#user-info-name').text(newUser.name);
+$('#user-info-email').text(newUser.email);
+$('#user-info-address').text(newUser.address);
+$('#user-info-step-goal').text(newUser.dailyStepGoal);
+$('#average-step-goal-all-users').text(userRepo.returnAllUsersAverageStepGoal());
+$('#user-water-by-day').text(hydration.returnFluidOzByDate(user.id, currentDate));
+$('#user-sleep-by-day').text(sleep.returnAmountSlept(user.id, currentDate));
+$('#user-sleep-quality-by-day').text(sleep.returnSleepQuality(user.id, currentDate));
+$('#user-sleep-by-week').text(sleep.returnSleepByWeek(user.id, currentDate));
+$('#user-sleep-quality-by-week').text(sleep.returnSleepQualityByWeek(user.id, currentDate));
+$('#user-average-sleep-quality').text(sleep.returnAverageSleepQuality(user.id));
+$('#user-average-hours-slept').text(sleep.returnAverageSleep(user.id));
+$('#user-current-step-count').text(activity.returnNumberOfStepsByDate(user.id, currentDate))
+$('#user-is-rested').text(displaySleepStatus())
+$('#user-current-mins-active').text(activity.returnActiveMinutesByDate(user.id, currentDate));
+$('#user-current-miles-walked').text(activity.returnMilesWalkedByDate(user, currentDate));
+$('#user-current-step-count-vs-average').text(activity.returnNumberOfStepsByDate(user.id, currentDate))
+$('#all-users-average-step-count').text(activity.returnAvgStepsTakenAllUsersByDate(currentDate));
+$('#user-current-stairs-climbed').text(activity.returnStairsClimbedByDate(user.id, currentDate))
+$('#all-users-average-stairs-climbed').text(activity.returnAvgStairsClimbedAllUsersByDate(currentDate));
+$('#user-current-active-mins').text(activity.returnActiveMinutesByDate(user.id, currentDate))
+$('#all-users-average-active-mins').text(activity.returnAvgActiveMinutesAllUsersByDate(currentDate));
+$('#user-step-count-by-week').text(activity.returnNumberOfStepsByWeek(user.id, currentDate))
+$('#user-stairs-climbed-by-week').text(activity.returnStairsClimbedByWeek(user.id, currentDate))
+$('#user-mins-active-by-week').text(activity.returnActiveMinutesByWeek(user.id, currentDate))
 
-  function populateFriends (userFriends) {
-    let friends = userFriends.map(friend => {
-      let userFriend = new User(userRepo.returnUserData(friend))
-      return ({id: userFriend.id, name: userFriend.name, steps: activity.returnNumberOfStepsByDate(userFriend.id, currentDate) })
-    });
-    return friends;
+function populateFriends (userFriends) {
+  let friends = userFriends.map(friend => {
+    let userFriend = new User(userRepo.returnUserData(friend))
+    return ({id: userFriend.id, name: userFriend.name, steps: activity.returnNumberOfStepsByDate(userFriend.id, currentDate) })
+  });
+  return friends;
+}
+
+friendObjs.forEach(friend => $('#friend-info').append(`<p>Friend name: ${friend.name}, steps: ${friend.steps}</p>`))
+
+function displaySleepStatus() {
+  if(this.isRested === true) {
+    $('#sleep-status').attr('src', '/images/ghost-happy.svg');
+  } else {
+    $('#sleep-status').attr('src', '/images/ghost-sad.svg');
   }
+}
 
-  let friendObjs = populateFriends(user.friends);
-    
-  $('#user-name').text(newUser.returnUserFirstName());
-  $('#user-info-name').text(newUser.name);
-  $('#user-info-email').text(newUser.email);
-  $('#user-info-address').text(newUser.address);
-  $('#user-info-step-goal').text(newUser.dailyStepGoal);
-  $('#average-step-goal-all-users').text(userRepo.returnAllUsersAverageStepGoal());
-  $('#user-water-by-day').text(hydration.returnFluidOzByDate(user.id, currentDate));
-  $('#user-water-by-week').text(hydration.returnFluidOzByWeek(user.id, currentDate));
-  $('#user-sleep-by-day').text(sleep.returnAmountSlept(user.id, currentDate));
-  $('#user-sleep-quality-by-day').text(sleep.returnSleepQuality(user.id, currentDate));
-  $('#user-sleep-by-week').text(sleep.returnSleepByWeek(user.id, currentDate));
-  $('#user-sleep-quality-by-week').text(sleep.returnSleepQualityByWeek(user.id, currentDate));
-  $('#user-average-sleep-quality').text(sleep.returnAverageSleepQuality(user.id));
-  $('#user-average-hours-slept').text(sleep.returnAverageSleep(user.id));
-  $('#user-current-step-count').text(activity.returnNumberOfStepsByDate(user.id, currentDate))
-  $('#user-current-mins-active').text(activity.returnActiveMinutesByDate(user.id, currentDate));
-  $('#user-current-miles-walked').text(activity.returnMilesWalkedByDate(user, currentDate));
-  $('#user-current-step-count-vs-average').text(activity.returnNumberOfStepsByDate(user.id, currentDate))
-  $('#all-users-average-step-count').text(activity.returnAvgStepsTakenAllUsersByDate(currentDate));
-  $('#user-current-stairs-climbed').text(activity.returnStairsClimbedByDate(user.id, currentDate))
-  $('#all-users-average-stairs-climbed').text(activity.returnAvgStairsClimbedAllUsersByDate(currentDate));
-  $('#user-current-active-mins').text(activity.returnActiveMinutesByDate(user.id, currentDate))
-  $('#all-users-average-active-mins').text(activity.returnAvgActiveMinutesAllUsersByDate(currentDate));
-  $('#user-step-count-by-week').text(activity.returnNumberOfStepsByWeek(user.id, currentDate))
-  $('#user-stairs-climbed-by-week').text(activity.returnStairsClimbedByWeek(user.id, currentDate))
-  $('#user-mins-active-by-week').text(activity.returnActiveMinutesByWeek(user.id, currentDate))
+function returnDatesOfWeek(userId, date) {
+  let userData = activity.findCurrentUserData(userId);
+  let index = userData.findIndex((data) => data.date === date);
 
-  friendObjs.forEach(friend => $('#friend-info').append(`<p>Friend name: ${friend.name}, steps: ${friend.steps}</p>`))
+  return userData.splice(index - 6, 7).map(day => day.date);
+}
 
+$('#user-water-by-week').text(hydration.returnFluidOzByWeek(user.id, currentDate));
+var ctx = $('#user-water-by-week');
+var hydrationByWeek = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: returnDatesOfWeek(user.id, currentDate),
+    datasets: [{
+      label: 'Water By Week',
+      data: hydration.returnFluidOzByWeek(user.id, currentDate),
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
 });
-

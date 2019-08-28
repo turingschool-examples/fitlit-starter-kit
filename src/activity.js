bda9bb7ef1ae5031106f180188888c253e31e086
Colin Turing <colin.koga@gmail.com>
2019-08-28 10:32:47 -0600

    Add three test for displaying a weeks worth of steps stairs climbed and min active

--- src/activity.js
+++ src/activity.js
@@ -9,18 +9,22 @@ class Activity {
 
   calculateMilesWalked(date, id) {
     let userRepo = new UserRepository(userData);
-    
+
     let stride = userRepo.users.find(user => user.id === id).strideLength;
 
-    let numberSteps = this.data.filter(user => user.date === date).find(user => user.userID === id).numSteps;
+    let numberSteps = this.data
+      .filter(user => user.date === date)
+      .find(user => user.userID === id).numSteps;
 
-    return parseFloat(((stride * numberSteps / 5280).toFixed(1)))
-  };
+    return parseFloat(((stride * numberSteps) / 5280).toFixed(1));
+  }
 
   calculateMinActive(date, id) {
-    let userActivity = this.data.filter(user => user.userID === id).find(day => day.date === date).minutesActive;
- 
-  return userActivity
+    let userActivity = this.data
+      .filter(user => user.userID === id)
+      .find(day => day.date === date).minutesActive;
+
+    return userActivity;
   }
 
   calculateAvgTimeActive(date, id) {
@@ -31,27 +35,29 @@ class Activity {
     let week = userActivity.splice(day - 5, day + 2);
 
     let totalMin = week.reduce((acc, min) => {
-      return acc += min.minutesActive
+      return (acc += min.minutesActive);
     }, 0);
 
     return parseFloat((totalMin / week.length).toFixed(1));
-  };
+  }
 
   compareGoal(date, id) {
     let userRepo = new UserRepository(userData);
 
     let user = userRepo.users.find(user => user.id === id);
 
-    let goal = user.dailyStepGoal
-    
-    let userSteps = this.data.filter(user => user.userID === id).find(steps => steps.date === date).numSteps;
+    let goal = user.dailyStepGoal;
+
+    let userSteps = this.data
+      .filter(user => user.userID === id)
+      .find(steps => steps.date === date).numSteps;
 
     if (userSteps > goal) {
-      return true
+      return true;
     } else {
-      return false
-    };
-  };
+      return false;
+    }
+  }
 
   findGoalDays(id) {
     let userRepo = new UserRepository(userData);
@@ -64,27 +70,62 @@ class Activity {
 
     let goalArray = userSteps.filter(steps => steps.numSteps > goal);
 
-    return goalArray
-  };
+    return goalArray;
+  }
 
   findMostStairs(id) {
     let userStairs = this.data.filter(user => user.userID === id);
 
-     userStairs.sort((a, b) => {
-     return b.flightsOfStairs - a.flightsOfStairs
+    userStairs.sort((a, b) => {
+      return b.flightsOfStairs - a.flightsOfStairs;
     });
-    
+
     return userStairs[0];
-  };
+  }
 
   calculateStepsAMin(date, id) {
     let userActivity = this.data.filter(user => user.userID === id);
 
     let day = userActivity.find(day => day.date === date);
 
-    let stepsAMin = (day.numSteps / day.minutesActive).toFixed(1)
-    
-    return parseFloat(stepsAMin)
+    let stepsAMin = (day.numSteps / day.minutesActive).toFixed(1);
+
+    return parseFloat(stepsAMin);
+  }
+
+  returnWeekStep(date, id) {
+    let userActivity = this.data.filter(user => user.userID === id);
+
+    let targetDay = userActivity.findIndex(day => day.date === date);
+
+    let targetWeek = userActivity.slice(targetDay - 6, targetDay + 1);
+
+    return targetWeek.map(elem => elem.numSteps);
+  }
+
+  returnWeekStairs(date, id) {
+    let userActivity = this.data.filter(user => user.userID === id);
+
+    let targetDay = userActivity.findIndex(day => day.date === date);
+
+    let targetWeek = userActivity.slice(targetDay - 6, targetDay + 1);
+
+    let array = targetWeek.map(elem => elem.flightsOfStairs);
+
+    console.log(array);
+    return array;
+  }
+
+  returnWeekMin(date, id) {
+    let userActivity = this.data.filter(user => user.userID === id);
+
+    let targetDay = userActivity.findIndex(day => day.date === date);
+
+    let targetWeek = userActivity.slice(targetDay - 6, targetDay + 1);
+
+    let array = targetWeek.map(elem => elem.minutesActive);
+
+    return array;
   }
 }
 

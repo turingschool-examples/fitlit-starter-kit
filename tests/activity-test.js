aded01fcaae6f2b5413618e64e9ea70d63813da4
Colin Turing <colin.koga@gmail.com>
2019-08-28 17:10:40 -0600

    Add consecutive increase step method to activity file and testing

--- tests/activity-test.js
+++ tests/activity-test.js
@@ -79,11 +79,71 @@ describe('Activity', () => {
     expect(activity.returnStairsDay("2019/06/20", 1)).to.equal(12);
   });
 
-  it('should return an array of data for a users friends', () => {
-    expect(activity.getFriends(1))
-  })
-
-  // it('should compare user steps for a week compared to friends', () => {
-  //   expect(activity.determineWinner('2019/06/20'))
-  // })
+  it('should return an array of only consecutive days with increasing steps', () => {
+    expect(activity.getConsecutiveIncrease(1)).to.deep.equal([
+    {
+      userID: 1,
+      date: '2019/06/15',
+      numSteps: 3577,
+      minutesActive: 140,
+      flightsOfStairs: 16
+    },
+    {
+      userID: 1,
+      date: '2019/06/16',
+      numSteps: 6637,
+      minutesActive: 175,
+      flightsOfStairs: 36
+    },
+    {
+      userID: 1,
+      date: '2019/06/17',
+      numSteps: 14329,
+      minutesActive: 168,
+      flightsOfStairs: 18
+    },
+    {
+      userID: 1,
+      date: '2019/06/18',
+      numSteps: 4419,
+      minutesActive: 165,
+      flightsOfStairs: 33
+    },
+    {
+      userID: 1,
+      date: '2019/06/19',
+      numSteps: 8429,
+      minutesActive: 275,
+      flightsOfStairs: 2
+    },
+    {
+      userID: 1,
+      date: '2019/06/20',
+      numSteps: 14478,
+      minutesActive: 140,
+      flightsOfStairs: 12
+    },
+    {
+      userID: 1,
+      date: '2019/06/21',
+      numSteps: 6760,
+      minutesActive: 135,
+      flightsOfStairs: 6
+    },
+    {
+      userID: 1,
+      date: '2019/06/22',
+      numSteps: 10289,
+      minutesActive: 119,
+      flightsOfStairs: 6
+    },
+    {
+      userID: 1,
+      date: '2019/06/23',
+      numSteps: 13928,
+      minutesActive: 218,
+      flightsOfStairs: 21
+    }
+    ]);
+  });
 });

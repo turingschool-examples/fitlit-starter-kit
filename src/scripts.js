c3df34c92534351d6c45cfbe83fc3b3b635d8b5e
posi7790 <posi7790@colorado.edu>
2019-08-28 09:23:13 -0600

    Use spread operator to prevent mutation of original data

--- src/scripts.js
+++ src/scripts.js
@@ -31,245 +31,246 @@ function dropYear(dates) {
   })
   return reformattedDates
 }
-$(document).ready(function() {
+$(document).ready(function () {
 
-//Packery Items 
-let $grid = $('.grid').packery({
-  itemSelector: '.grid-item',
-  columnWidth: 50,
-  rowHeight: 30,
-  gutter: 4,
-});
+  //Packery Items 
+  let $grid = $('.grid').packery({
+    itemSelector: '.grid-item',
+    columnWidth: 50,
+    rowHeight: 30,
+    gutter: 4,
+  });
 
-let $draggable = $('.draggable').draggabilly({
-  containment: true
-});
+  let $draggable = $('.draggable').draggabilly({
+    containment: true
+  });
 
-$grid.find('.grid-item').each(function (i, gridItem) {
-  let draggie = new Draggabilly(gridItem)
-  $grid.packery('bindDraggabillyEvents', draggie)
-});
+  $grid.find('.grid-item').each(function (i, gridItem) {
+    let draggie = new Draggabilly(gridItem)
+    $grid.packery('bindDraggabillyEvents', draggie)
+  });
 
 
-// Function to find user name
-function findUserName(id) {
-  return userData.find(user => user.id === id).name;
-}
-
-//User Section
-$('.username').text(`${user.returnUserName()}`)
-
-//Date Section
-$('.date').text(`${formattedDate}`);
-
-//Hydration
-$('.water-consumed').text(`${hydration.returnDailyFluidOunces(date)} ounces \n\n`);
-
-const weeklyOuncesChart = new Chart(document.getElementById('water-consumed-week').getContext('2d'), {
-  type: 'horizontalBar',
-  data: {
-    labels: dropYear(hydration.returnWeek()),
-    datasets: [{
-      data: hydration.returnWeeklyNumOunces(),
-      backgroundColor: [
-        'rgba(92, 117, 218, 0.2)',
-        'rgba(242, 188, 51, 0.2)',
-        'rgba(126, 221, 255, 0.2)',
-        'rgba(92, 117, 218, 0.2)',
-        'rgba(242, 188, 51, 0.2)',
-        'rgba(126, 221, 255, 0.2)',
-        'rgba(92, 117, 218, 0.2)'
-      ],
-    }]
-  },
-  options: {
-    legend: {
-      display: false,
-    },
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        },
-        scaleLabel: {
-          display: true,
-          labelString: '# of Ounces'
-        }
-      }]
-    }
+  // Function to find user name
+  function findUserName(id) {
+    return userData.find(user => user.id === id).name;
   }
-});
 
-//Sleep
-$('.hours-slept-day').text(`${sleep.returnSleepHours(date)} hours | ${sleep.returnSleepQuality(date)} quality`);
+  //User Section
+  $('.username').text(`${user.returnUserName()}`)
 
-const weeklySleepChart = new Chart(document.getElementById('sleep-week').getContext('2d'), {
-  type: 'line',
-  data: {
-    labels: dropYear(sleep.returnWeek(1)),
-    datasets: [{
-      data: sleep.returnWeekOfSleepHours(1),
-      label: "Sleep Hours",
-      borderColor: "rgba(92, 117, 218, 0.2)",
-      fill: false,
-      lineTension: 0.1
-    },
-    {
-      data: Array(7).fill(sleep.returnAvgSleepHours()),
-      label: "Average Hours of Sleep",
-      borderColor: "rgba(92, 117, 218, 0.2)",
-      fill: false,
-      borderDash: [10, 5]
-    },
-    {
-      data: sleep.returnWeekOfSleepQuality(1),
-      label: "Quality of Sleep",
-      borderColor: "rgba(242, 188, 51, 0.2)",
-      fill: false,
-      lineTension: 0.1
-    },
-    {
-      data: Array(7).fill(sleep.returnAvgSleepQuality()),
-      label: "Average Quality of Sleep",
-      borderColor: "rgba(242, 188, 51, 0.2)",
-      fill: false,
-      borderDash: [10, 5]
-    }
-    ]
+  //Date Section
+  $('.date').text(`${formattedDate}`);
 
-  },
-  options: {
-    elements: {
-      point: {
-        radius: 0
+  //Hydration
+  $('.water-consumed').text(`${hydration.returnDailyFluidOunces(date)} ounces \n\n`);
+
+  const weeklyOuncesChart = new Chart(document.getElementById('water-consumed-week').getContext('2d'), {
+    type: 'horizontalBar',
+    data: {
+      labels: dropYear(hydration.returnWeek()),
+      datasets: [{
+        data: hydration.returnWeeklyNumOunces(),
+        backgroundColor: [
+          'rgba(92, 117, 218, 0.2)',
+          'rgba(242, 188, 51, 0.2)',
+          'rgba(126, 221, 255, 0.2)',
+          'rgba(92, 117, 218, 0.2)',
+          'rgba(242, 188, 51, 0.2)',
+          'rgba(126, 221, 255, 0.2)',
+          'rgba(92, 117, 218, 0.2)'
+        ],
+      }]
+    },
+    options: {
+      legend: {
+        display: false,
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          },
+          scaleLabel: {
+            display: true,
+            labelString: '# of Ounces'
+          }
+        }]
       }
-    },
-    legend: {
-      display: false,
-    },
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        },
-        scaleLabel: {
-          display: true,
-          labelString: 'hours'
-        },
+    }
+  });
 
-      }, {
-        id: 'Quality of Sleep',
-        type: 'linear',
-        position: 'right',
-        ticks: {
-          beginAtZero: true,
-          min: 0,
-          max: 10
-        },
-        scaleLabel: {
-          display: true,
-          labelString: 'quality'
+  //Sleep
+  $('.hours-slept-day').text(`${sleep.returnSleepHours(date)} hours | ${sleep.returnSleepQuality(date)} quality`);
+
+  const weeklySleepChart = new Chart(document.getElementById('sleep-week').getContext('2d'), {
+    type: 'line',
+    data: {
+      labels: dropYear(sleep.returnWeek(1)),
+      datasets: [{
+        data: sleep.returnWeekOfSleepHours(1),
+        label: "Sleep Hours",
+        borderColor: "rgba(92, 117, 218, 0.2)",
+        fill: false,
+        lineTension: 0.1
+      },
+      {
+        data: Array(7).fill(sleep.returnAvgSleepHours()),
+        label: "Average Hours of Sleep",
+        borderColor: "rgba(92, 117, 218, 0.2)",
+        fill: false,
+        borderDash: [10, 5]
+      },
+      {
+        data: sleep.returnWeekOfSleepQuality(1),
+        label: "Quality of Sleep",
+        borderColor: "rgba(242, 188, 51, 0.2)",
+        fill: false,
+        lineTension: 0.1
+      },
+      {
+        data: Array(7).fill(sleep.returnAvgSleepQuality()),
+        label: "Average Quality of Sleep",
+        borderColor: "rgba(242, 188, 51, 0.2)",
+        fill: false,
+        borderDash: [10, 5]
+      }
+      ]
+
+    },
+    options: {
+      elements: {
+        point: {
+          radius: 0
         }
-      }]
+      },
+      legend: {
+        display: false,
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'hours'
+          },
+
+        }, {
+          id: 'Quality of Sleep',
+          type: 'linear',
+          position: 'right',
+          ticks: {
+            beginAtZero: true,
+            min: 0,
+            max: 10
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'quality'
+          }
+        }]
+      }
     }
-  }
-});
+  });
 
-$('.longest-sleepers').text(`${findUserName(sleepRepo.returnWeeklyLongestSleepers(1)[1])}: ${sleepRepo.returnWeeklyLongestSleepers(1)[0]} hours`);
+  $('.longest-sleepers').text(`${findUserName(sleepRepo.returnWeeklyLongestSleepers(1)[1])}: ${sleepRepo.returnWeeklyLongestSleepers(1)[0]} hours`);
 
-//Activity Section
+  //Activity Section
 
-var bar = new ProgressBar.Circle('.number-of-steps-day', {
-  color: '#aaa',
-  svgStyle: {
-    display: 'block',
-    width: '100%'
-  },
-  strokeWidth: 5,
-  trailWidth: 2,
-  easing: 'easeInOut',
-  duration: 1400,
-  text: {
-    autoStyleContainer: true
-  },
-  from: {
-    color: '#fff940',
-    width: 2
-  },
-  to: {
-    color: '#f2bc33',
-    width: 5
-  },
+  var bar = new ProgressBar.Circle('.number-of-steps-day', {
+    color: '#aaa',
+    svgStyle: {
+      display: 'block',
+      width: '100%'
+    },
+    strokeWidth: 5,
+    trailWidth: 2,
+    easing: 'easeInOut',
+    duration: 1400,
+    text: {
+      autoStyleContainer: true
+    },
+    from: {
+      color: '#fff940',
+      width: 2
+    },
+    to: {
+      color: '#f2bc33',
+      width: 5
+    },
 
-  step(state, circle) {
-    circle.path.setAttribute('stroke', state.color);
-    circle.path.setAttribute('stroke-width', state.width);
+    step(state, circle) {
+      circle.path.setAttribute('stroke', state.color);
+      circle.path.setAttribute('stroke-width', state.width);
 
-    var value = circle.value();
-    if (value === 0) {
-      circle.setText('');
-    } else {
-      circle.setText(`${activity.returnNumStepsDay(date)} steps`);
+      var value = circle.value();
+      if (value === 0) {
+        circle.setText('');
+      } else {
+        circle.setText(`${activity.returnNumStepsDay(date)} steps`);
+      }
+
     }
+  });
 
-  }
-});
+  let percentSteps = activity.returnNumStepsDay(date) / user.dailyStepGoal;
+  bar.animate(percentSteps > 1 ? percentSteps = 1 : percentSteps); // Number from 0.0 to 1.0
 
-let percentSteps = activity.returnNumStepsDay(date) / user.dailyStepGoal;
-bar.animate(percentSteps > 1 ? percentSteps = 1 : percentSteps); // Number from 0.0 to 1.0
+  $('.number-of-steps-goal').text(`Step Goal: ${user.dailyStepGoal}`);
+  $('.avg-number-of-steps-goal').text(`Average Step Goal: ${userRepo.returnAverageStepGoal()}`);
+  $('.number-of-minutes-active-day').text(`${activity.returnMinutesActive(date)}`);
+  $('.average-minutes-active').text(`${activityRepo.returnAverage(date, 'minutesActive')}`)
+  $('.distance').text(`${activity.returnNumStepsDay(date)}`);
+  $('.average-distance').text(`${activityRepo.returnAverage(date, 'numSteps')}`)
+  $('.stairs').text(`${activity.returnFlightsOfStairs(date)}`);
+  $('.average-stairs').text(`${activityRepo.returnAverage(date, 'flightsOfStairs')}`)
+  $('.distance-in-miles').text(`${activity.returnMilesWalked()} Miles`);
+  $('.most-active').text(`${activityRepo.returnMostActive()[0]}: ${activityRepo.returnMostActive()[1]} minutes`);
 
-$('.number-of-steps-goal').text(`Step Goal: ${user.dailyStepGoal}`);
-$('.avg-number-of-steps-goal').text(`Average Step Goal: ${userRepo.returnAverageStepGoal()}`);
-$('.number-of-minutes-active-day').text(`${activity.returnMinutesActive(date)}`);
-$('.average-minutes-active').text(`${activityRepo.returnAverage(date, 'minutesActive')}`)
-$('.distance').text(`${activity.returnNumStepsDay(date)}`);
-$('.average-distance').text(`${activityRepo.returnAverage(date, 'numSteps')}`)
-$('.stairs').text(`${activity.returnFlightsOfStairs(date)}`);
-$('.average-stairs').text(`${activityRepo.returnAverage(date, 'flightsOfStairs')}`)
-$('.distance-in-miles').text(`${activity.returnMilesWalked()} Miles`);
-$('.most-active').text(`${activityRepo.returnMostActive()[0]}: ${activityRepo.returnMostActive()[1]} minutes`);
+  // Friends
 
-// Friends
+  let userIDs = Object.keys(activity.returnFriendsStepCount()[0]);
 
-let userIDs = Object.keys(activity.returnFriendsStepCount()[0]);
-
-function insertFriendSteps() {
-  let list = `<ul class="friends_ul">`
-  userIDs.forEach(userID => {
-    let userName = findUserName(Number(userID));
-    list += `<li class="friends_li">
+  function insertFriendSteps() {
+    let list = `<ul class="friends_ul">`
+    userIDs.forEach(userID => {
+      let userName = findUserName(Number(userID));
+      list += `<li class="friends_li">
              <p class="friends--steps"><b>${userName}</b>:</p>
              <p>${activity.returnFriendsStepCount()[0][userID]} steps</p>`;
-  });
-  list += `</ul>`;
-  return list;
-}
+    });
+    list += `</ul>`;
+    return list;
+  }
 
-$('.friends-step').html(`${insertFriendSteps()}`);
+  $('.friends-step').html(`${insertFriendSteps()}`);
 
-// Challenges
+  // Challenges
 
-function insertStepStreak() {
-  let list = `<ul class="steps_ul">`
-  activity.returnThreeDayStepStreak().forEach(day => {
-    list += `<li class="date_li">
+  function insertStepStreak() {
+    let list = `<ul class="steps_ul">`
+    activity.returnThreeDayStepStreak().forEach(day => {
+      list += `<li class="date_li">
              <p class="dates"> ${day}`
-  })
-  list += `</ul>`;
-  return list;
-}
+    })
+    list += `</ul>`;
+    return list;
+  }
 
-$('.increasing-steps').html(`${insertStepStreak()}`);
+  $('.increasing-steps').html(`${insertStepStreak()}`);
 
-function insertStairStreak() {
-  let list = `<ul class="stairs_ul">`
-  activity.returnTwoDayStairStreak().forEach(day => {
-    list += `<li class="date_li">
+  function insertStairStreak() {
+    let list = `<ul class="stairs_ul">`
+    activity.returnTwoDayStairStreak().forEach(day => {
+      list += `<li class="date_li">
              <p class="dates"> ${day}`
-  })
-  list += `</ul>`;
-  return list;
-}
+    })
+    list += `</ul>`;
+    return list;
+  }
 
+  $('.increasing-stairs').html(`${insertStairStreak()}`);
 
 })

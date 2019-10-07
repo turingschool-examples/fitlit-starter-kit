834dd28e9f7bfed3c8202efe2a2aed1a622525d3
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-10-07 15:41:36 -0600

    Create dataModel file with draft classes, properties and methods

--- fitLit_dataModel.js
+++ fitLit_dataModel.js
@@ -1,44 +1,123 @@
 Data Model
 
-Users
-[
-  {
-    "id": [number],
-    "name": [string],
-    "address": [string],
-    "email": [string],
-    "strideLength": [number - feet],
-    "dailyStepGoal": [number - steps],
-    "friends": [array - one-way connection to other user(s)]
-  },
-]
+//UserRespository holds all user objects
+class UserRepository {
+  constructor(data) {
+    this.users = [{}];
+  }
+  getUser() {
 
-Activity
-[
-  {
-    "userID": [number],
-    "date": [string YYYY/MM/DD],
-    "numSteps": [number - steps],
-    "minutesActive": [number - minutes],
-    "flightsOfStairs": [number - flights]
-  },
-]
+  }
+  getBestSleepers() {
 
-Hydration
-[
-  {
-    "userID": [number],
-    "date": [string YYYY/MM/DD],
-    "numOunces": [number - ounces]
-  },
-]
+  }
+  getLongestSleepers() {
 
-Sleep
-[
-  {
-    "userID": [number],
-    "date": [string YYYY/MM/DD],
-    "hoursSlept": [number - hours],
-    "sleepQuality": [number - unitless]
-  },
-]
+  }
+  getWorstSleepers() {
+
+  }
+  calculateAverageStepGoal() {
+
+  }
+  calculateAverageSleepQuality() {
+
+  }
+  calculateAverageSteps() {
+
+  }
+  calculateAverageStairs() {
+
+  }
+  calculateAverageMinutesActive() {
+
+  }
+}
+
+class User { //holds the information for each individual user
+  constructor(userData) {
+    this.id = userData.id;
+    this.name = name;
+    this.address = address;
+    this.email = email;
+    this.strideLength = strideLength;
+    this.dailyStepGoal = dailyStepGoal;
+    this.friends = []
+    // will connect to other users
+  }
+  getFirstName() {
+
+  }
+}
+
+class Hydration { // instance that is updated each time a user drinks in a given day (will reset each day)
+  constructor(user) {
+    this.userId = user.id;
+    this.date = date;
+    this.ouncesToday = 0;
+    this.ouncesAverage = 0;
+    this.ouncesRecord = []
+  }
+  drink(amount) {
+    //will increase this.ouncesToday by amount
+    //will increase this.ouncesTotal by amount
+    //this.ouncesRecord.unshift(this.ouncesToday)
+  }
+  calculateAverageOunces() {
+    //take first seven elements in this.ouncesRecord to calculate week's average
+  }
+}
+
+class Activity { // instance that's updated each time a user walks or climbs in a given day (will reset each day)
+  constructor(user) {
+    this.userId = user.id;
+    this.date = date;
+    this.steps = 0;
+    this.minutesActiveToday = 0;
+    this.flightsOfStairs = 0;
+    this.milesWalked = 0;
+    this.minutesActiveRecord = [];
+    this.stepsRecord = [];
+    this.stairsRecord = [];
+    this.reachedStepGoal = false;
+    this.accomplishedDays = []; //will hold dates where they exceeded step goal
+    this.topClimbingDay = topClimbingDay;
+  }
+  walk() {
+
+  }
+  climb() {
+
+  }
+  calculateMiles() {
+
+  }
+  findAccomplishedDays() {
+
+  }
+  updateTopClimbingDay() {
+
+  }
+}
+
+class Sleep { // instance for the user's sleep each day 
+  constructor(user) {
+    this.userId = user.id;
+    this.date = date;
+    this.hoursSleptToday = 0;
+    this.sleepQualityToday = 0;
+    this.hoursSleptAverage = 0;
+    this.sleepQualityAverage = 0;
+    this.hoursRecord = [];
+    this.qualityRecord = []
+  }
+  sleep() {
+
+  }
+  calculateSleepQualityAverage() {
+    //unshift to put hoursSleptToday at beginning
+  }
+  calculateHoursSleptAverage() {
+    //unshift to put sleepQualityToday at beginning
+  }
+}

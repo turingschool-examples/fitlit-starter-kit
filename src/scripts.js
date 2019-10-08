25786933b2eb5186119c71f2b0ecb909a78f54d8
Allison Wagner <allisonjw01@gmail.com>
2019-10-08 16:56:47 -0600

    Begins adding data from user and users repo class to the DOM

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,21 @@
 $(function() {
+  const randomUser = Math.floor(Math.random() * 50) + 1   
   
+  const user = new User(userData[randomUser]);
+//   const UsersRepo = new UsersRepo(userData);
+//   const userSleep = new userSleep();
+//   const usersSleepRepo = new usersSleepRepo();
+//   const userHydration = new userHydration();
+//   const userActivity = new userActivity();
+//   const usersActivityRepo = UsersActivityRepo();
 
+$('.span__currentUser').text(user.getUserFirstName())  
+$('.user__address').text(userData[randomUser].address);
+$('.user__email').text(userData[randomUser].email);
+$('.user__stride').text(userData[randomUser].strideLength);
+$('.user__step').text(userData[randomUser].dailyStepGoal);
+$('.user__friends').text(userData[randomUser].friends);
+$('.compare__user-steps-to-all').text()
 
 
 });

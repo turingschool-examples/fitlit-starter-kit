a1cb4c3202df175ff9088a85e4fa03c15ab778ec
Alex Roth <jonesroth7@gmail.com>
2019-10-08 18:57:18 -0600

    Add user repo prelim tests

--- src/User-repo.js
+++ src/User-repo.js
@@ -1,4 +1,15 @@
-//need to add data and data array
+//need to add data and data  array
 const scripts = require('./scripts');
 
 const User = require('../src/User');
+
+class UserRepo {
+  constructor(users) {
+    this.users = users;
+  }
+  getDataFromID() {
+
+  }
+}
+
+module.exports = UserRepo;

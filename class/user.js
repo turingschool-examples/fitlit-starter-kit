d706b7d330b02547c2a90de4549d8c6fb8ddbae5
Jim Weist <jp_weist@yahoo.com>
2019-10-09 14:43:43 -0600

    should have a parameter to take in a userData object for user class done

--- class/user.js
+++ class/user.js
@@ -4,19 +4,11 @@ class User {
     this.name = userData.name;
     this.address = userData.address;
     this.email = userData.email;
-    this.StrideLength = userData.StrideLength;
+    this.dailyStepGoal = userData.dailyStepGoal;
+    this.strideLength = userData.strideLength;
     this.friends = userData.friends;
 
   }
 }
 
 module.exports = User;
-
-
-
-// this.id = userData.id
-// this.name = userData.name
-// this.address = userData.address
-// this.email = userData.email
-// this.StrideLength = userData.StrideLength
-// this.friends = userData.friends
--- test/user-test.js
+++ test/user-test.js
@@ -26,4 +26,20 @@ describe('User', function() {
     expect(userData.name).to.equal('Luisa Hane');
   });
 
+  it('should have a parameter to take in a userData object', function() {
+    expect(userData).to.deep.equal({
+      "id": 1,
+      "name": "Luisa Hane",
+      "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+      "email": "Diana.Hayes1@hotmail.com",
+      "strideLength": 4.3,
+      "dailyStepGoal": 10000,
+      "friends": [
+        16,
+        4,
+        8
+      ]
+    })
+  })
+
 });

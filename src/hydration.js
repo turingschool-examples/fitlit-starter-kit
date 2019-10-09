ea0676bda8b3fe3d3286b27b6b96f94854939f89
Cameron MacRae <cam.mac.cm@gmail.com>
2019-10-09 15:50:55 -0600

    Start hydration class/tests

---
+++ src/hydration.js
@@ -0,0 +1,19 @@
+class Hydration {
+  constructor(id) {
+    this.userID = id;
+  }
+  calculateAvgOunces(id, hydrationData) {
+    let ozDrank;
+    let days;
+    hydrationData.forEach(function(elem) {
+      if (elem.id === id) {
+        ozDrank = ozDrank + elem.numOunces;
+        days++;
+      }
+    return Math.floor(ozDrank / days);
+  }
+}
+
+ if (typeof module !== 'undefined') {
+   module.exports = Hydration;
+ }

147c0ef419333ef773e6dd49fdea6b09c94dd9ed
Ben Firth <bfirth93@gmail.com>
2019-10-09 13:43:49 -0600

    Create user friend list js

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,14 @@
 console.log("Hello World");
+
+function getFriends(user) {
+  let friendId = user.friends[0];
+  let myFriend = userData.find(function(homie) {
+    homie.id === friendId;
+    return myFriend
+  })
+  let domFriends = document.querySelector('.friend-names');
+  let domFriendSteps = document.querySelector('.friend-steps');
+  domFriends.innerText = myFriend.name;
+  domFriendSteps.innerText = myFriend.dailyStepGoal;
+}
+

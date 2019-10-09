fb0e237597327084ab483144a4c158e67b3149b5
Jim Weist <jp_weist@yahoo.com>
2019-10-08 19:51:02 -0600

    should have a parameter to take in user data -test done for user-repository

---
+++ class/user-repository.js
@@ -0,0 +1,7 @@
+class UserRepository {
+  constructor(data) {
+    this.allUsers = data;
+  }
+}
+
+module.exports = UserRepository;
---
+++ test/user-repository-test.js
@@ -0,0 +1,42 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const UserRepository = require('../class/user-repository');
+
+
+describe('UserRepository', function() {
+  let data = [];
+
+  beforeEach( () => {
+    data = new UserRepository([{
+      "id": 1,
+      "name": "Luisa Hane",
+      "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+      "email": "Diana.Hayes1@hotmail.com",
+      "strideLength": 4.3,
+      "dailyStepGoal": 10000,
+      "friends": [
+        16,
+        4,
+        8
+      ]
+    }])
+  }); //for each
+
+ it('should have a parameter to take in user data', function() {
+   expect(data.allUsers).to.deep.equal([{
+     "id": 1,
+     "name": "Luisa Hane",
+     "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+     "email": "Diana.Hayes1@hotmail.com",
+     "strideLength": 4.3,
+     "dailyStepGoal": 10000,
+     "friends": [
+       16,
+       4,
+       8
+     ]
+   }]);
+ })
+
+}); //end

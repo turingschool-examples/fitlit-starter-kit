473d6df9a4bfcfdc24702d948bbf6c039a8bf4b3
Allie McCarthy <mccarthy.allie@gmail.com>
2019-10-09 15:41:09 -0600

    Add final functionalit to dom for Iteration one, minus one

--- src/scripts.js
+++ src/scripts.js
@@ -6,32 +6,43 @@
 const data = ('../data/users');
 
 var sidebarName = document.getElementById('sidebarName');
+var stepGoalCard = document.getElementById('stepGoalCard');
+var headerText = document.getElementById('headerText');
+var userAddress = document.getElementById('userAddress');
+var userEmail = document.getElementById('userEmail');
+var userStridelength = document.getElementById('userStridelength');
 
 function startApp() {
   let userList = [];
   makeUsers(userList);
-  console.log(userList);
+  let userRepo = new UserRepo(userList);
   let userNow = getUserById(pickUser(), userList);
-  console.log(userNow);
-  sidebarName.innerText = userNow.name;
-  console.log(userList);
-
+  addInfoToSidebar(userNow)
 }
 
 function makeUsers(array) {
-  userData.forEach(function(data) {
-    let user = new User(data);
+  userData.forEach(function(dataItem) {
+    let user = new User(dataItem);
     array.push(user);
   })
 }
 
 function pickUser() {
-  return 1
-  //make this a random id
+  // return array.sort(a, b) => (array[Math.floor(Math.random() * (a + 1))] - array[Math.floor(Math.random() * (b + 1))]);
+  return Math.floor(Math.random() * 51);
 }
 
 function getUserById(id, array) {
   return array.find((user) => (user.id === id));
+}
+
+function addInfoToSidebar(user) {
+  sidebarName.innerText = user.name;
+  headerText.innerText = `${user.getFirstName()}'s Activity Tracker`;
+  stepGoalCard.innerText = `Your daily step goal is ${user.dailyStepGoal}. The average daily step goal is`;
+  userAddress.innerText = user.address;
+  userEmail.innerText = user.email;
+  userStridelength.innerText = `Your stridelength is ${user.strideLength} meters.`;
 }
 
 startApp();

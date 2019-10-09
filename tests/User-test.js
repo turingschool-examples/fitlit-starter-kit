d23711456c012e934be7f5ae6afa3b6a212a0eca
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-10-09 14:33:56 -0600

    Create passing tests for User properties

--- tests/User-test.js
+++ tests/User-test.js
@@ -9,13 +9,13 @@ describe('User', function() {
     let user;
     beforeEach(() => {
       user = new User({
-        "id": 1,
-        "name": "Luisa Hane",
-        "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
-        "email": "Diana.Hayes1@hotmail.com",
-        "strideLength": 4.3,
-        "dailyStepGoal": 10000,
-        "friends": [
+        'id': 1,
+        'name': 'Luisa Hane',
+        'address': '15195 Nakia Tunnel, Erdmanport VA 19901-1697',
+        'email': 'Diana.Hayes1@hotmail.com',
+        'strideLength': 4.3,
+        'dailyStepGoal': 10000,
+        'friends': [
           16,
           4,
           8
@@ -29,11 +29,24 @@ describe('User', function() {
     expect(user).to.be.an.instanceof(User);
   });
   it('should have an id', function() {
-
+    expect(user.id).to.equal(1);
   });
-  describe('', function() {
-    it('', function() {
-
-    })
-  })
+  it('should have a name', function() {
+    expect(user.name).to.equal('Luisa Hane');
+  });
+  it('should have an address', function() {
+    expect(user.address).to.equal('15195 Nakia Tunnel, Erdmanport VA 19901-1697');
+  });
+  it('should have an email address', function() {
+    expect(user.email).to.equal('Diana.Hayes1@hotmail.com');
+  });
+  it('should have a stride length', function() {
+    expect(user.strideLength).to.equal(4.3);
+  });
+  it('should have a daily step goal', function() {
+    expect(user.dailyStepGoal).to.equal(10000);
+  });
+  it('should have friends', function() {
+    expect(user.friends).to.deep.equal([16, 4, 8])
+  });
 });

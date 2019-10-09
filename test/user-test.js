1013eb70a597a8763caa18e13906f680dfea70e3
sertmer <scottertmer@gmail.com>
2019-10-09 16:02:04 -0600

    Write tests for user to be a function and user to be an instance, pass those tests

--- test/user-test.js
+++ test/user-test.js
@@ -1,16 +1,26 @@
 const chai = require('chai');
 const expect = chai.expect;
 const UserRepo = require('../src/UserRepo');
-const UsersData = require('../data/users');
+const usersSampleData = require('../data/users-sample');
 const User = require('../src/user');
 
 describe('User', () => {
 
+  let userRepo;
+  let user;
+  let email = "Dimitri.Bechtelar11@gmail.com";
+  
   beforeEach(() => {
-
+    userRepo = new UserRepo(usersSampleData);
+    user = new User(userRepo.getUserData(email));
   });
 
-  it('should...', () => {
-
+  it('should be a function', () => {
+    expect(User).to.be.a('function');
   });
+
+  it('should be an instance of UserRepo', () => {
+    expect(user).to.be.an.instanceOf(User);
+  });
+
 });

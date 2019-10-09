5934a37031ff2061d3a3c8926d4b8f841e91b3bf
James Miller <james.rex.miller.4@gmail.com>
2019-10-08 20:03:08 -0600

    Add icons folder, add icons to index

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,6 @@
-let randomNum = null
-let user = undefined
+let randomNum = null;
+let user = undefined;
+let userRepository = undefined;
 // const data = require('../data/users.js')
 
 function getRandomNum() {
@@ -18,10 +19,11 @@ $(window).on('load', function () {
   $('.user_address').text(`${user.address}`);
   $('.user_stride_length').text(`${user.strideLength}`);
   $('.user_step_goal').text(`${user.dailyStepGoal}`);
+  $('.div_steps_other_users_p').text(`${userRepository.calculateAverageStepGoal()}`)
 })
 
 function makeUsers(data) {
-    let userRepository = new UserRepository(data)
+    userRepository = new UserRepository(data)
     userRepository.findUser(randomNum)
     console.log(userRepository.currentUser)
      user = new User(userRepository.currentUser);

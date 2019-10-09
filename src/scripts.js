b5d3a834020eed5fefce83d659e2fb5140675f75
Andrew Kinstler <andrewckinstler@gmail.com>
2019-10-09 14:34:57 -0600

    Add DOM manipulation and jQuery selectors

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const userRepo = new UserRepo(userData);
+console.log(userRepo);
+const user = new User(userData[12]);
+
+$('.user-name').text(user.returnFirstName());
+$('.address').text(user.address);
+$('.email').text(user.email);
+$('.stride').text(user.strideLength);
+$('.step-goal').text(user.dailyStepGoal);
+$('.user-avg').text(userRepo.calculateAvgStepGoal());

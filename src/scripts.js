a71c47389ecd1a0546946e6aaedf898987407a95
John Adams <adamsjr8576@gmail.com>
2019-10-09 17:17:12 -0600

    Create login page and basic styling

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+const userData = require('../data/users');
+const sleepData = require('../data/sleep');
+const hydrationData = require('../data/hydration');
+const activityData = require('../data/activity');
+const UserRepo = require('./userRepo');
+const User = require('./user');
+
+
+
+
+$(document).ready(function(){
+
+
+
+
+
+
+
+
+
+
+});

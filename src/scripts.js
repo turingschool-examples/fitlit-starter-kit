067b590a7bc4d6b1f743d3201ac6fc4fd5e5872c
Ben Firth <bfirth93@gmail.com>
2019-10-10 15:02:06 -0600

    Refactor to jquery

--- src/scripts.js
+++ src/scripts.js
@@ -5,8 +5,48 @@ const data = {
   sleep: sleepData,
   activity: activityData
 }
-const userRepository = new UserRepository(data);
+let userRepository = new UserRepository(data);
 let user;
+$(document).ready(function() {
+  $('.login button').on('click', function() {
+    const nameGiven = `${$('.first-name').val()} ${$('.last-name').val()}`;
+    const currentUser = userRepository.findUserByName(nameGiven);
+    user = new User(currentUser);
+    $('.login-header').fadeOut(100);
+    $('.page-header').fadeIn(100);
+    console.log(user.name);
+    fillUserInfo();
+  });
+  $('.bio-info').on('click', function () {
+    $('.bio-info main').fadeToggle();
+  });
+  // $('.bio-info').on('mouseleave', function () {
+  //   setTimeout(function(){
+  //     $('.bio-info main').fadeOut(200);
+  //   }, 2000);
+  // });
+  $('.steps-goal footer p').on('click', function() {
+    $('.steps-goal .average').fadeToggle();
+    $(this).text($(this).text() == 'see average' ? 'close' : 'see average');
+  });
+  //
+  // $('.steps-goal').on('mouseleave', function () {
+  //   setTimeout(function(){
+  //     $('.steps-goal footer p').text('see average');
+  //     $('.steps-goal .average').fadeOut(200);
+  //   }, 2000);
+  // });
+  function fillUserInfo() {
+    console.log(user);
+    $('.page-header header h2').text(user.getFirstName());
+    $('.info .name').text(user.name);
+    $('.info .email').text(user.email);
+    $('.info .address').text(user.address);
+    $('.body .step').text(`${user.strideLength} feet`);
+    $('.steps-goal .goal').text(user.dailyStepGoal);
+    $('.steps-goal .average-goal').text(userRepository.calculateAverageStepGoal());
+  }
+});
 
 $(document).ready(function() {
   $('.login button').on('click', function() {
@@ -19,6 +59,9 @@ $(document).ready(function() {
     fillUserInfo();
     getFriends(user);
   });
+  $('.friend-names').text(`Name: ${myFriend.name}`);
+  $('.friend-steps').text(`Step Goal: ${myFriend.dailyStepGoal}`);
+}
 
   $('.bio-info').on('click', function () {
     $('.bio-info main').fadeToggle();

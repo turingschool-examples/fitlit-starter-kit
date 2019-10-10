3e56a317f8e0273b0803282e42aef69f9a5bf3e0
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-10-10 15:15:09 -0600

    Add functionality to instantiate user repo with access to array of users

--- src/scripts.js
+++ src/scripts.js
@@ -1,24 +1,12 @@
+let userRepository = new UserRepository();
+
 userData.forEach(user => {
   user = new User(user);
+  userRepository.users.push(user)
 });
 
-let userRepository = new UserRepository([userData]);
+// let userRepository = new UserRepository([userData]);
 
 var headerName = document.querySelector('#header-name');
 
-
-var user = new User({
-  'id': 1,
-  'name': 'Luisa Hane',
-  'address': '15195 Nakia Tunnel, Erdmanport VA 19901-1697',
-  'email': 'Diana.Hayes1@hotmail.com',
-  'strideLength': 4.3,
-  'dailyStepGoal': 10000,
-  'friends': [
-    16,
-    4,
-    8
-  ]
-});
-
-headerName.innerText = `${user.getFirstName()}'S `;
+headerName.innerText = `${userRepository.users[0].getFirstName()}'S `;

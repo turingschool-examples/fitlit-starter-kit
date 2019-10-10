126580b045004ab1bf5c37a950f2249d4e95f2d0
Alex Roth <jonesroth7@gmail.com>
2019-10-10 15:27:02 -0600

    Refactor Hydration methods

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,24 +1,19 @@
-const data = require('../data/hydration');
-const hydrationData = data.hydrationData;
-// const User = require('../src/User');
-
 class Hydration {
   constructor(hydrationData) {
     this.hydrationData = hydrationData;
   }
   calculateAverageOunces(id) {
     let perDayUserHydration = this.hydrationData.filter((data) => id === data.userID);
-    let sumUserHydration = perDayUserHydration.reduce((sumSoFar, data) => {
-      return sumSoFar = sumSoFar + data.numOunces;
-    }, 0);
-    return sumUserHydration/perDayUserHydration.length;
+    return perDayUserHydration.reduce((sumSoFar, data) => {
+      return sumSoFar += data.numOunces;
+    }, 0)/perDayUserHydration.length;
   }
   calculateDailyOunces(id, date) {
     let findOuncesByDate = this.hydrationData.find((data) => id === data.userID && date === data.date);
     return findOuncesByDate.numOunces;
   }
   calculateWeeklyOunces() {
-
+    
   }
 }
 

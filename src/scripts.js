4f4e969cc78c986415fa5688cb6bea16e15f3e82
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-10-10 15:22:50 -0600

    Set up variables for DOM manipulation for main steps card

--- src/scripts.js
+++ src/scripts.js
@@ -8,5 +8,21 @@ userData.forEach(user => {
 // let userRepository = new UserRepository([userData]);
 
 var headerName = document.querySelector('#header-name');
+var stepsUserStepsToday = document.querySelector('#steps-user-steps-today');
+var stepsInfoActiveMinutesToday = document.querySelector('#steps-info-active-minutes-today');
+var stepsInfoMilesWalkedToday = document.querySelector('#steps-info-miles-walked-today');
+// var stepsFriendActiveMinutesAverageToday = document.querySelector('#steps-friend-active-minutes-average-today');
+// var stepsFriendStepsAverageToday = document.querySelector('#steps-friend-steps-average-today');
+var stepsCalendarTotalActiveMinutesWeekly = document.querySelector('#steps-calendar-total-active-minutes-weekly');
+var stepsCalendarTotalStepsWeekly = document.querySelector('#steps-calendar-total-steps-weekly');
+
+
+
+
+
+
+
+
+
 
 headerName.innerText = `${userRepository.users[0].getFirstName()}'S `;

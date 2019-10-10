5929df7083ba2ae92afac9770353b39c69071641
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-10-10 15:50:47 -0600

    Replace innertext for user step goal

--- src/scripts.js
+++ src/scripts.js
@@ -18,13 +18,18 @@ let headerName = document.querySelector('#header-name');
 let stepsUserStepsToday = document.querySelector('#steps-user-steps-today');
 let stepsInfoActiveMinutesToday = document.querySelector('#steps-info-active-minutes-today');
 let stepsInfoMilesWalkedToday = document.querySelector('#steps-info-miles-walked-today');
+let stepsInfoUserStepGoal = document.querySelector('#steps-info-user-step-goal');
 // var stepsFriendActiveMinutesAverageToday = document.querySelector('#steps-friend-active-minutes-average-today');
 // var stepsFriendStepsAverageToday = document.querySelector('#steps-friend-steps-average-today');
 let stepsCalendarTotalActiveMinutesWeekly = document.querySelector('#steps-calendar-total-active-minutes-weekly');
 let stepsCalendarTotalStepsWeekly = document.querySelector('#steps-calendar-total-steps-weekly');
+let stepsFriendAverageStepGoal = document.querySelector('#steps-friend-average-step-goal');
+
 
 stepsUserStepsToday.innerText = activityData.find(activity => {
   return activity.userID === userRepository.users[userIndex].id && activity.date === todayDate;
 }).numSteps;
 
 headerName.innerText = `${userRepository.users[userIndex].getFirstName()}'S `;
+
+stepsInfoUserStepGoal.innerText = `${userRepository.users[userIndex].dailyStepGoal}`;
---
+++ tests/Activity-test.js
@@ -0,0 +1,6 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Activity = require('../src/Activity');
+const UserRepository = require('../src/UserRepository');
+const User = require('../src/User');

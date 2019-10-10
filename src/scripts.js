cafdc75964d5e8726deecf50eb9bc8d8e135a2b4
“Foster <fosterataylor@yahoo.com>
2019-10-10 15:15:16 -0600

    Add chart info for day hydration

--- src/scripts.js
+++ src/scripts.js
@@ -5,23 +5,27 @@ $(document).ready(() => {
     userRepository.returnCurrentUser(randomId)
     const currentPerson = userRepository.currentUser
     console.log(currentPerson)
-    const user = new User(currentPerson);
+    const user = new User(currentPerson);   
     const firstName = currentPerson.name.split(' ');
     const hydration = new Hydration(hydrationData, currentPerson.id);
-    console.log(hydration.calculateAvgDailyAmtDrankByUserIdAllTime(currentPerson.id))
-    console.log(hydration.returnDrinkAmtEachDayOverWeekByUser(currentPerson.id))
 
 
     $('.date').text(currentDate);
     $('.welcome-user').text(firstName[0]);
-    $('#myChart').text(hydration.calculateAvgDailyAmtDrankByUserIdAllTime(currentPerson.id))
-          var myChart = new Chart($('#myChart'), {
+    $('.profile-name').text(currentPerson.name);
+    $('.profile-address').text(currentPerson.address);
+    $('.profile-email').text(currentPerson.email);
+    $('.average-stepGoal').text(`Your Daily Step Goal was ${currentPerson.dailyStepGoal}, while everyone else was at ${userRepository.calculateAvgStepGoalAllUsers()}`);
+
+
+    $('#myChart').text(hydration.calculateAvgDailyAmtDrankByUserIdAllTime())
+          var myChart = new Chart($('#myWeekChart'), {
               type: 'bar',
               data: {
                   labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6","Day 7"],
                   datasets: [{
                       label: 'Daily Amount Water For Week',
-                      data: hydration.returnDrinkAmtEachDayOverWeekByUser(currentPerson.id),
+                      data: hydration.returnDrinkAmtEachDayOverWeekByUser(),
                       backgroundColor: [
                           'rgba(255, 99, 132, 0.2)',
                           'rgba(54, 162, 235, 0.2)',
@@ -51,5 +55,43 @@ $(document).ready(() => {
                   }
               }
           });
+    
+        // //   $('#myChart').text(hydration.calculateAvgDailyAmtDrankByUserIdAllTime(currentPerson.id))
+        //   var myChart = new Chart($('#myDayChart'), {
+        //       type: 'line',
+        //       data: {
+        //           labels: ["Day 1"],
+        //           datasets: [{
+        //               label: 'Water Consumption For Day',
+        //               data: [5,6],
+        //               backgroundColor: [
+        //                   'rgba(255, 99, 132, 0.2)',
+        //                   'rgba(54, 162, 235, 0.2)',
+        //                   'rgba(255, 206, 86, 0.2)',
+        //                   'rgba(75, 192, 192, 0.2)',
+        //                   'rgba(153, 102, 255, 0.2)',
+        //                   'rgba(255, 159, 64, 0.2)'
+        //               ],
+        //               borderColor: [
+        //                   'rgba(255,99,132,1)',
+        //                   'rgba(54, 162, 235, 1)',
+        //                   'rgba(255, 206, 86, 1)',
+        //                   'rgba(75, 192, 192, 1)',
+        //                   'rgba(153, 102, 255, 1)',
+        //                   'rgba(255, 159, 64, 1)'
+        //               ],
+        //               borderWidth: 1
+        //           }]
+        //       },
+        //       options: {
+        //           scales: {
+        //               yAxes: [{
+        //                   ticks: {
+        //                       beginAtZero:true
+        //                   }
+        //               }]
+        //           }
+        //       }
+        //   });
         
 })

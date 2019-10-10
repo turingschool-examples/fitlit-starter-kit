2fa598188da8414f6251c1d5b360412eb47bf670
sertmer <scottertmer@gmail.com>
2019-10-10 14:47:06 -0600

    Set up hydroUser-test

--- test/hydroUser-test.js
+++ test/hydroUser-test.js
@@ -1,16 +1,25 @@
 const chai = require('chai');
 const expect = chai.expect;
 const HydroRepo = require('../src/hydroRepo');
-const HydrationData = require('../data/hydration');
+const hydrationSampleData = require('../data/hydration-sample');
 const HydroUser = require('../src/hydroUser');
 
 describe('hydroUser', () => {
 
-  beforeEach(() => {
+  let hydroUser;
+  let hydroRepo;
 
+  beforeEach(() => {
+    hydroRepo = new HydroRepo(hydrationSampleData);
+    let userHydroData = hydroRepo.getUserHydroData(1);
+    hydroUser = new HydroUser(userHydroData);
+  });
+  
+  it('should be a function', () => {
+    expect(HydroUser).to.be.a('function');
   });
 
-  it('should...', () => {
-
+  it('should be an instance of UserRepo', () => {
+    expect(hydroUser).to.be.an.instanceOf(HydroUser);
   });
 });

1fad20429f1482ccea8fcf938add0c870e1f453f
Alex Roth <jonesroth7@gmail.com>
2019-10-10 01:52:11 -0600

    Add functionality and tests for hydration class: calculateAverageOunces, calculateDailyOunces

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,10 +1,28 @@
 const data = require('../data/hydration');
 const hydrationData = data.hydrationData;
-const scripts = require('./scripts');
+// const User = require('../src/User');
 
-const User = require('../src/User');
+class Hydration {
+  constructor(hydrationData) {
+    this.hydrationData = hydrationData;
+  }
+  calculateAverageOunces(id) {
+    let perDayUserHydration = this.hydrationData.filter((data) => id === data.userID);
+    let sumUserHydration = perDayUserHydration.reduce((sumSoFar, data) => {
+      return sumSoFar = sumSoFar + data.numOunces;
+    }, 0);
+    return sumUserHydration/perDayUserHydration.length;
+  }
+  calculateDailyOnces(id, date) {
+    let findOuncesByDate = this.hydrationData.find((data) => id === data.userID && date === data.date);
+    return findOuncesByDate.numOunces;
+  }
+  calculateWeeklyOunces() {
+    
+  }
+}
 
 
-// if (typeof module !== 'undefined') {
-//   module.exports = Hydration;
-// }
+if (typeof module !== 'undefined') {
+  module.exports = Hydration;
+}

7dabc225e35c007f99555e755c1bd469d084ee30
“Foster <fosterataylor@yahoo.com>
2019-10-11 13:48:23 -0600

    Add activity testing for first three methods

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -1,7 +1,11 @@
 const chai = require('chai');
 const expect = chai.expect;
 const Activity = require('../src/Activity');
+const User = require('../src/User');
+
 const mockActivityData = require('../mock/mockActivityData');
+const mockCurrentUserActivityData = require('../mock/mockCurrentUserActivity.js');
+
 
 let user;
 let activity
@@ -9,7 +13,7 @@ let activity
 describe('Activity', () => {
 
     beforeEach( () => {
-        activity = new Activity(mockActivityData);
+        activity = new Activity(mockActivityData,2,mockCurrentUserActivityData);
     });
 
     it('should return true', () => {
@@ -20,5 +24,27 @@ describe('Activity', () => {
         expect(Activity).to.be.a('function');
     });
 
+    it('should have a userId', () => {
+        expect(activity.userID).to.equal(2);
+    });
+
+    it('should find all Activity data for a specific user', () => {
+        activity.findCurrentUserActivityData();
+        expect(activity.currentUserActivityData.length).to.equal(7);
+    });
     
+    it('should find for a specific day the return for miles a user has walked', () => {
+        activity.findCurrentUserActivityData();
+        expect(activity.calculateMilesUserWalkedOnSpecificDate("2019/06/15")).to.equal(3.66);
+    });
+
+    it('should find for a specific day the activity level for a certain user', () => {
+        activity.findCurrentUserActivityData();
+        expect(activity.returnMinutesActiveByUserOnSpecificDate("2019/06/15")).to.equal(138);
+    });
+
+    it('should find for a specific day the activity level for a certain user', () => {
+        activity.findCurrentUserActivityData();
+        expect(activity.calculateAvgMinutesActiveForUserOnSpecificWeek()).to.equal(156);
+    });
 });

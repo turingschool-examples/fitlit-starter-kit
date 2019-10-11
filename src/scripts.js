acd86351d3adb6197cc5cb9a0c0598dc748eed53
Raisa Primerova <riyuhide@gmail.com>
2019-10-10 20:54:49 -0600

    Update login page css and arrearence of board

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,3 @@
-
 const data = {
   users: userData,
   hydration: hydrationData,
@@ -6,39 +5,31 @@ const data = {
   activity: activityData
 }
 let userRepository = new UserRepository(data);
+userRepository.findToday();
 let user;
+
 $(document).ready(function() {
+
   $('.login button').on('click', function() {
     const nameGiven = `${$('.first-name').val()} ${$('.last-name').val()}`;
     const currentUser = userRepository.findUserByName(nameGiven);
     user = new User(currentUser);
-    $('.login-header').fadeOut(100);
-    $('.page-header').fadeIn(100);
-    console.log(user.name);
+    $('.login-header, .page-header, .empty-board, .board').fadeToggle(100);
     fillUserInfo();
+    fillSleepWidget();
     getFriends(user.findFriends(userRepository));
   });
+
   $('.bio-info').on('click', function () {
     $('.bio-info main').fadeToggle();
   });
-  // $('.bio-info').on('mouseleave', function () {
-  //   setTimeout(function(){
-  //     $('.bio-info main').fadeOut(200);
-  //   }, 2000);
-  // });
+
   $('.steps-goal footer p').on('click', function() {
     $('.steps-goal .average').fadeToggle();
     $(this).text($(this).text() == 'see average' ? 'close' : 'see average');
   });
-  //
-  // $('.steps-goal').on('mouseleave', function () {
-  //   setTimeout(function(){
-  //     $('.steps-goal footer p').text('see average');
-  //     $('.steps-goal .average').fadeOut(200);
-  //   }, 2000);
-  // });
+
   function fillUserInfo() {
-    console.log(user);
     $('.page-header header h2').text(user.getFirstName());
     $('.info .name').text(user.name);
     $('.info .email').text(user.email);
@@ -47,12 +38,12 @@ $(document).ready(function() {
     $('.steps-goal .goal').text(user.dailyStepGoal);
     $('.steps-goal .average-goal').text(userRepository.calculateAverageStepGoal());
   }
-});
 
-function getFriends(friends) {
-  friends.forEach(friend => {
-    $('.friend-names').append(`<img src="" alt="">
-    <h5>Name: ${friend.name}</h5>
-    <p>step goal: ${friend.dailyStepGoal}</p>`);
-  })
-}
+  function getFriends(friends) {
+    friends.forEach(friend => {
+      $('.friend-names').append(`<img src="" alt="">
+      <h5>Name: ${friend.name}</h5>
+      <p>step goal: ${friend.dailyStepGoal}</p>`);
+    })
+  }
+});

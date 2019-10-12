80531043c018078e9265c037c66695e266e5adf3
Raisa Primerova <riyuhide@gmail.com>
2019-10-12 15:15:28 -0600

    Add updating information functionality to sleep block

--- src/scripts.js
+++ src/scripts.js
@@ -4,9 +4,11 @@ const data = {
   sleep: sleepData,
   activity: activityData
 }
-let userRepository = new UserRepository(data);
+
+const userRepository = new UserRepository(data);
 userRepository.findToday();
 let user;
+let sleep;
 
 $(document).ready(function() {
 
@@ -17,7 +19,8 @@ $(document).ready(function() {
     $('.login-header, .page-header, .empty-board, .board').fadeToggle(100);
     fillUserInfo();
     getFriends(user.findFriends(userRepository));
-    showHydration()
+    showHydration();
+    showSleep();
   });
 
   $('.bio-info').on('click', function () {
@@ -53,4 +56,122 @@ $(document).ready(function() {
     })
     $('.current-hydro').text(currentHydration.numOunces)
   }
+
+  function showSleep() {
+    sleep = new Sleep(userRepository);
+    sleep.changeDate(userRepository);
+    updateSleep();
+  }
+
+  function updateSleep() {
+    let $hours = sleep.hoursSlept;
+    let $quality = sleep.splitQuality();
+    $('.sleep-widget main').children('p').text($hours);
+    if ($quality[0] !== 0) {
+      setQuality($quality)
+    } else {
+      $('.stars img').attr('src', '../images/star-empty.svg');
+      $('.stars p').text($quality.join('.'))
+    }
+  }
+
+  function setQuality(quality) {
+    let $star = $('.stars').children(`img:nth-child(${quality[0]})`);
+    $star.siblings('p').text(quality.join('.'))
+    $star.prevAll().attr('src', '../images/star-full.svg');
+    $star.attr('src', '../images/star-full.svg');
+    if (quality[1] < 5) {
+      $star.next().attr('src', '../images/star-halfless.svg');
+      $star.next().nextAll().attr('src', '../images/star-empty.svg');
+    }
+    if (quality[1] > 5) {
+      $star.next().attr('src', '../images/star-halfmore.svg');
+      $star.next().nextAll().attr('src', '../images/star-empty.svg');
+    }
+    if (quality[1] === 5) {
+      $star.next().attr('src', '../images/star-half.svg');
+      $star.next().nextAll().attr('src', '../images/star-empty.svg');
+    }
+    if (!quality[1]) {
+      $star.next().nextAll().attr('src', '../images/star-empty.svg');
+    }
+  }
+
+  // WEEK DAYS SWITCHER
+  $('.dayly-show footer img').on('click', function() {
+    const $widgetType = $(this).closest('.widget').attr('data-type');
+    const $dayIndex = parseInt($(this).attr('data-index'));
+    $(this).attr('src', '../images/circle-clicked.svg').siblings().attr('src', '../images/circle.svg');
+    if ($widgetType === 'sleep') updateWeekDay(this, $dayIndex);
+  });
+
+  function updateWeekDay(target, index) {
+    const $weekInfo = sleep.getWeekFullInfo(userRepository);
+    let $qualities = sleep.getWeeklyInfo(userRepository, 'quality');
+    $(target).closest('.widget').find('.date').text($weekInfo[index].date);
+    $(target).closest('.widget').find('.section__number').text($weekInfo[index].hoursSlept);
+    const $quality = sleep.splitQuality($qualities[index]);
+    setQuality($quality);
+  }
+
+  // DROPDOWN MENU SCRIPTS
+  $('.dropdown header').on('click', function() {
+    $(this).siblings('div').toggle();
+  });
+
+  $('.dropdown div p').on('click', function() {
+    $(this).parent().siblings('header').children('p').text($(this).text());
+    $(this).parent().siblings('input').val($(this).text());
+    $(this).parent().hide();
+    $(this).closest('.widget').find('footer .last').attr('src', '../images/circle-clicked.svg').siblings().attr('src', '../images/circle.svg');
+    const $widgetType = $(this).closest('.widget').attr('data-type');
+    const $dayEntered = $(this).parent().siblings('input').val();
+
+    if ($dayEntered === 'Week') {
+      $(this).closest('.widget').find('.date, footer').show();
+      $(this).closest('.widget').find('.date').text(sleep.date);
+      sleep.updateInfo(userRepository);
+      updateSleep();
+    }
+
+    if ($dayEntered === 'Today') {
+      $(this).closest('.widget').find('.date, footer').hide();
+      if ($widgetType === 'sleep') {
+        sleep.changeDate(userRepository);
+        updateSleep();
+      };
+    }
+
+    if ($dayEntered === 'All time') {
+      $(this).closest('.widget').find('.date, footer').hide();
+      if ($widgetType === 'sleep') {
+        sleep.changeDate(userRepository, 'All day');
+        updateSleep();
+      };
+    }
+  });
+
+  $('.search label').on('click', function() {
+    $(this).parent().children().toggle();
+    $(this).parent().siblings('.dropdown').toggle();
+  });
+
+  $('.search button').on('click', function() {
+    const $widgetType = $(this).closest('.widget').attr('data-type');
+    const $dayEntered = $(this).siblings('input').val();
+    const $dateVal = /^\d{1,4}\/\d{1,2}\/\d{1,2}$/
+    const $dropdown = $(this).parent().siblings('.dropdown');
+    $(this).parent().children().toggle();
+    $dropdown.toggle();
+    $(this).closest('.widget').find('.date, footer').hide();
+    if ($dateVal.test($dayEntered)) {
+      $dropdown.children('header').children('p').text($dayEntered);
+      $dropdown.children('input').text($dayEntered);
+
+      if ($widgetType === 'sleep') {
+        sleep.changeDate(userRepository, $dayEntered);
+        updateSleep();
+      };
+    }
+  });
 });

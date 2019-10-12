2046286b95fa2f9fd3e1a751f828593b4660ea52
Raisa Primerova <riyuhide@gmail.com>
2019-10-11 19:48:29 -0600

    Fix visual issues with date searcher

--- src/dd_search.js
+++ src/dd_search.js
@@ -1,21 +1,29 @@
 $(document).ready(function() {
   $('.dropdown header').on('click', function() {
-    $('.dropdown div').toggle();
+    $(this).siblings('div').toggle();
   });
 
   $('.dropdown div p').on('click', function() {
-    $('.dropdown header p').text($(this).text());
-    $('.dropdown input').val($(this).text());
-    $('.dropdown div').hide();
+    $(this).parent().siblings('header').children('p').text($(this).text());
+    $(this).parent().siblings('input').val($(this).text());
+    $(this).parent().hide();
   });
 
   $('.search label').on('click', function() {
-    $('.search').children().toggle();
-    $('.dropdown').toggle();
+    $(this).parent().children().toggle();
+    $(this).parent().siblings('.dropdown').toggle();
   });
 
   $('.search button').on('click', function() {
-    $('.search').children().toggle();
-    $('.dropdown').toggle();
+    $date = $(this).siblings('input').val();
+    $parent = $(this).parent().siblings('.dropdown');
+    $(this).parent().children().toggle();
+    $parent.toggle();
+    if (/^\d{1,4}\/\d{1,2}\/\d{1,2}$/.test($date)) {
+      $parent.children('header').children('p').text($date);
+      $parent.children('input').text($date);
+    }
   });
+
+
 });

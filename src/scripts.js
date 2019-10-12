46c15b9fdbcd58e913443578ef1c4907bef0301a
Jim Weist <jp_weist@yahoo.com>
2019-10-12 16:05:36 -0600

    Working on geting data to screen

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,36 @@
-console.log("Hello World");
+$( document ).ready( () => {
+  $('.main__user').html(`${userData[0].name} <br>
+    ${userData[0].email} <br>
+    Friends: ${userData[0].friends}`);
+
+  console.log(hydrationData[0].numOunces)
+
+$('.main__sleep').html(`${sleepData[0].date} <br> Hours Slept: ${sleepData[0].hoursSlept} hours<br>
+    Sleep Quality: ${sleepData[0].sleepQuality}`);
+
+$('.main__hydration').html(`Hydration: ${hydrationData[0].numOunces} oz`);
+
+$('.main__activity').html(`Steps: ${activityData[0].numSteps} <br>
+  Mins Active: ${activityData[0].minutesActive} <br>
+  Flishts Stairs: ${activityData[0].flightsOfStairs}`);
+
+// $('.main__friends').html(`${}`)
+
+}); // end jquery
+
+// activityData = [
+//   {
+//     "userID": 1,
+//     "date": "2019/06/15",
+//     "numSteps": 3577,
+//     "minutesActive": 140,
+//     "flightsOfStairs": 16
+//   }
+
+
+
+
+// var mainUser = document.querySelector('.main__user');
+// var mainHydration = document.querySelector('.main__hydration');
+// var mainSleep = document.querySelector('.main__sleep');
+// var mainActivity = document.querySelector('.main__activity');

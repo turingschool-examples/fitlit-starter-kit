a44337c73617758b1f83ff6cce7657fd4a2f1590
Cameron MacRae <cam.mac.cm@gmail.com>
2019-10-12 10:41:53 -0600

    Fix table styling bug

--- src/scripts.js
+++ src/scripts.js
@@ -10,6 +10,7 @@ $('.stride').text(user.strideLength);
 $('.step-goal').text(user.dailyStepGoal);
 $('.user-avg').text(userRepo.calculateAvgStepGoal());
 $('.hydration-today').text(hydration.findOzByDay(3, '2019/06/22', hydrationData));
+
 $('#day-1').text(`Day 1: ${hydration.findOzByWeek(3, '2019/06/22', hydrationData)[0]}`);
 $('#day-2').text(`Day 2: ${hydration.findOzByWeek(3, '2019/06/22', hydrationData)[1]}`);
 $('#day-3').text(`Day 3: ${hydration.findOzByWeek(3, '2019/06/22', hydrationData)[2]}`);
@@ -18,7 +19,6 @@ $('#day-5').text(`Day 5: ${hydration.findOzByWeek(3, '2019/06/22', hydrationData
 $('#day-6').text(`Day 6: ${hydration.findOzByWeek(3, '2019/06/22', hydrationData)[5]}`);
 $('#day-7').text(`Day 7: ${hydration.findOzByWeek(3, '2019/06/22', hydrationData)[6]}`);
 
-//Our user class uses a property of `id` and our other classes use userID. Should consider changing if it doesn't break anything else.
 $('#hours-slept').text(sleep.showSleep(sleep.userID, '2019/06/22', 'hoursSlept', sleepData));
 $('#sleep-quality').text(sleep.showSleep(sleep.userID, '2019/06/22', 'sleepQuality', sleepData));
 $('#hours-slept-all').text(sleep.calculateAvgSleep(sleep.userID, 'hoursSlept', sleepData));

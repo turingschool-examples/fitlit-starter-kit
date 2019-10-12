84244d5c0dde8865ae30c0a32754846120b7d461
sertmer <scottertmer@gmail.com>
2019-10-12 11:51:55 -0600

    Add test for should be function, and should be instance - pass both

--- test/activityUser-test.js
+++ test/activityUser-test.js
@@ -1,16 +1,26 @@
 const chai = require('chai');
 const expect = chai.expect;
 const ActivityUser = require('../src/activityUser');
-const activityData = require('../data/activity');
+const activitySampleData = require('../data/activity-sample');
 const ActivityRepo = require('../src/activityRepo');
 
 describe('activityUser', () => {
 
-  beforeEach(() => {
+  let activityUser;
+  let activityRepo;
+  let userActivityData;
 
+  beforeEach(() => {
+    activityRepo = new ActivityRepo(activitySampleData);
+    userActivityData = activityRepo.getUserActivityData(1);
+    activityUser = new ActivityUser(userActivityData);
   });
 
-  it('should...', () => {
+  it('should be a function', () => {
+    expect(ActivityUser).to.be.a('function');
+  });
 
+  it('should be an instance of UserActivity', () => {
+    expect(activityUser).to.be.an.instanceOf(ActivityUser);
   });
 });

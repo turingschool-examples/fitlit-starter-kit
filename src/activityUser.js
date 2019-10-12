3502dd950459443f46166c561ebaba29093e7e11
sertmer <scottertmer@gmail.com>
2019-10-12 13:00:17 -0600

    Add test - should calc miles walked by day, pass test

--- src/activityUser.js
+++ src/activityUser.js
@@ -1,6 +1,15 @@
 class ActivityUser {
-  constructor(userActivityData){
+  constructor(userActivityData, userData){
     this.activityData = userActivityData;
+    this.userData = userData;
+  }
+
+  calcMilesByDay(date) {
+    let userDateData = this.activityData.find(activityData => {
+      return activityData.date === date;
+    })
+    let feetWalked = userDateData.numSteps * this.userData.strideLength;
+    return Math.round(feetWalked / 5280);
   }
 }
 
--- test/activityUser-test.js
+++ test/activityUser-test.js
@@ -3,17 +3,28 @@ const expect = chai.expect;
 const ActivityUser = require('../src/activityUser');
 const activitySampleData = require('../data/activity-sample');
 const ActivityRepo = require('../src/activityRepo');
+const UserRepo = require('../src/userRepo');
+const User = require('../src/user');
+const usersSampleData = require('../data/users-sample');
+
 
 describe('activityUser', () => {
 
   let activityUser;
   let activityRepo;
   let userActivityData;
+  let userRepo;
+  let user;
+  let userData;
+  let email = "Diana.Hayes1@hotmail.com";
 
   beforeEach(() => {
+    userRepo = new UserRepo(usersSampleData);
+    userData = userRepo.getUserData(email);
+    user = new User(userData);
     activityRepo = new ActivityRepo(activitySampleData);
     userActivityData = activityRepo.getUserActivityData(1);
-    activityUser = new ActivityUser(userActivityData);
+    activityUser = new ActivityUser(userActivityData, userData);
   });
 
   it('should be a function', () => {
@@ -26,5 +37,9 @@ describe('activityUser', () => {
 
   it('should have a single user\'s activity data', () => {
     expect(activityUser.activityData).to.deep.equal(userActivityData);
+  });
+
+  it('should know how many miles user walked on a given day', () => {
+    expect(activityUser.calcMilesByDay("2019/06/15")).to.equal(3);
   });
 });

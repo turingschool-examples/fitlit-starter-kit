cd3bfbbccb17e40e0c39cbf37b14019cc0750de8
Jim Weist <jp_weist@yahoo.com>
2019-10-13 12:50:20 -0600

    Random user picked up for display from user database

--- src/scripts.js
+++ src/scripts.js
@@ -1,21 +1,30 @@
 $( document ).ready( () => {
-  $('.main__user').html(`${userData[0].name} <br>
-    ${userData[0].email} <br>
-    Friends: ${userData[0].friends}`);
 
-  console.log(hydrationData[0].numOunces)
+  var randomFitLitUser = '';
+  function getRandomId(min = 1, max = 50 ) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    randomFitLitUser = (Math.floor(Math.random() * max - min + 1) + min);
+    console.log(randomFitLitUser)
+  }
+  getRandomId(1, 50);
 
-$('.main__sleep').html(`${sleepData[0].date} <br> Hours Slept: ${sleepData[0].hoursSlept} hours<br>
-    Sleep Quality: ${sleepData[0].sleepQuality}`);
+$('.main__user').html(`${userData[randomFitLitUser].name} <br>
+    ${userData[randomFitLitUser].email} <br>
+    Friends: ${userData[randomFitLitUser].friends}`);
+$('.main__sleep').html(`${sleepData[randomFitLitUser].date} <br> Hours Slept: ${sleepData[randomFitLitUser].hoursSlept} hours<br>
+    Sleep Quality: ${sleepData[randomFitLitUser].sleepQuality}`);
 
 $('.main__hydration').html(`<div>
-  <p>Hydration: ${hydrationData[0].numOunces} oz</p></div>`);
+  <p>Hydration: ${hydrationData[randomFitLitUser].numOunces} oz</p></div>`);
 
-$('.main__activity').html(`<div><p>Steps: ${activityData[0].numSteps} <br>
-  Mins Active: ${activityData[0].minutesActive} <br>
-  Flishts Stairs: ${activityData[0].flightsOfStairs} </p></div>`);
+$('.main__activity').html(`<div><p>Steps: ${activityData[randomFitLitUser].numSteps} <br>
+  Mins Active: ${activityData[randomFitLitUser].minutesActive} <br>
+  Flishts Stairs: ${activityData[randomFitLitUser].flightsOfStairs} </p></div>`);
 
 // $('.main__friends').html(`${}`)
+
+
 
 }); // end jquery
 

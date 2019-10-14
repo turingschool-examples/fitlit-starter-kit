89e94e2aba45fe42bfe22fc4cdebf87692641082
“Foster <fosterataylor@yahoo.com>
2019-10-14 17:35:14 -0600

    Add sleep and hydration data to dom

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,5 +1,3 @@
-const User = require('../src/User');
-
 class Sleep {
   constructor(sleepData, userId, userData) {
     this.currentSleepData = sleepData;
@@ -7,102 +5,116 @@ class Sleep {
     this.currentUserSleepData;
     this.userDataForSleep = userData
   }
-
   findCurrentUserSleepData() {
     this.currentUserSleepData = this.currentSleepData.filter((userInfo) =>
       userInfo.userID === this.userID);
       return this.currentUserSleepData;
   }
-
   calculateAvgHoursSleptPerDayByUser() {
     let avgSleep = this.currentUserSleepData.reduce((acc, day) => {
       acc += day.hoursSlept;
       return acc
     }, 0)/this.currentUserSleepData.length;
-
     return parseFloat(avgSleep.toFixed(2))
   }
-
   calculateAvgSleepQualityPerDayByUser() {
     let avgSleepQuality = this.currentUserSleepData.reduce((acc, day) => {
       acc += day.sleepQuality;
       return acc
     }, 0)/this.currentUserSleepData.length;
-
     return parseFloat(avgSleepQuality.toFixed(2))
   }
-
   returnHoursSleptByUserOnSpecificDate(date) {
     let sleepDataOnSpecificDate = this.currentUserSleepData.find(userBlock => {
       return (userBlock.date === date)
     });
-
     return sleepDataOnSpecificDate.hoursSlept
   }
-
   returnSleepQualityByUserOnSpecificDate(date) {
     let sleepQualityOnSpecificDate = this.currentUserSleepData.find(userBlock => {
       return (userBlock.date === date)
     });
-
     return sleepQualityOnSpecificDate.sleepQuality
   }
-
   calculateHoursSleptEachDayByUserOverSpecificWeek() {
     let sevenDaySleepData = this.currentUserSleepData.map(day => {
       return day.hoursSlept
     });
-
     return sevenDaySleepData.slice(-7)
   }
-
+  calculateEachDaysSleepQualityForUserOverSpecificWeek() {
+    let sevenDaySleepQualityData = this.currentUserSleepData.map(day => {
+      return day.sleepQuality
+    });
+    return sevenDaySleepQualityData.slice(-7)
+  }
   calculateAvgSleepQualityAllUsers() {
     let avgHrsSlept = this.currentSleepData.reduce((acc, dataBlock) => {
       acc += dataBlock.hoursSlept;
       return acc
     }, 0)/this.currentSleepData.length;
-
     return parseFloat(avgHrsSlept.toFixed(2))
   }
-
-//   findUsersWithAvgSleepQualityMoreThanThreeOverSpecificWeek(date) {
-//
-//     calcSleepQualityforSpecificWeek(date)
-//       sleepData.sort((a, b) => {
-//         return a.userID - b.userID
-//       });
-//       sleepData.sort((a, b) => {
-//         return a.date - b.date
-//       });
-//
-//
-// // Sort by ID then by date
-// // PseudoCode: OK. Make an array of all users average sleep quality for X week (7 dates)
-// // Figure out a way by using just day one to get the next six days from the seed
-// // Part two seems easier
-//   }
-
+  findUsersWithAvgSleepQualityMoreThanThreeOverSpecificWeek(date) {
+    let indexForUserOneAtSpecificDate = this.currentSleepData.findIndex(userBlock => {
+      return (userBlock.userID === 1 && userBlock.date === date)
+    });
+    let allUserSleepDataForSpecificWeek = this.currentSleepData.slice(indexForUserOneAtSpecificDate, (indexForUserOneAtSpecificDate + 349));
+    let finalWeekObject = allUserSleepDataForSpecificWeek.reduce((acc, userBlock) => {
+      if (!acc[userBlock.userID]) {
+        acc[userBlock.userID] = []
+      }
+      return acc
+    }, {});
+    let keysForFinalWeekObject = Object.keys(finalWeekObject);
+    let keysObject = keysForFinalWeekObject.reduce((acc, num) => {
+      if (!acc[num]) {
+        acc[num] = parseInt(num)
+      }
+      return acc
+    }, {});
+    allUserSleepDataForSpecificWeek.forEach(element => {
+      if (element.userID === keysObject[element.userID]) {
+        finalWeekObject[element.userID].push(element.sleepQuality)
+      }
+    });
+    let finalObjectValues = Object.values(finalWeekObject);
+    let averages = finalObjectValues.map(element => {
+      return (element[0] + element[1] + element[2] + element[3] + element[4] + element[5] + element[6]) / 7
+    });
+    averages.forEach(avg => {
+      finalWeekObject[averages.indexOf(avg) + 1] = parseFloat(avg.toFixed(2))
+    });
+    let finishedObjectKeys = Object.keys(finalWeekObject);
+    let bestSleeperIds = finishedObjectKeys.filter(key => {
+      return finalWeekObject[key] > 3
+    });
+    let parsedBestSleeperIds = bestSleeperIds.map(id => {
+      return parseInt(id)
+    });
+    return this.userDataForSleep.reduce((acc, userBlock) => {
+      if (parsedBestSleeperIds.includes(userBlock.id)) {
+         acc.push(userBlock.name)
+      }
+      return acc
+    }, []);
+  }
   findUsersSleptMostHoursIdentifiedByDate(date) {
     let allSleepDataOnSpecificDay = this.currentSleepData.filter(userBlock => {
       return userBlock.date === date
     });
-
     let hoursSleptForAllOnSpecificDay = allSleepDataOnSpecificDay.reduce((acc, userBlock) => {
       acc.push(userBlock.hoursSlept);
       return acc
     }, []);
-
-    var theMostHoursSlept =  Math.max(...hoursSleptForAllOnSpecificDay);
-
+    let theMostHoursSlept =  Math.max(...hoursSleptForAllOnSpecificDay);
     let deepestSleepers = allSleepDataOnSpecificDay.filter(userBlock => {
       return userBlock.hoursSlept === theMostHoursSlept
     });
-
     let idsForDeepestSleepers = deepestSleepers.reduce((acc, userBlock) => {
       acc.push(userBlock.userID);
       return acc
     }, []);
-
     return this.userDataForSleep.reduce((acc, userBlock) => {
       if (idsForDeepestSleepers.includes(userBlock.id)) {
          acc.push(userBlock.name)
@@ -110,23 +122,17 @@ class Sleep {
       return acc
     }, []);
   }
-
   findDateUserSleptBest() {
     let sleepQualities = this.currentUserSleepData.map(userBlock => {
       return userBlock.sleepQuality
     });
-
     let bestSleepQuality =  Math.max(...sleepQualities);
-
     let blockWithBestQualitySleep = this.currentUserSleepData.find(userBlock => {
       return userBlock.sleepQuality === bestSleepQuality
     });
-
     return blockWithBestQualitySleep.date
   }
 }
-
-
 if (typeof module !== 'undefined') {
     module.exports = Sleep;
 };
--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-    currentDate = new Date();
+    currentDate = "2019/09/16"
     const userRepository = new UserRepository(userData);
     const randomId = Math.ceil(Math.random() * 50 - 1);
     userRepository.returnCurrentUser(randomId)
@@ -8,8 +8,14 @@ $(document).ready(() => {
     const user = new User(currentPerson);   
     const firstName = currentPerson.name.split(' ');
     const hydration = new Hydration(hydrationData, currentPerson.id);
-    hydration.findCurrentUserHydrationData();
-    console.log(hydration.returnDrinkAmtEachDayOverWeekByUser())
+    const sleep = new Sleep(sleepData, currentPerson.id, userData)
+    console.log(hydration.findCurrentUserHydrationData());
+    console.log(sleep.findCurrentUserSleepData())
+    console.log(sleep.calculateHoursSleptEachDayByUserOverSpecificWeek())
+    console.log(hydration.calculateAmtDrankByUserSpecificDate(currentDate))
+    // console.log(sleep.returnHoursSleptByUserOnSpecificDate(currentDate)
+    
+    
 
 
     $('.date').text(currentDate);
@@ -18,82 +24,109 @@ $(document).ready(() => {
     $('.profile-address').text(currentPerson.address);
     $('.profile-email').text(currentPerson.email);
     $('.average-stepGoal').text(`Your Daily Step Goal was ${currentPerson.dailyStepGoal}, while everyone else was at ${userRepository.calculateAvgStepGoalAllUsers()}`);
+    $('.sleep1').text(`Your was hours slept each day over past week was${sleep.calculateHoursSleptEachDayByUserOverSpecificWeek()}`);
+    $('.hydration1').text(`Your water intake today was ${hydration.calculateAmtDrankByUserSpecificDate(currentDate)} oz of water`);
+    $('.hydration2').text(`Your water intake over the last week was ${hydration.returnDrinkAmtEachDayOverWeekByUser()} oz of water`);
+    $('.sleep2').text(`Your hours slept today ${sleep.returnHoursSleptByUserOnSpecificDate(currentDate)}`);
+    $('.sleep3').text(`Your sleep quality today ${sleep.returnSleepQualityByUserOnSpecificDate(currentDate)}`);
 
-
-    $('#myChart').text(hydration.calculateAvgDailyAmtDrankByUserIdAllTime())
-          var myChart = new Chart($('#myWeekChart'), {
-              type: 'bar',
-              data: {
-                  labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6","Day 7"],
-                  datasets: [{
-                      label: 'Daily Amount Water For Week',
-                      data: hydration.returnDrinkAmtEachDayOverWeekByUser(),
-                      backgroundColor: [
-                          'rgba(255, 99, 132, 0.2)',
-                          'rgba(54, 162, 235, 0.2)',
-                          'rgba(255, 206, 86, 0.2)',
-                          'rgba(75, 192, 192, 0.2)',
-                          'rgba(153, 102, 255, 0.2)',
-                          'rgba(255, 159, 64, 0.2)'
-                      ],
-                      borderColor: [
-                          'rgba(255,99,132,1)',
-                          'rgba(54, 162, 235, 1)',
-                          'rgba(255, 206, 86, 1)',
-                          'rgba(75, 192, 192, 1)',
-                          'rgba(153, 102, 255, 1)',
-                          'rgba(255, 159, 64, 1)'
-                      ],
-                      borderWidth: 1
-                  }]
-              },
-              options: {
-                  scales: {
-                      yAxes: [{
-                          ticks: {
-                              beginAtZero:true
-                          }
-                      }]
-                  }
-              }
-          });
     
-        // //   $('#myChart').text(hydration.calculateAvgDailyAmtDrankByUserIdAllTime(currentPerson.id))
-        //   var myChart = new Chart($('#myDayChart'), {
-        //       type: 'line',
-        //       data: {
-        //           labels: ["Day 1"],
-        //           datasets: [{
-        //               label: 'Water Consumption For Day',
-        //               data: [5,6],
-        //               backgroundColor: [
-        //                   'rgba(255, 99, 132, 0.2)',
-        //                   'rgba(54, 162, 235, 0.2)',
-        //                   'rgba(255, 206, 86, 0.2)',
-        //                   'rgba(75, 192, 192, 0.2)',
-        //                   'rgba(153, 102, 255, 0.2)',
-        //                   'rgba(255, 159, 64, 0.2)'
-        //               ],
-        //               borderColor: [
-        //                   'rgba(255,99,132,1)',
-        //                   'rgba(54, 162, 235, 1)',
-        //                   'rgba(255, 206, 86, 1)',
-        //                   'rgba(75, 192, 192, 1)',
-        //                   'rgba(153, 102, 255, 1)',
-        //                   'rgba(255, 159, 64, 1)'
-        //               ],
-        //               borderWidth: 1
-        //           }]
-        //       },
-        //       options: {
-        //           scales: {
-        //               yAxes: [{
-        //                   ticks: {
-        //                       beginAtZero:true
-        //                   }
-        //               }]
-        //           }
-        //       }
-        //   });
+
+
+
+
+
+
+    $(".sleep-btn").click(function () {
+    });
+
+    $(".hydration-btn").click(function () {
+    });
+
+    $(".activity-btn").click(function () {
+    });
+
+    $(".profile-btn").click(function () {
+    });
+
+
+
+    // $('#myChart').text(hydration.calculateAvgDailyAmtDrankByUserIdAllTime())
+    //       var myChart = new Chart($('#myWeekChart'), {
+    //           type: 'bar',
+    //           data: {
+    //               labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6","Day 7"],
+    //               datasets: [{
+    //                   label: 'Daily Amount Water For Week', 
+    //                   data: hydration.returnDrinkAmtEachDayOverWeekByUser(),
+    //                   backgroundColor: [
+    //                       'rgba(255, 99, 132, 0.2)',
+    //                       'rgba(54, 162, 235, 0.2)',
+    //                       'rgba(255, 206, 86, 0.2)',
+    //                       'rgba(75, 192, 192, 0.2)',
+    //                       'rgba(153, 102, 255, 0.2)',
+    //                       'rgba(255, 159, 64, 0.2)'
+    //                   ],
+    //                   borderColor: [
+    //                       'rgba(255,99,132,1)',
+    //                       'rgba(54, 162, 235, 1)',
+    //                       'rgba(255, 206, 86, 1)',
+    //                       'rgba(75, 192, 192, 1)',
+    //                       'rgba(153, 102, 255, 1)',
+    //                       'rgba(255, 159, 64, 1)'
+    //                   ],
+    //                   borderWidth: 1
+    //               }]
+    //           },
+    //           options: {
+    //               scales: {
+    //                   yAxes: [{
+    //                       ticks: {
+    //                           fontSize: 40
+    //                         //   beginAtZero:true
+    //                       }
+    //                   }]
+    //               }
+    //           }
+    //       });
+    
+    //     //   $('#myChart').text(hydration.calculateAvgDailyAmtDrankByUserIdAllTime(currentPerson.id))
+    // var myChart = new Chart($('#myDayChart'), {
+    //     // myChart.height(500);
+    //           type: 'line',
+    //           data: {
+    //               labels: ["Day 1"],
+    //               datasets: [{
+    //                   label: 'Water Consumption For Day',
+    //                   data: [5,6],
+    //                   backgroundColor: [
+    //                       'rgba(255, 99, 132, 0.2)',
+    //                       'rgba(54, 162, 235, 0.2)',
+    //                       'rgba(255, 206, 86, 0.2)',
+    //                       'rgba(75, 192, 192, 0.2)',
+    //                       'rgba(153, 102, 255, 0.2)',
+    //                       'rgba(255, 159, 64, 0.2)'
+    //                   ],
+    //                   borderColor: [
+    //                       'rgba(255,99,132,1)',
+    //                       'rgba(54, 162, 235, 1)',
+    //                       'rgba(255, 206, 86, 1)',
+    //                       'rgba(75, 192, 192, 1)',
+    //                       'rgba(153, 102, 255, 1)',
+    //                       'rgba(255, 159, 64, 1)'
+    //                   ],
+    //                   borderWidth: 1
+    //               }]
+    //           },
+    //           options: {
+    //               scales: {
+    //                   yAxes: [{
+    //                       ticks: {
+    //                           beginAtZero:true
+    //                       }
+    //                   }]
+    //               }
+    //           }
+    //       });
         
 })

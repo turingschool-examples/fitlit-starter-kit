08d4511b98b6a27fea57d299b2644654cf09527f
James Miller <james.rex.miller.4@gmail.com>
2019-10-14 12:51:13 -0600

    Add charts.sjs, add chart to hydro and activity section. Add friends count to user section

--- src/scripts.js
+++ src/scripts.js
@@ -26,6 +26,7 @@ $(window).on('load', function () {
   $('.user_stride_length').text(`${user.strideLength}`);
   $('.user_step_goal').text(`${user.dailyStepGoal}`);
   $('.div_steps_other_users_p').text(`${userRepository.calculateAverageStepGoal()}`);
+  $('.user_friends').text(`${user.friends.length}`);
   $('.hydro_day').text(hydro.findFluidDate('2019/09/15'));
   $('.hydro_week').text(hydro.findFluidWeek('2019/09/15').join(' '));
   $('.sleep_date').text('2019/09/15');
@@ -43,6 +44,98 @@ $(window).on('load', function () {
   $('.activity_min_active_week').text(activity.minActiveWeek('2019/09/15').join(' '));
   $('.activity_steps_week').text(activity.stepsWeek('2019/09/15').join(' '));
   $('.activity_stairs_week').text(activity.stairsWeek('2019/09/15').join(' '));
+
+
+
+
+
+
+  var ctx = document.getElementById("myChartHydro").getContext('2d');
+  var myChart = new Chart (ctx, {
+      type: 'bar',
+      data: {
+          labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+          datasets: [{
+              label: 'Ounces    This Week',
+              data: hydro.findFluidWeek('2019/09/15'),
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)',
+                  'rgba(30, 178, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)',
+                  'rgba(30, 178, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+  });
+
+
+  var ctx = document.getElementById("myChartStepsWeek").getContext('2d');
+  var myChart = new Chart (ctx, {
+      type: 'bar',
+      data: {
+          labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+          datasets: [{
+              label: 'Steps this Week',
+              data: activity.stepsWeek('2019/09/15'),
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)',
+                  'rgba(30, 178, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)',
+                  'rgba(30, 178, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+  });
+
+
+
+
 })
 
 $( function() {

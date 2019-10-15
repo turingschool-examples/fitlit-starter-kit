c46d15d5477a38f212cd45cbf0950f9dd649d488
Kayla Wood <kaylaewood@gmail.com>
2019-10-15 15:22:38 -0600

    Fix bugs in trend data on DOM

--- src/User.js
+++ src/User.js
@@ -82,8 +82,6 @@
     if (activity.numSteps >= this.dailyStepGoal) {
       this.accomplishedDays.unshift(activity.date);
     }
-    this.findTrendingStepDays();
-    this.findTrendingStairsDays();
   }
   findClimbingRecord() {
     return this.activityRecord.sort((a, b) => {
@@ -126,23 +124,23 @@
     }, 0) / 7).toFixed(1);
   }
   findTrendingStepDays() {
-    var positiveDays = [];
+    let positiveDays = [];
     for (var i = 0; i < this.activityRecord.length; i++) {
       if (this.activityRecord[i + 1] && this.activityRecord[i].steps > this.activityRecord[i + 1].steps) {
         positiveDays.unshift(this.activityRecord[i].date);
       } else if (positiveDays.length > 2) {
-        this.trendingStepDays.push(`You had a ${positiveDays.length} day streak from ${positiveDays[0]} - ${positiveDays[positiveDays.length - 1]}!`);
+        this.trendingStepDays.push(`Your most recent positive step streak was ${positiveDays[0]} - ${positiveDays[positiveDays.length - 1]}!`);
         positiveDays = [];
       }
     }
   }
   findTrendingStairsDays() {
-    var positiveDays = [];
+    let positiveDays = [];
     for (var i = 0; i < this.activityRecord.length; i++) {
       if (this.activityRecord[i + 1] && this.activityRecord[i].flightsOfStairs > this.activityRecord[i + 1].flightsOfStairs) {
         positiveDays.unshift(this.activityRecord[i].date);
       } else if (positiveDays.length > 2) {
-        this.trendingStairsDays.push(`You had a ${positiveDays.length} day streak from ${positiveDays[0]} - ${positiveDays[positiveDays.length - 1]}!`);
+        this.trendingStairsDays.push(`Your most recent positive climbing streak was  ${positiveDays[0]} - ${positiveDays[positiveDays.length - 1]}!`);
         positiveDays = [];
       }
     }

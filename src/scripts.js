2557a1e6200fd1328c58d564a9b1434ee38b8a28
“Foster <fosterataylor@yahoo.com>
2019-10-15 15:08:57 -0600

    Add jquery hide functionality for containers for hydration, sleep, activity data

--- src/scripts.js
+++ src/scripts.js
@@ -28,7 +28,7 @@ $(document).ready(() => {
     $('.profile-address').text(currentPerson.address);
     $('.profile-email').text(currentPerson.email);
     $('.average-stepGoal').text(`Your Daily Step Goal was ${currentPerson.dailyStepGoal}, while everyone else was at ${userRepository.calculateAvgStepGoalAllUsers()}`);
-    $('.hydration1').text(`Your water intake for the past day: ${hydration.calculateAmtDrankByUserSpecificDate(currentDate)} oz`);
+    $('.hydration1').text(`Your water intake for the day has been: ${hydration.calculateAmtDrankByUserSpecificDate(currentDate)} oz`);
     $('.hydration2').text(`Your water intake over each day of the last week: ${hydration.returnDrinkAmtEachDayOverWeekByUser().join(', ')} (oz)`);
     $('.sleep1').text(`Time spent sleeping for each night of the past week: ${sleep.calculateHoursSleptEachDayByUserOverSpecificWeek().join(', ')} (hours)`);
     $('.sleep2').text(`Your hours slept for the past day: ${sleep.returnHoursSleptByUserOnSpecificDate(currentDate)}`);
@@ -61,98 +61,26 @@ $(document).ready(() => {
 
 
     $(".sleep-btn").on('click', function() {
-      $(".sleep-container").toggle()
+        $(".sleep-container").toggle()
+        $(".hydration-container").hide()
+        $(".activity-container").hide()
     });
 
     $(".hydration-btn").on('click', function() {
-      $(".hydration-container").toggle()
+        $(".hydration-container").toggle()
+        $(".activity-container").hide()
+        $(".sleep-container").hide()
     });
 
     $(".activity-btn").on('click', function() {
-      $(".activity-container").toggle()
+        $(".activity-container").toggle()
+        $(".hydration-container").hide()
+        $(".sleep-container").hide()
     });
 
-    $(".profile-btn").on('click', function() {
+    $(".profile-btn").on('click', function () {
+        $(".profile-container").toggle()
     });
 
-
-
-    // $('#myChart').text(hydration.calculateAvgDailyAmtDrankByUserIdAllTime())
-    //       var myChart = new Chart($('#myWeekChart'), {
-    //           type: 'bar',
-    //           data: {
-    //               labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6","Day 7"],
-    //               datasets: [{
-    //                   label: 'Daily Amount Water For Week',
-    //                   data: hydration.returnDrinkAmtEachDayOverWeekByUser(),
-    //                   backgroundColor: [
-    //                       'rgba(255, 99, 132, 0.2)',
-    //                       'rgba(54, 162, 235, 0.2)',
-    //                       'rgba(255, 206, 86, 0.2)',
-    //                       'rgba(75, 192, 192, 0.2)',
-    //                       'rgba(153, 102, 255, 0.2)',
-    //                       'rgba(255, 159, 64, 0.2)'
-    //                   ],
-    //                   borderColor: [
-    //                       'rgba(255,99,132,1)',
-    //                       'rgba(54, 162, 235, 1)',
-    //                       'rgba(255, 206, 86, 1)',
-    //                       'rgba(75, 192, 192, 1)',
-    //                       'rgba(153, 102, 255, 1)',
-    //                       'rgba(255, 159, 64, 1)'
-    //                   ],
-    //                   borderWidth: 1
-    //               }]
-    //           },
-    //           options: {
-    //               scales: {
-    //                   yAxes: [{
-    //                       ticks: {
-    //                           fontSize: 40
-    //                         //   beginAtZero:true
-    //                       }
-    //                   }]
-    //               }
-    //           }
-    //       });
-
-    //     //   $('#myChart').text(hydration.calculateAvgDailyAmtDrankByUserIdAllTime(currentPerson.id))
-    // var myChart = new Chart($('#myDayChart'), {
-    //     // myChart.height(500);
-    //           type: 'line',
-    //           data: {
-    //               labels: ["Day 1"],
-    //               datasets: [{
-    //                   label: 'Water Consumption For Day',
-    //                   data: [5,6],
-    //                   backgroundColor: [
-    //                       'rgba(255, 99, 132, 0.2)',
-    //                       'rgba(54, 162, 235, 0.2)',
-    //                       'rgba(255, 206, 86, 0.2)',
-    //                       'rgba(75, 192, 192, 0.2)',
-    //                       'rgba(153, 102, 255, 0.2)',
-    //                       'rgba(255, 159, 64, 0.2)'
-    //                   ],
-    //                   borderColor: [
-    //                       'rgba(255,99,132,1)',
-    //                       'rgba(54, 162, 235, 1)',
-    //                       'rgba(255, 206, 86, 1)',
-    //                       'rgba(75, 192, 192, 1)',
-    //                       'rgba(153, 102, 255, 1)',
-    //                       'rgba(255, 159, 64, 1)'
-    //                   ],
-    //                   borderWidth: 1
-    //               }]
-    //           },
-    //           options: {
-    //               scales: {
-    //                   yAxes: [{
-    //                       ticks: {
-    //                           beginAtZero:true
-    //                       }
-    //                   }]
-    //               }
-    //           }
-    //       });
-
+ 
 })

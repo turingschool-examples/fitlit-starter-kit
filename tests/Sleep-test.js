d9dd411aea2d0589bfe3c75979899e53dfff1ee7
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-10-16 15:33:27 -0600

    Fix linting errors in Sleep-test.js

--- tests/Sleep-test.js
+++ tests/Sleep-test.js
@@ -66,29 +66,29 @@ describe('Sleep', function() {
     expect(sleep1).to.be.an.instanceof(Sleep);
   });
   it('should hold a userId', function() {
-    expect(sleep2.userId).to.equal(2);;
+    expect(sleep2.userId).to.equal(2);
   });
   it('should hold a date', function() {
-    expect(sleep3.date).to.equal("2019/07/17");;
+    expect(sleep3.date).to.equal("2019/07/17");
   });
   it('should hold hours slept', function() {
-    expect(sleep1.hoursSlept).to.equal(6.1);;
+    expect(sleep1.hoursSlept).to.equal(6.1);
   });
   it('should hold sleep quality', function() {
-    expect(sleep3.sleepQuality).to.equal(1.4);;
+    expect(sleep3.sleepQuality).to.equal(1.4);
   });
   describe('sleep', function() {
     it('should update user\'s slept hours record', function() {
-      expect(user1.sleepHoursRecord.length).to.equal(2);;
+      expect(user1.sleepHoursRecord.length).to.equal(2);
     });
     it('should update user\'s slept hours record', function() {
-      expect(user2.sleepQualityRecord.length).to.equal(1);;
+      expect(user2.sleepQualityRecord.length).to.equal(1);
     });
     it('should update user\'s slept hours average', function() {
-      expect(user1.hoursSleptAverage).to.equal('7.7');;
+      expect(user1.hoursSleptAverage).to.equal('7.7');
     });
     it('should update user\'s sleep quality average', function() {
-      expect(user1.sleepQualityAverage).to.equal('1.8');;
+      expect(user1.sleepQualityAverage).to.equal('1.8');
     });
   })
 });

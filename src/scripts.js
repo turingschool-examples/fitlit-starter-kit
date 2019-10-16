8efb5af30e32a9cfef19850539071f29f90ff295
Veronica Andrade <veronica726@gmail.com>
2019-10-16 11:34:08 -0600

    Add step goal comparison widget

--- src/scripts.js
+++ src/scripts.js
@@ -4,10 +4,11 @@ const randomNum = Math.floor(Math.random() * 50 + 1);
 const randomUser = users.getSingleUserData(randomNum);
 const hydration = new Hydration(hydrationData);
 const sleep = new Sleep(randomUser.id, sleepData);
-const activity = new Activity(activityData);
+const activity = new Activity(activityData, userData);
 const mainContainer = document.querySelector('.main__container')
 const header = document.querySelector('header');
 const widgets = document.querySelector('.daily-widget-container');
+const stepComparison = document.querySelector('.average-step-comparison')
 const todaysDate = grabDate();
 
 
@@ -40,29 +41,41 @@ header.insertAdjacentHTML('beforeend', `<h2 class='name--display'>Welcome, ${ran
 `)
 
 
-widgets.insertAdjacentHTML('afterbegin', `<div class="hydration--widget card">
+widgets.insertAdjacentHTML('afterbegin', `<div class="hydration--widget">
+        <div class="widget-card-container">
         <h3 class="widget-hydration-header">Daily Fluid Intake</h3>
         <p class="widget--informer">Today you drank</p>
         <img class="water-bottle-icon"src="../images/water_bottle-icon.svg">
         <p class="widget--variable hydration--variable">${hydration.calculateDailyIntake(todaysDate, randomUser.id)}oz</p>
-      </div>
-      <div class="sleep--widget card">
+        </div>
+        </div>
+      <div class="sleep--widget">
         <h3 class="widget-sleep-header">Last Night's Sleep</h3>
         <p class="widget--informer">Last night you slept</p>
-        <p class="widget--variable sleep--variable">7.5 hrs</p>
+        <pclass="widget--variable sleep--variable">${sleep.findHoursSlept(randomUser.id, todaysDate)}hrs</p>
         <img class="bed-icon" src="../images/single-bed-icon.svg">
-        <p class="widget--informer">Your Sleep Quality: 3.7</p>
+        <p class="widget--informer">Your Sleep Quality: ${sleep.findSleepQuality(randomUser.id, todaysDate)}</p>
       </div>
-      <div class="activity--widget card">
+      <div class="activity--widget">
         <h3 class="widget-activity-header">Steps</h3>
         <p class="widget--informer">Your steps</p>
-        <p class="widget--variable activity--variable">3,000 steps</p>
+        <p class="widget--variable activity--variable">${activity.findSingleDayInfo(randomUser.id, todaysDate).numSteps}</p>
         <img class="step-icon" src="../images/activity-icon.svg">
         <div class="activity-details">
-          <p class="widget-activity-informer">Miles: 7.8</p>
-          <p class="widget-activity-informer">Active minutes: 120 min</p>
+          <p class="widget-activity-informer">Miles: ${grabDailyMiles(randomUser.id, todaysDate)}</p>
+          <p class="widget-activity-informer">Active minutes: ${grabActiveMinutes(randomUser.id, todaysDate)} min</p>
         </div>
       </div>`)
+
+stepComparison.insertAdjacentHTML('afterbegin', `<div class="user-avg-steps">
+          <p>Your Step Goal:</p>
+          <span class="comparison-number">${randomUser.dailyStepGoal}</span>
+        </div>
+        <div class="line-divider"></div>
+        <div class="world-avg-steps">
+        <p>Overall Average Step Goal</p>
+        <p>${findOverallStepGoals()}</p>
+        </div>`)
 
 
 function grabDate() {
@@ -70,6 +83,27 @@ function grabDate() {
   return hydrationData[index].date;
 }
 
+function grabDailyMiles(id, date) {
+  let user = activity.findSingleUserData(id);
+  let day = activity.findSingleDayInfo(id, date);
+  let stepsPerMi = 5280 / user.strideLength;
+  return +((day.numSteps / stepsPerMi).toFixed(2))
+}
+
+function grabActiveMinutes(id, date) {
+  activity.findSingleDayInfo(id, date);
+  return activity.singleDay.minutesActive
+}
+
+function findOverallStepGoals() {
+  let usersStepAverage = userData.reduce((acc, user) => {
+    acc += user.dailyStepGoal
+    return acc;
+  }, 0)
+  return +((usersStepAverage / userData.length - 1).toFixed());
+}
+
+findOverallStepGoals();
 
 var ctx = document.getElementById('weekly--water').getContext('2d');
 var chart = new Chart(ctx, {
@@ -88,7 +122,7 @@ var chart = new Chart(ctx, {
 
 var sleepInfo = document.getElementById('weekly--sleep').getContext('2d');
 var sleepChart = new Chart ( sleepInfo, {
-  type: 'bar',
+  type: 'line',
   data: {
     labels: sleep.findAWeek(randomUser.id).map(day => day.date),
     datasets: [{

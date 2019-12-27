4a7535676d2381ca651e329372ce0a1f1722b1e5
Jesse Maxim <jessmaxim303@gmail.com>
2019-12-27 12:38:20 -0700

    Missing file

--- test/user-test.js
+++ test/user-test.js
@@ -1,2 +1,16 @@
 const chai = require('chai');
 const expect = chai.expect;
+
+describe('User', () => {
+
+  let user;
+  beforeEach(() => {
+    let userID = 0;
+    user = new User(userData)
+  });
+
+  it('should be a function', () => {
+    expect(User).to.be.a('function');
+  });
+
+});

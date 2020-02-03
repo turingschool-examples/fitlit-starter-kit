998e6605099f32dbf1a6d1154f4918a071796693
kevin johnson <kevinj902302@gmail.com>
2020-02-03 15:48:46 -0700

    Added class files and built out User class

---
+++ src/users.js
@@ -0,0 +1,25 @@
+class User {
+constructor(userInfo) {
+  this.id = userInfo.id;
+  this.name = userInfo.name;
+  this.address = userInfo.address;
+  this.email = userInfo.email;
+  this.strideLength = userInfo.strideLength;
+  this.dailyStepGoal = userInfo.dailyStepGoal;
+  this.friends = userInfo.friends;
+}
+
+  returnUserFirstName() {
+    
+  }
+
+
+
+
+
+
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = User;
+}

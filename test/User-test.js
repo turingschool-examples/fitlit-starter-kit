a3fd5af4cdf288da7fb1e38ab14e97465ac9fe64
Cristina Pena <cristinalpena@gmail.com>
2020-02-04 14:18:05 -0700

    Fix tests to work for user data object

--- test/User-test.js
+++ test/User-test.js
@@ -6,7 +6,7 @@ describe('User', function() {
   let user;
 
   beforeEach(function() {
-    user = new User({id:1, name: 'Luisa Hane', address: '15195 Nakia Tunnel, Erdmanport VA 19901-1697', email: 'Diana.Hayes1@hotmail.com', sstrideLength: 4.3, dailtStepGoal: 10000, friends: [16, 4, 8]});
+    user = new User({id:1, name: 'Luisa Hane', address: '15195 Nakia Tunnel, Erdmanport VA 19901-1697', email: 'Diana.Hayes1@hotmail.com', strideLength: 4.3, dailyStepGoal: 10000, friends: [16, 4, 8]});
   });
 
   it('should be a function', function() {

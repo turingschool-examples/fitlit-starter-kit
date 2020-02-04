c7ad2496aaac1a7a27804a794add879f33e01740
Madison Randle <madisonrandle25@gmail.com>
2020-02-03 22:29:37 -0700

    Write test for user properties

--- test/User-test.js
+++ test/User-test.js
@@ -18,6 +18,34 @@ describe('User', function() {
     expect(user).to.be.an.instanceof(User);
   });
 
+  it('should have an id', function() {
+    expect(user.id).to.equal(1);
+  });
+
+  it('should have a name', function() {
+    expect(user.name).to.equal('Luisa Hane');
+  });
+
+  it('should have an address', function() {
+    expect(user.address).to.equal('15195 Nakia Tunnel, Erdmanport VA 19901-1697');
+  });
+
+  it('should have an email', function() {
+    expect(user.email).to.equal('Diana.Hayes1@hotmail.com');
+  });
+
+  it('should have a stride length', function() {
+    expect(user.strideLength).to.equal(4.3);
+  });
+
+  it('should have a daily step goal', function() {
+    expect(user.dailyStepGoal).to.equal(10000);
+  });
+
+  it('should have friends', function() {
+    expect(user.friends).to.deep.equal([16, 4, 8]);
+  });
+
   it('should return a users first name only', function() {
     expect(user.getUsersFirstName()).to.equal('Luisa');
   })

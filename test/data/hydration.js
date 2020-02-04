b13168e324015102e9ee6b3e83f920af4aed0e77
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-02-04 13:10:21 -0700

    Add hydration data for testing purposes

---
+++ test/data/hydration.js
@@ -0,0 +1,107 @@
+const hydrationData = [
+  {
+    "userID": 1,
+    "date": "2020/02/02",
+    "numOunces": 27
+  },
+  {
+    "userID": 1,
+    "date": "2020/02/03",
+    "numOunces": 55
+  },
+  {
+    "userID": 1,
+    "date": "2020/02/04",
+    "numOunces": 87
+  },
+  {
+    "userID": 1,
+    "date": "2020/02/05",
+    "numOunces": 91
+  },
+  {
+    "userID": 1,
+    "date": "2020/02/06",
+    "numOunces": 80
+  },
+  {
+    "userID": 1,
+    "date": "2020/02/07",
+    "numOunces": 36
+  },
+  {
+    "userID": 1,
+    "date": "2020/02/08",
+    "numOunces": 84
+  },
+  {
+    "userID": 11,
+    "date": "2020/02/02",
+    "numOunces": 68
+  },
+  {
+    "userID": 11,
+    "date": "2020/02/03",
+    "numOunces": 29
+  },
+  {
+    "userID": 11,
+    "date": "2020/02/04",
+    "numOunces": 62
+  },
+  {
+    "userID": 11,
+    "date": "2020/02/05",
+    "numOunces": 77
+  },
+  {
+    "userID": 11,
+    "date": "2020/02/06",
+    "numOunces": 83
+  },
+  {
+    "userID": 11,
+    "date": "2020/02/07",
+    "numOunces": 24
+  },
+  {
+    "userID": 11,
+    "date": "2020/02/08",
+    "numOunces": 37
+  },
+  {
+    "userID": 111,
+    "date": "2020/02/02",
+    "numOunces": 53
+  },
+  {
+    "userID": 111,
+    "date": "2020/02/03",
+    "numOunces": 91
+  },
+  {
+    "userID": 111,
+    "date": "2020/02/04",
+    "numOunces": 49
+  },
+  {
+    "userID": 111,
+    "date": "2020/02/05",
+    "numOunces": 76
+  },
+  {
+    "userID": 111,
+    "date": "2020/02/06",
+    "numOunces": 92
+  },
+  {
+    "userID": 111,
+    "date": "2020/02/07",
+    "numOunces": 65
+  },
+  {
+    "userID": 111,
+    "date": "2020/02/08",
+    "numOunces": 51
+  },
+];

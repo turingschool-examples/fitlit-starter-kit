76de575e430c4e2c57e37fa486af80bc041b714c
Melissa Steele <melissa.steele09@gmail.com>
2020-02-04 15:25:30 -0700

    Add sleep class properties and methods

---
+++ src/sleep.js
@@ -0,0 +1,40 @@
+class Sleep {
+  constructor(sleepData) {
+    this.userID = userId,
+    this.date = date,
+    this.hoursSlept = hoursSlept,
+    this.sleepQuality = sleepQuality
+  }
+
+  calculateSleepAverage() {
+
+  }
+
+  returnHoursSlept() {
+
+  }
+
+  returnSleepQuality() {
+
+  }
+
+  returnWeeklyHoursSlept() {
+
+  }
+
+  returnWeeklySleepQuality() {
+
+  }
+
+  findQualitySleepers() {
+
+  }
+
+  findHighestSleepers() {
+
+  }
+
+  calculateWeeklyQualitySleepChange() {
+    
+  }
+}

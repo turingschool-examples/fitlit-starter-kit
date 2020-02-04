2e38e8613bf2a026b8bad99d852805e584bafca8
Madison Randle <madisonrandle25@gmail.com>
2020-02-03 19:55:26 -0700

    Add more class structure

--- src/Sleep.js
+++ src/Sleep.js
@@ -5,7 +5,34 @@ class Sleep {
     this.hoursSlept = hoursSlept;
     this.sleepQuality = sleepQuality;
   }
+
+  calculateAvgSleepHours() {
+    //
+  }
+
+  findDailySleepHours() {
+    // HOURS SLEPT FOR SPECIFIC DAY
+  }
+
+  findWeeklySleepHours() {
+    // HOURS SLEPT EACH DAY OVER THE COURSE OF A GIVEN WEEK
+  }
+
+  findWeeklySleepQuality() {
+    // SLEEP QUALITY EACH DAY OVER THE COURSE OF A GIVEN WEEK
+  }
+
+  findDailySleepQuality() {
+    // FIND THE SLEEP QUALITY FOR A SPECIFIC DAY
+  }
+
+  calculateAvgSleepQuality() {
+    // THE AVG SLEEP QUALITY PER DAY FOR ALL TIME
+  }
 }
+
+
+
 
 
 if (typeof module !== 'undefined') {

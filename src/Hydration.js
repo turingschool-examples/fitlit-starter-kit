d09cba24caca05b561417010f15733617a8ab81a
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-02-04 16:54:31 -0700

    Add intial Class method and test for Sleep

---
+++ src/Hydration.js
@@ -0,0 +1,21 @@
+class Hydration {
+  constructor() {
+    this.userID;
+    this.date;
+    this.numOunces;
+  }
+
+  calculateAverageFluidIntakeForUser() {
+  }
+
+  calculateFluidIntakeForDay() {
+  }
+
+  calculateTotalIntakeForWeek() {
+  }
+
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = Hydration;
+}
---
+++ test/hydration-test.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Hydration = require('../src/Hydration');
+
+describe('Hydration', function() {
+
+  it('should be a function', function() {
+    const hydration = new Hydration();
+
+    expect(Hydration).to.be.a('function');
+  });
+
+  it('should be an instance of Hydration', function() {
+    const hydration = new Hydration();
+
+    expect(hydration).to.be.an.instanceof(Hydration);
+  });
+
+});

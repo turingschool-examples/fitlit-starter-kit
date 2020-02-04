771f7ba0465ef6b3d0e9e19041a7c9c99e3b8be9
Madison Randle <madisonrandle25@gmail.com>
2020-02-03 20:56:10 -0700

    Create test structure

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -1,2 +1,19 @@
 const chai = require('chai');
 const expect = chai.expect;
+const Sleep = require('../src/Sleep');
+
+describe('Sleep', function() {
+  let sleep;
+
+  beforeEach(function() {
+    sleep = new Sleep(1, '2019/06/15', 6.1, 2.2);
+  });
+
+  it('should be a function', function() {
+    expect(Sleep).to.be.a('function');
+  });
+
+  it('should be an instance of Sleep', function() {
+    expect(sleep).to.be.an.instanceof(Sleep);
+  });
+});

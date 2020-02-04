7475f86eb809b1690912a66f10de648553815a3c
Melissa Steele <melissa.steele09@gmail.com>
2020-02-04 14:52:08 -0700

    Add userRepo class properties and methods

---
+++ src/userRepository.js
@@ -0,0 +1,33 @@
+class UserRepository {
+  constructor(data) {
+    this.users = user
+  }
+
+  returnUserData() {
+
+  }
+
+  countUsers() {
+
+  }
+
+  calculateAverageStepGoal() {
+
+  }
+
+  calculateAverageStep() {
+
+  }
+
+  calculateAverageSleepQuality() {
+
+  }
+
+  calculateAverageNumStairsClimbed() {
+
+  }
+
+  calculateAverageActiveMinutes() {
+
+  }
+}

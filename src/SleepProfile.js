c4dd628a5ada9192e3fb7b5bc6a44eb0c3e38f30
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-03 17:36:30 -0700

    Outline methods

---
+++ src/SleepProfile.js
@@ -0,0 +1,33 @@
+class Sleep {
+  constructor(userID) {
+//entries
+  }
+
+  calculateAvgHoursAllTime() {
+
+  }
+
+  calculateAvgHoursWeek(startDate) {
+
+  }
+
+  calculateSleepQualityAllTime() {
+
+  }
+
+  calculateSleepQualityWeek() {
+
+  }
+
+  findHoursSlept(date) {
+
+  }
+
+  findSleepQuality(date) {
+    
+  }
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = SleepProfile;
+}

6bdc0ce2b8a02d6ef903c505c17a3c063f8606cb
Cristina Pena <cristinalpena@gmail.com>
2020-02-04 14:22:39 -0700

    Fix user repo tests to accept user data objs

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -10,9 +10,9 @@ describe('User', function() {
   let userRepository;
 
   beforeEach(function() {
-    user1 = new User(1, 'Luisa Hane', '15195 Nakia Tunnel, Erdmanport VA 19901-1697', 'Diana.Hayes1@hotmail.com', 4.3, 10000, [16, 4, 8]);
-    user2 = new User(2, 'Jarvis Considine', '30086 Kathryn Port, Ciceroland NE 0727330086 Kathryn Port, Ciceroland NE 07273', 'Dimitri.Bechtelar11@gmail.com', 4.5, 5000, [9, 18, 24, 19]);
-    user3 = new User(3, 'Herminia Witting', '85823 Bosco Fork, East Oscarstad MI 85126-5660', 'Elwin.Tromp@yahoo.com', 4.4, 5000, [19, 11, 42, 33]);
+    user1 = new User({id:1, name: 'Luisa Hane', address: '15195 Nakia Tunnel, Erdmanport VA 19901-1697', email: 'Diana.Hayes1@hotmail.com', strideLength: 4.3, dailyStepGoal: 10000, friends: [16, 4, 8]});
+    user2 = new User({id: 2, name: 'Jarvis Considine', address: '30086 Kathryn Port, Ciceroland NE 0727330086 Kathryn Port, Ciceroland NE 07273', email: 'Dimitri.Bechtelar11@gmail.com', strideLength: 4.5, dailyStepGoal: 5000, friends: [9, 18, 24, 19]});
+    user3 = new User({id: 3, name: 'Herminia Witting', address: '85823 Bosco Fork, East Oscarstad MI 85126-5660', email: 'Elwin.Tromp@yahoo.com', strideLength: 4.4, dailyStepGoal: 5000, friends: [19, 11, 42, 33]});
     userRepository = new UserRepository([user1, user2, user3])
   });
 
@@ -24,7 +24,7 @@ describe('User', function() {
     expect(userRepository).to.be.an.instanceof(UserRepository);
   });
 
-  it('should except an array of user objects', function() {
+  it('should accept an array of user objects', function() {
     expect(userRepository.userData[0]).to.equal(user1);
     expect(userRepository.userData[1]).to.equal(user2);
     expect(userRepository.userData[2]).to.equal(user3);

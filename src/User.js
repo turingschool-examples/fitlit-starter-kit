a6e0ecd3ac03a7eed1de8a0532dcdecc91ef6da3
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-03 17:30:29 -0700

    Outline methods and pseudocoding comments

---
+++ src/User.js
@@ -0,0 +1,18 @@
+class User {
+  constructor(userData) {
+// id, name, address, email, strideLength, dailyStepGoal, friends(with friend ids)
+  }
+
+  returnFirstName() {
+
+  }
+
+  compareStepsWithFriend(friendID, startDate) {
+// Find total steps for user and for friend
+// Return a message that says “___ had __ more steps this week!”
+  }
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = UserRepository;
+}

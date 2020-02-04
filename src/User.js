7028f20c904577ac3b68e6e2aa1dfb2392eeccba
Karen Nguyen <knguyen4184@yahoo.com>
2020-02-04 12:21:00 -0700

    Write structure for User and UserRepository class

---
+++ src/User.js
@@ -0,0 +1,15 @@
+class User {
+  constructor(userData) {
+    this.id = userData.id;
+    this.name = userData.name;
+    this.address = userData.address;
+    this.email = userData.email;
+    this.strideLength = userData.strideLength;
+    this.dailyStepGoal = userData.dailyStepGoal;
+    this.friends = userData.friends;
+  }
+}
+
+  if (typeof module !== 'undefined') {
+    module.exports = User;
+  }

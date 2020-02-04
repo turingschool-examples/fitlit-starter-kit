ff916fbf8b1130a1ab96564f8428bb89a5977d83
Karen Nguyen <knguyen4184@yahoo.com>
2020-02-04 16:37:16 -0700

    Write basic test for the User class

--- test/user-test.js
+++ test/user-test.js
@@ -9,7 +9,8 @@ describe('User', function() {
   let user;
 
   beforeEach(() => {
-    user = new User({
+    user = new User(
+    {
       'id': 1,
       'name': 'Luisa Hane',
       'address': '15195 Nakia Tunnel, Erdmanport VA 19901-1697',
@@ -21,8 +22,8 @@ describe('User', function() {
         4,
         8
       ]
-    })
-  })
+    });
+  });
 
   it('should be a function', function() {
     expect(User).to.be.a('function');

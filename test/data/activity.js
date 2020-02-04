a10f981fbcd6ee7684fe1129509ce900fecd7ef9
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-02-04 13:03:37 -0700

    Add activity data for testing purposes

---
+++ test/data/activity.js
@@ -0,0 +1,149 @@
+const activityData = [
+  {
+    "userID": 1,
+    "date": "2020/02/02",
+    "numSteps": 3273,
+    "minutesActive": 14,
+    "flightsOfStairs": 20
+  },
+  {
+    "userID": 1,
+    "date": "2020/02/03",
+    "numSteps": 3740,
+    "minutesActive": 62,
+    "flightsOfStairs": 44
+  },
+  {
+    "userID": 1,
+    "date": "2020/02/04",
+    "numSteps": 11006,
+    "minutesActive": 89,
+    "flightsOfStairs": 38
+  },
+  {
+    "userID": 1,
+    "date": "2020/02/05",
+    "numSteps": 9683,
+    "minutesActive": 52,
+    "flightsOfStairs": 7
+  },
+  {
+    "userID": 1,
+    "date": "2020/02/06",
+    "numSteps": 7586,
+    "minutesActive": 244,
+    "flightsOfStairs": 34
+  },
+  {
+    "userID": 1,
+    "date": "2020/02/07",
+    "numSteps": 9937,
+    "minutesActive": 89,
+    "flightsOfStairs": 49
+  },
+  {
+    "userID": 1,
+    "date": "2020/02/08",
+    "numSteps": 5891,
+    "minutesActive": 99,
+    "flightsOfStairs": 36
+  },
+  {
+    "userID": 11,
+    "date": "2020/02/02",
+    "numSteps": 4165,
+    "minutesActive": 298,
+    "flightsOfStairs": 46
+  },
+  {
+    "userID": 11,
+    "date": "2020/02/03",
+    "numSteps": 3203,
+    "minutesActive": 39,
+    "flightsOfStairs": 24
+  },
+  {
+    "userID": 11,
+    "date": "2020/02/04",
+    "numSteps": 11263,
+    "minutesActive": 272,
+    "flightsOfStairs": 13
+  },
+  {
+    "userID": 11,
+    "date": "2020/02/05",
+    "numSteps": 4460,
+    "minutesActive": 228,
+    "flightsOfStairs": 36
+  },
+  {
+    "userID": 11,
+    "date": "2020/02/06",
+    "numSteps": 8986,
+    "minutesActive": 285,
+    "flightsOfStairs": 0
+  },
+  {
+    "userID": 11,
+    "date": "2020/02/07",
+    "numSteps": 7490,
+    "minutesActive": 282,
+    "flightsOfStairs": 2
+  },
+  {
+    "userID": 11,
+    "date": "2020/02/08",
+    "numSteps": 14520,
+    "minutesActive": 122,
+    "flightsOfStairs": 35
+  },
+  {
+    "userID": 111,
+    "date": "2020/02/02",
+    "numSteps": 8145,
+    "minutesActive": 194,
+    "flightsOfStairs": 2
+  },
+  {
+    "userID": 111,
+    "date": "2020/02/03",
+    "numSteps": 6531,
+    "minutesActive": 119,
+    "flightsOfStairs": 2
+  },
+  {
+    "userID": 111,
+    "date": "2020/02/04",
+    "numSteps": 4322,
+    "minutesActive": 63,
+    "flightsOfStairs": 2
+  },
+  {
+    "userID": 111,
+    "date": "2020/02/05",
+    "numSteps": 4178,
+    "minutesActive": 88,
+    "flightsOfStairs": 2
+  },
+  {
+    "userID": 111,
+    "date": "2020/02/06",
+    "numSteps": 4603,
+    "minutesActive": 218,
+    "flightsOfStairs": 2
+  },
+  {
+    "userID": 111,
+    "date": "2020/02/07",
+    "numSteps": 7765,
+    "minutesActive": 58,
+    "flightsOfStairs": 2
+  },
+  {
+    "userID": 111,
+    "date": "2020/02/08",
+    "numSteps": 8200,
+    "minutesActive": 135,
+    "flightsOfStairs": 2
+  }
+];

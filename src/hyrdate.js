b846e7e93966540f96245e409533335ef6f68fa3
Melissa Steele <melissa.steele09@gmail.com>
2020-02-04 15:15:26 -0700

    Add hyrdation class properties and methods

---
+++ src/hyrdate.js
@@ -0,0 +1,19 @@
+class Hydration {
+  constructor(hydrationData) {
+    this.userId = userID,
+    this.date = date,
+    this.numOunces = numOunces,
+  }
+
+  calculateAverageDailyConsumption() {
+
+  }
+
+  returnDailyConsumption() {
+
+  }
+
+  returnWeeklyConsumption() {
+
+  }
+}

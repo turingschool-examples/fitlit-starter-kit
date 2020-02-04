ba4b24eab762d47a44e3a3a5069ed28e35bc77f5
Ryan Bahan <ryan@ryanbahan.com>
2020-02-04 14:59:19 -0700

    Add testing data for TDD

---
+++ test-data/activity-test.js
@@ -0,0 +1,98 @@
+const activityData = [{
+    "userID": 1,
+    "date": "2019/06/15",
+    "numSteps": 3577,
+    "minutesActive": 140,
+    "flightsOfStairs": 16
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "numSteps": 4294,
+    "minutesActive": 138,
+    "flightsOfStairs": 10
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/17",
+    "numSteps": 7402,
+    "minutesActive": 116,
+    "flightsOfStairs": 33
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/18",
+    "numSteps": 3486,
+    "minutesActive": 114,
+    "flightsOfStairs": 32
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/19",
+    "numSteps": 11374,
+    "minutesActive": 213,
+    "flightsOfStairs": 13
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/20",
+    "numSteps": 14810,
+    "minutesActive": 287,
+    "flightsOfStairs": 18
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/21",
+    "numSteps": 2634,
+    "minutesActive": 107,
+    "flightsOfStairs": 5
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/15",
+    "numSteps": 10333,
+    "minutesActive": 114,
+    "flightsOfStairs": 31
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/16",
+    "numSteps": 6389,
+    "minutesActive": 41,
+    "flightsOfStairs": 33
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/17",
+    "numSteps": 8015,
+    "minutesActive": 106,
+    "flightsOfStairs": 37
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/18",
+    "numSteps": 11652,
+    "minutesActive": 20,
+    "flightsOfStairs": 24
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/19",
+    "numSteps": 9256,
+    "minutesActive": 108,
+    "flightsOfStairs": 2
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/20",
+    "numSteps": 9303,
+    "minutesActive": 27,
+    "flightsOfStairs": 14
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/21",
+    "numSteps": 8024,
+    "minutesActive": 216,
+    "flightsOfStairs": 32
+  }];

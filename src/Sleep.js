142f0b9214a79448bada8e47a31285588b3cdb40
Madison Randle <madisonrandle25@gmail.com>
2020-02-03 17:57:09 -0700

    Create class structure

---
+++ src/Sleep.js
@@ -0,0 +1,13 @@
+class Sleep {
+  constructor() {
+    this.userID = userID;
+    this.date = date;
+    this.hoursSlept = hoursSlept;
+    this.sleepQuality = sleepQuality;
+  }
+}
+
+
+if (typeof module !== 'undefined') {
+  module.exports = Sleep;
+}

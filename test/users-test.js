42348e366ab3f0d26a952fb0c5b576a3e9872925
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-02-04 17:56:16 -0700

    Change method name and fixed some syntax

--- test/users-test.js
+++ test/users-test.js
@@ -19,31 +19,31 @@ describe('User', function() {
     expect(user).to.be.an.instanceof(User);
   });
 
-  it.skip('should have an id', function() {
+  it('should have an id', function() {
     expect(user.id).to.equal(1)
-  })
+  });
 
-  it.skip('should have a name', function() {
-    expect(user.name).to.equal('Hanna Hudson')
-  })
+  it('should have a name', function() {
+    expect(user.name).to.equal('Hannah Hudson')
+  });
 
-  it.skip('should have an address', function() {
+  it('should have an address', function() {
     expect(user.address).to.equal('1234 Fake Street, Townsville PR 00000-0000')
-  })
+  });
 
-  it.skip('should have a stride length', function() {
+  it('should have a stride length', function() {
     expect(user.strideLength).to.equal(3.7)
-  })
+  });
 
-  it.skip('should have a daily step goal', function() {
+  it('should have a daily step goal', function() {
     expect(user.dailyStepGoal).to.equal(2000)
-  })
+  });
 
-  it.skip('should have some friends', function(){
+  it('should have some friends', function(){
     expect(user.friends).to.deep.equal([1, 111])
-  })
+  });
 
-  it.skip('should be able to return a first', function(){
-    expect(user.returnFirstName()).to.equal('Hannah')
-  })
-})
+  it('should be able to return a first name', function(){
+    expect(user.getFirstName()).to.equal('Hannah')
+  });
+});

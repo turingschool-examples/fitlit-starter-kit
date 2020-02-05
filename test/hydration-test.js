22a3d716f60e5aa6795edd83e1592d3668017ebe
Karen Nguyen <knguyen4184@yahoo.com>
2020-02-05 15:40:46 -0700

    Write test for getFluidConsumedDay

--- test/hydration-test.js
+++ test/hydration-test.js
@@ -41,6 +41,52 @@ describe('User', function() {
   });
 
   it('should show how many fluid ounces conusmed that day', function() {
-    expect(hydration.calculateAverageFluidsConsumed(1)).to.equal(37);
+    expect(hydration. getFluidConsumedDay(1, "2019/06/15")).to.equal(37);
+  });
+
+  it('should show the fluids consumed in the past 7 days', function() {
+    hydrationData = [
+      {
+        "userID": 1,
+        "date": "2019/06/15",
+        "numOunces": 37
+      },
+      {
+        "userID": 1,
+        "date": "2019/07/16",
+        "numOunces": 75
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/17",
+        "numOunces": 42
+      },
+      {
+        "userID": 1,
+        "date": "2019/07/18",
+        "numOunces": 10
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/19",
+        "numOunces": 31
+      },
+      {
+        "userID": 1,
+        "date": "2019/07/20",
+        "numOunces": 58
+      },
+      {
+        "userID": 1,
+        "date": "2019/07/21",
+        "numOunces": 64
+      },
+      {
+        "userID": 3,
+        "date": "2019/06/17",
+        "numOunces": 47
+      }
+    ]
+    expect(hydration.getFluidConsumedSevenDay()).to.deep.equal([37, 75, 42, 10, 31, 58, 64, 47]);
   });
 });

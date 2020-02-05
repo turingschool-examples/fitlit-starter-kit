232c0063163070ce36e5e1d4f800e19798b140c4
Olivia Webster <oliviacweb@gmail.com>
2020-02-05 15:59:44 -0700

    Add first name to Dom onload

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+
+
+let welcomeDisplay = document.querySelector('.display-name');
+
+
+
+window.onload = function() {
+   const usersRepository = new UsersRepository(2);
+   const userInfo = usersRepository.getUserDataById(userData);
+   const user = new User(userInfo);
+   welcomeDisplay.innerText = `Welcome, ${user.returnUserFirstName()}`;
+   console.log(user.returnUserFirstName());
+}

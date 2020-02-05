33bb0d37b13f0de09f0a3dca515cb6915f7077c1
kevin johnson <kevinj902302@gmail.com>
2020-02-04 21:53:21 -0700

    Made sample hydration dataset

---
+++ data/sampleHydrationData.js
@@ -0,0 +1,149 @@
+const sampleHydrationData = [
+  {
+    "userID": 1,
+    "date": "2019/06/15",
+    "numOunces": 37
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "numOunces": 75
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "numOunces": 47
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/15",
+    "numOunces": 85
+  },
+  {
+    "userID": 5,
+    "date": "2019/06/15",
+    "numOunces": 42
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "numOunces": 69
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/16",
+    "numOunces": 91
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/16",
+    "numOunces": 99
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/16",
+    "numOunces": 95
+  },
+  {
+    "userID": 5,
+    "date": "2019/06/16",
+    "numOunces": 79
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/17",
+    "numOunces": 96
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/17",
+    "numOunces": 96
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/17",
+    "numOunces": 28
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/17",
+    "numOunces": 82
+  },
+  {
+    "userID": 5,
+    "date": "2019/06/17",
+    "numOunces": 99
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/18",
+    "numOunces": 29
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/19",
+    "numOunces": 21
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/20",
+    "numOunces": 23
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/21",
+    "numOunces": 26
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/22",
+    "numOunces": 78
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/23",
+    "numOunces": 35
+  }
+];
+
+const user3weekOfOunces = [
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "numOunces": 47
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/16",
+    "numOunces": 99
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/17",
+    "numOunces": 28
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/18",
+    "numOunces": 29
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/19",
+    "numOunces": 21
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/20",
+    "numOunces": 23
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/21",
+    "numOunces": 26
+  }
+];
+
+if (typeof module !== 'undefined') {
+  module.exports = sampleHydrationData;
+}

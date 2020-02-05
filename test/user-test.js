61bc083e4881d0df0870ccd905c78d7b8cb85e03
Olivia Webster <oliviacweb@gmail.com>
2020-02-04 18:06:24 -0700

    Add method to return first name and pass test

--- test/user-test.js
+++ test/user-test.js
@@ -4,17 +4,60 @@ const expect = chai.expect;
 const User = require('../src/User');
 
 describe('User', function() {
-
   it('should be a function', function() {
-    const user = new User();
-
+   //  let user = {
+   //   "id": 4,
+   //   "name": "Mae Connelly",
+   //   "address": "28926 Schinner Islands, Turnermouth NE 23720-3230",
+   //   "email": "Marcos_Pollich@hotmail.com",
+   //   "strideLength": 3.1,
+   //   "dailyStepGoal": 4000,
+   //   "friends": [
+   //     48,
+   //     7,
+   //     44,
+   //     8
+   //   ]
+   // }
     expect(User).to.be.a('function');
   });
 
   it('should be an instance of User', function() {
-    const user = new User();
-
-    expect(user).to.be.an.instanceof(User);
+  let user1 = {
+     "id": 4,
+     "name": "Mae Connelly",
+     "address": "28926 Schinner Islands, Turnermouth NE 23720-3230",
+     "email": "Marcos_Pollich@hotmail.com",
+     "strideLength": 3.1,
+     "dailyStepGoal": 4000,
+     "friends": [
+       48,
+       7,
+       44,
+       8
+     ]
+   }
+  const user = new User(user1);
+  expect(user).to.be.an.instanceof(User);
   });
 
+  it('should return Users first name', function() {
+    let user1 = {
+       "id": 4,
+       "name": "Mae Connelly",
+       "address": "28926 Schinner Islands, Turnermouth NE 23720-3230",
+       "email": "Marcos_Pollich@hotmail.com",
+       "strideLength": 3.1,
+       "dailyStepGoal": 4000,
+       "friends": [
+         48,
+         7,
+         44,
+         8
+       ]
+     }
+    const user = new User(user1);
+    expect(user.returnUserFirstName()).to.equal("Mae");
+
+  });
 });

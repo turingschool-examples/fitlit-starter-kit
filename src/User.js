0794c2ca813ccdf462ed2a311fbc2f1d540fa699
Dan Reardon <dreardon1021@gmail.com>
2020-02-05 15:31:23 -0700

    Add user info DOM manipulation exluding friends

--- src/User.js
+++ src/User.js
@@ -3,6 +3,7 @@ class User {
     this.id = userData.id;
     this.name = userData.name;
     this.address = userData.address;
+    this.email = userData.email
     this.strideLength = userData.strideLength;
     this.dailyStepGoal = userData.dailyStepGoal;
     this.friends = userData.friends;
--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,20 @@
-let userRepo = new UserRepository(userData)
+let userRepo = new UserRepository(userData);
 
-const userName = document.querySelector('.user-name')
-const userAddress = document.querySelector('.user-address')
-
+const userName = document.querySelector('.user-name');
+const userAddress = document.querySelector('.user-address');
+const userEmail = document.querySelector('.user-email');
+const userStride = document.querySelector('.user-stride-length');
+const userStepGoal = document.querySelector('.user-step-goal')
+const userGoalAverage = document.querySelector('.step-goal-average');
 
 function populateUserInfo(id) {
   let user = new User(userRepo.getUserData(id))
   userName.innerHTML = user.name;
   userAddress.innerHTML = user.address;
+  userEmail.innerHTML = user.email;
+  userStride.innerHTML = user.strideLength;
+  userStepGoal.innerHTML = user.dailyStepGoal;
+  userGoalAverage.innerHTML = userRepo.getStepGoalAverage();
 }
 
 populateUserInfo(42)

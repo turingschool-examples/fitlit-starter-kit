f8f1eb663b3c57d678b610fb3208d3f0e6df386b
Olivia Webster <oliviacweb@gmail.com>
2020-02-05 16:34:37 -0700

    Add random number generator to return random info

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,3 @@
-
-
 let welcomeDisplay = document.querySelector('.display-name');
 let userFullName = document.querySelector('.full-name');
 let userAddress = document.querySelector('.address');
@@ -7,17 +5,19 @@ let userEmail = document.querySelector('.email');
 let userStrideLength = document.querySelector('.stride-length');
 let userStepGoal = document.querySelector('.daily-step-goal');
 
-
-
-
 window.onload = function() {
-   const usersRepository = new UsersRepository(2);
-   const userInfo = usersRepository.getUserDataById(userData);
-   const user = new User(userInfo);
-   welcomeDisplay.innerText = `Welcome, ${user.returnUserFirstName()}`;
-   userFullName.innerText = `Full Name: ${user.name}`;
-   userAddress.innerText = `Address: ${user.adress}`;
-   userEmail.innerText = `Email: ${user.email}`;
-   userStrideLength.innerText = `Stride Length: ${user.strideLength}`;
-   userStepGoal.innerText = `Daily Step Goal: ${user.dailyStepGoal}`;
- }
+  const usersRepository = new UsersRepository(getRandomNumber());
+  const userInfo = usersRepository.getUserDataById(userData);
+  const user = new User(userInfo);
+  welcomeDisplay.innerText = `Welcome, ${user.returnUserFirstName()}`;
+  userFullName.innerText = `Full Name: ${user.name}`;
+  userAddress.innerText = `Address: ${user.adress}`;
+  userEmail.innerText = `Email: ${user.email}`;
+  userStrideLength.innerText = `Stride Length: ${user.strideLength}`;
+  userStepGoal.innerText = `Daily Step Goal: ${user.dailyStepGoal}`;
+}
+
+function getRandomNumber() {
+ randomNumber = Math.floor(Math.random() * (50 - 1) + 1);
+ return randomNumber;
+}

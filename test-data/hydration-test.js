5a4cd4608754b7242a89ac11f5d4752e4c09983e
Chris Postma <cjrpostma@gmail.com>
2020-02-04 17:08:56 -0700

    Add incomplete UserRepository methods

--- test-data/hydration-test.js
+++ test-data/hydration-test.js
@@ -1,70 +1,72 @@
-const hydrationData = [{
-    "userID": 1,
-    "date": "2019/06/15",
-    "numOunces": 37
+const hydrationDataTest = [
+  {
+    userID: 1,
+    date: "2019/06/15",
+    numOunces: 37
   },
   {
-    "userID": 1,
-    "date": "2019/06/16",
-    "numOunces": 75
+    userID: 1,
+    date: "2019/06/16",
+    numOunces: 75
   },
   {
-    "userID": 1,
-    "date": "2019/06/17",
-    "numOunces": 47
+    userID: 1,
+    date: "2019/06/17",
+    numOunces: 47
   },
   {
-    "userID": 1,
-    "date": "2019/06/18",
-    "numOunces": 85
+    userID: 1,
+    date: "2019/06/18",
+    numOunces: 85
   },
   {
-    "userID": 1,
-    "date": "2019/06/19",
-    "numOunces": 42
+    userID: 1,
+    date: "2019/06/19",
+    numOunces: 42
   },
   {
-    "userID": 1,
-    "date": "2019/06/20",
-    "numOunces": 87
+    userID: 1,
+    date: "2019/06/20",
+    numOunces: 87
   },
   {
-    "userID": 1,
-    "date": "2019/06/21",
-    "numOunces": 94
+    userID: 1,
+    date: "2019/06/21",
+    numOunces: 94
   },
   {
-    "userID": 4,
-    "date": "2019/06/15",
-    "numOunces": 84
+    userID: 4,
+    date: "2019/06/15",
+    numOunces: 84
   },
   {
-    "userID": 4,
-    "date": "2019/06/16",
-    "numOunces": 39
+    userID: 4,
+    date: "2019/06/16",
+    numOunces: 39
   },
   {
-    "userID": 4,
-    "date": "2019/06/17",
-    "numOunces": 75
+    userID: 4,
+    date: "2019/06/17",
+    numOunces: 75
   },
   {
-    "userID": 4,
-    "date": "2019/06/18",
-    "numOunces": 51
+    userID: 4,
+    date: "2019/06/18",
+    numOunces: 51
   },
   {
-    "userID": 4,
-    "date": "2019/06/19",
-    "numOunces": 100
+    userID: 4,
+    date: "2019/06/19",
+    numOunces: 100
   },
   {
-    "userID": 4,
-    "date": "2019/06/20",
-    "numOunces": 30
+    userID: 4,
+    date: "2019/06/20",
+    numOunces: 30
   },
   {
-    "userID": 4,
-    "date": "2019/06/21",
-    "numOunces": 98
-  }];
+    userID: 4,
+    date: "2019/06/21",
+    numOunces: 98
+  }
+];
--- test-data/users-test.js
+++ test-data/users-test.js
@@ -1,57 +1,40 @@
-const userData = [
+const userDataTest = [
   {
-    "id": 1,
-    "name": "Luisa Hane",
-    "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
-    "email": "Diana.Hayes1@hotmail.com",
-    "strideLength": 4.3,
-    "dailyStepGoal": 10000,
-    "friends": [
-      16,
-      4,
-      8
-    ]
+    id: 1,
+    name: "Luisa Hane",
+    address: "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+    email: "Diana.Hayes1@hotmail.com",
+    strideLength: 4.3,
+    dailyStepGoal: 10000,
+    friends: [2, 3, 4]
   },
   {
-    "id": 4,
-    "name": "Mae Connelly",
-    "address": "28926 Schinner Islands, Turnermouth NE 23720-3230",
-    "email": "Marcos_Pollich@hotmail.com",
-    "strideLength": 3.1,
-    "dailyStepGoal": 4000,
-    "friends": [
-      48,
-      7,
-      44,
-      8
-    ]
+    id: 2,
+    name: "Mae Connelly",
+    address: "28926 Schinner Islands, Turnermouth NE 23720-3230",
+    email: "Marcos_Pollich@hotmail.com",
+    strideLength: 3.1,
+    dailyStepGoal: 4000,
+    friends: [48, 7, 44, 8]
   },
   {
-    "id": 8,
-    "name": "Laney Abshire",
-    "address": "86416 Koch Inlet, North Kaciefurt MA 80635",
-    "email": "Janice_Nitzsche2@yahoo.com",
-    "strideLength": 2.8,
-    "dailyStepGoal": 2000,
-    "friends": [
-      11,
-      41,
-      23,
-      49
-    ]
+    id: 3,
+    name: "Laney Abshire",
+    address: "86416 Koch Inlet, North Kaciefurt MA 80635",
+    email: "Janice_Nitzsche2@yahoo.com",
+    strideLength: 2.8,
+    dailyStepGoal: 2000,
+    friends: [11, 41, 23, 49]
   },
   {
-    "id": 16,
-    "name": "Garnett Cruickshank",
-    "address": "992 Zita Mall, North Tremainemouth MA 19312-3532",
-    "email": "Laverna47@hotmail.com",
-    "strideLength": 3.9,
-    "dailyStepGoal": 10000,
-    "friends": [
-      25,
-      31,
-      3,
-      16
-    ]
+    id: 4,
+    name: "Garnett Cruickshank",
+    address: "992 Zita Mall, North Tremainemouth MA 19312-3532",
+    email: "Laverna47@hotmail.com",
+    strideLength: 3.9,
+    dailyStepGoal: 10000,
+    friends: [25, 31, 3, 16]
   }
 ];
+
+module.exports = userDataTest;

0e6d0753655a17694bf79569b91b354254110653
Karen Nguyen <knguyen4184@yahoo.com>
2020-02-05 14:59:10 -0700

    Write method to calculate average fluid consumed

--- src/Hydration.js
+++ src/Hydration.js
@@ -2,6 +2,16 @@ class Hydration {
   constructor(hydrationData) {
     this.hydrationData = hydrationData;
   }
+
+  calculateAverageFluidsConsumed(userID) {
+   let averageFluids = this.hydrationData.filter(data => data.userID === userID)
+   let sum =  averageFluids.reduce((acc, fluids) => {
+      return acc += fluids.numOunces;
+    }, 0) 
+    return sum / averageFluids.length
+  }
+
+  
 }
 
 if (typeof module !== 'undefined') {

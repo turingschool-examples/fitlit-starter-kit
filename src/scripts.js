fc55d7b5533497377d6ba61024fe4c74f41cade7
Olivia Webster <oliviacweb@gmail.com>
2020-02-05 16:45:41 -0700

    Add average step goal for all users to user card for comparison

--- src/scripts.js
+++ src/scripts.js
@@ -4,6 +4,7 @@ let userAddress = document.querySelector('.address');
 let userEmail = document.querySelector('.email');
 let userStrideLength = document.querySelector('.stride-length');
 let userStepGoal = document.querySelector('.daily-step-goal');
+let allUsersAvgStepGoal = document.querySelector('.all-users-step-goal-average');
 
 window.onload = function() {
   const usersRepository = new UsersRepository(getRandomNumber());
@@ -11,10 +12,11 @@ window.onload = function() {
   const user = new User(userInfo);
   welcomeDisplay.innerText = `Welcome, ${user.returnUserFirstName()}`;
   userFullName.innerText = `Full Name: ${user.name}`;
-  userAddress.innerText = `Address: ${user.adress}`;
+  userAddress.innerText = `Address: ${user.address}`;
   userEmail.innerText = `Email: ${user.email}`;
   userStrideLength.innerText = `Stride Length: ${user.strideLength}`;
-  userStepGoal.innerText = `Daily Step Goal: ${user.dailyStepGoal}`;
+  userStepGoal.innerText = `Your Daily Step Goal: ${user.dailyStepGoal}`;
+  allUsersAvgStepGoal.innerText = `Average User's Step Goal: ${usersRepository.calculateAverageStepGoal(userData)}`
 }
 
 function getRandomNumber() {

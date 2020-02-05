9ad7d00142722484d39b7f0786155b93520f2672
Trond Makonese <trondation@gmail.com>
2020-02-05 11:20:30 -0700

    Initial set up.

---
+++ src/UserRepo.js
@@ -0,0 +1,20 @@
+class UserRepo {
+  constructor(data) {
+    this.data = data;
+  }
+
+  returnUserInfoById(userId) {
+    return this.data.find(user => user.id === userId)
+  }
+
+  averageStepsAllUsers() {
+    return this.data.reduce((acc, users) => {
+    (acc += users.dailyStepGoal)
+    return Math.round(acc / this.data.length);
+    }, 0)
+  }
+}
+
+if(typeof module !== 'undefined') {
+  module.exports = UserRepo;
+};

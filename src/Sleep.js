026e5cdf4ddc9ecf82c96370d752bb75dfab05a2
Erik Raith <raithhtiar@gmail.com>
2020-02-05 15:37:08 -0700

    [test] Add testing for getDailSleep and getDailySleepQuality

--- src/Sleep.js
+++ src/Sleep.js
@@ -2,6 +2,21 @@ class Sleep {
   constructor(sleepData) {
     this.sleepData = sleepData;
   }
+  calcAvgSleepHrTotalDays(userID) {
+    let filteredSleepItems = this.sleepData.filter(userEntry => userEntry.userID === userID);
+    let sum = filteredSleepItems.reduce(function(total, curVal) {
+      return total + curVal.hoursSlept;
+    }, 0);
+    return sum / filteredSleepItems.length;
+  }
+  calcAvgSleepQualityTotalDays(userID) {
+    let filteredSleepQuality = this.sleepData.filter(userEntry => userEntry.userID === userID);
+    let sum = filteredSleepQuality.reduce(function(total, curVal) {
+      return total + curVal.sleepQuality;
+    }, 0);
+    return sum / filteredSleepQuality.length;
+  }
+  
 }
 
 if (typeof module !== 'undefined') {
--- test/sleep-test.js
+++ test/sleep-test.js
@@ -6,53 +6,122 @@ const Sleep = require('../src/Sleep');
 
 describe('Sleep', function() {
 
-  let sleepData,sleep;
+      let sleepData, sleep;
 
-  beforeEach(() => {
-    sleepData = [
-      {
-        "userID": 1,
-        "date": "2019/06/14",
-        "hoursSlept": 3,
-        "sleepQuality": 2.6
-      },
-      {
-        "userID": 1,
-        "date": "2019/06/15",
-        "hoursSlept": 3,
-        "sleepQuality": 4.7
-      },
-      {
-        "userID": 1,
-        "date": "2019/06/16",
-        "hoursSlept": 3,
-        "sleepQuality": 4.7
-      },
-      {
-        "userID": 4,
-        "date": "2019/06/15",
-        "hoursSlept": 5.4,
-        "sleepQuality": 3
-      }
-    ];
-    sleep = new Sleep(sleepData);
-  });
+      beforeEach(() => {
+        sleepData = [{
+            "userID": 1,
+            "date": "2019/06/14",
+            "hoursSlept": 3,
+            "sleepQuality": 2.6
+          },
+          {
+            "userID": 1,
+            "date": "2019/06/15",
+            "hoursSlept": 3,
+            "sleepQuality": 4.7
+          },
+          {
+            "userID": 1,
+            "date": "2019/06/16",
+            "hoursSlept": 3,
+            "sleepQuality": 4.7
+          },
+          {
+            "userID": 4,
+            "date": "2019/06/15",
+            "hoursSlept": 5.4,
+            "sleepQuality": 3
+          }
+        ];
+        sleep = new Sleep(sleepData);
+      });
 
-  it('should be a function', function() {
-    expect(Sleep).to.be.a('function');
-  });
+      it('should be a function', function() {
+        expect(Sleep).to.be.a('function');
+      });
 
-  it('should be an instance of sleep', function() {
-    expect(sleep).to.be.an.instanceof(Sleep);
-  });
+      it('should be an instance of sleep', function() {
+        expect(sleep).to.be.an.instanceof(Sleep);
+      });
 
-  it('should calculate average num of hours slepts per day',function(){
-    expect(sleep.calcAvgSleepHrTotalDays(1)).to.equal(3);
-  });
+      it('should calculate average num of hours slepts per day', function() {
+        expect(sleep.calcAvgSleepHrTotalDays(1)).to.equal(3);
+      });
 
-  it('should calculate the average sleep quality per day',function(){
-    expect(sleep.calcAvgSleepQualityTotalDays(1)).to.equal(4);
-  })
+      it('should calculate the average sleep quality per day', function() {
+        expect(sleep.calcAvgSleepQualityTotalDays(1)).to.equal(4);
+      });
 
+      it('should return hours of sleep of day', function() {
+        expect(sleep.getDailySleep(1, "2019/06/15")).to.equal(3);
+      });
 
-});
+      it('should return qualty of sleep of day', function() {
+        expect(sleep.getDailySleepQuality(1, "2019/06/15")).to.equal(4.7);
+      });
+
+      describe('Sleep 7 Days', function() {
+
+        let sleepData, sleep;
+
+        beforeEach(() => {
+          sleepData = [{
+              "userID": 1,
+              "date": "2019/06/11",
+              "hoursSlept": 3,
+              "sleepQuality": 2.6
+            },
+            {
+              "userID": 1,
+              "date": "2019/06/12",
+              "hoursSlept": 3,
+              "sleepQuality": 4.7
+            },
+            {
+              "userID": 1,
+              "date": "2019/06/13",
+              "hoursSlept": 3,
+              "sleepQuality": 4.7
+            },
+            {
+              "userID": 1,
+              "date": "2019/06/14",
+              "hoursSlept": 3,
+              "sleepQuality": 4.7
+            },
+            {
+              "userID": 1,
+              "date": "2019/06/15",
+              "hoursSlept": 3,
+              "sleepQuality": 4.7
+            },
+            {
+              "userID": 1,
+              "date": "2019/06/16",
+              "hoursSlept": 3,
+              "sleepQuality": 4.7
+            },
+            {
+              "userID": 1,
+              "date": "2019/06/17",
+              "hoursSlept": 3,
+              "sleepQuality": 4.7
+            },
+            {
+              "userID": 4,
+              "date": "2019/06/15",
+              "hoursSlept": 5.4,
+              "sleepQuality": 3
+            }
+          ];
+          sleep = new Sleep(sleepData);
+        });
+        it('should be able to return an array of the previous 7 days sleep', function() {
+
+        });
+
+        it('should be able to return an array of the previous 7 days sleepQuality', function() {
+
+        });
+      });

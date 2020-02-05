bd1c317e6ed5025c7904b7e6aff6fae752b1ff7a
Dan Reardon <dreardon1021@gmail.com>
2020-02-05 16:50:07 -0700

    Add friends names to DOM with user  function

--- src/User.js
+++ src/User.js
@@ -6,13 +6,26 @@ class User {
     this.email = userData.email
     this.strideLength = userData.strideLength;
     this.dailyStepGoal = userData.dailyStepGoal;
-    this.friends = userData.friends;
+    this.friends = userData.friends
+    this.friendsNames = []
   }
 
   getFirstName() {
     return this.name.split(' ')[0];
   }
+
+  getUserFriendNames() {
+    let userIds = userData.filter(user => {
+      return user.id
+    })
+    userIds.forEach((i) => {
+      if(this.friends.includes(i.id)) {
+        this.friendsNames.push(i.name)
+      }
+    })
+  }
 }
+
 
 if (typeof module !== 'undefined') {
   module.exports = User;
--- src/scripts.js
+++ src/scripts.js
@@ -4,8 +4,10 @@ const userName = document.querySelector('.user-name');
 const userAddress = document.querySelector('.user-address');
 const userEmail = document.querySelector('.user-email');
 const userStride = document.querySelector('.user-stride-length');
-const userStepGoal = document.querySelector('.user-step-goal')
+const userStepGoal = document.querySelector('.user-step-goal');
 const userGoalAverage = document.querySelector('.step-goal-average');
+const friendsList = document.querySelector('.friends-list');
+const stepComparison = document.querySelector('.step-goal-comparison')
 
 function populateUserInfo(id) {
   let user = new User(userRepo.getUserData(id))
@@ -15,6 +17,10 @@ function populateUserInfo(id) {
   userStride.innerHTML = user.strideLength;
   userStepGoal.innerHTML = user.dailyStepGoal;
   userGoalAverage.innerHTML = userRepo.getStepGoalAverage();
+  stepComparison.innerHTML = `Your goal is ${Math.floor((user.dailyStepGoal / userRepo.getStepGoalAverage()) * 100 - 100)}%
+    under/over the average step goal of all users`
+  user.getUserFriendNames()
+  friendsList.innerHTML = user.friendsNames;
 }
 
-populateUserInfo(42)
+populateUserInfo(41);

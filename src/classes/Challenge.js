4584d0ac726405cbf56e047ca50e63b64c6eafd3
Ryan Bahan <ryan@ryanbahan.com>
2020-02-04 20:04:33 -0700

    Add inital functionality for challenge class

--- src/classes/Challenge.js
+++ src/classes/Challenge.js
@@ -1,3 +1,22 @@
+class Challenge {
+  constructor() {
+    this.trends = null,
+    this.leaderboard = null,
+    this.challengeGoal = 5000,
+    this.users = [];
+  }
+  getUsers(currentUser) {
+    this.users.push(...currentUser.friends);
+    this.users.unshift(currentUser)
+  }
+  getTrends() {
+
+  };
+  getLeaderboard() {
+
+  };
+};
+
 if(typeof module !== 'undefined'){
   module.exports = Challenge;
 }

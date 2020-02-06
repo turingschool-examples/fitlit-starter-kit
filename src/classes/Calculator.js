cfe40c17bf1738ba34d294c29e86d3f6b12f752a
Chris Postma <cjrpostma@gmail.com>
2020-02-05 20:55:11 -0700

    Add getUserWeekTotal method to class Calculator

--- src/classes/Calculator.js
+++ src/classes/Calculator.js
@@ -4,18 +4,74 @@ class Calculator {
   }
 
   getUserDayTotal(category, date, metric) {
-    return category.find(day => day.date === date)[metric]
-  }
-
-  getUserWeekTotal(category, date, metric) {
-
+    return category.find(day => day.date === date)[metric];
   }
 
   getUserAllTimeAvg(category, metric) {
     return category.reduce((average, items) => {
       average += items[metric] / category.length;
       return Math.round(average * 100) / 100;
-    }, 0)
+    }, 0);
+  }
+
+  getUserWeekTotal(category, date, metric) {
+    let beginWeekDay = this.modifyDays(this.stringToDate(date), -6);
+    const endWeekDay = this.stringToDate(date);
+    const week = [];
+    const metricData = [];
+
+    while (beginWeekDay <= endWeekDay) {
+      week.push(beginWeekDay);
+      beginWeekDay = this.modifyDays(beginWeekDay, 1);
+    }
+
+    week.forEach(day => {
+      const currentDay = this.formatDate(day);
+      metricData.push(this.getUserDayTotal(category, currentDay, metric));
+    });
+
+    return { dates: week, metrics: metricData };
+  }
+
+  stringToDate(string) {
+    // This makes three variables corresponding to the array returned by string.split('/') using array destructuring.
+    const [year, month, day] = string.split("/");
+    return new Date(year, month - 1, day);
+  }
+
+  formatDate(date) {
+    // this method converts a date object to a string for use in getUserWeekTotal() method. That method requires a string date.
+    const year = date.getFullYear();
+    let month = `${date.getMonth() + 1}`;
+    let day = `${date.getDate()}`;
+
+    if (month.length < 2) {
+      month = `0${month}`;
+    }
+    if (day.length < 2) {
+      day = `0${day}`;
+    }
+
+    return [year, month, day].join("/");
+  }
+
+  modifyDays(date, days) {
+    // this method creates a new date that is in the future or past based on number of days passed to its days parameter
+    const copy = new Date(Number(date));
+    copy.setDate(date.getDate() + days);
+    return copy;
+  }
+
+  getWeekDay(date) {
+    return [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ][date.getDay()];
   }
 }
 
--- test/Calculator-test.js
+++ test/Calculator-test.js
@@ -1,10 +1,9 @@
-const chai = require('chai');
+const chai = require("chai");
 const { expect } = chai;
-const Calculator = require('../src/classes/Calculator');
+const Calculator = require("../src/classes/Calculator");
 const userDataTest = require("../test-data/users-test");
 
 const state = {
-
   currentUser: {
     id: 1,
     name: "Luisa Hane",
@@ -16,20 +15,19 @@ const state = {
   },
 
   currentUserData: {
-
     hydrationData: [
-      { userID: 1, date: '2019/06/15', numOunces: 37 },
-      { userID: 1, date: '2019/06/16', numOunces: 75 },
-      { userID: 1, date: '2019/06/17', numOunces: 47 },
-      { userID: 1, date: '2019/06/18', numOunces: 85 },
-      { userID: 1, date: '2019/06/19', numOunces: 42 },
-      { userID: 1, date: '2019/06/20', numOunces: 87 },
-      { userID: 1, date: '2019/06/21', numOunces: 94 }
+      { userID: 1, date: "2019/06/15", numOunces: 37 },
+      { userID: 1, date: "2019/06/16", numOunces: 75 },
+      { userID: 1, date: "2019/06/17", numOunces: 47 },
+      { userID: 1, date: "2019/06/18", numOunces: 85 },
+      { userID: 1, date: "2019/06/19", numOunces: 42 },
+      { userID: 1, date: "2019/06/20", numOunces: 87 },
+      { userID: 1, date: "2019/06/21", numOunces: 94 }
     ],
     activityData: [
       {
         userID: 1,
-        date: '2019/06/15',
+        date: "2019/06/15",
 
         numSteps: 3577,
         minutesActive: 140,
@@ -37,42 +35,42 @@ const state = {
       },
       {
         userID: 1,
-        date: '2019/06/16',
+        date: "2019/06/16",
         numSteps: 4294,
         minutesActive: 138,
         flightsOfStairs: 10
       },
       {
         userID: 1,
-        date: '2019/06/17',
+        date: "2019/06/17",
         numSteps: 7402,
         minutesActive: 116,
         flightsOfStairs: 33
       },
       {
         userID: 1,
-        date: '2019/06/18',
+        date: "2019/06/18",
         numSteps: 3486,
         minutesActive: 114,
         flightsOfStairs: 32
       },
       {
         userID: 1,
-        date: '2019/06/19',
+        date: "2019/06/19",
         numSteps: 11374,
         minutesActive: 213,
         flightsOfStairs: 13
       },
       {
         userID: 1,
-        date: '2019/06/20',
+        date: "2019/06/20",
         numSteps: 14810,
         minutesActive: 287,
         flightsOfStairs: 18
       },
       {
         userID: 1,
-        date: '2019/06/21',
+        date: "2019/06/21",
         numSteps: 2634,
         minutesActive: 107,
         flightsOfStairs: 5
@@ -81,33 +79,33 @@ const state = {
     sleepData: [
       {
         userID: 1,
-        date: '2019/06/15',
+        date: "2019/06/15",
         hoursSlept: 6.1,
         sleepQuality: 2.2
       },
-      { userID: 1, date: '2019/06/16', hoursSlept: 5.4, sleepQuality: 3 },
+      { userID: 1, date: "2019/06/16", hoursSlept: 5.4, sleepQuality: 3 },
       {
         userID: 1,
-        date: '2019/06/17',
+        date: "2019/06/17",
         hoursSlept: 8.1,
         sleepQuality: 3.5
       },
       {
         userID: 1,
-        date: '2019/06/18',
+        date: "2019/06/18",
         hoursSlept: 6.1,
         sleepQuality: 3.5
       },
-      { userID: 1, date: '2019/06/19', hoursSlept: 4.7, sleepQuality: 4 },
+      { userID: 1, date: "2019/06/19", hoursSlept: 4.7, sleepQuality: 4 },
       {
         userID: 1,
-        date: '2019/06/20',
+        date: "2019/06/20",
         hoursSlept: 10.1,
         sleepQuality: 1.3
       },
       {
         userID: 1,
-        date: '2019/06/21',
+        date: "2019/06/21",
         hoursSlept: 7.9,
         sleepQuality: 1.6
       }
@@ -115,26 +113,45 @@ const state = {
   }
 };
 
-describe('Calculator', function() {
+describe("Calculator", function() {
   let calculator;
 
   beforeEach("instantiate new calculator", function() {
-     calculator = new Calculator(state.currentUser.id);
-  })
+    calculator = new Calculator(state.currentUser.id);
+  });
 
-  it('should be an instance of calculator', function() {
-    expect(calculator).to.be.an.instanceof(Calculator)
-  })
+  it("should be an instance of calculator", function() {
+    expect(calculator).to.be.an.instanceof(Calculator);
+  });
 
-  describe('Hydration', function() {
+  describe("Hydration", function() {
+    it("should get the user's total ounces for a given date", function() {
+      expect(
+        calculator.getUserDayTotal(
+          state.currentUserData.hydrationData,
+          "2019/06/17",
+          "numOunces"
+        )
+      ).to.equal(47);
+    });
 
-    it("should get the user's total ounces for a given date" , function() {
-      expect(calculator.getUserDayTotal(state.currentUserData.hydrationData, '2019/06/17', "numOunces")).to.equal(47);
-    })
+    it("should get the user's overall average hydration", function() {
+      expect(
+        calculator.getUserAllTimeAvg(
+          state.currentUserData.hydrationData,
+          "numOunces"
+        )
+      ).to.equal(66.71);
+    });
 
-    it("should get the user's overall average hydration" , function() {
-
-      expect(calculator.getUserAllTimeAvg(state.currentUserData.hydrationData, "numOunces")).to.equal(66.71);
-    })
-  })
-})
+    it("should return a metric for each day over the past seven days inclusive", function() {
+      expect(
+        calculator.getUserWeekTotal(
+          state.currentUserData.hydrationData,
+          "2019/06/21",
+          "numOunces"
+        )
+      );
+    });
+  });
+});

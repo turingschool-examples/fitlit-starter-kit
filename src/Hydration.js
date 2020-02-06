97c7e4741234a31a3732276091d6dab8755d775a
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-02-05 18:10:10 -0700

    Add calculateAverageFluidIntakeForUser function for Hydration

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,11 +1,24 @@
 class Hydration {
-  constructor() {
-    this.userID;
+  constructor(id) {
+    this.userID = id;
     this.date;
     this.numOunces;
   }
 
-  calculateAverageFluidIntakeForUser() {
+  calculateAverageFluidIntakeForUser(hydrationDatas) {
+    let userOunceIntakes= [];
+    let test = hydrationDatas.filter(hydrationData => {
+      if(hydrationData.userID === this.userID) {
+        userOunceIntakes.push(hydrationData.numOunces)
+      }
+    })
+    let totalOunces = userOunceIntakes.reduce((acc, ounce) => {
+        acc += ounce;
+        return acc;
+    }, 0)
+    let ounceAverage = Math.trunc(totalOunces / userOunceIntakes.length);
+
+    return ounceAverage;
   }
 
   calculateFluidIntakeForDay() {

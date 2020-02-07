90a094e69a4f045f98b1cf8d59a289c1948fd4ec
Karen Nguyen <knguyen4184@yahoo.com>
2020-02-07 15:26:45 -0700

    Write method to get minutes active from prev 7 days

--- src/Activity.js
+++ src/Activity.js
@@ -13,6 +13,22 @@ class Activity {
     let currentUser = this.activityData.filter(data => data.userID === userID);
     return currentUser.find(minute => minute.date === date).minutesActive;
   }
+
+  getPrevDaysActive(userID, startDate) {
+    let startDateParsed = new Date(startDate);
+    let endDateParsed = new Date(startDate);
+    endDateParsed.setDate(startDateParsed.getDate() - 7);
+    let userActiveData = this.activityData.filter(userEntry => userEntry.userID === userID);
+    let userActiveDaysData = userActiveData.filter(function(activeDayData) {
+      let day = new Date(activeDayData.date);
+      if (day <= startDateParsed && day >= endDateParsed) {
+        return true;
+      }
+    });
+    return userActiveDaysData.map(data => data.minutesActive);
+  }
+
+
 }
 
 if (typeof module !== 'undefined') {

b1111ac4272966cf9a4f1e96461b345e8865a6d3
Trond Makonese <trondation@gmail.com>
2020-02-07 07:59:08 -0700

    create sleep repo test.

---
+++ test/SleepRepo-test.js
@@ -0,0 +1,23 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const SleepRepo = require('../src/sleepRepo');
+const allSleepInfo = require('../data/sleep-test-data');
+
+let sleepRepo;
+
+
+describe('User Repo', () => {
+
+  beforeEach(() => {
+    sleepRepo = new SleepRepo(allSleepInfo);
+  })
+
+  it('should be a function', () => {
+    expect(SleepRepo).to.be.a('function');
+  })
+
+  it('should be an instance of the user repo', () => {
+    expect(sleepRepo).to.be.an.instanceof(SleepRepo);
+  })
+});

204394db068c2edb43a6b4dd69cb1042220563fe
Dan Reardon <dreardon1021@gmail.com>
2020-02-07 13:47:34 -0700

    Add findGoodSleepers function

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,52 +1,73 @@
 class Sleep {
   constructor(sleepData) {
-    this.sleepData = sleepData
+    this.sleepData = sleepData;
   }
   getUserData(id) {
     return this.sleepData.filter(obj => {
-      return obj.userID === id
+      return obj.userID === id;
     })
   }
   calculateAverageHours(id) {
     return this.getUserData(id).reduce((acc, object) => {
-      return Math.round(acc += object.hoursSlept / this.getUserData(id).length)
+      return Math.round(acc += object.hoursSlept / this.getUserData(id).length);
     }, 0);
   }
   calculateAverageQuality(id) {
     return this.getUserData(id).reduce((acc, object) => {
-      return Math.round(acc += object.sleepQuality / this.getUserData(id).length)
+      return Math.round(acc += object.sleepQuality / this.getUserData(id).length);
     }, 0);
   }
   getDayHours(id, date) {
-    return this.getUserData(id).find(day => day.date === date).hoursSlept
+    return this.getUserData(id).find(day => day.date === date).hoursSlept;
   }
   getDayQuality(id, date) {
-    return this.getUserData(id).find(day => day.date === date).sleepQuality
+    return this.getUserData(id).find(day => day.date === date).sleepQuality;
   }
   getHoursByWeek(id, date) {
     let endDate = this.getUserData(id).findIndex(object => object.date === date);
-    let filteredData = this.getUserData(id).map(day => day.hoursSlept)
+    let filteredData = this.getUserData(id).map(day => day.hoursSlept);
     return endDate - 6 >= 0 ?
-      filteredData.slice(endDate - 6, endDate + 1):
+      filteredData.slice(endDate - 6, endDate + 1) :
       filteredData.slice(0, endDate + 1);
   }
   getQualityByWeek(id, date) {
     let endDate = this.getUserData(id).findIndex(object => object.date === date);
-    let filteredData = this.getUserData(id).map(day => day.sleepQuality)
+    let filteredData = this.getUserData(id).map(day => day.sleepQuality);
     return endDate - 6 >= 0 ?
       filteredData.slice(endDate - 6, endDate + 1) :
       filteredData.slice(0, endDate + 1);
   }
   getAverageQualityOfUsers() {
     const average = this.sleepData.reduce((acc, object) => {
-      return acc += object.sleepQuality / this.sleepData.length
+      return acc += object.sleepQuality / this.sleepData.length;
     }, 0 )
     return average.toFixed(2)
   }
-  findGoodSleepers() {
-
+  findGoodSleepers(date) {
+    let goodSleepers = []
+    let ids = []
+    this.sleepData.forEach(obj => {
+      if (!ids.includes(obj.userID)) {
+        ids.push(obj.userID);
+      }
+    })
+    ids.forEach(id => {
+      let endDate = this.getUserData(id).findIndex(object => object.date === date);
+      let filteredData = this.getUserData(id).map(day => day.sleepQuality);
+      let weeklyNumbers = endDate - 6 >= 0 ?
+        filteredData.slice(endDate - 6, endDate + 1) :
+        filteredData.slice(0, endDate + 1);
+      let average = weeklyNumbers.reduce((acc, num) => {
+        return (acc += num / weeklyNumbers.length)
+      }, 0)
+      if(average >= 3) {
+        goodSleepers.push(id)
+      }
+    })
+    return goodSleepers
   }
 }
+
 
 
 
--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -25,38 +25,40 @@ describe('Sleep', function() {
   })
 
   it('can calculate average hours slept per day', function() {
-    expect(sleep.calculateAverageHours(111)).to.equal(8)
+    expect(sleep.calculateAverageHours(111)).to.equal(8);
   })
 
   it('can calculate average sleep quality', function() {
-    expect(sleep.calculateAverageQuality(1)).to.equal(4)
+    expect(sleep.calculateAverageQuality(1)).to.equal(4);
   })
 
   it('can find hours on exact date', function() {
-    expect(sleep.getDayHours(1, '2020/02/03')).to.equal(9.1)
+    expect(sleep.getDayHours(1, '2020/02/03')).to.equal(9.1);
   })
 
   it('should find quality on exact date', function() {
-    expect(sleep.getDayQuality(1, '2020/02/03')).to.equal(2.7)
+    expect(sleep.getDayQuality(1, '2020/02/03')).to.equal(2.7);
   })
 
   it('should be able to find sleep hr values by week', function() {
-    let weekData = sleepData.slice(0, 7)
-    let filteredData = weekData.map(day => day.hoursSlept)
-    expect(sleep.getHoursByWeek(1, '2020/02/08')).to.deep.equal(filteredData)
+    let weekData = sleepData.slice(0, 7);
+    let filteredData = weekData.map(day => day.hoursSlept);
+    expect(sleep.getHoursByWeek(1, '2020/02/08')).to.deep.equal(filteredData);
   })
 
   it('should be able to find quality values by week', function() {
-    let weekData = sleepData.slice(0, 7)
-    let filteredData = weekData.map(day => day.sleepQuality)
-    expect(sleep.getQualityByWeek(1, '2020/02/08')).to.deep.equal(filteredData)
+    let weekData = sleepData.slice(0, 7);
+    let filteredData = weekData.map(day => day.sleepQuality);
+    expect(sleep.getQualityByWeek(1, '2020/02/08')).to.deep.equal(filteredData);
   })
 
   it('should be able to find average quality for all users', function() {
-    expect(sleep.getAverageQualityOfUsers()).to.equal('2.71')
+    expect(sleep.getAverageQualityOfUsers()).to.equal('2.71');
   })
 
   it('should be able to find users with high quality sleep', function() {
-    
+    let weekData = sleepData.slice(0, 7);
+    let filteredData = weekData.map(day => day.sleepQuality);
+    expect(sleep.findGoodSleepers('2020/02/08')).to.deep.equal([1]);
   })
 })

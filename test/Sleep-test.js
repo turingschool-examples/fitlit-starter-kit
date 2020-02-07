14ccaf8a6a7f9919b76f4a21ced7967c73a3f200
Dan Reardon <dreardon1021@gmail.com>
2020-02-07 12:46:23 -0700

    Add weekly sleep functions

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -9,7 +9,6 @@ describe('Sleep', function() {
   let sleep;
   beforeEach(function() {
     sleep = new Sleep(sleepData);
-    sleep.getUserData(1)
   });
 
   it('should be a function', function() {
@@ -33,7 +32,31 @@ describe('Sleep', function() {
     expect(sleep.calculateAverageQuality(1)).to.equal(4)
   })
 
-  it('can find data on exact date', function() {
-    expect(sleep.getDayHours(1, "2020/02/03")).to.equal(9.1)
+  it('can find hours on exact date', function() {
+    expect(sleep.getDayHours(1, '2020/02/03')).to.equal(9.1)
+  })
+
+  it('should find quality on exact date', function() {
+    expect(sleep.getDayQuality(1, '2020/02/03')).to.equal(2.7)
+  })
+
+  it('should be able to find sleep hr values by week', function() {
+    let weekData = sleepData.slice(0, 7)
+    let filteredData = weekData.map(day => day.hoursSlept)
+    expect(sleep.getHoursByWeek(1, '2020/02/08')).to.deep.equal(filteredData)
+  })
+
+  it('should be able to find quality values by week', function() {
+    let weekData = sleepData.slice(0, 7)
+    let filteredData = weekData.map(day => day.sleepQuality)
+    expect(sleep.getQualityByWeek(1, '2020/02/08')).to.deep.equal(filteredData)
+  })
+
+  it('should be able to find average quality for all users', function() {
+    expect(sleep.getAverageQualityOfUsers()).to.equal('2.71')
+  })
+
+  it('should be able to find users with high quality sleep', function() {
+    
   })
 })

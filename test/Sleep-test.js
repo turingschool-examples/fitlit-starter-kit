be35704d23e27e0b0a1d1ee7574e2c96667c15ec
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-02-08 14:27:47 -0700

    Change expected value for calculateAverageHours test

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -20,7 +20,7 @@ describe('Sleep', function() {
   });
 
   it('Can calculate average hours slept per day', function() {
-    expect(sleep.calculateAverageHours(111)).to.equal(8);
+    expect(sleep.calculateAverageHours(111)).to.equal(7.1);
   })
 
   it('Can calculate average sleep quality', function() {

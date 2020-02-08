81737238378dc2962a764d36d27dd7885b6b85a5
Dan Reardon <dreardon1021@gmail.com>
2020-02-08 12:12:17 -0700

    Connect hydration and script

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,5 @@
 let userRepo = new UserRepository(userData);
+let user = new User(userRepo.getUserData(42));
 
 const userName = document.querySelector('.user-name');
 const userAddress = document.querySelector('.user-address');
@@ -8,9 +9,9 @@ const userStepGoal = document.querySelector('.user-step-goal');
 const userGoalAverage = document.querySelector('.step-goal-average');
 const friendsList = document.querySelector('.friends-list');
 const stepComparison = document.querySelector('.step-goal-comparison')
+const dayHydrationBox = document.querySelector('.hydration-day')
 
-function populateUserInfo(id) {
-  let user = new User(userRepo.getUserData(id))
+function populateUserInfo() {
   userName.innerText = user.name;
   userAddress.innerText = user.address;
   userEmail.innerText = user.email;
@@ -20,8 +21,13 @@ function populateUserInfo(id) {
   stepComparison.innerText  = (user.dailyStepGoal > userRepo.getStepGoalAverage()) ? 
   `You're step goal is ${user.dailyStepGoal - userRepo.getStepGoalAverage()} steps above the average` :
     `You're step goal is ${userRepo.getStepGoalAverage() - user.dailyStepGoal} steps below the average`
-  friendsList.innerHTML = friendsList.innerHTML = user.friends.map
-  (friendID => userRepo.getUserData(friendID).name)
+  friendsList.innerHTML = friendsList.innerHTML = user.friends.map(friendID => userRepo.getUserData(friendID).name)
 }
 
-populateUserInfo(41);
+function populateHydrationInfo(hydrationData, id) {
+  let hydration = new hydration(hydrationData)
+
+}
+
+populateUserInfo();
+populateHydrationInfo(hydrationData, user.id)

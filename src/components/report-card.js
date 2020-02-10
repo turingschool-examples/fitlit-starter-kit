58a917b3ba90aa15b687ab92e1ff7853d22b028c
“Greg_Grimes” <cggrimes@yahoo.com>
2020-02-09 19:15:12 -0700

    Add letter grades next to values

--- src/components/report-card.js
+++ src/components/report-card.js
@@ -1,11 +1,18 @@
 const reportCard = {
   generateHtmlString(state) {
+    const aScore = {grade: "A", score: 5};
+    const bScore = {grade: "B", score: 4};
+    const cScore = {grade: "C", score: 3};
+    const dScore = {grade: "D", score: 2};
+    const fScore = {grade: "F", score: 1};
+
     const calculator = new Calculator(state.currentUser.id);
     const userDailySteps = calculator.getUserDayTotal(
       state.currentUserData.activityData,
       state.currentDay,
       "numSteps"
     );
+
     const userActiveTime = calculator.getUserDayTotal(
       state.currentUserData.activityData,
       state.currentDay,
@@ -36,119 +43,119 @@ const reportCard = {
       "numOunces"
     );
 
-    const dailyGrades = this.getGrades();
+    const dailyGrades = getGrades();
 
     return `<h2>Report Card: </h2>
             <h2>Your Daily Average</h2>
             <section>
-              <div class="steps"><i class="fas fa-shoe-prints"></i>&nbsp;&nbsp;${userDailySteps}</div>
-              <div class="flights"><i class="far fa-building"></i>&nbsp;&nbsp;${userFloorsClimbed}</div>
-              <div class="minutes-active"><i class="fas fa-walking"></i>&nbsp;&nbsp;${userActiveTime}</div>
-              <div class="sleep-quality"><i class="far fa-thumbs-up"></i>&nbsp;&nbsp;${userSleepQuality}</div>
-              <div class="sleep-time"><i class="fas fa-bed"></i>&nbsp;&nbsp;${userSleep}</div>
-              <div class="ounces-water"><i class="fas fa-mug-hot"></i>&nbsp;&nbsp;${userHydration}</div>
+              <div class="steps"><i class="fas fa-shoe-prints"></i>&nbsp;&nbsp;${userDailySteps}&nbsp;&nbsp;${dailyGrades.stepsGrade.grade}</div>
+              <div class="flights"><i class="far fa-building"></i>&nbsp;&nbsp;${userFloorsClimbed}&nbsp;&nbsp;${dailyGrades.flightsGrade.grade}</div>
+              <div class="minutes-active"><i class="fas fa-walking"></i>&nbsp;&nbsp;${userActiveTime}&nbsp;&nbsp;${dailyGrades.minutesActiveGrade.grade}</div>
+              <div class="sleep-quality"><i class="far fa-thumbs-up"></i>&nbsp;&nbsp;${userSleepQuality}&nbsp;&nbsp;${dailyGrades.sleepQualGrade.grade}</div>
+              <div class="sleep-time"><i class="fas fa-bed"></i>&nbsp;&nbsp;${userSleep}&nbsp;&nbsp;${dailyGrades.sleepLengthGrade.grade}</div>
+              <div class="ounces-water"><i class="fas fa-mug-hot"></i>&nbsp;&nbsp;${userHydration}&nbsp;&nbsp;${dailyGrades.hydrationGrade.grade}</div>
             </section>`;
 
-  },
+    function getGrades() {
+      let grades = {
+        stepsGrade: calculateStepsGrade(),
+        flightsGrade: calculateFlightsGrade(),
+        minutesActiveGrade: calculateMinutesActiveGrade(),
+        sleepQualGrade: calculateSleepQualGrade(),
+        sleepLengthGrade: calculateSleepLengthGrade(),
+        hydrationGrade: calculateHydrationGrade()
+      };
+      return grades;
+    };
 
-  getGrades: function() {
-    let grades = [];
-    grades.push(this.calculateStepsGrade(),
-                this.calculateFlightsGrade(),
-                this.calculateMinutesActiveGrade(),
-                this.calculateSleepQualGrade(),
-                this.calculateSleepLengthGrade(),
-                this.calculateHydrationGrade());
-    return grades;
-  },
+    function calculateStepsGrade() {
+      let dS = userDailySteps;
+      if(dS >= 12000){
+        return aScore;
+      } else if(9000 <= dS && dS < 12000) {
+        return bScore;
+      } else if(6500 <= dS && dS < 9000) {
+        return cScore;
+      } else if(4000 <= dS && dS < 6500) {
+        return dScore;
+      } else if(dS < 4000) {
+        return fScore;
+      }
+    };
 
-  calculateStepsGrade: function() {
-    let dS = userDailySteps;
-    if(dS >= 12000){
+    function calculateFlightsGrade() {
+      let f = userFloorsClimbed;
+      if(35 < f && f < 51){
+        return aScore;
+      } else if(20 < f && f < 36) {
+        return bScore;
+      } else if(7 < f && f < 21) {
+        return cScore;
+      } else if(1 < f && f < 8) {
+        return dScore;
+      } else if(f < 2) {
+        return fScore;
+      }
+    };
 
-    } else if(9000 <= dS < 12000) {
+    function calculateMinutesActiveGrade() {
+      let mA = userActiveTime;
+      if(mA > 200){
+        return aScore;
+      } else if(130 < mA && mA < 201) {
+        return bScore;
+      } else if(80 < mA && mA < 132) {
+        return cScore;
+      } else if(45 < mA && mA < 81) {
+        return dScore;
+      } else if(mA < 46) {
+        return fScore;
+      }
+    };
 
-    } else if(6500 <= dS < 9000) {
+    function calculateSleepQualGrade() {
+      let sQ = userSleepQuality;
+      if(sQ >= 4 ){
+        return aScore;
+      } else if(3 <= sQ && sQ <= 3.9) {
+        return bScore;
+      } else if(2 <= sQ && sQ <= 2.9) {
+        return cScore;
+      } else if(1.5 <= sQ && sQ <= 1.9) {
+        return dScore;
+      } else if(1 <= sQ && sQ <= 1.4) {
+        return fScore;
+      }
+    };
 
-    } else if(4000 <= dS < 6500) {
+    function calculateSleepLengthGrade() {
+      let sL = userSleep;
+      if(sL >= 8 ){
+        return aScore;
+      } else if( 7 <= sL && sL < 8) {
+        return bScore;
+      } else if(6 <= sL && sL < 7) {
+        return cScore;
+      } else if(5 <= sL && sL < 6) {
+        return dScore;
+      } else if(sL < 5) {
+        return fScore;
+      }
+    };
 
-    } else if(dS < 4000) {
-
-    }
-  },
-
-  calculateFlightsGrade: function() {
-    let f = userFloorsClimbed;
-    if(35 < f < 51){
-
-    } else if(20 < f < 36) {
-
-    } else if(7 < f < 21) {
-
-    } else if(1 < f < 8) {
-
-    } else if(f < 2) {
-
-    }
-  },
-
-  calculateMinutesActiveGrade: function() {
-    let mA = userActiveTime;
-    if(mA > 200){
-
-    } else if(130 < mA < 201) {
-
-    } else if(80 < mA < 132) {
-
-    } else if(45 < mA < 81) {
-
-    } else if(mA < 46) {
-
-    }
-  },
-
-  calculateSleepQualGrade: function() {
-    let sQ = userSleepQuality;
-    if(sQ >= 4 ){
-
-    } else if(3 <= sQ <= 3.9) {
-
-    } else if(2 <= sQ <= 2.9) {
-
-    } else if(1.5 <= sQ <= 1.9) {
-
-    } else if(1 <= sQ <= 1.4) {
-
-    }
-  },
-
-  calculateSleepLengthGrade: function() {
-    let sL = userSleep;
-    if(sL >= 8 ){
-
-    } else if( 7 <= sL < 8) {
-
-    } else if(6 <= sL < 7) {
-
-    } else if(5 <= sL < 6) {
-
-    } else if(sL < 5) {
-
-    }
-  },
-
-  calculateHydrationGrade: function() {
-    let h = userHydration;
-    if(h > 80){
-
-    } else if(65 < h < 81) {
-
-    } else if(50 < h < 66) {
-
-    } else if(35 < h < 51) {
-
-    } else if(h < 36) {
-
-    }
-  },
+    function calculateHydrationGrade() {
+      let h = userHydration;
+      if(h > 80){
+        return aScore;
+      } else if(65 < h && h < 81) {
+        return bScore;
+      } else if(50 < h && h < 66) {
+        return cScore;
+      } else if(35 < h && h < 51) {
+        return dScore;
+      } else if(h < 36) {
+        return fScore;
+      }
+    };
+  }
 }

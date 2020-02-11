1fd6a4d2721142890a2801bff51f6ec0a322ceba
Trond Makonese <trondation@gmail.com>
2020-02-11 10:53:00 -0700

    Create method skeletons.

--- src/ActivityRepo.js
+++ src/ActivityRepo.js
@@ -2,6 +2,24 @@ class ActivityRepo {
   constructor(activityData) {
     this.activityData = activityData;
   }
+
+  allUserStairsClimbedByDate() {
+
+  }
+
+  allStepsTakenByDate() {
+
+  }
+
+  allMinutesActiveByDate() {
+
+  }
+
+  addDays(date, daysToAdd) {
+    var newDate = new Date(date.valueOf());
+    newDate.setDate(newDate.getDate() + daysToAdd);
+    return newDate;
+  }
 }
 
 if(typeof module !== 'undefined') {

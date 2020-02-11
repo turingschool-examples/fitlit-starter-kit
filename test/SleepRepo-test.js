eecfd755f2546fb6c957ca59e8680a14f25583c2
Trond Makonese <trondation@gmail.com>
2020-02-10 20:07:47 -0700

    Write additional test for sleep methods.

--- test/SleepRepo-test.js
+++ test/SleepRepo-test.js
@@ -24,4 +24,16 @@ describe('User Repo', () => {
   it('should be return all users average sleep quality', () => {
     expect(sleepRepo.averageSleepQuality()).to.equal(3);
   })
+
+  it('should be return the user with the most sleep this week', () => {
+    expect(sleepRepo.usersWithMostSleep('2019/06/15')).to.deep.equal({ userID: 3, date: '2019/06/15', hoursSlept: 10.8, sleepQuality: 4.7 });
+  })
+
+  it('should be return the user with the higest user sleep quality for the week.', () => {
+    expect(sleepRepo.usersWithHigestSleepQuality('2019/06/15')).to.deep.equal({ userID: 2, date: '2019/06/15', hoursSlept: 7, sleepQuality: 4.7 });
+  })
+
+  it.skip('should be return the user with the best sleep quality for the week', () => {
+    expect(sleepRepo.usersWithSleepQualityOver3('2019/06/15').to.equal(3));
+  })
 });

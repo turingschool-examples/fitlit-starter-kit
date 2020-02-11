a8983e6e150e3f6bab125c75127e24031eec9aa6
Trond Makonese <trondation@gmail.com>
2020-02-11 11:47:10 -0700

    Test all class methods.

--- test/ActivityRepo-test.js
+++ test/ActivityRepo-test.js
@@ -19,4 +19,26 @@ describe('Activity Repo', () => {
   it('should be an instance of the user repo', () => {
     expect(activityData).to.be.an.instanceof(ActivityRepo);
   })
+
+  it('should return average flight of stairs climbed for all users', () => {
+    expect(activityData.allUserStairsClimbedByDate("2019/06/15")).to.equal(24);
+  })
+
+  it('should return average number of steps taken for all users', () => {
+    expect(activityData.averageStepsTakenByDate("2019/06/15")).to.equal(8098);
+  })
+
+  it('should return average minutes active for all users', () => {
+    expect(activityData.averageMinutesActiveByDate("2019/06/15")).to.equal(151);
+  })
+
+  it('should return all time higest flights of stairs climbed for all users', () => {
+    expect(activityData.allTimeFlightsClimbed()).to.deep.equal({
+      userID: 17,
+      date: '2019/06/15',
+      numSteps: 12982,
+      minutesActive: 231,
+      flightsOfStairs: 46
+    });
+  })
 })

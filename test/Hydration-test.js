7973f3c237ac212e8c049d42646a91a99e2ce163
Trond Makonese <trondation@gmail.com>
2020-02-11 10:01:01 -0700

    need to look at the return amount drank by the day method.

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -40,15 +40,11 @@ describe('Hydration default properties', () => {
     expect(hydration.fluidConsumedByDate("2019/06/15")).to.equal(37);
   })
 
-  // it('it should have a date', () => {
-  //   expect(hydration.fluidConsumededWeekly("2019/06/15")).to.equal(37);
-  // })
-
   it('it should have a date', () => {
     expect(hydration.fluidConsumedALlTime(1)).to.equal(5831);
   })
 
-  it('it should have a date', () => {
-    expect(hydration.fluidConsumededWeekly()).to.deep.equal([ 97, 65, 25, 22, 90, 81, 87 ]);
+  it('it should have fluid consumed over the week', () => {
+    expect(hydration.fluidConsumededWeekly("2019/06/15", 14)).to.deep.equal();
   })
 })

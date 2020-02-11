52cd0f6d82cdca0191b747ee07be65c9cd87aa30
Trond Makonese <trondation@gmail.com>
2020-02-11 11:46:22 -0700

    Create and test all activity methods.

--- src/ActivityRepo.js
+++ src/ActivityRepo.js
@@ -3,16 +3,60 @@ class ActivityRepo {
     this.activityData = activityData;
   }
 
-  allUserStairsClimbedByDate() {
+  allUserStairsClimbedByDate(weekStart) {
+    weekStart = new Date(weekStart)
+    let weekEnd = this.addDays(weekStart, 6);
+
+    let data = this.activityData.filter(function(obj) {
+      var date = new Date(obj.date);
+      return date >= weekStart && date <= weekEnd;
+    });
+
+    let averageStairsClimbed = this.activityData.reduce((acc, users) => {
+    acc += users.flightsOfStairs;
+    return acc
+    }, 0)
+    return Math.round(averageStairsClimbed / this.activityData.length);
 
   }
 
-  allStepsTakenByDate() {
+  averageStepsTakenByDate(weekStart) {
+    weekStart = new Date(weekStart)
+    let weekEnd = this.addDays(weekStart, 6);
 
+    let data = this.activityData.filter(function(obj) {
+      var date = new Date(obj.date);
+      return date >= weekStart && date <= weekEnd;
+    });
+
+    let averageStepsTaken = this.activityData.reduce((acc, users) => {
+    acc += users.numSteps;
+    return acc
+    }, 0)
+    return Math.round(averageStepsTaken / this.activityData.length);
   }
 
-  allMinutesActiveByDate() {
+  averageMinutesActiveByDate(weekStart) {
+    weekStart = new Date(weekStart)
+    let weekEnd = this.addDays(weekStart, 6);
 
+    let data = this.activityData.filter(function(obj) {
+      var date = new Date(obj.date);
+      return date >= weekStart && date <= weekEnd;
+    });
+
+    let averageMinutesActive = this.activityData.reduce((acc, users) => {
+    acc += users.minutesActive;
+    return acc
+    }, 0)
+    return Math.round(averageMinutesActive  / this.activityData.length);
+  }
+
+  allTimeFlightsClimbed() {
+    let sortedFlights = this.activityData.sort((a, b) => {
+      return b.flightsOfStairs - a.flightsOfStairs;
+    })
+    return sortedFlights.shift();
   }
 
   addDays(date, daysToAdd) {

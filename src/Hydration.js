efde00a0340b9dccffdbfa50da8436c7f4a318f1
Alex Sodemann <asodemann18@gmail.com>
2020-05-12 17:21:13 -0600

    Create properties and corresponding tests for Hydration class

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,9 +1,13 @@
 class Hydration {
-  constructor() {
-
+  constructor(hydrationData) {
+    this.userID = hydrationData.userID;
+    this.date = hydrationData.date;
+    this.numOunces = hydrationData.numOunces;
   }
 }
 
 if (typeof module !== 'undefined') {
   module.exports = Hydration;
 }
+
+// For a user, how many fluid ounces they consumed for a specific day (identified by a date)
--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -4,8 +4,33 @@ const expect = chai.expect;
 const Hydration = require('../src/Hydration');
 
 describe('Hydration', () => {
+  beforeEach(() => {
+    hydration = new Hydration({
+      "userID": 1,
+      "date": "2019/06/15",
+      "numOunces": 37
+    })
+  })
 
   it('should be a function', () => {
     expect(Hydration).to.be.a('function')
   })
+
+  it('should return new instance of Hydration', () => {
+    expect(hydration).to.be.an.instanceOf(Hydration);
+  })
+
+  it('should have an id', () => {
+    expect(hydration.userID).to.equal(1);
+  })
+
+  it('should have a date', () => {
+    expect(hydration.date).to.equal('2019/06/15');
+  })
+
+  it('should have number of ounces drank', () => {
+    expect(hydration.numOunces).to.equal(37);
+  })
+
+
 })

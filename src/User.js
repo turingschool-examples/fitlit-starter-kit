fb2674d0b7aec21bbe595c5f78a123d6b1e17f73
Alex Sodemann <asodemann18@gmail.com>
2020-05-12 16:19:57 -0600

    Create properties/methods for User class

--- src/User.js
+++ src/User.js
@@ -1,6 +1,16 @@
 class User {
-  constructor() {
+  constructor(userData) {
+    this.id = userData.id;
+    this.name = userData.name;
+    this.address = userData.address;
+    this.email = userData.email;
+    this.strideLength = userData.strideLength;
+    this.dailyStepGoal = userData.dailyStepGoal;
+    this.friends = userData.friends;
+  }
 
+  getName() {
+    return this.name.split(' ')[0];
   }
 }
 

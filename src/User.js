4695ea09ecd84d49449549952a6f25c71da3e9da
Joe Haefling <josephhaefling@gmail.com>
2020-05-12 13:37:58 -0600

    Create user tests and getFirstName method

--- src/User.js
+++ src/User.js
@@ -1,12 +1,17 @@
 class User {
-  constructor(user) {
-    this.id = user.id;
-    this.name = user.name;
-    this.address = user.address;
-    this.email = user.email;
-    this.strideLength = user.strideLength;
-    this.stepGoal = user.dailyStepGoal;
-    this.userFriends = user.friends;
+  constructor(userData) {
+    this.id = userData.id;
+    this.name = userData.name;
+    this.address = userData.address;
+    this.email = userData.email;
+    this.strideLength = userData.strideLength;
+    this.dailyStepGoal = userData.dailyStepGoal;
+    this.userFriends = userData.friends;
+  }
+
+  getFirstName() {
+    let name = this.name.split(' ')
+    return name[0]
   }
 }
 

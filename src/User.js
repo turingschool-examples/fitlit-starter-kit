765196af6eae71a18c6bc7b19113335da3c13f1d
Horacio Borrego <hborrego365@gmail.com>
2020-05-12 19:36:02 -0600

    :crystal_ball:Create function displayUserData

--- src/User.js
+++ src/User.js
@@ -1,8 +1,8 @@
 class User {
     constructor(userData) {
         this.id = userData.id;
-        this.name = userData.id;
-        this.address = userData.id;
+        this.name = userData.name;
+        this.address = userData.address;
         this.email = userData.email;
         this.strideLength = userData.strideLength;
         this.dailyStepGoal = userData.dailyStepGoal;
--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,22 @@
+let user;
 const displayUserInfo = document.querySelector('.display-user-info');
+window.onload = createRandomUser(), displayUserData()
+
+
+
+function createRandomUser() {
+  let randomUser = Math.floor(Math.random() * userData.length)
+  return user = new User(userData[randomUser])
+}
+
+function displayUserData() {
+  displayUserInfo.innerHTML = `
+  <p>User: ${user.name}</p>
+  <p>Address: ${user.address}</p>
+  <p>Email: ${user.email}</p>
+  <p>Stride Length: ${user.strideLength}</p>
+  <p>Daily Step Goal: ${user.dailyStepGoal}</p>
+  <p>Friends: ${user.friends}</p>
+  <p>User Id: ${user.id}</p>
+  `
+}

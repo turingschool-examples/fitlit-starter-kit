8dff707086a8703a102f3bb1675e2a93614aab55
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-05-13 15:12:02 -0600

    :bar_chart: User Repository Tests

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,10 +1,29 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-import UserRepository from '../src/UserRepository';
-
+const UserRepository = require('../src/UserRepository')
+const userDataTest = require('../data/userDataTest')
 
 describe('UserRepository', function() {
+  let userRepository
 
-    
-}
+  beforeEach(() => {
+    userRepository = new UserRepository(userDataTest)
+  })
+
+it('should be a function', function() {
+    expect(UserRepository).to.be.a('function');
+  });
+
+  it('should be an instance of user repository', function() {
+    expect(userRepository).to.be.an.instanceof(UserRepository);
+  });
+
+  it('should return a user object when searching by id', function() {
+    expect(userRepository.getUserDataById(2)).to.equal(userDataTest[1]);
+  });
+
+  it('should calculate the total users step goals', function() {
+    expect(userRepository.calculateAverageStepGoalForAllUsers()).to.equal(6667)
+  })
+})

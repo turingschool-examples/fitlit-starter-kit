a38c04c26ee7c0a6ae43a9c5e78e29dacbfa0c2b
Charles Wefso <cwefso@gmail.com>
2020-05-13 14:48:31 -0600

    Adds method to sleep.js to return users sleep data

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,9 +1,23 @@
 class Sleep {
-  constructor() {
+  constructor(sleepData, user) {
+    this.sleepData = sleepData;
+    this.currentUser = user;
+  }
 
+  getUserSleepData() {
+    return this.sleepData.filter(sleep => sleep.userID === this.currentUser.id)
+  }
+
+  getAverageDailySleep() {
+    let userSleepData = this.getUserSleepData()
+    let userAverage = userSleepData.reduce((acc, userData) => {
+      acc += userData.hoursSlept
+      return acc
+    }, 0)
+    return Math.round(userAverage / userSleepData.length)
   }
 }
 
-if (typeof module !== 'undefined') {
-  module.exports = Sleep;
-}
+  if (typeof module !== 'undefined') {
+    module.exports = Sleep;
+  }
--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -1,11 +1,110 @@
 const chai = require('chai')
 const expect = chai.expect
 const Sleep = require('../src/Sleep')
+const User = require('../src/User')
 
+let sleep
+let user1
+let sleepData = [
+    {
+      "userID": 1,
+      "date": "2019/06/15",
+      "hoursSlept": 6.1,
+      "sleepQuality": 2.2
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/15",
+      "hoursSlept": 7,
+      "sleepQuality": 4.7
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/15",
+      "hoursSlept": 10.8,
+      "sleepQuality": 4.7
+    },
+    {
+      "userID": 4,
+      "date": "2019/06/15",
+      "hoursSlept": 5.4,
+      "sleepQuality": 3
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/16",
+      "hoursSlept": 4.1,
+      "sleepQuality": 3.8
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/16",
+      "hoursSlept": 7.5,
+      "sleepQuality": 3.8
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/16",
+      "hoursSlept": 10.7,
+      "sleepQuality": 3.4
+    },
+    {
+      "userID": 4,
+      "date": "2019/06/16",
+      "hoursSlept": 8.3,
+      "sleepQuality": 4.5
+  },
+]
 
-describe('Sleep', function() {
+let userData = [
+  {
+    "id": 1,
+    "name": "Luisa Hane",
+    "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+    "email": "Diana.Hayes1@hotmail.com",
+    "strideLength": 4.3,
+    "dailyStepGoal": 10000,
+    "friends": [
+      16,
+      4,
+      8
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Jarvis Considine",
+    "address": "30086 Kathryn Port, Ciceroland NE 07273",
+    "email": "Dimitri.Bechtelar11@gmail.com",
+    "strideLength": 4.5,
+    "dailyStepGoal": 5000,
+    "friends": [
+      9,
+      18,
+      24,
+      19
+    ]
+  }
+]
+
+describe('sleep', function() {
+
+  beforeEach(function() {
+    user1 = new User(userData[0])
+    sleep = new Sleep(sleepData, user1)
+  })
 
   it('should be an function', function() {
     expect(Sleep).to.be.a('function')
-  });
+  })
+  it('should have sleepData', function() {
+    expect(sleep.sleepData).to.equal(sleepData)
+  })
+  it('should have a currentUser', function() {
+    expect(sleep.currentUser).to.deep.equal(user1)
+  })
+  it('should be able to get a users sleep data', function() {
+    expect(sleep.getUserSleepData()).to.deep.equal([sleepData[0], sleepData[4]])
+  })
+  
+
 })

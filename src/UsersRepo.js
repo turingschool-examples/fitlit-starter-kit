c30b18395b2368b71b17d05f74bec302f2eae14a
Kyle Wong <kylewong2510@gmail.com>
2020-05-13 07:29:27 -0600

    Create usersRepo class and tests

--- src/UsersRepo.js
+++ src/UsersRepo.js
@@ -1,7 +1,18 @@
 class UsersRepo {
-  constructor() {
-
+  constructor(users) {
+    this.allUsers = users
   }
+
+  getUser(id) {
+    return this.allUsers.find(user => user.id === id)
+  }
+
+  getAvgStepGoal() {
+    return this.allUsers.reduce((acc, user) => {
+      return acc += user.dailyStepGoal / this.allUsers.length
+    }, 0)
+  }
+
 }
 
 if (typeof module !== 'undefined') {
--- test/UsersRepo-test.js
+++ test/UsersRepo-test.js
@@ -4,8 +4,82 @@ const expect = chai.expect;
 const UsersRepo = require('../src/UsersRepo');
 
 describe('User Repository', () => {
+  let user1;
+  let user2;
+  let user3;
+  let user4;
+  let usersRepo;
 
+  beforeEach(() => {
+    user1 = {
+      "id": 1,
+      "name": "Luisa Hane",
+      "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+      "email": "Diana.Hayes1@hotmail.com",
+      "strideLength": 4.3,
+      "dailyStepGoal": 10000,
+      "friends": [16, 4, 8]
+    }
+    
+    user2 = {
+      "id": 2,
+      "name": "Jarvis Considine",
+      "address": "30086 Kathryn Port, Ciceroland NE 07273",
+      "email": "Dimitri.Bechtelar11@gmail.com",
+      "strideLength": 4.5,
+      "dailyStepGoal": 5000,
+      "friends": [9, 18, 24, 19]
+    }
+    
+    user3 = {
+      "id": 3,
+      "name": "Herminia Witting",
+      "address": "85823 Bosco Fork, East Oscarstad MI 85126-5660",
+      "email": "Elwin.Tromp@yahoo.com",
+      "strideLength": 4.4,
+      "dailyStepGoal": 5000,
+      "friends": [19, 11, 42, 33]
+    }
+
+    user4 = {
+      "id": 4,
+      "name": "Mae Connelly",
+      "address": "28926 Schinner Islands, Turnermouth NE 23720-3230",
+      "email": "Marcos_Pollich@hotmail.com",
+      "strideLength": 3.1,
+      "dailyStepGoal": 4000,
+      "friends": [48, 7, 44, 8]
+    }
+
+  })
+  
   it('should be a function', () => {
     expect(UsersRepo).to.be.a('function')
-  })
+  });
+  
+  it('should be an instance of UsersRepo', () => {
+    usersRepo = new UsersRepo([user1, user2, user3, user4])
+    expect(usersRepo).to.be.an.instanceOf(UsersRepo)
+  });
+
+  it('should take in an array of users', () => {
+    usersRepo = new UsersRepo([user1, user2, user3, user4])
+    expect(usersRepo.allUsers).to.deep.equal([user1, user2, user3, user4])
+  });
+
+  // it('should take in an only an array of users', () => {
+  //   usersRepo = new UsersRepo({user})
+  //   expect(usersRepo.)
+  // });
+
+  it('should take return a user, given an id', () => {
+    usersRepo = new UsersRepo([user1, user2, user3, user4])
+    expect(usersRepo.getUser(2)).to.deep.equal(user2)
+  });
+
+  it('should find the average step goal of all users', () => {
+    usersRepo = new UsersRepo([user1, user2, user3, user4])
+    expect(usersRepo.getAvgStepGoal()).to.deep.equal(6000)
+  });
+  
 })

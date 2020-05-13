b121eb3ab0823c33fc9fca17da31a61aa20c68fd
Horacio Borrego <hborrego365@gmail.com>
2020-05-12 20:49:20 -0600

    :crystal_ball:Create calcaluateAverageStepGoal method

--- src/dataRepository.js
+++ src/dataRepository.js
@@ -1,11 +1,21 @@
 class DataRepository {
-    constructor(usersData) {
-        this.usersData = usersData
+    constructor(userData) {
+        this.userData = userData
         this.users = []
     }
 
+    calculateAverageStepGoal() {
+// take the total / userData.length
+    const totalDailyStepGoal = this.userData.reduce((acc, data) => {
+        acc += data.dailyStepGoal
+    return acc
+    }, 0)
+    return Math.round(totalDailyStepGoal/this.userData.length)
+    }
 
 }
+
+   
 
 if (typeof module !== 'undefined') {
     module.exports = DataRepository;

11ab63caaf96725fccccb7c992190e850c0ee502
Carly Clift <carlymclift@gmail.com>
2020-05-12 18:55:31 -0600

    Change tests to be in ES6 version

--- test/UserRepoitory-test.js
+++ test/UserRepoitory-test.js
@@ -5,7 +5,7 @@ const UserRepository = require('../src/UserRepository');
 const User = require('../src/User-class');
 const userData = require('../data/users');
 
-describe('UserRepository', function() {
+describe('UserRepository', () => {
 
 	let user1;
 	let user2;
@@ -20,15 +20,15 @@ describe('UserRepository', function() {
 		userRepository = new UserRepository([user1, user2, user3])
 	})
 
-	it('should be a function', function() {
+	it('should be a function', () => {
 			expect(UserRepository).to.be.a('function');
 	})
 
-	it('should be an instance of UserRepository', function() {
+	it('should be an instance of UserRepository', () => {
 			expect(userRepository).to.be.an.instanceof(UserRepository);
 		});
 
-	it('should have data from the user', function() {
+	it('should have data from the user', () => {
 			expect(userData).to.equal(userData);
     })
     
@@ -37,7 +37,7 @@ describe('UserRepository', function() {
             expect(noUser.data).to.equal(undefined);
     })
 
-	it('should be able to get data by the user id', function() {
+	it('should be able to get data by the user id', () => {
 			let getData = userRepository.getDataById(2)
 			expect(getData).to.equal(user2)
     })
@@ -47,7 +47,7 @@ describe('UserRepository', function() {
             expect(getData).to.equal(undefined)
     })
 
-	it('should be able to get average step goal for user', function() {
+	it('should be able to get average step goal for user', () => {
             let getSteps = userRepository.fetchAverageStepGoal(2)
 			expect(getSteps).to.equal(6667)
     })

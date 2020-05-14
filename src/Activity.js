9dc502c05a505e0673cc8d63f45c189b003fdd9f
Alex Sodemann <asodemann18@gmail.com>
2020-05-14 16:15:01 -0600

    Create happy path tests for Activity class

--- src/Activity.js
+++ src/Activity.js
@@ -1,9 +1,36 @@
-class Activity {
-  constructor() {
+const User = require('../src/User');
 
+class Activity {
+  constructor(id, activityData) {
+    this.userActivity = this.getUserActivity(id, activityData);
+  }
+
+  getUserActivity(id, activityData) {
+    return activityData.filter(activity => {
+      return activity.userID === id;
+    })
+  }
+
+  getMilesWalked(date) {
+    let user = new User(this.userActivity[0])
+    console.log(user);
   }
 }
 
 if (typeof module !== 'undefined') {
   module.exports = Activity;
 }
+
+//stride.length = 4.3
+// 3577
+// For a specific day (specified by a date), return the miles a user has walked based on their number of steps (use their strideLength to help calculate this)
+
+// For a user, (identified by their userID) how many minutes were they active for a given day (specified by a date)?
+
+// For a user, how many minutes active did they average for a given week (7 days)?
+
+// For a user, did they reach their step goal for a given day (specified by a date)?
+
+// For a user, find all the days where they exceeded their step goal
+
+// For a user, find their all-time stair climbing record
--- test/Activity-test.js
+++ test/Activity-test.js
@@ -2,10 +2,92 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Activity = require('../src/Activity');
+const activitySampleData = require('../sample-data/activity-sample')
+
+let activity1, activity2, activity3, activity4,
+  activity5, activity6, activity7, activity8,
+  activity9, activity10, activity11, activity12,
+  activity13;
+
+let activityData;
+let activity;
 
 describe('Activity', () => {
+  beforeEach(() => {
+    activity1 = activitySampleData[0];
+    activity2 = activitySampleData[1];
+    activity3 = activitySampleData[2];
+    activity4 = activitySampleData[3];
+    activity5 = activitySampleData[4];
+    activity6 = activitySampleData[5];
+    activity7 = activitySampleData[6];
+    activity8 = activitySampleData[7];
+    activity9 = activitySampleData[8];
+    activity10 = activitySampleData[9];
+    activity11 = activitySampleData[10];
+    activity12 = activitySampleData[11];
+    activity13 = activitySampleData[12];
+
+    activityData = [
+      activity1,
+      activity2,
+      activity3,
+      activity4,
+      activity5,
+      activity6,
+      activity7,
+      activity8,
+      activity9,
+      activity10,
+      activity11,
+      activity12,
+      activity13,
+    ];
+
+    activity = new Activity(1, activityData);
+  })
 
   it('should be a function', () => {
     expect(Activity).to.be.a('function')
+  })
+
+  it('should return new instance of Activity', () => {
+    expect(activity).to.be.an.instanceOf(Activity);
+  })
+
+  it('should throw an error if no arguments is passed as an argument', () => {
+    expect(() => { new Activity() }).to.throw(Error);
+  })
+
+  it('should return user Activity Data', () => {
+    expect(activityData.length).to.equal(13);
+  })
+
+  it('should return a specific user Activity Data', () => {
+    expect(activity.userActivity.length).to.equal(10);
+  })
+
+  it('should return how many miles a user has walked for a given day', () => {
+    expect(activity.getMilesWalked('2019/06/15')).to.equal(3);
+  })
+
+  it('should return how many minutes active a user was on a given day', () => {
+    expect(activity.getMinutesActive('2019/06/15')).to.equal(140);
+  })
+
+  it('should return average minutes active a user was during a given week', () => {
+    expect(activity.getWeeklyAvgMinutesActive('2019/06/15')).to.equal(160);
+  })
+
+  it('should return average minutes active a user was during a given week', () => {
+    expect(activity.reachStepGoal('2019/06/15')).to.equal('Did not meet step goal');
+  })
+
+  it('should return average minutes active a user was during a given week', () => {
+    expect(activity.exceedStepGoal()).to.deep.equal(["2019/06/19","2019/06/20", "2019/06/22"]);
+  })
+
+  it('should return a user all-time stair climbing record', () => {
+    expect(activity.exceedStepGoal()).to.deep.equal(37);
   })
 })

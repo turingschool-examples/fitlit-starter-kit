bcc96500eb5d0336bb6cd5b6c8f46c87dfab96c9
Alex Sodemann <asodemann18@gmail.com>
2020-05-14 18:08:27 -0600

    Add sad path test and logic to methods. Edit hydration-test file

--- src/Activity.js
+++ src/Activity.js
@@ -12,8 +12,53 @@ class Activity {
   }
 
   getMilesWalked(date) {
-    let user = new User(this.userActivity[0])
-    console.log(user);
+    let user = new User();
+    //console.log(user);
+  }
+
+  getMinutesActive(date) {
+    let newDate = this.checkDate(date);
+      if (date === newDate) {
+        let dailyActivity = this.userActivity.filter(activity =>activity.date === date);
+      
+        return dailyActivity.reduce((acc, active) => {
+          return acc += active.minutesActive;
+        }, 0)
+    } else {return 'You must pass a valid date'};
+  }
+
+  getWeeklyAvgMinutesActive(date) {
+    let newDate = this.checkDate(date);
+    if (date === newDate) {
+      let activityDate = this.userActivity.find(activity => {
+        return activity.date === date;
+      })
+
+      let firstDate = this.userActivity.indexOf(activityDate);
+      let allDays = this.userActivity
+        .slice(firstDate, firstDate+7)
+        .map(activity => activity.minutesActive);
+
+      let avg = allDays.reduce((acc, activity) => {
+          return acc += activity / allDays.length;
+        }, 0)
+      
+        return Math.ceil(avg);  
+    } else {return 'You must pass a valid date'};
+  }
+
+  getMaxStairsClimbed() {
+    let stairSort = this.userActivity.sort((a, b) => b.flightsOfStairs - a.flightsOfStairs);
+    
+    return stairSort[0].flightsOfStairs;
+  }
+
+  checkDate(date) {
+    let isDate = new Date(date);
+    let newIsDate = isDate.getFullYear() +"/"+ 
+      ("0" + (isDate.getMonth() + 1)).slice(-2) +"/"+ 
+      ("0" + isDate.getDate()).slice(-2);
+    return newIsDate;
   }
 }
 
@@ -25,12 +70,6 @@ if (typeof module !== 'undefined') {
 // 3577
 // For a specific day (specified by a date), return the miles a user has walked based on their number of steps (use their strideLength to help calculate this)
 
-// For a user, (identified by their userID) how many minutes were they active for a given day (specified by a date)?
-
-// For a user, how many minutes active did they average for a given week (7 days)?
-
 // For a user, did they reach their step goal for a given day (specified by a date)?
 
 // For a user, find all the days where they exceeded their step goal
-
-// For a user, find their all-time stair climbing record
--- test/Activity-test.js
+++ test/Activity-test.js
@@ -67,7 +67,12 @@ describe('Activity', () => {
     expect(activity.userActivity.length).to.equal(10);
   })
 
-  it('should return how many miles a user has walked for a given day', () => {
+  it('should be an empty arry if invalid id is passed as an argument', () => {
+    activity2 = new Activity(500, activityData)
+    expect(activity2).to.deep.equal({ userActivity: [] });
+  })
+
+  it.skip('should return how many miles a user has walked for a given day', () => {
     expect(activity.getMilesWalked('2019/06/15')).to.equal(3);
   })
 
@@ -75,19 +80,43 @@ describe('Activity', () => {
     expect(activity.getMinutesActive('2019/06/15')).to.equal(140);
   })
 
+  it('should only take a date as an argument', () => {
+    expect(activity.getMinutesActive('test')).to.equal('You must pass a valid date');
+  })
+
+  it('should only take a date in the correct format as an argument', () => {
+    expect(activity.getMinutesActive('2019-06-15')).to.equal('You must pass a valid date');
+  })
+
   it('should return average minutes active a user was during a given week', () => {
     expect(activity.getWeeklyAvgMinutesActive('2019/06/15')).to.equal(160);
   })
 
-  it('should return average minutes active a user was during a given week', () => {
+  it('should only take a date as an argument', () => {
+    expect(activity.getMinutesActive('test')).to.equal('You must pass a valid date');
+  })
+
+  it('should only take a date in the correct format as an argument', () => {
+    expect(activity.getWeeklyAvgMinutesActive('2019-06-15')).to.equal('You must pass a valid date');
+  })
+
+  it.skip('should return whether or not a user reached a step goal', () => {
     expect(activity.reachStepGoal('2019/06/15')).to.equal('Did not meet step goal');
   })
 
-  it('should return average minutes active a user was during a given week', () => {
+  it.skip('should return whether or not a user exceeded a step goal', () => {
     expect(activity.exceedStepGoal()).to.deep.equal(["2019/06/19","2019/06/20", "2019/06/22"]);
   })
 
   it('should return a user all-time stair climbing record', () => {
-    expect(activity.exceedStepGoal()).to.deep.equal(37);
+    expect(activity.getMaxStairsClimbed()).to.equal(37);
+  })
+
+  it('should return a user all-time stair climbing record even if an argument is given', () => {
+    expect(activity.getMaxStairsClimbed(3)).to.equal(37);
+  })
+
+  it('should return a date', () => {
+    expect(activity.checkDate('2019/06/15')).to.equal('2019/06/15');
   })
 })

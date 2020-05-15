b228521f4463c3f1f33142c45829afd1ffa94140
Kyle Wong <kylewong2510@gmail.com>
2020-05-14 20:02:52 -0600

    Pass all (but one) sad path tests

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -61,6 +61,10 @@ describe('Sleep', () => {
     expect(sleep.userSleep.length).to.equal(10)
   })
 
+  it('should return a date', () => {
+    expect(sleep.checkDate('2019/06/15')).to.equal('2019/06/15');
+  })
+
   it('should return hours slept for a given day', () => {
     expect(sleep.getDailySleepHours("2019/06/23")).to.equal(8.9)
   })
@@ -89,24 +93,24 @@ describe('Sleep', () => {
     expect(sleep.getAvgSleepHours()).to.equal(6.95)
   })
 
-  it('should return a specific user average Hydration Data for all time even when an argument is passed', () => {
-    expect(sleep.getAvgSleepHours('test')).to.equal(37);
+  it('should calculate average all time sleep hours even when an argument is passed', () => {
+    expect(sleep.getAvgSleepHours('test')).to.equal(6.95);
   })
 
   it('should calculate average all time sleep quality', () => {
     expect(sleep.getAvgSleepQual()).to.equal(3.1)
   })
 
-  it('should return a specific user average Hydration Data for all time even when an argument is passed', () => {
-    expect(sleep.getAvgSleepQual('test')).to.equal(37);
+  it('should calculate average all time sleep quality even when an argument is passed', () => {
+    expect(sleep.getAvgSleepQual('test')).to.equal(3.1);
   })
 
   it('should return specific sleep hours over 7 days', () => {
     expect(sleep.getWeeklySleepHours("2019/06/15")).to.deep.equal([6.1, 7, 10.8, 5.4, 4.1, 9.6, 5.1])
   })
 
-  it('should return a specific user Hydration Data over x days if there aren\t 7 days worth of data', () => {
-    expect(sleep.getWeeklySleepHours('2019/06/20')).to.deep.equal([42, 46, 33, 20, 45]);
+  it('should return specific sleep hours over x days if there aren\'t 7 days worth of data', () => {
+    expect(sleep.getWeeklySleepHours('2019/06/20')).to.deep.equal([9.6, 5.1, 8.1, 8.9, 4.4]);
   })
 
   it('should only take a date as an argument', () => {
@@ -121,8 +125,8 @@ describe('Sleep', () => {
     expect(sleep.getWeeklySleepQual("2019/06/15")).to.deep.equal([2.2, 4.7, 4.7, 3, 3.6, 2.9, 2.6])
   })
 
-  it('should return a specific user Hydration Data over x days if there aren\t 7 days worth of data', () => {
-    expect(sleep.getWeeklySleepQual('2019/06/20')).to.deep.equal([42, 46, 33, 20, 45]);
+  it('should return specific sleep quality over x days if there aren\'t 7 days worth of data', () => {
+    expect(sleep.getWeeklySleepQual('2019/06/20')).to.deep.equal([2.9, 2.6, 3.5, 2.2, 1.6]);
   })
 
   it('should only take a date as an argument', () => {

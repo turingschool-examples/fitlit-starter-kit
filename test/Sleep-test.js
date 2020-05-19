b5422fbe6698a341426525144b67e15398c234d9
Charles Wefso <cwefso@gmail.com>
2020-05-19 14:51:04 -0600

    Fixes sleep-test to have updated test for new sleepData

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -174,13 +174,37 @@ let sleepData = [
       "hoursSlept": 10.6,
       "sleepQuality": 2.7
     },
+    {
+      "userID": 1,
+      "date": "2019/06/22",
+      "hoursSlept": 7,
+      "sleepQuality": 3
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/22",
+      "hoursSlept": 4.8,
+      "sleepQuality": 3.3
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/22",
+      "hoursSlept": 9.8,
+      "sleepQuality": 2.1
+    },
+    {
+      "userID": 4,
+      "date": "2019/06/22",
+      "hoursSlept": 7.7,
+      "sleepQuality": 1.5
+    }
 ]
 
 let sortedSleepers =
-[{"userID":1,"entries":[{"date":"2019/06/15","hoursSlept":6.1,"sleepQuality":2.2},{"date":"2019/06/16","hoursSlept":4.1,"sleepQuality":3.8},{"date":"2019/06/17","hoursSlept":8,"sleepQuality":2.6},{"date":"2019/06/18","hoursSlept":10.4,"sleepQuality":3.1},{"date":"2019/06/19","hoursSlept":10.7,"sleepQuality":1.2},{"date":"2019/06/20","hoursSlept":9.3,"sleepQuality":1.2},{"date":"2019/06/21","hoursSlept":7.8,"sleepQuality":4.2}]},
- {"userID":2,"entries":[{"date":"2019/06/15","hoursSlept":7,"sleepQuality":4.7},{"date":"2019/06/16","hoursSlept":7.5,"sleepQuality":3.8},{"date":"2019/06/17","hoursSlept":5.7,"sleepQuality":3},{"date":"2019/06/18","hoursSlept":10.8,"sleepQuality":3.2},{"date":"2019/06/19","hoursSlept":9.6,"sleepQuality":2.5},{"date":"2019/06/20","hoursSlept":10.1,"sleepQuality":2.4},{"date":"2019/06/21","hoursSlept":4.3,"sleepQuality":4.8}]},
- {"userID":3,"entries":[{"date":"2019/06/15","hoursSlept":10.8,"sleepQuality":4.7},{"date":"2019/06/16","hoursSlept":10.7,"sleepQuality":3.4},{"date":"2019/06/17","hoursSlept":5.3,"sleepQuality":4.9},{"date":"2019/06/18","hoursSlept":9.8,"sleepQuality":2.6},{"date":"2019/06/19","hoursSlept":7.2,"sleepQuality":3.4},{"date":"2019/06/20","hoursSlept":9.4,"sleepQuality":1.2},{"date":"2019/06/21","hoursSlept":8.9,"sleepQuality":3.7}]},
- {"userID":4,"entries":[{"date":"2019/06/15","hoursSlept":5.4,"sleepQuality":3},{"date":"2019/06/16","hoursSlept":8.3,"sleepQuality":4.5},{"date":"2019/06/17","hoursSlept":5.7,"sleepQuality":1.1},{"date":"2019/06/18","hoursSlept":5.9,"sleepQuality":2.5},{"date":"2019/06/19","hoursSlept":5.2,"sleepQuality":2.3},{"date":"2019/06/20","hoursSlept":8.3,"sleepQuality":1.9},{"date":"2019/06/21","hoursSlept":10.6,"sleepQuality":2.7}]}
+[{"userID":1,"entries":[{"date":"2019/06/15","hoursSlept":6.1,"sleepQuality":2.2},{"date":"2019/06/16","hoursSlept":4.1,"sleepQuality":3.8},{"date":"2019/06/17","hoursSlept":8,"sleepQuality":2.6},{"date":"2019/06/18","hoursSlept":10.4,"sleepQuality":3.1},{"date":"2019/06/19","hoursSlept":10.7,"sleepQuality":1.2},{"date":"2019/06/20","hoursSlept":9.3,"sleepQuality":1.2},{"date":"2019/06/21","hoursSlept":7.8,"sleepQuality":4.2},{"date":"2019/06/22","hoursSlept": 7, "sleepQuality": 3}]},
+ {"userID":2,"entries":[{"date":"2019/06/15","hoursSlept":7,"sleepQuality":4.7},{"date":"2019/06/16","hoursSlept":7.5,"sleepQuality":3.8},{"date":"2019/06/17","hoursSlept":5.7,"sleepQuality":3},{"date":"2019/06/18","hoursSlept":10.8,"sleepQuality":3.2},{"date":"2019/06/19","hoursSlept":9.6,"sleepQuality":2.5},{"date":"2019/06/20","hoursSlept":10.1,"sleepQuality":2.4},{"date":"2019/06/21","hoursSlept":4.3,"sleepQuality":4.8},{"date":"2019/06/22","hoursSlept":4.8,"sleepQuality":3.3}]},
+ {"userID":3,"entries":[{"date":"2019/06/15","hoursSlept":10.8,"sleepQuality":4.7},{"date":"2019/06/16","hoursSlept":10.7,"sleepQuality":3.4},{"date":"2019/06/17","hoursSlept":5.3,"sleepQuality":4.9},{"date":"2019/06/18","hoursSlept":9.8,"sleepQuality":2.6},{"date":"2019/06/19","hoursSlept":7.2,"sleepQuality":3.4},{"date":"2019/06/20","hoursSlept":9.4,"sleepQuality":1.2},{"date":"2019/06/21","hoursSlept":8.9,"sleepQuality":3.7},{"date":"2019/06/22","hoursSlept":9.8,"sleepQuality":2.1}]},
+ {"userID":4,"entries":[{"date":"2019/06/15","hoursSlept":5.4,"sleepQuality":3},{"date":"2019/06/16","hoursSlept":8.3,"sleepQuality":4.5},{"date":"2019/06/17","hoursSlept":5.7,"sleepQuality":1.1},{"date":"2019/06/18","hoursSlept":5.9,"sleepQuality":2.5},{"date":"2019/06/19","hoursSlept":5.2,"sleepQuality":2.3},{"date":"2019/06/20","hoursSlept":8.3,"sleepQuality":1.9},{"date":"2019/06/21","hoursSlept":10.6,"sleepQuality":2.7},{"date":"2019/06/22","hoursSlept":7.7,"sleepQuality":1.5}]}
 ]
 
 
@@ -263,14 +287,14 @@ describe('sleep', function() {
     expect(sleep1.currentUser).to.deep.equal(user1)
   })
   it('should be able to get a users sleep data', function() {
-    expect(sleep1.getUserSleepData()).to.deep.equal([sleepData[0], sleepData[4], sleepData[8], sleepData[12], sleepData[16], sleepData[20],  sleepData[24]])
-    expect(sleep2.getUserSleepData()).to.deep.equal([sleepData[1], sleepData[5], sleepData[9], sleepData[13], sleepData[17],  sleepData[21], sleepData[25]])
+    expect(sleep1.getUserSleepData()).to.deep.equal([sleepData[0], sleepData[4], sleepData[8], sleepData[12], sleepData[16], sleepData[20],  sleepData[24], sleepData[28]])
+    expect(sleep2.getUserSleepData()).to.deep.equal([sleepData[1], sleepData[5], sleepData[9], sleepData[13], sleepData[17],  sleepData[21], sleepData[25], sleepData[29]])
   })
   it('should be able to get users average daily sleep', function() {
-    expect(sleep1.getAverageDailySleep()).to.equal(8.06)
+    expect(sleep1.getAverageDailySleep()).to.equal(7.92)
   })
   it('should be able to get users average daily sleep quality', function() {
-    expect(sleep1.getAverageSleepQuality()).to.equal(2.61)
+    expect(sleep1.getAverageSleepQuality()).to.equal(2.66)
   })
   it('should be able to get a how many hours a user slept on a specific day', function() {
     expect(sleep1.getSleepForSpecificDay('2019/06/16')).to.equal(4.1)
@@ -279,33 +303,35 @@ describe('sleep', function() {
     expect(sleep1.getQualityForSpecificDay('2019/06/16')).to.equal(3.8)
   })
   it('should be able to get a week of hours sleep data', function() {
-    expect(sleep1.getOneUserWeekOfSleepData('2019/06/15', sleepData)).to.deep.equal([
-      { userID: 1, date: '2019/06/15', hoursSlept: 6.1, sleepQuality: 2.2 },
-      { userID: 1, date: '2019/06/16', hoursSlept: 4.1, sleepQuality: 3.8 },
-      { userID: 1, date: '2019/06/17', hoursSlept: 8, sleepQuality: 2.6 },
-      { userID: 1, date: '2019/06/18', hoursSlept: 10.4, sleepQuality: 3.1 },
-      { userID: 1, date: '2019/06/19', hoursSlept: 10.7, sleepQuality: 1.2 },
+    expect(sleep1.getOneUserWeekOfSleepData('2019/06/22', sleepData)).to.deep.equal([
+      { userID: 1, date: '2019/06/22', hoursSlept: 7, sleepQuality: 3 },
+      { userID: 1, date: '2019/06/21', hoursSlept: 7.8, sleepQuality: 4.2 },
       { userID: 1, date: '2019/06/20', hoursSlept: 9.3, sleepQuality: 1.2 },
-      { userID: 1, date: '2019/06/21', hoursSlept: 7.8, sleepQuality: 4.2 }]
+      { userID: 1, date: '2019/06/19', hoursSlept: 10.7, sleepQuality: 1.2 },
+      { userID: 1, date: '2019/06/18', hoursSlept: 10.4, sleepQuality: 3.1 },
+      { userID: 1, date: '2019/06/17', hoursSlept: 8, sleepQuality: 2.6 },
+      { userID: 1, date: '2019/06/16', hoursSlept: 4.1, sleepQuality: 3.8 },
+      { userID: 1, date: '2019/06/15', hoursSlept: 6.1, sleepQuality: 2.2 },
+    ]
     )
   })
   it('should be able to get total hours slept for each day in a week for one user', function() {
-    expect(sleep1.getWeekofHoursSlept('2019/06/15')).to.deep.equal([6.1, 4.1, 8, 10.4, 10.7, 9.3, 7.8])
+    expect(sleep1.getWeekofHoursSlept('2019/06/21')).to.deep.equal([ 7.8, 9.3, 10.7, 10.4, 8, 4.1, 6.1 ])
   })
   it('should be able to get quality of sleep for each day in a week for one user', function() {
-    expect(sleep1.getWeekofSleepQuality('2019/06/15')).to.deep.equal([2.2, 3.8, 2.6, 3.1, 1.2, 1.2, 4.2])
+    expect(sleep1.getWeekofSleepQuality('2019/06/21')).to.deep.equal([ 4.2, 1.2, 1.2, 3.1, 2.6, 3.8, 2.2 ])
   })
   it('should be able to get all users average daily sleep quality', function() {
-    expect(sleep1.getAllUsersAverageSleepQuality()).to.deep.equal(3.02)
+    expect(sleep1.getAllUsersAverageSleepQuality()).to.deep.equal(2.95)
   })
   it('should be able to get sorted sleepers', function() {
     expect(sleep.sortSleeps()).to.deep.equal(sortedSleepers)
   })
   it('should be able to get best sleepers', function() {
     expect(sleep.getBestSleepers('2019/06/15')).to.deep.equal([
-      { userID: 2, sleepAverage: 3.49 },
-      { userID: 3, sleepAverage: 3.41 },
-      { userID: 1, sleepAverage: 2.61 }
+      { userID: 2, sleepAverage: 3.96 },
+      { userID: 3, sleepAverage: 3.71 },
+      { userID: 1, sleepAverage: 3.04 }
     ])
   })
   it('should be able to get top sleeper for a day', function() {

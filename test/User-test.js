1348ad77120ca46dd981eb7f482ffe2403fa9511
Michael Walker <Michael.e.walker.87@gmail.com>
2020-08-23 13:06:16 -0600

    Remove the user2 and user3 from beforeEach

--- test/User-test.js
+++ test/User-test.js
@@ -18,35 +18,6 @@ describe("User", () => {
                   8
                 ]
               },
-    user2 =  {
-                "id": 2,
-                "name": "Jarvis Considine",
-                "address": "30086 Kathryn Port, Ciceroland NE 07273",
-                "email": "Dimitri.Bechtelar11@gmail.com",
-                "strideLength": 4.5,
-                "dailyStepGoal": 5000,
-                "friends": [
-                  9,
-                  18,
-                  24,
-                  19
-                ]
-              },
-    user3 =  {
-              "id": 3,
-              "name": "Herminia Witting",
-              "address": "85823 Bosco Fork, East Oscarstad MI 85126-5660",
-              "email": "Elwin.Tromp@yahoo.com",
-              "strideLength": 4.4,
-              "dailyStepGoal": 5000,
-              "friends": [
-                19,
-                11,
-                42,
-                33
-              ]
-            },
-
     userRepository = new UserRepository([user1,user2,user3]);
     user = new User(userRepository.userData[0]);
   });

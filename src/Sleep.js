8c1551033b4a82b28085aa9ea0a9f5f10673fa19
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-24 19:57:46 -0400

    Add methods to replicate hydration

--- src/Sleep.js
+++ src/Sleep.js
@@ -4,6 +4,21 @@ class Sleep {
     this.userSleep = sleepData.filter(dailySleep => dailySleep.userID === id);
     return this.userSleep;
   }
+  averageAllTimeSleep() {
+    let allTimeSleep = this.userSleep.reduce((sleep, day) =>{
+      sleep += day.sleepQuality;
+      return sleep 
+    }, 0)
+    return Number((allTimeSleep / this.userSleep.length).toFixed(1))
+  }
+  daySleep(dateSelected) {
+    return this.userSleep.find(day => day.date === dateSelected).hoursSlept;
+  }
+  dailySleepPerGivenWeek(startDate) {         
+    let startingDate = this.userSleep.find(day => day.date === startDate);
+    let firstDay = this.userSleep.indexOf(startingDate);
+    return this.userSleep.slice(firstDay, firstDay + 7).map(day => day.hoursSlept)
+  }
 }
 if (typeof module !== 'undefined') {
   module.exports = Sleep;

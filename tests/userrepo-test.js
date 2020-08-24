cf1d21b1db0f9f39b1b65e41364bd24dba6035be
Brigette Doelp <brigette.doelp@gmail.com>
2020-08-24 14:27:41 -0700

    Set up bones for testing block

---
+++ tests/userrepo-test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+import userTestData from './testData/user-test-data';
+
+const UserRepo = require('../src/UserRepo');
+
+describe('UserRepo', function() {
+  let userRepo
+  beforeEach(() => {
+    userRepo = new UserRepo()
+  })
+
+  
+
+
+
+
+
+
+
+
+
+
+})

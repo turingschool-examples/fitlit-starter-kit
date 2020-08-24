0f9e91e41021ddd870be5ec348a61bce0c200317
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-24 19:38:00 -0400

    Add userSleepData method

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,5 +1,9 @@
 class Sleep {
   constructor() {}
+  userSleepData(sleepData, id) {
+    this.userSleep = sleepData.filter(dailySleep => dailySleep.userID === id);
+    return this.userSleep;
+  }
 }
 if (typeof module !== 'undefined') {
   module.exports = Sleep;

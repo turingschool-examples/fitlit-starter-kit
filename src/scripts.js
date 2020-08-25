a5d02cc2b498ca22ecd121b534863d18fcb86a28
Hanna Kim <hannakim91@gmail.com>
2020-08-25 14:49:23 -0600

    Add query selectors for HTML elements for user info

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,8 @@
 console.log("Hello World");
+
+const greeting = document.querySelector('.greeting');
+const userName = document.querySelector('.user-name');
+const userAddress = document.querySelector('.user-address');
+const userStrideLength = document.querySelector('.user-stride-length');
+const userDailyStepGoal = document.querySelector('.user-daily-step-goal');
+const userFriends = document.querySelector('.user-friends');

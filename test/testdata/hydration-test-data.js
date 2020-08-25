b708fe5c1045b333dc2771bab2f5fa97af41a222
Brigette Doelp <brigette.doelp@gmail.com>
2020-08-25 15:58:11 -0700

    Create data file for tests

---
+++ test/testdata/hydration-test-data.js
@@ -0,0 +1,52 @@
+const hydroData = [
+  {
+    "userID": 1,
+    "date": "2019/06/15",
+    "numOunces": 37
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "numOunces": 5
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/17",
+    "numOunces": 10
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/18",
+    "numOunces": 15
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/19",
+    "numOunces": 20
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/20",
+    "numOunces": 25
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/21",
+    "numOunces": 30
+  },
+  {
+    "userID": 1,
+    "date": "2019/09/15",
+    "numOunces": 35
+  },
+  {
+    "userID": 1,
+    "date": "2019/02/15",
+    "numOunces": 40
+  },
+  {
+    "userID": 1,
+    "date": "2019/10/15",
+    "numOunces": 45
+  },
+]

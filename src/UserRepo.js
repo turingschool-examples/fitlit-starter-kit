2135498bc0a77e0d1b12f3ca7353da6db53f33f3
Brigette Doelp <brigette.doelp@gmail.com>
2020-08-24 18:10:19 -0700

    Calculate average steps #4

--- src/UserRepo.js
+++ src/UserRepo.js
@@ -7,8 +7,20 @@ class UserRepo {
       return user.id === idNum
     })
   }
+  calculateAverageStepGoals() {
+    const totalOfStepGoals = this.users.reduce((average, user) => {
+      average += user.dailyStepGoal
+      return average
+    }, 0)
+    const averageStepGoal = Math.round(totalOfStepGoals / this.users.length)
+    return averageStepGoal
+  }
 };
 
+//i want to take the stepGoals of each user and average them out.
+//add all counts up, then divide by the number of users, rounding down in the event of a decimal
+//need to iterate through addding and reassigning the aveStepGoal var
+//then divide that by this.users.length.
 
 
 

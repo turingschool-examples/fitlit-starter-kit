62dea3f4af9a7fb09c83d39d7365d521b1d13ff4
Brigette Doelp <brigette.doelp@gmail.com>
2020-08-24 18:31:51 -0700

    Pass in individual user's data

--- src/User.js
+++ src/User.js
@@ -1,6 +1,12 @@
 class User {
   constructor(userData) {
-    
+    this.id = userData.id;
+    this.name = userData.name;
+    this.address = userData.address;
+    this.email = userData.email;
+    this.strideLength = userData.strideLength;
+    this.dailyStepGoal = userData.dailyStepGoal;
+    this.friends = userData.friends;
   }
 }
 

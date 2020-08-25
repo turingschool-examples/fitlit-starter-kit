a4fba5cef87c23d6a070fa741ff8b0edb1891e6b
Brigette Doelp <brigette.doelp@gmail.com>
2020-08-25 15:58:33 -0700

    Test for class instantiation

---
+++ test/hydration-test.js
@@ -0,0 +1,81 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const HydrationRepo = require('../src/Hydration');
+
+describe('HydrationRepo', () => {
+  let hydrationRepo;
+  beforeEach(() => {
+    const hydrationData = [
+      {
+        "userID": 1,
+        "date": "2019/06/15",
+        "numOunces": 37
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/16",
+        "numOunces": 5
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/17",
+        "numOunces": 10
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/18",
+        "numOunces": 15
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/19",
+        "numOunces": 20
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/20",
+        "numOunces": 25
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/21",
+        "numOunces": 30
+      },
+      {
+        "userID": 1,
+        "date": "2019/09/15",
+        "numOunces": 35
+      },
+      {
+        "userID": 1,
+        "date": "2019/02/15",
+        "numOunces": 40
+      },
+      {
+        "userID": 1,
+        "date": "2019/10/15",
+        "numOunces": 45
+      },
+    ]
+    hydrationRepo = new HydrationRepo(hydrationData);
+  });
+
+  it('should be a function', () => {
+    expect(HydrationRepo).to.be.a('function');
+  })
+
+  it('should instantiate a class of Hydration', () => {
+    expect(hydrationRepo).to.be.an.instanceof(HydrationRepo);
+  })
+
+
+
+
+
+
+
+
+
+
+})

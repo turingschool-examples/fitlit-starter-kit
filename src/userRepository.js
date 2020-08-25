68ac101d8b6dd5cc31930f659dc48d0ee77bf58f
Ian Holladay <holladayian@gmail.com>
2020-08-24 21:03:01 -0600

    refactor returnTotalAverageStepGoal

--- src/userRepository.js
+++ src/userRepository.js
@@ -3,7 +3,22 @@ class UserRepository {
     this.data = data;
   }
 
+  returnUserData(userID) {
+    return this.data.filter(datum => {
+      if (datum.id === userID) {
+        return datum
+      }
+    })
+  }
+  returnTotalAverageStepGoal() {
+    let totalStepGoal = 0;
+    this.data.forEach(datum => {
+      return (totalStepGoal += datum['dailyStepGoal']);
+    })
+    return Math.floor(totalStepGoal / this.data.length);
+  }
 }
+
 
 
 module.exports = UserRepository;

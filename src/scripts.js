2f5e151d98fe08f630c0bee28a6be370320bc845
Hanna Kim <hannakim91@gmail.com>
2020-08-25 16:04:02 -0600

    Add event listener on load to display basic user data

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,30 @@
 console.log("Hello World");
 
 const greeting = document.querySelector('.greeting');
-const userName = document.querySelector('.user-name');
-const userAddress = document.querySelector('.user-address');
-const userStrideLength = document.querySelector('.user-stride-length');
-const userDailyStepGoal = document.querySelector('.user-daily-step-goal');
-const userFriends = document.querySelector('.user-friends');
+const userCard = document.querySelector('.user-card');
+
+window.addEventListener('load', displayUserData);
+
+function displayUserData() {
+  const user = new User(userData[33])
+  const userRepo = new UserRepo(userData)
+  const friendList = user.friends.map(friend => {
+  const info = userRepo.searchUsersByID(friend)
+    return info.name
+  })
+
+  const userName = document.querySelector('.user-name');
+  const userAddress = document.querySelector('.user-address');
+  const userStrideLength = document.querySelector('.user-stride-length');
+  const userDailyStepGoal = document.querySelector('.user-daily-step-goal');
+  const userFriends = document.querySelector('.user-friends');
+
+  userName.innerText = user.name
+  userAddress.innerText = user.address
+  userStrideLength.innerText = user.strideLength
+  userDailyStepGoal.innerText = user.dailyStepGoal
+  userFriends.innerText = friendList
+}
+
+
+

cabd76aea20f92b66ab96bf8ae6afba8c6df57c6
Ian Holladay <holladayian@gmail.com>
2020-08-24 18:21:22 -0600

    add hydration samples data

---
+++ data/hydrationSamples.js
@@ -0,0 +1,47 @@
+const hydrationSampleData = [
+  {
+    "userID": 1,
+    "date": "2019/06/15",
+    "numOunces": 37
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "numOunces": 75
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "numOunces": 47
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "numOunces": 69
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/17",
+    "numOunces": 68
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/18",
+    "numOunces": 67
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/19",
+    "numOunces": 66
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/20",
+    "numOunces": 65
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/21",
+    "numOunces": 64
+  }
+];

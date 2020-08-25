b9d56ac579dff6df3fef3e762dff3f6e4c0d8f62
Ian Holladay <holladayian@gmail.com>
2020-08-24 18:24:32 -0600

    add activity sample data

---
+++ data/activitySamples.js
@@ -0,0 +1,65 @@
+const activitySampleData = [
+  {
+    "userID": 1,
+    "date": "2019/06/15",
+    "numSteps": 3577,
+    "minutesActive": 140,
+    "flightsOfStairs": 16
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "numSteps": 4294,
+    "minutesActive": 138,
+    "flightsOfStairs": 10
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "numSteps": 7402,
+    "minutesActive": 116,
+    "flightsOfStairs": 33
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "numSteps": 6637,
+    "minutesActive": 175,
+    "flightsOfStairs": 36
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/17",
+    "numSteps": 6636,
+    "minutesActive": 176,
+    "flightsOfStairs": 35
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/18",
+    "numSteps": 6635,
+    "minutesActive": 177,
+    "flightsOfStairs": 34
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/19",
+    "numSteps": 6634,
+    "minutesActive": 178,
+    "flightsOfStairs": 33
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/20",
+    "numSteps": 6633,
+    "minutesActive": 179,
+    "flightsOfStairs": 32
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/21",
+    "numSteps": 6632,
+    "minutesActive": 180,
+    "flightsOfStairs": 31
+  }
+];

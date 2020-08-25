f705045428178611e62f3d6a17aa83fe49607836
Brigette Doelp <brigette.doelp@gmail.com>
2020-08-24 18:15:14 -0700

    Remove pseudocode

--- src/UserRepo.js
+++ src/UserRepo.js
@@ -17,10 +17,7 @@ class UserRepo {
   }
 };
 
-//i want to take the stepGoals of each user and average them out.
-//add all counts up, then divide by the number of users, rounding down in the event of a decimal
-//need to iterate through addding and reassigning the aveStepGoal var
-//then divide that by this.users.length.
+
 
 
 

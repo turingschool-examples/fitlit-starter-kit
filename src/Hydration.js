3e86a030c48fe67d1b95c0cf6378246c240708fb
Brigette Doelp <brigette.doelp@gmail.com>
2020-08-26 12:43:11 -0700

    Calculate average fl oz imbibed by single user

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,6 +1,20 @@
 class HydrationRepo {
   constructor(hydroData) {
-
+    this.hydroData = hydroData;
+  }
+  findUserAvgDailyHydration(userID) {
+    const singleUserEntries = this.hydroData.filter(userEntry => {
+        return userID === userEntry.userID
+    });
+    const singleUserHydrationData = singleUserEntries.map(userData => {
+      return userData.numOunces
+    });
+    const totalSingleUserHydrationData = singleUserHydrationData.reduce((sum, num) => {
+      sum += num;
+      return sum
+    }, 0);
+    const avgUserHydrationData = totalSingleUserHydrationData / singleUserHydrationData.length
+    return avgUserHydrationData
   }
 }
 

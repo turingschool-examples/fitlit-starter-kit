3d130ef116c24119de8d48e9687ef30dca3b07b5
Brigette Doelp <brigette.doelp@gmail.com>
2020-08-26 12:43:48 -0700

    Test find average fl oz of single user

--- test/hydration-test.js
+++ test/hydration-test.js
@@ -10,7 +10,7 @@ describe('HydrationRepo', () => {
       {
         "userID": 1,
         "date": "2019/06/15",
-        "numOunces": 37
+        "numOunces": 5
       },
       {
         "userID": 1,
@@ -57,6 +57,31 @@ describe('HydrationRepo', () => {
         "date": "2019/10/15",
         "numOunces": 45
       },
+      {
+        "userID": 42,
+        "date": "2019/06/15",
+        "numOunces": 48
+      },
+      {
+        "userID": 43,
+        "date": "2019/06/15",
+        "numOunces": 47
+      },
+      {
+        "userID": 44,
+        "date": "2019/06/15",
+        "numOunces": 44
+      },
+      {
+        "userID": 45,
+        "date": "2019/06/15",
+        "numOunces": 24
+      },
+      {
+        "userID": 46,
+        "date": "2019/06/15",
+        "numOunces": 89
+      }
     ]
     hydrationRepo = new HydrationRepo(hydrationData);
   });
@@ -67,6 +92,11 @@ describe('HydrationRepo', () => {
 
   it('should instantiate a class of Hydration', () => {
     expect(hydrationRepo).to.be.an.instanceof(HydrationRepo);
+  })
+
+  it('should calculate average all water ever imbibed by specific user', () => {
+    expect(hydrationRepo.findUserAvgDailyHydration(1)).to.equal(23);
+
   })
 
 

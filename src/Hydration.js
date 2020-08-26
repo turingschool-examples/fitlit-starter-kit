e6b0925fa48cb9c4b1cbcad295be769c488a1b15
Brigette Doelp <brigette.doelp@gmail.com>
2020-08-26 15:45:48 -0700

    Return week of hydroData based on a starting date

--- src/Hydration.js
+++ src/Hydration.js
@@ -2,14 +2,14 @@ class HydrationRepo {
   constructor(hydroData) {
     this.hydroData = hydroData;
   }
-  findUserAvgDailyHydration(userID) {
-    const singleUserEntries = this.findDataByID(userID)
-    const singleUserTotalOz = singleUserEntries.reduce((singleUserTotal, data) => {
-      singleUserTotal += data.numOunces
-      return singleUserTotal
-    }, 0)
-    return singleUserTotalOz / singleUserEntries.length
+
+  findDataByID(userID) {
+    const singleUserEntries = this.hydroData.filter(userEntry => {
+      return userID === userEntry.userID
+    });
+    return singleUserEntries
   }
+
   findDailyHydration(userID, date) {
     const singleUserEntries = this.findDataByID(userID)
     const singleDayHydration = singleUserEntries.find(userEntry => {
@@ -17,11 +17,23 @@ class HydrationRepo {
     })
     return singleDayHydration.numOunces
   }
-  findDataByID(userID) {
-    const singleUserEntries = this.hydroData.filter(userEntry => {
-      return userID === userEntry.userID
-    });
-    return singleUserEntries
+
+  findWeeklyHydration(userID, endDate) {
+    const singleUserEntries = this.findDataByID(userID)
+    const endingIndex = singleUserEntries.map(singleEntry => {
+      return singleEntry.date
+    }).indexOf(endDate);
+    const startingIndex = endingIndex - 5
+    return singleUserEntries.splice(startingIndex - 1, endingIndex - 1)
+  }
+
+  findUserAvgDailyHydration(userID) {
+    const singleUserEntries = this.findDataByID(userID)
+    const singleUserTotalOz = singleUserEntries.reduce((singleUserTotal, data) => {
+      singleUserTotal += data.numOunces
+      return singleUserTotal
+    }, 0)
+    return singleUserTotalOz / singleUserEntries.length
   }
 }
 

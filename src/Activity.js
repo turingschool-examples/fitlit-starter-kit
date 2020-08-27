43e7fa931a0f958db50a061a23dbb0929015166f
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-27 16:27:59 -0400

    Finish functionality on walkedMilesPerDay method

--- src/Activity.js
+++ src/Activity.js
@@ -1,14 +1,15 @@
 const UserRepository = require("./UserRepository")
 const userData = require("../data/users")
-userRepository = new UserRepository([userData[0],userData[1]]);
+const userRepository = new UserRepository([userData[0],userData[1]]);
 
 class Activity {
   constructor(activitySet) {
     this.activitySet = activitySet;
   }
-  walkedMilesPerDay(day,id){
-    let userStrideLength = userRepository.returnUserData(id).strideLength
-     console.log(userStrideLength);
+  walkedMilesPerDay(dateSelected, id) {
+    let userStrideLength = userRepository.returnUserData(id).strideLength;
+    let dayData = this.activitySet.find(day => day.date === dateSelected && day.userID === id);
+    return Math.round((dayData.numSteps * userStrideLength / 5280) * 10) / 10;
   }
 }
 if (typeof module !== 'undefined') {

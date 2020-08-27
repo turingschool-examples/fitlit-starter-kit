539e4f08e75443af2ef05abc79b894c0e0adede5
Hanna Kim <hannakim91@gmail.com>
2020-08-27 15:49:11 -0600

    Set up activity-test-data.js

--- test/ActivityRepo-test.js
+++ test/ActivityRepo-test.js
@@ -1,12 +1,13 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const activityData = require('../testdata/activity-test-data');
 const ActivityRepo = require('../src/ActivityRepo');
 
 describe('ActivityRepo', () => {
   let activityRepo;
   beforeEach(() => {
-    activityRepo = new ActivityRepo(hydrationData);
+    activityRepo = new ActivityRepo(activityData);
   });
 
   it('should be a function', () => {
@@ -16,5 +17,5 @@ describe('ActivityRepo', () => {
   it('should instantiate a class of Hydration', () => {
     expect(activityRepo).to.be.an.instanceof(ActivityRepo);
   });
-  
+
 });
---
+++ test/testdata/activity-test-data.js
@@ -0,0 +1,5 @@
+const activityData = [
+  
+]
+
+module.exports = activityData;

ce51bc1c519525abc73b32e41c3f949776ef179c
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-27 16:46:56 -0400

    Add helper functions and averageWeeklyMinutes method

--- src/Activity.js
+++ src/Activity.js
@@ -6,11 +6,26 @@ class Activity {
   constructor(activitySet) {
     this.activitySet = activitySet;
   }
+  getDayData(dateSelected, id) {
+    return this.activitySet.find(day => day.date === dateSelected && day.userID === id);
+  }
+  weeklySleepProperties(dateSelected, id, property) {
+    let startingDate = this.getDayData(dateSelected, id);
+    let firstDay = this.activitySet.indexOf(startingDate);
+    return this.activitySet.slice(firstDay, firstDay + 7).map(day => day[property])
+  }
   walkedMilesPerDay(dateSelected, id) {
+    let dayData = this.getDayData(dateSelected, id)
     let userStrideLength = userRepository.returnUserData(id).strideLength;
-    let dayData = this.activitySet.find(day => day.date === dateSelected && day.userID === id);
     return Math.round((dayData.numSteps * userStrideLength / 5280) * 10) / 10;
   }
+  minutesActivePerDay(dateSelected, id) {
+    return this.getDayData(dateSelected, id).minutesActive
+}
+  averageWeeklyMinutes(dateSelected, id, property) {
+    let weeklyActivity = this.weeklySleepProperties(dateSelected, id, property)
+    return Math.round((weeklyActivity.reduce((allMinutes, minute) => allMinutes + minute, 0) / 7) * 10) / 10;
+}
 }
 if (typeof module !== 'undefined') {
   module.exports = Activity;

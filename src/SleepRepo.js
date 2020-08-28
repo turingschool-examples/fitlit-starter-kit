65496f343dca420e163aee06a647ce4ce32aaaa0
Brigette Doelp <brigette.doelp@gmail.com>
2020-08-27 20:01:33 -0700

    Find average hours slept by single user

--- src/SleepRepo.js
+++ src/SleepRepo.js
@@ -1,6 +1,22 @@
 class SleepRepo {
-  constructor() {
-    
+  constructor(sleepData) {
+    this.sleepData = sleepData
+  }
+
+  findDataByID(userID) {
+    const entries = this.sleepData.filter(userEntry => {
+      return userID === userEntry.userID
+    });
+    return entries
+  }
+
+  calculateAvgSleep(userID) {
+    const entries = this.findDataByID(userID);
+    const totalSleep = entries.reduce((total, data) => {
+      total += data.hoursSlept
+      return total
+    }, 0)
+    return totalSleep / entries.length 
   }
 }
 

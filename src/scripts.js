dd676933ee3bae4c205c4c889d750b77e363f3fe
Katy St Sauveur <katyrogowski@yahoo.com>
2020-08-28 14:20:53 -0600

    Info card diplays user info

--- src/scripts.js
+++ src/scripts.js
@@ -1,15 +1,14 @@
 
 
 
-// console.log("Hello World");
-// console.log("DATA", userData)
-// console.log("A", Activiy)
 
-// window.addEventListener('load', onLoad);
 
-// function onLoad() {
-//   chooseRandomUser();
-// }
+window.addEventListener('load', onLoad);
+
+function onLoad() {
+  chooseRandomUser();
+  displayUserInfo();
+}
 
 function chooseRandomUser() {
   const randomUser = Math.floor(Math.random() * userData.length)
@@ -19,12 +18,35 @@ function chooseRandomUser() {
   greeting.innerHTML = `Welcome, ${user.returnFristName()}!`
 }
 
-chooseRandomUser();
 
 
+function displayUserInfo() {
+  // let infoCard = document.querySelector('.user-info-card')
+  // console.log(infoCard)
+  // infoCard.innerHTML =
+  // console.log(user)
+  let userAddress = document.querySelector('.user-address')
+  userAddress.innerHTML += `${user.userData.address}`
+  let userEmail = document.querySelector('.user-email')
+  userEmail.innerHTML += `${user.userData.email}`
+  let userStride = document.querySelector('.user-stride')
+  userStride.innerHTML += `${user.userData.strideLength}`
+  let userStepGoal = document.querySelector('.user-step-goal')
+  userStepGoal.innerHTML += `${user.userData.dailyStepGoal}`
+  let userFriends = document.querySelector('.user-friends')
+  userAddress.innerHTML += `${user.userData.friends}`
+  console.log(user.userData.friends)
+}
 
 
-// Create an info card on the dashboard with all of user’s info on the page--want it to display name, address, email, strideLength & stepgoal
+// <h3 class="user-address">Address:</h3>
+// <h3 class="user-email">Email:</h3>
+// <h3 class="user-stride">Stride:</h3>
+// <h3 class="user-step-goal">Steps:</h3>
+// <h3 class="user-friends">Friends:</h3>
+
+
+// Create an info card on the dashboard with all of user’s info on the page--want it to display name, address, email, strideLength & stepgoal, friends
 
 // Display their first name somewhere prominently on the page to welcome them
 

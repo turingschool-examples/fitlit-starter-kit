77110473fbab4065bd35a43b2212453893268607
Michael Walker <Michael.e.walker.87@gmail.com>
2020-08-30 09:40:51 -0600

    Add Activity

--- src/scripts.js
+++ src/scripts.js
@@ -14,6 +14,9 @@ function onLoad() {
   displayDailySleep();
   displayWeeklySleep();
   allTimeSleep();
+  displayDayActivity();
+  displayWeeklyActivity();
+  compareDayActivity();
 }
 
 function chooseRandomUser() {
@@ -94,10 +97,10 @@ function displayWeeklySleep() {
   sleepWeekly.innerHTML +=
   `<h2>Sleep Data For The Week</h2>
   <p> Weekly sleep data:
-  Weekly Hours Slept
-  ${sleep.weeklySleepProperties("2019/06/15", user.userData.id, "hoursSlept")}
-  Weekly Sleep Quality
-  ${sleep.weeklySleepProperties("2019/06/15", user.userData.id, "sleepQuality")}
+    Weekly Hours Slept
+    ${sleep.weeklySleepProperties("2019/06/15", user.userData.id, "hoursSlept")}
+    Weekly Sleep Quality
+    ${sleep.weeklySleepProperties("2019/06/15", user.userData.id, "sleepQuality")}
   </p>
   `
 }
@@ -107,18 +110,63 @@ function allTimeSleep() {
   sleepAllTime.innerHTML +=
   `<h2>Sleep Data For All Time</h2>
   <p> All time sleep data:
-  All time Average Hours Slept
-  ${sleep.averageAllTimeSleep(user.userData.id, "hoursSlept")}
-  All time Average Sleep Quality
-  ${sleep.averageAllTimeSleep(user.userData.id, "sleepQuality")}
+    All time Average Hours Slept
+    ${sleep.averageAllTimeSleep(user.userData.id, "hoursSlept")}
+    All time Average Sleep Quality
+    ${sleep.averageAllTimeSleep(user.userData.id, "sleepQuality")}
   </p>
   `
 }
 
+function displayDayActivity() {
+  activity = new Activity(activityData);
+  let dayActivity = document.querySelector('.day-activity-card')
+  dayActivity.innerHTML +=
+  `<h2>Activity Data For The Day</h2>
+  <p> Daily Activity Data:
+    Today's step data
+    ${activity.getDayData("2019/06/15", user.userData.id).numSteps}
+    Today's mintues active data
+    ${activity.getDayData("2019/06/15", user.userData.id).minutesActive}
+    Today's distance walked data
+    ${activity.walkedMilesPerDay("2019/06/15", user.userData.id)}
+  </p>
+  `
+}
 
+function displayWeeklyActivity() {
+  let weeklyActivity = document.querySelector('.weekly-activity-card')
+  weeklyActivity.innerHTML +=
+  `<h2>Activity Data For The Week</h2>
+  <p> Weekly Activity Data:
+    Weekly step data
+    ${activity.weeklyActivityProperties("2019/06/15", user.userData.id, 'numSteps')}
+    Weekly mintues active data
+    ${activity.weeklyActivityProperties("2019/06/15", user.userData.id, 'minutesActive')}
+    Weekly flights of stairs climbed data
+    ${activity.weeklyActivityProperties("2019/06/15", user.userData.id, 'flightsOfStairs')}
+  </p>
+  `
+}
 
-
-
+function compareDayActivity() {
+  let compareDayActivity = document.querySelector('.comparison-activity-card')
+  let activityFindAllUsers = activity.findDayActivity("2019/06/15")
+  compareDayActivity .innerHTML +=
+  `<h2>Activity Data For The Day Compared To All Users</h2>
+  <p> Daily Activity Data Comparison:
+    Average step data compared to all users
+    Your Average ${activity.averageWeeklyMinutes("2019/06/15", user.userData.id, "numSteps")}
+    All Users Average ${activityFindAllUsers.numSteps}</br>
+    Average mintues active data compared to all users
+    Your Average ${activity.averageWeeklyMinutes("2019/06/15", user.userData.id, "minutesActive")}
+    All Users Average ${activityFindAllUsers.minutesActive}</br>
+    Average flights of stairs climbed data compared to all users
+    Your Average ${activity.averageWeeklyMinutes("2019/06/15", user.userData.id, "flightsOfStairs")}
+    All Users Average ${activityFindAllUsers.flightsOfStairs}
+  </p>
+  `
+}
 
 // Create an info card on the dashboard with all of user’s info on the page--want it to display name, address, email, strideLength & stepgoal, friends
 

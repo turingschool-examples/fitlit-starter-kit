ed21c1fe0b8e5f44ee5997878631c5a344618cfb
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-30 17:15:51 -0400

    Adjust test suite for refactored methods

--- test/HydrationRepository-test.js
+++ test/HydrationRepository-test.js
@@ -2,8 +2,8 @@ const chai = require("chai");
 const expect = chai.expect;
 const HydrationRepository = require("../src/HydrationRepository")
 
-describe("HydrationRepository", () => {
-  let sampleHydrationData, hydrationRepository, filterUser1, filterUser2, user1Data, weeklyData;
+describe.only("HydrationRepository", () => {
+  let sampleHydrationData, hydrationRepository, filterUser1, filterUser2, weeklyData;
   beforeEach(() => {
     sampleHydrationData = [ {
       "userID": 1,
@@ -21,24 +21,24 @@ describe("HydrationRepository", () => {
       "numOunces": 96
     },
     {
-       "userID": 1,
-       "date": "2019/06/18",
-       "numOunces": 61
+      "userID": 1,
+      "date": "2019/06/18",
+      "numOunces": 61
     },
     {
-        "userID": 1,
-        "date": "2019/06/19",
-        "numOunces": 91
+      "userID": 1,
+      "date": "2019/06/19",
+      "numOunces": 91
     },
     {
-        "userID": 1,
-        "date": "2019/06/20",
-        "numOunces": 50
+      "userID": 1,
+      "date": "2019/06/20",
+      "numOunces": 50
     },
     {
-        "userID": 1,
-        "date": "2019/06/21",
-        "numOunces": 50
+      "userID": 1,
+      "date": "2019/06/21",
+      "numOunces": 50
     },
     {
       "userID": 2,
@@ -56,31 +56,10 @@ describe("HydrationRepository", () => {
       "numOunces": 96
     },
     ]
-    filterUser1 = [
-      sampleHydrationData[0],
-      sampleHydrationData[1],
-      sampleHydrationData[2],
-      sampleHydrationData[3],
-      sampleHydrationData[4],
-      sampleHydrationData[5],
-      sampleHydrationData[6]
-    ]
-    filterUser2 = [
-      sampleHydrationData[7],
-      sampleHydrationData[8],
-      sampleHydrationData[9]
-    ]
-    weeklyData = [
-      sampleHydrationData[0].numOunces,
-      sampleHydrationData[1].numOunces,
-      sampleHydrationData[2].numOunces,
-      sampleHydrationData[3].numOunces,
-      sampleHydrationData[4].numOunces,
-      sampleHydrationData[5].numOunces,
-      sampleHydrationData[6].numOunces
-    ]
+    filterUser1 = sampleHydrationData.slice(0, 7);
+    filterUser2 = sampleHydrationData.slice(7, 10);
+    weeklyData = { date: sampleHydrationData[0].date, ounces: sampleHydrationData[0].numOunces}
     hydrationRepository = new HydrationRepository(sampleHydrationData);
-    user1Data = hydrationRepository.userHydrationData(sampleHydrationData, 1);
   });
 
   it("should be a function", () => {
@@ -96,7 +75,6 @@ describe("HydrationRepository", () => {
   });
 
   it("should store another users data", () => {
-    user2Data = hydrationRepository.userHydrationData(sampleHydrationData, 2);
     expect(hydrationRepository.userHydrationData(2)).to.be.deep.equal(filterUser2);
   });
 
@@ -105,11 +83,13 @@ describe("HydrationRepository", () => {
   });
 
   it("should should return ounces for a specified day", () =>{
-    expect(hydrationRepository.dayOunces("2019/06/16")).to.equal(69)
+    expect(hydrationRepository.dayOunces("2019/06/16", 1)).to.equal(69)
   });
 
   it("should return daily ounces over 7 day period", () => {
-    expect(hydrationRepository.dailyOuncesPerGivenWeek("2019/06/15")).to.deep.equal(weeklyData)
+    expect(hydrationRepository.dailyOuncesPerGivenWeek("2019/06/15", 1)[0]).to.deep.equal(weeklyData)
   });
+
+  
 
 });

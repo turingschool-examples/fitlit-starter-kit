3243b57203152e4cdf3caad0cb1c21fe78e4a134
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-30 17:23:04 -0400

    Adjust styling

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -2,22 +2,17 @@
 class UserRepository {
   constructor(userData) {
     this.userData = userData;
-    // console.log('2', userData)
-    //gives back an array of three user objects
   }
-  returnUserData(id){
+  returnUserData(id) {
     return this.userData.find(user => user.id === id);
   }
-  getAvgStepGoal(){
+  getAvgStepGoal() {
     return this.userData.reduce((allAvgSteps, user) => {
       return allAvgSteps += user.dailyStepGoal/this.userData.length
-    },0);
+    }, 0);
   }
   returnFriendFullName(users) {
-    console.log(users)
-    console.log(this.userData.friends)
     let test = users.map(friendId => this.returnUserData(friendId).name);
-    console.log(test)
     return test
   }
 }

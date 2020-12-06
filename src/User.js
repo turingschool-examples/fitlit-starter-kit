0dfc9d4554697fdcac68b6ddfe39893b4cc520e2
Tashia Davis <tashiadavis10@gmail.com>
2020-12-06 10:06:24 -0700

    Add User and UserRepository class outlines

---
+++ src/User.js
@@ -0,0 +1,19 @@
+class User {
+  // It should have a parameter to take in a userData object
+  constructor(userData) {
+    this.id = userData.id;
+    this.name = userData.name;
+    this.address = userData.address;
+    this.email = userData.email;
+    this.strideLength = userData.dailyStepGoal;
+    this.friends = userData.friends;
+  }
+
+  returnFirstName() { // Return a user’s first name only
+    // return this.name // access f name only
+  }
+}
+
+
+// Each user holds on to the user properties from the data file
+// const user = new User(userData[5]) // comes from returnUserData method in UserRepository class
---
+++ src/UserRepository.js
@@ -0,0 +1,25 @@
+class UserRepository {
+  constructor(data) { // It should have a parameter to take in user data
+    // this.data = data; ????
+  }
+
+  returnUserData(user.id) { // Given a user’s ID, what is their user data?
+    // user.find(user.id => user)
+  }
+
+  calculateAverageStepGoal() { // The average step goal amongst all users
+    // userData.dailyStepGoal.reduce(acc, sum) => return acc + sum / userData.length;
+  }
+}
+
+
+
+// UserRepository class
+// new UserRepository(data);
+// A UserRepository holds onto all of the User objects
+//
+//
+// const cards = prototypeQuestions.map(cardIndex => {
+//   const card = new Card(cardIndex.id, cardIndex.question, cardIndex.answers, cardIndex.correctAnswer);
+//   return card;
+// });

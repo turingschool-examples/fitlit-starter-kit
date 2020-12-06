7fbd2d5317ad98e5429b70cb8b7e779e3bbc54c1
Tashia Davis <tashiadavis10@gmail.com>
2020-12-06 11:38:17 -0700

    Pass User property tests

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -16,18 +16,6 @@ class UserRepository {
 
 }
 
-
-
-// UserRepository class
-// new UserRepository(data);
-// A UserRepository holds onto all of the User objects
-//
-//
-// const cards = prototypeQuestions.map(cardIndex => {
-//   const card = new Card(cardIndex.id, cardIndex.question, cardIndex.answers, cardIndex.correctAnswer);
-//   return card;
-// });
-
 if (typeof module !== 'undefined') {
   module.exports = UserRepository;
 }
--- test/User-test.js
+++ test/User-test.js
@@ -1,48 +1,54 @@
 const chai = require('chai');
 const expect = chai.expect;
+const testData = require('../data/test-data');
+const userTestData = testData.testUsers;
 const User = require('../src/User');
+const UserRepository = require('../src/UserRepository');
 
 describe('User', () => {
-  let user;
+  let allUserData, user1, user2, user3;
 
   beforeEach(() => {
-    user = new User(userData);
+    allUserData = new UserRepository(userTestData);
+    user1 = new User(allUserData.data[0]);
+    user2 = new User(allUserData.data[1]);
+    user3 = new User(allUserData.data[2]);
   })
 
-  it.skip('should be a function', () => {
+  it('should be a function', () => {
     expect(User).to.be.a('function');
   })
 
-  it.skip('should be an instance of User', () => {
-    expect(user).to.be.an.instanceof(User);
+  it('should be an instance of User', () => {
+    expect(user1).to.be.an.instanceof(User);
   })
 
-  it.skip('should have an id', () => {
-    expect(user.id).to.equal();
+  it('should have an id', () => {
+    expect(user1.id).to.equal(1);
   })
 
-  it.skip('should have a name', () => {
-    expect(user.name).to.equal();
+  it('should have a name', () => {
+    expect(user2.name).to.equal('Tashia Davis');
   })
 
-  it.skip('should have an address', () => {
-    expect(user.address).to.equal();
+  it('should have an address', () => {
+    expect(user3.address).to.equal('28476 Turing Dr, Denver CO 357687');
   })
 
-  it.skip('should have an email', () => {
-    expect(user.email).to.equal();
+  it('should have an email', () => {
+    expect(user1.email).to.equal('gonzagasucks@hotmail.com');
   })
 
-  it.skip('should have a stride length', () => {
-    expect(user.strideLength).to.equal();
+  it('should have a stride length', () => {
+    expect(user2.strideLength).to.equal(1.2);
   })
 
-  it.skip('should have a daily step goal', () => {
-    expect(user.dailyStepGoal).to.equal();
+  it('should have a daily step goal', () => {
+    expect(user3.dailyStepGoal).to.equal(8520);
   })
 
-  it.skip('should have some friends', () => {
-    expect(user.friends).to.deep.equal();
+  it('should have some friends', () => {
+    expect(user1.friends).to.deep.equal([2, 3]);
   })
 
   it.skip('should return a users first name', () => {

8a81870bbaf10aaefc40297187ce01a7f25a5413
Tashia Davis <tashiadavis10@gmail.com>
2020-12-06 10:31:55 -0700

    Create test data file

---
+++ data/test-data.js
@@ -0,0 +1,38 @@
+const testData = [
+  {
+    "id": 1,
+    "name": "Cole Fiscus",
+    "address": "7362 Gonzaga Blvd, Spokane WA 19982",
+    "email": "gonzagasucks@hotmail.com",
+    "strideLength": 5.8,
+    "dailyStepGoal": 400,
+    "friends": [
+      2,
+      3
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Tashia Davis",
+    "address": "3748 Oregon Ave, Eugene OR 12983",
+    "email": "skoducks@gmail.com",
+    "strideLength": 1.2,
+    "dailyStepGoal": 15000,
+    "friends": [
+      1,
+      3
+    ]
+  },
+  {
+    "id": 3,
+    "name": "Kayla Skannah",
+    "address": "28476 Turing Dr, Denver CO 357687",
+    "email": "skannah@yahoo.com",
+    "strideLength": 3.2,
+    "dailyStepGoal": 8520,
+    "friends": [
+      1,
+      2
+    ]
+  }
+];

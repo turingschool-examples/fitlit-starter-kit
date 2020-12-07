f291b6cea5cec4399efd228f5a5ca880dd269eb4
srslie <aliceruppert@gmail.com>
2020-12-07 03:00:38 -0700

    Use smaller sample because users.js isn't importing properly

--- tests/UserRepository-test.js
+++ tests/UserRepository-test.js
@@ -1,14 +1,83 @@
 const chai = require('chai');
 const expect = chai.expect;
-const userData = require('../data/users.js')
 const UserRepository = require('../src/UserRepository');
 const User = require('../src/User');
 
 describe('UserRespository', function() {
-  let userRepository;
+  let userRepository, user1;
 
   beforeEach(function() {
-    userRepository = new UserRepository(userData);
+    userRepository = new UserRepository([
+      {
+        "id": 1,
+        "name": "Luisa Hane",
+        "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+        "email": "Diana.Hayes1@hotmail.com",
+        "strideLength": 4.3,
+        "dailyStepGoal": 10000,
+        "friends": [
+          16,
+          4,
+          8
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Jarvis Considine",
+        "address": "30086 Kathryn Port, Ciceroland NE 07273",
+        "email": "Dimitri.Bechtelar11@gmail.com",
+        "strideLength": 4.5,
+        "dailyStepGoal": 5000,
+        "friends": [
+          9,
+          18,
+          24,
+          19
+        ]
+      },
+      {
+        "id": 3,
+        "name": "Herminia Witting",
+        "address": "85823 Bosco Fork, East Oscarstad MI 85126-5660",
+        "email": "Elwin.Tromp@yahoo.com",
+        "strideLength": 4.4,
+        "dailyStepGoal": 5000,
+        "friends": [
+          19,
+          11,
+          42,
+          33
+        ]
+      },
+      {
+        "id": 4,
+        "name": "Mae Connelly",
+        "address": "28926 Schinner Islands, Turnermouth NE 23720-3230",
+        "email": "Marcos_Pollich@hotmail.com",
+        "strideLength": 3.1,
+        "dailyStepGoal": 4000,
+        "friends": [
+          48,
+          7,
+          44,
+          8
+        ]
+      },
+      {
+        "id": 5,
+        "name": "Erick Schaden",
+        "address": "514 Mayert Walk, Jordaneside SC 55023-6523",
+        "email": "Vanessa_Gerhold@gmail.com",
+        "strideLength": 3.1,
+        "dailyStepGoal": 8000,
+        "friends": [
+          13,
+          44,
+          49,
+          33,
+          10
+        ]
+      }]);
     user1 = new User({
         "id": 1,
         "name": "Luisa Hane",
@@ -33,16 +102,16 @@ describe('UserRespository', function() {
   });
 
   it('should hold all User objects', function() {
-    expect(userRepository.users.length).to.equal(50);
+    expect(userRepository.users.length).to.equal(5);
     expect(userRepository.users[0]).to.be.an.instanceof(User);
   });
 
   it('should return a user data given an id', function() {
-    expect(userRepository.getUserData(1)).to.deeply.equal(user1)
+    expect(userRepository.getUserData(1)).to.deep.equal(user1)
   });
 
   it('should return average step goal amongst all users', function() {
-    expect(userRepository.findAverageStepGoal()).to.equal(6700)
+    expect(userRepository.findAverageStepGoal()).to.equal(6400)
   })
 
 });

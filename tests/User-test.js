5b0ff063e886bd7e17b8faaae88585b70f060741
srslie <aliceruppert@gmail.com>
2020-12-07 00:56:33 -0700

    Add tests for function and instance of User

--- tests/User-test.js
+++ tests/User-test.js
@@ -21,3 +21,11 @@ describe('User', function() {
       })
   });
 
+  it('should be a function', function() {
+    expect(User).to.be.a('function');
+  });
+
+  it('should be an instance of User', function() {
+    expect(user).to.be.an.instanceof(User);
+  });
+

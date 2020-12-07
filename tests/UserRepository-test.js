b01f54466cc986fa688d575753d37a3a7d53d980
srslie <aliceruppert@gmail.com>
2020-12-07 01:55:46 -0700

    Create mock user1 User for tests

--- tests/UserRepository-test.js
+++ tests/UserRepository-test.js
@@ -8,6 +8,19 @@ describe('UserRespository', function() {
 
   beforeEach(function()) {
     userRepository = new UserRepository(userData);
+    user1 = new User({
+        "id": 1,
+        "name": "Luisa Hane",
+        "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+        "email": "Diana.Hayes1@hotmail.com",
+        "strideLength": 4.3,
+        "dailyStepGoal": 10000,
+        "friends": [
+          16,
+          4,
+          8
+        ]
+      });
   });
 
   it('should be a function', function() {

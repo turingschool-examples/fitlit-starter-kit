5164e4aa63bf66c052f8983fd4fa5dd51bf654bd
srslie <aliceruppert@gmail.com>
2020-12-07 00:56:53 -0700

    Add tests for properties

--- tests/User-test.js
+++ tests/User-test.js
@@ -29,3 +29,13 @@ describe('User', function() {
     expect(user).to.be.an.instanceof(User);
   });
 
+  it('should have all properties', function() {
+    expect(user.userID).to.equal(1)
+    expect(user.name).to.equal('Luisa Hane')
+    expect(user.address).to.equal('15195 Nakia Tunnel, Erdmanport VA 19901-1697')
+    expect(user.email).to.equal('Diana.Hayes1@hotmail.com')
+    expect(user.strideLength).to.equal(4.3)
+    expect(user.dailyStepGoal).to.equal(10000)
+    expect(user.friends).to.deep.equal([16, 4, 8])
+  });
+

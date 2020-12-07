b6f0c549bd298d6a8ad31ac23b83f6236f1515eb
srslie <aliceruppert@gmail.com>
2020-12-07 00:58:37 -0700

    Add User properties

--- src/User.js
+++ src/User.js
@@ -1,1 +1,11 @@
+class User {
+  constructor(userData) {
+    this.userID = userData.id
+    this.name = userData.name
+    this.address = userData.address
+    this.email = userData.email
+    this.strideLength = userData.strideLength
+    this.dailyStepGoal = userData.dailyStepGoal
+    this.friends = userData.friends
+  }
 module.exports = User;

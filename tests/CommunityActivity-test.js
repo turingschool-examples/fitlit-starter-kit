3ab1327446725f84ed8d8cb9a569e89f5a16f1be
srslie <aliceruppert@gmail.com>
2020-12-07 04:05:32 -0700

    Add test to check for Activity objects in the community

--- tests/CommunityActivity-test.js
+++ tests/CommunityActivity-test.js
@@ -1,46 +1,69 @@
 const chai = require('chai');
 const expect = chai.expect;
 const CommunityActivity = require('../src/CommunityActivity');
+const Activity = require('../src/Activity');
 
 describe('CommunityActivity', function() {
-  let communityActivity;
+  let communityActivity, activity;
 
   beforeEach(function() {
     communityActivity = new CommunityActivity([
-      { "userID": 1,
+      {
+        "userID": 1,
+        "date": "2019/06/15",
+        "numSteps": 3577,
+        "minutesActive": 140,
+        "flightsOfStairs": 16
+      },
+      {
+        "userID": 2,
+        "date": "2019/06/15",
+        "numSteps": 4294,
+        "minutesActive": 138,
+        "flightsOfStairs": 10
+      },
+      {
+        "userID": 3,
+        "date": "2019/06/15",
+        "numSteps": 7402,
+        "minutesActive": 116,
+        "flightsOfStairs": 33
+      },
+      {
+        "userID": 4,
+        "date": "2019/06/15",
+        "numSteps": 3486,
+        "minutesActive": 114,
+        "flightsOfStairs": 32
+      },
+      {
+         "userID": 5,
+        "date": "2019/06/15",
+        "numSteps": 11374,
+        "minutesActive": 213,
+        "flightsOfStairs": 13
+      }
+    ]);
+    activity = new Activity({
+      "userID": 1,
       "date": "2019/06/15",
       "numSteps": 3577,
       "minutesActive": 140,
-      "flightsOfStairs": 16 },
-    { "userID": 2,
-      "date": "2019/06/15",
-      "numSteps": 4294,
-      "minutesActive": 138,
-      "flightsOfStairs": 10 },
-    { "userID": 3,
-      "date": "2019/06/15",
-      "numSteps": 7402,
-      "minutesActive": 116,
-      "flightsOfStairs": 33 },
-    { "userID": 4,
-      "date": "2019/06/15",
-      "numSteps": 3486,
-      "minutesActive": 114,
-      "flightsOfStairs": 32 },
-    { "userID": 5,
-      "date": "2019/06/15",
-      "numSteps": 11374,
-      "minutesActive": 213,
-      "flightsOfStairs": 13 }
-    ]);
+      "flightsOfStairs": 16
+     });
   });
 
   it('should be a function', function() {
     expect(CommunityActivity).to.be.a('function');
-  })
+  });
 
   it('should be an instance of a CommunityActivity', function() {
     expect(communityActivity).to.be.an.instanceof(CommunityActivity)
+  });
+
+  if('should hold Activity objects for all users', function() {
+    expect(communityActivity.activities.length).to.equal(5)
+    expect(communityActivity.activities[0]).to.deep.equal(activity)
   })
 
   it('should return community average stairs climbed on a specific date', function() {

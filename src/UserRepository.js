643a7c312a2ba65e8323d52fcd912f59c9d3495e
srslie <aliceruppert@gmail.com>
2020-12-07 03:01:33 -0700

    Set up User requirements, constructor to instiate new Users

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,1 +1,9 @@
+const User = require('./User')
+const userData = require('../data/users.js')
+
+class UserRepository {
+  constructor(data = []) {
+    this.users = data.map(user => new User(user))
+  }
+
 module.exports = UserRepository;

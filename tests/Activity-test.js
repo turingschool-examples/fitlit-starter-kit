1649ee5f441acc47dc876237be8102fd86438431
srslie <aliceruppert@gmail.com>
2020-12-07 03:49:05 -0700

    Set up beforeEach for test

--- tests/Activity-test.js
+++ tests/Activity-test.js
@@ -2,3 +2,16 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Activity = require('../src/Activity');
+
+describe('Activity', function() {
+  let activity;
+
+  beforeEach(function() {
+    activity = new Activity({
+      "userID": 1,
+      "date": "2019/06/15",
+      "numSteps": 3577,
+      "minutesActive": 140,
+      "flightsOfStairs": 16
+    });
+  });

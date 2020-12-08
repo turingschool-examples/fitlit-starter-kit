b42e680e3fb6d6be26a73e91458be873bf130cf1
Tashia Davis <tashiadavis10@gmail.com>
2020-12-08 14:52:09 -0700

    Create Hydration test data file

---
+++ data/hydration-test-data.js
@@ -0,0 +1,42 @@
+const hydrationData = [
+  {
+    "userID": 12,
+    "date": "2019/06/15",
+    "numOunces": 37
+  },
+  {
+    "userID": 12,
+    "date": "2019/06/16",
+    "numOunces": 75
+  },
+  {
+    "userID": 12,
+    "date": "2019/06/17",
+    "numOunces": 47
+  },
+  {
+    "userID": 12,
+    "date": "2019/06/18",
+    "numOunces": 85
+  },
+  {
+    "userID": 12,
+    "date": "2019/06/19",
+    "numOunces": 42
+  },
+  {
+    "userID": 12,
+    "date": "2019/06/20",
+    "numOunces": 87
+  },
+  {
+    "userID": 12,
+    "date": "2019/06/21",
+    "numOunces": 94
+  },
+  {
+    "userID": 12,
+    "date": "2019/06/22",
+    "numOunces": 84
+  }
+];

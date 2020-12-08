22afa51e8994536fc1f4fbbda84afea448b18ce0
srslie <aliceruppert@gmail.com>
2020-12-07 22:50:04 -0700

    Create the community UserRepository and the user

--- src/scripts.js
+++ src/scripts.js
@@ -8,3 +8,10 @@ let user = null
 
 window.addEventListener('load', loadPage)
 
+//this could just be done on lines 5 and 6,
+//but if we want to ever load from local storage...
+const createCommunity = () => {
+  community = new UserRepository(userData)
+  user = community.users[0]
+}
+

77d0ab3517e169067426f3892d33b1a4ec78426b
Tashia Davis <tashiadavis10@gmail.com>
2020-12-08 14:12:36 -0700

    Add User Info Card, calculate avg daily step goal to dashboard

--- src/scripts.js
+++ src/scripts.js
@@ -1,18 +1,30 @@
 // ~~~~~ QUERY SELECTORS ~~~~~
 const userNameGreeting = document.querySelector(".user-greeting");
+const userInfoCard = document.querySelector(".user-info-card");
 
 // ~~~~~ EVENT LISTENERS ~~~~~
 window.onload = start;
 
 // ~~~~~ GLOBAL VARIABLES ~~~~~
-
+const users = userData.map(userObject => {
+  const user = new User(userObject);
+  return user;
+});
+const userRepository = new UserRepository(users);
 
 // ~~~~~ FUNCTIONS ~~~~~
 function start() {
-  const users = userData.map(userObject => {
-    const user = new User(userObject);
-    return user;
-  });
-  const userRepository = new UserRepository(users);
-  userNameGreeting.innerText = `Hello, ${users[5].returnFirstName()}`
+  userNameGreeting.innerText = `Hello, ${users[5].returnFirstName()}`;
+  displayUserInfoCard(users[5]);
+}
+
+function displayUserInfoCard(user) {
+  userInfoCard.innerHTML = `
+    <p>User ID: ${user.id}</p>
+    <p>Name: ${user.name}</p>
+    <p>Email: ${user.email}</p>
+    <p>Stride length: ${user.strideLength}</p>
+    <p>Friends: ${user.friends}</p>
+    <p>Daily Step Goal: ${user.dailyStepGoal}</p>
+    <p>Avg User Step Goal: ${userRepository.calculateAverageStepGoal()}</p>`;
 }

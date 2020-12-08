5bc7bb04d81fa51a1671421b5acda2be5c6be910
Tashia Davis <tashiadavis10@gmail.com>
2020-12-08 15:58:55 -0700

    Set up Hydration test beforeEach

--- data/hydration-test-data.js
+++ data/hydration-test-data.js
@@ -1,42 +1,87 @@
-const testHydration = [
-  {
-    "userID": 12,
+const testHydration = [{
+    "userID": 1,
     "date": "2019/06/15",
-    "numOunces": 37
+    "numOunces": 27
   },
   {
-    "userID": 12,
+    "userID": 1,
     "date": "2019/06/16",
     "numOunces": 75
   },
   {
-    "userID": 12,
+    "userID": 1,
     "date": "2019/06/17",
     "numOunces": 47
   },
   {
-    "userID": 12,
+    "userID": 1,
     "date": "2019/06/18",
     "numOunces": 85
   },
   {
-    "userID": 12,
+    "userID": 1,
     "date": "2019/06/19",
     "numOunces": 42
   },
   {
-    "userID": 12,
+    "userID": 1,
     "date": "2019/06/20",
     "numOunces": 87
   },
   {
-    "userID": 12,
+    "userID": 1,
     "date": "2019/06/21",
     "numOunces": 94
   },
   {
-    "userID": 12,
+    "userID": 1,
     "date": "2019/06/22",
     "numOunces": 84
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "numOunces": 45
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/16",
+    "numOunces": 22
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/17",
+    "numOunces": 67
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/18",
+    "numOunces": 62
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/19",
+    "numOunces": 78
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/20",
+    "numOunces": 1
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/21",
+    "numOunces": 90
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/22",
+    "numOunces": 28
   }
 ];
+
+if (typeof module !== 'undefined') {
+  module.exports = {
+    testHydration
+  }
+}
--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -1,29 +1,43 @@
 const chai = require('chai');
 const expect = chai.expect;
+const testData = require('../data/user-test-data');
+const userTestData = testData.testUsers;
 const hydrationTestDataFile = require('../data/hydration-test-data');
 const hydrationTestDataArray = hydrationTestDataFile.testHydration;
 const Hydration = require('../src/Hydration');
+const User = require('../src/User');
 
 describe('Hydration', () => {
-  // let users;
+  let users, hydrationObjects1, hydrationObjects2, hydration1, hydration2;
 
   beforeEach(() => {
-    // users = userTestData.map(userObject => {
-    //   const user = new User(userObject);
-    //   return user;
-    // });
+    users = userTestData.map(userObject => {
+      const user = new User(userObject);
+      return user;
+    });
+    hydrationObjects1 = hydrationTestDataArray.filter(hydrationObject => {
+      hydrationObject.id === users[0].id; // is this working?
+    });
+    hydration1 = new Hydration(hydrationObjects1);
+    hydrationObjects2 = hydrationTestDataArray.filter(hydrationObject => {
+      hydrationObject.id === users[1].id;
+    });
+    hydration2 = new Hydration(hydrationObjects2);
   })
 
   it('should be a function', () => {
     expect(Hydration).to.be.a('function');
   })
 
-  // it('should be an instance of Hydration', () => {
-  //   expect(users[0]).to.be.an.instanceof(User);
-  // })
-  //
-  // it('should return a users first name', () => {
-  //   expect(users[1].returnFirstName()).to.equal('Tashia');
-  // })
+  it('should be an instance of Hydration', () => {
+    expect(hydration1).to.be.an.instanceof(Hydration);
+  })
+
+  it.skip('should contain an array of hydration objects with the same id', () => {
+    // RETURNING EMPTY ARRAYS
+    console.log(hydration1.hydrationObjects)
+    console.log(hydrationObjects1)
+    expect(hydration1.hydrationObjects).to.equal(hydrationObjects1);
+  })
 
 })

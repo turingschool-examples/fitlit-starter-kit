cec31694fe372b0adf3f1f4de874fc8f512be129
srslie <aliceruppert@gmail.com>
2020-12-07 22:49:03 -0700

    Add global variables and querySelectors

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+const userMain = document.querySelector('.userMain')
+const userProfile = document.querySelector('.userProfile')
+const friends = document.querySelector('.friends')
+
+//since multiple classes will need these, global
+let community = null
+let user = null
+

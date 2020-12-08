3b3be701d16983b688489b5efd13a31cb78d0cef
Tashia Davis <tashiadavis10@gmail.com>
2020-12-08 13:55:07 -0700

    Create dashboard first name greeting

--- src/scripts.js
+++ src/scripts.js
@@ -1,14 +1,18 @@
 // ~~~~~ QUERY SELECTORS ~~~~~
-const userNameGreeting = document.queryselector(".user-greeting");
+const userNameGreeting = document.querySelector(".user-greeting");
 
 // ~~~~~ EVENT LISTENERS ~~~~~
 window.onload = start;
 
 // ~~~~~ GLOBAL VARIABLES ~~~~~
 
-const userRepository = new UserRepository(?)
 
 // ~~~~~ FUNCTIONS ~~~~~
 function start() {
-
+  const users = userData.map(userObject => {
+    const user = new User(userObject);
+    return user;
+  });
+  const userRepository = new UserRepository(users);
+  userNameGreeting.innerText = `Hello, ${users[5].returnFirstName()}`
 }

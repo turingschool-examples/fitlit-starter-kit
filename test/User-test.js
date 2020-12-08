31679b4094a4a4258d873f679abb19ec5038d8dc
mainlyetcetera <ericcampbell294@gmail.com>
2020-12-08 15:11:42 -0700

    Add data for user test

--- test/User-test.js
+++ test/User-test.js
@@ -4,5 +4,22 @@ const expect = chai.expect;
 const User = require('../src/User');
 
 describe('User', () => {
-  
+  let data, user;
+
+  beforeEach(() => {
+    data = {
+      "id": 1,
+      "name": "Brian Forbes",
+      "address": "123 Blah St, Denver CO, 66666",
+      "email": "stuffandthings@gmail.com",
+      "strideLength": 2.4,
+      "dailyStepGoal": 10000,
+      "friends": [
+        2,
+        3,
+        4
+      ]
+    };
+
+  });    
 });

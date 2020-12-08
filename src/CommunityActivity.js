093815fa6708955f75a296d59f9ee06f4401a559
srslie <aliceruppert@gmail.com>
2020-12-07 21:16:27 -0700

    Create CommunityActivity class skeleton

--- src/CommunityActivity.js
+++ src/CommunityActivity.js
@@ -1,1 +1,23 @@
+const activityData = require(../data/activity)
+
+class CommunityActivity {
+  constructor(userRespository) {
+    
+  }
+
+  findAverageSteps(date) {
+
+  }
+
+  findAverageStairs(date) {
+
+  }
+
+  findAverageActiveMinutes(date) {
+
+  }
+}
+
+
+
 module.exports = CommunityActivity

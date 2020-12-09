f49d770583b1acf7ec311bf7ee22c5b6387fcd35
conconartist <conconart.info@gmail.com>
2020-12-08 19:09:38 -0700

    Add instances to Hydration test

--- tests/Hydration-test.js
+++ tests/Hydration-test.js
@@ -3,11 +3,14 @@ const expect = chai.expect;
 const Hydration = require('../src/Hydration');
 const User = require('../src/User');
 describe('Hydration', () => {
-  let hydration, user1, user2;
+  let hydration1, hydration2;
+  let user1, user2;
 
   beforeEach(() => {
-    hydration = new Hydration(
-    )
+    hydration1 = new Hydration(2, "2019/04/15", 25);
+    hydration2 = new Hydration(15, "2019/02/25", 0);
+    hydration3 = new Hydration(20, "2019/12/02", 100);
   })
+  
   it()
 })

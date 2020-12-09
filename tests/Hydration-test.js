223e9fb21aed87b8fb9d6387398324bdbde2fecd
conconartist <conconart.info@gmail.com>
2020-12-08 18:52:36 -0700

    Setup framework for Hydration class

---
+++ tests/Hydration-test.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Hydration = require('../src/Hydration');
+const User = require('../src/User');
+describe('Hydration', () => {
+  let hydration, user1, user2;
+
+  beforeEach(() => {
+    hydration = new Hydration(
+    )
+  })
+  it()
+})

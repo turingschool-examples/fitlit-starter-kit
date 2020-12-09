e0d5949b368f9dcc5704e25bd92c253fa7820a29
Cole Fiscus <coletfiscus@gmail.com>
2020-12-09 14:09:56 -0700

    Create weekly sleep repo tests

--- test/SleepRepository-test.js
+++ test/SleepRepository-test.js
@@ -41,13 +41,6 @@ describe('SleepRepository', () => {
   // For a user, how many hours they slept for a specific day (identified by a date)
   // For a user, their sleep quality for a specific day (identified by a date)
 
-
-  // For a user, how many hours slept each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
-  // For a user, their sleep quality each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
-  // For all users, the average sleep quality
-  // Find all users who average a sleep quality greater than 3 for a given week (7 days) - you should be able to calculate this for any week, not just the latest week
-  // For a given day (identified by the date), find the users who slept the most number of hours (one or more if they tied)
-
   it('should return a users average number of hours slept per day', () => {
     expect(sleepRepository.calculateUserAverageHoursSleptAllTime(1)).to.equal(7.9);
     expect(sleepRepository.calculateUserAverageHoursSleptAllTime(2)).to.deep.equal(5.9);
@@ -68,25 +61,63 @@ describe('SleepRepository', () => {
     expect(sleepRepository.returnSleepQualityByDate(2, "2019/06/16")).to.equal(2);
   })
 
-  it.skip('should return a users oz of water consumed each day over the course of a week', () => {
-    expect(sleepRepository.returnOuncesByWeek(1, "2019/06/15")).to.deep.equal({
-      "2019/06/15": 27,
-      "2019/06/16": 75,
-      "2019/06/17": 47,
-      "2019/06/18": 85,
-      "2019/06/19": 42,
-      "2019/06/20": 87,
-      "2019/06/21": 94
-    });
-    expect(sleepRepository.returnOuncesByWeek(2, "2019/06/16")).to.deep.equal({
-      "2019/06/16": 22,
-      "2019/06/17": 67,
-      "2019/06/18": 62,
-      "2019/06/19": 78,
+  // For a user, how many hours slept each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
+  // For a user, their sleep quality each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
+  // For all users, the average sleep quality
+
+
+  // Find all users who average a sleep quality greater than 3 for a given week (7 days) - you should be able to calculate this for any week, not just the latest week
+  // For a given day (identified by the date), find the users who slept the most number of hours (one or more if they tied)
+
+  it.skip('should return a users hours slept each night over the course of a week', () => {
+    expect(sleepRepository.returnNightlyHoursSleptByWeek(1, "2019/06/15")).to.deep.equal({
+      "2019/06/15": 8,
+      "2019/06/16": 7,
+      "2019/06/17": 6,
+      "2019/06/18": 5,
+      "2019/06/19": 9,
+      "2019/06/20": 10,
+      "2019/06/21": 9.5
+      });
+
+    expect(sleepRepository.returnNightlyHoursSleptByWeek(2, "2019/06/16")).to.deep.equal({
+      "2019/06/16": 5.2,
+      "2019/06/17": 4.7,
+      "2019/06/18": 10.5,
+      "2019/06/19": 8.7,
       "2019/06/20": 1,
-      "2019/06/21": 90,
-      "2019/06/22": 28
+      "2019/06/21": 7.4,
+      "2019/06/22": 5.9
     });
   })
 
+  it.skip('should return a users sleep quality each night over the course of a week', () => {
+    expect(sleepRepository.returnNightlySleepQualityByWeek(1, "2019/06/15")).to.deep.equal({
+      "2019/06/15": 4.4,
+      "2019/06/16": 3.2,
+      "2019/06/17": 3,
+      "2019/06/18": 1.6,
+      "2019/06/19": 3.2,
+      "2019/06/20": 4.9,
+      "2019/06/21": 3.3
+    });
+
+    expect(sleepRepository.returnNightlySleepQualityByWeek(2, "2019/06/16")).to.deep.equal({
+      "2019/06/16": 2,
+      "2019/06/17": 4.3,
+      "2019/06/18": 2.2,
+      "2019/06/19": 1.2,
+      "2019/06/20": 1.1,
+      "2019/06/21": 4.3,
+      "2019/06/22": 4.9
+    });
+  })
+
+  it('should return the average sleep quality for all users', () => {
+    
+  });
+
+  it('should return the average sleep quality for all users', () => {
+
+  });
 })

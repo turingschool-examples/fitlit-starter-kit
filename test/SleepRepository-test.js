e9256fe7aea9296745f6de1bc7dd5d08d9f9c7fc
Cole Fiscus <coletfiscus@gmail.com>
2020-12-09 13:34:54 -0700

    Create average user SleepRepo tests

--- test/SleepRepository-test.js
+++ test/SleepRepository-test.js
@@ -6,49 +6,60 @@ const Sleep = require('../src/Sleep');
 const SleepRepository = require('../src/SleepRepository');
 
 describe('SleepRepository', () => {
-  // let hydrationData, hydrationRepository;
-  //
-  // beforeEach(() => {
-  //   hydrationData = hydrationTestDataArray.map(hydrationObject => {
-  //     const hydration = new Hydration(hydrationObject);
-  //     return hydration;
-  //   });
-  //   hydrationRepository = new HydrationRepository(hydrationData);
-  // })
+  let sleepData, sleepRepository;
 
-  it.skip('should be a function', () => {
-    expect(HydrationRepository).to.be.a('function');
+  beforeEach(() => {
+    sleepData = sleepTestDataArray.map(sleepObject => {
+      const sleep = new Sleep(sleepObject);
+      return sleep;
+    });
+    sleepRepository = new SleepRepository(sleepData);
   })
 
-  it.skip('should be an instance of HydrationRepository', () => {
-    expect(hydrationRepository).to.be.an.instanceof(HydrationRepository);
+  it('should be a function', () => {
+    expect(SleepRepository).to.be.a('function');
   })
 
-  it.skip('should hold all Hydration objects', () => {
-    expect(hydrationRepository.hydrationInstanceData[0]).to.deep.equal(hydrationData[0]);
+  it('should be an instance of SleepRepository', () => {
+    expect(sleepRepository).to.be.an.instanceof(SleepRepository);
   })
 
-  it.skip('should return a users hydration data given their user ID', () => {
-    function returnHydrationTestData(id) {
-      return hydrationTestDataArray.filter(hydration => hydration.userID === id);
+  it('should hold all Sleep objects', () => {
+    expect(sleepRepository.sleepInstanceData[0]).to.deep.equal(sleepData[0]);
+  })
+
+  it('should return a users sleep data given their user ID', () => {
+    function returnSleepTestData(id) {
+      return sleepTestDataArray.filter(sleep => sleep.userID === id);
     }
-    expect(hydrationRepository.returnHydrationData(1)).to.deep.equal(returnHydrationTestData(1));
-    expect(hydrationRepository.returnHydrationData(2)).to.deep.equal(returnHydrationTestData(2));
+    expect(sleepRepository.returnSleepData(1)).to.deep.equal(returnSleepTestData(1));
+    expect(sleepRepository.returnSleepData(2)).to.deep.equal(returnSleepTestData(2));
   })
 
-  it.skip('should return a users average oz consumed per day for all time', () => {
-    expect(hydrationRepository.calculateAverageDailyOuncesAllTime(1)).to.deep.equal(67);
-    expect(hydrationRepository.calculateAverageDailyOuncesAllTime(2)).to.deep.equal(49);
+  // For a user (identified by their userID), the average number of hours slept per day
+  // For a user, their average sleep quality per day over all time
+
+
+  // For a user, how many hours they slept for a specific day (identified by a date)
+  // For a user, their sleep quality for a specific day (identified by a date)
+  // For a user, how many hours slept each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
+  // For a user, their sleep quality each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
+  // For all users, the average sleep quality
+  // Find all users who average a sleep quality greater than 3 for a given week (7 days) - you should be able to calculate this for any week, not just the latest week
+  // For a given day (identified by the date), find the users who slept the most number of hours (one or more if they tied)
+
+  it('should return a users average number of hours slept per day', () => {
+    expect(sleepRepository.calculateUserAverageHoursSleptAllTime(1)).to.deep.equal(7.9);
+    expect(sleepRepository.calculateUserAverageHoursSleptAllTime(2)).to.deep.equal(5.9);
   })
 
-  it.skip('should return a users oz consumed for a specific day', () => {
-    expect(hydrationRepository.returnOuncesByDate(1, "2019/06/18")).to.deep.equal(85);
-    expect(hydrationRepository.returnOuncesByDate(2, "2019/06/16")).to.deep.equal(22);
+  it('should return a users average sleep quality per day over all time', () => {
+    expect(sleepRepository.calculateUserAverageSleepQualityAllTime(1)).to.deep.equal(3.5);
+    expect(sleepRepository.calculateUserAverageSleepQualityAllTime(2)).to.deep.equal(2.6);
   })
 
-  // For a user, how many fluid ounces of water consumed each day over the course of a week (7 days) - return the amount for each day
   it.skip('should return a users oz of water consumed each day over the course of a week', () => {
-    expect(hydrationRepository.returnOuncesByWeek(1, "2019/06/15")).to.deep.equal({
+    expect(sleepRepository.returnOuncesByWeek(1, "2019/06/15")).to.deep.equal({
       "2019/06/15": 27,
       "2019/06/16": 75,
       "2019/06/17": 47,
@@ -57,7 +68,7 @@ describe('SleepRepository', () => {
       "2019/06/20": 87,
       "2019/06/21": 94
     });
-    expect(hydrationRepository.returnOuncesByWeek(2, "2019/06/16")).to.deep.equal({
+    expect(sleepRepository.returnOuncesByWeek(2, "2019/06/16")).to.deep.equal({
       "2019/06/16": 22,
       "2019/06/17": 67,
       "2019/06/18": 62,

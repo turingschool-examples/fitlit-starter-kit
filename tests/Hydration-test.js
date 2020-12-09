f631943c8d8736c896ae6bce96890c867d678813
conconartist <conconart.info@gmail.com>
2020-12-09 15:00:00 -0700

    Update test instances for Hydration class

--- tests/Hydration-test.js
+++ tests/Hydration-test.js
@@ -9,95 +9,57 @@ describe('Hydration', () => {
     hydration4, hydration5, hydration6,
     hydration7, hydration8, hydration9,
     hydration10, hydration11;
-  let hydrations stats = [];
+  let hydrationStats1, hydrationStats2;
   // let user1, user2;
 
   beforeEach(() => {
-    hydration1 = new Hydration({
-      "userID": 2,
-      "date": "2019/04/15",
-      "numOunces": 25
-    });
+    hydration1 = new Hydration({"userID": 2, "date": "2019/04/15", "numOunces": 25});
+    hydration2 = new Hydration({"userID": 2, "date": "2019/04/16", "numOunces": 40});
+    hydration3 = new Hydration({"userID": 2, "date": "2019/04/17", "numOunces": 45});
+    hydration4 = new Hydration({"userID": 2, "date": "2019/04/18", "numOunces": 23});
+    hydration5 = new Hydration({"userID": 2, "date": "2019/04/19", "numOunces": 80});
+    hydration6 = new Hydration({"userID": 2, "date": "2019/04/20", "numOunces": 49});
+    hydration7 = new Hydration({"userID": 2, "date": "2019/04/21", "numOunces": 88});
+    hydration8 = new Hydration({"userID": 2, "date": "2019/04/22", "numOunces": 75});
+    hydration9 = new Hydration({"userID": 2, "date": "2019/04/23", "numOunces": 29});
 
-    hydration2 = new Hydration({
-      "userID": 2,
-      "date": "2019/04/16",
-      "numOunces": 40
-    });
+    hydration10 = new Hydration({"userID": 15,"date": "2019/02/25","numOunces": 10});
+    hydration11 = new Hydration({"userID": 15,"date": "2019/02/26","numOunces": 100});
+    hydration12 = new Hydration({"userID": 15,"date": "2019/02/27","numOunces": 45});
+    hydration13 = new Hydration({"userID": 15,"date": "2019/02/28","numOunces": 60});
+    hydration14 = new Hydration({"userID": 15,"date": "2019/03/01","numOunces": 70});
+    hydration15 = new Hydration({"userID": 15,"date": "2019/03/02","numOunces": 82});
+    hydration16 = new Hydration({"userID": 15,"date": "2019/03/03","numOunces": 20});
 
-    hydration3 = new Hydration({
-    "userID": 2,
-    "date": "2019/04/17",
-    "numOunces": 45
-    });
-
-    hydration4 = new Hydration({
-    "userID": 2,
-    "date": "2019/04/18",
-    "numOunces": 23
-    });
-
-    hydration5 = new Hydration({
-    "userID": 2,
-    "date": "2019/04/19",
-    "numOunces": 80
-    });
-
-    hydration6 = new Hydration({
-    "userID": 2,
-    "date": "2019/04/20",
-    "numOunces": 49
-    });
-
-    hydration7 = new Hydration({
-    "userID": 2,
-    "date": "2019/04/21",
-    "numOunces": 88
-    });
-
-    hydration8 = new Hydration({
-    "userID": 2,
-    "date": "2019/04/22",
-    "numOunces": 75
-    });
-
-    hydration9 = new Hydration({
-    "userID": 2,
-    "date": "2019/04/23",
-    "numOunces": 29
-    });
-
-    hydration10 = new Hydration({
-      "userID": 15,
-      "date": "2019/02/25",
-      "numOunces": 0
-    });
-
-    hydration11 = new Hydration({
+    hydration17 = new Hydration({
       "userID": 20,
       "date": "2019/12/02",
       "numOunces": 100
     });
-    hydrationStats = [
+    hydrationStats1 = [
       hydration1, hydration2, hydration3, hydration4, hydration5,
       hydration6, hydration7, hydration8, hydration9
     ];
+    hydrationStats2 = [
+      hydration10, hydration11, hydration12, hydration13, hydration14,
+      hydration15, hydration16
+    ]
   })
 
   it('should have a userID property', () => {
     expect(hydration1.userID).to.equal(2);
     expect(hydration10.userID).to.equal(15);
-    expect(hydration11.userID).to.equal(20);
+    expect(hydration17.userID).to.equal(20);
   })
   it('should include a date as a property', () => {
     expect(hydration1.date).to.equal("2019/04/15");
     expect(hydration10.date).to.equal("2019/02/25");
-    expect(hydration11.date).to.equal("2019/12/02");
+    expect(hydration17.date).to.equal("2019/12/02");
   })
   it('should have a property that states the number of ounces of water consumed', () => {
     expect(hydration1.numOunces).to.equal(25);
     expect(hydration10.numOunces).to.equal(0);
-    expect(hydration11.numOunces).to.equal(100);
+    expect(hydration17.numOunces).to.equal(100);
   })
   it('should be able to calculate the average fluid ounces consumed per day for all time for the given userID', () => {
     //.filter userID

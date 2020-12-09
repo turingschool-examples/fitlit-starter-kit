1f70374fc06c5fb09d7f61eb52f3d6a2a8b08bd3
conconartist <conconart.info@gmail.com>
2020-12-08 20:40:12 -0700

    Add tests blocks for Hydration class methods

--- tests/Hydration-test.js
+++ tests/Hydration-test.js
@@ -94,7 +94,16 @@ describe('Hydration', () => {
     expect(hydration10.numOunces).to.equal(0);
     expect(hydration11.numOunces).to.equal(100);
   })
-  it('should be able to calculate the average fluid ounces consumed per day for all time for the given userID')
-  it('should be able to calculate how many fluid ounces were consumed on a given day')//date
-  it('should be able to calculate how many fluid ounces were consumed each day over the course of 7days and return the amount')
+  it('should be able to calculate the average fluid ounces consumed per day for all time for the given userID', () => {
+    //.filter userID
+    //.reduce add all numbers and divide by the length to get the average
+    //this may need to go in another class User? 
+  })
+  it('should be able to calculate how many fluid ounces were consumed on a given day', () => {
+    //.find date and return numOunces
+  })
+  it('should be able to calculate how many fluid ounces were consumed each day over the course of 7days and return the amount', () => {
+    //.find date and add 1 to each day? to create a new array? or sort?
+    //add numOunces and divide by 7
+  })
 })

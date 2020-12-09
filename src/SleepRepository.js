a38f97ac1fa26bd90d3256ed8e83e89ff031bf4f
Tashia Davis <tashiadavis10@gmail.com>
2020-12-08 20:39:58 -0700

    Add SleepRepository class outline

--- src/SleepRepository.js
+++ src/SleepRepository.js
@@ -1,40 +1,70 @@
-class HydrationRepository {
-   constructor(hydrationInstanceData) {
-     this.hydrationInstanceData = hydrationInstanceData;
+class SleepRepository {
+   constructor(sleepInstanceData) {
+     this.sleepInstanceData = sleepInstanceData;
    }
 
-   returnHydrationData(id) {
-     return this.hydrationInstanceData.filter(hydration => hydration.userID === id);
+   returnSleepData(id) {
+     return this.sleepInstanceData.filter(sleep => sleep.userID === id);
    }
 
-   calculateAverageDailyOuncesAllTime(id) {
-     const allUserHydration = this.returnHydrationData(id);
-     const userTotalOzAllTime = allUserHydration.reduce((totalOz, water) => {
-       return totalOz + water.numOunces;
-     }, 0);
-     return Math.floor(userTotalOzAllTime / allUserHydration.length);
+   calculateUserAverageHoursSleptAllTime(id) {
+     // const allUserHydration = this.returnHydrationData(id);
+     // const userTotalOzAllTime = allUserHydration.reduce((totalOz, water) => {
+     //   return totalOz + water.numOunces;
+     // }, 0);
+     // return Math.floor(userTotalOzAllTime / allUserHydration.length);
    }
 
-   returnOuncesByDate(id, date) {
-     const allUserHydration = this.returnHydrationData(id);
-     const ozByDate = allUserHydration.find(water => water.date === date);
-     return ozByDate.numOunces;
+   calculateUserAverageSleepQualityAllTime(id) {
+     // const allUserHydration = this.returnHydrationData(id);
+     // const userTotalOzAllTime = allUserHydration.reduce((totalOz, water) => {
+     //   return totalOz + water.numOunces;
+     // }, 0);
+     // return Math.floor(userTotalOzAllTime / allUserHydration.length);
    }
 
-   returnOuncesByWeek(id, date) {
-     const allUserHydration = this.returnHydrationData(id);
-     const dateArray = date.split('/') // STRINGS IN AN ARRAY
-     console.log(dateArray)
-     console.log(dateArray[2] + 1)
-     // const ozByWeek = allUserHydration.
-     // we have an array of hydration objects for 1 user id
-     // we want an object w/ key of date and value of oz for that date
-     // given one date, how do we make the key (Date) increase 7 times (6 times?)
-     // date is a string, we need to .split("/") to make it an array so we can just increase the last number
+   returnSleepHoursByDate(id, date) {
+     // const allUserHydration = this.returnHydrationData(id);
+     // const ozByDate = allUserHydration.find(water => water.date === date);
+     // return ozByDate.numOunces;
+   }
+
+   returnSleepQualityByDate(id, date) {
+     // const allUserHydration = this.returnHydrationData(id);
+     // const ozByDate = allUserHydration.find(water => water.date === date);
+     // return ozByDate.numOunces;
+   }
+
+   returnNightlyHoursSleptByWeek(id, date) {
+     // const allUserHydration = this.returnHydrationData(id);
+   }
+
+   returnNightlySleepQualityByWeek(id, date) {
+     // const allUserHydration = this.returnHydrationData(id);
+   }
+
+   returnNightlySleepQualityByWeek(id, date) {
+     // const allUserHydration = this.returnHydrationData(id);
+   }
+
+   calculateAllUsersAverageSleepQualityAllTime(id) {
+     // const allUserHydration = this.returnHydrationData(id);
+     // const userTotalOzAllTime = allUserHydration.reduce((totalOz, water) => {
+     //   return totalOz + water.numOunces;
+     // }, 0);
+     // return Math.floor(userTotalOzAllTime / allUserHydration.length);
+   }
+
+   calculateUsersWithSleepQualityGreaterThan3() {
+
+   }
+
+   returnUsersWhoSleptMostOnDate() {
+
    }
 
  }
 
  if (typeof module !== 'undefined') {
-   module.exports = HydrationRepository;
+   module.exports = SleepRepository;
  }

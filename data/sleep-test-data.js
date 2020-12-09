a03ecf0122968331a654c25586aab7b89f69930c
Tashia Davis <tashiadavis10@gmail.com>
2020-12-08 20:24:32 -0700

    Add completed sleep test data file

---
+++ data/sleep-test-data.js
@@ -0,0 +1,103 @@
+const testSleep = [{
+    "userID": 1,
+    "date": "2019/06/15",
+    "hoursSlept": 8,
+    "sleepQuality": 4.4
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "hoursSlept": 7,
+    "sleepQuality": 3.2
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/17",
+    "hoursSlept": 6,
+    "sleepQuality": 3
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/18",
+    "hoursSlept": 5,
+    "sleepQuality": 1.6
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/19",
+    "hoursSlept": 9,
+    "sleepQuality": 3.2
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/20",
+    "hoursSlept": 10,
+    "sleepQuality": 4.9
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/21",
+    "hoursSlept": 9.5,
+    "sleepQuality": 3.3
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/22",
+    "hoursSlept": 8.5,
+    "sleepQuality": 4
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "hoursSlept": 4,
+    "sleepQuality": 1
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/16",
+    "hoursSlept": 5.2,
+    "sleepQuality": 2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/17",
+    "hoursSlept": 4.7,
+    "sleepQuality": 4.3
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/18",
+    "hoursSlept": 10.5,
+    "sleepQuality": 2.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/19",
+    "hoursSlept": 8.7,
+    "sleepQuality": 1.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/20",
+    "hoursSlept": 1,
+    "sleepQuality": 1.1
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/21",
+    "hoursSlept": 7.4,
+    "sleepQuality": 4.3
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/22",
+    "hoursSlept": 5.9,
+    "sleepQuality": 4.9
+  }
+];
+
+if (typeof module !== 'undefined') {
+  module.exports = {
+    testSleep
+  }
+}

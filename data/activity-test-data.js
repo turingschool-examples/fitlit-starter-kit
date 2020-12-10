63bd29b0b7d0d03be8dd04bf17aad3aa646a03be
Cole Fiscus <coletfiscus@gmail.com>
2020-12-10 14:50:56 -0700

    Create activity test data

--- data/activity-test-data.js
+++ data/activity-test-data.js
@@ -1,1 +1,118 @@
-const testActivity = []
+const testActivity = [{
+  "userID": 1,
+  "date": "2019/06/15",
+  "numSteps": 3577,
+  "minutesActive": 140,
+  "flightsOfStairs": 16
+},
+{
+  "userID": 1,
+  "date": "2019/06/16",
+  "numSteps": 4294,
+  "minutesActive": 138,
+  "flightsOfStairs": 10
+},
+{
+  "userID": 1,
+  "date": "2019/06/17",
+  "numSteps": 7402,
+  "minutesActive": 116,
+  "flightsOfStairs": 33
+},
+{
+  "userID": 1,
+  "date": "2019/06/18",
+  "numSteps": 3486,
+  "minutesActive": 114,
+  "flightsOfStairs": 32
+},
+{
+  "userID": 1,
+  "date": "2019/06/19",
+  "numSteps": 11374,
+  "minutesActive": 213,
+  "flightsOfStairs": 13
+},
+{
+  "userID": 1,
+  "date": "2019/06/20",
+  "numSteps": 14810,
+  "minutesActive": 287,
+  "flightsOfStairs": 18
+},
+{
+  "userID": 1,
+  "date": "2019/06/21",
+  "numSteps": 2634,
+  "minutesActive": 107,
+  "flightsOfStairs": 5
+},
+{
+  "userID": 1,
+  "date": "2019/06/22",
+  "numSteps": 10333,
+  "minutesActive": 114,
+  "flightsOfStairs": 31
+},
+{
+  "userID": 2,
+  "date": "2019/06/15",
+  "numSteps": 4587,
+  "minutesActive": 163,
+  "flightsOfStairs": 18
+},
+{
+  "userID": 2,
+  "date": "2019/06/16",
+  "numSteps": 4887,
+  "minutesActive": 112,
+  "flightsOfStairs": 19
+},
+{
+  "userID": 2,
+  "date": "2019/06/17",
+  "numSteps": 7864,
+  "minutesActive": 151,
+  "flightsOfStairs": 28
+},
+{
+  "userID": 2,
+  "date": "2019/06/18",
+  "numSteps": 6885,
+  "minutesActive": 98,
+  "flightsOfStairs": 40
+},
+{
+  "userID": 2,
+  "date": "2019/06/19",
+  "numSteps": 15135,
+  "minutesActive": 220,
+  "flightsOfStairs": 21
+},
+{
+  "userID": 2,
+  "date": "2019/06/20",
+  "numSteps": 10001,
+  "minutesActive": 101,
+  "flightsOfStairs": 10
+},
+{
+  "userID": 2,
+  "date": "2019/06/21",
+  "numSteps": 7975,
+  "minutesActive": 99,
+  "flightsOfStairs": 3
+},
+{
+  "userID": 2,
+  "date": "2019/06/22",
+  "numSteps": 18546,
+  "minutesActive": 287,
+  "flightsOfStairs": 40
+}];
+
+if (typeof module !== 'undefined') {
+  module.exports = {
+    testActivity;
+  }
+}

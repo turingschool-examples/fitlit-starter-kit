9fb2a12e6a66bb8af59977c53b01545cc9fef15c
conconartist <conconart.info@gmail.com>
2020-12-09 23:04:30 -0700

    Add calculateTotalWaterPerDay method to DisplayCalculator class

--- src/DisplayCalculator.js
+++ src/DisplayCalculator.js
@@ -1,9 +1,14 @@
 class DisplayCalculator {
-  calculateAvgPerDay(userID, hydrationStats){
+  calculateAvgWaterPerDay(userID, hydrationStats) {
     const numOuncesData = hydrationStats.filter(water => (water.userID === userID));
     const waterConsumed = numOuncesData.map(water => water.numOunces);
     const avgWaterConsumed = (waterConsumed.reduce((a, b) => a + b, 0)) / waterConsumed.length;
     return avgWaterConsumed;
+  }
+  calculateTotalWaterPerDay(hydrationStats, userID, date) {
+    const userWater = hydrationStats.filter(water => water.userID === userID);
+    const waterOnDay = userWater.find(waterDrank => (waterDrank.date == date));
+    return waterOnDay.numOunces;
   }
 }
 module.exports = DisplayCalculator;

279c3311a384d243d4413399c4b0c0af0b0e5178
Brian Forbes <brianandrewf81@gmail.com>
2020-12-10 13:50:49 -0700

    Write 4 tests for properties/methods of User class

--- test/User-test.js
+++ test/User-test.js
@@ -20,7 +20,24 @@ describe('User', () => {
         4
       ]
     };
-    
+
     user = new User(data);
-  });    
+  });
+
+  it('should be able to take in user data object as an argument', () =>{
+    expect(user.id).to.equal(1);
+    expect(user.friends).to.deep.equal([2, 3, 4]);
+  });
+
+  it('each user should have a name', () => {
+    expect(user.name).to.equal('Brian Forbes');
+  });
+
+  it('each user should have an address', () => {
+    expect(user.address).to.equal('123 Blah St, Denver CO, 66666');
+  });
+
+  it(`should have a way to return the user's name`, () => {
+    expect(provideUserName()).to.equal('Brian Forbes');
+  });
 });

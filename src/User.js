f66cfcb94d9e295ffe0a4f242681b33f23585803
Brian Forbes <brianandrewf81@gmail.com>
2020-12-10 15:46:13 -0700

    Build User class with properties and method to pass both tests in User-test.js

--- src/User.js
+++ src/User.js
@@ -1,6 +1,19 @@
 class User {
-  constructor() {}
-}
+  constructor(userData) {
+    this.id = userData.id;
+    this.name = userData.name;
+    this.address = userData.address;
+    this.email = userData.email;
+    this.strideLength = userData.strideLength;
+    this.dailyStepGoal = userData.dailyStepGoal;
+    this.friends = userData.friends
+  };
+
+  provideUsersFirstName() {
+    const firstName = this.name.split(' ')[0];
+    return firstName;
+  };
+};
 
 if (typeof module !== 'undefined') {
   module.exports = User;

6b98e69fb17adb5107517194e87978da117406f4
Cole Fiscus <coletfiscus@gmail.com>
2020-12-09 22:46:58 -0700

    Add instantiations of HydrationRepo and showing weekly ounces drank

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,16 @@
-if (typeof module !== 'undefined') {
-  var User = require('./User');
-  var UserRepository = require('./UserRepository');
-  var userData = require('../data/user-test-data');
-}
+// if (typeof module !== 'undefined') {
+//   var User = require('./User');
+//   var UserRepository = require('./UserRepository');
+//   var userData = require('../data/users');
+//   var Hydration = require('./Hydration');
+//   var HydrationRepository = require('./HydrationRepository')
+//   var hydrationData = require('../data/hydration')
+// }
 
 // ~~~~~ QUERY SELECTORS ~~~~~
-const userNameGreeting = document.querySelector(".user-greeting");
-const userInfoCard = document.querySelector(".user-info-card");
+const userNameGreeting = document.querySelector('.user-greeting');
+const userInfoCard = document.querySelector('.user-info-card');
+const hydrationSection = document.querySelector('.hydration')
 
 // ~~~~~ EVENT LISTENERS ~~~~~
 window.onload = start;
@@ -18,10 +22,17 @@ const users = userData.map(userObject => {
 });
 const userRepository = new UserRepository(users);
 
+const hydrationObjects = hydrationData.map(hydrationObject => {
+  const hydration = new Hydration(hydrationObject);
+  return hydration;
+});
+const hydrationRepository = new HydrationRepository(hydrationObjects);
+
 // ~~~~~ FUNCTIONS ~~~~~
 function start() {
   userNameGreeting.innerText = `Hello, ${users[5].returnFirstName()}`;
   displayUserInfoCard(users[5]);
+  displayHydrationInfo(users[5], "2019/06/24")
 }
 
 function displayUserInfoCard(user) {
@@ -33,4 +44,19 @@ function displayUserInfoCard(user) {
     <p>Friends: ${user.friends}</p>
     <p>Daily Step Goal: ${user.dailyStepGoal}</p>
     <p>Avg User Step Goal: ${userRepository.calculateAverageStepGoal()}</p>`;
+}
+
+function displayHydrationInfo(user, date) {
+  const pastWeekHydrationObjects = hydrationRepository.returnOuncesByWeek(5, date)
+  hydrationSection.innerHTML = `
+    <h2>HYDRATION!</h2>
+    <p>Water consumed today: ounces</p>
+    <p>Water consumed the past week:</p>
+    <p>${pastWeekHydrationObjects[0].date}: ${pastWeekHydrationObjects[0].numOunces} ounces</p>
+    <p>${pastWeekHydrationObjects[1].date}: ${pastWeekHydrationObjects[1].numOunces} ounces</p>
+    <p>${pastWeekHydrationObjects[2].date}: ${pastWeekHydrationObjects[2].numOunces} ounces</p>
+    <p>${pastWeekHydrationObjects[3].date}: ${pastWeekHydrationObjects[3].numOunces} ounces</p>
+    <p>${pastWeekHydrationObjects[4].date}: ${pastWeekHydrationObjects[4].numOunces} ounces</p>
+    <p>${pastWeekHydrationObjects[5].date}: ${pastWeekHydrationObjects[5].numOunces} ounces</p>
+    <p>${pastWeekHydrationObjects[6].date}: ${pastWeekHydrationObjects[6].numOunces} ounces</p>`;
 }

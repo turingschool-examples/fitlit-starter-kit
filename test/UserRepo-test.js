5e64f7f481b2252d4409d3d5f60d88d8be7db0b8
Brian Forbes <brianandrewf81@gmail.com>
2020-12-09 17:09:39 -0700

    Complete UserRepo tests

--- test/UserRepo-test.js
+++ test/UserRepo-test.js
@@ -75,5 +75,7 @@ describe('UserRepo', () => {
     expect(userRepo.data[0].name).to.equal('Brian Forbes');
   });
 
-  it.skip('')
+  it.skip('should be able calculate average step goal among all users', () => {
+    expect(userRepo.userStepGoalAverage()).to.equal(7501.25);
+  });
 });

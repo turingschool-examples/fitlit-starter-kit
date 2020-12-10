8d3411aef8709bf626477f82001a5284310e3b84
conconartist <conconart.info@gmail.com>
2020-12-10 15:06:29 -0700

    Refactor CommunityHydration class

--- src/CommunityHydration.js
+++ src/CommunityHydration.js
@@ -3,14 +3,14 @@ class CommunityHydration {
     this.hydrations = data.map(user => new Hydration(user))
   }
 
-  calculateAvgWaterPerDay(userID, hydrationStats) {
-    const numOuncesData = hydrationStats.filter(water => (water.userID === userID));
+  calculateAvgWaterPerDay(userID) {
+    const numOuncesData = this.hydrations.filter(water => (water.userID === userID));
     const waterConsumed = numOuncesData.map(water => water.numOunces);
     const avgWaterConsumed = (waterConsumed.reduce((a, b) => a + b, 0)) / waterConsumed.length;
     return avgWaterConsumed;
   }
-  calculateTotalWaterPerDay(hydrationStats, userID, date) {
-    const userWater = hydrationStats.filter(water => water.userID === userID);
+  calculateTotalWaterPerDay(userID, date) {
+    const userWater = this.hydrations.filter(water => water.userID === userID);
     const waterOnDay = userWater.find(waterDrank => (waterDrank.date === date));
     return waterOnDay.numOunces;
   }

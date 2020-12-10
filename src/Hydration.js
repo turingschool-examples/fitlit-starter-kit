dd81c4bacafbff3963e4655d498f0932345ee47b
conconartist <conconart.info@gmail.com>
2020-12-09 22:16:53 -0700

    Refactor Hydration class

--- src/Hydration.js
+++ src/Hydration.js
@@ -4,11 +4,5 @@ class Hydration {
     this.date = hydrationForUser.date;
     this.numOunces = hydrationForUser.numOunces;
   }
-  calculateAvgPerDay(hydrationStats) {
-    const numOuncesData = hydrationStats.filter(water => (water.userID === this.userID));
-    const waterConsumed = numOuncesData.map(water => water.numOunces);
-    const totalWaterConsumed = (waterConsumed.reduce((a, b) => a + b, 0)) / waterConsumed.length;
-    return totalWaterConsumed;
-  }
 }
 module.exports = Hydration;

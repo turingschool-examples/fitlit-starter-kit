33caad00ab2aab4f1b01718e7f73d75048e1ea23
Brian Forbes <brianandrewf81@gmail.com>
2020-12-10 19:53:00 -0700

    Write query selector and function to display users first name

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,6 @@
 const currentUser = new User(userData[0]);
+let firstNameDisplay = document.querySelector('.first-name-section');
+
+const displayUsersFirstName = () => {
+  firstNameDisplay.innerText = `Hello ${currentUser.provideUsersFirstName()}!`
+}

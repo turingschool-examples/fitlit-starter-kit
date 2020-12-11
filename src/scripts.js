93a503a917ed7fe5c094e3e4bc1b38ac39683578
Brian Forbes <brianandrewf81@gmail.com>
2020-12-10 21:53:40 -0700

    Write functions and query selectors to display user data on DOM

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,29 @@
 const currentUser = new User(userData[0]);
-let firstNameDisplay = document.querySelector('.first-name-section');
+const firstNameDisplay = document.querySelector('.first-name-section');
+const fullNameDisplay = document.querySelector('.full-name');
+const addressDisplay = document.querySelector('.address');
+const emailDisplay = document.querySelector('.email');
+const strideLengthDisplay = document.querySelector('.stride');
+const dailyStepGoal = document.querySelector('.daily-step-goal');
+const friendsList = document.querySelector('.friends');
 
-const displayUsersFirstName = () => {
+
+// const displayUsersFirstName = () => {
+//   firstNameDisplay.innerText = `Hello ${currentUser.provideUsersFirstName()}!`
+// }
+
+const displayAllUserData = () => {
   firstNameDisplay.innerText = `Hello ${currentUser.provideUsersFirstName()}!`
-}
+  fullNameDisplay.innerText += `${currentUser.name}`;
+  addressDisplay.innerText += `${currentUser.address}`;
+  emailDisplay.innerText += `${currentUser.email}`;
+  strideLengthDisplay.innerText += `${currentUser.strideLength}`;
+  dailyStepGoal.innerText += `${currentUser.dailyStepGoal}`;
+  displayFriendsByName();
+};
+
+function displayFriendsByName() {
+  currentUser.friends.forEach(id => {
+  friendsList.innerText += ` ${userData[id - 1].name}. `;
+  });
+};

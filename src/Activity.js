612e1b9c15d6481db53c8caa7a80e0045b53cb57
tashiad <tashiadavis10@gmail.com>
2020-12-11 08:24:37 -0700

    Complete stairs record and exceeded step goal methods

--- src/Activity.js
+++ src/Activity.js
@@ -1,14 +1,14 @@
 class Activity {
-   constructor(activityData) {
-     this.userID = activityData.userID;
-     this.date = activityData.date;
-     this.numSteps = activityData.numSteps;
-     this.minutesActive = activityData.minutesActive;
-     this.flightsOfStairs = activityData.flightsOfStairs;
-   }
+  constructor(activityData) {
+    this.userID = activityData.userID;
+    this.date = activityData.date;
+    this.numSteps = activityData.numSteps;
+    this.minutesActive = activityData.minutesActive;
+    this.flightsOfStairs = activityData.flightsOfStairs;
+  }
 
- }
+}
 
- if (typeof module !== 'undefined') {
-   module.exports = Activity;
- }
+if (typeof module !== 'undefined') {
+  module.exports = Activity;
+}
--- src/ActivityRepository.js
+++ src/ActivityRepository.js
@@ -1,57 +1,66 @@
 class ActivityRepository {
-   constructor(activityInstanceData) {
-     this.activityInstanceData = activityInstanceData;
-   }
+  constructor(activityInstanceData) {
+    this.activityInstanceData = activityInstanceData;
+  }
 
-   returnActivityData(id) {
-     return this.activityInstanceData.filter(activity => activity.userID === id);
-   }
+  returnActivityData(id) {
+    return this.activityInstanceData.filter(activity => activity.userID === id);
+  }
 
-   returnMilesWalked(user, date) {
-     const allUserActivity = this.returnActivityData(user.id);
-     const userActivityDate = allUserActivity.find(activity => activity.date === date);
-     return Number(((user.strideLength * userActivityDate.numSteps) / 5280).toFixed(1));
-   }
+  returnMilesWalked(user, date) {
+    const allUserActivity = this.returnActivityData(user.id);
+    const userActivityDate = allUserActivity.find(activity => activity.date === date);
+    return Number(((user.strideLength * userActivityDate.numSteps) / 5280).toFixed(1));
+  }
 
-   returnMinutesActive(id, date) {
-     const allUserActivity = this.returnActivityData(id);
-     const userActivityDate = allUserActivity.find(activity => activity.date === date);
-     return userActivityDate.minutesActive;
-   }
+  returnMinutesActive(id, date) {
+    const allUserActivity = this.returnActivityData(id);
+    const userActivityDate = allUserActivity.find(activity => activity.date === date);
+    return userActivityDate.minutesActive;
+  }
 
-   calculateWeeklyAverageMinutesActive(id, date) {
-     const allUserActivity = this.returnActivityData(id);
-     const activityDates = allUserActivity.map(activity => activity.date);
-     const indexOfMatchingActivityDate = activityDates.indexOf(date);
-     const weekOfUserActivity =  allUserActivity.slice(indexOfMatchingActivityDate - 6, indexOfMatchingActivityDate + 1);
-     const totalUserActiveMinutes = weekOfUserActivity.reduce((totalMinutes, activity) => totalMinutes + activity.minutesActive, 0)
-     return Math.round((totalUserActiveMinutes / 7));
-   }
+  calculateWeeklyAverageMinutesActive(id, date) {
+    const allUserActivity = this.returnActivityData(id);
+    const activityDates = allUserActivity.map(activity => activity.date);
+    const indexOfMatchingActivityDate = activityDates.indexOf(date);
+    const weekOfUserActivity =  allUserActivity.slice(indexOfMatchingActivityDate - 6, indexOfMatchingActivityDate + 1);
+    const totalUserActiveMinutes = weekOfUserActivity.reduce((totalMinutes, activity) => totalMinutes + activity.minutesActive, 0)
+    return Math.round((totalUserActiveMinutes / 7));
+  }
 
-   determineStepGoalAchieved(user, date) {
-     const allUserActivity = this.returnActivityData(user.id);
-     const userActivityDate = allUserActivity.find(activity => activity.date === date);
-     return (user.dailyStepGoal <= userActivityDate.numSteps);
-     // For a user, did they reach their step goal for a given day (specified by a date)?
-   }
+  determineStepGoalAchieved(user, date) {
+    const allUserActivity = this.returnActivityData(user.id);
+    const userActivityDate = allUserActivity.find(activity => activity.date === date);
+    return (user.dailyStepGoal <= userActivityDate.numSteps);
+  }
 
-   findDaysWithExceededStepGoal() {
-     // For a user, find all the days where they exceeded their step goal
-   }
+  findDaysExceededStepGoal(user) {
+    const daysExceededStepGoal = [];
+    const allUserActivity = this.returnActivityData(user.id);
+    allUserActivity.forEach(activity => {
+      if (activity.numSteps >= user.dailyStepGoal) {
+        daysExceededStepGoal.push(activity.date);
+      }
+    })
+    return daysExceededStepGoal;
+  }
 
-   findFlightsOfStairsRecord() {
-     // For a user, find their all-time stair climbing record
-   }
+  findFlightsOfStairsRecord(id) {
+    const allUserActivity = this.returnActivityData(id);
+    const allFlightsOfStairs = allUserActivity.map(activity => activity.flightsOfStairs);
+    const max = Math.max(...allFlightsOfStairs);
+    return max;
+  }
 
-   calculateAllUsersActivityAveragesOnDay() {
-     // For all users, what is the average number of:
-        // stairs climbed for a specified date
-        // steps taken for a specific date
-        // minutes active for a specific date
-   }
+  getAllUsersActivityAvgOnDay(date) {
+    // For all users, what is the average number of:
+    // stairs climbed for a specified date
+    // steps taken for a specific date
+    // minutes active for a specific date
+  }
 
- }
+}
 
- if (typeof module !== 'undefined') {
-   module.exports = ActivityRepository;
- }
+if (typeof module !== 'undefined') {
+  module.exports = ActivityRepository;
+}

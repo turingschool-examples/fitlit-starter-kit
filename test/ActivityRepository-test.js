da2145a892d0255007cbb065fac40cc242ce25da
Cole Fiscus <coletfiscus@gmail.com>
2020-12-10 18:24:27 -0700

    Add ActivityRepo miles and minutes method tests

--- test/ActivityRepository-test.js
+++ test/ActivityRepository-test.js
@@ -9,7 +9,7 @@ const ActivityRepository = require('../src/ActivityRepository');
 const User = require('../src/User');
 
 describe('Activity', () => {
-  let activityData;
+  let activityData, activityRepository;
 
   beforeEach(() => {
     activityData = activityTestDataArray.map(activityObject => {
@@ -29,6 +29,16 @@ describe('Activity', () => {
 
   it('should hold all Activity objects', () => {
     expect(activityRepository.activityInstanceData[0]).to.deep.equal(activityData[0]);
+  })
+
+  it('should return miles walked on date', () => {
+    expect(activityRepository.returnMilesWalked(1, "2019/06/15")).to.equal(3.9);
+    expect(activityRepository.returnMilesWalked(2, "2019/06/15")).to.equal(1.0);
+  })
+
+  it('should return minutes active on date', () => {
+    expect(activityRepository.returnMinutesActive(1, "2019/06/15")).to.equal(3.9);
+    expect(activityRepository.returnMinutesActive(2, "2019/06/15")).to.equal(1.0);
   })
 
 })

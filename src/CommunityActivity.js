70ec84df3f80b10019566eb40b501bff15304131
srslie <aliceruppert@gmail.com>
2020-12-12 14:06:24 -0700

    Start a findAverages function

--- src/CommunityActivity.js
+++ src/CommunityActivity.js
@@ -1,13 +1,27 @@
 const activityData = require(../data/activity)
 
 class CommunityActivity {
-  constructor(userRespository) {
-    
+  constructor(activity) {
+    this.activities = activity.map(activity => new Activity())
   }
 
-  findAverageSteps(date) {
-
+  dateToNumber(date) {
+    return parseInt(date.split('/').join(''))
   }
+
+  findAverage(startDate, endDate, fitnessThing) {
+    const startNum = dateToNumber(startDate)
+    const endNum = dateToNumber(endDate)
+
+    const activitiesDuringThisTimePeriod = this.activities.filter(activities => {
+      const activityDateNum = dateToNumber(activities.date)
+      return activityDateNum >= startDate && activityDateNum <= endDate
+    })
+
+    const totalActivity = activitiesDuringThisTimePeriod.reduce((activityTotal, activity) => activityTotal + activity[fitnessThing], 0)
+
+    return totalActivity / activitiesDuringThisTimePeriod.length
+}
 
   findAverageStairs(date) {
 

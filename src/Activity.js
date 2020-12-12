f50f7b2dca9bb6635f778498bba0e566548119a4
srslie <aliceruppert@gmail.com>
2020-12-12 14:08:37 -0700

    Add properties, verify if step goal, get steps in miles methods

--- src/Activity.js
+++ src/Activity.js
@@ -1,6 +1,19 @@
 class Activity {
-  constructor() {
+  constructor(data) {
+    this.userID = data.userID;
+    this.date = data.date;
+    this.numSteps = data.numSteps;
+    this.minutesActive = data.minutesActive;
+    this.flightsofStairs = data.flightsOfStairs
+  }
 
+  verifyIfStepGoal(user) {
+    return user.dailyStepGoal <= this.numSteps
+  }
+
+  getStepMiles(user) {
+    const distanceInFeet = user.strideLength * this.numSteps
+    5280 / distanceInFeet
   }
 
 

ecd265f63d09e2727d54ac2d9c675a355157ed80
conconartist <conconart.info@gmail.com>
2020-12-11 21:42:32 -0700

    Refactor Hydration test file

--- tests/Hydration-test.js
+++ tests/Hydration-test.js
@@ -2,7 +2,6 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Hydration = require('../src/Hydration');
-// const User = require('../src/User');
 
 describe('Hydration', () => {
   let hydration1, hydration10, hydration17;

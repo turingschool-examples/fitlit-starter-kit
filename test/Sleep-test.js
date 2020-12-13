db179f8d686c1bdfa2bc5a33c095a327d9610b67
Brian Forbes <brianandrewf81@gmail.com>
2020-12-12 20:16:13 -0700

    Test written and pending, set up complete, ready for more unit tests

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -2,10 +2,10 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Sleep = require('../src/Sleep');
-const SleepRepo = require('../src/SleepRepo');
+// const SleepRepo = require('../src/SleepRepo');
 
 describe('Hydration', () => {
-  let sleepData, oneData;
+  let sleepData, instanceData;
 
   beforeEach(() => {
     sleepData = [
@@ -16,52 +16,81 @@ describe('Hydration', () => {
         "sleepQuality": 2.5
       },
       {
-        "userID": 1,
+        "userID": 2,
         "date": "2019/06/15",
         "hoursSlept": 7,
         "sleepQuality": 3
       },
       {
         "userID": 1,
-        "date": "2019/06/15",
+        "date": "2019/06/16",
         "hoursSlept": 6.1,
         "sleepQuality": 2.2
-      }
+      },
       {
-        "userID": 1,
-        "date": "2019/06/15",
+        "userID": 2,
+        "date": "2019/06/16",
         "hoursSlept": 6.3,
         "sleepQuality": 2.3
       },
       {
         "userID": 1,
-        "date": "2019/06/15",
+        "date": "2019/06/17",
         "hoursSlept": 7,
         "sleepQuality": 3
       },
       {
-        "userID": 1,
-        "date": "2019/06/15",
+        "userID": 2,
+        "date": "2019/06/17",
         "hoursSlept": 6.1,
         "sleepQuality": 2.2
       },
       {
         "userID": 1,
-        "date": "2019/06/15",
+        "date": "2019/06/18",
         "hoursSlept": 6.6,
         "sleepQuality": 2.5
       },
       {
+        "userID": 2,
+        "date": "2019/06/18",
+        "hoursSlept": 7,
+        "sleepQuality": 3
+      },
+      {
         "userID": 1,
-        "date": "2019/06/15",
+        "date": "2019/06/19",
+        "hoursSlept": 7,
+        "sleepQuality": 3
+      },
+      {
+        "userID": 2,
+        "date": "2019/06/19",
+        "hoursSlept": 6.1,
+        "sleepQuality": 2.2
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/20",
+        "hoursSlept": 6.6,
+        "sleepQuality": 2.5
+      },
+      {
+        "userID": 2,
+        "date": "2019/06/20",
         "hoursSlept": 7,
         "sleepQuality": 3
       },
     ];
-    oneData = new Sleep(sleepData[0]);
+    instanceData = new Sleep(sleepData[0]);
 
   });
 
-  it.skip()
+  it.skip('should have an appropriate constructor', () => {
+    expect(instanceData.id).to.equal(1);
+    expect(instanceData.date).to.equal('2019/06/15');
+    expect(instanceData.hoursSlept).to.equal(6.6);
+    expect(instanceData.sleepQuality).to.equal(2.5);
+  });
 
-};
+});

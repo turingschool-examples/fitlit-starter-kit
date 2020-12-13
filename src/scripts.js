b33a79ff01a20d1e850199bd4ab117e600dcc688
Cole Fiscus <coletfiscus@gmail.com>
2020-12-12 23:51:01 -0700

    Edit styling, add color, split data

--- src/scripts.js
+++ src/scripts.js
@@ -53,15 +53,23 @@ function start() {
 
 function displayUserInfoCard(user) {
   userInfoCard.innerHTML = `
-    <h1 class="app-name">FitLit</h1>
-    <h2 class="user-greeting">Hello, ${user.returnFirstName()}</h2>
-    <p>User ID: ${user.id}</p>
-    <p>Name: ${user.name}</p>
-    <p>Email: ${user.email}</p>
-    <p>Stride length: ${user.strideLength}</p>
-    <p>Friends: ${user.friends}</p>
-    <p>Daily Step Goal: ${user.dailyStepGoal}</p>
-    <p>Avg User Step Goal: ${userRepository.calculateAverageStepGoal()}</p>`;
+    <div class="app-name-and-greeting">
+      <h1>FitLit !</h1>
+      <h2>Hello, ${user.returnFirstName()}!</h2>
+    </div>
+    <div class="widget">
+      <p>User ID: ${user.id}</p>
+      <p>Name: ${user.name}</p>
+      <p>Email: ${user.email}</p>
+    </div>
+    <div class="widget">
+      <p>Friends: ${user.friends}</p>
+    </div>
+    <div class="widget">
+      <p>Stride length: ${user.strideLength}</p>
+      <p>Daily Step Goal: ${user.dailyStepGoal}</p>
+      <p>Avg User Step Goal: ${userRepository.calculateAverageStepGoal()}</p>
+    </div>`;
 }
 
 function displayHydrationInfo(user, date) {
@@ -69,25 +77,34 @@ function displayHydrationInfo(user, date) {
   // pastWeekHydrationObjects.forEach(hydration => hydration.date = moment(hydration.date, "MMM-DD")
   hydrationSection.innerHTML = `
     <h2>HYDRATION!</h2>
-    <p>Water consumed today: ${hydrationRepository.returnOuncesByDate(user.id, date)} ounces</p>
-    <p>Water consumed the past week:</p>
-    <p>${pastWeekHydrationObjects[0].date}: ${pastWeekHydrationObjects[0].numOunces} ounces</p>
-    <p>${pastWeekHydrationObjects[1].date}: ${pastWeekHydrationObjects[1].numOunces} ounces</p>
-    <p>${pastWeekHydrationObjects[2].date}: ${pastWeekHydrationObjects[2].numOunces} ounces</p>
-    <p>${pastWeekHydrationObjects[3].date}: ${pastWeekHydrationObjects[3].numOunces} ounces</p>
-    <p>${pastWeekHydrationObjects[4].date}: ${pastWeekHydrationObjects[4].numOunces} ounces</p>
-    <p>${pastWeekHydrationObjects[5].date}: ${pastWeekHydrationObjects[5].numOunces} ounces</p>
-    <p>${pastWeekHydrationObjects[6].date}: ${pastWeekHydrationObjects[6].numOunces} ounces</p>`;
+    <div class="widget">
+      <p>Water consumed today: ${hydrationRepository.returnOuncesByDate(user.id, date)} ounces</p>
+    </div>
+    <div class="widget">
+      <p>Water consumed the past week:</p>
+      <p>${pastWeekHydrationObjects[0].date}: ${pastWeekHydrationObjects[0].numOunces} ounces</p>
+      <p>${pastWeekHydrationObjects[1].date}: ${pastWeekHydrationObjects[1].numOunces} ounces</p>
+      <p>${pastWeekHydrationObjects[2].date}: ${pastWeekHydrationObjects[2].numOunces} ounces</p>
+      <p>${pastWeekHydrationObjects[3].date}: ${pastWeekHydrationObjects[3].numOunces} ounces</p>
+      <p>${pastWeekHydrationObjects[4].date}: ${pastWeekHydrationObjects[4].numOunces} ounces</p>
+      <p>${pastWeekHydrationObjects[5].date}: ${pastWeekHydrationObjects[5].numOunces} ounces</p>
+      <p>${pastWeekHydrationObjects[6].date}: ${pastWeekHydrationObjects[6].numOunces} ounces</p>
+    </div>`;
 }
 
 function displaySleepInfo(user, date) {
   const pastWeekSleepObjects = sleepRepository.getSleepDataByWeek(user.id, date);
   sleepSection.innerHTML = `
     <h2>SLEEP!</h2>
-    <p>Hours slept last night: ${sleepRepository.getSleepHoursByDate(user.id, date)}</p>
-    <p>Sleep quality last night: ${sleepRepository.getSleepQualityByDate(user.id, date)}</p>
-    <p>All-time average sleep quality: ${sleepRepository.getUserAvgSleepQualityAllTime(user.id)}</p>
-    <p>All-time average hours slept: ${sleepRepository.getUserAvgHoursSleptAllTime(user.id)}</p>
+    <div class="widget">
+      <p>Hours slept last night: ${sleepRepository.getSleepHoursByDate(user.id, date)}</p>
+      <p>Sleep quality last night: ${sleepRepository.getSleepQualityByDate(user.id, date)}</p>
+    </div>
+    <div class="widget">
+      <p>All-time average sleep quality: ${sleepRepository.getUserAvgSleepQualityAllTime(user.id)}</p>
+      <p>All-time average hours slept: ${sleepRepository.getUserAvgHoursSleptAllTime(user.id)}</p>
+    </div>
+    <div class="widget">
     <p>Sleep stats for the week:</p>
     <p>${pastWeekSleepObjects[0].date}: ${pastWeekSleepObjects[0].hoursSlept} hours slept, ${pastWeekSleepObjects[0].sleepQuality} sleep quality</p>
     <p>${pastWeekSleepObjects[1].date}: ${pastWeekSleepObjects[1].hoursSlept} hours slept, ${pastWeekSleepObjects[1].sleepQuality} sleep quality</p>
@@ -96,20 +113,25 @@ function displaySleepInfo(user, date) {
     <p>${pastWeekSleepObjects[4].date}: ${pastWeekSleepObjects[4].hoursSlept} hours slept, ${pastWeekSleepObjects[4].sleepQuality} sleep quality</p>
     <p>${pastWeekSleepObjects[5].date}: ${pastWeekSleepObjects[5].hoursSlept} hours slept, ${pastWeekSleepObjects[5].sleepQuality} sleep quality</p>
     <p>${pastWeekSleepObjects[6].date}: ${pastWeekSleepObjects[6].hoursSlept} hours slept, ${pastWeekSleepObjects[6].sleepQuality} sleep quality</p>
-    `;
+    </div>`;
 }
 
 function displayActivityInfo(user, date) {
   const pastWeekActivityObjects = activityRepository.getActivityDataByWeek(user.id, date);
   activitySection.innerHTML = `
     <h2>ACTIVITY!</h2>
-    <p>Steps taken today: ${activityRepository.returnStepsTaken(user, date)}</p>
-    <p>Minutes active today: ${activityRepository.returnMinutesActive(user, date)}</p>
-    <p>Distance walked today: ${activityRepository.returnMilesWalked(user, date)} miles</p>
-    <p>Flights of stairs climbed today: ${activityRepository.returnStairs(user, date)}</p>
-    <p>All users steps taken today: ${activityRepository.getAllUserAvgSteps(date)}</p>
-    <p>All users minutes active today: ${activityRepository.getAllUserTotalMins(date)}</p>
-    <p>All users flights of stairs climbed today: ${activityRepository.getAllUserTotalStairs(date)}</p>
+    <div class="widget">
+      <p>Steps taken today: ${activityRepository.returnStepsTaken(user, date)}</p>
+      <p>Minutes active today: ${activityRepository.returnMinutesActive(user, date)}</p>
+      <p>Distance walked today: ${activityRepository.returnMilesWalked(user, date)} miles</p>
+      <p>Flights of stairs climbed today: ${activityRepository.returnStairs(user, date)}</p>
+    </div>
+    <div class="widget">
+      <p>All users steps taken today: ${activityRepository.getAllUserAvgSteps(date)}</p>
+      <p>All users minutes active today: ${activityRepository.getAllUserTotalMins(date)}</p>
+      <p>All users flights of stairs climbed today: ${activityRepository.getAllUserTotalStairs(date)}</p>
+    </div>
+    <div class="widget">
     <p>Activity stats for the week:</p>
     <p>${pastWeekActivityObjects[0].date}: ${pastWeekActivityObjects[0].numSteps} steps taken, ${pastWeekActivityObjects[0].minutesActive} minutes active, ${pastWeekActivityObjects[0].flightsOfStairs} flights of stairs climbed</p>
     <p>${pastWeekActivityObjects[1].date}: ${pastWeekActivityObjects[1].numSteps} steps taken, ${pastWeekActivityObjects[1].minutesActive} minutes active, ${pastWeekActivityObjects[0].flightsOfStairs} flights of stairs climbed</p>
@@ -118,5 +140,5 @@ function displayActivityInfo(user, date) {
     <p>${pastWeekActivityObjects[4].date}: ${pastWeekActivityObjects[4].numSteps} steps taken, ${pastWeekActivityObjects[4].minutesActive} minutes active, ${pastWeekActivityObjects[0].flightsOfStairs} flights of stairs climbed</p>
     <p>${pastWeekActivityObjects[5].date}: ${pastWeekActivityObjects[5].numSteps} steps taken, ${pastWeekActivityObjects[5].minutesActive} minutes active, ${pastWeekActivityObjects[0].flightsOfStairs} flights of stairs climbed</p>
     <p>${pastWeekActivityObjects[6].date}: ${pastWeekActivityObjects[6].numSteps} steps taken, ${pastWeekActivityObjects[6].minutesActive} minutes active, ${pastWeekActivityObjects[0].flightsOfStairs} flights of stairs climbed</p>
-    `;
+    </div>`;
 }

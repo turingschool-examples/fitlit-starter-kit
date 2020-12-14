2100c842530449179f87484007763729295b449a
Brian Forbes <brianandrewf81@gmail.com>
2020-12-13 21:09:43 -0700

    Write tests for longest sleeper,and user with weekly sleep quality 3 or greater

--- test/SleepRepo-test.js
+++ test/SleepRepo-test.js
@@ -21,6 +21,12 @@ describe('SleepRepo', () => {
       "sleepQuality": 2.1
       },
       {
+      "userID": 3,
+      "date": "2019/06/15",
+      "hoursSlept": 7.5,
+      "sleepQuality": 3.3
+      },
+      {
       "userID": 1,
       "date": "2019/06/16",
       "hoursSlept": 6.2,
@@ -31,6 +37,12 @@ describe('SleepRepo', () => {
       "date": "2019/06/16",
       "hoursSlept": 6.3,
       "sleepQuality": 2.2
+      },
+      {
+      "userID": 3,
+      "date": "2019/06/16",
+      "hoursSlept": 7.5,
+      "sleepQuality": 3.3
       },
       {
       "userID": 1,
@@ -45,6 +57,12 @@ describe('SleepRepo', () => {
       "sleepQuality": 2.4
       },
       {
+      "userID": 3,
+      "date": "2019/06/17",
+      "hoursSlept": 7.5,
+      "sleepQuality": 3.3
+      },
+      {
       "userID": 1,
       "date": "2019/06/18",
       "hoursSlept": 6.6,
@@ -55,6 +73,12 @@ describe('SleepRepo', () => {
       "date": "2019/06/18",
       "hoursSlept": 6.7,
       "sleepQuality": 2.6
+      },
+      {
+      "userID": 3,
+      "date": "2019/06/18",
+      "hoursSlept": 7.5,
+      "sleepQuality": 3.3
       },
       {
       "userID": 1,
@@ -69,6 +93,12 @@ describe('SleepRepo', () => {
       "sleepQuality": 2.7
       },
       {
+      "userID": 3,
+      "date": "2019/06/19",
+      "hoursSlept": 7.5,
+      "sleepQuality": 3.3
+      },
+      {
       "userID": 1,
       "date": "2019/06/20",
       "hoursSlept": 7.1,
@@ -79,6 +109,12 @@ describe('SleepRepo', () => {
       "date": "2019/06/20",
       "hoursSlept": 7.2,
       "sleepQuality": 3.0
+      },
+      {
+      "userID": 3,
+      "date": "2019/06/20",
+      "hoursSlept": 7.5,
+      "sleepQuality": 3.3
       },
       {
       "userID": 1,
@@ -93,6 +129,12 @@ describe('SleepRepo', () => {
       "sleepQuality": 2.6
       },
       {
+      "userID": 3,
+      "date": "2019/06/21",
+      "hoursSlept": 7.5,
+      "sleepQuality": 3.3
+      },
+      {
       "userID": 1,
       "date": "2019/06/22",
       "hoursSlept": 6.0,
@@ -103,6 +145,12 @@ describe('SleepRepo', () => {
       "date": "2019/06/22",
       "hoursSlept": 6.2,
       "sleepQuality": 2.1
+      },
+      {
+      "userID": 3,
+      "date": "2019/06/22",
+      "hoursSlept": 7.5,
+      "sleepQuality": 3.3
       },
       {
       "userID": 1,
@@ -117,6 +165,12 @@ describe('SleepRepo', () => {
       "sleepQuality": 2.1
       },
       {
+      "userID": 3,
+      "date": "2019/06/23",
+      "hoursSlept": 7.5,
+      "sleepQuality": 3.3
+      },
+      {
       "userID": 1,
       "date": "2019/06/24",
       "hoursSlept": 6.2,
@@ -127,6 +181,12 @@ describe('SleepRepo', () => {
       "date": "2019/06/24",
       "hoursSlept": 6.3,
       "sleepQuality": 2.2
+      },
+      {
+      "userID": 3,
+      "date": "2019/06/24",
+      "hoursSlept": 7.5,
+      "sleepQuality": 3.3
       }
     ];
 
@@ -141,16 +201,24 @@ describe('SleepRepo', () => {
       expect(sleepRepo.returnOverallAverageSleepQuality(1)).to.deep.equal(2.4);
     });
 
-    it.skip('should be able to return hours slept each day over the course of a week given a user and starting date', () => {
+    it.skip('should be able to return hours slept each day over the course of a week given a user and end date', () => {
       expect(sleepRepo.returnWeekOfDailyHoursSlept(1, "2019/06/21")).to.deep.equal([6, 6.2, 6.4, 6.6, 6.8, 7.1, 7.4]);
     });
 
-    it.skip('should be able to return sleep quality for each day over the course of a week given a user and starting date', () {
+    it.skip('should be able to return sleep quality for each day over the course of a week given a user and end date', () {
       expect(sleepRepo.returnWeekOfDailySleepQuality(1, "2019/06/21")).to.deep.equal([2, 2.2, 2.3, 2.5, 2.6, 2.9, 3.2]);
     });
 
     it.skip('should have a way to return the average sleep quality for a user given their user ID', () => {
       expect(sleepRepo.userAverageSleepQuality(1)).to.equal(2.4)
+    });
+
+    it.skip('should be able to return all users with average sleep quality 3 or greater for a week when given an end date', () => {
+      expect(sleepRepo.returnUserWithSleepQualityThreeOrGreater()).to.deep.equal(3);
+    });
+
+    it.skip('for a given date, should be able to find the user(s) who slept the most number of hours (one or more if they tied)', () => {
+      expect(sleepRepo.returnLongestSleeper("2019/06/15")).to.equal(3);
     });
 
 

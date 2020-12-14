65579e546bc0ea84f72440dda93a57ef4df1acd8
mainlyetcetera <ericcampbell294@gmail.com>
2020-12-14 16:40:27 -0700

    Expect instance to return whether step goal has been reached

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -2,9 +2,10 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Activity = require('../src/Activity');
+const UserRepo = require('../src/UserRepo');
 
 describe('Activity', () => {
-  let data, activity;
+  let data, activity, dataset, userRepo;
 
   beforeEach(() => {
     data = {
@@ -16,6 +17,60 @@ describe('Activity', () => {
     };
 
     activity = new Activity(data);
+
+    dataset = [
+      {
+        "id": 1,
+        "name": "Brian Forbes",
+        "address": "123 Blah St, Denver CO, 66666",
+        "email": "stuffandthings@gmail.com",
+        "strideLength": 2.4,
+        "dailyStepGoal": 10000,
+        "friends": [
+          2,
+          3,
+          4
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Eric Campbell",
+        "address": "123 SomeOther St, Denver CO, 66666",
+        "email": "mainlyetcetera@gmail.com",
+        "strideLength": 2.3,
+        "dailyStepGoal": 5,
+        "friends": [
+          1,
+          3
+        ]
+      },
+      {
+        "id": 3,
+        "name": "David Whitaker",
+        "address": "124 Random Lane, Denver CO, 66666",
+        "email": "damwhitmaybeidontknow@gmail.com",
+        "strideLength": 2.6,
+        "dailyStepGoal": 10000,
+        "friends": [
+          1,
+          4
+        ]
+      },
+      {
+        "id": 4,
+        "name": "Travis Rollins",
+        "address": "234 Very Random Street, Denver CO, 66666",
+        "email": "laskdjfaslkdj@gmail.com",
+        "strideLength": 2.1,
+        "dailyStepGoal": 10000,
+        "friends": [
+          1,
+          3
+        ]
+      }
+    ]
+
+    userRepo = new UserRepo(dataset);
   });
 
   it.skip('should have an appropriate constructor', () => {
@@ -30,5 +85,14 @@ describe('Activity', () => {
     const minutes = activity.returnMinutes();
 
     expect(minutes).to.equal(data.minutesActive);
+  });
+
+  it.skip('should return whether step goal was reached', () => {    
+    const user = userRepo.data.find(user => user.id === activity.id);
+    const goal = user.dailyStepGoal;
+    const didHitGoal = activity.checkStepGoal();
+
+    expect(activity.steps).to.be.at.least(user.datilyStepGoal);
+    expect(didHitGoal).to.equal(true);    
   });
 });

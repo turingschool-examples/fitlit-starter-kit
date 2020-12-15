aa16991c056dc524dabae5d00d0c1790b6770bcd
conconartist <conconart.info@gmail.com>
2020-12-14 19:08:19 -0700

    Add to CommunitySleep class

--- src/CommunitySleep.js
+++ src/CommunitySleep.js
@@ -8,7 +8,24 @@ class CommunitySleep {
     const userSleep = this.sleeps.filter(sleeper => (sleeper.userID === userID));
     const amountSlept = userSleep.map(sleeper => sleeper.hoursSlept);
     const avgHoursSlept = (amountSlept.reduce((a, b) => a + b, 0)) / amountSlept.length;
-    return avgHoursSlept.toFixed(1);
+    return Math.round(avgHoursSlept * 10) / 10;
+  }
+  calculateAvgSleepQualPerDay(userID) {
+    const userSleep = this.sleeps.filter(sleeper => (sleeper.userID === userID));
+    const sleepQuality = userSleep.map(sleeper => sleeper.sleepQuality);
+    const avgSleepQuality = (sleepQuality.reduce((a, b) => a + b, 0)) / sleepQuality.length;
+    return Math.round(avgSleepQuality * 10) / 10;
+  }
+  findHrsSleptOnDay(userID, date) {
+    const hoursSlept = this.sleeps.find(sleeper => sleeper.userID === userID && sleeper.date === date);
+    return hoursSlept.hoursSlept;
+  }
+  findSleepQualityOnDay(userID, date) {
+    const sleepQuality = this.sleeps.find(sleeper => sleeper.userID === userID && sleeper.date === date);
+    return sleepQuality.sleepQuality;
+  }
+  calculateSleepWeek(userID, startDate, endDate) {
+    
   }
 }
 module.exports = CommunitySleep;

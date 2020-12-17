d7bc7bae8eccf0263db40aa63b20c21e5491d0f6
srslie <aliceruppert@gmail.com>
2020-12-17 01:10:42 -0700

    Refactor and reorganize scripts

--- src/scripts.js
+++ src/scripts.js
@@ -1,239 +1,125 @@
 /* eslint-disable max-len */
-const userMain = document.querySelector('.user-main')
-const greeting = document.querySelector('.greeting')
-const userProfile = document.querySelector('.user-profile')
-const friends = document.querySelector('.friends')
-const steps = document.querySelector('.steps')
-const water = document.querySelector('.water');
-const activeMinutes = document.querySelector('.active-minutes')
-const stairs = document.querySelector('.stairs');
-const userLatestSleep = document.querySelector('.user-latest-sleep-stats')
-const sleepHours = document.querySelector('.sleep-hours')
-const sleepQuality = document.querySelector('.sleep-quality')
-const todayDateSelector = document.getElementById("today")
-const startDateSelector = document.getElementById("start")
-const endDateSelector = document.getElementById("end")
-const graph = document.querySelector('.graph');
-const graphTitle = document.querySelector('.title-graph');
 
-//since multiple methods will need these, global
-let community = new UserRepository(userData);
-let user = community.users[0];
-let communityHydration = new CommunityHydration(hydrationData);
-let hydration = communityHydration.hydrations[0];
-let communityActivity = new CommunityActivity(activityData);
-let activity = communityActivity.activities[0];
-let communitySleep = new CommunitySleep(sleepData);
-let sleep = communitySleep.sleeps[0];
+//GLOBAL VARIABLES
 let today = '2019/09/22'
 let startDate = '2019/09/16'
 let endDate = '2019/09/22'
 
+const community = new UserRepository(userData);
+const user = community.users[0];
+const communityHydration = new CommunityHydration(hydrationData);
+const communityActivity = new CommunityActivity(activityData);
+const activity = communityActivity.activities[0]
+const communitySleep = new CommunitySleep(sleepData);
+const sleep = communitySleep.sleeps[0]
 
+const weekActivities = communityActivity.findWeekActivities(startDate, endDate, user)
+const weekSleep = communitySleep.calculateSleepStatsWeek(user.userID, startDate, endDate)
+const weekWater = communityHydration.calculateTotalWeek(user.userID, startDate, endDate)
+
+//EVENT LISTENERS
 window.addEventListener('load', loadPage);
-todayDateSelector.addEventListener("change", function() {today = this.value.split('-').join('/')});
-startDateSelector.addEventListener("change", function() {startDate = this.value.split('-').join('/')});
-endDateSelector.addEventListener("change", function() {endDate = this.value.split('-').join('/')});
+document.getElementById("today").addEventListener("change", function() {today = this.value.split('-').join('/')});
+document.getElementById("start").addEventListener("change", function() {startDate = this.value.split('-').join('/')});
+document.getElementById("end").addEventListener("change", function() {endDate = this.value.split('-').join('/')});
 
-//GREETING:
-function greetUser() {
-  greeting.insertAdjacentHTML('afterbegin', `<h2 class="greeting">Welcome ${user.getFirstName()}!</h2>`)
+//GREETING & PROFILE:
+const greetAndShowProfile = () => {
+  document.querySelector('.greeting-input').innerText =  `Welcome ${user.getFirstName()}!`
+  document.querySelector('.user-name').innerText =  `Name: ${user.name}`
+  document.querySelector('.user-address').innerText = `Address: ${user.address}`
+  document.querySelector('.user-email').innerText = `Email: ${user.email}`
+  document.querySelector('.user-stride-length').innerText = `Stride Length: ${user.strideLength} ft`
+  document.querySelector('.user-friends-length').innerText = `FitFriends: ${user.friends.length}`
 }
 
-//USER DATA:
-const showProfile = () => {
-  userProfile.insertAdjacentHTML('beforeend', `Name: ${user.name}<br>
-  Address: ${user.address}<br>
-  Email: ${user.email}<br>
-  Stride Length: ${user.strideLength} ft<br>
-  FitFriends: ${user.friends.length}
-  `)
+//If we want to do week display in just a text format too, this and the "week-list-data" section I added in HTML would do that:
+
+//HELPER FUNCTION FOR WEEKSTAT DISPLAY
+const displayStatsForWeek = (week, fitnessType, displayArea) => {
+  const display = week.map(day => `<p>${day.date}: ${day[fitnessType]}</p>`).join('')
+  displayArea.innerHTML = `<h2>Display of ${fitnessType}:</h2><p>${display}</p>`
 }
 
-//ADD TODAY SLEEP STATS:
-const showLatestSleepStatsUser = () => {
-  userLatestSleep.insertAdjacentHTML('beforeend', `
-    <p class="sleep-quantity-last-night">You slept ${communitySleep.findHrsSleptOnDay(sleep.userID, today)} hrs</p>
-    <p class="sleep-quality-last-night">Your sleep quality score: ${communitySleep.findSleepQualityOnDay(sleep.userID, today)}</p>
-    `)
+//DISPLAY WEEK STATS
+const displayWeekStats = () => {
+  displayStatsForWeek(weekActivities, 'minutesActive', document.querySelector('.week-minutes-active'))
+  displayStatsForWeek(weekActivities, 'numSteps', document.querySelector('.week-steps'))
+  displayStatsForWeek(weekWater, 'numOunces', document.querySelector('.week-water'))
+  displayStatsForWeek(weekSleep, 'hoursSlept', document.querySelector('.week-sleep-hours'))
+  displayStatsForWeek(weekSleep, 'numSteps', document.querySelector('.week-sleep-quality'))
 }
 
-//ADD ALL-TIME SLEEP STATS:
-//sleep dada for latest day: hours slept and quality of sleep
-const showAllSleepQuantity = () => {
-  sleepHours.insertAdjacentHTML('afterbegin', `You average ${communitySleep.calculateAvgSleepHrsPerDay(sleep.userID)} hrs/day`)
-}
-
-const showAllSleepQuality = () => {
-  sleepQuality.insertAdjacentHTML('afterbegin', `You average a score of ${communitySleep.calculateAvgSleepQualPerDay(sleep.userID)} /day`)
-}
-//ADD WEEK SLEEP STATS:
-//hours slept and quality of sleep
-const showSleepStatsWeek = (startDate, endDate) => {
-  graphTitle.insertAdjacentHTML('afterend', `
-    <article class="sleep-stats-week">Hours slept over the week of ${startDate}-${endDate}:
-    <p>Day 1: ${communitySleep.calculateSleepStatsWeek(sleep.userID, startDate, endDate)[0].hoursSlept} HRS</p>
-    <p>Day 2: ${communitySleep.calculateSleepStatsWeek(sleep.userID, startDate, endDate)[1].hoursSlept} HRS</p>
-    <p>Day 3: ${communitySleep.calculateSleepStatsWeek(sleep.userID, startDate, endDate)[2].hoursSlept} HRS</p>
-    <p>Day 4: ${communitySleep.calculateSleepStatsWeek(sleep.userID, startDate, endDate)[3].hoursSlept} HRS</p>
-    <p>Day 5: ${communitySleep.calculateSleepStatsWeek(sleep.userID, startDate, endDate)[4].hoursSlept} HRS</p>
-    <p>Day 6: ${communitySleep.calculateSleepStatsWeek(sleep.userID, startDate, endDate)[5].hoursSlept} HRS</p>
-    <p>Day 7: ${communitySleep.calculateSleepStatsWeek(sleep.userID, startDate, endDate)[6].hoursSlept} HRS</p>
-    </article>
-  `)
-}
-
-//ADD TODAY's HYDRATION STATS:
+//DISPLAY HYDRATION STATS:
 const showHydrationStats = () => {
-  water.insertAdjacentHTML('beforeend',
-    `<p class="water-stats">Water consumed today: ${communityHydration.calculateTotalWaterOnDay(hydration.userID, today)} OZ</p>
-    `)
+  document.querySelector('.water-stats').innerText = `Water consumed today: ${communityHydration.calculateTotalWaterOnDay(user.userID, today)} OZ`
 }
 
-//ADD WEEKLY HYDRATION STATS:
-const showHydrationStatsWeek = (startDate, endDate) => {
-  graphTitle.insertAdjacentHTML('afterend', `
-    <article class="water-stats-week">Water consumed over the week of ${startDate}-${endDate}:
-    <p>Day 1: ${communityHydration.calculateTotalWeek(hydration.userID, startDate, endDate)[0].numOunces} OZ</p>
-    <p>Day 2: ${communityHydration.calculateTotalWeek(hydration.userID, startDate, endDate)[1].numOunces} OZ</p>
-    <p>Day 3: ${communityHydration.calculateTotalWeek(hydration.userID, startDate, endDate)[2].numOunces} OZ</p>
-    <p>Day 4: ${communityHydration.calculateTotalWeek(hydration.userID, startDate, endDate)[3].numOunces} OZ</p>
-    <p>Day 5: ${communityHydration.calculateTotalWeek(hydration.userID, startDate, endDate)[4].numOunces} OZ</p>
-    <p>Day 6: ${communityHydration.calculateTotalWeek(hydration.userID, startDate, endDate)[5].numOunces} OZ</p>
-    <p>Day 7: ${communityHydration.calculateTotalWeek(hydration.userID, startDate, endDate)[6].numOunces} OZ</p>
-    </article>
-  `)
-}
-
-//STEPS STATS:
-const showStepGoalComparison = () => {
-  steps.insertAdjacentHTML('beforeend',
-  `<h2>Steps</h2>
-  <article class="user-step-goal">
-    <h3>Your Daily Step Goal</h3>
-      <p>${community.findAverageStepGoal()}</p>
-  </article>
-  <article class="community-step-goal">
-    <h3>Community Average Step Goal</h3>
-      <p>${user.dailyStepGoal}</p>
-  </article>`)
-}
-
+//DISPLAY STEPS STATS:
 const showIfUserMetStepGoal = () => {
+  const goalDisplay = document.querySelector('.user-met-step-goal-today')
   if (activity.verifyIfStepGoal(user)) {
-    steps.innerHTML = '<p>You\'ve met your Daily Step Goal!</p>'
+    goalDisplay.innerHTML = '<p>You\'ve met your Daily Step Goal!</p>'
   } else {
-    steps.innerHTML = '<p>Keep working toward your Daily Step Goal.</p>'
+    goalDisplay.innerHTML = '<p>Keep working toward your Daily Step Goal.</p>'
   }
 }
 
-const showUserStepsInMiles = () => {
-  steps.insertAdjacentHTML('beforeend', `<p>Miles: ${activity.getStepMiles(user)}</p>`)
-}
-
-const showStepsToday = () => {
-  steps.insertAdjacentHTML('beforeend', `<p>Steps: ${activity.numSteps}</p>`)
-}
-
-const showCommunityStepsToday = () => {
-  steps.insertAdjacentHTML('beforeend', `<p>Community Average Steps: ${communityActivity.findCommunityAverage(today, 'numSteps')}</p>`)
-}
-
-const showNumberOfDaysExceedingStepGoal = () => {
-  steps.insertAdjacentHTML('beforeend', `<p>Days Step Goal Has Been Met: ${communityActivity.daysExceedingStepGoal(user).length}</p>`)
-}
-
-
-//ADD ACTIVITY STATS:
-
-const showMinutesActiveToday = () => {
-  activeMinutes.insertAdjacentHTML('beforeend', `<p>Minutes Active: ${activity.minutesActive}</p>`)
-}
-
-const showCommunityActiveMinutesToday = () => {
-  activeMinutes.insertAdjacentHTML('beforeend', `<p>Community Average Minutes Active: ${communityActivity.findCommunityAverage(today, 'minutesActive')}</p>`)
-}
-
-const showUserActiveMinutesWeekAverage = () => {
-  activeMinutes.insertAdjacentHTML('beforeend', `<p>Weekly Average Minutes Active: ${communityActivity.findWeekActiveMinutesAverage("2019/09/16", "2019/09/22", user)}</p>`)
-}
-
-const showCommunityStairsToday = () => {
-  stairs.insertAdjacentHTML('beforeend', `<p>Community Average Stairs Climbed: ${communityActivity.findCommunityAverage(today, 'stairsClimbed')}</p>`)
-}
-
-const showUserStairRecord = () => {
-  stairs.insertAdjacentHTML('beforeend', `<p>Your Record Stairs Climbed: ${communityActivity.findRecordStairs(user)}</p>`)
-}
-
-const showStepStatsWeek = (startDate, endDate) => {
-  let weekActivities = communityActivity.findWeekActivities(startDate, endDate, user)
-
-  let activitiesDisplay = weekActivities.reduce((display, activity) => {
-    display += `<p>${activity.date}: ${activity.numSteps} steps</p>`
-    return display
-  }, '')
-
-  graphTitle.insertAdjacentHTML('afterend',
-    `<article class="steps-stats-week">Steps taken over the week of ${startDate}-${endDate}:` + activitiesDisplay + '</article>')
-}
-
-const showMinutesActiveStatsWeek = (startDate, endDate) => {
-  let weekActivities = communityActivity.findWeekActivities(startDate, endDate, user)
-
-  let activitiesDisplay = weekActivities.reduce((display, activity) => {
-    display += `<p>${activity.date}: ${activity.minutesActive} minutes active</p>`
-    return display
-  }, '')
-
-  graphTitle.insertAdjacentHTML('afterend',
-    `<article class="minutes-active-stats-week">Minutes Active over the week of ${startDate}-${endDate}:` + activitiesDisplay + '</article>')
-}
-
-const showActivitiesChartWhateverWeek = (startDate, endDate) => {
-  let weekActivities = communityActivity.findWeekActivities(startDate, endDate, user)
-
-  let activitiesDisplay = weekActivities.reduce((display, activity) => {
-    display += `<p>${activity.date}: ${activity.stairsClimbed} stairs climbed</p>`
-    return display
-  }, '')
-
-  graphTitle.insertAdjacentHTML('afterend',
-    `<article class="stairs-stats-week">Stairs Climbed over the week of ${startDate}-${endDate}:` + activitiesDisplay + '</article>')
-}
-
-const showSleepStats = () => {
-  showLatestSleepStatsUser();
-  showAllSleepQuantity();
-  showAllSleepQuality();
-  showSleepStatsWeek("2019/09/16", "2019/09/22");
-}
-
-const showActivityStats = () => {
+const showStepStats = () => {
   showIfUserMetStepGoal()
-  showNumberOfDaysExceedingStepGoal()
-  showStepsToday()
-  showUserStepsInMiles()
-  showCommunityStepsToday()
-  showMinutesActiveToday()
-  showCommunityActiveMinutesToday()
-  showUserActiveMinutesWeekAverage()
-  showCommunityStairsToday()
-  showUserStairRecord()
-  // showStepStatsWeek("2019/09/16", "2019/09/22")
-  // showMinutesActiveStatsWeek("2019/09/16", "2019/09/22")
- 
+  document.querySelector('.user-step-goal-input').innerText = `${user.dailyStepGoal}`
+  document.querySelector('.community-step-goal-input').innerText = `${community.findAverageStepGoal()}`
+  document.querySelector('.user-step-miles').innerHTML = `Miles: ${activity.getStepMiles(user)}`
+  document.querySelector('.user-steps-today').innerHTML = `Steps: ${activity.numSteps}`
+  document.querySelector('.user-days-exceeding-goal').innerHTML = `Community Average Steps: ${communityActivity.findCommunityAverage(today, 'numSteps')}`
+  document.querySelector('.community-steps-today').innerHTML = `Days Step Goal Has Been Met: ${communityActivity.daysExceedingStepGoal(user).length}`
+}
+
+//DISPLAY ACTIVITY STATS:
+const showMinutesActiveStats = () => {
+  document.querySelector('.user-minutes-active').innerText = `Minutes Active: ${activity.minutesActive}`
+  document.querySelector('.user-week-average-minutes-active').innerText = `Community Average Minutes Active: ${communityActivity.findCommunityAverage(today, 'minutesActive')}`
+  document.querySelector('.community-average-minutes-active').innerText = `Weekly Average Minutes Active: ${communityActivity.findWeekActiveMinutesAverage(startDate, endDate, user)}`
+}
+
+//DISPLAY STAIRS STATS:
+const showStairsStats = () => {
+  document.querySelector('.community-average-stairs').innerText = `Community Average Stairs Climbed: ${communityActivity.findCommunityAverage(today, 'stairsClimbed')}`
+  document.querySelector('.user-record-stairs').innerText =  `Your Record Stairs Climbed: ${communityActivity.findRecordStairs(user)}`
+}
+
+//DISPLAY SLEEP STATS
+const showSleepStats = () => {
+  document.querySelector('.sleep-quantity-last-night').innerText = `You slept ${communitySleep.findHrsSleptOnDay(user.userID, today)} hrs`
+  document.querySelector('.sleep-quality-last-night').innerText = `Your sleep quality score: ${communitySleep.findSleepQualityOnDay(user.userID, today)}`
+  document.querySelector('.sleep-average-hours-input').innerText = `You average ${communitySleep.calculateAvgSleepHrsPerDay(user.userID)} hrs/day`
+  document.querySelector('.sleep-average-quality-input').innerText = `You average a score of ${communitySleep.calculateAvgSleepQualPerDay(user.userID)}/day`
 }
 
 //FRIENDS:
 const showFriends = () => {
-  const friendDisplay = user.friends.map(friend => {
-    const friendName = community.getUserData(friend).getFirstName()
-    return `<div>${friendName}</div>`
-  })
-
-  friends.insertAdjacentHTML('afterbegin', friendDisplay.join(' '))
+  const friendsDisplay = user.friends.map(friend => `<div class="friend-name">${community.getUserData(friend).getFirstName()}</div>`)
+  document.querySelector('.friends-names').innerHTML = `${friendsDisplay.join('')}`
 }
+
+//we can add other calls to this onload function
+function loadPage() {
+  greetAndShowProfile()
+  displayWeekStats()
+  showHydrationStats()
+  showStepStats()
+  showMinutesActiveStats()
+  showStairsStats()
+  showSleepStats()
+  showFriends()
+}
+
+//GRAPH HELPER FUNCTIONS
+const mapWeek = (week, fitnessType) => week.map(day => day[fitnessType])
+
+const weekDates = weekActivities.map(day => day.date)
+
+const makeYs = (number) => new Array(weekActivities.length).fill(number)
 
 const findFriendSteps = (friend) => {
   friendData = community.getUserData(friend)
@@ -243,37 +129,8 @@ const findFriendSteps = (friend) => {
 
 const getFriendName = (friend) => community.getUserData(friend).getFirstName()
 
-
-//we can add other calls to this onload function
-function loadPage() {
-  greetUser()
-  showProfile()
-  showStepGoalComparison()
-  showHydrationStats()
-  showHydrationStatsWeek(startDate, endDate)
-  showFriends()
-  showActivityStats()
-  showSleepStats()
-}
-
-const weekActivities = communityActivity.findWeekActivities(startDate, endDate, user)
-const weekSleep = communitySleep.calculateSleepStatsWeek(sleep.userID, startDate, endDate)
-const weekWater = communityHydration.calculateTotalWeek(hydration.userID, startDate, endDate)
-
-const mapWeek = (week, fitnessType) => week.map(day => day[fitnessType])
-const weekDates = weekActivities.map(day => day.date)
-const makeYs = (number) => new Array(weekActivities.length).fill(number)
-
-
-const stepCanvas = document.getElementById('stepChart').getContext('2d')
-const waterCanvas = document.getElementById('waterChart').getContext('2d')
-const stairsCanvas = document.getElementById('stairsChart').getContext('2d')
-const activeMinutesCanvas = document.getElementById('activeMinutesChart').getContext('2d')
-const sleepCanvas = document.getElementById('sleepChart').getContext('2d')
-const friendCanvas = document.getElementById('friendChart').getContext('2d')
-
-//STEPS
-const stepChart = new Chart(stepCanvas, {
+//STEPS GRAPH
+const stepChart = new Chart(document.getElementById('stepChart').getContext('2d'), {
   type: 'bar',
   data: {
     labels: weekDates,
@@ -310,8 +167,9 @@ const stepChart = new Chart(stepCanvas, {
     }]
   }
 });
-//STAIRS
-const stairsChart = new Chart(stairsCanvas, {
+
+//STAIRS GRAPH
+const stairsChart = new Chart(document.getElementById('stairsChart').getContext('2d'), {
   type: 'bar',
   data: {
     labels: weekDates,
@@ -340,8 +198,9 @@ const stairsChart = new Chart(stairsCanvas, {
     }]
   }
 });
-//ACTIVE MINUTES
-const activeMinutesChart = new Chart(activeMinutesCanvas, {
+
+//ACTIVE MINUTES GRAPH
+const activeMinutesChart = new Chart(document.getElementById('activeMinutesChart').getContext('2d'), {
   type: 'bar',
   data: {
     labels: weekDates,
@@ -361,8 +220,9 @@ const activeMinutesChart = new Chart(activeMinutesCanvas, {
     }]
   }
 });
-//WATER
-const waterChart = new Chart(waterCanvas, {
+
+//WATER GRAPH
+const waterChart = new Chart(document.getElementById('waterChart').getContext('2d'), {
   type: 'bar',
   data: {
     labels: weekDates,
@@ -375,8 +235,9 @@ const waterChart = new Chart(waterCanvas, {
     }] 
   }
 });
-//SLEEP
-const sleepChart = new Chart(sleepCanvas, {
+
+//SLEEP GRAPH
+const sleepChart = new Chart(document.getElementById('sleepChart').getContext('2d'), {
   type: 'bar',
   data: {
     labels: weekDates,
@@ -412,8 +273,9 @@ const sleepChart = new Chart(sleepCanvas, {
     }]
   }
 });
-// FRIENDS
-const friendChart = new Chart(friendCanvas, {
+
+// FRIENDS GRAPH
+const friendChart = new Chart(document.getElementById('friendChart').getContext('2d'), {
   type: 'line',
   data: {
     labels: weekDates,

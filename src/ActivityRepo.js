44a3844ec935d3e7361068f4c396879f259eb75e
Brian Forbes <brianandrewf81@gmail.com>
2020-12-17 20:14:06 -0700

    Finish displaying week data for user given a date and user ID

--- src/ActivityRepo.js
+++ src/ActivityRepo.js
@@ -2,7 +2,7 @@ class ActivityRepo {
   constructor(activityData, userDataset) {
     this.activityData = activityData;
     this.userData = userDataset;
-    this.dailyAverages = {}; 
+    this.dailyAverages = {};
     this.weeklyAverages = {};
   }
 
@@ -15,21 +15,21 @@ class ActivityRepo {
     }
   }
 
-  findWeekDates(endDate) {    
+  findWeekDates(endDate) {
     const lastTwo = endDate.slice(-2);
     let diff;
     if (parseInt(lastTwo) < 7) {
       diff = 7 - parseInt(lastTwo);
     }
-    
-    let dates = [endDate, endDate, endDate, endDate, endDate, endDate, endDate];    
+
+    let dates = [endDate, endDate, endDate, endDate, endDate, endDate, endDate];
     let toRemove31 = diff - 1 || 0;
     let toRemove30 = diff - 1 || 0;
     let toRemove28 = diff - 1 || 0;
-    
-    const newDates = dates.reduce((acc, date, index) => {      
+
+    const newDates = dates.reduce((acc, date, index) => {
       const toRemove = 6 - index;
-      const month = endDate.slice(5, 7);      
+      const month = endDate.slice(5, 7);
       const has31 = [1, 3, 5, 7, 8, 10, 12];
       const has30 = [4, 6, 9, 11];
       const has28 = [2];
@@ -39,12 +39,12 @@ class ActivityRepo {
         if (newMonth.toString().length === 1) {
           newMonth = `0${newMonth}`
         }
-        
+
         if (newDate.toString().length === 1) {
           newDate = `0${newDate}`;
         }
-        
-        date = `2019/${newMonth}/${newDate}`;        
+
+        date = `2019/${newMonth}/${newDate}`;
         toRemove31 -= 1;
       } else if (parseInt(date.slice(-2)) - toRemove <= 0 && has30.includes(parseInt(month))) {
         let newMonth = parseInt(month) - 1;
@@ -52,11 +52,11 @@ class ActivityRepo {
         if (newMonth.toString().length === 1) {
           newMonth = `0${newMonth}`
         }
-        
+
         if (newDate.toString().length === 1) {
           newDate = `0${newDate}`;
         }
-        
+
         date = `2019/${newMonth}/${newDate}`;
         toRemove30 -= 1;
       } else if (parseInt(date.slice(-2)) - toRemove <= 0 && has28.includes(parseInt(month))) {
@@ -65,20 +65,20 @@ class ActivityRepo {
         if (newMonth.toString().length === 1) {
           newMonth = `0${newMonth}`
         }
-        
+
         if (newDate.toString().length === 1) {
           newDate = `0${newDate}`;
         }
-        
+
         date = `2019/${newMonth}/${newDate}`;
         toRemove28 -= 1;
-      } else {        
-        let newDate = parseInt(lastTwo) - toRemove;        
-        
+      } else {
+        let newDate = parseInt(lastTwo) - toRemove;
+
         if (newDate.toString().length === 1) {
           newDate = `0${newDate}`;
         }
-        
+
         date = endDate.slice(0, 8) + newDate;
       }
 
@@ -91,18 +91,18 @@ class ActivityRepo {
 
   findWeekAndUser(id, endDate) {
     const user = this.userData.find(data => data.id === id);
-    const week = this.findWeekDates(endDate);    
+    const week = this.findWeekDates(endDate);
     return {
       user: user,
       week: week
-    }    
+    }
   }
 
-  calculateMiles(id, date) {    
+  calculateMiles(id, date) {
     const allData = this.findDayAndUser(id, date);
     const stride = allData.user.strideLength;
     const miles = (allData.day.numSteps * stride) / 5280;
-    const formattedMiles = parseFloat(miles.toFixed(2));        
+    const formattedMiles = parseFloat(miles.toFixed(2));
     return formattedMiles;
   }
 
@@ -114,21 +114,21 @@ class ActivityRepo {
     return Math.round(data.reduce((total, day) => total + day, 0) / 7);
   }
 
-  calculateAvgMinutesActive(id, endDate) {    
+  calculateAvgMinutesActive(id, endDate) {
     return this.calculateAvgData(id, endDate, 'minutesActive');
-  }  
+  }
 
   calculateAvgSteps(id, endDate) {
-    return this.calculateAvgData(id, endDate, 'steps');
+    return this.calculateAvgData(id, endDate, 'numSteps');
   }
 
   calculateAvgStairs(id, endDate) {
-    return this.calculateAvgData(id, endDate, 'stairs');
+    return this.calculateAvgData(id, endDate, 'flightsOfStairs');
   }
 
   calculateDaysExceededGoal(id) {
     const goal = this.userData.find(user => user.id === id).dailyStepGoal;
-    return this.activityData.filter(data => data.numSteps >= goal && data.userID === id).map(data => data.date);    
+    return this.activityData.filter(data => data.numSteps >= goal && data.userID === id).map(data => data.date);
   }
 
   calculateStairRecord(id) {
@@ -141,7 +141,7 @@ class ActivityRepo {
     }, 0);
   }
 
-  calculateDailyAverages(date) {   
+  calculateDailyAverages(date) {
     const dateData = this.activityData.filter(data => data.date === date);
     const piecesOfData = dateData.length;
     const totals = dateData.reduce((totals, data) => {
@@ -168,7 +168,7 @@ class ActivityRepo {
   calculateWeeklyAverages(endDate) {
     const week = this.findWeekDates(endDate);
     const weeklyTotals = week.reduce((weekData, date) => {
-      this.calculateDailyAverages(date);      
+      this.calculateDailyAverages(date);
       if (!weekData.stairs && !weekData.steps && !weekData.minutesActive) {
         weekData.stairs = this.dailyAverages[date].stairs;
         weekData.steps = this.dailyAverages[date].steps;

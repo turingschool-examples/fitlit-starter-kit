f279c2a4de9953d63b8a83e9ca08295b92e6f1ee
mcraig2342 <matthewcookecraig10@gmail.com>
2021-03-29 17:14:24 -0600

    Create user repo tests, make createUsers and returnUserData methods

--- Test/UserRepository-test.js
+++ Test/UserRepository-test.js
@@ -1,3 +1,31 @@
 const chai = require('chai');
 const expect = chai.expect;
 const User = require ('../src/User');
+const UserRepository = require ('../src/UserRepository');
+const userTestingData = require ('../Test/User-TestingData.js');
+
+describe('User', () => {
+  let user, userRepository;
+  beforeEach(() => {
+    userRepository = new UserRepository(userTestingData);
+  });
+
+  it('should be a function', () => {
+    expect(UserRepository).to.be.a('function');
+  });
+
+  it('should be an instance of user repository', () => {
+    expect(userRepository).to.be.an.instanceof(UserRepository);
+  });
+
+  it('should be able to create and store multiple users', () => {
+    userRepository.createUsers(userTestingData);
+    expect(userRepository.users).to.be.an('array').with.a.lengthOf(3);
+  });
+
+  it('should be able to return user data if given an id', () => {
+    userRepository.createUsers(userTestingData);
+    expect(userRepository.returnUserData(1)).to.deep.equal(userRepository.userData[0]);
+  });
+
+});
--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,7 +1,28 @@
+const User = require ('../src/User');
+
 class UserRepository {
-  constructor() {
+  constructor(userData) {
     this.users = [];
+    this.userData = userData;
   }
+
+  createUsers() {
+    this.userData.forEach((userDataObject, i) => {
+      var user = new User(this.userData[i]);
+      this.users.push(user);
+    });
+  }
+
+  returnUserData(id) {
+    let currentUserData
+    this.userData.forEach((userDataObject, i) => {
+      if (this.userData[i].id === id) {
+        currentUserData = this.userData[i];
+      }
+    });
+   return currentUserData;
+  }
+
 }
 
 if (typeof module !== 'undefined') {

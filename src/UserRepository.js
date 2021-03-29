688e9a6953df1599b71e59029ef0ecdf612da48e
Riley Martin <riley.willow@gmail.com>
2021-03-29 17:01:47 -0600

    Structure UserRepository class file and test suite

---
+++ src/UserRepository.js
@@ -0,0 +1,7 @@
+class UserRepository(data) {
+  constructor(){
+
+  }
+}
+
+module.exports = UserRespository;
---
+++ tests/UserRepository-tests.js
@@ -0,0 +1,16 @@
+const expect = require("chai").expect;
+
+describe("UserRepository class", () => {
+  it("Should hold all User objects", () => {
+
+  });
+  it("Should have a parameter to take in user data", () => {
+
+  });
+  it("Should have a method to determine user data given a user ID", () => {
+
+  });
+  it("Should determine the average step goal amongst all users", () => {
+
+  });
+});

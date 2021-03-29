8a1f47cd56686c3b026f6fb3783ba89b98776228
bhohnco <bryanhohn13@gmail.com>
2021-03-29 14:19:55 -0600

    Files build and data test file created

---
+++ Test/User-TestingData.js
@@ -0,0 +1,103 @@
+const userTestingData = [
+  {
+    "id": 1,
+    "name": "Luisa Hane",
+    "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+    "email": "Diana.Hayes1@hotmail.com",
+    "strideLength": 4.3,
+    "dailyStepGoal": 10000,
+    "friends": [
+      16,
+      4,
+      8
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Jarvis Considine",
+    "address": "30086 Kathryn Port, Ciceroland NE 07273",
+    "email": "Dimitri.Bechtelar11@gmail.com",
+    "strideLength": 4.5,
+    "dailyStepGoal": 5000,
+    "friends": [
+      9,
+      18,
+      24,
+      19
+    ]
+  },
+  {
+    "id": 3,
+    "name": "Herminia Witting",
+    "address": "85823 Bosco Fork, East Oscarstad MI 85126-5660",
+    "email": "Elwin.Tromp@yahoo.com",
+    "strideLength": 4.4,
+    "dailyStepGoal": 5000,
+    "friends": [
+      19,
+      11,
+      42,
+      33
+    ]
+  }
+]
+
+const hydationTestingData = [
+  {
+    "userID": 1,
+    "date": "2019/06/15",
+    "numOunces": 37
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "numOunces": 75
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "numOunces": 47
+  }]
+
+const sleepTestingData = [
+  {
+    "userID": 1,
+    "date": "2019/06/15",
+    "hoursSlept": 6.1,
+    "sleepQuality": 2.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "hoursSlept": 7,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "hoursSlept": 10.8,
+    "sleepQuality": 4.7
+  }]
+
+const activityTestingData = [
+  {
+    "userID": 1,
+    "date": "2019/06/15",
+    "numSteps": 3577,
+    "minutesActive": 140,
+    "flightsOfStairs": 16
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "numSteps": 4294,
+    "minutesActive": 138,
+    "flightsOfStairs": 10
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "numSteps": 7402,
+    "minutesActive": 116,
+    "flightsOfStairs": 33
+  }]
---
+++ src/UserRepository.js
@@ -0,0 +1,5 @@
+class UserRepository {
+  constructor(data) {
+    this.users = [{}];
+  }
+};

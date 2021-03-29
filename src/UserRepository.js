4029575b8519074c79c9dff6bbc83f1bb084f9e9
pcmueller <pmuellerleile@gmail.com>
2021-03-29 16:51:53 -0600

    Add sleepData methods to UserRepository.js

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -25,4 +25,26 @@ class UserRepository {
     return this.avgStepGoal;
   }
 
+    calculateAllUserSleepQuality() {
+    /* iterate through sleep.js dataset, 
+    accumulate all sleepQuality values, 
+    and divide by length of the array */
+  }
+
+  retrieveBestWeeklySleepers(dataset) {
+    /* filter through the sleepData array 
+    for any elements with sleepQuality value  > 3, 
+    store element's 'name' value in new array, 
+    and return array */
+  }
+
+  retrieveBestSleeper(date) {
+    /* use find() to identify first element in sleepData 
+    array with same data as parameter, declare let bestSleeper 
+    and assign value of that sleepData[n].id, 
+    then iterate through array and if 
+    sleepData[i].hoursSlept > bestSleeper.hoursSlept,
+    bestSleeper = sleepData[i].id, then return bestSleeper */
+  }
+
 }

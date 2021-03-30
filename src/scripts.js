2ef8b761be014a045bc10d05c7cdc6cd6b018e8e
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-03-30 13:40:59 -0600

    Add all querySelectors

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,37 @@
-console.log("Hello World");
+//querySelectors
+var headerBanner = document.querySelector('#headerBanner');
+var headerMessage = document.querySelector('#headerMessage');
+
+var homegrid = document.querySelector('#homeGrid');
+var userInfo = document.querySelector('#userInfo');
+var picture = document.querySelector('#picture');
+var stepGoal = document.querySelector('#stepGoal');
+
+var hydrationGrid = document.querySelector('#hydrationGrid');
+var dailyWater = document.querySelector('#dailyWater');
+var weeklyWater = document.querySelector('#weeklyWater');
+
+var sleepGrid = document.querySelector('#sleepGrid');
+var dailySleep = document.querySelector('#dailySleep');
+var weeklySleep = document.querySelector('#weeklySleep');
+var qualitySleep = document.querySelector('3qualitySleep');
+
+var activityGrid  = document.querySelector('#activityGrid');
+var dailySteps = document.querySelector('#dailySteps');
+var dailyActivity = document.querySelector('#dailyActivity');
+var weeklySteps = document.querySelector('#weeklySteps');
+var compareUsers = document.querySelector('#compareUsers');
+
+var navBar = document.querySelector('#navBar');
+var homeButton = document.querySelector('#homeButton');
+var hydrationButton = document.querySelector('#hydrationButton');
+var sleepButton = document.querySelector('#sleepButton');
+var activityButton = document.querySelector('#activityButton');
+
+//variables
+
+
+//Event Listeners
+
+
+//Functions

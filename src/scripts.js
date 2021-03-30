82d32cf2771528bdc9065d809a099944862310c7
bhohnco <bryanhohn13@gmail.com>
2021-03-30 07:53:30 -0600

    Add eventlistener for userinfo dropdown

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const User = require ('../src/User');
+const UserRepository = require ('../src/UserRepository');
+
+const userInfoButton = document.querySelector('#userinfo-button');
+const userInfoDropdown = document.querySelector('#user-info-page');
+
+function showDropdown() {
+  userInfoDropdown.classList.toggle('hide');
+}
+
+  userInfoButton.addEventListener('click', showDropdown);

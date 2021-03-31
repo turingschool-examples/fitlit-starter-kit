63a11a6ec0ed5d088d064359931486aa4c8007a6
Riley Martin <riley.willow@gmail.com>
2021-03-31 14:52:45 -0600

    Save progress before merging with updated main

--- test/AllUserSleep-tests.js
+++ test/AllUserSleep-tests.js
@@ -7,7 +7,13 @@ describe("AllUserSleep", () => {
   beforeEach(() => {
 
   });
-  it("Should...", () => {
+  it("Should calculate the average sleep quality of all users", () => {
+
+  });
+  it("Should find all users who average a sleep quality greater than 3 for any given week", () => {
+
+  });
+  it("Should find the users (1 or more if they tied) who slept the most number of hours for a given day (identified by the date)", () => {
 
   });
 });
--- test/UserSleep-tests.js
+++ test/UserSleep-tests.js
@@ -7,7 +7,22 @@ describe("UserSleep", () => {
   beforeEach(() => {
 
   });
-  it("Should...", () => {
+  it("Should take in a parameter for userID", () => {
+
+  });
+  it("Should calculate the average number of hours slept per day", () => {
+
+  });
+  it("Should calculate how many hours they slept for a specific day (identified by a date)", () => {
+
+  });
+  it("Should calculate their sleep quality for a specific day (identified by a date)", () => {
+
+  });
+  it("Should calculate how many hours they slept each day over the course of any week", () => {
+
+  });
+  it("Should calculate their sleep quality each day over the course of any week", () => {
 
   });
 });

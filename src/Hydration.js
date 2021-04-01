e033a641cae6e4259458fd6b1f7f91286fe9ee5c
bhohnco <bryanhohn13@gmail.com>
2021-03-31 21:20:02 -0600

    Most methods complete, refactoring class/constructor

--- src/Hydration.js
+++ src/Hydration.js
@@ -4,12 +4,33 @@ class Hydration {
   constructor(hydrationData, id) {
     this.data = hydrationData;
     this.id = id;
-    this.ouncesToday = 0;
-    this.ouncesAverage = 0;
-    this.ouncesRecord = []
+    this.user = this.buildUserHydrationData();
   }
 
+  buildUserHydrationData() {
+    return this.data.filter(user => user.userID === this.id);
   }
+
+  calculateAverageOunces() {
+    let totalOz = this.user.reduce((ozTotal, currentUser) => {
+      ozTotal += currentUser.numOunces;
+      return ozTotal
+    }, 0)
+    return Math.floor(totalOz / this.user.length);
+  }
+
+  calculateDailyOunces(date) {
+    let daySelected = this.user.find(day => day.date === date);
+    return daySelected.numOunces;
+  }
+
+  calculateWeeklyOz(date) {
+    let indexOfCurrentDay = this.user.findIndex(day => day.date === date);
+    return this.user.slice(indexOfCurrentDay - 6, indexOfCurrentDay + 1)
+  }
+}
+
+
 
 
 

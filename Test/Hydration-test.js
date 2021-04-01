c8107aeef5323a5e963e2e0c519c63db3e1ba7e4
bhohnco <bryanhohn13@gmail.com>
2021-04-01 08:31:33 -0600

    Methods pass, refactor needed

--- Test/Hydration-test.js
+++ Test/Hydration-test.js
@@ -1,7 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
 const Hydration = require('../src/Hydration');
-const User = require ('../src/User');
 const hydrationTestData = require ('../Test/Hydration-TestingData');
 
 describe('Hydration', () => {
@@ -38,9 +37,24 @@ describe('Hydration', () => {
   });
 
   it('should calculate the weekly consumed ounce by date', () => {
-    expect(hydrate1.calculateWeeklyOz()).to.equal();
-    expect(hydrate2.calculateWeeklyOz()).to.equal();
+    expect(hydrate1.calculateWeeklyOz("2019/06/15")).to.deep.equal([
+      { userID: 1, date: '2019/06/15', numOunces: 37 },
+      { userID: 1, date: '2019/06/16', numOunces: 69 },
+      { userID: 1, date: '2019/06/17', numOunces: 96 },
+      { userID: 1, date: '2019/06/18', numOunces: 61 },
+      { userID: 1, date: '2019/06/19', numOunces: 91 },
+      { userID: 1, date: '2019/06/20', numOunces: 50 },
+      { userID: 1, date: '2019/06/21', numOunces: 50 }
+    ])
+    expect(hydrate2.calculateWeeklyOz("2019/06/18")).to.deep.equal([
+      { userID: 2, date: '2019/06/18', numOunces: 9 },
+      { userID: 2, date: '2019/06/19', numOunces: 91 },
+      { userID: 2, date: '2019/06/20', numOunces: 32 },
+      { userID: 2, date: '2019/06/21', numOunces: 45 },
+      { userID: 2, date: '2019/06/22', numOunces: 12 },
+      { userID: 2, date: '2019/06/23', numOunces: 41 },
+      { userID: 2, date: '2019/06/24', numOunces: 91 }
+    ]);
   });
-
-
+  
 });

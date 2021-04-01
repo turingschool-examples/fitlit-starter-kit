bd438954728dd297e6165bd4cbe5d6f2afb57dca
pcmueller <pmuellerleile@gmail.com>
2021-03-31 19:24:42 -0600

    Add additional UserRepository testing

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -80,15 +80,20 @@ describe('UserRepository', function() {
   });
 
   it("should be able to retrieve the ounces drank by a user on a specific date", function() {
-    const numOunces = userRepo.retrieveNumOuncesByDate(2, '2019/06/16');
+    const numOunces1 = userRepo.retrieveNumOuncesByDate(1, '2019/06/18');
+    const numOunces2 = userRepo.retrieveNumOuncesByDate(2, '2019/06/16');
+    const numOunces3 = userRepo.retrieveNumOuncesByDate(3, '2019/06/20');
 
-    expect(numOunces).to.equal(91);
+    expect(numOunces1).to.equal(61);
+    expect(numOunces2).to.equal(91);
+    expect(numOunces3).to.equal(51);
   });
 
   it("should calculate the average daily water intake for a user over the course of a week", function() {
-    const avgWeeklyWater = userRepo.calculateAvgWeeklyWater(1, '2019/06/16');
-    
-    expect(avgWeeklyWater).to.equal(66);
+    const avgWeeklyWater1 = userRepo.calculateAvgWeeklyWater(1, '2019/06/16');
+    // add 2 more method calls
+
+    expect(avgWeeklyWater1).to.equal(66);
   });
 
 });

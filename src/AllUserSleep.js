fca81a83c48bcf0991344d64088b911f166e8af8
Riley Martin <riley.willow@gmail.com>
2021-04-01 20:10:16 -0600

    Write AllUserSleep tests and method to calculate the average sleep quality

--- src/AllUserSleep.js
+++ src/AllUserSleep.js
@@ -2,6 +2,24 @@ class AllUserSleep {
   constructor(sleepData) {
     this.sleepData = sleepData;
   }
+  calcAvgSleepQuality() {
+    const totalSleepQuality = this.sleepData.reduce((total, num) => {
+      return total + num.sleepQuality
+    }, 0)
+    const avgSleepQuality = totalSleepQuality / this.sleepData.length
+    return avgSleepQuality
+  }
+
+  calcAboveAvgSleepQuality(date) {
+    //determine specific weeks
+    //calculate average sleep quality for each user (create instances of users?)
+    //return users who have a sleep quality > 3
+  }
+
+  calcMostSleep(date) {
+    //take in date, look for highest number of hours slept, return user(s)
+
+  }
 }
 
 

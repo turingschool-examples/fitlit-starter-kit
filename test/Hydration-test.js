eac3761bd79fc2bdbc7e1e52b358a038991aa32e
Nichele Dunn <the7yearitch@hotmail.com>
2021-04-01 19:29:01 -0600

    Stash for pull

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -68,13 +68,12 @@ describe("Hydration class", () => {
   // return how many fluid oz were drank each day over a 1 week period
   // return the amount for each day
 
+
+
   // Create classes and methods that can calculate:
   //
   // √ For a user (√ identified by their userID - this is the same for all methods requiring a specific user’s data), √ the average fluid ounces consumed per day for all time
   // For a user, how many fluid ounces they √ consumed for a specific day √ (identified by a date)
   // For a user, √ how many fluid ounces of water consumed each day over the course of a week (7 days) - √ return the amount for each day
-
-
   // it should take in a user and a user repository as properties -- should it?? hydration data includes an id, date, and numOunces
-
   // it should be able to select a specific date -- should it? this might be something that comes later; shouldn't need to test the dependencies

a78b1c58046566e496fb3b487c4f3e75699caa44
Riley Martin <riley.willow@gmail.com>
2021-04-07 14:36:31 -0600

    Add sad path testing to Hydration class for when a given date is not defined

--- src/helpers/calcAverage.js
+++ src/helpers/calcAverage.js
@@ -1,8 +1,15 @@
 const calcAverage = (data, property) => {
+  let numUndefined = 0;
   const total = data.reduce((total, dataPoint) => {
-    return total + dataPoint[property]
+    if (dataPoint[property] !== undefined) {
+      console.log(dataPoint[property])
+      return total + dataPoint[property]
+    } else {
+      numUndefined += 1;
+      return total
+    }
   }, 0)
-  const avg = total / data.length
+  const avg = total / (data.length - numUndefined)
   return avg
 }
 

72ea25941b46e6fec8e54d0f3b16557115eb460c
Nichele Dunn <the7yearitch@hotmail.com>
2021-04-06 20:38:57 -0600

    ✨ Add many lines of console logging to hydration graph

--- src/scripts.js
+++ src/scripts.js
@@ -37,12 +37,12 @@ function welcomeUser() {
 
 function displayInfoCard() {
   infoCard.innerHTML = `
-    <!-- <p class="user-detail card-flex">Name: ${newUser.name}</p> -->
+    <!-- <p class="user-detail radness">Name: ${newUser.name}</p> -->
     <!-- <p class="user-detail radness">Address: ${newUser.address}</p> -->
     <!-- <p class="user-detail radness">Email: ${newUser.email}</p> -->
     <p class="user-detail radness">Stride Length: ${newUser.strideLength}</p>
     <p class="user-detail radness">Daily Step Goal: ${newUser.dailyStepGoal}</p>
-    <p class="user-detail radness">Friends: ${newUser.friends}</p>
+    <!-- <p class="user-detail radness">Friends: ${newUser.friends}</p> -->
   `
 }
 
@@ -82,7 +82,8 @@ function displayHydrationData(data) {
   `
 
   const weekHydration = newUserHydration.calcOverWeek(todayDate.date, "numOunces");
-  console.log("this week's hydration data", weekHydration);
+
+  // console.log("this week's hydration data", weekHydration);
   //data is currently an array of objects
   //these can be broken up using object keys and object values to create an HTML table or implement chart js
   // weekHydrationDataDisplay.innerHTML = `
@@ -96,8 +97,35 @@ function displayHydrationData(data) {
 
 // 🧪 test function to render water graph for weekly intake
 function renderWeeklyHydrGraph(hydrData) {
-
+  // console.log("hydrData >>>", hydrData);
+  // const hydrOz = hydrData.forEach(dataPoint => Object.values(hydrData)); did not work
+  // console.log("forEach? >>>", hydrData.forEach((hydrDate, i) => i.numOunces)); did not work
+  const [ day7, day6, day5, day4, day3, day2, day1 ] = hydrData;
+  console.log("getting to line 103");
+  const hydrOz = hydrData.map(dataPoint => Object.values(dataPoint));
+  console.log("getting to line 105");
+  // console.log("hydrOz >>>", hydrOz);
+  // console.log("day1.values >>> ", Object.values(day1));
+  const graphData = {
+    labels: [ "7 days ago", "6 days ago", "5 days ago", "4 days ago", "3 days ago", "yesterday", "today"],
+    datasets: [{
+      label: "daily ounces over for the last 7 days",
+      data: hydrOz,
+      backgroundColor: ["#2561dd", "#3b71e0", "#5181e4", "#6791e7", "#7da1ea", "#93b1ee", "#a9c1f1"],
+    }]
+  };
+  // console.log("graphData >>>", graphData);
+  console.log("the graph is not rendering");
+  let hydrGraphR = new Chart(hydrGraph, {
+    type: "polarArea",
+    data: graphData,
+  });
 }
+
+
+
+
+
 
 function displaySleepData(data) {
   const todayDate = newUserSleep.mostRecentDayData();

27f74a2bc5b153f09fb5599ac398b4bc8f5d164a
Nichele Dunn <the7yearitch@hotmail.com>
2021-04-07 17:33:26 -0600

    💄 Update hydration and sleep cards

--- src/scripts.js
+++ src/scripts.js
@@ -41,7 +41,7 @@ function displayInfoCard() {
     <!-- <p class="user-detail radness">Name: ${newUser.name}</p> -->
     <!-- <p class="user-detail radness">Address: ${newUser.address}</p> -->
     <!-- <p class="user-detail radness">Email: ${newUser.email}</p> -->
-    <p class="user-detail radness">stride length: ${newUser.strideLength}</p>
+    <p class="user-detail radness">your stride length: ${newUser.strideLength}</p>
     <!-- <p class="user-detail radness">Daily Step Goal: ${newUser.dailyStepGoal}</p> -->
     <!-- <p class="user-detail radness">Friends: ${newUser.friends}</p> -->
   `;

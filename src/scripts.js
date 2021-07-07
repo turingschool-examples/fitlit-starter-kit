b6b2893c0198f9cc6d8d92d8a366bc3c65cbf718
Hannah Hudson <hannah.hch@gmail.com>
2021-07-07 12:09:44 -0600

    Add image example

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,15 @@
-// Example on how you will import your files. This is so webpack can find them!
+// This is the JavaScript entry file - your code begins here
+// Do not delete or rename this file ********
 
+// An example of how you tell webpack to use a CSS file
 import './css/styles.css';
+
+// An example of how you tell webpack to use an image (also need to link to it in the index.html)
+import './images/turing-logo.png'
+
+console.log('This is the JavaScript entry file - your code begins here.');
+
+// An example of how you tell webpack to use a JS file
+
 import userData from './data/users';
 import UserRepository from './UserRepository';
-
-
-console.log("Hello World");

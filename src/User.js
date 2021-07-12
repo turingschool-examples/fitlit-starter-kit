153550b6a1172a9e3cc6864daf9b76b5bc7fe372
Erica <ericaspitz522@gmail.com>
2021-07-12 14:30:50 -0600

    Add name, address, and email properties and tests

--- src/User.js
+++ src/User.js
@@ -4,6 +4,9 @@ import { userData } from "../src/data/users";
 class User {
   constructor(user) {
     this.id = user.id;
+    this.name = user.name;
+    this.address = user.address;
+    this.email = user.email;
   }
 }
 
--- test/User-test.js
+++ test/User-test.js
@@ -29,7 +29,23 @@ describe.only('User', () => {
   });
 
   it('user should have an id', () => {
+    expect(user.id).to.be.a('number')
     expect(user.id).to.equal(1);
-  })
+  });
+
+  it('user should have an name', () => {
+    expect(user.name).to.be.a('string');
+    expect(user.name).to.equal('Luisa Hane');
+  });
+
+  it('user should have an address', () => {
+    expect(user.address).to.be.a('string');
+    expect(user.address).to.equal('15195 Nakia Tunnel, Erdmanport VA 19901-1697');
+  });
+
+  it('user should have an email', () => {
+    expect(user.email).to.be.a('string');
+    expect(user.email).to.equal('Diana.Hayes1@hotmail.com');
+  });
 
 });

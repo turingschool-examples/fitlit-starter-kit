3808f043f378d65f3df91019a5129d6315ee28b9
Erica <ericaspitz522@gmail.com>
2021-07-12 14:16:08 -0600

    Set up User class and test and beforeEach hook

---
+++ test/User-test.js
@@ -0,0 +1,27 @@
+import { expect } from 'chai';
+import User from '../src/User';
+
+describe.only('User', () => {
+  let user;
+
+  beforeEach(() => {
+    user = new User({
+      "id": 1,
+      "name": "Luisa Hane",
+      "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+      "email": "Diana.Hayes1@hotmail.com",
+      "strideLength": 4.3,
+      "dailyStepGoal": 10000,
+      "friends": [
+        16,
+        4,
+        8
+      ]
+    })
+  })
+
+  it('should be a function', function () {
+    expect(User).to.be.a('function');
+  });
+
+});

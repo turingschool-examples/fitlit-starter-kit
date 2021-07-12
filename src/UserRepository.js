d870fb6e5bb6a255ef443d85c67f8d14558a16f4
Mae Duphorne <maelduphorne@gmail.com>
2021-07-12 16:01:05 -0600

    Test for userRepository property and add returnUserData method and test

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,5 +1,11 @@
 class UserRepository {
+  constructor(userData) {
+    this.userData = userData;
+  }
 
-}
+  returnUserData(id) {
+    return this.userData.find(user => user.id === id);
+  };
+};
 
 export default UserRepository;

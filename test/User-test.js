42d152d09e81b5bffa55534ce155d14712ca25c5
Matt Roden <mattroden92@gmail.com>
2021-07-13 15:52:32 -0600

    Pass new user instance test

--- test/User-test.js
+++ test/User-test.js
@@ -1,18 +1,27 @@
 import { expect } from 'chai';
 import User from '../src/User';
 
-//Setup
-var userData = {
-  "id": 1,
-  "name": "Luisa Hane",
-  "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
-  "email": "Diana.Hayes1@hotmail.com",
-  "strideLength": 4.3,
-  "dailyStepGoal": 10000,
-  "friends": [
-    16,
-    4,
-    8
-  ]
-};
-var user = new User(userData);
+describe.only('User', () => {
+
+  it('should be a function', () => {
+    expect(User).to.be.a('function');
+  })
+
+  it('should be an instance of user', () => {
+    const userData = {
+      "id": 1,
+      "name": "Luisa Hane",
+      "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+      "email": "Diana.Hayes1@hotmail.com",
+      "strideLength": 4.3,
+      "dailyStepGoal": 10000,
+      "friends": [
+        16,
+        4,
+        8
+      ]
+    };
+    const user = new User(userData);
+    expect(user).to.be.an.instanceOf(User);
+  });
+});

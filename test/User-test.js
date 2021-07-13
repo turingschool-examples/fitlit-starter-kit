56828dfca0a4c5b2ffabb3ea9936a10446e58727
Matt Roden <mattroden92@gmail.com>
2021-07-13 15:59:25 -0600

    Pass all user tests

--- test/User-test.js
+++ test/User-test.js
@@ -24,4 +24,22 @@ describe.only('User', () => {
     const user = new User(userData);
     expect(user).to.be.an.instanceOf(User);
   });
+
+  it('should return a user\'s first name only', () => {
+    const userData = {
+      "id": 1,
+      "name": "Luisa Hane",
+      "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+      "email": "Diana.Hayes1@hotmail.com",
+      "strideLength": 4.3,
+      "dailyStepGoal": 10000,
+      "friends": [
+        16,
+        4,
+        8
+      ]
+    };
+    const user = new User(userData);
+    expect(user.getFirstName()).to.equal('Luisa');
+  })
 });

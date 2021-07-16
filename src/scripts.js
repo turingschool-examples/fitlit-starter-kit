fe8a065722e9f219a15f1fe6ba4eb717c2805b96
Erica <ericaspitz522@gmail.com>
2021-07-15 19:21:33 -0600

    Display avg step goal for all users on dom

--- src/scripts.js
+++ src/scripts.js
@@ -42,35 +42,35 @@ const generateUser = () => {
 const setUpUserRepo = () => {
   fetchAPIData('users')
   .then(data => userRepo = new UserRepository(data.userData))
-  .then(data => userRepo = userRepo.userData)
-  // .then(data => console.log(userRepo))
-  // .then(data => displayStepGoals(userRepo))
+  // .then(data => userRepo = userRepo.userData)
+  .then(data => console.log(userRepo))
+  .then(data => displayStepGoals(userRepo, user))
 }
 
 const generateSleep = () => {
   fetchAPIData('sleep')
   .then(data => sleep = new Sleep(data.sleepData[user.id - 1]))
-  .then(data => console.log(sleep))
+  // .then(data => console.log(sleep))
 }
 
 const setUpSleepRepo = () => {
     fetchAPIData('sleep')
     .then(data => sleepRepo = new SleepRepo(data.sleepData))
     .then(data => sleepRepo = sleepRepo.sleepData)
-    .then(data => console.log(sleepRepo))
+    // .then(data => console.log(sleepRepo))
   }
 
 const generateHydration = () => {
   fetchAPIData('hydration')
   .then(data => hydration = new Hydration(data.hydrationData[user.id - 1]))
-  .then(data => console.log(hydration))
+  // .then(data => console.log(hydration))
 }
 
 const setUpHydrationRepo = () => {
     fetchAPIData('hydration')
     .then(data => hydrationRepo = new HydrationRepo(data.hydrationData))
     .then(data => hydrationRepo = hydrationRepo.hydrationData)
-    .then(data => console.log(hydrationRepo))
+    // .then(data => console.log(hydrationRepo))
   }
 
 // ON PAGE LOAD
@@ -92,10 +92,9 @@ const displayGreeting = (user) => {
 };
 //Display how the specific user's step goal compares to the average step goal amongst all Users
   //
-// const displayStepGoals = (userRepo) => {
-//   console.log(userRepo);
-//   const userAvg = userRepo.calculateAvgStepGoal();
-//   return userAvg
-  // stepGoal2.innerText = `Step Goal: ${user.dailyStepGoal}`
-  // avgSteps.innerText = `Average Steps for all users: ${userAvg}`;
-// };
+const displayStepGoals = (userRepo, user) => {
+  const userAvg = userRepo.calculateAvgStepGoal();
+  console.log(userAvg)
+  stepGoal2.innerText = `Step Goal: ${user.dailyStepGoal}`
+  avgSteps.innerText = `Average Steps for all users: ${userAvg}`;
+};

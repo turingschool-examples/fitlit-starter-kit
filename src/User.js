d9bcf8fb7c4a8f954c71e1f6305be51a71eb8ec0
David Tran <frozenpho@gmail.com>
2021-10-16 11:20:38 -0600

    Create User.js

---
+++ src/User.js
@@ -0,0 +1,11 @@
+class User {
+  constructor(id, name, address, email, strideLength, dailyStepGoal, friends) {
+    this.id = id;
+    this.name = name;
+    this.address = address;
+    this.email = email;
+    this.strideLength = strideLength;
+    this.dailyStepGoal = dailyStepGoal;
+    this.friends = friends;
+  }
+}

c3a489d5944ecb7cd340b1269324297ee4a7c2d0
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2021-10-16 16:24:16 -0400

    Allow for functionality on user repository files

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,5 +1,16 @@
 class UserRepository {
-
-}
+  constructor(user){
+    this.users = [];
+    this.users.push(user);
+  };
+  showData(id){
+    return this.users.find((user) => {return user.id === id})
+  };
+  calculateAvgStepGoal(){
+  const goals = this.users.map((user) => {return user.dailyStepGoal});
+  const average = goals.reduce((a,b) => a+b, 0) / goals.length;
+  return average
+  };
+};
 
 export default UserRepository;

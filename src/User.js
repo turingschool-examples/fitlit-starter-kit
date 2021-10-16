7f7950e4b53717c750fa79fba94fa1d38a75eb82
David Tran <frozenpho@gmail.com>
2021-10-16 11:30:04 -0600

    Refactor for object parameter

--- src/User.js
+++ src/User.js
@@ -1,11 +1,33 @@
 class User {
-  constructor(id, name, address, email, strideLength, dailyStepGoal, friends) {
-    this.id = id;
-    this.name = name;
-    this.address = address;
-    this.email = email;
-    this.strideLength = strideLength;
-    this.dailyStepGoal = dailyStepGoal;
-    this.friends = friends;
+  constructor(data) {
+    this.id = data.id;
+    this.name = data.name;
+    this.address = data.address;
+    this.email = data.email;
+    this.strideLength = data.strideLength;
+    this.dailyStepGoal = data.dailyStepGoal;
+    this.friends = data.friends;
   }
+  returnFirstName() {
+    return this.name;
+  }
+  
 }
+
+
+var user1 = {
+  "id": 3,
+  "name": "Herminia Witting",
+  "address": "85823 Bosco Fork, East Oscarstad MI 85126-5660",
+  "email": "Elwin.Tromp@yahoo.com",
+  "strideLength": 4.4,
+  "dailyStepGoal": 5000,
+  "friends": [
+    19,
+    11,
+    42,
+    33
+  ]
+};
+var user2 = new User(user1)
+console.log(user2.returnFirstName())

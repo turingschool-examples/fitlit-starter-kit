3a856f69dff2d170bd942e4a1806cccabe19a62b
Anna Kummer <annamkummer@gmail.com>
2021-10-17 10:50:14 -0600

    Update test file to write out data rather than pull from users.js file

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,13 +1,40 @@
 import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
 import User from '../src/User';
-const userTestData = require('../src/data/users')
 
 describe('User Repository', () => {
-  let userRepository;
+  let userData, userRepository;
 
   beforeEach(function(){
-    userRepository = new UserRepository(userTestData);
+    userData = [
+      {
+        "id": 1,
+        "name": "Luisa Hane",
+        "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+        "email": "Diana.Hayes1@hotmail.com",
+        "strideLength": 4.3,
+        "dailyStepGoal": 10000,
+        "friends": [16, 4, 8]
+      },
+      {
+        "id": 2,
+        "name": "Jarvis Considine",
+        "address": "30086 Kathryn Port, Ciceroland NE 07273",
+        "email": "Dimitri.Bechtelar11@gmail.com",
+        "strideLength": 4.5,
+        "dailyStepGoal": 5000,
+        "friends": [9, 18, 24, 19]
+      },
+      {
+        "id": 3,
+        "name": "Herminia Witting",
+        "address": "85823 Bosco Fork, East Oscarstad MI 85126-5660",
+        "email": "Elwin.Tromp@yahoo.com",
+        "strideLength": 4.4,
+        "dailyStepGoal": 5000,
+        "friends": [19, 11, 42, 33]
+      }]
+    userRepository = new UserRepository(userData);
   });
 
   it('should be a function', function () {
@@ -15,16 +42,11 @@ describe('User Repository', () => {
   });
 
   it('should display user data based on user id', function () {
-    expect(userRepository.showData(1)).to.deep.equal(userTestData[0]);
+    expect(userRepository.showData(1)).to.deep.equal(userData[0]);
     expect(userRepository.showData(0)).to.deep.equal(undefined);
   });
 
   it('should display average step goal amongst all users',function () {
-    const user = userTestData[0];
-    const user1 = userTestData[1];
-    const user2 = userTestData[2];
-    const users = [user, user1, user2]
-    userRepository = new UserRepository(users)
     expect(userRepository.calculateAvgStepGoal()).to.equal(6666.67);
   });
 

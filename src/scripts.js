15d21e2ec2b5b6f8a821e548407410489e35aa16
Anna Kummer <annamkummer@gmail.com>
2021-10-17 15:25:55 -0600

    Update styling

--- src/scripts.js
+++ src/scripts.js
@@ -3,11 +3,12 @@
 
 // An example of how you tell webpack to use a CSS file
 import './css/styles.css';
+// import userDeets from './fetch.js';
 
+//
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
-console.log('This is the JavaScript entry file - your code begins here.');
+import './images/user.png'
 
 // An example of how you tell webpack to use a JS file
 
@@ -15,8 +16,8 @@ import userData from './data/users';
 import UserRepository from './UserRepository';
 import User from './User';
 
-const header = document.querySelector('.header')
-const activitySection = document.querySelector('.activity')
+const header = document.querySelector('#header')
+const activitySection = document.querySelector('#activity')
 
 const loadUser = () => {
   const userRepository = new UserRepository(userData);
@@ -28,14 +29,21 @@ const loadUser = () => {
 
 const displayUserInfo = (user) => {
   header.innerHTML = `
-    <h1>Welcome, ${user.displayFirstName()}</h1>
-    <p>Name: ${user.name} Address: ${user.address} Email: ${user.email}</p>
+    <div class='welcome-box'>
+      <img src="./images/user.png" alt='user-icon' class='header header-image'>
+      <h1 class='header'>Welcome, ${user.displayFirstName()}</h1>
+    </div>
+    <div class='header user-info'>
+    <p class='header'>Name: ${user.name}<br>
+    Address: ${user.address}<br>
+    Email: ${user.email}</p>
+    </div>
   `
 }
 
 const displayStepGoalComparison = (currentUser, allUsers) => {
   activitySection.innerHTML = `
-    <h2>Your daily step goal is ${currentUser.dailyStepGoal}, and the average for everyone is ${allUsers.calculateAvgStepGoal()}<h2>
+    <h2 class='activity'>Your daily step goal is ${currentUser.dailyStepGoal}, and the average for everyone is ${allUsers.calculateAvgStepGoal()}<h2>
   `
 }
 

1cf5b27f5681ee266539aade22e506d113310b7e
danielsolomon332 <daniel.solomon332@gmail.com>
2022-02-06 10:20:06 -0700

    Add user test data and begin writing UserRepository-test

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,8 +1,19 @@
 import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
+import userData from '../data/user-test-data';
 
 describe('User Repository', () => {
-  it('should be a function', function () {
+  let = userRepository;
+
+  beforeEach(() => {
+    userRepository = new UserRepository()
+  })
+
+  it('should be a function', () => {
     expect(UserRepository).to.be.a('function');
+  });
+
+  it('should be an instance of User Repsoitory', => {
+    expect(userRepository).to.be.an.instanceof(UserRepository)
   });
 });

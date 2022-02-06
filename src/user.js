1d2ecc8b3bca51b6e364482f99c36ef7e13a5773
Ronald Head <ronaldlhead@gmail.com>
2022-02-06 14:23:05 -0700

    Set up user class skeleton

---
+++ src/user.js
@@ -0,0 +1,21 @@
+//single user representation
+//single parameter to pass in user data object - users.js
+//method to return user's first name only
+
+class User {
+  constructor(userObject) {
+    this.id = userObject.id;
+    this.name = userObject.name;
+    this.address = userObject.address;
+    this.email = userObject.email;
+    this.strideLength = userObject.strideLength;
+    this.dailyStepGoal = userObject.dailyStepGoal;
+    this.friends = userObject.friends;
+  }
+
+  firstName() {
+    
+  }
+
+
+}

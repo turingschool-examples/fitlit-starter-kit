248994bcf28c207adba374108e47b673cab04bb5
danielsolomon332 <daniel.solomon332@gmail.com>
2022-02-06 10:40:53 -0700

    Write userRepository test

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -3,17 +3,37 @@ import UserRepository from '../src/UserRepository';
 import userData from '../data/user-test-data';
 
 describe('User Repository', () => {
-  let = userRepository;
+  let userRepository;
+  let user1;
+  let user2;
 
   beforeEach(() => {
-    userRepository = new UserRepository()
-  })
+    userRepository = new UserRepository(userData)
+    user1 = new User(userData[0]);
+    user2 = new User(userData[1]);
+  });
 
   it('should be a function', () => {
     expect(UserRepository).to.be.a('function');
   });
 
-  it('should be an instance of User Repsoitory', => {
-    expect(userRepository).to.be.an.instanceof(UserRepository)
+  it('should be an instance of UserRepsoitory', => {
+    expect(userRepository).to.be.an.instanceof(UserRepository);
+  });
+
+  it('should hold the users', () => {
+    expect(userRepository.users).to.be.an('array');
+  });
+
+  it('should hold an instance of User', () => {
+    expect(user1).to.eql(userRepository.users[0]);
+  });
+
+  it('should be able to return the user by id', () => {
+    expect(userRepository.getUser(user1.id)).to.eql(user1);
+  });
+
+  it('should be able to return all of the user\'s average step count', () => {
+    expect(userRepository.averageStepGoal()).to.eql(7500);
   });
 });

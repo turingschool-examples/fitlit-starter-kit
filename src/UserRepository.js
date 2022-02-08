55a0380d8cad6691e902c48c028192d2d0c3b6f8
Rory Magee <roryemagee@gmail.com>
2022-02-07 20:38:18 -0700

    Add current work on UserRepository class and tests

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,4 +1,15 @@
 class UserRepository {
+  constructor(data) {
+    this.allData = [data];
+  }
+
+  addData(data) {
+    this.allData.push(data);
+  }
+
+  dataByID(id) {
+    this.allData.find()
+  }
 
 }
 
--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,8 +1,26 @@
 import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
+import User from '../src/User';
+import userData from '../src/data/users';
 
 describe('User Repository', () => {
-  it('should be a function', function () {
+
+  let data1, data2;
+
+    beforeEach(() => {
+      data1 = new User(userData[0]);
+      data2 = new User(userData[1]);
+    });
+
+  it('should be a function', () => {
     expect(UserRepository).to.be.a('function');
   });
+
+  it('should hold all of the user objects', () => {
+    const repository = new UserRepository(data1);
+    expect(repository.allData[0]).to.equal(data1);
+    repository.addData(data2);
+    expect(repository.allData[1]).to.equal(data2);
+  })
+
 });

02706406d8d66e8a9bd7d273b758c8f36f753885
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-02-07 21:02:00 -0700

    Update user information to page with info card

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,6 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
-console.log(userData,"<>>>>userData")
 // An example of how you tell webpack to use a CSS file
 import './css/styles.css';
 
@@ -18,6 +17,7 @@ import UserRepository from './js/UserRepository';
 
 const userName = document.querySelector('#userName');
 const stepGoal = document.querySelector('#stepGoal');
+const infoCard = document.querySelector('#infoCard');
 
 const users = new UserRepository(userData);
 let currentUser;
@@ -26,8 +26,22 @@ const getRandomUser = () => {
   currentUser = users.getUser(Math.floor(Math.random() * users.users.length));
 }
 const updateUser = () => {
+  let friends = `Friends: `
   userName.innerText = `Welcome ${currentUser.getName()}`;
-  stepGoal.innerText = `Your step goal: ${currentUser.dailyStepGoal} / Average: ${users.averageStepGoal()}`  
+  stepGoal.innerText = `Your step goal: ${currentUser.dailyStepGoal} / Average: ${users.averageStepGoal()}`
+  currentUser.friends.forEach(friend => {
+    friends += `${users.getUser(friend).getName()}, `
+  });
+  friends = friends.slice(0, -2)
+  infoCard.innerHTML = `
+    <p>Name: ${currentUser.name}</p>
+    <p>Id: ${currentUser.id}</p>
+    <p>Address: ${currentUser.address}</p>
+    <p>Email: ${currentUser.email}</p>
+    <p>Stride Length: ${currentUser.strideLength}</p>
+    <p>${friends}</p>`;
+
+  console.log(currentUser)
 }
 
 const start = () => {

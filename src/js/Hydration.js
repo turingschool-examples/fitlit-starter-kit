9942b3c8b5368f8d8da0448f197d29a2d2bedec3
Dillon Parker <prkr93@gmail.com>
2022-02-09 11:43:15 -0700

    Populate hydration class using filtered data

---
+++ src/js/Hydration.js
@@ -0,0 +1,9 @@
+class Hydration {
+  constructor(data, id) {
+    this.id = id;
+    this.days = data.filter(day => day.userID === this.id);
+  }
+  //43 50 50 91 61 96 69
+}
+
+export default Hydration;

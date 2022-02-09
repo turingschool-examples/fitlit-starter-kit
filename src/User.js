4a4854baf2b2cc8ab41d57445da1d6d1d110270a
Rory Magee <roryemagee@gmail.com>
2022-02-08 17:53:57 -0700

    Complete iteration 1 classes and tests. Close #1, Close #5

--- src/User.js
+++ src/User.js
@@ -1,10 +1,16 @@
 class User {
   constructor(userData) {
-    this.userData = userData;
+    this.id = userData.id;
+    this.name = userData.name;
+    this.address = userData.address;
+    this.email = userData.email;
+    this.strideLength = userData.strideLength;
+    this.dailyStepGoal = userData.dailyStepGoal;
+    this.friends = userData.friends;
   }
 
   returnFirstName() {
-    return this.userData.name.split(' ', 1)[0];
+    return this.name.split(' ', 1)[0];
   }
 
 }
--- src/UserRepository.js
+++ src/UserRepository.js
@@ -13,13 +13,6 @@ class UserRepository {
   }
 
   averageStepGoal() {
-    const output = this.allData.reduce((acc, curr) => {
-      return (acc.dailyStepGoal + curr.dailyStepGoal);
-    });
-    return (output/this.allData.length);
-  }
-
-  averageStepGoal() {
     const output = this.allData.map(data => data.dailyStepGoal).reduce((acc, curr) =>  (acc + curr));
     return (output/this.allData.length);
   }

b25de5b4b750bdaa435019fe911a96a8bb66074c
Dillon Parker <prkr93@gmail.com>
2022-02-08 17:07:20 -0700

    Clean up scripts file

--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,9 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
 // An example of how you tell webpack to use a CSS file
 import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
 
 // An example of how you tell webpack to use a JS file
 import {
@@ -18,6 +14,7 @@ import {
 } from './apiCalls';
 
 import UserRepository from './js/UserRepository';
+
 
 const userName = document.querySelector('#userName');
 const stepGoal = document.querySelector('#stepGoal');

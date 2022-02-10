2e5d59a5fcf4d606d2882aecb33929f7e14faf54
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-02-10 15:39:15 -0700

    Import necessary variables and data

---
+++ test/Sleep-test.js
@@ -0,0 +1,6 @@
+import { expect } from 'chai';
+import UserRepository from '../src/js/UserRepository';
+import userTestData from '../src/data/user-test-data';
+import sleepTestData from '../src/data/sleep-test-data';
+import User from '../src/js/User';
+import Sleep from '../src/js/Sleep';

5c6df9941e94971b8e0a31676ad539065fc07c2d
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-02-10 21:16:23 -0700

    Create functionality to gather all of the user's sleep info and getAverage

--- src/js/Sleep.js
+++ src/js/Sleep.js
@@ -1,6 +1,13 @@
 class Sleep {
-  constructor() {
-
+  constructor(data, userId) {
+    this.days = data.filter(day => day.userID === userId);
+  }
+  getAverage() {
+    let result = this.days.reduce((acc, curr) => {
+      acc += curr.hoursSlept
+      return acc
+    }, 0);
+    return parseFloat((result / this.days.length).toFixed(1));
   }
 }
 

7cf7f7b7aa5100305784f3b883a72ca9c9e24fef
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-02-11 13:02:06 -0700

    Create functionality to return the week of sleep quality

--- src/js/Sleep.js
+++ src/js/Sleep.js
@@ -9,6 +9,43 @@ class Sleep {
     }, 0);
     return parseFloat((result / this.days.length).toFixed(1));
   }
+
+  getAverageQuality() {
+    let result = this.days.reduce((acc, curr) => {
+      acc += curr.sleepQuality;
+      return acc;
+    }, 0);
+    return parseFloat((result / this.days.length).toFixed(1));
+  }
+
+  getSleep(date) {
+    let result = this.days.find(day => day.date === date).hoursSlept;
+    return result;
+  }
+
+  getSleepQuality(date) {
+    let result = this.days.find(day => day.date === date).sleepQuality;
+    return result;
+  }
+
+  getWeekSleep(date) {
+    let result = {};
+    let i = this.days.findIndex(day => day.date === date)
+    if(this.days.length - i > 7) {
+      result = this.days.slice(i, i + 7);
+      result = result.map(day => ({date: day.date, hoursSlept: day.hoursSlept}))
+    }
+    return result;
+  }
+  getWeekQuality(date) {
+    let result = {};
+    let i = this.days.findIndex(day => day.date === date)
+    if(this.days.length - i > 7) {
+      result = this.days.slice(i, i + 7);
+      result = result.map(day => ({date: day.date, sleepQuality: day.sleepQuality}))
+    }
+    return result;
+  }
 }
 
 export default Sleep;

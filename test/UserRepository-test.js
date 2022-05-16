fa8f56fed5c9f73f2aa94ebc659325b001402824
Cyanne Jones <jonescyanne@yahoo.com>
2022-05-16 17:45:10 -0600

    Add instanceof class and userData storage tests

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,8 +1,24 @@
 import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
+import userData from '../src/data/users.js'
 
 describe('User Repository', () => {
+
+  let userRepository;
+
+  beforeEach( () => {
+    userRepository = new UserRepository(userData);
+  });
+
   it('should be a function', function () {
     expect(UserRepository).to.be.a('function');
+  });
+
+  it('should be an instance of UserRepository', () => {
+    expect(userRepository).to.be.an.instanceof(UserRepository)
+  });
+
+  it('should store user data', () => {
+    expect(userRepository.users).to.equal(userData);
   });
 });

d853d6e1fc7953e3bab05c0ffde0fcb9819f7a2d
Cami Anderson <camidantas@icloud.com>
2022-05-16 21:09:57 -0400

    Add functionality to getUserData method
    Create sampleData.js for testing

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -3,7 +3,12 @@ class UserRepository {
         this.data = data;
     }
     getUserData(id){
-
+        const data = this.data.find((obj) => {
+            if(obj.id === id){
+                return obj;
+            }
+        })
+        return data;
     }
     averageStepGoal(){
         //return average
--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,8 +1,27 @@
 import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
+import  userData  from '../src/data/sampleData';
 
 describe('User Repository', () => {
   it('should be a function', function () {
     expect(UserRepository).to.be.a('function');
+  });
+  it('should be able to get user data by id', function(){
+    const user1 = new UserRepository(userData)
+
+    expect(user1.getUserData(2)).to.deep.equal({
+      "id": 2,
+      "name": "Jarvis Considine",
+      "address": "30086 Kathryn Port, Ciceroland NE 07273",
+      "email": "Dimitri.Bechtelar11@gmail.com",
+      "strideLength": 4.5,
+      "dailyStepGoal": 5000,
+      "friends": [
+        9,
+        18,
+        24,
+        19
+      ]
+    });
   });
 });

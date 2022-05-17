640e996a48c890f4dc23fbf8bbb40c56a91ad836
Megan Schuetz <mschuetz411@gmail.com>
2022-05-17 15:44:24 -0600

    finish the basic structure needed for users and userrepo classes

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,22 +1,20 @@
-
-
 class UserRepository {
   constructor(data) {
     this.users = data;
-  }
+  };
 
   getUserById(id) {
     const foundData = this.users.find(user => user.id === id);
       return foundData;
-  }
+  };
 
   calculateAvgStepGoal() {
     const allUsersAvgStepGoal = this.users.reduce((totalSteps, user) => {
       totalSteps += user.dailyStepGoal;
       return totalSteps;
-    }, 0)
+    }, 0);
     return Math.round(allUsersAvgStepGoal / this.users.length);
-  }
+  };
 }
 
 export default UserRepository;
---
+++ test/User-test.js
@@ -0,0 +1,42 @@
+import { expect } from 'chai';
+import User from '../src/User';
+
+describe('User', () => {
+
+    let eachUser;  
+    
+    beforeEach(() => {
+        let user = {
+            "id": 1,
+            "name": "Luisa Hane",
+            "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+            "email": "Diana.Hayes1@hotmail.com",
+            "strideLength": 4.3,
+            "dailyStepGoal": 10000,
+            "friends": [
+              16,
+              4,
+              8
+            ]
+          };
+        eachUser = new User(user);
+    })
+
+    it('should be a function', function () {
+        expect(User).to.be.a('function');
+      });
+    
+      it('should be an instance of User Repository', () => {
+        expect(eachUser).to.be.an.instanceOf(User);
+      });
+
+      it('should create an object', () => {
+          expect(eachUser).to.be.a('object')
+      });
+
+      it('should return first name', () => {
+          expect(eachUser.getUserFirstName()).to.equal('Luisa Hane')
+      });
+    
+
+})

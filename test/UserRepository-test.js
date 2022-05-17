0c6aff902b2819075dadc900aa629e0fd94cb00f
Cyanne Jones <jonescyanne@yahoo.com>
2022-05-16 18:07:19 -0600

    Add test to assert getUserById function

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -21,4 +21,8 @@ describe('User Repository', () => {
   it('should store user data', () => {
     expect(userRepository.users).to.equal(userData);
   });
+
+  it('should have a function that returns a user object by its id number', () => {
+    expect(userRepository.getUserById(1)).to.deep.equal(userData[0]);
+  });
 });

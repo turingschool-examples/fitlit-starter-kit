6ec4b62eb1f5a89889b9a129d68b8cc6f92ccd96
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-05-17 15:49:35 -0600

    Add method and testing

--- src/sleep-repository.js
+++ src/sleep-repository.js
@@ -2,35 +2,10 @@ class SleepRepository {
   constructor(sleepData){
     this.data = sleepData;
   }
-  getHoursSleptById(userId) {
-    const foundData = this.users.find(user => user.id === userId);
+  getUserData(userId) {
+    const foundData = this.data.find(user => user.userID === userId);
       return foundData;
   }
 }
 
-
-
-
-
-
-
-// class UserRepository {
-//   constructor(data) {
-//     this.users = data;
-//   }
-//
-//   getUserById(id) {
-//     const foundData = this.users.find(user => user.id === id);
-//       return foundData;
-//   }
-//
-//   calculateAvgStepGoal() {
-//     const allUsersAvgStepGoal = this.users.reduce((totalSteps, user) => {
-//       totalSteps += user.dailyStepGoal;
-//       return totalSteps;
-//     }, 0)
-//     return Math.round(allUsersAvgStepGoal / this.users.length);
-//   }
-// }
-
-export default UserRepository;
+export default SleepRepository;
--- test/sleep-repository-test.js
+++ test/sleep-repository-test.js
@@ -1,3 +1,45 @@
 import { expect } from 'chai';
-import UserRepository from '../src/UserRepository';
-import User from '../src/User';
+import SleepRepository from '../src/sleep-repository';
+// import Sleep from '../src/Sleep';
+
+describe('Sleep Repository', () => {
+
+  let sleepData
+  let sleepRepo
+
+  beforeEach( () => {
+    let sleepData = [
+      {
+        "userID":1,
+        "date":"2019/06/15",
+        "hoursSlept":6.1,
+        "sleepQuality":2.2
+      },
+      {
+        "userID":2,
+        "date":"2019/06/15",
+        "hoursSlept":7,
+        "sleepQuality":4.7
+      },
+      {
+        "userID":3,
+        "date":"2019/06/15",
+        "hoursSlept":10.8,
+        "sleepQuality":4.7
+      },
+    ]
+
+    sleepRepo = new SleepRepository(sleepData);
+
+  });
+
+  it('should be a function', function () {
+    expect(SleepRepository).to.be.a('function');
+  });
+  it('should instantiate a sleep repository', function () {
+    expect(sleepRepo).to.be.an.instanceOf(SleepRepository)
+  });
+  it('should return user data when given id', function () {
+    expect(sleepRepo.getUserData(1)).to.deep.equal(sleepRepo.data[0]);
+  });
+});

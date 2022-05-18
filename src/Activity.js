369a729e4a8416072a78ce1a4ccb45b9f5f7ed75
Kyle Howard <kylehoward1228@yahoo.com>
2022-05-18 17:12:16 -0600

    Add activity methods for most data types

--- src/Activity.js
+++ src/Activity.js
@@ -23,14 +23,85 @@ class Activity {
   };
 
   activityStepsForWeek(userId) {
-    const lastElement = userData.indexOf(userData[userData.length - 1]);
     const userData = this.findUser(userId);
+    const lastElement = userData.indexOf(userData[userData.length - 1]);
+    // const userData = this.findUser(userId);
     const weekData = userData.slice(lastElement - 6);
-    return weekData.map((data) => data.numSteps);
+
+    return weekData.map((data) => 
+    data.numSteps);
+  };
+
+  stairClimbRecord(userId) {
+    let findUser = this.findUser(userId);
+
+    let stairs = findUser
+      .map((user) => {
+        return user.flightsOfStairs;
+      })
+      .sort((a, b) => {
+        return b - a;
+      });
+    return stairs.shift();
+  };
+
+  milesPerDay(userId, date) {
+    let findUser = this.findUser(userId);
+    let numberSteps = findUser.find((user) => {
+      if (user.date === date) {
+        return user;
+      }
+    }).numSteps;
+    let miles = numberSteps / 2000;
+
+    return Math.round(miles * 100) / 100;
+  };
+
+  activityFlightsPerWeek(userId) {
+    const userFlightsData = this.findUser(userId);
+
+    const lastElement = userFlightsData.indexOf(
+      userFlightsData[userFlightsData.length - 1]
+    );
+    const weekData = userFlightsData.slice(lastElement - 6);
+
+    return weekData.map((data) => data.flightsOfStairs);
+  };
+
+  hitDailyStepGoal(userId, date) {
+    let findUser = this.findUser(userId);
+    let user = userData.find((user) => {
+      if (user.id === userId) {
+        return user;
+      }
+    });
+
+    let stepsDay = findUser.find((user) => {
+      if (user.date === date) {
+        return user;
+      }
+    }).numSteps;
+    if (user.dailyStepGoal <= stepsDay) {
+      return true;
+    } else return false;
+  };
+
+  allDaysStepGoal(userId) {
+    let findUser = this.findUser(userId);
+    let user = userData.find((user) => {
+      if (user.id === userId) {
+        return user;
+      }
+    });
+    let week = findUser.reduce((a, b) => {
+      if (b.numSteps >= user.dailyStepGoal) {
+        a.push(b.date);
+      }
+      return a;
+    }, []);
+    return week;
   };
 
 }
-
-
 
 export default Activity;

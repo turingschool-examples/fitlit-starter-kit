b41291c5ab9cc9ad0a107af0ef54d130da766ba7
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-05-18 17:31:16 -0600

    Add all methods and tests

--- src/sleep.js
+++ src/sleep.js
@@ -4,55 +4,57 @@
 class Sleep {
   constructor(userId, allUserInstances){
     this.userId = userId;
-//  gathers all objects within sleep data with matching ID. results in array.
     this.allUserInstances = allUserInstances;
-//  average sleep quality per day over all time
     this.avgSleepQuality = this.calculateAvgDailySleepQuality();
-//  sleep quality for a specified day
-    this.sleepQuality = 0;
-
-
-//  hours slept per day over course of a given week
-    this.sevenDayAverage = 0;
-
-
-  }
+  };
   calculateAvgDailySleepQuality() {
     let overallSleepQualitySum = this.allUserInstances.reduce((totalAcc, instance) => {
-    totalAcc += instance.hoursSlept
-    return totalAcc
+    totalAcc += instance.hoursSlept;
+    return totalAcc;
   }, 0)
-    let result = overallSleepQualitySum / this.allUserInstances.length
-    return result
-  }
+    let result = overallSleepQualitySum / this.allUserInstances.length;
+    return result;
+  };
   findObjectByDate(day) {
     let specifiedObject = this.allUserInstances.find(userObject => userObject.date === day);
-    console.log("INDEX: ", this.allUserInstances.indexOf(specifiedObject))
     return specifiedObject;
   }
-  returnHoursSlept(day) {
-    let specifiedObject = this.findObjectByDate(day);
-    return specifiedObject.hoursSlept;
-  }
-  returnSleepQuality(day) {
-    let specifiedObject = this.findObjectByDate(day);
-    return specifiedObject.sleepQuality;
-  }
+  // returnHoursSlept(day) {
+  //   let specifiedObject = this.findObjectByDate(day);
+  //   return specifiedObject.hoursSlept;
+  // }
+  // returnSleepQuality(day) {
+  //   let specifiedObject = this.findObjectByDate(day);
+  //   return specifiedObject.sleepQuality;
+  // }
   returnObjectByDate(day, objectType) {
     let specifiedObject = this.findObjectByDate(day);
     return specifiedObject[objectType];
   }
-  calculateSevenDayAvg(userId, startDate) {
-let startingObject = this.findObjectByDate(startDate);
-let index = this.allUserInstances.indexOf(startingObject);
-let objectsWithinDateRange = this.allUserInstances.slice(this.allUserInstances[index], 7);
-let totalHoursSlept = objectsWithinDateRange.reduce((acc, object) => {
-  acc += object.hoursSlept
-  return acc
-},0);
-let result = totalHoursSlept/objectsWithinDateRange.length
-return result
-  }
-}
+//   calculateSevenDayAvg(startDate) {
+// let startingObject = this.findObjectByDate(startDate);
+// let index = this.allUserInstances.indexOf(startingObject);
+// let objectsWithinDateRange = this.allUserInstances.slice(this.allUserInstances[index], 7);
+// let totalHoursSlept = objectsWithinDateRange.reduce((acc, object) => {
+//   acc += object.hoursSlept
+//   return acc
+// },0);
+// let result = totalHoursSlept / objectsWithinDateRange.length
+// result = (Math.round(result * 10) / 10)
+// return result
+//   }
+  calculateAvg(startDate, type) {
+    let startingObject = this.findObjectByDate(startDate);
+    let index = this.allUserInstances.indexOf(startingObject);
+    let objectsWithinDateRange = this.allUserInstances.slice(this.allUserInstances[index], 7);
+    let total = objectsWithinDateRange.reduce((acc, object) => {
+      acc += object[type];
+      return acc;
+    },0);
+    let result = total / objectsWithinDateRange.length;
+    result = (Math.round(result * 10) / 10);
+    return result;
+    };
+  };
 
 export default Sleep;
--- test/sleep-test.js
+++ test/sleep-test.js
@@ -46,6 +46,42 @@ beforeEach( () => {
     "hoursSlept":10.7,
     "sleepQuality":3.4
     },
+    {
+      "userID":3,
+    "date":"2019/06/17",
+    "hoursSlept":10.7,
+    "sleepQuality":3.4
+    },
+    {
+      "userID":3,
+    "date":"2019/06/18",
+    "hoursSlept":10.7,
+    "sleepQuality":3.4
+    },
+    {
+      "userID":3,
+    "date":"2019/06/19",
+    "hoursSlept":10.7,
+    "sleepQuality":3.4
+    },
+    {
+      "userID":3,
+    "date":"2019/06/20",
+    "hoursSlept":10.7,
+    "sleepQuality":3.4
+    },
+    {
+      "userID":3,
+    "date":"2019/06/21",
+    "hoursSlept":10.7,
+    "sleepQuality":3.4
+    },
+    {
+      "userID":3,
+    "date":"2019/06/22",
+    "hoursSlept":15,
+    "sleepQuality":3.4
+    },
   ];
 
     sleepRepo = new SleepRepository(sleepData);
@@ -75,16 +111,32 @@ beforeEach( () => {
     expect(sleep.avgSleepQuality).to.equal(5.1);
   });
 
-  it('should return the amount of hours slept for a specified day', () => {
+  it.skip('should return the amount of hours slept for a specified day', () => {
     expect(sleep.returnHoursSlept("2019/06/15")).to.equal(6.1);
   });
 
-  it('should return the quality of sleep for a specified day', () => {
+  it.skip('should return the quality of sleep for a specified day', () => {
     expect(sleep.returnSleepQuality("2019/06/15")).to.equal(2.2);
   });
+
   it('should return any object', () => {
     expect(sleep.returnObjectByDate("2019/06/15", "hoursSlept")).to.equal(6.1);
     expect(sleep.returnObjectByDate("2019/06/16", "sleepQuality")).to.equal(3.8);
   });
 
+  it.skip('should return the average of hours slept within specified week', () => {
+    let user3 = new Sleep(3, sleepRepo.getAllUserData(3))
+    expect(user3.calculateSevenDayAvg("2019/06/15")).to.equal(10.7);
+  });
+
+  it.skip('should return the quality of hours slept within specified week', () => {
+    let user3 = new Sleep(3, sleepRepo.getAllUserData(3))
+    expect(user3.calculateSevenDayAvg("2019/06/15")).to.equal(10.7);
+  });
+
+  it('should return the average of anything specified within a week', () => {
+    let user3 = new Sleep(3, sleepRepo.getAllUserData(3))
+    expect(user3.calculateAvg("2019/06/15", "sleepQuality")).to.equal(3.6);
+    expect(user3.calculateAvg("2019/06/15", "hoursSlept")).to.equal(10.7);
+  });
 });

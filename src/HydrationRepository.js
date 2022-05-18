e68a9f3b209d599f1ad7a60cb19c335079c805d7
“Corinne <corinne.canavan@gmail.com>
2022-05-17 18:35:39 -0600

    Add methods getUserById and getAvgFluidOunces; add HydrationRepository-test.js

--- src/HydrationRepository.js
+++ src/HydrationRepository.js
@@ -2,4 +2,21 @@ class HydrationRepository {
   constructor(hydrationData) {
     this.hydrationData = hydrationData;
   }
+
+  getUserById(id) {
+    const foundData = this.hydrationData.filter(data => data.userID === id);
+      return foundData;
+  }
+
+  getAvgFluidOunces() {
+    const allHydrationDataById = this.getUserById(id);
+    const totalFluidOunces = allHydrationDataById.reduce((totalOunces, hydroObj) => {
+      totalOunces += hydroObj.numOunces;
+      return totalOunces;
+    }, 0)
+    return totalFluidOunces / allHydrationDataById.length;
+  }
 }
+
+
+export default HydrationRepository;

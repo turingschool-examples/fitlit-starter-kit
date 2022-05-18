8d06d593b681acde4ccbfc4628b8de1fdb8b7737
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-05-18 13:00:02 -0600

    Build method to calc avg hours slept

--- src/sleep.js
+++ src/sleep.js
@@ -8,14 +8,14 @@ class Sleep {
     this.allUserInstances = allUserInstances;
 //  average sleep quality per day over all time
     this.avgSleepQuality = this.calculateAvgDailySleepQuality();
-
-
 //  sleep quality for a specified day
     this.sleepQuality = 0;
 
 
 //  hours slept per day over course of a given week
-    this.weeklyAverage = 0;
+    this.sevenDayAverage = 0;
+
+
   }
   calculateAvgDailySleepQuality() {
     let overallSleepQualitySum = this.allUserInstances.reduce((totalAcc, instance) => {
@@ -27,6 +27,7 @@ class Sleep {
   }
   findObjectByDate(day) {
     let specifiedObject = this.allUserInstances.find(userObject => userObject.date === day);
+    console.log("INDEX: ", this.allUserInstances.indexOf(specifiedObject))
     return specifiedObject;
   }
   returnHoursSlept(day) {
@@ -41,9 +42,17 @@ class Sleep {
     let specifiedObject = this.findObjectByDate(day);
     return specifiedObject[objectType];
   }
+  calculateSevenDayAvg(userId, startDate) {
+let startingObject = this.findObjectByDate(startDate);
+let index = this.allUserInstances.indexOf(startingObject);
+let objectsWithinDateRange = this.allUserInstances.slice(this.allUserInstances[index], 7);
+let totalHoursSlept = objectsWithinDateRange.reduce((acc, object) => {
+  acc += object.hoursSlept
+  return acc
+},0);
+let result = totalHoursSlept/objectsWithinDateRange.length
+return result
+  }
 }
 
 export default Sleep;
-
-//I think I am creating an object that will live within
-//the User class instantiation.

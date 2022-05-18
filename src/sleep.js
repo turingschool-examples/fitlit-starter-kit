184d3b9d8e89c3ab2ebde10ded96d6fcc9fdd900
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-05-17 19:03:17 -0600

    Add avg daily sleep quality method and test

--- src/sleep.js
+++ src/sleep.js
@@ -1,12 +1,29 @@
+
+
+
 class Sleep {
-  constructor(userId){
+  constructor(userId, allUserInstances){
     this.userId = userId;
+//  gathers all objects within sleep data with matching ID. results in array.
+    this.allUserInstances = allUserInstances;
+
+
 //  average sleep quality per day over all time
     this.avgSleepQuality = 0;
+
+
 //  sleep quality for a specified day
     this.sleepQuality = 0;
 //  hours slept per day over course of a given week
     this.weeklyAverage = 0;
+  }
+  calculateAvgDailySleepQuality() {
+    let overallSleepQualitySum = this.allUserInstances.reduce((totalAcc, instance) => {
+    totalAcc += instance.hoursSlept
+    return totalAcc
+  }, 0)
+    let result = overallSleepQualitySum / this.allUserInstances.length
+    return result
   }
 }
 

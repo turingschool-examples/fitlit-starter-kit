797bd0bcd9d294d626ea4eb221080e6ff768335c
“Corinne <corinne.canavan@gmail.com>
2022-05-18 13:07:30 -0600

    Add getFluidOuncesEachDayOfWeek using dayjs; add test

--- src/HydrationRepository.js
+++ src/HydrationRepository.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs'
+
 class HydrationRepository {
   constructor(hydrationData) {
     this.hydrationData = hydrationData;
@@ -26,6 +28,41 @@ class HydrationRepository {
         return totalOunces;
       }, 0)
       return Math.round(hydrationByDate);
+  }
+
+  getFluidOuncesEachDayOfWeek(id, date) {
+    // get all user hydration instances
+    const allHydrationDataById = this.getUserById(id);
+
+    // create array of 7 dates starting with date param
+    const weekFromDate = [0,0,0,0,0,0,0].map((el, index) => {
+    return dayjs(date).add([index], 'day').format('YYYY/MM/DD');
+    })
+
+    // Iterate over week array and for each date, add the date as a key
+    // in the acc, and initialize the value at 0.
+    // (e.g., acc = [{"2019/06/15" : 0}])
+
+    // Then, get all user-specific hydration data (allHydrationDataById),
+    // and filter for matching date string. This returns an array of hydro objects specific
+    // to the date string.
+    // (e.g., .filter => [{date: "2019/06/15"...}, {date: "2019/06/15", ....}])
+
+    // Finally, use forEach to iterate over items in the array of filtered date
+    // hydration instances, and add their respective numOunces property to the value
+    // of the date key in the acc
+    // (e.g., forEach => [{date: "2019/06/15", numOz: 1}, {date: "2019/06/15", numOz: 2}]
+    // acc = {"2019/06/15": 3}))
+    
+    const hydroWeek = weekFromDate.reduce((weekObj, date) => {
+      weekObj[date] = 0;
+      allHydrationDataById.filter(hydroObj => hydroObj.date === date)
+      .forEach(el => {
+        weekObj[date] += el.numOunces;
+      })
+      return weekObj;
+    }, {})
+    return hydroWeek;
   }
 }
 
--- test/HydrationRepository-test.js
+++ test/HydrationRepository-test.js
@@ -1,11 +1,14 @@
 import { expect } from 'chai';
+import dayjs from 'dayjs'
 import HydrationRepository from '../src/HydrationRepository';
 import Hydration from '../src/Hydration';
 
 describe('Hydration Repository', () => {
 
 let hydrationData;
-let hydrationRepository;
+let userHydrationData;
+let hydrationRepository1;
+let hydrationRepository2;
 
   beforeEach(() => {
     hydrationData = [
@@ -41,7 +44,46 @@ let hydrationRepository;
       }
     ];
 
-    hydrationRepository = new HydrationRepository(hydrationData);
+    userHydrationData = [
+      {
+        "userID": 1,
+        "date": "2019/06/15",
+        "numOunces": 37
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/16",
+        "numOunces": 69
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/17",
+        "numOunces": 96
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/18",
+        "numOunces": 61
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/19",
+        "numOunces": 91
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/20",
+        "numOunces": 50
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/21",
+        "numOunces": 50
+      }
+    ];
+
+    hydrationRepository1 = new HydrationRepository(hydrationData);
+    hydrationRepository2 = new HydrationRepository(userHydrationData);
   });
 
   it('should be a function', function () {
@@ -49,17 +91,31 @@ let hydrationRepository;
   });
 
   it('should be an instance of Hydration Repository', () => {
-    expect(hydrationRepository).to.be.an.instanceOf(HydrationRepository);
+    expect(hydrationRepository1).to.be.an.instanceOf(HydrationRepository);
   });
 
   it('should return a user\'s hydration data when given an id', () => {
       const hydrationDataUser1 = hydrationData.filter(obj => obj.userID === 1)
 
-    expect(hydrationRepository.getUserById(1)).to.deep.equal(hydrationDataUser1);
-    expect(hydrationRepository.getUserById(1).length).to.equal(2);
+    expect(hydrationRepository1.getUserById(1)).to.deep.equal(hydrationDataUser1);
+    expect(hydrationRepository1.getUserById(1).length).to.equal(2);
   });
 
   it('should return the average fluid ounces of a user when given an id', () => {
-    expect(hydrationRepository.getAvgFluidOuncesById(1)).to.equal(53);
+    expect(hydrationRepository1.getAvgFluidOuncesById(1)).to.equal(53);
   });
+
+  it('should return an object with 7 dates as keys and 7 numbers as values', () => {
+    const hydrationObject = {
+        "2019/06/15": 37,
+        "2019/06/16": 69,
+        "2019/06/17": 96,
+        "2019/06/18": 61,
+        "2019/06/19": 91,
+        "2019/06/20": 50,
+        "2019/06/21": 50
+    }
+
+    expect(hydrationRepository2.getFluidOuncesEachDayOfWeek(1, "2019/06/15")).to.deep.equal(hydrationObject);
+  })
 });

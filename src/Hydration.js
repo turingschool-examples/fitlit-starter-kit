7484273043a4d6de5132022d2a00a6ae8192cda5
“Corinne <corinne.canavan@gmail.com>
2022-05-17 18:01:50 -0600

    Add class structure to Hydration.js; add initial tests for function and instanceOf

---
+++ src/Hydration.js
@@ -0,0 +1,10 @@
+class Hydration {
+  constructor(hydrationObj) {
+    this.userID = hydrationObj.userID;
+    this.date = hydrationObj.date;
+    this.numOunces = hydrationObj.numOunces;
+  }
+}
+
+
+export default Hydration;

de721505e46d65b6bae915a1daffae6d19d7e6a0
Megan Schuetz <mschuetz411@gmail.com>
2022-05-18 18:11:21 -0600

    clear out scripts.js because those changes were on another branch

--- src/scripts.js
+++ src/scripts.js
@@ -20,33 +20,5 @@ import User from './User'
 
 
 
-let 
 
-//random ID
-const getRandomID = () => {
-    return Math.floor(Math.random() * userData.length)
-}
-//make all users from data into user objects
-//could make more dynamic with params to do for every repo
-const getAllUsers = () => {
-    const createUsersArray = userData.map((user) => {
-        return new User(user)
-    });
- putUsersInRepo(createUsersArray) 
-}
-console.log(getAllUsers())
-
-//put array of all user objects in user-repo
-const putUsersInRepo = (usersArray) => {
-    let userRepo = new UserRepository(usersArray)
-    displayName(userRepo)
-}
-
-const getRandomUser = (userRepo) => {
-    displayName(userRepo.getUserById(getRandomID))
-}
-
-const displayName = (user) => {
-
-}
 

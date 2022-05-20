41591ab82d6aeb373996982a0e5e275ba8856bfc
Cami Anderson <camidantas@icloud.com>
2022-05-20 15:55:30 -0400

    Add tests to HydartionRepository

--- src/data/sampleData-hydration.js
+++ src/data/sampleData-hydration.js
@@ -4,6 +4,31 @@ const hydrationData = [  {
   "numOunces": 37
   },
   {
+  "userID": 1,
+  "date": "2019/06/16",
+  "numOunces": 75
+  },
+  {
+  "userID": 1,
+  "date": "2019/06/17",
+  "numOunces": 47
+  },
+  {
+  "userID": 1,
+  "date": "2019/06/18",
+  "numOunces": 85
+  },
+  {
+  "userID": 1,
+  "date": "2019/06/19",
+  "numOunces": 42
+  },
+  {
+  "userID": 1,
+  "date": "2019/06/20",
+  "numOunces": 87
+  },
+  {
   "userID": 2,
   "date": "2019/06/15",
   "numOunces": 75
--- test/HydrationRepository-test.js
+++ test/HydrationRepository-test.js
@@ -1,9 +1,26 @@
 import { expect } from 'chai';
 import HydrationRepository from '../src/HydrationRepository';
 import  hydrationData  from '../src/data/sampleData-hydration';
+import Hydration from '../src/Hydration';
 
 describe('Hydration Repository', () => {
     it('should be a function', function () {
       expect(HydrationRepository).to.be.a('function');
+    });
+    it('should have users', function () {
+        const h2oRepository = new HydrationRepository(hydrationData)
+        expect(h2oRepository.users[0]).is.instanceOf(Hydration);
+    });
+    it('should display average water intake of all time', function () {
+        const h2oRepository = new HydrationRepository(hydrationData)
+        expect(h2oRepository.displayAllTimeAvgOunces(1)).to.equal(62);
+    });
+    it('should display daily water intake', function () {
+        const h2oRepository = new HydrationRepository(hydrationData)
+        expect(h2oRepository.displayDailyAvgOunces(1, "2019/06/15")).to.equal(37);
+    });
+    it('should display weekly water intake', function () {
+        const h2oRepository = new HydrationRepository(hydrationData)
+        expect(h2oRepository.displayDailyAvgOunces(1, "2019/06/15")).to.equal(37);
     });
 })

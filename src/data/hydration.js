f5922c4c9486b19a36a55c13be29f31fc948dd9d
Lourdes Benites <lourdesben95@gmail.com>
2022-05-20 15:20:32 -0600

    Add 2 more days of data

--- src/data/hydration.js
+++ src/data/hydration.js
@@ -1249,7 +1249,507 @@
             userID: 50,
             date: "2019/06/19",
             numOunces: 48
-            }
+            },
+            {
+                userID: 1,
+                date: "2019/06/20",
+                numOunces: 50
+                },
+                {
+                userID: 2,
+                date: "2019/06/20",
+                numOunces: 71
+                },
+                {
+                userID: 3,
+                date: "2019/06/20",
+                numOunces: 51
+                },
+                {
+                userID: 4,
+                date: "2019/06/20",
+                numOunces: 95
+                },
+                {
+                userID: 5,
+                date: "2019/06/20",
+                numOunces: 89
+                },
+                {
+                userID: 6,
+                date: "2019/06/20",
+                numOunces: 85
+                },
+                {
+                userID: 7,
+                date: "2019/06/20",
+                numOunces: 80
+                },
+                {
+                userID: 8,
+                date: "2019/06/20",
+                numOunces: 89
+                },
+                {
+                userID: 9,
+                date: "2019/06/20",
+                numOunces: 58
+                },
+                {
+                userID: 10,
+                date: "2019/06/20",
+                numOunces: 78
+                },
+                {
+                userID: 11,
+                date: "2019/06/20",
+                numOunces: 68
+                },
+                {
+                userID: 12,
+                date: "2019/06/20",
+                numOunces: 31
+                },
+                {
+                userID: 13,
+                date: "2019/06/20",
+                numOunces: 86
+                },
+                {
+                userID: 14,
+                date: "2019/06/20",
+                numOunces: 52
+                },
+                {
+                userID: 15,
+                date: "2019/06/20",
+                numOunces: 50
+                },
+                {
+                userID: 16,
+                date: "2019/06/20",
+                numOunces: 53
+                },
+                {
+                userID: 17,
+                date: "2019/06/20",
+                numOunces: 24
+                },
+                {
+                userID: 18,
+                date: "2019/06/20",
+                numOunces: 22
+                },
+                {
+                userID: 19,
+                date: "2019/06/20",
+                numOunces: 42
+                },
+                {
+                userID: 20,
+                date: "2019/06/20",
+                numOunces: 28
+                },
+                {
+                userID: 21,
+                date: "2019/06/20",
+                numOunces: 79
+                },
+                {
+                userID: 22,
+                date: "2019/06/20",
+                numOunces: 22
+                },
+                {
+                userID: 23,
+                date: "2019/06/20",
+                numOunces: 59
+                },
+                {
+                userID: 24,
+                date: "2019/06/20",
+                numOunces: 66
+                },
+                {
+                userID: 25,
+                date: "2019/06/20",
+                numOunces: 69
+                },
+                {
+                userID: 26,
+                date: "2019/06/20",
+                numOunces: 60
+                },
+                {
+                userID: 27,
+                date: "2019/06/20",
+                numOunces: 58
+                },
+                {
+                userID: 28,
+                date: "2019/06/20",
+                numOunces: 21
+                },
+                {
+                userID: 29,
+                date: "2019/06/20",
+                numOunces: 95
+                },
+                {
+                userID: 30,
+                date: "2019/06/20",
+                numOunces: 95
+                },
+                {
+                userID: 31,
+                date: "2019/06/20",
+                numOunces: 85
+                },
+                {
+                userID: 32,
+                date: "2019/06/20",
+                numOunces: 50
+                },
+                {
+                userID: 33,
+                date: "2019/06/20",
+                numOunces: 54
+                },
+                {
+                userID: 34,
+                date: "2019/06/20",
+                numOunces: 54
+                },
+                {
+                userID: 35,
+                date: "2019/06/20",
+                numOunces: 23
+                },
+                {
+                userID: 36,
+                date: "2019/06/20",
+                numOunces: 53
+                },
+                {
+                userID: 37,
+                date: "2019/06/20",
+                numOunces: 60
+                },
+                {
+                userID: 38,
+                date: "2019/06/20",
+                numOunces: 28
+                },
+                {
+                userID: 39,
+                date: "2019/06/20",
+                numOunces: 38
+                },
+                {
+                userID: 40,
+                date: "2019/06/20",
+                numOunces: 31
+                },
+                {
+                userID: 41,
+                date: "2019/06/20",
+                numOunces: 26
+                },
+                {
+                userID: 42,
+                date: "2019/06/20",
+                numOunces: 99
+                },
+                {
+                userID: 43,
+                date: "2019/06/20",
+                numOunces: 49
+                },
+                {
+                userID: 44,
+                date: "2019/06/20",
+                numOunces: 59
+                },
+                {
+                userID: 45,
+                date: "2019/06/20",
+                numOunces: 51
+                },
+                {
+                userID: 46,
+                date: "2019/06/20",
+                numOunces: 96
+                },
+                {
+                userID: 47,
+                date: "2019/06/20",
+                numOunces: 21
+                },
+                {
+                userID: 48,
+                date: "2019/06/20",
+                numOunces: 74
+                },
+                {
+                userID: 49,
+                date: "2019/06/20",
+                numOunces: 50
+                },
+                {
+                userID: 50,
+                date: "2019/06/20",
+                numOunces: 71
+                },
+                {
+                userID: 1,
+                date: "2019/06/21",
+                numOunces: 50
+                },
+                {
+                userID: 2,
+                date: "2019/06/21",
+                numOunces: 27
+                },
+                {
+                userID: 3,
+                date: "2019/06/21",
+                numOunces: 41
+                },
+                {
+                userID: 4,
+                date: "2019/06/21",
+                numOunces: 91
+                },
+                {
+                userID: 5,
+                date: "2019/06/21",
+                numOunces: 73
+                },
+                {
+                userID: 6,
+                date: "2019/06/21",
+                numOunces: 90
+                },
+                {
+                userID: 7,
+                date: "2019/06/21",
+                numOunces: 95
+                },
+                {
+                userID: 8,
+                date: "2019/06/21",
+                numOunces: 37
+                },
+                {
+                userID: 9,
+                date: "2019/06/21",
+                numOunces: 83
+                },
+                {
+                userID: 10,
+                date: "2019/06/21",
+                numOunces: 95
+                },
+                {
+                userID: 11,
+                date: "2019/06/21",
+                numOunces: 25
+                },
+                {
+                userID: 12,
+                date: "2019/06/21",
+                numOunces: 20
+                },
+                {
+                userID: 13,
+                date: "2019/06/21",
+                numOunces: 55
+                },
+                {
+                userID: 14,
+                date: "2019/06/21",
+                numOunces: 32
+                },
+                {
+                userID: 15,
+                date: "2019/06/21",
+                numOunces: 86
+                },
+                {
+                userID: 16,
+                date: "2019/06/21",
+                numOunces: 61
+                },
+                {
+                userID: 17,
+                date: "2019/06/21",
+                numOunces: 99
+                },
+                {
+                userID: 18,
+                date: "2019/06/21",
+                numOunces: 30
+                },
+                {
+                userID: 19,
+                date: "2019/06/21",
+                numOunces: 55
+                },
+                {
+                userID: 20,
+                date: "2019/06/21",
+                numOunces: 60
+                },
+                {
+                userID: 21,
+                date: "2019/06/21",
+                numOunces: 93
+                },
+                {
+                userID: 22,
+                date: "2019/06/21",
+                numOunces: 73
+                },
+                {
+                userID: 23,
+                date: "2019/06/21",
+                numOunces: 54
+                },
+                {
+                userID: 24,
+                date: "2019/06/21",
+                numOunces: 71
+                },
+                {
+                userID: 25,
+                date: "2019/06/21",
+                numOunces: 22
+                },
+                {
+                userID: 26,
+                date: "2019/06/21",
+                numOunces: 51
+                },
+                {
+                userID: 27,
+                date: "2019/06/21",
+                numOunces: 20
+                },
+                {
+                userID: 28,
+                date: "2019/06/21",
+                numOunces: 27
+                },
+                {
+                userID: 29,
+                date: "2019/06/21",
+                numOunces: 29
+                },
+                {
+                userID: 30,
+                date: "2019/06/21",
+                numOunces: 98
+                },
+                {
+                userID: 31,
+                date: "2019/06/21",
+                numOunces: 94
+                },
+                {
+                userID: 32,
+                date: "2019/06/21",
+                numOunces: 84
+                },
+                {
+                userID: 33,
+                date: "2019/06/21",
+                numOunces: 84
+                },
+                {
+                userID: 34,
+                date: "2019/06/21",
+                numOunces: 81
+                },
+                {
+                userID: 35,
+                date: "2019/06/21",
+                numOunces: 20
+                },
+                {
+                userID: 36,
+                date: "2019/06/21",
+                numOunces: 38
+                },
+                {
+                userID: 37,
+                date: "2019/06/21",
+                numOunces: 86
+                },
+                {
+                userID: 38,
+                date: "2019/06/21",
+                numOunces: 46
+                },
+                {
+                userID: 39,
+                date: "2019/06/21",
+                numOunces: 39
+                },
+                {
+                userID: 40,
+                date: "2019/06/21",
+                numOunces: 36
+                },
+                {
+                userID: 41,
+                date: "2019/06/21",
+                numOunces: 49
+                },
+                {
+                userID: 42,
+                date: "2019/06/21",
+                numOunces: 52
+                },
+                {
+                userID: 43,
+                date: "2019/06/21",
+                numOunces: 75
+                },
+                {
+                userID: 44,
+                date: "2019/06/21",
+                numOunces: 96
+                },
+                {
+                userID: 45,
+                date: "2019/06/21",
+                numOunces: 27
+                },
+                {
+                userID: 46,
+                date: "2019/06/21",
+                numOunces: 57
+                },
+                {
+                userID: 47,
+                date: "2019/06/21",
+                numOunces: 85
+                },
+                {
+                userID: 48,
+                date: "2019/06/21",
+                numOunces: 30
+                },
+                {
+                userID: 49,
+                date: "2019/06/21",
+                numOunces: 64
+                },
+                {
+                userID: 50,
+                date: "2019/06/21",
+                numOunces: 36
+                }
 ];
 
 export default hydrationData;

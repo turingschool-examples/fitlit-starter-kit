7bcf31a55c5b51f6084ad3a53de8512eff921f56
Lourdes Benites <lourdesben95@gmail.com>
2022-05-19 20:15:20 -0600

    Add getAllTimeOuncesAverage method

--- src/HydrationRepository.js
+++ src/HydrationRepository.js
@@ -1,6 +1,22 @@
 class HydrationRepository {
-    constructor() {
-        
+    constructor(data) {
+        this.hydrationData = data;
+    }
+
+    getHydrationDataForUser(idNum) {
+        const hydrationDataForUser = this.hydrationData.filter((obj) => {
+            return obj.userID === idNum;
+        })
+        return hydrationDataForUser;
+    }
+
+    getAllTimeOuncesAverage(idNum) {
+        const hydrationDataForUser = this.getHydrationDataForUser(idNum);
+        let averageUserHydration = hydrationDataForUser.reduce((acc, cur) => {
+            acc += cur.numOunces
+            return acc;
+        }, 0)
+        return averageUserHydration / hydrationDataForUser.length;
     }
 }
 export default HydrationRepository;

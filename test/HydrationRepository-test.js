eca5cf48acb8872f2058b9368bfa2f7e5ecc06ae
Lourdes Benites <lourdesben95@gmail.com>
2022-05-19 20:17:59 -0600

    Add test for getAllTimesOuncesAverage

--- test/HydrationRepository-test.js
+++ test/HydrationRepository-test.js
@@ -19,8 +19,22 @@ describe('HydrationRepository', () => {
         expect(hydrationRepository).to.be.an.instanceof(HydrationRepository)
     })
 
-    it('should take in a users id', () => {
-        expect()
+    it('should hold the users hydration data', () => {
+        expect(hydrationRepository.hydrationData).to.equal(hydrationData);
+    })
+
+    it('should return all of users hydration data given its id number', () => {
+        expect(hydrationRepository.getHydrationDataForUser(1)).to.deep.equal([
+            { userID: 1, date: '2019/06/15', numOunces: 37 },
+            { userID: 1, date: '2019/06/16', numOunces: 69 },
+            { userID: 1, date: '2019/06/17', numOunces: 96 },
+            { userID: 1, date: '2019/06/18', numOunces: 61 },
+            { userID: 1, date: '2019/06/19', numOunces: 91 }
+          ]);
+    })
+
+    it('should have a function that returns the average ounces consumed for all time ', () => {
+        expect(hydrationRepository.getAllTimeOuncesAverage(1)).to.equal(70.8);
     })
 
 })
@@ -29,10 +43,6 @@ describe('HydrationRepository', () => {
 
 
 
-
-
-
-// For a user (identified by their userID - this is the same for all methods requiring a specific user’s data), the average fluid ounces consumed per day for all time
 
 
 // For a user, how many fluid ounces they consumed for a specific day (identified by a date)

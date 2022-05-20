09c0c750a30ba1bbc0780579c39463df42cdf4f6
Megan Schuetz <mschuetz411@gmail.com>
2022-05-19 20:34:28 -0600

    start fetch calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,21 +2,28 @@
 import UserRepository from './UserRepository';
 
 
-const userFetchData = fetch("https:fitlit-api.herokuapp.com/api/v1/users")
+const fetchUserData = () => {
+    return fetch("https:fitlit-api.herokuapp.com/api/v1/users").then(response => response.json())
+} 
 
-Promise.all([userFetchData])
-  .then(files => {
-      files.forEach(file => {
-          process(file.json())
-      })
-  })
+const fetchUserActivity = () => {
+    return fetch("https://fitlit-api.herokuapp.com/api/v1/activity").then(response => response.json())
+} 
 
-const userData = [];
-let process = (prom) => {
-  prom.then(data => {
-    let repo = new UserRepository(data)
-    userData.push(repo)
-  })
+const fetchUserSleep = () => {
+    return fetch("https://fitlit-api.herokuapp.com/api/v1/sleep").then(response => response.json())
 }
 
-export default userData;
+const fetchUserHydration = () => {
+    return fetch("https://fitlit-api.herokuapp.com/api/v1/hydration").then(response => response.json())
+}
+//response is encrypted
+//promise grabs the data out of the air and pins it down
+//second then creates copy reassigns it to global variable
+//.then gives access but doesn't return similar to forEach()
+const getPromiseAll = () => {
+    return Promise.all([fetchUserData(), fetchUserActivity(), fetchUserSleep(), fetchUserHydration()])
+}
+
+
+export default getPromiseAll;

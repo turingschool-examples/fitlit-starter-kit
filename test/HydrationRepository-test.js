353891bafe9901045a8ca82cf59957c2e837830e
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-05-19 19:29:48 -0600

    Add sample data file for hydration

--- test/HydrationRepository-test.js
+++ test/HydrationRepository-test.js
@@ -1,9 +1,9 @@
 import { expect } from 'chai';
-import HydrationRepository from '../src/Hydration';
+import HydrationRepository from '../src/HydrationRepository';
+import  hydrationData  from '../src/data/sampleData-hydration';
 
 describe('Hydration Repository', () => {
     it('should be a function', function () {
       expect(HydrationRepository).to.be.a('function');
     });
-
 })

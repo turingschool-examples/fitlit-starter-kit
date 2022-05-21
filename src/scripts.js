e7815ae01e79e392b02005e32797b02df420a3f9
“Corinne <corinne.canavan@gmail.com>
2022-05-20 18:00:44 -0600

    Minor syntax edits

--- src/scripts.js
+++ src/scripts.js
@@ -21,20 +21,19 @@ import ActivityRepository from './Activity';
 import HydrationRepository from './HydrationRepository';
 import User from './User'
 
-let friends = document.getElementById('friends')
-let welcomeName = document.getElementById('name')
-let stepGoal = document.getElementById('step-goal')
-let stepsTaken = document.getElementById('steps-taken')
-let minsActive = document.getElementById('mins-active')
-let flights = document.getElementById('flights')
-let sleep = document.getElementById('sleep')
-let weeklySleep = document.getElementById('weekly-sleep')
-let avgSleep = document.getElementById('.avg-sleep')
-let waterDrank = document.getElementById('water')
-let weeklyWater = document.getElementById('weekly-water')
-let email = document.getElementById('email')
-let avgStepGoal = document.getElementById('avg-step-goal')
-
+let friends = document.getElementById('friends');
+let welcomeName = document.getElementById('name');
+let stepGoal = document.getElementById('step-goal');
+let stepsTaken = document.getElementById('steps-taken');
+let minsActive = document.getElementById('mins-active');
+let flights = document.getElementById('flights');
+let sleep = document.getElementById('sleep');
+let weeklySleep = document.getElementById('weekly-sleep');
+let avgSleep = document.getElementById('.avg-sleep');
+let waterDrank = document.getElementById('water');
+let weeklyWater = document.getElementById('weekly-water');
+let email = document.getElementById('email');
+let avgStepGoal = document.getElementById('avg-step-goal');
 
 // window.addEventListener('load', Promise.all())
 
@@ -46,97 +45,45 @@ let displayedUsersID;
 
 Promise.all([fetchUserData(), fetchUserActivity(), fetchUserSleep(), fetchUserHydration()])
   .then(data => {
-    console.log('seeifData', data)
       userDataHelper(data[0].userData);
       hydrationDataHelper(data[3].hydrationData);
-  })
+  });
 
-// //usually reassign to global variables
+//usually reassign to global variables
 
 function userDataHelper(data) {
-    console.log('outside',data)
-    displayedUsersID = Math.floor(Math.random() * 50)
-    const usersArray = getAllUsers(data)
-    userRepo = new UserRepository(usersArray)
-    displayUserInfo(userRepo.getUserById(displayedUsersID), userRepo)
+    displayedUsersID = Math.floor(Math.random() * 50);
+    const usersArray = getAllUsers(data);
+    userRepo = new UserRepository(usersArray);
+    displayUserInfo(userRepo.getUserById(displayedUsersID), userRepo);
 }
 
 function getAllUsers(userData) {
     const createUsersArray = userData.map((user) => {
-        return new User(user)
+        return new User(user);
     });
-    return createUsersArray
+    return createUsersArray;
 }
 
 function displayUserInfo(user, userRepo) {
-  welcomeName.innerText = `Welcome, ${user.getUserFirstName()}`
-  stepGoal.innerText = `${user.dailyStepGoal}`
-  email.innerText = `${user.email}`
+  welcomeName.innerText = `Welcome, ${user.getUserFirstName()}`;
+  stepGoal.innerText = `${user.dailyStepGoal}`;
+  email.innerText = `${user.email}`;
 
   const getFriendsNames = user.friends.map((friend) => {
-    return userRepo.getUserById(friend).name
-  })
-  friends.innerText = `${getFriendsNames}`
-  avgStepGoal.innerText = `${userRepo.calculateAvgStepGoal()}`
+    return userRepo.getUserById(friend).name;
+  });
+  friends.innerText = `${getFriendsNames}`;
+  avgStepGoal.innerText = `${userRepo.calculateAvgStepGoal()}`;
 }
 
-// hydration helpers:
 function hydrationDataHelper(data) {
-  console.log("hydroData", data);
   hydrationRepo = new HydrationRepository(data);
-  console.log("hydrationRepo", hydrationRepo);
-  console.log("wtf", userRepo.getUserById(displayedUsersID).id);
-    console.log("wtf2", displayedUsersID);
   displayHydrationInfo(displayedUsersID, hydrationRepo);
 }
 
 function displayHydrationInfo(id, hydrationRepo) {
-  console.log("id", id);
   waterDrank.innerText += `: ${hydrationRepo.getFluidOuncesByDate(id, "2020/01/22")} ounces`;
-  console.log("does this even work", hydrationRepo.getFluidOuncesByDate(id, "2020/01/22"));
   const waterByWeek = hydrationRepo.getFluidOuncesEachDayOfWeek(id, "2020/01/16");
   weeklyWater.innerText += JSON.stringify(waterByWeek);
-  console.log("wtf does this look like", hydrationRepo.getFluidOuncesEachDayOfWeek(id, "2020/01/16"))
 }
-
-
-// WORKED TO INSTANTIATE ALL REPOS
-// function instantiateRepoType(data) {
-//   console.log("i've been hit");
-//   userRepo = new UserRepository(data[0]);
-//   activityRepo = new ActivityRepository(data[1]);
-//   sleepRepo = new SleepRepository(data[2]);
-//   hydrationRepo = new HydrationRepository(data[3]);
-//   console.log("userRepo", userRepo);
-//   console.log("activityRepo", activityRepo);
-//   console.log("sleepRepo", sleepRepo);
-//   console.log("hydrationRepo", hydrationRepo);
-// }
-
-
-
-
-// function getRandomID() {
-//     return Math.floor(Math.random() * userData.length)
-// }
-//make all users from data into user objects
-//could make more dynamic with params to do for every repo
-
-// function getAllUsers() {
-//   console.log(userData)
-//     const createUsersArray = userData.map((user) => {
-//         return new User(user)
-//     });
-//     putUsersInRepo(createUsersArray)
-// }
-// // console.log(getAllUsers())
-// //put array of all user objects in user-repo
-// function putUsersInRepo(usersArray) {
-//     let userRepo = new UserRepository(usersArray)
-//     getRandomUser(userRepo)
-// }
-//
-// function getRandomUser(userRepo) {
-//     displayUserInfo(userRepo.getUserById(getRandomID()), userRepo)
-// }
-//

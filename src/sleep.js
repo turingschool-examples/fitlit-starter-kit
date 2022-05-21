97ba854ad22bfe650f9cbbb4facf1e74744fa373
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-05-20 20:03:35 -0600

    Add methods to return latest object, find sleep quality

--- src/sleep.js
+++ src/sleep.js
@@ -3,8 +3,21 @@ class Sleep {
     this.userId = userId;
     this.allUserInstances = allUserInstances;
     this.avgSleepQuality = this.calculateAvgDailySleepQuality();
+    this.avgHoursSlept = this.calculateAvgHoursSlept();
   };
+  returnLatest() {
+    return this.allUserInstances[0]
+  }
   calculateAvgDailySleepQuality() {
+    let overallSleepQualitySum = this.allUserInstances.reduce((totalAcc, instance) => {
+    totalAcc += instance.sleepQuality;
+    return totalAcc;
+  }, 0)
+    let result = overallSleepQualitySum / this.allUserInstances.length;
+    result = (Math.round(result * 10) / 10)
+    return result;
+  };
+  calculateAvgHoursSlept() {
     let overallSleepQualitySum = this.allUserInstances.reduce((totalAcc, instance) => {
     totalAcc += instance.hoursSlept;
     return totalAcc;

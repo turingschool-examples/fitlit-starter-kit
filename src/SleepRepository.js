b85b79efafb66bcb6921a2c59abc7e6c8f5ce973
Baskanbetul <btlbskn@hotmail.com>
2022-05-20 22:36:17 -0600

    Add sleep hours by day functionality

--- src/SleepRepository.js
+++ src/SleepRepository.js
@@ -2,6 +2,7 @@ class SleepRepository {
   constructor(data) {
     this.sleepData = data;
   }
+
   getSleepDataForUser(idNum) {
     const sleepDataForUser = this.sleepData.filter((obj) => {
       if (obj.userID === idNum) {
@@ -10,23 +11,33 @@ class SleepRepository {
     });
     return sleepDataForUser;
   }
+
   getAverageSleepQualityForAll(idNum) {
-      const averageSleeps = this.getSleepDataForUser(idNum);
-        let averageUserSleepHours = averageSleeps.reduce((acc, cur) => {
-          acc += cur.sleepQuality;
-          return acc;
-        }, 0);
-    return averageUserSleepHours / averageSleeps.length;
+    const averageSleeps = this.getSleepDataForUser(idNum);
+    let averageUserSleepQualityForAll = averageSleeps.reduce((acc, cur) => {
+      acc += cur.sleepQuality;
+      return acc;
+    }, 0);
+    return averageUserSleepQualityForAll / averageSleeps.length;
+  }
+
+  getAverageSleepHours(idNum) {
+    const sleepDataForUser = this.getSleepDataForUser(idNum);
+    let averageUserSleepHours = sleepDataForUser.reduce((acc, cur) => {
+      acc += cur.hoursSlept;
+      return acc;
+    }, 0);
+    return averageUserSleepHours / sleepDataForUser.length;
   }
   
-//   getAverageSleepHours(idNum) {
-//     const sleepDataForUser = this.getSleepDataForUser(idNum);
-//     let averageUserSleepHours = sleepDataForUser.reduce((acc, cur) => {
-//       acc += cur.hoursSlept;
-//       return acc;
-//     }, 0);
-//     return averageUserSleepHours / sleepDataForUser.length;
-//   }
+  getSleepHoursByDay(date) {
+    const sleepHoursDay = this.sleepData.find((element) => {
+      if (element.date === date) {
+        return element;
+      }
+    });
+    return sleepHoursDay.hoursSlept;
+  }
 }
 
 export default SleepRepository;

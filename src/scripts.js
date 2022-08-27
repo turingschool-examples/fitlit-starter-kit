d7faf35a67fa24ee99a0b901e7dda34ace2047e6
Andrew Knapick <ajknapick@yahoo.com>
2022-08-27 17:50:09 -0500

    Create random user function, write HTML

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,14 @@
 // This is the JavaScript entry file - your code begins here
+
+
 // Do not delete or rename this file ********
 
 console.log(userData,"<>>>>userData")
 // An example of how you tell webpack to use a CSS file
 import './css/styles.css';
-
+import User from './user'
+import UserRepository from './UserRepository';
+import { sampleUsers } from './sample-data';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
@@ -14,4 +18,12 @@ console.log('This is the JavaScript entry file - your code begins here.');
 
 import userData from './data/users';
 
-import UserRepository from './UserRepository';
+
+function getRandomUser() {
+  let randomUser = sampleUsers[Math.floor(Math.random() * sampleUsers.length)]
+  newUser = new User(randomUser)
+}
+getRandomUser();
+console.log(newUser);
+
+

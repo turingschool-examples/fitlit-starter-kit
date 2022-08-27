fa829754b3e923a9f75eb6d6dacda1a1c04ac84d
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-08-27 12:04:59 -0600

    Add: constructor with properties

--- src/User.js
+++ src/User.js
@@ -1,5 +1,13 @@
 class User {
-    
+    constructor(userData){
+        this.id= userData.id
+        this.name= userData.name
+        this.address= userData.address
+        this.email= userData.email
+        this.strideLength= userData.strideLength
+        this.dailyStepGoal= userData.dailyStepGoal
+        this.friends= userData.friends
+    }
 }
 
 module.exports = User;

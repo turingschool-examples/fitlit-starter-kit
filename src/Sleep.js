d8609ec961fc078af387be6c39d1cdfcdb90b6e5
Leah Young <leah.young21@alumni.colostate.edu>
2022-08-27 13:39:37 -0600

    add sleep and hydration classes

---
+++ src/Sleep.js
@@ -0,0 +1,14 @@
+class Sleep {
+  constructor(sleepData) {
+    this.userID = sleepData.userID;
+    this.date = sleepData.date;
+    this.hoursSlept = sleepData.hoursSlept;
+    this.sleepQuality = sleepData.sleepQuality;
+
+  }
+}
+
+
+
+
+export default Sleep;

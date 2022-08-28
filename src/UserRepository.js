5862ed7cbeab01c8adfc78b6a1c206d3cc008b23
Nicole Forseth <forseth.nico@gmail.com>
2022-08-28 13:43:57 -0600

    Create user repo class and unit tests

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,5 +1,21 @@
 class UserRepository {
+  constructor(data) {
+    this.allUserData = data;
+    this.avgUserStepGoal = null;
+  }
+  findUserData(id) {
+    return this.allUserData[id - 1];
+  }
 
+  findAverageStepGoal() {
+    const userSteps = this.allUserData.map((user) => user.dailyStepGoal);
+    const allUserSteps = userSteps.reduce((total, userStep) => {
+      total += userStep;
+      return total;
+    }, 0);
+    this.avgUserStepGoal = Math.round(allUserSteps / this.allUserData.length);
+    return this.avgUserStepGoal;
+  }
 }
 
 export default UserRepository;
--- src/data/users.js
+++ src/data/users.js
@@ -676,4 +676,7 @@
     ]
   }
 ];
+
+
+
 export default userData;

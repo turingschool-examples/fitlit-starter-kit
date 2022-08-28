c062fbf76fea8564aa24dab9d014fa091a86661a
Emily Miles <ms.emily.j.miles@gmail.com>
2022-08-28 18:09:42 -0400

    'Refactor User files with ES6 functions'

--- test/user-test.js
+++ test/user-test.js
@@ -2,76 +2,56 @@ import { expect } from 'chai';
 import User from '../src/user';
 
 
-describe('User', function() {
+describe('User', () => {
     let user1;
     let user2;
+
     beforeEach(() => {
-        user1 = new User({
-            "id": 1,
-            "name": "Luisa Hane",
-            "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
-            "email": "Diana.Hayes1@hotmail.com",
-            "strideLength": 4.3,
-            "dailyStepGoal": 10000,
-            "friends": [
-            16,
-            4,
-            8
-            ]
-        });
-        user2 = new User({
-            "id": 2,
-            "name": "Jarvis Considine",
-            "address": "30086 Kathryn Port, Ciceroland NE 07273",
-            "email": "Dimitri.Bechtelar11@gmail.com",
-            "strideLength": 4.5,
-            "dailyStepGoal": 5000,
-            "friends": [
-            9,
-            18,
-            24,
-            19
-            ]
-        });
+        user1 = new User({id: 1, name: 'Luisa Hane', address: '15195 Nakia Tunnel, Erdmanport VA 19901-1697', email: 'Diana.Hayes1@hotmail.com', strideLength: 4.3, dailyStepGoal: 10000, friends: [16, 4, 8]});
+        user2 = new User({id: 2, name: 'Jarvis Considine', address: '30086 Kathryn Port, Ciceroland NE 07273', email: 'Dimitri.Bechtelar11@gmail.com', strideLength: 4.5, dailyStepGoal: 5000, friends: [9, 18, 24, 19]});
     });
 
-
-    it('should be a function', function() {
-    
+    it('should be a function', () => {
         expect(User).to.be.a('function');
-    })
-    it('should have an ID', function() {
+    });
+
+    it('should have an ID', () => {
         expect(user1.id).to.equal(1);
         expect(user2.id).to.equal(2);
-    })
-    it('should be able to have a name', function() {
+    });
+
+    it('should be able to have a name', () => {
         expect(user1.name).to.equal("Luisa Hane");
         expect(user2.name).to.equal("Jarvis Considine");
-    })
-    it('should be able to have an address', function() {
+    });
+
+    it('should be able to have an address', () => {
         expect(user1.address).to.equal("15195 Nakia Tunnel, Erdmanport VA 19901-1697");
         expect(user2.address).to.equal("30086 Kathryn Port, Ciceroland NE 07273");
-    })
-    it('should be able tohave an email', function() {
+    });
+
+    it('should be able tohave an email', () => {
         expect(user1.email).to.equal("Diana.Hayes1@hotmail.com");
         expect(user2.email).to.equal("Dimitri.Bechtelar11@gmail.com");
-    })
-    it('should be able to have a stride length', function() {
+    });
+
+    it('should be able to have a stride length', () => {
         expect(user1.strideLength).to.equal(4.3);
         expect(user2.strideLength).to.equal(4.5);
-    })
-    it('should be able to have a daily step goal', function() {
+    });
+
+    it('should be able to have a daily step goal', () => {
         expect(user1.dailyStepGoal).to.equal(10000);
         expect(user2.dailyStepGoal).to.equal(5000);
-    })
-    it('should be able to have friends', function() {
+    });
+
+    it('should be able to have friends', () => {
         expect(user1.userFriends).to.deep.equal([16, 4, 8]);
         expect(user2.userFriends).to.deep.equal([9, 18, 24, 19]);
-    })
-    it('should return a user\'s first name', function() {  
+    });
+
+    it('should return a user\'s first name', () => {  
         expect(user1.returnName()).to.equal("Luisa");
-        expect(user2.returnName()).to.equal("Jarvis")      
-    })
-
-
+        expect(user2.returnName()).to.equal("Jarvis");      
+    });
 })

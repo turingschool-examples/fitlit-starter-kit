fafe13867707051ee32059679649327921167f08
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-08-28 14:39:34 -0600

    Add tests and pass for user class

---
+++ test/user-test.js
@@ -0,0 +1,77 @@
+import { expect } from 'chai';
+import User from '../src/user';
+
+
+describe('User', function() {
+    let user1;
+    let user2;
+    beforeEach(() => {
+        user1 = new User({
+            "id": 1,
+            "name": "Luisa Hane",
+            "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+            "email": "Diana.Hayes1@hotmail.com",
+            "strideLength": 4.3,
+            "dailyStepGoal": 10000,
+            "friends": [
+            16,
+            4,
+            8
+            ]
+        });
+        user2 = new User({
+            "id": 2,
+            "name": "Jarvis Considine",
+            "address": "30086 Kathryn Port, Ciceroland NE 07273",
+            "email": "Dimitri.Bechtelar11@gmail.com",
+            "strideLength": 4.5,
+            "dailyStepGoal": 5000,
+            "friends": [
+            9,
+            18,
+            24,
+            19
+            ]
+        });
+    });
+
+
+    it('should be a function', function() {
+    
+        expect(User).to.be.a('function')
+    })
+    it('should have an ID', function() {
+        expect(user1.id).to.deep.equal(1)
+        expect(user2.id).to.deep.equal(2)
+    })
+    it('should be able to have a name', function() {
+        expect(user1.name).to.deep.equal("Luisa Hane")
+        expect(user2.name).to.deep.equal("Jarvis Considine")
+    })
+    it('should be able to have an address', function() {
+        expect(user1.address).to.deep.equal("15195 Nakia Tunnel, Erdmanport VA 19901-1697")
+        expect(user2.address).to.deep.equal("30086 Kathryn Port, Ciceroland NE 07273")
+    })
+    it('should be able tohave an email', function() {
+        expect(user1.email).to.deep.equal("Diana.Hayes1@hotmail.com")
+        expect(user2.email).to.deep.equal("Dimitri.Bechtelar11@gmail.com")
+    })
+    it('should be able to have a stride length', function() {
+        expect(user1.strideLength).to.deep.equal(4.3)
+        expect(user2.strideLength).to.deep.equal(4.5)
+    })
+    it('should be able to have a daily step goal', function() {
+        expect(user1.dailyStepGoal).to.deep.equal(10000)
+        expect(user2.dailyStepGoal).to.deep.equal(5000)
+    })
+    it('should be able to have friends', function() {
+        expect(user1.userFriends).to.deep.equal([16, 4, 8])
+        expect(user2.userFriends).to.deep.equal([9, 18, 24, 19])
+    })
+    it('should return a user\'s first name', function() {  
+        expect(user1.returnName()).to.equal("Luisa")
+        expect(user2.returnName()).to.equal("Jarvis")      
+    })
+
+
+})

04d2d53de1f3fee06175f71a09a909a14f596013
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-08-28 15:03:30 -0600

    remove unnecessary deep and add semicolons

--- test/user-test.js
+++ test/user-test.js
@@ -38,38 +38,38 @@ describe('User', function() {
 
     it('should be a function', function() {
     
-        expect(User).to.be.a('function')
+        expect(User).to.be.a('function');
     })
     it('should have an ID', function() {
-        expect(user1.id).to.deep.equal(1)
-        expect(user2.id).to.deep.equal(2)
+        expect(user1.id).to.equal(1);
+        expect(user2.id).to.equal(2);
     })
     it('should be able to have a name', function() {
-        expect(user1.name).to.deep.equal("Luisa Hane")
-        expect(user2.name).to.deep.equal("Jarvis Considine")
+        expect(user1.name).to.equal("Luisa Hane");
+        expect(user2.name).to.equal("Jarvis Considine");
     })
     it('should be able to have an address', function() {
-        expect(user1.address).to.deep.equal("15195 Nakia Tunnel, Erdmanport VA 19901-1697")
-        expect(user2.address).to.deep.equal("30086 Kathryn Port, Ciceroland NE 07273")
+        expect(user1.address).to.equal("15195 Nakia Tunnel, Erdmanport VA 19901-1697");
+        expect(user2.address).to.equal("30086 Kathryn Port, Ciceroland NE 07273");
     })
     it('should be able tohave an email', function() {
-        expect(user1.email).to.deep.equal("Diana.Hayes1@hotmail.com")
-        expect(user2.email).to.deep.equal("Dimitri.Bechtelar11@gmail.com")
+        expect(user1.email).to.equal("Diana.Hayes1@hotmail.com");
+        expect(user2.email).to.equal("Dimitri.Bechtelar11@gmail.com");
     })
     it('should be able to have a stride length', function() {
-        expect(user1.strideLength).to.deep.equal(4.3)
-        expect(user2.strideLength).to.deep.equal(4.5)
+        expect(user1.strideLength).to.equal(4.3);
+        expect(user2.strideLength).to.equal(4.5);
     })
     it('should be able to have a daily step goal', function() {
-        expect(user1.dailyStepGoal).to.deep.equal(10000)
-        expect(user2.dailyStepGoal).to.deep.equal(5000)
+        expect(user1.dailyStepGoal).to.equal(10000);
+        expect(user2.dailyStepGoal).to.equal(5000);
     })
     it('should be able to have friends', function() {
-        expect(user1.userFriends).to.deep.equal([16, 4, 8])
-        expect(user2.userFriends).to.deep.equal([9, 18, 24, 19])
+        expect(user1.userFriends).to.deep.equal([16, 4, 8]);
+        expect(user2.userFriends).to.deep.equal([9, 18, 24, 19]);
     })
     it('should return a user\'s first name', function() {  
-        expect(user1.returnName()).to.equal("Luisa")
+        expect(user1.returnName()).to.equal("Luisa");
         expect(user2.returnName()).to.equal("Jarvis")      
     })
 

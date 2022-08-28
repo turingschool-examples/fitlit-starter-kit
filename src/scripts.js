cb70b0ecd3dd6e95772ebcccbc9d53186385614c
William Hobson <will.hobson85@gmail.com>
2022-08-28 15:02:41 -0400

    Start building fetch

--- src/scripts.js
+++ src/scripts.js
@@ -3,12 +3,17 @@
 
 // Do not delete or rename this file ********
 
-console.log(userData,"<>>>>userData")
+// console.log(userData,"<>>>>userData")
 // An example of how you tell webpack to use a CSS file
 import './css/styles.css';
 import User from './user'
 import UserRepository from './UserRepository';
 import { sampleUsers } from './sample-data';
+import { fetchAllData } from './apiCalls';
+
+let allUsers
+
+
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
@@ -18,12 +23,25 @@ console.log('This is the JavaScript entry file - your code begins here.');
 
 import userData from './data/users';
 
-
-function getRandomUser() {
-  let randomUser = sampleUsers[Math.floor(Math.random() * sampleUsers.length)]
-  newUser = new User(randomUser)
+function initializeData() {
+  Promise.all([fetchAllData('users'), fetchAllData('sleep'), fetchAllData('hydration')]).then(
+    (data) =>  {
+      allUsers = new UserRepository(data[0])
+      console.log("users", data[0]);
+      console.log("sleep", data[1]);
+      console.log("hydration", data[2]);
+    }
+  );
 }
-getRandomUser();
-console.log(newUser);
+initializeData()
 
+console.log("all users 38", allUsers);
+
+function getRandomUser(allUsers) {
+  let randomUser = allUsers[Math.floor(Math.random() * allUsers.length)]
+  newUser = new User(randomUser)
+  console.log("new User", newUser);
+}
+
+getRandomUser();
 

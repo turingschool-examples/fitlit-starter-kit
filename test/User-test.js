796b4fa2f539e57f495136a3b838d7e6bd4866c4
Leah Young <leah.young21@alumni.colostate.edu>
2022-08-28 17:09:54 -0600

    add tests for User class properties

--- test/User-test.js
+++ test/User-test.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import User from '../src/User';
 import UserRepository from '../src/UserRepository';
-import { user } from '../src/data/users';
+import { users } from '../src/data/users';
 
 describe('User', () => {
 
@@ -38,5 +38,35 @@ describe('User', () => {
     expect(user1).to.be.an.instanceOf(User);
   });
 
-  
+  it('should have a user id', () => {
+    expect(user1.id).to.equal(1);
+  });
+
+  it('should have a user name', () => {
+    expect(user1.name).to.equal('Luisa Hane');
+  });
+
+  it('should have a user address', () => {
+    expect(user1.address).to.equal('15195 Nakia Tunnel, Erdmanport VA 19901-1697');
+  });
+
+  it('should have a user email', () => {
+    expect(user1.email).to.equal('Diana.Hayes1@hotmail.com');
+  });
+
+  it('should have a user\s strideLength', () => {
+    expect(user1.strideLength).to.equal(4.3);
+  });
+
+  it('should have a user\s daily step goal', () => {
+    expect(user1.dailyStepGoal).to.equal(10000);
+  });
+
+  it('should have a list of user\s friends', () => {
+    expect(user1.friends.length).to.equal(3);
+  });
+
+  it('should return a users name', () => {
+    expect(user1.returnUserName()).to.equal('Luisa');
+  });
 });

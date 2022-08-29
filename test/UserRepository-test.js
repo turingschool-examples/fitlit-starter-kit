2d7ae0012359a62f06697c4f0a3826fef7e13d75
Morgan Berry <morganlyn.berry@gmail.com>
2022-08-29 15:00:09 -0600

    Add tests for the USerRepo and method structures for UserRepo

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,8 +1,31 @@
 import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
+import { userData } from '../src/data/users';
 
 describe('User Repository', () => {
+  let userRepository;
+
+    beforeEach(() => {
+      userRepository = new UserRepository(userData);
+    });
+
   it('should be a function', function () {
     expect(UserRepository).to.be.a('function');
   });
+
+  it('should be an instance of UserRepositiry', () => {
+    expect(userRepository).to.be.an.instanceOf(UserRepository);
+  })
+
+  it('should hold all the users data', () => {
+    expect(userRepository.findUserData().to.deep.equal())
+  })
+
+  it('should return a users data when given a user/s id', () => {
+    expect(userRepository.findUserData().to.deep.equal())
+  })
+
+  it('should return an average step goal amonst all users', () => {
+    expect(userRepository.calculateAverageStepGoals()).toequal();
+  })
 });

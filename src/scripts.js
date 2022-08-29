a652e4938f7b0d7ae601e6a613f3cce229fd0faf
Morgan Berry <morganlyn.berry@gmail.com>
2022-08-29 08:24:57 -0600

    Add organization to scripts.js file and a few lines to the HTML

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,49 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
+// ######### Imports ###########
+import userData from './data/users';
+import sleepData from './data/sleepData';
+import hydrationData from './data/hydrationData';
+import UserRepository from './UserRepository';
+import User from './User';
+import Sleep from './Sleep';
+import Hydration from './Hydration';
+
+// ######### Query Selectors ###########
+
+
+
+
+// ######### Global Variables ###########
+// let user;
+// let userData;
+// let hydrationData;
+// let sleepData;
+
+
+
+
+// ######### Promises ###########
+//This is were our fetch (API) will go!
+
+
+
+
+// ######### Event Listeners ###########
+//window.addEventListener('load', getPromiseData);
+
+
+
+
+// ######### On-Load Function ###########
+
+// function getRandomPageUser() {
+//   const pageNameIndex = randomIndex(pageNames);
+//   return pageNames[pageNameIndex];
+// }
+//
+// function welcomeUser() {
+//   userWelcome.innerText = `${getRandomPageuser()} ${user.name}?`;
+// }
+
 
 console.log(userData,"<>>>>userData")
 // An example of how you tell webpack to use a CSS file
@@ -11,12 +55,3 @@ import './images/turing-logo.png'
 console.log('This is the JavaScript entry file - your code begins here.');
 console.log(sleepData)
 console.log(Sleep)
-// An example of how you tell webpack to use a JS file
-
-import userData from './data/users';
-import sleepData from './data/sleepData';
-import hydrationData from './data/hydrationData';
-import UserRepository from './UserRepository';
-import User from './User';
-import Sleep from './Sleep';
-import Hydration from './Hydration';

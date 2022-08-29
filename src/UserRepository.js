3e47369a273e354dfd761e48885b848be6474e64
Colin Ciervo <colinciervo@@gmail.com>
2022-08-29 18:20:40 -0400

    Userrepository add class functions

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,6 +1,21 @@
 class UserRepository {
-    constructor() {
-        
+    constructor(data) {
+        this.data = data
+    }
+    findUsersData(id) {
+        const wholeUser = this.data.reduce((acc, person) => {
+            if (person.id === id) {
+                acc.push(person)
+                return acc
+            }
+            return acc
+        },[])
+    }
+    avgStepGoal() {
+        const stepGoal = this.data.reduce((acc, person) => {
+            acc += parseInt(person.dailyStepGoal)
+            return acc
+        },0) / this.data.length
     }
 
 }

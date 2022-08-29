f3b86d602a4c8e4287ecc7a46aa0b89a38329636
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-08-29 17:54:15 -0600

    Ref: refactored functions

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,5 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
-const userRepository = new UserRepository(userData);
-const randomIndex = getRandomIndex();
-const userInfo = userRepository.findUserData(randomIndex);
-const user = new User(userInfo);
 console.log(userData, "<>>>>userData");
 // An example of how you tell webpack to use a CSS file
 import "./css/styles.css";
@@ -18,32 +14,70 @@ console.log("This is the JavaScript entry file - your code begins here.");
 import userData from "./data/users";
 
 import UserRepository from "./UserRepository";
+//variables
+const userRepository = new UserRepository(userData);
+const randomIndex = getRandomIndex();
+const userInfo = userRepository.findUserData(randomIndex);
+const user = new User(userInfo);
 
-// Query Selectors
-const welcomeMessage = document.guerySelector(".welcome-message");
-const userName = document.querySelector(".user-name-card");
-const friends = document.querySelector(".friends");
+// Query Selectors by class Name
+const welcomeMessage = document.getElementsByClassName("welcome-message");
+const userName = document.getElementsByClassName("user-info-card");
+const friends = document.getElementsByClassName("friend-card");
 
 // EVent Listeners
+window.addEventListener("load", changeWelcomeMessage);
 welcomeMessage.addEventListener(changeInnerText);
 
 function getRandomIndex() {
-  const userDataIndex = Math.floor(Math.random() * userData.length);
+  return Math.floor(Math.random() * userData.length);
 }
+
 function changeWelcomeMessage() {
   welcomeMessage.innerText = `Welcome, ${user.getFirstName()}!`;
+  displayUserDetails();
 }
+
 function displayUserDetails() {
-  userName.innerHTML += `<h3 class="user-name">Hi, ${user.getFirstName()}!</h3>
-    <p class="email">${user.email}</p>
-    <p class="address">${user.address}</p>
-    <p class ="stride-length">Stride Length: ${user.strideLength}</p>`;
+  userName.innerHTML += `
+    <h3 class="user-name">Hi, ${user.getFirstName()}!</h3>
+    <ul>
+        <li class="email"> Email: ${user.email} </li>
+        <li class="address"> ${user.address} </li>
+        <li class ="stride-length"> ${user.strideLength} </li>
+    </ul>`;
+
+  displayFriends();
 }
+
 function displayFriends() {
-  user.friends.map((friend) => {
-    const userFriend = userRepository.findUserData(friend);
-    return (friends.innerHTML += `
-      <li>${userFriend.name}</li>
-      `);
+  const userName = user.friends.map(
+    (friend) => `<li> ${userInfo.findUserData(friend)} </li>`
+  );
+
+  friend.innerHTML += userName.reduce((acc, name) => {
+    acc = name.name;
+    return `<ul> ${acc} </ul>`;
   });
+  usersGoals();
 }
+
+function usersGoals() {
+  section += `
+        <h2> Goals </h2>
+        <p> ${user.dailyStepGoal} </p>`;
+}
+
+
+/// sleep section need a sleep class 
+
+// function displySleep(){
+//     sleepDetails.innerHTML += `
+//         <ul>
+//             <li> ${user.d}</li>
+//             <li> </li>
+//             <li> </li>
+//             <li> </li>
+//         </ul>
+//     `
+// }

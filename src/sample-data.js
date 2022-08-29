8b883617d46568cf3194f48469efdfe9aa6f4a2e
William Hobson <will.hobson85@gmail.com>
2022-08-29 00:32:04 -0400

    Compile sample data

--- src/sample-data.js
+++ src/sample-data.js
@@ -42,4 +42,87 @@ const sampleUsers =
   ]
 }]
 
-export {sampleUsers}
+const sampleHydration = [
+  {
+    userID: 1,
+    date: "2019/06/15",
+    numOunces: 37
+  },
+  {
+    userID: 1,
+    date: "2019/06/16",
+    numOunces: 69
+  },
+  {
+    userID: 1,
+    date: "2019/06/17",
+    numOunces: 96
+  },
+  {
+    userID: 1,
+    date: "2019/06/18",
+    numOunces: 61
+  },
+  {
+    userID: 1,
+    date: "2019/06/19",
+    numOunces: 91
+  },
+  {
+    userID: 1,
+    date: "2019/06/20",
+    numOunces: 50
+  },
+  {
+    userID: 1,
+    date: "2019/06/21",
+    numOunces: 50
+  },
+  {
+    userID: 1,
+    date: "2019/06/22",
+    numOunces: 43
+  },
+  {
+    userID: 4,
+    date: "2019/06/15",
+    numOunces: 85
+  },
+  {
+    userID: 4,
+    date: "2019/06/16",
+    numOunces: 95
+  },
+  {
+    userID: 4,
+    date: "2019/06/17",
+    numOunces: 82
+  },
+  {
+    userID: 4,
+    date: "2019/06/18",
+    numOunces: 93
+  },
+  {
+    userID: 4,
+    date: "2019/06/19",
+    numOunces: 21
+  },
+  {
+    userID: 4,
+    date: "2019/06/20",
+    numOunces: 95
+  },
+  {
+    userID: 4,
+    date: "2019/06/21",
+    numOunces: 91
+  },
+  {
+    userID: 4,
+    date: "2019/06/22",
+    numOunces: 34
+  }
+]
+
+export {sampleUsers, sampleHydration}

07fc5bcb20da568ec8a1334690e1501f0eaeb04f
Jeffrey Cook <cookturingedu@gmail.com>
2022-08-29 20:50:17 -0400

    Test if User class is a function

---
+++ test/User-test.js
@@ -0,0 +1,32 @@
+import { expect } from "chai";
+import User from "../src/User";
+
+describe("User", () => {
+  let user1;
+  let user2;
+  beforeEach(() => {
+    user1 = new User({
+      id: 1,
+      name: "Luisa Hane",
+      address: "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+      email: "Diana.Hayes1@hotmail.com",
+      strideLength: 4.3,
+      dailyStepGoal: 10000,
+      friends: [16, 4, 8]
+    });
+    user2 = new User({
+      id: 2,
+      name: "Jarvis Considine",
+      address: "30086 Kathryn Port, Ciceroland NE 07273",
+      email: "Dimitri.Bechtelar11@gmail.com",
+      strideLength: 4.5,
+      dailyStepGoal: 5000,
+      friends: [9, 18, 24, 19]
+    });
+  });
+
+  it.skip("should be a function", () => {
+    expect(User).to.be.a("function");
+  });
+
+});

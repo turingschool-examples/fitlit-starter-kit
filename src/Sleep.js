2e541a86fd9b083872d315467d897dc6ccca2672
Nicole Forseth <forseth.nico@gmail.com>
2022-08-30 17:35:39 -0600

    Write TDD and Sleep class, add test sleep data.

---
+++ src/Sleep.js
@@ -0,0 +1,12 @@
+class Sleep {
+    constructor(data) {
+        this.userID = data.userID;
+        this.date = data.date;
+        this.hoursSlept = data.hoursSlept;
+        this.sleepQuality = data.sleepQuality;
+    }
+
+
+}
+
+export default Sleep;

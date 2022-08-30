6fdf1ec2d949d0f363ec549ab40f41b9f07f2fdb
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-08-30 17:49:17 -0500

    Complete dashboard for user info and step goals

--- src/scripts.js
+++ src/scripts.js
@@ -14,22 +14,45 @@ const userEmail = document.querySelector("#user-info-email");
 const userStride = document.querySelector("#user-info-stride");
 const userFriends = document.querySelector("#user-info-friends");
 const userCard = document.querySelector("#user-info-card");
+const userWelcome = document.querySelector("#nav-user-name");
 const userStepsAverage = document.querySelector("#user-step-average"); // Single User
 const overallStepsAverage = document.querySelector("#step-goal-average"); // All Users
 
 // Instances
-let user = new User();
+let userRepo = new UserRepository(userData);
+userRepo.findUsersData(1);
+userRepo.avgStepGoal();
+let thisUser = userRepo.currentUser;
+let user = new User(thisUser[0]);
 
 // Event Listeners
-window.addEventListener("load", displayUserCard);
+window.addEventListener("load", loadHandler);
+
+// Handlers 
+function loadHandler() {
+    displayUserCard();
+    showFirstName();
+    compareStepGoal();
+}
 
 // Functions
 const getRandomIndex = array => {
     return Math.floor(Math.random() * array.length + 1);
 };
 
-// dislayUserCard()
+function displayUserCard() {
+    userName.innerHTML = `Name: ${user.name}`;
+    userAddress.innerHTML = `Address: ${user.address}`;
+    userEmail.innerHTML = `Email: ${user.email}`;
+    userStride.innerHTML = `Stride: ${user.strideLength}`;
+    userFriends.innerHTML = `Friends: ${user.friends}`;
+}
 
-// showFirstName()
+function showFirstName() {
+    userWelcome.innerHTML = `Welcome ${user.returnFirstName()}`;
+};
 
-// compareStepGoal()
+function compareStepGoal() {
+    userStepsAverage.innerHTML = `Daily Step Goal: ${user.dailyStepGoal}`;
+    overallStepsAverage.innerHTML = `Overall Step Goal: ${userRepo.averageStepGoal}`;
+}

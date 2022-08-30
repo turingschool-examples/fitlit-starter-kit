54e160b12a837749d83303b5f32778d6b7215cee
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-08-30 15:54:35 -0500

    Add query selectors and adjust HTML ids

--- src/scripts.js
+++ src/scripts.js
@@ -1,17 +1,35 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
-
-console.log(userData,"<>>>>userData")
-// An example of how you tell webpack to use a CSS file
+// Imports
 import './css/styles.css';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
-console.log('This is the JavaScript entry file - your code begins here.');
-
-// An example of how you tell webpack to use a JS file
-
 import userData from './data/users';
-
 import UserRepository from './UserRepository';
+import User from './User';
+
+// Query Selectors
+const userName = document.querySelector("#user-info-name");
+const userAddress = document.querySelector("#user-info-address");
+const userEmail = document.querySelector("#user-info-email");
+const userStride = document.querySelector("#user-info-stride");
+const userFriends = document.querySelector("#user-info-friends");
+const userCard = document.querySelector("#user-info-card");
+const userStepsAverage = document.querySelector("#user-step-average"); // Single User
+const overallStepsAverage = document.querySelector("#step-goal-average"); // All Users
+
+// Instances
+let user = new User();
+
+// Event Listeners
+window.addEventListener("load", displayUserCard);
+
+// Functions
+const getRandomIndex = array => {
+    return Math.floor(Math.random() * array.length + 1);
+};
+
+// dislayUserCard()
+
+// showFirstName()
+
+// compareStepGoal()

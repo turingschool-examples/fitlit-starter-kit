574c2089d6b47f4c5d6a9d4fef329d662438ab83
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-08-30 15:39:08 -0600

    add User fetch calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,19 @@
 // Your fetch requests will live here!
+function fetchData(details) {
+  return fetch(`https://fitlit-api.herokuapp.com/api/v1/${details}`)
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+}
+const promiseAll = () => {
+  const result = Promise.all([
+    fetchData("users"),
+    fetchData("hydration"),
+    fetchData("sleep"),
+  ]).then((response) => {
+    return response;
+  });
+  console.log(result);
+  return result;
+};
 
-
-console.log('I will be a fetch request!')
-
+export { promiseAll };
--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,3 @@
-console.log(userData, "<>>>>userData");
 // Import CSS and Images
 import "./css/styles.css";
 import "./images/lighting.png";
@@ -6,24 +5,36 @@ import "./images/sandals.png";
 import "./images/water-bottle.png";
 import "./images/logo_transparent.png";
 import "./images/avatar-male.png";
-
+//Import fetch
+import { promiseAll } from "./apiCalls.js";
 //Import Classes
-import userData from "./data/users";
 import User from "./User";
 import UserRepository from "./UserRepository";
 
 //Global Variables
-const user = new User(userData[getRandomIndex(userData)]);
-const allUsers = userData.map((user) => new User(user));
-const userRepository = new UserRepository(allUsers);
-
+let user;
+let userRepository;
+let allUsers;
+let userData;
+let hydrationData;
+let sleepData;
 // Query Selectors
 const userDetails = document.querySelector(".user-card");
 const friendsList = document.querySelector(".friends-card");
 const stepDetails = document.querySelector(".step-card");
 
 // EVent Listeners
-window.addEventListener("load", displayDashboard);
+window.addEventListener("load", promiseAll);
+
+promiseAll().then((responses) => {
+  userData = responses[0];
+  user = new User(userData.userData[getRandomIndex(userData.userData)]);
+  allUsers = userData.userData.map((user) => new User(user));
+  userRepository = new UserRepository(allUsers);
+  hydrationData = responses[1].hydrationData;
+  sleepData = responses[2].sleepData;
+  displayDashboard();
+});
 
 function getRandomIndex(userData) {
   return Math.floor(Math.random() * userData.length);
@@ -42,7 +53,7 @@ function displayUserDetails() {
     <p class="email">Email: ${user.email} </p>
     <p class="address">Address: ${user.address} </p>
     <p class ="stride-length">Stride Length (ft): ${user.strideLength} </p>
-    <p class ="step-goal">Daily Step Goal: ${user.dailyStepGoal}</p>`
+    <p class ="step-goal">Daily Step Goal: ${user.dailyStepGoal}</p>`;
 }
 
 function displayFriends() {
@@ -52,7 +63,8 @@ function displayFriends() {
   );
   const firstNames = foundFriends.map((friend) => friend.getFirstName());
   firstNames.forEach(
-    (friend) => (friendsList.innerHTML += `<section class="friend">
+    (friend) =>
+      (friendsList.innerHTML += `<section class="friend">
     <img
     src="./images/avatar-male.png"
     alt="male avatar"
@@ -65,10 +77,10 @@ function displayFriends() {
 }
 
 function displaySteps() {
-  stepDetails.innerHTML = '';
-  const averageSteps = userRepository.findAverageStepGoal()
-  const comparison = Math.round((user.dailyStepGoal/averageSteps)*100)
+  stepDetails.innerHTML = "";
+  const averageSteps = userRepository.findAverageStepGoal();
+  const comparison = Math.round((user.dailyStepGoal / averageSteps) * 100);
   stepDetails.innerHTML += `<section class='step-comparison-message'><p>Average Step Goal for All Users: ${userRepository.avgUserStepGoal}.</p>
   <p>Your step goal is: ${user.dailyStepGoal}.</p>
-  <p>Your daily step goal is ${comparison}% compared to all average users.</p></section>`
+  <p>Your daily step goal is ${comparison}% compared to all average users.</p></section>`;
 }

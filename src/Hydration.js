2b30439464a3b6365ae48f2c2306d3fa03a64e4a
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-08-30 19:58:55 -0600

    Add calculate ounces per day nethod

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,22 +1,43 @@
 import User from '../src/user';
+import hydrationData from './data/sample-hydration';
 
 
 class Hydration {
-    constructor(hydrationData) {
-      console.log(hydrationData)
-      this.currentUser = hydrationData.userID;
+    constructor(userId, hydrationData) {
+      this.id = userId;
       this.date = hydrationData.date;
       this.ounces = hydrationData.numOunces
-
+      this.hydrationData = hydrationData
       
 
     }
-    getAverageOunces(userData) {
-      const avgOunces = userData.forEach(day => {
-        
-      });
+    ouncesPerDay(date, userInfo) {
+      let dailyOunces = userInfo.find(entry => {
+        if (this.id === entry.userID)
+        return entry.date === date
+      })
+      return {[date]: dailyOunces.numOunces}
+    }
+    ouncesPerWeek
+
+      
+
 
     }
-}
+    // ouncesPerWeek(){}
+    // ouncesPerLife(){}
+    //   const userAvgOunces = userData.reduce((userIDs, user) => {
+    //     new User(user)
+      
+    //   console.log(userAvgOunces)
+    //   return userAvgOunces
+    // }, [])
+/*We want create an array where the keys are user ids and the avg Ounces. the value of avg
+ounces is avg ounces per user. 
+iterate through hydrationData to get id and then another iteration to get get numOunces.
+*/
+  
+    
+  
 
 export default Hydration
--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -2,14 +2,15 @@ import { expect } from 'chai';
 import User from '../src/user';
 import Hydration from '../src/Hydration';
 import hydrationData from "../src/data/sample-hydration";
+import userData from '../src/data/users.js'
 
 describe('Hydration', () => {
   let hydrate1;
   let hydrate2;
   
   beforeEach(() => {
-    hydrate1 = new Hydration(hydrationData)
-    // hydrate2 = new Hydration(2, sampleHydration)
+    hydrate1 = new Hydration(1, hydrationData)
+    hydrate2 = new Hydration(2, hydrationData)
 
   })
   it('should be a function', () => {
@@ -19,9 +20,16 @@ describe('Hydration', () => {
     expect(hydrate1.id).to.equal(1)
   })
   it ('should have a date', () => {
-    expect(hydrate1.date).to.equal()
+    expect(hydrate1.date).to.equal("2019/06/15")
 
   })
+  it ('should calculate the daily ounces per user', () => {
+    expect(hydrate1.ouncesPerDay('2019/06/15', hydrationData)).to.deep.equal({'2019/06/15': 37})
+    expect(hydrate1.ouncesPerDay('2019/06/16', hydrationData)).to.deep.equal({'2019/06/16': 69})
+  })
+  // it ('should calculate weekly ouces per user', () => {
+
+  // })
  
 })
 //

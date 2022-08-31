58d88581dbb84ec6a20796a18b5a823fc63e3db6
Colin Ciervo <colinciervo@@gmail.com>
2022-08-31 14:28:53 -0400

    Issue with local

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,27 +1,26 @@
 class UserRepository {
     constructor(data) {
         this.data = data
-        this.currentUser
-        this.averageStepGoal
-    }
-    findUsersData(id) {
-        const wholeUser = this.data.reduce((acc, person) => {
-            if (person.id === id) {
-                acc.push(person)
-                return acc
-            }
-            return acc
-        },[])
-        this.currentUser = wholeUser
-    }
-    avgStepGoal() {
-        const stepGoal = this.data.reduce((acc, person) => {
-            acc += parseInt(person.dailyStepGoal)
-            return acc
-        },0) / this.data.length
-        this.averageStepGoal = stepGoal
+        //this.currentUser
+        //this.averageStepGoal
     }
 
+    findUsersData(idNum) {
+      const chosenUser = this.data.find(user => {
+        return user.id === idNum;
+      });
+      return chosenUser;
+    };
+
+    avgStepGoal() {
+        const stepGoals = this.data.map(steps => {
+            return steps.dailyStepGoal;
+        });
+        const stepGoalSum = stepGoals.reduce((previousSteps, currentSteps) => {
+          return previousSteps + currentSteps;
+        }, 0);
+        return stepGoalSum / stepGoals.length;
+    };
 }
 
 export default UserRepository;

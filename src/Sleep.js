6ae5e84ec4b2833c4b075c9b3ecc92aee7afc550
Hannah.Celemen <hcelemen@icloud.com>
2022-08-31 17:21:20 -0600

    Add Sleep testing and class

---
+++ src/Sleep.js
@@ -0,0 +1,50 @@
+class Sleep {
+  constructor(userId, sleepData) {
+    this.sleepDataPerUser = sleepData.filter(data => data.userID === userId);
+    this.sleepData = sleepData;
+  }
+
+  // getAverage(minDate, maxDate, numDays) {
+  //   const average = array => array.reduce((a, b) => a + b) / array.length;
+  //   console.log(average([1,2,3,4,5]));
+  // }
+
+  getSleepDataByGivenDay(date, type) {
+    const dailySleep = this.sleepDataPerUser.reduce((acc, data) => {
+      if (data.date === date) {
+        acc = data[type];
+      }
+      
+      return acc;
+    }, 0);
+
+    return dailySleep;
+  }
+
+  getAvgSleepData(type, dataSet) {
+    let sum, average;
+    sum = dataSet.reduce((acc, data) => {
+      if (data[type]) {
+        acc += data[type];
+      }
+
+      return acc;
+    }, 0);
+
+    average = sum / dataSet.length;
+    return Number(average.toFixed(1));
+  }
+
+  getDailySleepByWeek(minDate, maxDate, type) {
+    const start = this.sleepDataPerUser.findIndex(data => data.date === minDate); 
+    const end = this.sleepDataPerUser.findIndex(data => data.date === maxDate);
+    const week = this.sleepDataPerUser.slice(start, end + 1);
+    const sleepPerDay = week.map(data => data[type]);
+
+    return sleepPerDay;
+  }
+  
+}
+
+
+export default Sleep;

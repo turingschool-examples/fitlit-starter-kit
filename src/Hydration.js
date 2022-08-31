f78d238b83e9e41d65a922401c5ac1254420c1a0
Thomas Peterson <thomasedwardpeterson@gmail.com>
2022-08-31 17:51:29 -0600

    Adds methods to hydration

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,24 +1,26 @@
 class Hydration {
-  constructor(hydrationData) {
+  constructor(hydrationData,) {
     this.userID = hydrationData.userID;
     this.date = hydrationData.date;
     this.numOunces = hydrationData.numOunces;
   }
 
-    findUserDataID(id) {
-      const singleHydrationData = this.hydrationData.filter(hydrationObj => hydrationObj.userID === id);
-      console.log('Single Hydration data', singleHydrationData)
-
-      return singleHydrationData;
+    findUserDataID(waterArray,id) {
+      const singleHydrationData = waterArray.filter(hydrationObj => hydrationObj.userID === id);
+      if (singleHydrationData.length === 0) {
+        return "Invalid user ID. Please verify user ID and try again."
+      }
+      console.log(singleHydrationData)
+      return singleHydrationData[0].userID
     };
-
+      
 
     usersDailyOunces(id) {
       let userHydration = findUserDataID(id)
       let sumOfOunces = 0;
       const dailyOunces = userHydrationData.forEach(ounce => sum += ounce.numOunces);
       console.log('sum', sum)
-      return (sum / userHydration.length).toFixed(2)
+      return (sumOfOunces / userHydration.length).toFixed(2)
     }
 }
 

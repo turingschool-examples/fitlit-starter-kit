d7f933edee260af56175f805cb31d3a8ae8d0411
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-08-30 20:04:46 -0600

    Add: test for getWeeklyFluids

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -1,27 +1,64 @@
 import { expect } from "chai";
+///import Hydration from "../src/Hydration";
+import User from "../src/User";
+const userTestData = require("../src/data/userTestData");
 
 describe("Hydration", () => {
-  it("should have an id", () => {
-    expect(hydration.id).to.equal(1);
+  let data;
+  let user;
+  let hydrationData;
+  beforeEach(() => {
+    hydrationData = [
+      {
+        userID: 1,
+        date: "2019/06/15",
+        numOunces: 37,
+      },
+      {
+        userID: 2,
+        date: "2019/06/15",
+        numOunces: 75,
+      },
+      {
+        userID: 3,
+        date: "2019/06/15",
+        numOunces: 47,
+      },
+      {
+        userID: 1,
+        date: "2019/06/15",
+        numOunces: 37,
+      },
+      {
+        userID: 1,
+        date: "2019/06/18",
+        numOunces: 61,
+      },
+      {
+        userID: 1,
+        date: "2019/06/16",
+        numOunces: 69,
+      },
+    ];
+    data = userTestData;
+    user = new User(data[0]);
   });
 
-  it.skip("should have a date", () => {
-    expect(hydration.date).to.equal("2019/06/15");
+  it("should return the average fluid ounces consumed per day for all time", () => {
+    expect(user.calAverageFluid(hydrationData, 1)).to.equal(204);
   });
 
-  it.skip("should have number of ounces", () => {
-    expect(hydration.numOunces).to.equal(37);
+  it("should return fluid ounces they consumed for a specific day (identified by a date)", () => {
+    expect(user.getDayFluid(hydrationData, "2019/06/15")).to.equal(74);
   });
 
-  it.skip("should return the average fluid ounces consumed per day for all time", () => {
-    expect(hydration.calAverageFluid()).to.equal();
-  });
-
-  it.skip("should return fluid ounces they consumed for a specific day (identified by a date)", () => {
-    expect(hydration.getDayFluid("2019/06/15")).to.equal(37);
-  });
-
-  it.skip("should return fluid ounces of water consumed each day over the course of a week (7 days)", () => {
-    expect(hydration.getWeeklyFluids()).to.equal("2019/06/15");
+  it("should return fluid ounces of water consumed each day over the course of a week (7 days)", () => {
+    console.log(user.getWeeklyFluids(hydrationData));
+    expect(user.getWeeklyFluids(hydrationData)).to.deep.equal([
+      { date: "2019/06/15", numOunces: 37 },
+      { date: "2019/06/15", numOunces: 37 },
+      { date: "2019/06/18", numOunces: 61 },
+      { date: "2019/06/16", numOunces: 69 },
+    ]);
   });
 });

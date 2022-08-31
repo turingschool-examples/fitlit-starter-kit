0f7d50b6515586d2d4762d0f802cf3b6bf8701b8
Emily Miles <ms.emily.j.miles@gmail.com>
2022-08-30 22:15:25 -0400

    Fix snytax inconsistencies

--- src/scripts.js
+++ src/scripts.js
@@ -35,42 +35,42 @@ function fetchAllData() {
     .then(data => {
       userData = data[0],
       sleepData = data[1],
-      hydrationData = data[2]
-      currentUser = new User(userData[Math.floor(Math.random() * userData.length)])
+      hydrationData = data[2],
+      currentUser = new User(userData[Math.floor(Math.random() * userData.length)]);
       // hydration = new Hydration(hydrationData)
       // sleep = new Sleep(sleepData)
       renderGreeting(currentUser);
       renderFriendsList(currentUser, userData);
-      renderProfile(currentUser)
+      renderProfile(currentUser);
     }
   );
-}
+};
 
 function loadUserInfo() {
-  fetchAllData()
-}
+  fetchAllData();
+};
 
 function renderGreeting(user) {
   const userFirstName = user.name.split(' ')[0];
   greeting.innerHTML = `Hello, ${userFirstName}!`;
-}
+};
 
 function renderFriendsList(singleUser, userList) {
   const friendNames = userList
     .filter(user => {
       if(singleUser.userFriends.includes(user.id)) {
-        return user.name
+        return user.name;
       }
-    })
+    });
   return friendNames.forEach(friend => {
     friendsList.innerHTML += 
     `<button class="friend">${friend.name}</button>`
   }); 
-}
+};
 
 function renderProfile(user) {
   fullName.innerText = `${user.name}`;
   userAddress.innerText = `${user.email}`;
   userEmail.innerText = `${user.address}`;
   stepGoal.innerText = `${user.dailyStepGoal}`;
-}
+};

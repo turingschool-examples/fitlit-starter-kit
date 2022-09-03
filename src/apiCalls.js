5ddccd882278e52f2c5a4ac5db4065b2f03544f8
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2022-09-02 21:56:00 -0600

    add graphs

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -13,4 +13,5 @@ const fetchAll = () => {
   ])
 }
 
+
 export { fetchAll }
--- src/scripts.js
+++ src/scripts.js
@@ -8,12 +8,14 @@ import './images/icons8-walking-100.png'
 import './images/IMG_4293.png'
 import UserRepository from './UserRepository';
 import { fetchAll } from './apiCalls';
+import datepicker from 'js-datepicker'
 
-// global variables //
+
+// global variables // <--- i do not trust these
 let userAPIData
 let sleepAPIData
 let hydrationAPIData
-console.log("hi")
+
 
 // promises //
 const getFetch = () => {
@@ -37,7 +39,7 @@ const welcomeDisplay = document.querySelector('.header-welcome')
 const stepsGoalDisplay = document.querySelector('.steps-content-goal') 
 const stepsCurrentDisplay = document.querySelector('.steps-content-current')
 const stepsFriendsList = document.querySelectorAll('.step-friend')
-const stepsFriendsDisplay = document.querySelector('.steps-content-friends')
+const stepsFriendsDisplay = document.querySelector('.steps-content-header')
 const friend1 = document.getElementById('friend1')
 const friend2 = document.getElementById('friend2')
 const friend3 = document.getElementById('friend3')
@@ -76,7 +78,7 @@ applyUserName()
 
 function showUserInfo() {
   if (welcomeDisplay.innerText === "WELCOME,") {
-    welcomeDisplay.innerText = `USER ADRESS GOES HERE`;
+    welcomeDisplay.innerText = `USER ADRESS & stride length GOES HERE`;
     userNameDisplay.innerText = ""
   } else {
     welcomeDisplay.innerHTML = "WELCOME,";
@@ -116,7 +118,7 @@ function showStepsFriends() {
       labels: ["Steps Goal", "Steps Remaining"],
       datasets: [{
         label: 'Graph Label', // steps / sleep / hydro
-        backgroundColor: ["rgba(4, 104, 255, 0.6)", "rgba(255, 255, 255, 0)"],
+        backgroundColor: ["rgba(4, 104, 255, 0.6)", "rgb(255, 125, 0, .6)"],
         data: stepsData // [steps goal, ]
       }]
     },
@@ -135,6 +137,20 @@ var barColors = [
   "rgb(75, 0, 130, .6)",
   "rgb(150, 0, 210, .6)"];
 
+  new Chart("compare-avg-goal", {
+    type: "bar",
+    data: {
+      labels: ["Your goal", "Average FitLit Goal"], // bar titles - add friends' names here
+      datasets: [{
+        label: 'Sleep Quality By Day', // steps / sleep / hydro
+        backgroundColor: barColors,
+        data: yValues // add friends' data here
+      }]
+    },
+    // options: {...}
+  });
+  
+
 new Chart("steps-friends-chart", {
   type: "bar",
   data: {
@@ -147,6 +163,34 @@ new Chart("steps-friends-chart", {
   },
   // options: {...}
 });
+
+new Chart("week-in-water", {
+  type: "bar",
+  data: {
+    labels: xValues, // bar titles - add friends' names here
+    datasets: [{
+      label: 'OZ Drank Per Day', // steps / sleep / hydro
+      backgroundColor: "rgba(4, 104, 255, 0.6)",
+      data: yValues // add friends' data here
+    }]
+  },
+  // options: {...}
+});
+
+new Chart("chosen-week-in-water", {
+  type: "bar",
+  data: {
+    labels: xValues, // range of dates
+    datasets: [{
+      label: 'OZ Drank Per Day', // steps / sleep / hydro
+      backgroundColor: "rgba(4, 104, 255, 0.6)",
+      data: yValues // add friends' data here
+    }]
+  },
+  // options: {...}
+});
+
+
 
 new Chart("hydro-homies", {
   type: "bar",
@@ -161,7 +205,7 @@ new Chart("hydro-homies", {
   // options: {...}
 });
 
-new Chart("sleephours", {
+new Chart("average-sleep-hours", {
   type: "bar",
   data: {
     labels: xValues, // bar titles - add friends' names here
@@ -175,7 +219,7 @@ new Chart("sleephours", {
 });
 
 
-new Chart("sleepquality", {
+new Chart("average-sleep-quality", {
   type: "bar",
   data: {
     labels: xValues, // bar titles - add friends' names here
@@ -188,3 +232,19 @@ new Chart("sleepquality", {
   // options: {...}
 });
 
+const pickerUIElement = document.querySelector('.picker')
+const picker = datepicker(pickerUIElement)
+/*
+Datepicker takes 2 arguments:
+1 - selector - two possibilities:
+  1 string - a CSS selector, such as '.my-class', '#my-id', or 'div'.
+  or
+  1 DOM node - provide a DOM node, such as document.querySelector('#my-id').
+2 - (optional) An object full of options.
+
+The return value of the datepicker function is the datepicker instance. See the methods and properties below.
+
+You can use Datepicker with any type of element you want. If used with an <input> element (the common use case), then the <input>'s value will automatically be set when selecting a date.
+
+NOTE: Datepicker will not change the value of input fields with a type of date - <input type="date">. This is because those input's already have a built in calendar and can cause problems. Use <input type="text"> instead.
+*/

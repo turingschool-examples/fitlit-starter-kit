14351d8bde910e09c5711bac1e538d9aac96eb2d
Emily Miles <ms.emily.j.miles@gmail.com>
2022-09-12 18:30:59 -0400

    Refactor to match addition of charts file

--- src/scripts.js
+++ src/scripts.js
@@ -1,22 +1,22 @@
 // Import styles:
 import './css/styles.css';
 import './images/icons8-plus-67.png';
-import './images/icons8-sustainable-energy-96.png';
+import './images/icons8-high-voltage-48.png';
 import './images/icons8-water-96.png';
 import './images/icons8-zzz-96.png';
+import './images/fitlit-logo.png';
 
 // Import local files:
 import fetchData from './apiCalls.js';
+import charts from './charts.js';
 import UserRepository from './UserRepository';
 import User from './User';
 import Hydration from './Hydration';
 import Sleep from './Sleep';
 
-// Import third party libraries:
-
 // Query Selectors
 const greeting = document.querySelector('.greeting');
-const friendsList = document.querySelector('#friendsList');
+const friendsList = document.querySelector('.friends-list');
 const fullName = document.querySelector('.full-name');
 const userAddress = document.querySelector('.user-address');
 const userEmail = document.querySelector('.user-email');
@@ -37,7 +37,8 @@ function fetchAllData() {
   .then(data => {
     userData = data[0],
     sleepData = data[1],
-    hydrationData = data[2],
+    hydrationData = data[2]
+
     currentUser = new User(userData[Math.floor(Math.random() * userData.length)]);
     hydration = new Hydration(currentUser.id, hydrationData);
     sleep = new Sleep(currentUser.id, sleepData);
@@ -45,7 +46,7 @@ function fetchAllData() {
     
     loadUserInfo();
   });
-}
+};
 
 // Event Listeners
 window.addEventListener('load', fetchAllData);
@@ -58,20 +59,25 @@ function loadUserInfo() {
   renderGreeting();
   renderFriendsList();
   renderProfile();
+  renderSleepAverages();
+  charts.renderOuncesPerDay(hydration, '2020/01/22');
+  charts.renderOuncesByWeek(hydration, 194, 201);
+  charts.renderSleepChartByDay(sleep);
+  charts.renderSleepChartByWeek(sleep);
 };
 
 function renderGreeting() {
   const userFirstName = currentUser.name.split(' ')[0];
-      greeting.innerHTML = `Hello, ${userFirstName}!`;
+  greeting.innerHTML = `Hello, ${userFirstName}!`;
 };
 
 function renderFriendsList() {
-  const friendNames = userData
-  .filter(user => {
-    if(currentUser.userFriends.includes(user.id)) {
+  const friendNames = userData.filter(user => {
+    if (currentUser.userFriends.includes(user.id)) {
       return user.name;
     }
   });
+
   return friendNames.forEach(friend => {
     friendsList.innerHTML += 
     `<button class="friend">${friend.name}</button>`
@@ -82,5 +88,11 @@ function renderProfile() {
   fullName.innerText = `${currentUser.name}`;
   userAddress.innerText = `${currentUser.email}`;
   userEmail.innerText = `${currentUser.address}`;
-  stepGoal.innerText = `${currentUser.dailyStepGoal}`;
+  stepGoal.innerText += ` ${currentUser.dailyStepGoal}
+  Average Step Goal: ${allUsers.returnAverageStepGoal()}`;
+};
+
+function renderSleepAverages() {
+  sleepAverages.innerText = `Average Hours of Sleep: ${sleep.getAvgSleepData('hoursSlept', sleepData)}
+  Average Sleep Quality: ${sleep.getAvgSleepData('sleepQuality', sleepData)}`;
 };

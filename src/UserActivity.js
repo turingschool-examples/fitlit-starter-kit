3ebe82eb60d73e72c94de52ed5fe83e05799f8bd
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-09-13 08:56:01 -0600

    Add: numOfStepsMethod

--- src/UserActivity.js
+++ src/UserActivity.js
@@ -1,9 +1,11 @@
-class UserActivity{
-    constructor(UserActivityData){
-        this.userId = UserActivityData.userId
-        this.date = UserActivityData.date
-        this.minutesActive = UserActivityData.minutesActive
-        this.flightsOfStairs = UserActivityData.flightsOfStairs
-    }
+class UserActivity {
+  constructor(UserActivityData) {
+    this.userId = UserActivityData.userId;
+    this.date = UserActivityData.date;
+    this.minutesActive = UserActivityData.minutesActive;
+    this.flightsOfStairs = UserActivityData.flightsOfStairs;
+  }
+  //method for user miles walked
+  numOfSteps(activityData, activityDate) {}
 }
 module.exports = UserActivity;

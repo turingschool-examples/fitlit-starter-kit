72e26ad5e59dff731f93238c0c2c28ecbaaf9e31
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-09-13 08:54:03 -0600

    Add: properties to constructor

--- src/UserActivity.js
+++ src/UserActivity.js
@@ -1,8 +1,9 @@
-const User = require("./User")
-
 class UserActivity{
     constructor(UserActivityData){
-
+        this.userId = UserActivityData.userId
+        this.date = UserActivityData.date
+        this.minutesActive = UserActivityData.minutesActive
+        this.flightsOfStairs = UserActivityData.flightsOfStairs
     }
 }
 module.exports = UserActivity;

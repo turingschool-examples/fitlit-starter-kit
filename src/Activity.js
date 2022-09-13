fd422a5d46c8ebac708b6dc653cad004f382345a
Leah Young <leah.young21@alumni.colostate.edu>
2022-09-12 19:46:29 -0600

    add tests to activity test file

--- src/Activity.js
+++ src/Activity.js
@@ -1,7 +1,31 @@
 class Activity {
-    constructor() {
-
+    constructor(userData) {
+    this.userData = userData;    
     }
+
+  findUserDataID(id) {
+    const activityData = this.userData.filter(activityObj => activityObj.userID === id);
+    return activityData;
+  };
+
+  getMilesFromStrideLength(id) {
+    let userStrideData = this.findUserDataID(id);
+    const strideLengthByDate = userStrideData.map((userDate) => userDate.date).pop();
+  }
+
 }
 
 export default Activity;
+
+
+
+// For a specific day (specified by a date), return the miles a user has walked based on their number of steps (use their strideLength to help calculate this)
+// For a user, (identified by their userID) how many minutes were they active for a given day (specified by a date)?
+// For a user, how many minutes active did they average for a given week (7 days)?
+// For a user, did they reach their step goal for a given day (specified by a date)?
+// For a user, find all the days where they exceeded their step goal // filter method!
+// For a user, find their all-time stair climbing record // find method!
+// For all users, what is the average number of:
+// stairs climbed for a specified date
+// steps taken for a specific date
+// minutes active for a specific date
--- test/Activity-test.js
+++ test/Activity-test.js
@@ -3,19 +3,100 @@ import Activity from '../src/Activity';
 
 
 describe('Activity', () => {
+    let userActivityData, singleUserActivity;
 
     beforeEach(() => {
-        
+        userActivityData = [{
+            userID: 1,
+            date: "2019/06/15",
+            numSteps: 3577,
+            minutesActive: 140,
+            flightsOfStairs: 16
+            },
+            {
+            userID: 1,
+            date: "2019/06/16",
+            numSteps: 6637,
+            minutesActive: 175,
+            flightsOfStairs: 36
+            },
+            {
+            userID: 1,
+            date: "2019/06/17",
+            numSteps: 14329,
+            minutesActive: 168,
+            flightsOfStairs: 18
+            }]
+
+        singleUserActivity = new Activity(userActivityData);
+    
     })
+    it('should be a function',  () => {
+        expect(Activity).to.be.a('function')
+    });
+
+    it('should instantiate Sleep',() => {
+        expect(singleUserActivity).to.be.an.instanceOf(Activity);
+    });
+
+    it('should be able to identify a user by their ID', () => {
+        expect(singleUserActivity.findUserDataID(1)).to.deep.equal(userActivityData);
+    });
+
+    it('should return a user\s number of steps', () => {
+        expect(userActivityData[0].numSteps).to.equal(3577)
+    });
+
+    it('should return a user\s minutes active', () => {
+        expect(userActivityData[0].minutesActive).to.equal(140) 
+    });
+
+    it('should return a user\s flights of stairs climbed', () => {
+        expect(userActivityData[0].flightsOfStairs).to.equal(16)
+    });
+
+    it('should return the miles a user has walked based on their number of steps') {
+
+    };
+
+    it('should return how many minutes were they active for a given day') {
+
+    };
+
+    it('should return the average minutes active for a given week') {
+
+    };
+
+    it('should return the miles a user has walked based on their number of steps') {
+
+    };
+    
+    it('should return true if a user achieved their daily step goal') {
+
+    };
+
+    it('should return false if a user did not achieve their daily step goal') {
+
+    };
+
+    it('should return all the days where they exceeded their step goal') {
+
+    };
+
+    it('should return their all-time stair climbing record') {
+
+    };
+
+    it('should return the average stairs climbed for a specified date') {
+
+    };
+
+    it('should return the average steps taken for a specific date') {
+
+    };
+
+    it('should return the average minutes active for a specific date') {
+
+    };
 })
 
-// For a specific day (specified by a date), return the miles a user has walked based on their number of steps (use their strideLength to help calculate this)
-// For a user, (identified by their userID) how many minutes were they active for a given day (specified by a date)?
-// For a user, how many minutes active did they average for a given week (7 days)?
-// For a user, did they reach their step goal for a given day (specified by a date)?
-// For a user, find all the days where they exceeded their step goal
-// For a user, find their all-time stair climbing record
-// For all users, what is the average number of:
-// stairs climbed for a specified date
-// steps taken for a specific date
-// minutes active for a specific date

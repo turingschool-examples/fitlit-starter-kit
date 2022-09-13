10f1560b81b6448bb82a5f16acda8c8bb79b1a4c
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-09-13 08:42:35 -0600

    Add: globel test data variables

--- test/UserActivity-test.js
+++ test/UserActivity-test.js
@@ -1,3 +1,7 @@
 import { expect } from "chai";
 import User from "../src/User";
 import UserActivity from "../src/UserActivity";
+
+const userTestData = require("../src/data/userTestData");
+const userActivityTestData = require("../src/data/userActivityTestData")
+

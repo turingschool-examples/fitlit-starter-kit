815b74ea4d666307f497b8dbf5ff943cb89af1f3
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-09-13 16:12:28 -0600

    add minutes active method and test

--- src/UserActivity.js
+++ src/UserActivity.js
@@ -5,7 +5,7 @@ class UserActivity {
 
   milesBasedOnSteps(activityDate, user) {
     let userActivity = this.data.filter((data) => data.date === activityDate);
-    if (userActivity === []) {
+    if (userActivity.length < 1) {
       return "Sorry no data available for given date";
     } else {
       let numberOfSteps = userActivity.reduce((acc, activity) => {
@@ -17,16 +17,18 @@ class UserActivity {
       return parseFloat(numberOfMilesWalked.toFixed(2));
     }
   }
-
-  //   getDayMilesWalked(userActivityTestData, date) {
-
-  //     let userActivity = new UserActivity(userActivityTestData);
-
-  //     let numberOFSteps = userActivity.numOfSteps(userActivityTestData, date);
-
-  //     let userMiles = numberOFSteps / this.strideLength / 2000;
-  //     return parseFloat(userMiles.toFixed(2));
-  //   }
+  minutesActive(activityDate, user) {
+    let userActivity = this.data.filter((data) => data.date === activityDate);
+    if (userActivity.length < 1) {
+      return "Sorry no data available for given date";
+    } else {
+      let activeTime = userActivity.reduce((activeMinutes, activity) => {
+        activeMinutes += activity.minutesActive;
+        return activeMinutes;
+      }, 0);
+      return activeTime;
+    }
+  }
 }
 
 module.exports = UserActivity;
--- test/UserActivity-test.js
+++ test/UserActivity-test.js
@@ -15,12 +15,20 @@ describe("UserActivity", () => {
     user = new User(userData[0]);
   });
 
-  it.only("should return the miles a user has walked based on their number of steps specified by a date (using the strideLength)", () => {
+  it("should return the miles a user has walked based on their number of steps specified by a date (using the strideLength)", () => {
     const result = userActivity.milesBasedOnSteps("2019/06/15", user);
     expect(result).to.equal(2.91);
   });
   it("should only work if data is available for date", () => {
     const result = userActivity.milesBasedOnSteps("2022/08/11", user);
+    expect(result).to.equal("Sorry no data available for given date");
+  });
+  it("should have a method to calculate how many minutes a user was active for a given date", () => {
+    const result = userActivity.minutesActive("2019/06/15", user);
+    expect(result).to.equal(140);
+  });
+  it("should only work if data is available for date", () => {
+    const result = userActivity.minutesActive("2022/08/11", user);
     expect(result).to.equal("Sorry no data available for given date");
   });
 });

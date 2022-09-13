d30feab530d5b5bbaa75d583d770fb882619da31
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-09-13 08:58:48 -0600

    fix: syntax

--- src/UserActivity.js
+++ src/UserActivity.js
@@ -1,18 +1,19 @@
-class UserActivity{
-    constructor(UserActivityData){
-        this.userId = UserActivityData.userId
-        this.date = UserActivityData.date
-        this.minutesActive = UserActivityData.minutesActive
-        this.flightsOfStairs = UserActivityData.flightsOfStairs
-    }
-    //method for user miles walked
-numOfSteps(activityData, activityDate) {
-    let userActivity = activityData.find(data => 
-        data.date === activityDate && activityData.userID === this.userId)
-     
-    let walkedMiles = (userActivity.numSteps)
-    return walkedMiles
+class UserActivity {
+  constructor(UserActivityData) {
+    this.userId = UserActivityData.userId;
+    this.date = UserActivityData.date;
+    this.minutesActive = UserActivityData.minutesActive;
+    this.flightsOfStairs = UserActivityData.flightsOfStairs;
+  }
+  //method for user miles walked
+  numOfSteps(activityData, activityDate) {
+    let userActivity = activityData.find(
+      (data) =>
+        data.date === activityDate && activityData.userID === this.userId
+    );
 
-}
+    let walkedMiles = userActivity.numSteps;
+    return walkedMiles;
+  }
 }
 module.exports = UserActivity;

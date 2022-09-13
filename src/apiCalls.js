3238cae81807d52ec41f5fe8fea7cd1a0a24030c
Hannah.Celemen <hcelemen@icloud.com>
2022-09-13 16:50:39 -0600

    Change fetch url

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 const fetchData = (fileName, dataKey) => {
-  return fetch(`https://fitlit-api.herokuapp.com/api/v1/${fileName}`)  
+  return fetch(`http://localhost:3001/api/v1/${fileName}`)  
     .then(response => response.json())
     .then(data => data[dataKey])
     .catch(error => console.log('There was a problem loading your data. Please try again.', error))

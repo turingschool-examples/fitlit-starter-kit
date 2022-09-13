224e098307595e40fead74eac117f73645842901
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-09-13 08:57:39 -0600

    Add: find method for finding user activity in a given data

--- src/UserActivity.js
+++ src/UserActivity.js
@@ -1,11 +1,18 @@
-class UserActivity {
-  constructor(UserActivityData) {
-    this.userId = UserActivityData.userId;
-    this.date = UserActivityData.date;
-    this.minutesActive = UserActivityData.minutesActive;
-    this.flightsOfStairs = UserActivityData.flightsOfStairs;
-  }
-  //method for user miles walked
-  numOfSteps(activityData, activityDate) {}
+class UserActivity{
+    constructor(UserActivityData){
+        this.userId = UserActivityData.userId
+        this.date = UserActivityData.date
+        this.minutesActive = UserActivityData.minutesActive
+        this.flightsOfStairs = UserActivityData.flightsOfStairs
+    }
+    //method for user miles walked
+numOfSteps(activityData, activityDate) {
+    let userActivity = activityData.find(data => 
+        data.date === activityDate && activityData.userID === this.userId)
+     
+    let walkedMiles = (userActivity.numSteps)
+    return walkedMiles
+
+}
 }
 module.exports = UserActivity;

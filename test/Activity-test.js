519d0b91aef017f07bd77db93e2ba7f83a7f54c5
Leah Young <leah.young21@alumni.colostate.edu>
2022-09-12 18:44:15 -0600

    create activity class and test files

---
+++ test/Activity-test.js
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+import Activity from '../src/Activity';
+
+
+describe('Activity', () => {
+
+    beforeEach(() => {
+        
+    })
+})
+
+// For a specific day (specified by a date), return the miles a user has walked based on their number of steps (use their strideLength to help calculate this)
+// For a user, (identified by their userID) how many minutes were they active for a given day (specified by a date)?
+// For a user, how many minutes active did they average for a given week (7 days)?
+// For a user, did they reach their step goal for a given day (specified by a date)?
+// For a user, find all the days where they exceeded their step goal
+// For a user, find their all-time stair climbing record
+// For all users, what is the average number of:
+// stairs climbed for a specified date
+// steps taken for a specific date
+// minutes active for a specific date

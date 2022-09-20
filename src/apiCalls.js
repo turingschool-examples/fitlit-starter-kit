3750dba104261d6bc6a9e57e009713e9d1524b9d
Morgan Berry <morganlyn.berry@gmail.com>
2022-09-19 19:46:51 -0600

    add edits to html

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -5,7 +5,7 @@ const fetchData = (url) => {
 const fetchAll = () => {
   return Promise.all([fetchData('http://localhost:3001/api/v1/users'), fetchData('http://localhost:3001/api/v1/sleep'), fetchData('http://localhost:3001/api/v1/hydration'), fetchData('http://localhost:3001/api/v1/activity')])
 }
-//POST
+
 const postSleep = (id,userInputSleepData) => {
   fetch('http://localhost:3001/api/v1/sleep', {
   method: 'POST',
@@ -37,24 +37,21 @@ const postHydration = (id,userInputHydration) => {
     .then(data => console.log(data))
     .catch(err => console.log(err))
 }
-//GET
+
 
 const fetchSleep = () => {
   Promise.all([fetchData('http://localhost:3001/api/v1/sleep')])
- .then(data => console.log('ya think this will work?',data))
+ .then(data => console.log(data))
 }
 const fetchHydration = () => {
   Promise.all([fetchData('http://localhost:3001/api/v1/hydration')])
-  .then(data => console.log('ya think this will work? water addition', data))}
+  .then(data => console.log(data))}
 
 
 export { fetchAll }
 export { postSleep }
 export { postHydration }
 export { postActivity }
-
-
-
 
 
 const postActivity = (id, userInputActivityData) => {
@@ -75,13 +72,12 @@ const postActivity = (id, userInputActivityData) => {
       console.log(error.message)
       displayErrorMessage(error)
 
-
 })
 }
 
 const fetchActivity = () => {
   Promise.all([fetchData('http://localhost:3001/api/v1/activity')])
- .then(data => console.log('ya think this will work? Activity addition',data))
+ .then(data => console.log(data))
 }
 fetchActivity();
 

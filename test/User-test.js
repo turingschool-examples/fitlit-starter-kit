f0dbdd5eb085a96052536d15d6b4d3b2c2765beb
Craig Weller <crg.weller@gmail.com>
2022-12-03 14:11:20 -0700

    update some syntax

--- test/User-test.js
+++ test/User-test.js
@@ -72,3 +72,16 @@ describe('User', () => {
 // Should have a parameter to take in a userData object
 //Each user should have id, name, address, email, stride length, daily step goal, friends   hydration/sleep?
 // Should have method to return users first name
+
+
+// Let user1 = new User({"id": 1,
+//     "name": "Luisa Hane",
+//     "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+//     "email": "Diana.Hayes1@hotmail.com",
+//     "strideLength": 4.3,
+//     "dailyStepGoal": 10000,
+//     "friends": [
+//       16,
+//       4,
+//       8
+//     ]})

c01e99835ca5b98603e5e395d7bcabf5bab8b030
Craig Weller <crg.weller@gmail.com>
2022-12-03 14:41:10 -0700

    refactor and add beforeeach

--- test/User-test.js
+++ test/User-test.js
@@ -2,64 +2,63 @@ import { expect } from 'chai';
 import User from '../src/User';
 // import UserRepository from '../src/UserRepository';
 import users from '../src/data/users';
-
+import userData from '../src/data/users';
 
 
 describe('User', () => {
+    let user1, user2, user3
+    beforeEach(() => {
+        user1 = new User(userData)
+        user2 = new User(userData)
+        user3 = new User(userData)
+    })
+
     it('should be a function', function () { 
         expect(User).to.be.a('function');
     });
 
     it('should instantiate a new User', function () {
-    let user = new User() 
 
     expect(User).to.be.a('function');
     })
 
     it('should have a Userdata parameter', function () {
-    let user = new User(userData);
 
     expect(userData).to.equal({});
     })
 
     it('should have an id', function () {
-    let user = new User(userData);
 
     expect(user.id).to.equal(user.id);
     })
     
     it('should have an name', function () {
-    let user = new User(userData);
 
     expect(user.name).to.equal(user.name);
     })
         
     it('should have an address', function () {
-    let user = new User(userData);
 
     expect(user.address).to.equal(user.address);
     })
     
     it('should have an email', function () {
-    let user = new User(userData);
 
     expect(user.email).to.equal(user.email);
     })
 
     it('should have a strideLength', function () {
-    let user = new User(userData);
 
     expect(user.strideLength).to.equal(user.strideLength);
     })
     
     it('should have a step goal', function () {
-    let user = new User(userData);
 
     expect(user.dailyStepGoal).to.equal(user.dailyStepGoal);
     })
 
     it('should have friends', function () {
-    let user = new User(userData);
+
 
     expect(user.friends).to.equal(user.friends);
     })

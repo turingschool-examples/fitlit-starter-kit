c1a87c61330340c3c74e70b6c291a0e8e4703135
Rick Vermeil <rickv85@gmail.com>
2022-12-04 16:38:22 -0700

    Initial commit

---
+++ src/data/User-test-data.js
@@ -0,0 +1,42 @@
+const userTestData = [ 
+ {
+    "id": 20,
+    "name": "Ora O'Connell",
+    "address": "79585 Tania Ports, North Lillie MI 38947-4029",
+    "email": "Audreanne.Gulgowski6@yahoo.com",
+    "strideLength": 3.4,
+    "dailyStepGoal": 8000,
+    "friends": [
+      2,
+      12,
+      11,
+      33
+    ]
+  },
+  {
+    "id": 21,
+    "name": "Alexandrea Wehner",
+    "address": "314 Richmond Islands, Brekkefort WI 71702-6994",
+    "email": "Americo_Hammes31@gmail.com",
+    "strideLength": 3,
+    "dailyStepGoal": 9000,
+    "friends": [
+      29,
+      19
+    ]
+  },
+  {
+    "id": 22,
+    "name": "Maria Kemmer",
+    "address": "3416 Zoey Cliff, West Eda PA 17789-2282",
+    "email": "Nya25@yahoo.com",
+    "strideLength": 4.2,
+    "dailyStepGoal": 2000,
+    "friends": [
+      40,
+      44,
+      14
+    ]
+  }];
+
+  module.exports = userTestData;
---
+++ test/User-test.js
@@ -0,0 +1,8 @@
+const chai = require("chai");
+const expect = chai.expect;
+import { userTestData } from '../data/User-test-data.js';
+
+describe('User', function() {
+
+});
+

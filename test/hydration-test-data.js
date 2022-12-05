a5a10cf6d9750719b7af55e35a3b0f291461e1bd
Rick Vermeil <rickv85@gmail.com>
2022-12-04 18:02:19 -0700

    Add hydration and sleep test data samples

---
+++ test/hydration-test-data.js
@@ -0,0 +1,67 @@
+// This data represents two days of hydration data for the User-test
+// group (id#'s 20-22) and another 3 users (id#'s 1-3)
+
+const hydrationTestData = [
+  {
+    userID: 20,
+    date: "2019/06/15",
+    numOunces: 23
+    },
+    {
+    userID: 21,
+    date: "2019/06/15",
+    numOunces: 23
+    },
+    {
+    userID: 22,
+    date: "2019/06/15",
+    numOunces: 54
+    },
+  {
+    userID: 20,
+    date: "2019/06/16",
+    numOunces: 80
+    },
+    {
+    userID: 21,
+    date: "2019/06/16",
+    numOunces: 36
+    },
+    {
+    userID: 22,
+    date: "2019/06/16",
+    numOunces: 41
+    },
+  {
+    userID: 1,
+    date: "2019/06/15",
+    numOunces: 37
+    },
+    {
+    userID: 2,
+    date: "2019/06/15",
+    numOunces: 75
+    },
+    {
+    userID: 3,
+    date: "2019/06/15",
+    numOunces: 47
+    },
+  {
+    userID: 1,
+    date: "2019/06/16",
+    numOunces: 69
+    },
+    {
+    userID: 2,
+    date: "2019/06/16",
+    numOunces: 91
+    },
+    {
+    userID: 3,
+    date: "2019/06/16",
+    numOunces: 99
+    },
+  ];
+
+  module.exports = hydrationTestData;
---
+++ test/sleep-test-data.js
@@ -0,0 +1,79 @@
+// This data represents two days of sleep data for the User-test
+// group (id#'s 20-22) and another 3 users (id#'s 1-3)
+
+const sleepTestData = [
+  {
+    userID: 20,
+    date: "2019/06/15",
+    hoursSlept: 5.9,
+    sleepQuality: 1.6
+    },
+    {
+    userID: 21,
+    date: "2019/06/15",
+    hoursSlept: 9.6,
+    sleepQuality: 1
+    },
+    {
+    userID: 22,
+    date: "2019/06/15",
+    hoursSlept: 9,
+    sleepQuality: 3.1
+    },
+  {
+    userID: 20,
+    date: "2019/06/16",
+    hoursSlept: 4.3,
+    sleepQuality: 1.4
+    },
+    {
+    userID: 21,
+    date: "2019/06/16",
+    hoursSlept: 4.8,
+    sleepQuality: 1.3
+    },
+    {
+    userID: 22,
+    date: "2019/06/16",
+    hoursSlept: 5,
+    sleepQuality: 3.2
+    },
+  {
+    userID: 1,
+    date: "2019/06/15",
+    hoursSlept: 6.1,
+    sleepQuality: 2.2
+    },
+    {
+    userID: 2,
+    date: "2019/06/15",
+    hoursSlept: 7,
+    sleepQuality: 4.7
+    },
+    {
+    userID: 3,
+    date: "2019/06/15",
+    hoursSlept: 10.8,
+    sleepQuality: 4.7
+    },
+  {
+    userID: 1,
+    date: "2019/06/16",
+    hoursSlept: 4.1,
+    sleepQuality: 3.8
+    },
+    {
+    userID: 2,
+    date: "2019/06/16",
+    hoursSlept: 7.5,
+    sleepQuality: 3.8
+    },
+    {
+    userID: 3,
+    date: "2019/06/16",
+    hoursSlept: 10.7,
+    sleepQuality: 3.4
+    },
+];
+
+module.exports = sleepTestData;

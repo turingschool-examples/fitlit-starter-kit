84cf5d1a6db8b19dd0903e19eb2f42d28dee324a
Faye Rosenshein <rosefaye.a@gmail.com>
2022-12-05 15:10:58 -0700

    feat: Add methods to UserRepository

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,12 +1,21 @@
 class UserRepository {
-    constructor(userData,hydrationData,sleepData) {
-        this.selectedUserId = userData.id
-        this.selectedHydrationData = hydrationData
-        this.selectedSleepData = sleepData
+    constructor(oneUserData, allUserData, hydrationData, sleepData) {
+        this.selectedUser = oneUserData
+        this.userData = allUserData
+        this.hydrationData = hydrationData
+        this.sleepData = sleepData
     }
-returnNumOunces() {
-    return this.selectedHydrationData.numOunces
-}
+    findUserId() {
+        return this.selectedUser.id
+    }
+    averageSteps() {
+        let total = 0
+        this.userData.forEach(user =>
+            total += user.dailyStepGoal
+        )
+        let averageSteps = total / this.userData.length
+        return averageSteps
+    }
 }
 
 export default UserRepository;
--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -7,16 +7,18 @@ import userTestData from './User-test-data'
 
 describe('User Repository', () => {
   let selectedUserInt;
-  let selectedUserId;
-  let selectedHydrationData;
-  let selectedSleepData;
+  let selectedUser;
+  let userData
+  let hydrationData;
+  let sleepData;
   let fullUserData;
   beforeEach(() => {
   selectedUserInt = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
-  selectedUserId = new User(userTestData[selectedUserInt]);
-  selectedHydrationData = hydrationTestData.find(data => data.userID === selectedUserId.id);
-  selectedSleepData = sleepTestData.find(data => data.userID === selectedUserId.id);
-  fullUserData = new UserRepository(selectedUserId, selectedHydrationData, selectedSleepData);
+  userData = userTestData
+  selectedUser = new User(userTestData[selectedUserInt]);
+  hydrationData = hydrationTestData;
+  sleepData = sleepTestData;
+  fullUserData = new UserRepository(selectedUser,userData, hydrationData, sleepData);
   });
   it('should be a function', function () {
     expect(UserRepository).to.be.a('function');
@@ -24,13 +26,24 @@ describe('User Repository', () => {
   it('should have an instance that takes the parameters for selectedUser, selectedHydrationData,selectedSleepData', function () {
     expect(fullUserData).to.be.an.instanceOf(UserRepository);
   })
-  it('should have properties of userData, hydrationData, sleepData', function () {
-    expect(fullUserData.selectedUserId).to.equal(userTestData[selectedUserInt].id);
-    expect(fullUserData.selectedHydrationData).to.equal(hydrationTestData.find(data => data.userID === selectedUserId.id));
-    expect(fullUserData.selectedSleepData).to.equal(sleepTestData.find(data => data.userID === selectedUserId.id));
+  it('should have the full user-data as a parameter', function (){
+    expect(fullUserData.userData).to.deep.equal(userTestData);
   })
-  it('should be able to return the number of ounces drank for a day', function () {
-    const ouncesDrank = fullUserData.returnNumOunces()
-    expect(ouncesDrank).to.equal(selectedHydrationData.numOunces)
+  it('should have properties of selectedUser, hydrationData, sleepData', function () {
+    expect(fullUserData.selectedUser).to.deep.equal(userTestData[selectedUserInt]);
+    expect(fullUserData.userData).to.deep.equal(userTestData)
+    expect(fullUserData.hydrationData).to.equal(hydrationTestData);
+    expect(fullUserData.sleepData).to.equal(sleepTestData);
+  })
+  it('should be able to be given an id and return that user\'s data', function () {
+    expect(fullUserData.findUserId()).to.equal(fullUserData.selectedUser.id)
+  })
+  it('should calculate average dailyStepGoals for all users', function () {
+    let total = 0
+    userTestData.forEach(user => 
+        total += user.dailyStepGoal   
+    )
+    let averageStepGoal = total/userTestData.length
+    expect(fullUserData.averageSteps()).to.equal(averageStepGoal)
   })
 });

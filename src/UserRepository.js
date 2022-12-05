046aaa61af1e438d7daf2646163d25150abbf1de
Faye Rosenshein <rosefaye.a@gmail.com>
2022-12-05 11:52:04 -0700

    feat:Add properties to UserRepository class

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,5 +1,12 @@
 class UserRepository {
-
+    constructor(userData,hydrationData,sleepData) {
+        this.selectedUserId = userData.id
+        this.selectedHydrationData = hydrationData
+        this.selectedSleepData = sleepData
+    }
+returnNumOunces() {
+    return this.selectedHydrationData.numOunces
+}
 }
 
 export default UserRepository;
--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,8 +1,36 @@
 import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
+import User from '../src/User';
+import hydrationTestData from './hydration-test-data';
+import sleepTestData from './sleep-test-data';
+import userTestData from './User-test-data'
 
 describe('User Repository', () => {
+  let selectedUserInt;
+  let selectedUserId;
+  let selectedHydrationData;
+  let selectedSleepData;
+  let fullUserData;
+  beforeEach(() => {
+  selectedUserInt = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+  selectedUserId = new User(userTestData[selectedUserInt]);
+  selectedHydrationData = hydrationTestData.find(data => data.userID === selectedUserId.id);
+  selectedSleepData = sleepTestData.find(data => data.userID === selectedUserId.id);
+  fullUserData = new UserRepository(selectedUserId, selectedHydrationData, selectedSleepData);
+  });
   it('should be a function', function () {
     expect(UserRepository).to.be.a('function');
   });
+  it('should have an instance that takes the parameters for selectedUser, selectedHydrationData,selectedSleepData', function () {
+    expect(fullUserData).to.be.an.instanceOf(UserRepository);
+  })
+  it('should have properties of userData, hydrationData, sleepData', function () {
+    expect(fullUserData.selectedUserId).to.equal(userTestData[selectedUserInt].id);
+    expect(fullUserData.selectedHydrationData).to.equal(hydrationTestData.find(data => data.userID === selectedUserId.id));
+    expect(fullUserData.selectedSleepData).to.equal(sleepTestData.find(data => data.userID === selectedUserId.id));
+  })
+  it('should be able to return the number of ounces drank for a day', function () {
+    const ouncesDrank = fullUserData.returnNumOunces()
+    expect(ouncesDrank).to.equal(selectedHydrationData.numOunces)
+  })
 });

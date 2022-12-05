7253829158f63e4e081a275a4694b65961dbdaae
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-12-05 16:41:42 -0600

    Create UserRepository Class and tests

---
+++ src/User.js
@@ -0,0 +1,18 @@
+
+class User {
+  constructor(userData) {
+		this.id = 
+		this.name =
+		this.address = 
+		this.email =
+		this.strideLength =
+		this.dailyStepGoal = 
+		this.friends =
+    }
+
+	findFirstName() {
+        // code
+    }
+}
+
+export default User;
--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,5 +1,18 @@
 class UserRepository {
-
+	constructor(userData) {
+		this.data = userData;
+	}
+	findUser(userID) {
+	    return this.data.find((user) => {
+		return  user.id === userID;
+		});
+	}
+	findAvrgStepGoal(userData) {
+        const calculateAvrgGoal = this.data.reduce((accum, data) => {
+            return accum += data.dailyStepGoal;
+        }, 0) / userData.length
+        return parseInt(calculateAvrgGoal.toFixed(0));
+    }
 }
 
 export default UserRepository;

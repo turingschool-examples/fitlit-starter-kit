6c84e0f16f23a0dd9873c72505d7abc5d1c80acd
Ian Nordby <ian.c.nordby@gmail.com>
2022-12-04 17:41:50 -0700

    Add test for id property

--- test/User-test.js
+++ test/User-test.js
@@ -1,20 +1,24 @@
-import { expect } from 'chai';
-import userData from '../src/data/users';
-import User from '../src/User'
+import { expect } from "chai";
+import userData from "../src/data/users";
+import User from "../src/User";
 
-describe('User', function () {
-  let user
-  let user2
+describe("User", function () {
+  let user;
+  let user2;
 
   beforeEach(() => {
-    user = new User(userData[0])
-  })
+    user = new User(userData[0]);
+  });
 
-  it("Should be a function", function() {
-    expect(User).to.be.a('function')
-  })
+  it("Should be a function", function () {
+    expect(User).to.be.a("function");
+  });
 
-  it('Should be an instance of User', function() {
-    expect(user).to.be.an.instanceof(User)
-  })
-})
+  it("Should be an instance of User", function () {
+    expect(user).to.be.an.instanceof(User);
+  });
+
+  it("Should have an id", function () {
+    expect(user.id).to.equal(1);
+  });
+});

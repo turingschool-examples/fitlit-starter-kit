2cac58472559da10d162fe6adcda1bd7b55ff26c
Patt Sookmark <pijiya@sookmark.com>
2022-12-06 15:20:21 -0700

    Add beforeEach setup

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -1,6 +1,12 @@
 import { expect } from "chai";
 import Hydration from "../src/Hydration";
+import hydration from "../data/hydration";
 
 describe('Hydration', function(){
-    
+    let data 
+    beforeEach(()=>{
+        data = hydration
+        userHydrate = new Hydration(data)
+    })
+
 })

dc4193e57fea1be56ae5fc151a2ad333e0cdb6ca
Natalie Kinsaul <n.kinsaul@gmail.com>
2022-12-05 16:39:41 -0800

    Add User class properties and tests

---
+++ test/User-test.js
@@ -0,0 +1,52 @@
+import { expect } from 'chai';
+import User from '../src/User';
+import userData from '../src/data/users';
+
+// const chai = require("chai");
+// const expect = chai.expect;
+// const User = require("../src/User.js");
+// const userData = require("../src/data/users.js")
+
+describe ("User", function() {
+    let user 
+    beforeEach(function() {
+        user = new User(userData[0])
+    })
+
+    it("should be a function", function() {
+        expect(User).to.be.a('function');
+    });
+
+    it('should be an instance of User', function() {
+        expect(user).to.be.an.instanceOf(User);
+    });
+
+    it('should have an id', function() {
+        expect(user.id).to.equal(1);
+    });
+
+    it('should have a name', function() {
+        expect(user.name).to.equal('Luisa Hane');
+    });
+
+    it('should have an address', function() {
+        expect(user.address).to.equal('15195 Nakia Tunnel, Erdmanport VA 19901-1697')
+    });
+
+    it('should have an email', function() {
+        expect(user.email).to.equal('Diana.Hayes1@hotmail.com');
+    });
+
+    it('should have a stride length', function() {
+        expect(user.strideLength).to.equal(4.3);
+    });
+
+    it('should have a daily step goal', function() {
+        expect(user.dailyStepGoal).to.equal(10000);
+    });
+
+    it('should have friends', function() {
+        expect(user.friends).to.deep.equal([16, 4, 8]);
+    })
+
+})

0c7fdd1aedfd4007653ea98c8862e898ede7c86b
Patt Sookmark <pijiya@sookmark.com>
2022-12-06 16:17:29 -0700

    Add calcAvgWaterConsumption method

--- src/Hydration.js
+++ src/Hydration.js
@@ -2,6 +2,11 @@ class Hydration{
     constructor(data){
         this.data = data
 
+
+    }
+    calcAvgWaterConsumption(id){
+        const result = this.data.filter(waterLog => waterLog.userID === id).reduce((total,waterLog,_,arr) => total + waterLog.numOunces  / arr.length  ,0)
+        return Math.round(result)
     }
 }
 

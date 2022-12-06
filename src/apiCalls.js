a6c0bb401af74faac37a0c0f74286e152b534256
jordan smith <jordan.smith6035@gmail.com>
2022-12-06 14:25:17 -0700

    Add fetch user data function to translate promise response

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,8 @@
 // Your fetch requests will live here!
-
+const fetchUserData = (url) => {
+    return fetch(url)
+    .then(response => response.json())
+}
 
 const fetchAll = () => {
     return Promise.all([fetchUserData('https://fitlit-api.herokuapp.com/api/v1/users'), fetchUserData('https://fitlit-api.herokuapp.com/api/v1/sleep'), fetchUserData('https://fitlit-api.herokuapp.com/api/v1/hydration')])

f0a2c0c400bf22fa9d4f4c4483979ac0039e4f3d
Kirsten Smith <brownsmith.kirsten@gmail.com>
2022-12-05 20:37:10 -0500

    Add describe block and it shells

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -2,3 +2,17 @@ const chai = require("chai");
 const expect = chai.expect;
 import sleepTestData from './sleep-test-data';
 import Sleep from '../src/Sleep';
+
+describe('Sleep', function() {
+    it('should be a function', function() {
+
+    });
+
+    it('should create instance out of data from the sleep file', function() {
+
+    });
+
+    it('should have 4 properties: userID, date, hoursSlept, and sleepQuality', function() {
+
+    })
+})

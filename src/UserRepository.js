3a66c9b60dbf1e4e83b620344ec15162a68d2a9a
Faye Rosenshein <rosefaye.a@gmail.com>
2022-12-05 17:03:21 -0700

    feat:Create randomize method in UserRepositroy-test class

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,20 +1,24 @@
 class UserRepository {
-    constructor(oneUserData, allUserData, hydrationData, sleepData) {
-        this.selectedUser = oneUserData
-        this.userData = allUserData
-        this.hydrationData = hydrationData
-        this.sleepData = sleepData
+    constructor(allUserData, hydrationData, sleepData) {
+        this.userData = allUserData;
+        this.hydrationData = hydrationData;
+        this.sleepData = sleepData;
+        this.selectedUser;
     }
-    findUserId() {
-        return this.selectedUser.id
+    findUser(id) {
+        return this.userData.find(user => {
+            return  user.id === id});
+        }
+    randomizeUser() {
+        let selectedUserIndex = Math.floor(Math.random() * (this.userData.length - 0 + 1)) + 0
+        this.selectedUser = this.userData[selectedUserIndex]
+        return selectedUserIndex;
     }
     averageSteps() {
-        let total = 0
-        this.userData.forEach(user =>
-            total += user.dailyStepGoal
-        )
-        let averageSteps = total / this.userData.length
-        return averageSteps
+        let averageStepGoal = this.userData.reduce((acc, user) => {
+            return acc + user.dailyStepGoal
+        }, 0);
+        return Number((averageStepGoal/this.userData.length).toFixed(0))
     }
 }
 
--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,49 +1,48 @@
 import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
-import User from '../src/User';
 import hydrationTestData from './hydration-test-data';
 import sleepTestData from './sleep-test-data';
 import userTestData from './User-test-data'
 
 describe('User Repository', () => {
-  let selectedUserInt;
-  let selectedUser;
-  let userData
-  let hydrationData;
-  let sleepData;
   let fullUserData;
   beforeEach(() => {
-  selectedUserInt = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
-  userData = userTestData
-  selectedUser = new User(userTestData[selectedUserInt]);
-  hydrationData = hydrationTestData;
-  sleepData = sleepTestData;
-  fullUserData = new UserRepository(selectedUser,userData, hydrationData, sleepData);
+  fullUserData = new UserRepository(userTestData, hydrationTestData, sleepTestData);
   });
   it('should be a function', function () {
     expect(UserRepository).to.be.a('function');
   });
-  it('should have an instance that takes the parameters for selectedUser, selectedHydrationData,selectedSleepData', function () {
+  it('should have an instance that takes the parameters for selectedUser,userData, hydrationData,sleepData', function () {
     expect(fullUserData).to.be.an.instanceOf(UserRepository);
   })
-  it('should have the full user-data as a parameter', function (){
+  it('should be an array of all user data', function (){
+    expect(fullUserData.userData).to.have.lengthOf(3);
     expect(fullUserData.userData).to.deep.equal(userTestData);
   })
-  it('should have properties of selectedUser, hydrationData, sleepData', function () {
-    expect(fullUserData.selectedUser).to.deep.equal(userTestData[selectedUserInt]);
-    expect(fullUserData.userData).to.deep.equal(userTestData)
+  it('should have properties of selectedUser, userData, hydrationData, sleepData', function () {
+    expect(fullUserData.userData).to.deep.equal(userTestData);
     expect(fullUserData.hydrationData).to.equal(hydrationTestData);
     expect(fullUserData.sleepData).to.equal(sleepTestData);
   })
   it('should be able to be given an id and return that user\'s data', function () {
-    expect(fullUserData.findUserId()).to.equal(fullUserData.selectedUser.id)
+    expect(fullUserData.findUser(21)).to.deep.equal({
+      "id": 21,
+      "name": "Alexandrea Wehner",
+      "address": "314 Richmond Islands, Brekkefort WI 71702-6994",
+      "email": "Americo_Hammes31@gmail.com",
+      "strideLength": 3,
+      "dailyStepGoal": 9000,
+      "friends": [
+        29,
+        19
+      ]
+    });
+  })
+  it('should randomly find an id and use that id to select a user',function () {
+    const indexNumber = fullUserData.randomizeUser()
+    expect(fullUserData.selectedUser).to.deep.equal(userTestData[indexNumber])
   })
   it('should calculate average dailyStepGoals for all users', function () {
-    let total = 0
-    userTestData.forEach(user => 
-        total += user.dailyStepGoal   
-    )
-    let averageStepGoal = total/userTestData.length
-    expect(fullUserData.averageSteps()).to.equal(averageStepGoal)
+    expect(fullUserData.averageSteps()).to.equal(6333);
   })
 });

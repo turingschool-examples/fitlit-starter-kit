909cbc7d1029ba945b8dd566fcb5b55e9ab4e967
Natalie Kinsaul <n.kinsaul@gmail.com>
2022-12-05 17:22:28 -0800

    Add users array to UserRepository and test

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,5 +1,12 @@
 class UserRepository {
-
+    constructor(users = []) {
+        this.users = users;
+    }
+    avgStepGoal() {
+        
+        //need to find the average step goal between users
+        //need array.length
+    }
 }
 
 export default UserRepository;
--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,8 +1,31 @@
 import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
+import User from '../src/User';
+import userData from '../src/data/users';
 
 describe('User Repository', () => {
+  let userRepo 
+  beforeEach(function () {
+    const user1 = new User(userData[0]);
+    const user2 = new User(userData[1]);
+    const user3 = new User(userData[2]);
+    userRepo = new UserRepository([user1, user2, user3]);
+  });
+
   it('should be a function', function () {
     expect(UserRepository).to.be.a('function');
   });
+
+  it('should be an instance of user repository', function() {
+    expect(userRepo).to.be.an.instanceOf(UserRepository);
+  });
+
+  it('should store users data', function() {
+    expect(userRepo.users).to.deep.equal([userData[0], userData[1], userData[2]]);
+  });
+
+  it('should have an average step goal', function() {
+    expect(userRepo.avgStepGoal()).to.equal(6667);
+  })
+
 });

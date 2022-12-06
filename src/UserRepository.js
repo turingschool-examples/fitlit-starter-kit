791fca16b3736f9494cd9930bc973a80c0209b67
Angie Staffieri <angie.staffieri@gmail.com>
2022-12-05 17:07:25 -0700

    Adds the user repository class and tests

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,5 +1,20 @@
 class UserRepository {
-
+    constructor(users) {
+        this.users = users
+    }
+    getUserData(id) {
+        const userInfo = this.users.find(user => {
+            return id === user.id
+        })
+        return userInfo
+    }
+    getAverageStepGoal() {
+        const steps = this.users.reduce((acc, element) => {
+            acc += element.dailyStepGoal
+            return acc
+        }, 0)
+        return Math.round(steps/this.users.length)
+    }
 }
 
 export default UserRepository;

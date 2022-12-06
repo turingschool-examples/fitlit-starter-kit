dbe229747f660c4db55d99d8a9a1990c37638084
Kirsten Smith <brownsmith.kirsten@gmail.com>
2022-12-05 20:51:18 -0500

    Add it statements and asserts

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -5,14 +5,22 @@ import Sleep from '../src/Sleep';
 
 describe('Sleep', function() {
     it('should be a function', function() {
-
+        expect(Sleep).to.be.a('function');
     });
 
     it('should create instance out of data from the sleep file', function() {
+        const dayOfSleep = new Sleep()
 
+        expect(dayOfSleep).to.be.an.instanceOf(Sleep); 
     });
 
     it('should have 4 properties: userID, date, hoursSlept, and sleepQuality', function() {
+        const dayOfSleep = new Sleep(sleepTestData[0])
 
+
+        expect(dayOfSleep.userId).to.equal()
+        expect(dayOfSleep.date).to.equal()
+        expect(dayOfSleep.hoursSlept).to.equal()
+        expect(dayOfSleep.sleepQuality).to.equal()
     })
 })

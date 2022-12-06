1e48ef4fbf1333f02821ccf7666d5b6d1d432273
Anna Peterson <reveragb@gmail.com>
2022-12-05 17:23:07 -0700

    Write promise all, refactor getUser function

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,16 @@
 // import './images/turing-logo.png'
 //console.log(userData,"<>>>>userData")
 import './css/styles.css';
-// import User fromgit  '../src/User';
+import {getAPIData} from './apiCalls'
+import User from '../src/User';
 // import userData from '../src/data/users'
-// import UserRepository from './UserRepository';
+import UserRepository from './UserRepository';
 
 // Global Variables
 let one = 1
+let users
+let sleep
+let hydration
 
 //Query Selectors
 var infoBox = document.querySelector('.zero')
@@ -16,15 +20,33 @@ var infoBox = document.querySelector('.zero')
 window.addEventListener('load', displayUserInfo)
 
 //Event Handlers
+function getAllData() {
+  Promise.all([getAPIData('users'), getAPIData('sleep'), getAPIData('hydration')])
+    .then((data) => {
+      users = new UserRepository(data[0])
+      sleep = data[1]
+      hydration = data[2]
+
+      console.log('users', users)
+      console.log('sleep', sleep)
+      console.log('hydration', hydration)
+    })
+    .then()
+
+}
+
 function displayUserInfo() {
-  infoBox.innerHTML = "Hello"
+  // infoBox.innerHTML = "Hello"
+  getAllData()
+  getUser()
 }
 
 // Functions
 function getUser() {
-  let randomIndex = Math.floor(Math.random() * userRepository.data.length);
-  let randomUser = userRepository.data[randomIndex];
+  let randomIndex = Math.floor(Math.random() * users.data.length);
+  let randomUser = users.data[randomIndex];
   currentUser = new User(randomUser);
+  console.log('current user', currentUser)
 }
 
 

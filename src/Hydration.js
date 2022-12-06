381227edd10693277757cda945bbeae249ecf4c2
Patt Sookmark <pijiya@sookmark.com>
2022-12-06 16:30:19 -0700

    Add consumeBydate method

--- src/Hydration.js
+++ src/Hydration.js
@@ -8,6 +8,10 @@ class Hydration{
         const result = this.data.filter(waterLog => waterLog.userID === id).reduce((total,waterLog,_,arr) => total + waterLog.numOunces  / arr.length  ,0)
         return Math.round(result)
     }
+
+    consumeBydate(id,date){
+        return this.data.find(waterLog => waterLog.userID === id && waterLog.date === date).numOunces
+    }
 }
 
 export default Hydration

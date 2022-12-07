2af4a8d80a98ad664c90d64cdb97e16ba7e6aca7
Jocelle Bautista <jcllebautista@gmail.com>
2022-12-06 20:15:34 -0500

    Draft randomizer

--- src/scripts.js
+++ src/scripts.js
@@ -1,17 +1,17 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-console.log(userData,"<>>>>userData")
-// An example of how you tell webpack to use a CSS file
 import './css/styles.css';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
-console.log('This is the JavaScript entry file - your code begins here.');
-
-// An example of how you tell webpack to use a JS file
-
 import userData from './data/users';
-
 import UserRepository from './UserRepository';
+
+// console.log(userData,"<>>>>userData")
+let user
+// const makeRandomUser = 
+user = new User(userData[Math.floor(Math.random() * userData.length)])
+const getRandomIndex = (user) => {
+  return Math.floor(Math.random() * user.length)
+}
+console.log(user,"<>>>>randomize")
+
+
+
+

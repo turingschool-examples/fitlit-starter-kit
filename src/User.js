65ac262ff86d792a9edbc8ff5f6d20a4da88cd30
El Brewster <brewsterel3@gmail.com>
2022-12-06 21:14:30 -0800

    Add user and user repository classes

---
+++ src/User.js
@@ -0,0 +1,19 @@
+class User {
+    constructor (userData) {
+        this.id = userData.id;
+        this.name = userData.name;
+        this.address = userData.address;
+        this.email = userData.email;
+        this.strideLength = userData.strideLength;
+        this.dailyStepGoal = userData.dailyStepGoal;
+        this.friends = userData.friends;
+    }
+    
+    getFirstName() {
+        const splitName = this.name.split(' ');
+        return splitName[0];
+    }
+    
+}
+
+module.exports = User;
--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,5 +1,24 @@
 class UserRepository {
+    constructor(users = []) {
+        this.users = users;
+    }
 
+    avgStepGoal() {
+        const stepGoal = this.users.reduce((sum, user) => {
+            return sum += user.dailyStepGoal
+        },0);
+        return Math.round(stepGoal / this.users.length);
+    }
+
+    getUserData(userId) {
+        return this.users.find(user => {
+            return user.id === userId
+        });
+    };
+
+    addNewUser(user) {
+        this.users.push(user);
+    };
 }
 
 export default UserRepository;

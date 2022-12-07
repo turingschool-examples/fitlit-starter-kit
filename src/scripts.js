20aad2e7a77e2e284f8c2c032e0dd8560d8456e1
Jocelle Bautista <jcllebautista@gmail.com>
2022-12-06 23:03:27 -0500

    Create random instance of user

--- src/scripts.js
+++ src/scripts.js
@@ -1,16 +1,46 @@
 import './css/styles.css';
 import './images/turing-logo.png'
 import userData from './data/users';
+import User from './User';
 import UserRepository from './UserRepository';
 
-// console.log(userData,"<>>>>userData")
+const userName = document.querySelector('#user-name')  
 let user
-// const makeRandomUser = 
+// let userRepository
+let currentUser
+
+// let thisData = userData.map(justID => {
+  // return justID.id}) //array of Numbers [userData.id]
+  
 user = new User(userData[Math.floor(Math.random() * userData.length)])
-const getRandomIndex = (user) => {
-  return Math.floor(Math.random() * user.length)
-}
-console.log(user,"<>>>>randomize")
+let testNewUser = new UserRepository(user)
+// const getRandomIndex = Math.floor(Math.random() * 50)
+// if(thisData[justID.id] === getRandomIndex)
+
+  console.log('here', user)
+  window.addEventListener('load', () => {
+    user
+  })
+
+
+function displayName() {
+  userName.innerText = ` ${user.showFirstName()}`
+   }
+
+displayName()
+
+// const makeClasses = (users) => {
+//   userRepository = new UserRepository(users);
+//   if (currentUser === undefined){
+//     getRandomUser();
+//   }}
+// const getRandomUser = () => {
+//   currentUser = new User(userData]);
+//   userRepository = new UserRepository(currentUser)
+// }
+
+//getrandomIndex  compare user Id
+//userRepo has users
 
 
 

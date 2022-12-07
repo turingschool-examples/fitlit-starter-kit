4fb0dce867a5714dbb1bf94e67918f5dd506ecdc
Anna Peterson <reveragb@gmail.com>
2022-12-07 15:08:46 -0700

    Begin writing code for avg daily water

--- src/User.js
+++ src/User.js
@@ -1,10 +1,17 @@
 class User {
-  constructor(userData) {
+  constructor(userData, sleepData, hydrationData) {
     this.userData = userData;
+    this.sleepData = sleepData;
+    this.hydrationData = hydrationData;
   }
 
   getFirstName() {
     return this.userData.name.split(' ')[0];
+  }
+
+// Hydration
+  getAvgDailyWater(userID) {
+    console.log(this.hydrationData.hydrationData)
   }
 }
 

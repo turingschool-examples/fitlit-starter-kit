93af8f835e33e0b882f07fa5c7fea1522f315146
Kirsten Smith <brownsmith.kirsten@gmail.com>
2022-12-07 09:00:17 -0500

    Add find quality and hours for given date test

--- test/User-test.js
+++ test/User-test.js
@@ -94,5 +94,9 @@ it.skip('should find the hours slept for a given date', function() {
 });
 
 it.skip('should find the sleep quality for a given date', function() {
+  selectedUser = new User(userTestData[0]);
+  const sleepData = sleepData.filter(data => data.userID === selectedUser.id);
+  selectedUser.sleepData = sleepData;
 
+  expect(selectedUser.findDaySleep("2019/06/16")).to.equal(1.4);
 })

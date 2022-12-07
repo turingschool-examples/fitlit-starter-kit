2d271f6265bf8841d3c2991285ba0e0aa2caa739
Rae Gebhart <raegebhart@gmail.com>
2022-12-07 15:00:19 -0700

    Refactor scripts file

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,6 @@
 import './css/styles.css';
 import './images/turing-logo.png'
 import { fetchAll } from './apiCalls'
-
 import userData from './data/users';
 import User from './User';
 import UserRepository from './UserRepository';
@@ -10,84 +9,26 @@ let allUserData;
 let allUserSleep;
 let allUserHydro
 
-
-
 const userDisplay = document.querySelector('#userInfo')
 const userNameDisplay = document.querySelector('#userName')
 const userStepGoalAvg = document.querySelector('#stepGoalAvg')
 const hydrationBox = document.querySelector('#hydration')
 
-const users = [{
-  "id": 1,
-  "name": "Luisa Hane",
-  "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
-  "email": "Diana.Hayes1@hotmail.com",
-  "strideLength": 4.3,
-  "dailyStepGoal": 10000,
-  "friends": [
-    16,
-    4,
-    8
-  ]
-},
-{
-  "id": 2,
-  "name": "Jarvis Considine",
-  "address": "30086 Kathryn Port, Ciceroland NE 07273",
-  "email": "Dimitri.Bechtelar11@gmail.com",
-  "strideLength": 4.5,
-  "dailyStepGoal": 5000,
-  "friends": [
-    9,
-    18,
-    24,
-    19
-  ]
-},
-{
-  "id": 3,
-  "name": "Herminia Witting",
-  "address": "85823 Bosco Fork, East Oscarstad MI 85126-5660",
-  "email": "Elwin.Tromp@yahoo.com",
-  "strideLength": 4.4,
-  "dailyStepGoal": 5000,
-  "friends": [
-    19,
-    11,
-    42,
-    33
-  ]
-},]
-
-
-
-
-const userRepo = new UserRepository(users.map(user => new User(user)))
-const user1 = new User(users[0])
-
-
-
-// const getFetch = () => {
 fetchAll()
   .then(data => {
-    // allUserData = data[0].userData;
     allUserData = new UserRepository(data[0].userData.map(user => new User(user)))
     allUserSleep = data[1].sleepData;
     allUserHydro = data[2].hydrationData;
-    pageLoadHandler(allUserData, allUserSleep, allUserHydro)
+    pageLoadHandler()
 })
-// }
 
-
-function pageLoadHandler(dataSet1, dataSet2, dataSet3) {
-  console.log(allUserData.userData[0])
-  displayUserName(allUserData.userData[0])
-  displayUserInfo(allUserData.userData[0])
-  displayComparedStepGoal(allUserData.userData[0], allUserData)
+function pageLoadHandler() {
+  const user = allUserData.userData[0]
+  console.log(user)
+  displayUserName(user)
+  displayUserInfo(user)
+  displayComparedStepGoal(user, allUserData)
 }
-
-
-
 
 const displayUserName = function(user) {
   userNameDisplay.innerText = `Welcome, ${user.getFirstName()}!`
@@ -114,9 +55,3 @@ const displayCurrentDayHydration = function(currentHydration) {
   hydrationBox.innerHTML = `
   <p>${currentHydration}</p>`
 }
-
-
-
-// displayUserInfo(user1)
-// displayUserName(user1)
-// displayComparedStepGoal(user1, userRepo)

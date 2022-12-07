9de291501554590190d0a5215f9b7c59c234c8af
Rick Vermeil <rickv85@gmail.com>
2022-12-07 15:27:58 -0700

    Add fetched data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,32 @@
-// Your fetch requests will live here!
+let userData
+let sleepData
+let hydroData
 
-
-console.log('I will be a fetch request!')
-
+function loadUserData() {
+    const userURL = 'https://fitlit-api.herokuapp.com/api/v1/users'
+    return fetch(userURL)
+        .then((response) => response.json())
+        .then((data) => {
+            userData = data
+            return userData
+        })
+}
+function loadSleepData() {
+    const sleepURL = 'https://fitlit-api.herokuapp.com/api/v1/sleep'
+    return fetch(sleepURL)
+        .then((response) => response.json())
+        .then((data) => {
+            sleepData = data
+            return sleepData
+        })
+}
+function loadHydrationData() {
+    const hydrationURL = 'https://fitlit-api.herokuapp.com/api/v1/hydration'
+    return fetch(hydrationURL)
+        .then((response) => response.json())
+        .then((data) => {
+            hydroData = data
+            return hydroData
+        })
+}
+export default { loadUserData, loadSleepData, loadHydrationData };
--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,10 @@
+
+
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
-console.log(userData,"<>>>>userData")
 // An example of how you tell webpack to use a CSS file
-import './css/styles.css';
+import './html-css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
@@ -11,7 +12,22 @@ import './images/turing-logo.png'
 console.log('This is the JavaScript entry file - your code begins here.');
 
 // An example of how you tell webpack to use a JS file
+const header1 = document.querySelector('h1')
+const userPromise = apiCalls.loadUserData()
+const hydrationPromise = apiCalls.loadHydrationData()
+const sleepPromise = apiCalls.loadSleepData()
 
-import userData from './data/users';
+window.addEventListener('load', function() {
+  Promise.all([userPromise, hydrationPromise, sleepPromise])
+      .then((values) => {
+          {
+              let userRepo = new UserRepository(values[0],values[1],values[2])
+              console.log(userRepo)
+              console.log('script: ',values[0].userData[0].id);
+              header1.innerText = values[0].userData[0].name
+          }
+  })
+});
 
+import apiCalls from './apiCalls';
 import UserRepository from './UserRepository';

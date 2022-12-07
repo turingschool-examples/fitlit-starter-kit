d1a10d47bec61ca83c2213cd79f86632b5a61c7b
Quynh Luu <quynhtlluu@gmail.com>
2022-12-07 14:27:09 -0800

    Add imports and exports for Hydration class

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,2 +1,7 @@
+/*Hydration
+     constructor({userHydrationData})
+         this.userID = userHydrationData.userID
+         this.date = userHydrationData.date
+         this.numOunces = userHydrationData.numOunces */
 
-
+module.exports = Hydration;
---
+++ test/Hydration-test.js
@@ -0,0 +1,4 @@
+import { expect } from 'chai';
+import Hydration from '../src/Hydration';
+import userData from '../src/data/users';
+

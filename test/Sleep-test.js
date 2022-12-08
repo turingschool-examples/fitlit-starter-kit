ac80f9262c9b5d56175dccadea034727d58d7528
jordan smith <jordan.smith6035@gmail.com>
2022-12-08 13:28:36 -0700

    Add sleep data file for sleep testing

---
+++ test/Sleep-test.js
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+import Sleep from "../src/Sleep";
+import sleepRepo from "../src/data/sleep";
+
+
+describe("Sleep", function () {
+    let userSleep;
+    let sleepRepo;
+
+
+    beforeEach(() => {
+        
+    })
+})

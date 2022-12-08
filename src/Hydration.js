ae548a9db0d7061d00da9c83fee03414f406db93
Quynh Luu <quynhtlluu@gmail.com>
2022-12-07 17:38:22 -0800

    Set up temporary Hydration class

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,9 +1,10 @@
 import userHydrationData from "./data/hydration-data"
 
-Hydration
-     constructor({userHydrationData})
+class Hydration {
+     constructor(userHydrationData) {
          this.userID = userHydrationData.userID
          this.date = userHydrationData.date
          this.numOunces = userHydrationData.numOunces
-
+     }
+}
 module.exports = Hydration;

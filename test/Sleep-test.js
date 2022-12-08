8e5545e5949e75e3e5ec94cfd646036d47d89caf
jordan smith <jordan.smith6035@gmail.com>
2022-12-08 14:52:25 -0700

    Add sleep quality for all users test

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -48,4 +48,7 @@ describe("Sleep", function () {
         expect(userSleep.returnSleepQualityByWeek(1, '2019/06/15')).to.deep.equal([ 2.2, 4.7, 4.7, 3, 3.6, 2.9, 2.6 ])
         expect(userSleep.returnSleepQualityByWeek(1, '2019/06/21')).to.deep.equal([ 2.2, 4.7, 4.7, 3, 3.6, 2.9, 2.6 ])
     })
+    it("should return avg sleep quality for all users", function() {
+        expect(userSleep.returnAvgSleepForAllUsers()).to.equal(3.1)
+    })
 })

2b45307e4ab0f8e2d723484c406fdc933ffdfd73
jordan smith <jordan.smith6035@gmail.com>
2022-12-08 13:38:25 -0700

    Add sleep function test

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -4,12 +4,16 @@ import sleepRepo from "../src/data/sleep";
 
 
 describe("Sleep", function () {
-    let userSleep;
     let sleepRepository;
-
-
+    let userSleep;
+    
     beforeEach(() => {
         sleepRepository = sleepRepo
         userSleep = new Sleep(sleepRepository)
     })
+
+    it("it should be a function", function(){
+        expect(Sleep).to.be.a("function")
+    })
+    
 })

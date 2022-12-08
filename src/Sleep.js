4edf703da9a455dbcd2e09430c2e98c356e52799
jordan smith <jordan.smith6035@gmail.com>
2022-12-08 14:12:38 -0700

    Add calc avg sleep quality per day function

--- src/Sleep.js
+++ src/Sleep.js
@@ -7,6 +7,11 @@ class Sleep {
         const result = this.sleepData.filter(sleepLog => sleepLog.userID === id).reduce((total, sleepLog, _, arr ) => total + sleepLog.hoursSlept / arr.length ,0)
         return +result.toFixed(1)
     }
+
+    calcAvgSleepQualityPerDay(id) {
+        const result = this.sleepData.filter(sleepLog => sleepLog.userID === id).reduce((total, sleepLog, _, arr ) => total + sleepLog.sleepQuality / arr.length ,0)
+        return +result.toFixed(1)
+    }
     }
 
 

72a89becf1a3ce75e76726e944a69c2f8ae73596
jordan smith <jordan.smith6035@gmail.com>
2022-12-08 13:47:02 -0700

    Fix sleep data

--- src/data/sleep.js
+++ src/data/sleep.js
@@ -5,44 +5,44 @@ const sleepRepo = [{
     sleepQuality: 2.2
     },
     {
-    userID: 2,
-    date: "2019/06/15",
+    userID: 1,
+    date: "2019/06/16",
     hoursSlept: 7,
     sleepQuality: 4.7
     },
     {
-    userID: 3,
-    date: "2019/06/15",
+    userID: 1,
+    date: "2019/06/17",
     hoursSlept: 10.8,
     sleepQuality: 4.7
     },
     {
-    userID: 4,
-    date: "2019/06/15",
+    userID: 1,
+    date: "2019/06/18",
     hoursSlept: 5.4,
     sleepQuality: 3
     },
     {
-    userID: 5,
-    date: "2019/06/15",
+    userID: 1,
+    date: "2019/06/19",
     hoursSlept: 4.1,
     sleepQuality: 3.6
     },
     {
-    userID: 6,
-    date: "2019/06/15",
+    userID: 1,
+    date: "2019/06/20",
     hoursSlept: 9.6,
     sleepQuality: 2.9
     },
     {
-    userID: 7,
-    date: "2019/06/15",
+    userID: 1,
+    date: "2019/06/21",
     hoursSlept: 5.1,
     sleepQuality: 2.6
     },
     {
-    userID: 8,
-    date: "2019/06/15",
+    userID: 1,
+    date: "2019/06/22",
     hoursSlept: 8.1,
     sleepQuality: 3.5
     },

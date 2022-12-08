0b603ce7fc89a5afdb049a29e78e781447bff80e
jordan smith <jordan.smith6035@gmail.com>
2022-12-08 14:25:00 -0700

    Add hours slept by week

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -27,13 +27,21 @@ describe("Sleep", function () {
     it("should calculate average sleep per day", function() {
         expect(userSleep.calcAvgSleepPerDay(1)).to.equal(7.0)
     })
+
     it("should calculate average sleep quality per day over all time", function() {
         expect(userSleep.calcAvgSleepQualityPerDay(1)).to.equal(3.4)
     })
+
     it("should return how many hours the user slept for specific day", function() {
         expect(userSleep.returnHoursSleptByDate(1, '2019/06/15')).to.equal(6.1)
     })
+
     it("should return sleep quality for the user for specific day", function() {
         expect(userSleep.returnSleepQualityByDate(1, '2019/06/15')).to.equal(2.2)
+    })
+
+    it("should calculate hours slept over a given week", function() {
+        expect(userSleep.returnHoursSleptByWeek(1, '2019/06/15')).to.deep.equal([5.9, 4.6, 9.2, 8.1, 10.7, 7.2, 6.5])
+        expect(userSleep.returnHoursSleptByWeek(1, '2019/06/21')).to.deep.equal([5.9, 4.6, 9.2, 8.1, 10.7, 7.2, 6.5])
     })
 })

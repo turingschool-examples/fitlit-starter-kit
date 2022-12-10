9d473646d09d2818b36a8b8040f56b64fba93712
Ian Nordby <ian.c.nordby@gmail.com>
2022-12-09 21:00:22 -0700

    Add stepGoalChart function to export

--- src/charts.js
+++ src/charts.js
@@ -53,17 +53,36 @@ const createChart = function (hydration, sleepQuality, sleepHours) {
   });
 };
 
-const sleepChart = function(currentSleep, avgSleepQuality, avgHoursSlept) {
-  new Chart(document.getElementById('allTimeSleep'), {
-    type: 'polarArea',
+const sleepChart = function (currentSleep, avgSleepQuality, avgHoursSlept) {
+  new Chart(document.getElementById("allTimeSleep"), {
+    type: "polarArea",
     data: {
-      labels: ['Current Sleep', 'Avg Sleep Quality', 'Avg Hours Slept'],
-      datasets: [{
-        label: 'Sleep Info',
-        data: [currentSleep, avgSleepQuality, avgHoursSlept],
-      }]
-    }
-  })
-}
+      labels: ["Current Sleep", "Avg Sleep Quality", "Avg Hours Slept"],
+      datasets: [
+        {
+          label: "Sleep Info",
+          data: [currentSleep, avgSleepQuality, avgHoursSlept],
+        },
+      ],
+    },
+  });
+};
 
-export { createChart, sleepChart };
+const stepGoalChart = function (stepGoal, avgStepGoal) {
+  new Chart(document.getElementById("stepGoalAvg"), {
+    type: "bar",
+    data: {
+      labels: ["My step Goal", "Average Step Goal"],
+      datasets: [{
+        axis: "y",
+        label: "Step Info",
+        data: [stepGoal, avgStepGoal],
+      }],
+    },
+    options: {
+      indexAxis: "y",
+    },
+  });
+};
+
+export { createChart, sleepChart, stepGoalChart };

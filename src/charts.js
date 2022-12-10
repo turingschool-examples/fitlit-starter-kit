173c4cf8a52531de1b066872e64c7ce2eb2a95c6
Ian Nordby <ian.c.nordby@gmail.com>
2022-12-09 19:50:43 -0700

    Add axis titles to main chart

--- src/charts.js
+++ src/charts.js
@@ -1,48 +1,56 @@
-import { Chart } from 'chart.js/auto'
+import { Chart } from "chart.js/auto";
 
-const createChart = function(hydration, sleepQuality, sleepHours) {
-  new Chart(
-    document.getElementById('weeklyChart'), 
-    {
-      type: 'bar',
-      data: {
-        labels: hydration.dates,
-        datasets: [
-          {
-            label: hydration.label,
-            yAxisID: 'hydration',
-            data: hydration.count
-          }, 
-          {
-            label: sleepQuality.label,
-            yAxisID: 'sleep',
-            data: sleepQuality.count
+const createChart = function (hydration, sleepQuality, sleepHours) {
+  new Chart(document.getElementById("weeklyChart"), {
+    type: "bar",
+    data: {
+      labels: hydration.dates,
+      datasets: [
+        {
+          label: hydration.label,
+          yAxisID: "hydration",
+          data: hydration.count,
+        },
+        {
+          label: sleepQuality.label,
+          yAxisID: "sleep",
+          data: sleepQuality.count,
+        },
+        {
+          label: sleepHours.label,
+          yAxisID: "sleep",
+          data: sleepHours.count,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        hydration: {
+          type: "linear",
+          title: {
+            display: true,
+            text: "Water Intake",
           },
-          {
-            label: sleepHours.label,
-            yAxisID: 'sleep',
-            data: sleepHours.count
-          }
-        ] 
+          position: "left",
+        },
+        sleep: {
+          type: "linear",
+          title: {
+            display: true,
+            text: "Sleep Info",
+          },
+          position: "right",
+          ticks: {
+            max: 15,
+            min: 0,
+          },
+          grid: {
+            drawOnChartArea: false,
+          },
+        },
       },
-      options: {
-        scales: {
-          hydration: {
-            type: 'linear',
-            position: 'left'
-          },
-          sleep: {
-            type: 'linear',
-            position: 'right',
-            ticks: {
-              max: 15,
-              min: 0
-          }
-          },
-        }
-      }
-    }
-  )
-}
+    },
+  });
+};
 
-export { createChart }
+export { createChart };

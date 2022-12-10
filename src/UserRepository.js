63ced200ad3a6646adf27784f04695ab45137775
jordan smith <jordan.smith6035@gmail.com>
2022-12-09 23:42:01 -0700

    Add get friend data method in userRepository to get users friends first names

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -6,6 +6,19 @@ class UserRepository {
     getUserData(id){
         return this.userData.find((user)=> user.id===id)
     }
+
+    getFriendData(friendIDs) {
+        let friendNames = []
+        const friends = friendIDs.filter(e => {
+                this.userData.forEach(t => {
+                if(t.id === e) {
+                    friendNames.push(t.name.split(' ')[0]) 
+                }
+            })
+        })
+        return friendNames 
+    }
+
     calculateAverageStepGoal(){
         return this.userData.reduce((stepAvg, user) => {
             const result = stepAvg + user.dailyStepGoal / this.userData.length

718afe088a5865001e819394197b9c4a1c86f773
Quynh Luu <quynhtlluu@gmail.com>
2022-12-10 10:04:53 -0800

    Remove comments and semi-colons in the Hydration class test file

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -2,8 +2,6 @@ import { expect } from 'chai';
 import Hydration from '../src/Hydration.js';
 import hydrationData from './MockHydroData.js';
 
-// "hydrationData": line 3 is a declaration
-
 describe ("Hydration", function() {
     let hydration;
     let hydrationData;
@@ -90,52 +88,48 @@ describe ("Hydration", function() {
         hydration = new Hydration(3, miniHydrationData);
         hydration.getOneUserData(miniHydrationData2);
     });
-//is it a function?
+    
     it('should be a function', function() {
         expect(Hydration).to.be.a('function');
     });
-//is it deep equal a Class instance?
+
     it('should instantiate our good friend Hydration', function() {
         expect(hydration.dataSet).to.deep.equal(miniHydrationData);
     });
-//test property userID
+
     it('should have a userID', function() {
         expect(hydration.userID).to.equal(miniHydrationData[0].userID);
     });
-//test property dataSet
+
     it('should receive information from a data set', function() {
         expect(hydration.dataSet).to.equal(miniHydrationData)
     });
-//test property oneUserDataSet
+
     it('should have a property to store a data set for one user', function() {
         expect(hydration.oneUserDataSet).to.be.a.property;
-    })
-//test property today
+    });
+
     it('should have a property to store a date for today', function() {
         expect(hydration.today).to.be.a.property;
-    })
-//-------methods------------
-//method0
+    });
+
     it('should have a method to create a hydration data set for a particular user', function() {
         const oneUserDataSet = hydration.getOneUserData(miniHydrationData2);
         expect(oneUserDataSet).to.deep.equal(miniHydrationData);
-    })
-//method1
+    });
+
     it('should have a method to return the average fluid ounces consumed per day by a user for all time', function() {
         const averageHydrationPerDay = hydration.getAvgConsumed();
         expect(averageHydrationPerDay).to.equal(56);
     });
-//method2
+
     it('should have a method to return the ounces water consumed for a particular date passed as an argument', function() {
         const ouncesConsumedByDate = hydration.getOneDayTotal("2019/06/15");
         expect(ouncesConsumedByDate).to.equal(47);
     });
 
-//method3
     it('should have a method to return the user hydration for the most recent 7 days', function() {
         const ouncesConsumedInAWeek = hydration.getOneWeekTotal();
         expect(ouncesConsumedInAWeek).to.be.deep.equal(miniHydrationData);
-    })
+    });
 });
-//revisit: are there different possible outcomes to test for based on different arguments being passed in?
-//I think most of these methods are manipulate user data, return one thing, so mainly test 'does it return the thing' or not

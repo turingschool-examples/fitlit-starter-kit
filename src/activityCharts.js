aceb2c65e9a49ab8047e6028f2f0f55a36b69e3b
Kirsten Smith <brownsmith.kirsten@gmail.com>
2022-12-09 19:13:08 -0500

    Finalize first chart

--- src/activityCharts.js
+++ src/activityCharts.js
@@ -75,4 +75,3 @@ const updateHydroDateChart = () => {
 // }
 
 export default { updateHydroDateChart, todaysHydroChart };
-//export {  };
--- src/scripts.js
+++ src/scripts.js
@@ -4,8 +4,8 @@
 // An example of how you tell webpack to use a CSS file
 import './html-css/styles.css';
 import activityCharts from './activityCharts';
-import updateHydroDateChart from './activityCharts';
-import { todaysHydroChart } from './activityCharts'
+// import updateHydroDateChart from './activityCharts';
+// import { todaysHydroChart } from './activityCharts'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'

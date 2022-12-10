8ca8943ef6d904789aad917914cde3f5d90d9700
Anna Peterson <reveragb@gmail.com>
2022-12-10 13:12:52 -0700

    Add charts file

---
+++ src/charts.js
@@ -0,0 +1,33 @@
+import Chart from 'chart.js/auto';
+
+// Steps
+var xValues = ["Your Steps", "Steps Remaining"];
+var yValues = [55, 45];
+var barColors = [
+  "#b91d47",
+  "#00aba9",
+  "#2b5797",
+  "#e8c3b9",
+  "#1e7145"
+];
+const steps = document.getElementById('stepGoal');
+function makeCharts() {
+  new Chart(steps, {
+    type: "doughnut",
+    data: {
+      labels: ["Your Steps", "Steps Remaining"],
+      datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "World Wide Wine Production 2018"
+      }
+    }
+  });
+}
+
+export default makeCharts;

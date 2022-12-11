b0701de9bd7148cc6d3af0e31210dcb40ffd1800
Keenan Southall <keenan.southall@yahoo.com>
2022-12-11 12:08:14 -0700

    Delete comments on apiCalls.js

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,13 +1,7 @@
-// Your fetch requests will live here!
-// const usersURL =  'https://fitlit-api.herokuapp.com/api/v1/users'
-// const sleepURL = 'https://fitlit-api.herokuapp.com/api/v1/sleep'
-// const hydrationURL = 	'https://fitlit-api.herokuapp.com/api/v1/hydration'
-
 function getAPIData(info) {
   const fetchedInfo = fetch(`https://fitlit-api.herokuapp.com/api/v1/${info}`)
     .then((res) => res.json())
   return fetchedInfo
-} 
+}
 
-
-export {getAPIData}
+export { getAPIData }

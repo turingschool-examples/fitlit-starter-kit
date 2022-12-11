7720008d9fea9b8cb28476021b1a0704be4d686f
Craig Weller <crg.weller@gmail.com>
2022-12-10 18:00:16 -0700

    add basic sleep info

--- src/charts.js
+++ src/charts.js
@@ -86,6 +86,30 @@ function dailySleepQualityChart(yValues) {
     }
   })
 }
+
+var xValues = ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7'];
+
+new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{ 
+      data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
+      borderColor: "red",
+      fill: false
+    }, 
+    { 
+      data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
+      borderColor: "blue",
+      fill: false
+    }]
+  },
+  options: {
+    legend: {display: false}
+  }
+});
+
+
 function loadCharts(stepYValues, dailyH2OYValues, sleepYValues, qualityYValues ) {
   makeStepCharts(stepYValues)
   makeDailyH20Charts(dailyH2OYValues)

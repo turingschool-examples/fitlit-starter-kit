85e65517fddd173a77c7871b921e8905e9d66c6c
Ian Nordby <ian.c.nordby@gmail.com>
2022-12-12 13:32:52 -0700

    Removed unused functions

--- src/scripts.js
+++ src/scripts.js
@@ -5,15 +5,13 @@ import User from "./User";
 import UserRepository from "./UserRepository";
 import Hydration from "./Hydration";
 import Sleep from "./Sleep";
-import { createChart, createSmallBarChart, stepGoalChart } from "./charts";
+import { createChart, createSmallBarChart } from "./charts";
 
 let allUserData;
 let allUserSleep;
 let allUserHydro;
 let currentUser;
 let currentDate;
-let dropDownCalendar;
-
 
 const userDisplay = document.querySelector("#userInfo");
 const userNameDisplay = document.querySelector("#userName");
@@ -23,49 +21,86 @@ const displayWeekly = document.querySelector("#week");
 const currentSleep = document.querySelector("#currentDaySleep");
 const allTimeSleep = document.querySelector("#allTimeSleep");
 const calendarBtn = document.querySelector("#calendarBtn");
-const calendar = document.getElementById('myDate')
+const calendar = document.getElementById("myDate");
 
-calendarBtn.addEventListener("click", function() {
-  createChart(allUserHydro.returnWeeklyWaterConsumption(currentUser.id, calendar.value.replace(/-/g, '/')), allUserSleep.returnSleepQualityByWeek(currentUser.id, calendar.value.replace(/-/g, '/')), allUserSleep.returnHoursSleptByWeek(currentUser.id, calendar.value.replace(/-/g, '/')))
-})
+calendarBtn.addEventListener("click", function () {
+  createChart(
+    allUserHydro.returnWeeklyWaterConsumption(
+      currentUser.id,
+      calendar.value.replace(/-/g, "/")
+    ),
+    allUserSleep.returnSleepQualityByWeek(
+      currentUser.id,
+      calendar.value.replace(/-/g, "/")
+    ),
+    allUserSleep.returnHoursSleptByWeek(
+      currentUser.id,
+      calendar.value.replace(/-/g, "/")
+    )
+  );
+});
 
 fetchAll().then((data) => {
-  console.log(data);
   allUserData = new UserRepository(
     data[0].userData.map((user) => new User(user))
   );
   allUserSleep = new Sleep(data[1].sleepData);
   allUserHydro = new Hydration(data[2].hydrationData);
-  // currentUser = allUserData.userData[0];
-  console.log(Math.floor(Math.random() * allUserData.userData.length))
-  currentUser = allUserData.userData[Math.floor(Math.random() * allUserData.userData.length)];
-  console.log(allUserHydro);
+  currentUser =
+    allUserData.userData[
+      Math.floor(Math.random() * allUserData.userData.length)
+    ];
   currentDate = allUserHydro.data.slice(-1)[0].date;
-  calendar.setAttribute('min', allUserHydro.data.slice(0, 1)[0].date.replace(/\//g, '-'))
-  calendar.setAttribute('max', currentDate.replace(/\//g, '-'))
-  calendar.setAttribute('value', currentDate.replace(/\//g, '-'))
+  calendar.setAttribute(
+    "min",
+    allUserHydro.data.slice(0, 1)[0].date.replace(/\//g, "-")
+  );
+  calendar.setAttribute("max", currentDate.replace(/\//g, "-"));
+  calendar.setAttribute("value", currentDate.replace(/\//g, "-"));
   pageLoadHandler();
 });
-
 
 function pageLoadHandler(event) {
   displayUserName(currentUser);
   displayUserInfo(currentUser, allUserData);
-  // displayComparedStepGoal(currentUser, allUserData);
   displayCurrentDayHydration(allUserHydro, currentDate);
-  // displayWeeklyInfo(allUserHydro, allUserSleep, currentDate);
-  // displayCurrentDaySleep();
-  // displayAllTimeSleep();
-  console.log(currentUser)
-  createChart(allUserHydro.returnWeeklyWaterConsumption(currentUser.id, currentDate), allUserSleep.returnSleepQualityByWeek(currentUser.id, currentDate), allUserSleep.returnHoursSleptByWeek(currentUser.id, currentDate))
-  createSmallBarChart("allTimeSleep", ["Average Quality", "Average Slept"],"Avg Sleep Quality",[allUserSleep.calcAvgSleepQualityPerDay(currentUser.id), allUserSleep.calcAvgSleepPerDay(currentUser.id)], ["rgba(255, 243, 199, .2)", "rgba(186, 239, 195, .2)"], ["rgb(255, 243, 199)", "rgb(186, 239, 195)"])
-  createSmallBarChart("dailySleep", ["Daily Quality", "Daily Slept"], "Daily Sleep",[allUserSleep.returnSleepQualityByDate(currentUser.id, currentDate), allUserSleep.returnHoursSleptByDate(currentUser.id, currentDate)], ["rgba(255, 243, 199, .2)", "rgba(186, 239, 195, .2)"], ["rgb(255, 243, 199)", "rgb(186, 239, 195)"])
-  createSmallBarChart("stepGoalAvg", ["My Step Goal", "Average Step Goal"], 'Steps',[currentUser.dailyStepGoal, allUserData.calculateAverageStepGoal()], ["rgba(253, 221, 224, .2)"],["rgb(253, 221, 224)"], 'y')
-  // dropDownCalendar = document.getElementById('myDate').value
-  // console.log(dropDownCalendar)
+  createChart(
+    allUserHydro.returnWeeklyWaterConsumption(currentUser.id, currentDate),
+    allUserSleep.returnSleepQualityByWeek(currentUser.id, currentDate),
+    allUserSleep.returnHoursSleptByWeek(currentUser.id, currentDate)
+  );
+  createSmallBarChart(
+    "allTimeSleep",
+    ["Average Quality", "Average Slept"],
+    "Avg Sleep Quality",
+    [
+      allUserSleep.calcAvgSleepQualityPerDay(currentUser.id),
+      allUserSleep.calcAvgSleepPerDay(currentUser.id),
+    ],
+    ["rgba(255, 243, 199, .2)", "rgba(186, 239, 195, .2)"],
+    ["rgb(255, 243, 199)", "rgb(186, 239, 195)"]
+  );
+  createSmallBarChart(
+    "dailySleep",
+    ["Daily Quality", "Daily Slept"],
+    "Daily Sleep",
+    [
+      allUserSleep.returnSleepQualityByDate(currentUser.id, currentDate),
+      allUserSleep.returnHoursSleptByDate(currentUser.id, currentDate),
+    ],
+    ["rgba(255, 243, 199, .2)", "rgba(186, 239, 195, .2)"],
+    ["rgb(255, 243, 199)", "rgb(186, 239, 195)"]
+  );
+  createSmallBarChart(
+    "stepGoalAvg",
+    ["My Step Goal", "Average Step Goal"],
+    "Steps",
+    [currentUser.dailyStepGoal, allUserData.calculateAverageStepGoal()],
+    ["rgba(253, 221, 224, .2)"],
+    ["rgb(253, 221, 224)"],
+    "y"
+  );
 }
-
-
 
 const displayUserName = function (user) {
   userNameDisplay.innerText = `Welcome, ${user.getFirstName()}!`;
@@ -84,45 +119,8 @@ const displayUserInfo = function (user, repository) {
   </div>`;
 };
 
-const displayComparedStepGoal = function (user, repository) {
-  userStepGoalAvg.innerHTML = `<p>Daily step goal: ${
-    user.dailyStepGoal
-  } Step goal average:${repository.calculateAverageStepGoal()}</p>`;
-};
-
 const displayCurrentDayHydration = function (hydration, date) {
   hydrationBox.innerHTML = `
   <i class="fa-regular fa-glass fa-2xl"></i>
   <p>Today's Water: ${hydration.consumeBydate(currentUser.id, date)} oz</p>`;
 };
-
-const displayWeeklyInfo = function (hydration, sleep, date) {
-  displayWeekly.innerHTML = `
-  <p>Water: ${hydration.returnWeeklyWaterConsumption(currentUser.id, date)}</p>
-  <p>Sleep Hours: ${sleep.returnHoursSleptByWeek(currentUser.id, date)} </p>
-  <p>Sleep Quality: ${sleep.returnSleepQualityByWeek(currentUser.id, date)} </p>
-  `;
-};
-
-const displayCurrentDaySleep = function () {
-  currentSleep.innerHTML = `<p>Current Sleep: ${allUserSleep.returnHoursSleptByDate(
-    currentUser.id,
-    currentDate
-  )}</p>`;
-};
-
-const displayAllTimeSleep = function () {
-  allTimeSleep.innerHTML = `
-  <p>Sleep Quality avg all time: ${allUserSleep.calcAvgSleepQualityPerDay(
-    currentUser.id
-  )}</p>
-  <p>Sleep hour avg all time: ${allUserSleep.calcAvgSleepPerDay(
-    currentUser.id
-  )}</p>
-  `;
-};
-
-
-
-
-

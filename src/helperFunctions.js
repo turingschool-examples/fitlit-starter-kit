53d83f5da13d1fa87a0dde8d7f6166abdc51b8b8
Rae Gebhart <raegebhart@gmail.com>
2023-01-04 15:28:40 -0700

    Add a weeklyData method

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -6,4 +6,28 @@ const userDataForDate = function (data, id, date) {
   return data.find((el) => el.userID === id && el.date === date);
 };
 
-export { checkID, userDataForDate };
+const weeklyData = function (data, query, label, id, date) {
+  const userInfo = data.filter((el) => el.userID === id);
+  const queryData = userInfo.map((el) => el[query]);
+  const dates = userInfo.map((el) => el.date);
+  const index = userInfo.findIndex(
+    (el) => el.date === date && el.userID === id
+  );
+  if (index === -1) {
+    return "No data found for date selected";
+  }
+  if (!queryData[index + 6]) {
+    return {
+      count: queryData.slice(-7),
+      label: label,
+      dates: dates.slice(-7),
+    };
+  }
+  return {
+    count: queryData.slice(index, index + 7),
+    label: label,
+    dates: dates.slice(index, index + 7),
+  };
+};
+
+export { checkID, userDataForDate, weeklyData };

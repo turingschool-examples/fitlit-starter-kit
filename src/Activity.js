1622df5deffd5b04b77b614e90d5e6a3677154a4
Rae Gebhart <raegebhart@gmail.com>
2023-01-03 17:49:56 -0700

    Pass calculateMilesForDate test

--- src/Activity.js
+++ src/Activity.js
@@ -2,6 +2,11 @@ class Activity {
   constructor(data) {
     this.data = data;
   }
+  calculateMilesForDate(id, date, strideLength) {
+    const userActivity = this.data.find(el => el.userID === id && el.date === date)
+    console.log(userActivity)
+    return (userActivity.numSteps * strideLength / 5280).toFixed(2) * 1
+  }
 }
 
 export default Activity;

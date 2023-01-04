16d933f170c5d3ff5f486a02f0cd4f9e536122c8
Rae Gebhart <raegebhart@gmail.com>
2023-01-04 14:29:12 -0700

    Write test for calculateMilesForDate if id is not found

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -26,6 +26,7 @@ describe("Activity", () => {
   it("Should return error message if no data found", function () {
     const userRepo = new UserRepository(userData)
     expect(userActivity.calculateMilesForDate(1, "2018/06/15", userRepo.getUserData(1).strideLength)).to.equal('No data found for inputs')
+    expect(userActivity.calculateMilesForDate(99, "2019/06/15", userRepo.getUserData(1).strideLength)).to.equal('No data found for inputs')
   })
   it("Should return minutes active by day", function () {
     expect(userActivity.findMintuesActiveForDate(1, "2019/06/15")).to.equal(140)

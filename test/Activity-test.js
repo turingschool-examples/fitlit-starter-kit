e5a2957694656ca4130ab0a54a7b08b1707953ca
Rae Gebhart <raegebhart@gmail.com>
2023-01-04 15:55:07 -0700

    Add averageMinutesActiveForWeek test for if no data is found for date

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -38,4 +38,7 @@ describe("Activity", () => {
   it("Should return minutes active for a given week", function () {
     expect(userActivity.averageMinutesActiveForWeek(1, "2019/06/15")).to.equal(159)
   })
+  it('Should return error message if no data is found for average minutes active for week', function () {
+    expect(userActivity.averageMinutesActiveForWeek(1, "2018/06/15")).to.equal("No data found for date selected")
+  })
 });

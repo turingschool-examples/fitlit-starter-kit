33532bd9b28ea443af2fbefcf868da9b97f00674
Rae Gebhart <raegebhart@gmail.com>
2023-01-04 14:12:49 -0700

    Pass findMintuesActiveForDate test

--- src/Activity.js
+++ src/Activity.js
@@ -2,10 +2,16 @@ class Activity {
   constructor(data) {
     this.data = data;
   }
+
   calculateMilesForDate(id, date, strideLength) {
     const userActivity = this.data.find(el => el.userID === id && el.date === date)
     console.log(userActivity)
     return (userActivity.numSteps * strideLength / 5280).toFixed(2) * 1
+  }
+
+  findMintuesActiveForDate(id, date) {
+    const userActive = this.data.find(el => el.userID === id && el.date === date).minutesActive
+    return userActive
   }
 }
 

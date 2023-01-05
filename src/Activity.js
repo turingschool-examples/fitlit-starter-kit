902f13ca285b94ae7b9fda115fc9e87385f709b1
Ian Nordby <ian.c.nordby@gmail.com>
2023-01-05 09:39:26 -0700

    Add findHighestStairsClimbed method

--- src/Activity.js
+++ src/Activity.js
@@ -1,54 +1,73 @@
-import { userDataForDate, weeklyData } from './helperFunctions'
+import { userDataForDate, weeklyData } from "./helperFunctions";
 class Activity {
   constructor(data) {
     this.data = data;
   }
 
   calculateMilesForDate(id, date, strideLength) {
-    const userActivity = userDataForDate(this.data, id, date)
+    const userActivity = userDataForDate(this.data, id, date);
     if (!userActivity) {
-      return 'No data found for inputs'
+      return "No data found for inputs";
     }
-    return (userActivity.numSteps * strideLength / 5280).toFixed(2) * 1
+    return ((userActivity.numSteps * strideLength) / 5280).toFixed(2) * 1;
   }
 
   findMintuesActiveForDate(id, date) {
-    const userActive = userDataForDate(this.data, id, date)
+    const userActive = userDataForDate(this.data, id, date);
     if (!userActive) {
-      return 'No data found for inputs'
+      return "No data found for inputs";
     }
-    return userActive.minutesActive
+    return userActive.minutesActive;
   }
 
   averageMinutesActiveForWeek(id, date) {
-    const minutesActiveObject = weeklyData(this.data, 'minutesActive', 'Minutes Active', id, date)
-    if (typeof minutesActiveObject !== 'string') {
-      return minutesActiveObject.count.reduce((acc, curr, _, arr) => acc + curr / arr.length, 0).toFixed(0) * 1
+    const minutesActiveObject = weeklyData(
+      this.data,
+      "minutesActive",
+      "Minutes Active",
+      id,
+      date
+    );
+    if (typeof minutesActiveObject !== "string") {
+      return (
+        minutesActiveObject.count
+          .reduce((acc, curr, _, arr) => acc + curr / arr.length, 0)
+          .toFixed(0) * 1
+      );
     } else {
-      return minutesActiveObject
+      return minutesActiveObject;
     }
   }
 
   dailyStepGoaAchieved(id, date, stepGoal) {
-    const userData = userDataForDate(this.data, id, date)
-    if(!userData) {
-      return 'No data found for inputs'
+    const userData = userDataForDate(this.data, id, date);
+    if (!userData) {
+      return "No data found for inputs";
     }
-    return userData.numSteps > stepGoal
+    return userData.numSteps > stepGoal;
   }
 
   findDatesOverStepGoal(id, stepGoal) {
-    const userData = this.data.filter(user => user.userID === id)
-    if(userData.length === 0) {
-      return 'No User Found'
+    const userData = this.data.filter((user) => user.userID === id);
+    if (userData.length === 0) {
+      return "No User Found";
     }
-    const datesAchivedGoal = userData.filter(el => el.numSteps > stepGoal)
-    if(datesAchivedGoal.length === 0) {
-      return 'No Step Goals Met'
+    const datesAchivedGoal = userData.filter((el) => el.numSteps > stepGoal);
+    if (datesAchivedGoal.length === 0) {
+      return "No Step Goals Met";
     }
-    return datesAchivedGoal.map(el => el.date)
+    return datesAchivedGoal.map((el) => el.date);
+  }
+
+  findHighestStairsClimbed(id) {
+    return this.data.reduce((highest, data) => {
+      if (data.userID === id && data.flightsOfStairs > highest) {
+        highest = data.flightsOfStairs
+        return highest
+      }
+      return highest
+    }, 0);
   }
 }
-
 
 export default Activity;

5b6f4cf5a4302ebf4bf14936249a340a1f07e5ac
jordan smith <jordan.smith6035@gmail.com>
2023-01-07 14:24:32 -0700

    Fix weekly step count test

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -161,6 +161,6 @@ describe("Activity", () => {
   });
 
   it('Should calculate weekly step count', function () {
-    expect(userActivity.weeklyStepCountByDay(1, "2019/06/15")).to.deep.equal([3577, 4294, 7402, 3486, 11374, 14810, 2634])
+    expect(userActivity.weeklyStepCountByDay(1, "2019/06/15")).to.deep.equal({count:[3577, 4294, 7402, 3486, 11374, 14810, 2634], label:'Steps', dates:["2019/06/15", "2019/06/16", "2019/06/17", "2019/06/18", "2019/06/19", "2019/06/20", "2019/06/21"]})
   })
 });

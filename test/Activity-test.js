e83faa43bc2ec91d465c6e305eedf189a018f459
jordan smith <jordan.smith6035@gmail.com>
2023-01-07 14:40:57 -0700

    Add testing for weeklyminutesactivebyday

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -164,7 +164,7 @@ describe("Activity", () => {
     expect(userActivity.weeklyStepCountByDay(1, "2019/06/15")).to.deep.equal({count:[3577, 4294, 7402, 3486, 11374, 14810, 2634], label:'Steps', dates:["2019/06/15", "2019/06/16", "2019/06/17", "2019/06/18", "2019/06/19", "2019/06/20", "2019/06/21"]})
   })
 
-  it('Should return error message if no data exists', function () {
-    expect(userActivity.weeklyStepCountByDay(1, "2018/06/15")).to.deep.equal('No data exists for input')
+  it('Should return minutes active weekly data', function () {
+    expect(userActivity.weeklyMinutesActiveByDay(1, "2019/06/15")).to.deep.equal({count:[140, 138, 116, 114, 213, 287, 107], label:'Minutes Active', dates:["2019/06/15", "2019/06/16", "2019/06/17", "2019/06/18", "2019/06/19", "2019/06/20", "2019/06/21"]})
   })
 });

15600edc068256ddc7d393cd3198ecea39431a8a
Ian Nordby <ian.c.nordby@gmail.com>
2023-01-06 20:59:23 -0700

    Comment out new weeklyData to test other functions

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -9,60 +9,36 @@ const userDataForDate = function (data, id, date) {
   return data.find((el) => el.userID === id && el.date === date);
 };
 
-const weeklyData = function (data, query, label, id, date, rerun) {
-  const userInfo = data.filter((el) => el.userID === id);
-  const queryData = userInfo.map((el) => el[query]);
-  const dates = userInfo.map((el) => el.date);
-  const index = userInfo.findIndex(
-    (el) => el.date === date && el.userID === id
-  );
-  console.log(date)
-  const newDate = dayjs(date).add(1, 'day').format("YYYY/MM/DD")
-  console.log('newDate', newDate)
-  const currentCount = rerun.count || []
-  const currentDates = rerun.dates || []
-  const dataFiller = {
-    count: [...currentCount, null],
-    label: label,
-    dates: [... currentDates, date]
-  }
-  if (dataFiller.count.length === 7) {
-        return dataFiller
-  }
-    //   weeklyData(data, query, label, id, newDate, dataFiller)
-
-  // if (index === -1) {
-  //   //
-  //   // const a = dayjs(date)
-  //   // const b = a.add(7, 'day')
-    
-  //   console.log('newDate', newDate)
-  //   weeklyData(data, query, label, id, date, {})
-  // }
-  if (!queryData[index + 6]) {
-    return {
-      count: queryData.slice(-7),
-      label: label,
-      dates: dates.slice(-7),
-    };
-  }
-  return {
-    count: queryData.slice(index, index + 7),
-    label: label,
-    dates: dates.slice(index, index + 7),
-  };
-};
-
-// const weeklyData = function (data, query, label, id, date) {
+// const weeklyData = function (data, query, label, id, date, rerun) {
 //   const userInfo = data.filter((el) => el.userID === id);
 //   const queryData = userInfo.map((el) => el[query]);
 //   const dates = userInfo.map((el) => el.date);
 //   const index = userInfo.findIndex(
 //     (el) => el.date === date && el.userID === id
 //   );
-//   if (index === -1) {
-//     return "No data found for date selected";
+//   console.log(date)
+//   const newDate = dayjs(date).add(1, 'day').format("YYYY/MM/DD")
+//   console.log('newDate', newDate)
+//   const currentCount = rerun.count || []
+//   const currentDates = rerun.dates || []
+//   const dataFiller = {
+//     count: [...currentCount, null],
+//     label: label,
+//     dates: [... currentDates, date]
 //   }
+//   if (dataFiller.count.length === 7) {
+//         return dataFiller
+//   }
+//     //   weeklyData(data, query, label, id, newDate, dataFiller)
+
+//   // if (index === -1) {
+//   //   //
+//   //   // const a = dayjs(date)
+//   //   // const b = a.add(7, 'day')
+    
+//   //   console.log('newDate', newDate)
+//   //   weeklyData(data, query, label, id, date, {})
+//   // }
 //   if (!queryData[index + 6]) {
 //     return {
 //       count: queryData.slice(-7),
@@ -77,6 +53,30 @@ const weeklyData = function (data, query, label, id, date, rerun) {
 //   };
 // };
 
+const weeklyData = function (data, query, label, id, date) {
+  const userInfo = data.filter((el) => el.userID === id);
+  const queryData = userInfo.map((el) => el[query]);
+  const dates = userInfo.map((el) => el.date);
+  const index = userInfo.findIndex(
+    (el) => el.date === date && el.userID === id
+  );
+  if (index === -1) {
+    return "No data found for date selected";
+  }
+  if (!queryData[index + 6]) {
+    return {
+      count: queryData.slice(-7),
+      label: label,
+      dates: dates.slice(-7),
+    };
+  }
+  return {
+    count: queryData.slice(index, index + 7),
+    label: label,
+    dates: dates.slice(index, index + 7),
+  };
+};
+
 const testSequentialDates = (arr) => {
   return arr.every((curr, index, ar) => {
     if(index === ar.length-1) {
@@ -89,4 +89,8 @@ const testSequentialDates = (arr) => {
   })
 }
 
-export { checkID, userDataForDate, weeklyData, testSequentialDates };
+const userDataForID = (id, data) => {
+  return data.filter(el => el.userID === id)
+}
+
+export { checkID, userDataForDate, weeklyData, testSequentialDates, userDataForID };

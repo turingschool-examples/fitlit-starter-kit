b7253982212d16c675aa8ce474166db5442e2b57
Rae Gebhart <raegebhart@gmail.com>
2023-01-08 09:51:45 -0700

    Add function for post requests

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
-const fetchUserData = (url) => {
-  return fetch(url).then((response) => response.json());
+const fetchUserData = (url, object) => {
+  return fetch(url, object).then((response) => response.json());
 };
 
 const fetchAll = () => {
@@ -7,8 +7,33 @@ const fetchAll = () => {
     fetchUserData("http://localhost:3001/api/v1/users"),
     fetchUserData("http://localhost:3001/api/v1/sleep"),
     fetchUserData("http://localhost:3001/api/v1/hydration"),
-    fetchUserData("http://localhost:3001/api/v1/activity")
+    fetchUserData("http://localhost:3001/api/v1/activity"),
   ]);
 };
 
-export { fetchAll };
+const testPost = async function (sleep, hydration, activity) {
+  const postObject = {
+    method: "POST",
+    body: {},
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const promises = await Promise.all([
+    fetchUserData("http://localhost:3001/api/v1/sleep", {
+      ...postObject,
+      body: JSON.stringify(sleep),
+    }),
+    fetchUserData("http://localhost:3001/api/v1/hydration", {
+      ...postObject,
+      body: JSON.stringify(hydration),
+    }),
+    fetchUserData("http://localhost:3001/api/v1/activity", {
+      ...postObject,
+      body: JSON.stringify(activity),
+    }),
+  ]);
+  return promises;
+};
+
+export { fetchAll, testPost };

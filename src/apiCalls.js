59d0d53189af556c858311a7aa1a1f096469d33d
Rae Gebhart <raegebhart@gmail.com>
2023-01-08 17:11:21 -0700

    Fixed json not a function error

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,10 +1,10 @@
 const fetchUserData = (url, object) => {
   return fetch(url, object).then((response) => {
-    if(!response.ok) {
-      throw new Error('bad happened')
+    if (!response.ok) {
+      throw new Error("Unable To Post Your Data. Try Later.");
     }
-    return response.json()
-  })
+    return response.json();
+  });
 };
 
 const fetchAll = () => {
@@ -16,16 +16,14 @@ const fetchAll = () => {
   ]);
 };
 
-// const fetchAll = () => {
-//   return Promise.all([
-//     fetchUserData("http://localhost:3001/api/v1/users"),
-//     fetchUserData("http://localhost:3001/api/v1/sleep"),
-//     fetchUserData("http://localhost:3001/api/v1/hydration"),
-//     fetchUserData("http://localhost:3001/api/v1/activity"),
-//   ]);
-// };
-
-const postData = function (sleep, hydration, activity, updateData, rerenderPage, user) {
+const postData = function (
+  sleep,
+  hydration,
+  activity,
+  updateData,
+  rerenderPage,
+  user
+) {
   const postObject = {
     method: "POST",
     body: {},
@@ -46,21 +44,17 @@ const postData = function (sleep, hydration, activity, updateData, rerenderPage,
       ...postObject,
       body: JSON.stringify(activity),
     }),
-  ])
-  .then(response => {
-    // console.log(updateData, rerenderPage)
-    // console.log(response.ok)
+  ]).then((response) => {
     if (response.status >= 300) {
-      console.log('throwing error')
-      throw new Error(`${response.status}: ${response.statusText}`)
+      console.log("throwing error");
+      throw new Error(`${response.status}: ${response.statusText}`);
     }
-    fetchAll().then(data => {
-      updateData(data, user)
-      rerenderPage()
-    })
-    // fetch data and rerender page without reloading
-    return response.json()
-  })
+    fetchAll().then((data) => {
+      updateData(data, user);
+      rerenderPage();
+    });
+    return response;
+  });
 };
 
 export { fetchAll, postData };

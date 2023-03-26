3750127ee1b79ac72f68b84e95b8f69b8302f933
Dustin Gouner <dustingouner20@gmail.com>
2023-03-26 14:43:42 -0600

    feat: build method to return user data by id

--- src/User.js
+++ src/User.js
@@ -1,7 +1,25 @@
+import mock from '../src/data/mock';
+
 class User {
-    constructor() {
-        
+    constructor(userObj) {
+        this.id =  userObj.id
+        this.name = userObj.name
+        this.address = userObj.address
+        this.email = userObj.email
+        this.strideLength = userObj.strideLength
+        this.dailyStepGoal = userObj.dailyStepGoal
+        this.friends = userObj.friends
     }
+    getUserData(num) {
+        const user = mock.users.reduce((acc, index) => {
+        if (index.id === num) {
+            return index
+        }
+            return acc
+        }, {})
+        return user
+    }
+   
 }
 
 export default User;

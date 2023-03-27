f4072cd06191dff92f8eddfe261c0452ccc1bb73
Kelli Watkins <watkinskellil@gmail.com>
2023-03-27 11:06:37 -0600

    Add hydration and testing

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,23 +1,77 @@
 import User from './User.js'
-import hydrationData from 'src/data/hydration.js'
+import hydrationData from './data/hydration'
 
 class Hydration {
     constructor(entry) {
-        this.userID = entry.userID;
-        this.date = entry.date;
-        this.numOunces = entry.ounces
+        // this.userID = entry.userID;
+        // this.date = entry.date;
+        // this.numOunces = entry.numOunces
+        this.hydrationData = entry
     }
 
-    calculateAverageFluidDay(user, date) {
+    findUserID(id) {
+        console.log(this.hydrationData)
+        let currentUser = this.hydrationData.filter(data => data.userID === id)
+        return currentUser
+    }
+
+    calculateAverageFluidDay(id, date) {
         const userHydration = hydrationData.filter((data) => {
-            if (user.ID === data.userID && date === data.date) {
+            if (id.user.ID === data.userID && date === data.date) {
                 return numOunces
             }
             return userHydration
         })
+
+        calculateAverageFluidWeek(user)
 
 
     }
 }
 
 export default Hydration;
+
+// getAvgFluidOunces(id) {
+//     const userDrinks = this.hydrationData.filter(data => data.userID === id);
+//     const totalOunces = userDrinks.reduce((acc, data) => {
+//         acc += data.numOunces;
+//         return acc;
+//     }, 0);
+//     return Math.floor(totalOunces / userDrinks.length);
+// }
+//   getAvgFluidOunces(id) {
+//     const userDrinks = this.hydrationData.filter(data => data.userID === id);
+//     const totalOunces = userDrinks.reduce((acc, data) => {
+//       acc += data.numOunces;
+//       return acc; 
+//     }, 0);
+//     return Math.floor(totalOunces / userDrinks.length);
+//   }
+
+class Hydration {
+    constructor(data) {
+        this.data = data;
+    }
+
+    getDailyWaterIntake(date) {
+        return this.data.find(user => user.date === date).numOunces;
+    }
+
+    const hydrationData = [
+        {
+            userID: 1,
+            date: '2019/06/15',
+            numOunces: 37
+        },
+        {
+            userID: 2,
+            date: '2019/06/15',
+            numOunces: 75
+        },
+        {
+            userID: 3,
+            date: '2019/06/15',
+            numOunces: 47
+        }
+      }
+module.exports = hydraationData
--- test/Hydration-Test.js
+++ test/Hydration-Test.js
@@ -1,20 +1,26 @@
 import { expect } from 'chai';
 import Hydration from '../src/Hydration';
-import hydrationData from 'src/data/hydration.js'
+import hydrationData from '../src/data/hydration'
+
 
 describe('Hydration', () => {
-    let hydration;
+    let hydration, currentUser;
 
     beforeEach(() => {
         hydration = new Hydration(hydrationData[0])
+        currentUser = hydration.findUserID(1)
     });
 
+    console.log(currentUser)
     it('Should be a function', () => {
         expect(Hydration).to.be.a('function');
     });
 
-    it('Should be able to take in data', () => {
-        expect(hydration.userID).to.equal(1);
-    });
+    // it('Should be able to take in data', () => {
+    //     expect(hydration.userID).to.equal(1);
+    //     expect(hydration.date).to.equal("2023/03/24");
+    //     expect(hydration.numOunces).to.equal(28)
+    // });
 
 })
+

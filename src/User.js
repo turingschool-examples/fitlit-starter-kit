fb1296deb2c55d060c865f30c10c463e88ce9d3a
Houda Jawad <houdajawad@gmail.com>
2023-03-27 15:56:52 -0700

    Add the user class

---
+++ src/User.js
@@ -0,0 +1,11 @@
+class User {
+    constructor(user){
+        this.id = user.id;
+        this.address = user.address;
+        this.name = user.name;
+        this.email = user.email;
+        this.strideLength = user.strideLength;
+        this.dailyStepGoal = user.dailyStepGoal;
+        this.friends = user.friends;
+    }
+}

c427a3902bb9b82ec4172e04f07ff1153282cdf8
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-03-27 10:34:45 -0400

    Update name for test data files and add test data to the files

---
+++ src/data/hydration-test-data.js
@@ -0,0 +1,41 @@
+export default { hydrationTestData: [{
+    "userID": 1,
+    "date": "2023/03/24",
+    "numOunces": 28
+  },
+  {
+    "userID": 2,
+    "date": "2023/03/24",
+    "numOunces": 35
+  },
+  {
+    "userID": 3,
+    "date": "2023/03/24",
+    "numOunces": 95
+  },
+  {
+    "userID": 4,
+    "date": "2023/03/24",
+    "numOunces": 74
+  },
+  {
+    "userID": 5,
+    "date": "2023/03/24",
+    "numOunces": 47
+  },
+  {
+    "userID": 6,
+    "date": "2023/03/24",
+    "numOunces": 86
+  },
+  {
+    "userID": 7,
+    "date": "2023/03/24",
+    "numOunces": 74
+  },
+  {
+    "userID": 8,
+    "date": "2023/03/24",
+    "numOunces": 36
+  }
+  ]};

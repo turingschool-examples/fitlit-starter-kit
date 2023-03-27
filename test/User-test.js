4db9837c52c8b7c3e93b622479d66a86f486585c
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-03-27 10:11:17 -0400

    Update file link in User-test

--- test/User-test.js
+++ test/User-test.js
@@ -1,8 +1,8 @@
 import { expect } from 'chai';
-import UserRepository from '../src/UserRepository';
+import User from '../src/User';
 
 describe('User Repository', () => {
   it('should be a function', function () {
-    expect(UserRepository).to.be.a('function');
+    expect(User).to.be.a('function');
   });
 });

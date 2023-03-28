35dd6d5073d4c52d0f3916dbbf135cb1dc42554f
Isaiah Richardson <capcinematic@gmail.com>
2023-03-28 17:37:06 -0600

    check for avg/specific oz

--- test/user-hydration-test.js
+++ test/user-hydration-test.js
@@ -8,4 +8,20 @@ const userHydration = require("../src/userHydration")
 
 describe('userHydration', () => {
   let userHydration;
+
+  beforeEach(() => {
+    userHydration = new userHydration(User.userId, hydrationData)
+  });
+
+  it("Should calculate the all time average daily ounces", () => {
+    const average = userHydration.calculateDailyOunces();
+    expect(average).to.equal('avgNumOz');
+  });
+
+  it("Should calculate how many ounces are consumed for a specific day ", () => {
+    const specificDayOz = userHydration.calculateSingleDayOunces(hydrationData[i]['date']);
+    expect(specificDayOz).to.equal('specificOz');
+  });
+  it("Should calculate the average daily ounces", () => {});
+  it("Should calculate the average daily ounces", () => {});
 })

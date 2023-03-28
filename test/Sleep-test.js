e0e91eddb0f1794dae5c54a60356d3b699ffc612
Adam Meza <Adammeza95@gmail.com>
2023-03-28 18:50:30 -0400

    finish sleep class

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -7,14 +7,14 @@ describe('Sleep Repository', () => {
   let sleep;
   beforeEach(() => {
     sleep = new Sleep(sleepTestData.sleepTestData);
+    sleep.sortData()
   });
 
-  it.skip('should store an array of data', () => {
-    expect(sleep.data).to.equal(sleepTestData.sleepTestData);
+  it('should store an array of data', () => {
+    expect(sleep.data.length).to.equal(sleepTestData.sleepTestData.length);
   });
 
   it('should be able to sort based on date', ()=> {
-    sleep.sortData()
     expect(sleep.data[2]).to.deep.equal({ 
       "userID":1,"date":"2023/03/03","hoursSlept":8.4,"sleepQuality":3.5})
   })
@@ -27,22 +27,19 @@ describe('Sleep Repository', () => {
     expect(sleep.getAverage("sleepQuality")).to.equal(3.1625);
   });
 
-  it.skip('should retrieve sleep hours data', () => {
-    expect(sleep.getSleepInfoForSpecificDate("2023/03/01", "hoursSlept")).to.equal(9.6);
+  it('should retrieve sleep hours data', () => {
+    expect(sleep.getInfoForSpecificDate("2023/03/01", "hoursSlept")).to.equal(9.6);
   });
   
-  it.skip('should retrieve sleep quality data', () => {
-    expect(sleep.getSleepInfoForSpecificDate("2023/03/01", "sleepQuality")).to.equal(4.3);
+  it('should retrieve sleep quality data', () => {
+    expect(sleep.getInfoForSpecificDate("2023/03/01", "sleepQuality")).to.equal(4.3);
   });
   
-  it.skip('should retrieve sleep hours for specified week', () => {
-    sleep.hoursSleptInWeek("2023/03/01");
-    
-    expect(sleep.hoursSleptInWeek("2023/03/11")).to.deep.equal([9.6, 8.4, 9.7, 4.7, 8]);
+  it('should retrieve sleep hours for specified week', () => {
+    expect(sleep.getInfoForPastWeek("hoursSlept")).to.deep.equal([9.6, 9.7, 8.4, 8, 4.7, 4.2, 4.1]);
   });
-  it.skip('should retrieve sleep quality for specified week', () => {
-    sleep.qualitySleptInWeek("2023/03/01");
 
-    expect(sleep.weekQuality).to.deep.equal([4.3, 3.5, 4.7, 3, 3.1]);
+  it('should retrieve sleep quality for specified week', () => {
+    expect(sleep.getInfoForPastWeek("sleepQuality")).to.deep.equal([4.3, 4.7, 3.5, 3.1, 3, 1.2, 3.9]);
   });
 });

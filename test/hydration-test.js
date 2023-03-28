daee022fa7337dc21b2c9c08841f7216425b1db7
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-03-27 22:32:44 -0700

    Add test to update hydration property, add test to calculate the average liquid

--- test/hydration-test.js
+++ test/hydration-test.js
@@ -1,4 +1,6 @@
-import { expect } from 'chai';
+import {
+  expect
+} from 'chai';
 import Hydration from '../src/classes/Hydration.js';
 import hydrationTestData from './hydration-test-data.js';
 import User from '../src/User.js';
@@ -6,47 +8,33 @@ import UserRepository from "../src/UserRepository";
 
 
 describe("Hydration", () => {
-  let hydration, testRepository, testUser, users;
+  let hydration;
 
   beforeEach(() => {
     hydration = new Hydration(hydrationTestData);
-    testRepository = new UserRepository(users);
-   users = [
-     {
-       "id": 1,
-       "name": "Trystan Gorczany",
-       "address": "9484 Lucas Flat, West Kittymouth WA 67504",
-       "email": "Taurean_Pollich31@gmail.com",
-       "strideLength": 4,
-       "dailyStepGoal": 7000,
-       "friends": [
-         5,
-         43,
-         46,
-         11
-    ] }
-       ]
-  
-    // user1 = new UserRepository()
+
   });
   it("should be a function", function () {
     expect(Hydration).to.be.a("function");
   });
 
-  it("should be instance of Hydration", function () {
+  it("should be an instance of Hydration", function () {
     expect(hydration).to.be.an.instanceOf(Hydration);
   });
 
-  it("should have a property that holds the hydration data", () => {
+  it("should have a property that holds the hydration data object", () => {
     hydration = new Hydration(hydrationTestData[0]);
-    expect(hydration.data).to.deep.equal(
-      { userID: 1, date: "2023/03/24", numOunces: 28 },
-    );
+    expect(hydration.data).to.deep.equal({
+      userID: 1,
+      date: "2023/03/24",
+      numOunces: 28
+    }, );
   });
 
   it("should be able to find hydration data based on the user", () => {
     expect(hydration.findUserById(1)).to.have.a.lengthOf(3);
   })
+  
   it("should be able to able to calculate the average liquid a user consumes", () => {
     expect(hydration.calculateAverageFluidPerUser(1)).to.equal(33);
   });

13f3e3f4c9a9b1c7ea0aea5d0c8b40cc77150dfe
Matt Saqueli <msaqueli22@turing.edu>
2023-03-27 22:29:44 -0500

    refactor: User class, User Repository class, file placement

--- src/data/UserRepository.js
+++ src/data/UserRepository.js
@@ -1,17 +1,9 @@
-
-import users from './user-test-data';
-
 class UserRepository {
-  constructor(userInfo) {
-    this.id = userInfo.id;
-    this.name = userInfo.name;
-    this.address = userInfo.address;
-    this.email = userInfo.email;
-    this.strideLength = userInfo.strideLength;
-    this.dailyStepGoal = userInfo.dailyStepGoal;
-    this.friends = userInfo.friends;
+  constructor(allUserInfo, hydrationInfo) {
+    this.userInfo = allUserInfo;
+    this.hydrationInfo = hydrationInfo
   };
-  
+
 };
 
 
---
+++ test/hydration-test-data.js
@@ -0,0 +1,79 @@
+const hydrationTestData = [
+    {
+      "userID": 1,
+      "date": "2023/03/24",
+      "numOunces": 28
+    },
+    {
+      "userID": 2,
+      "date": "2023/03/24",
+      "numOunces": 35
+    },
+    {
+      "userID": 3,
+      "date": "2023/03/24",
+      "numOunces": 95
+    },
+    {
+      "userID": 4,
+      "date": "2023/03/24",
+      "numOunces": 74
+    },
+    {
+      "userID": 5,
+      "date": "2023/03/24",
+      "numOunces": 47
+    },
+    {
+      "userID": 6,
+      "date": "2023/03/24",
+      "numOunces": 86
+    },
+    {
+      "userID": 7,
+      "date": "2023/03/24",
+      "numOunces": 74
+    },
+    {
+      "userID": 8,
+      "date": "2023/03/24",
+      "numOunces": 36
+    },
+    {
+      "userID": 9,
+      "date": "2023/03/24",
+      "numOunces": 40
+    },
+    {
+      "userID": 10,
+      "date": "2023/03/24",
+      "numOunces": 49
+    },
+    {
+      "userID": 11,
+      "date": "2023/03/24",
+      "numOunces": 48
+    },
+    {
+      "userID": 12,
+      "date": "2023/03/24",
+      "numOunces": 24
+    },
+    {
+      "userID": 13,
+      "date": "2023/03/24",
+      "numOunces": 97
+    },
+    {
+      "userID": 14,
+      "date": "2023/03/24",
+      "numOunces": 45
+    },
+    {
+      "userID": 15,
+      "date": "2023/03/24",
+      "numOunces": 100
+    }
+  ]
+
+  export default hydrationTestData;

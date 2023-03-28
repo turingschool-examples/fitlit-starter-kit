4aee0a2a6dfe63fb881aa1ff120432c315ed669c
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-03-27 20:46:08 -0700

    Refactor hydration class to include property for hydration data to take in the array of objects and create method to calculate average fluid per user

--- src/classes/Hydration.js
+++ src/classes/Hydration.js
@@ -1,10 +1,15 @@
 class Hydration {
   constructor(hydrationData) {
-    this.id = hydrationData.userID;
-    this.date = hydrationData.date;
-    this.ounces = hydrationData.numOunces;
+    this.data = hydrationData;
   }
-
+  
+  calculateAverageFluidPerUser(userID) {
+    let allUsersFluid = this.data.filter((user) => userID === user.userID);
+    console.log(allUsersFluid);
+    return allUsersFluid.reduce((acc, user) => {
+      return (acc += user.numOunces) / allUsersFluid.length.toFixed(2);
+    }, 0);
+  }
 }
 
 export default Hydration;

72eaa22aa46f4ffd4f84c5de87e76172a9ce018e
Adriane Sutherland <asutherland91@gmail.com>
2023-03-29 15:15:31 -0700

    Add testing and functionality for the dailyMinutesActive and stepGoalMet methods

--- src/classes/Activity.js
+++ src/classes/Activity.js
@@ -1,16 +1,27 @@
-const dayjs = require('dayjs')
-
 class Activity {
   constructor(activityData) {
     this.data = activityData;
   }
   
-  dailyMinutesActive(user, date) {
+  dailyMinutesActive(userID, date) {
     const activeTime = this.data.find((entry) => {
-      return entry.userID === user && entry.date === date
+      return entry.userID === userID && entry.date === date
     });
     return activeTime.minutesActive;
   };
+
+  stepGoalMet(user, date) {
+    const stepsToday = this.data.find((entry) => {
+      return entry.date === date && user === user
+    });
+    console.log(stepsToday.numSteps);
+    if(user.dailyStepGoal <= stepsToday.numSteps) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 }
 
 export default Activity;

ee4b64cc3033cf5cf7471ff5109afc82824ea158
Isaiah Richardson <capcinematic@gmail.com>
2023-03-28 18:47:04 -0600

    add function for calculating a week

--- src/userHydration.js
+++ src/userHydration.js
@@ -15,15 +15,29 @@ class UserHydration {
     return allTimeAverage
   }
 
-  calculateSingleDayOunces(day) {
-
-    const allDays = this.hydrationInfo.filter(dayInfo => dayInfo.date === day)
-    const totalOunces = allDays.reduce((acc, cur) => {
-      return acc += cur.numOunces
-    }, 0)
-
-    return totalOunces
+    calculateOuncesLastSevenDays() {
+      const sevenDayOunces = [];
+  
+      for (let i = 6; i >= 0; i--) {
+        const date = new Date();
+        date.setDate(date.getDate() - i);
+  
+        const dayInfo = this.hydrationInfo.find(info => {
+          const infoDate = new Date(info.date);
+          return infoDate.toDateString() === date.toDateString();
+        });
+  
+        if (dayInfo) {
+          sevenDayOunces.push(dayInfo.numOunces);
+        } else {
+          sevenDayOunces.push(0);
+        }
+      }
+  
+      return sevenDayOunces;
+    }
   }
+  
 
   }
 

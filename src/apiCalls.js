12900109021a4d38ac81b1002fe6d7d304da7fc7
Trey Marshall <marshall.trey92@yahoo.com>
2023-03-29 15:19:43 -0500

    feat: Initial fetch setup for all classes.

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,12 +1,12 @@
 const userDataFetch = (data) => {
   return fetch(`https://fitlit-api.herokuapp.com/api/v1/${data}`)
-    .then(data => data.json)
+    .then(data => data.json())
     .catch(err => console.log(`Error at: ${err}`))
 }
 
 
 console.log('I will be a fetch request!')
 
-export default userDataFetch;
+export { userDataFetch };
 
 
--- src/scripts.js
+++ src/scripts.js
@@ -4,8 +4,31 @@
 
 // An example of how you tell webpack to use a CSS file
 import './css/styles.css';
+import User from './data/User';
+import { userDataFetch } from './apiCalls';
 
+let users, hydration, sleep, activity //other vars;
 
+Promise.all([userDataFetch('users'), userDataFetch('hydration'), userDataFetch('sleep'), userDataFetch('activity')])
+  .then(data => {
+    users = data[0].users
+    console.log("This is:", users)
+    hydration = data[1].hydrationData
+    console.log(hydration)
+    sleep = data[2].sleepData
+    console.log(sleep)
+    activity = data[3].activityData
+    console.log(activity)
+  })
+  .then(() => {
+    displayUsers()
+    // call functions here 
+  })
+function displayUsers() {
+  console.log(users)
+}
+
+console.log(users)
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png';
 
@@ -17,7 +40,7 @@ import userData from './data/users';
 console.log("User Data:", userData);
 
 import SomeClassYouChangeTheName from './SomeClassYouChangeTheName';
-import users from './data/users';
+// import users from './data/users';
 
 const newUser = new User();
 

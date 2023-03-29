f98d05969fea1f6fb182205092067b67360dac15
Houda Jawad <houdajawad@gmail.com>
2023-03-29 16:17:19 -0700

    Add method to calculate the miles walked

--- src/classes/Activity.js
+++ src/classes/Activity.js
@@ -4,7 +4,14 @@ class Activity {
   constructor(activityData) {
     this.data = activityData;
   }
-  
+ 
+  calculateMilesWalked(date, user) {
+    const dayActivities = this.data.find(activity => activity.date === date);
+    const steps = dayActivities.numSteps;
+    const miles = Math.round((steps * user.strideLength)/5280);
+    return miles; 
+  }
+
 }
 
 export default Activity;

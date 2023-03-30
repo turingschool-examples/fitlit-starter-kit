b1a822a13616a141df254151b9097f4a11b03be2
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-03-29 18:08:38 -0700

    Add testing for finding users sleep quality on specified date

--- test/sleep-test.js
+++ test/sleep-test.js
@@ -1,4 +1,6 @@
-import {expect} from 'chai';
+import {
+  expect
+} from 'chai';
 import Sleep from '../src/classes/Sleep.js';
 import sleepTestData from "./sleep-test-data";
 
@@ -8,12 +10,12 @@ describe('Sleep', () => {
   beforeEach(() => {
     sleep = new Sleep(sleepTestData);
   });
-  
-  it('should be a function', function() {
+
+  it('should be a function', function () {
     expect(Sleep).to.be.a('function');
   });
 
-  it('should be an instance of Sleep', function() {
+  it('should be an instance of Sleep', function () {
     expect(sleep).to.be.an.instanceOf(Sleep);
   });
 
@@ -24,18 +26,23 @@ describe('Sleep', () => {
       "date": "2023/06/28",
       "hoursSlept": 8.4,
       "sleepQuality": 1.8
-      }, );
+    }, );
   });
 
-  it('should be able to calculate average sleep hours', function() {
+  it('should be able to calculate average sleep hours', function () {
     expect(sleep.calculateAverageSleepHours(1)).to.be.equal(8.48);
   });
 
-  it('should be able to calculate average sleep quality', function() {
+  it('should be able to calculate average sleep quality', function () {
     expect(sleep.calculateAverageSleepQuality(1)).to.be.equal(2.52);
   });
 
-  it('should be able to find sleep hours on a specific date', function() {
+  it('should be able to find sleep hours on a specific date', function () {
     expect(sleep.findSleepHoursOnDate(1, "2023/06/28")).to.be.equal(8.4);
+  });
+  it("should be able to find a user's sleep quality on a specified date", () => {
+    expect(sleep.findSleepQualityOnDate(1, "2023/06/28")).to.equal(1.8);
+    expect(sleep.findSleepQualityOnDate(3, "2023/06/28")).to.equal(4.6);
+    expect(sleep.findSleepQualityOnDate(4, "2023/06/30")).to.equal(3.4);
   });
 });

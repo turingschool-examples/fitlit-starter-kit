65da97f0efc5768ba7c2daff1f19c131090b8aab
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-03-29 18:06:21 -0700

    method to find sleep quality to specified date

--- src/classes/Sleep.js
+++ src/classes/Sleep.js
@@ -3,29 +3,35 @@ class Sleep {
     this.data = sleepData;
   }
 
-  calculateAverageSleepHours(userID){
-    let userSleepData = this.data.filter(user => user.userID === userID);
+  calculateAverageSleepHours(userID) {
+    let userSleepData = this.data.filter((user) => user.userID === userID);
     let totalSleepHours = userSleepData.reduce((acc, user) => {
       acc += user.hoursSlept;
       return acc;
-    }, 0)
+    }, 0);
     return Number((totalSleepHours / userSleepData.length).toFixed(2));
-  };
+  }
 
-  calculateAverageSleepQuality(userID){
-    let userSleepData = this.data.filter(user => user.userID === userID);
+  calculateAverageSleepQuality(userID) {
+    let userSleepData = this.data.filter((user) => user.userID === userID);
     let totalSleepQuality = userSleepData.reduce((acc, user) => {
       acc += user.sleepQuality;
       return acc;
-    }, 0)
+    }, 0);
     return Number((totalSleepQuality / userSleepData.length).toFixed(2));
-  };
+  }
 
-  findSleepHoursOnDate(userID, date){
-    let userSleepData = this.data.filter(user => user.userID === userID);
-    let sleepHours = userSleepData.find(user => user.date === date);
+  findSleepHoursOnDate(userID, date) {
+    let userSleepData = this.data.filter((user) => user.userID === userID);
+    let sleepHours = userSleepData.find((user) => user.date === date);
     return sleepHours.hoursSlept;
-  };
+  }
+
+  findSleepQualityOnDate(userID, date) {
+    let filteredSleepData = this.data.filter((user) => user.userID === userID);
+    let sleepQuality = filteredSleepData.find((user) => user.date === date);
+    return sleepQuality.sleepQuality;
+  }
 };
 
 export default Sleep;

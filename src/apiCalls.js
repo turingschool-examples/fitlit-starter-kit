43ae3992ec9963bd742a84b45f7244fb92d9a338
John Featherstone <johnwfeatherstone.personal@gmail.com>
2023-03-30 10:31:42 -0600

    Fully integrates API calls and fixes Sleep DOM manipulators to render the data correctly on screen - comments out function requiring method that does not currently exist.

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,46 +1,51 @@
-// let hydration;
-// let user;
-// let userID = 1;
-// let date = "2023/03/24";
+let hydration;
+let user;
+let userID = 1;
+// need function to generate random user
+let date = "2023/03/24";
+// need function to pull latest date? 
+let sleep;
 
-// import UserRepository from './classes/UserRepository';
-// import User from './classes/User';
-// import Hydration from './classes/Hydration';
-// import { displayUserCard, displayStepUserVsAllUsers, displayUserGreeting, displayhydrationCard, displayLatestSleepData, displayAllTimeSleepData} from './scripts';
+import UserRepository from './classes/UserRepository';
+import User from './classes/User';
+import Hydration from './classes/Hydration';
+import Sleep from './classes/Sleep';
+import { displayUserCard, displayStepUserVsAllUsers, displayUserGreeting, displayhydrationCard, displayLatestSleepData, displayAllTimeSleepData} from './scripts';
+// add new DOM manipulation functions made in scripts.js to this import object and the export object in scripts.
 
+function fetchUsers() {
+  return fetch("https://fitlit-api.herokuapp.com/api/v1/users")
+    .then(response => response.json());
+};
 
-// function fetchUsers() {
-//   return fetch("https://fitlit-api.herokuapp.com/api/v1/users")
-//     .then(response => response.json());
-// };
+function fetchHydration() {
+  return fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
+    .then((response) => response.json());
+};
 
-// function fetchHydration() {
-//   return fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
-//     .then((response) => response.json());
-// };
+function fetchSleep() {
+  return fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
+    .then((response) => response.json());
+};
 
-// function fetchSleep() {
-//   return fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
-//     .then((response) => response.json());
-// };
+Promise.all([fetchUsers(), fetchHydration(), fetchSleep()])
+  .then(([userData, hydrationData, sleepData]) => {
+    const userBase = new UserRepository(userData.users);
+    user = new User(userBase.getUser(userID));
+    displayUserCard(user);
+    displayStepUserVsAllUsers(user, userBase);
+    displayUserGreeting(user);
 
-// Promise.all([fetchUsers(), fetchHydration(), fetchSleep()])
-//   .then(([userData, hydrationData, sleepData]) => {
-//     const userBase = new UserRepository(userData.users);
-//     user = new User(userBase.getUser(userID));
-//     displayUserCard(user);
-//     displayStepUserVsAllUsers(user, userBase);
-//     displayUserGreeting(user);
+    hydration = new Hydration(hydrationData.hydrationData);
+    displayhydrationCard(hydration, userID, date);
 
-//     hydration = new Hydration(hydrationData.hydrationData);
-//     displayhydrationCard(hydration, userID, date);
-
-//     sleepData = new Sleep(sleepData.sleepData);
-//     displayLatestSleepData(sleepData, userID, date);
-//     displayAllTimeSleepData(sleepData, userID);
-//     // sleep DOM manipulation functions go here
-//   })
-//   .catch(error => {
-//     console.error('Error fetching data:', error);
-//   });
+    sleep = new Sleep(sleepData.sleepData);
+    console.log(sleep)
+    // displayLatestSleepData(sleepData, userID, date);
+    displayAllTimeSleepData(sleep, userID);
+    // sleep DOM manipulation functions go here
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
 

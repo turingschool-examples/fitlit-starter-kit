219fd6b14819d38d62b6815b496c220231accc85
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-03-30 12:48:18 -0400

    Update to test for invalid data

--- src/Sleep.js
+++ src/Sleep.js
@@ -10,15 +10,23 @@ class Sleep {
   }
   
   getAverage(dataType) {
-    let total = this.data.reduce((acc, currentValue) => {
-      acc += currentValue[dataType]
-      return acc
-      }, 0)
-    return parseFloat((total / this.data.length).toFixed(4))
+    if (dataType === "hoursSlept" || dataType === "sleepQuality") {
+      let total = this.data.reduce((acc, currentValue) => {
+        acc += currentValue[dataType]
+        return acc
+        }, 0)
+      return parseFloat((total / this.data.length).toFixed(4))
+    } else {
+      return `${dataType} is not a valid argument!`
+    }
   }
 
   getInfoForSpecificDate(date, infoType){
-    return this.data.find(sleep => sleep.date === date)[infoType]
+    if (this.data.some(sleep => sleep.date === date)){
+      return this.data.find(sleep => sleep.date === date)[infoType]
+    } else {
+      return "There is no data for this date";
+    }
   }
 
   getInfoForPastWeek(infoType) {

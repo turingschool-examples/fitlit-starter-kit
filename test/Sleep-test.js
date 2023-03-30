61787c76e378da047a340010dc2f24bfb64b479b
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-03-30 12:48:42 -0400

    Update to test for invalid data

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -7,7 +7,15 @@ describe('Sleep Repository', () => {
   let sleep;
   beforeEach(() => {
     sleep = new Sleep(sleepTestData.sleepTestData);
-    sleep.sortData()
+    sleep.sortData();
+  });
+  
+  it('should be a function', () => {
+    expect(Sleep).to.be.a('function');
+  });
+
+  it('should be an instance of Sleep', () => {
+    expect(sleep).to.be.instanceof(Sleep);
   });
 
   it('should store an array of data', () => {
@@ -16,7 +24,7 @@ describe('Sleep Repository', () => {
 
   it('should be able to sort based on date', ()=> {
     expect(sleep.data[2]).to.deep.equal({ 
-      "userID":3,"date":"2023/03/24","hoursSlept":9.7,"sleepQuality":4.7})
+      "userID":3,"date":"2023/03/24","hoursSlept":9.7,"sleepQuality":4.7});
   })
 
   it('should calculate average hours slept', () => {
@@ -27,12 +35,20 @@ describe('Sleep Repository', () => {
     expect(sleep.getAverage("sleepQuality")).to.equal(3.0259);
   });
 
+  it('should return error if invalid argument is given', () => {
+    expect(sleep.getAverage("Tacos")).to.equal("Tacos is not a valid argument!")
+  });
+
   it('should retrieve sleep hours data', () => {
     expect(sleep.getInfoForSpecificDate("2023/03/24", "hoursSlept")).to.equal(9.6);
   });
   
   it('should retrieve sleep quality data', () => {
     expect(sleep.getInfoForSpecificDate("2023/03/24", "sleepQuality")).to.equal(4.3);
+  });
+
+  it('should return an error if date is not available', () => {
+    expect(sleep.getInfoForSpecificDate("1889/04/25", "sleepQuality")).to.equal("There is no data for this date")
   });
   
   it('should retrieve sleep hours for specified week', () => {

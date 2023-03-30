63bff6a4321af6114fe44fc01838a31ae242d8f7
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-03-29 20:09:46 -0400

    Feat:Add user info to DOM reformatted name and info to fit in user info card

--- src/scripts.js
+++ src/scripts.js
@@ -1,53 +1,56 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
-
 // An example of how you tell webpack to use a CSS file
+// Webpack links
 import './css/styles.css';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-// import Chart from 'chart.js'
-
-
-// (async function() {
-//   const data = [
-//     { year: 2010, count: 10 },
-//     { year: 2011, count: 20 },
-//     { year: 2012, count: 15 },
-//     { year: 2013, count: 25 },
-//     { year: 2014, count: 22 },
-//     { year: 2015, count: 30 },
-//     { year: 2016, count: 28 },
-//   ];
-
-//   const test = [ 3, 13, 7, 13, 10, 13, 16]
-//   const placement = document.getElementById('test')
+import userTestData from '../src/data/user-test-data';
+import User from '../src/User'
+import Sleep from '../src/Sleep';
+// import Hydration from '../src/Hydration';
+// import hydrationTestData  from '../src/data/hydration-test-data';
+import sleepTestData from '../src/data/sleep-test-data';
+import './images/turing-logo.png';
+// import './images/turing-logo.png';
 
 
-//   new Chart(
-//     placement,
-//     {
-//       type: 'line',
-//       data: {
-//         labels: data.map(row => row.year),
-//         datasets: [
-//           {
-//             label: "none",
-//             data: test,
-//             backgroundColor: "#FF6384"
-//           }
-//         ]
-//       }
-//     }
-//   );
-// })();
+// Queury selectors
+const firstName = document.querySelector('#userName')
+const userInfo = document.querySelector('#userInfo')
+const sleepCurrent = document.querySelector('#sleepBox')
+const sleepWeek = document.querySelector('#sleepBoxWeek')
+const sleepAvg = document.querySelector('#sleepBoxAvg')
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// Global Variables
+ let user = new User(userTestData.userTestData[0]);
+ let sleep = new Sleep(sleepTestData.sleepTestData);
+ //  let hydration = new Hydration();
 
-// An example of how you tell webpack to use a JS file
+// Event listeners
 
-import userData from './data/users';
-console.log("User Data:", userData);
+// DOM Methods
+const displayCurrentUser = (user) => {
+  firstName.innerText = `${user.getName()}`;
+  userInfo.innerHTML = `<h4>Address: ${user.address}</h4>
+  <h4>Email: ${user.email}</h4> 
+  <h4>Stride Length: ${user.strideLength}</h4>
+  <h4>Daily Step Goal: ${user.dailyStepGoal}</h4>
+  <h4>Friends: ${user.getFriends(userTestData.userTestData)}</h4>
+  <h4>Your Step Goal Compared to All Users: ${user.dailyStepGoal}/${user.getAverage(userTestData.userTestData)}</h4>
+  `
+}
+displayCurrentUser(user)
+
+const displaySleepInfo = (sleep) => {
+  const latestSleep = sleep.data[sleep.data.length - 1];
+  const pastWeekSleep = sleep.getInfoForPastWeek('hoursSlept');
+  const avgQuality = sleep.getAverage('sleepQuality');
+  const avgHours = sleep.getAverage('hoursSlept');
+  sleepCurrent.innerHTML = `<h4>Latest Hours Slept: ${latestSleep.hoursSlept} Latest Quality of Sleep: ${latestSleep.sleepQuality}</h4>`;
+  sleepWeek.innerHTML = `<h4> Sleep Pattern Last 7 Days: ${pastWeekSleep.join(', ')}</h4>`;
+  sleepAvg.innerHTML = `<h4> Average Sleep Quality: ${avgQuality.toFixed(1)} Average Hours Slept: ${avgHours.toFixed(1)}</h4>`;
+}
+displaySleepInfo(sleep)
 
 
 

364eb0f9cc9d316dcdc091e9f7dd1ac5a5889f7c
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-03-29 19:32:05 -0700

    Add sample sleep data over a week

--- test/sleep-test-data.js
+++ test/sleep-test-data.js
@@ -1,172 +1,207 @@
-const sleepTestData = [
-  {
-  "userID": 1,
-  "date": "2023/06/28",
-  "hoursSlept": 8.4,
-  "sleepQuality": 1.8
+const sleepTestData = [{
+    "userID": 1,
+    "date": "2023/06/28",
+    "hoursSlept": 8.4,
+    "sleepQuality": 1.8
   },
   {
-  "userID": 2,
-  "date": "2023/06/28",
-  "hoursSlept": 8.4,
-  "sleepQuality": 3.7
+    "userID": 2,
+    "date": "2023/06/28",
+    "hoursSlept": 8.4,
+    "sleepQuality": 3.7
   },
   {
-  "userID": 3,
-  "date": "2023/06/28",
-  "hoursSlept": 5.2,
-  "sleepQuality": 4.6
+    "userID": 3,
+    "date": "2023/06/28",
+    "hoursSlept": 5.2,
+    "sleepQuality": 4.6
   },
   {
-  "userID": 4,
-  "date": "2023/06/28",
-  "hoursSlept": 9.7,
-  "sleepQuality": 2.5
+    "userID": 4,
+    "date": "2023/06/28",
+    "hoursSlept": 9.7,
+    "sleepQuality": 2.5
   },
   {
-  "userID": 5,
-  "date": "2023/06/28",
-  "hoursSlept": 5.8,
-  "sleepQuality": 4.6
+    "userID": 5,
+    "date": "2023/06/28",
+    "hoursSlept": 5.8,
+    "sleepQuality": 4.6
   },
   {
-  "userID": 6,
-  "date": "2023/06/28",
-  "hoursSlept": 8,
-  "sleepQuality": 3.5
+    "userID": 6,
+    "date": "2023/06/28",
+    "hoursSlept": 8,
+    "sleepQuality": 3.5
   },
   {
-  "userID": 7,
-  "date": "2023/06/28",
-  "hoursSlept": 7.1,
-  "sleepQuality": 3.4
+    "userID": 7,
+    "date": "2023/06/28",
+    "hoursSlept": 7.1,
+    "sleepQuality": 3.4
   },
   {
-  "userID": 1,
-  "date": "2023/06/29",
-  "hoursSlept": 10.8,
-  "sleepQuality": 1.9
+    "userID": 1,
+    "date": "2023/06/29",
+    "hoursSlept": 10.8,
+    "sleepQuality": 1.9
   },
   {
-  "userID": 2,
-  "date": "2023/06/29",
-  "hoursSlept": 5.6,
-  "sleepQuality": 1.4
+    "userID": 2,
+    "date": "2023/06/29",
+    "hoursSlept": 5.6,
+    "sleepQuality": 1.4
   },
   {
-  "userID": 3,
-  "date": "2023/06/29",
-  "hoursSlept": 7,
-  "sleepQuality": 1.1
+    "userID": 3,
+    "date": "2023/06/29",
+    "hoursSlept": 7,
+    "sleepQuality": 1.1
   },
   {
-  "userID": 4,
-  "date": "2023/06/29",
-  "hoursSlept": 6.8,
-  "sleepQuality": 1.4
+    "userID": 4,
+    "date": "2023/06/29",
+    "hoursSlept": 6.8,
+    "sleepQuality": 1.4
   },
   {
-  "userID": 5,
-  "date": "2023/06/29",
-  "hoursSlept": 7.6,
-  "sleepQuality": 2.5
+    "userID": 5,
+    "date": "2023/06/29",
+    "hoursSlept": 7.6,
+    "sleepQuality": 2.5
   },
   {
-  "userID": 6,
-  "date": "2023/06/29",
-  "hoursSlept": 5.3,
-  "sleepQuality": 4.2
+    "userID": 6,
+    "date": "2023/06/29",
+    "hoursSlept": 5.3,
+    "sleepQuality": 4.2
   },
   {
-  "userID": 7,
-  "date": "2023/06/29",
-  "hoursSlept": 5.5,
-  "sleepQuality": 2.3
+    "userID": 7,
+    "date": "2023/06/29",
+    "hoursSlept": 5.5,
+    "sleepQuality": 2.3
   },
   {
-  "userID": 1,
-  "date": "2023/06/30",
-  "hoursSlept": 8.2,
-  "sleepQuality": 3
+    "userID": 1,
+    "date": "2023/06/30",
+    "hoursSlept": 8.2,
+    "sleepQuality": 3
   },
   {
-  "userID": 2,
-  "date": "2023/06/30",
-  "hoursSlept": 10.3,
-  "sleepQuality": 3.5
+    "userID": 2,
+    "date": "2023/06/30",
+    "hoursSlept": 10.3,
+    "sleepQuality": 3.5
   },
   {
-  "userID": 3,
-  "date": "2023/06/30",
-  "hoursSlept": 6.1,
-  "sleepQuality": 4.3
+    "userID": 3,
+    "date": "2023/06/30",
+    "hoursSlept": 6.1,
+    "sleepQuality": 4.3
   },
   {
-  "userID": 4,
-  "date": "2023/06/30",
-  "hoursSlept": 5.3,
-  "sleepQuality": 3.4
+    "userID": 4,
+    "date": "2023/06/30",
+    "hoursSlept": 5.3,
+    "sleepQuality": 3.4
   },
   {
-  "userID": 5,
-  "date": "2023/06/30",
-  "hoursSlept": 4.5,
-  "sleepQuality": 4.1
+    "userID": 5,
+    "date": "2023/06/30",
+    "hoursSlept": 4.5,
+    "sleepQuality": 4.1
   },
   {
-  "userID": 6,
-  "date": "2023/06/30",
-  "hoursSlept": 8.2,
-  "sleepQuality": 4.1
+    "userID": 6,
+    "date": "2023/06/30",
+    "hoursSlept": 8.2,
+    "sleepQuality": 4.1
   },
   {
-  "userID": 7,
-  "date": "2023/06/30",
-  "hoursSlept": 6,
-  "sleepQuality": 1.1
+    "userID": 7,
+    "date": "2023/06/30",
+    "hoursSlept": 6,
+    "sleepQuality": 1.1
   },
   {
-  "userID": 1,
-  "date": "2023/07/01",
-  "hoursSlept": 6.5,
-  "sleepQuality": 3.4
+    "userID": 1,
+    "date": "2023/07/01",
+    "hoursSlept": 6.5,
+    "sleepQuality": 3.4
   },
   {
-  "userID": 2,
-  "date": "2023/07/01",
-  "hoursSlept": 10.2,
-  "sleepQuality": 4
+    "userID": 2,
+    "date": "2023/07/01",
+    "hoursSlept": 10.2,
+    "sleepQuality": 4
   },
   {
-  "userID": 3,
-  "date": "2023/07/01",
-  "hoursSlept": 9.9,
-  "sleepQuality": 4.6
+    "userID": 3,
+    "date": "2023/07/01",
+    "hoursSlept": 9.9,
+    "sleepQuality": 4.6
   },
   {
-  "userID": 4,
-  "date": "2023/07/01",
-  "hoursSlept": 10,
-  "sleepQuality": 3.5
+    "userID": 4,
+    "date": "2023/07/01",
+    "hoursSlept": 10,
+    "sleepQuality": 3.5
   },
   {
-  "userID": 5,
-  "date": "2023/07/01",
-  "hoursSlept": 5.6,
-  "sleepQuality": 1.5
+    "userID": 5,
+    "date": "2023/07/01",
+    "hoursSlept": 5.6,
+    "sleepQuality": 1.5
   },
   {
-  "userID": 6,
-  "date": "2023/07/01",
-  "hoursSlept": 9.4,
-  "sleepQuality": 4
+    "userID": 6,
+    "date": "2023/07/01",
+    "hoursSlept": 9.4,
+    "sleepQuality": 4
   },
   {
-  "userID": 7,
-  "date": "2023/07/01",
-  "hoursSlept": 4.1,
-  "sleepQuality": 3.1
-  }
+    "userID": 7,
+    "date": "2023/07/01",
+    "hoursSlept": 4.1,
+    "sleepQuality": 3.1
+  },
+  {
+    "userID": 3,
+    "date": "2023 / 03 / 29",
+    "hoursSlept": 5.6,
+    "sleepQuality": 1.4
+  },
+  {
+    "userID": 3,
+    "date": "2023/03/28",
+    "hoursSlept": 5.2,
+    "sleepQuality": 4.6
+  },
+  {
+    "userID": 3,
+    "date": "2023/03/27",
+    "hoursSlept": 8.5,
+    "sleepQuality": 2.9
+  },
+  {
+    "userID": 3,
+    "date": "2023/03/26",
+    "hoursSlept": 4.1,
+    "sleepQuality": 2
+  },
+  {
+    "userID": 3,
+    "date": "2023/03/25",
+    "hoursSlept": 9.5,
+    "sleepQuality": 1.8
+  },
+  {
+    "userID": 3,
+    "date": "2023/03/24",
+    "hoursSlept": 9.7,
+    "sleepQuality": 4.7
+  },
 ]
 
 export default sleepTestData;

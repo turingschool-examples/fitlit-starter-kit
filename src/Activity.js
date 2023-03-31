490400979e9576c10306e833f41119e96c175936
Kelli Watkins <watkinskellil@gmail.com>
2023-03-31 17:14:57 -0600

    feat: Add method and test for step goal

--- src/Activity.js
+++ src/Activity.js
@@ -22,6 +22,17 @@ class Activity {
     return minutes
   }
 
+  reachStepGoal(user, date) {
+    const currentUserActivity = this.activity.filter(data => data.userID === user.id)
+
+    const userActivity = currentUserActivity.filter(data => data.date === date)
+
+    if (userActivity[0].numSteps >= user.dailyStepGoal) {
+      return true
+    } else {
+      return false
+    }
+  }
 
 }
 
--- test/Activity-Test.js
+++ test/Activity-Test.js
@@ -30,7 +30,7 @@ describe('Activity', function () {
   it('should have a method to find minutes active by day', function () {
     expect(activity1.minutesActiveByDay(user, '2023/03/25')).to.equal(111)
   });
-  // it('should be a function', function() {
-  //   expect(Activity).to.be.a('function')
-  // });
+  it('should have a method to determine if user reached their daily step goal', function () {
+    expect(activity1.reachStepGoal(user, '2023/03/25')).to.equal(true)
+  });
 })

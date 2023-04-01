d6c09b284b9eff9ee048f688b044c1ebd58c44e6
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-03-31 18:14:12 -0700

    Add method to display user's friend's by name

--- src/classes/UserRepository.js
+++ src/classes/UserRepository.js
@@ -1,19 +1,31 @@
 class UserRepository {
   constructor(users) {
-    this.users = users;    
+    this.users = users;
   }
 
-  getUser(id) { 
+  getUser(id) {
     return this.users.find(user => user.id === id);
   }
 
   calculateAverageStepGoal() {
-  	let averageSteps = this.users.reduce((acc, user) => {
+    let averageSteps = this.users.reduce((acc, user) => {
       acc += user.dailyStepGoal;
       return acc;
     }, 0)
-
     return Math.trunc(averageSteps / this.users.length);
+  };
+
+  returnUserFriendsName(id) {
+    const friendsObj = this.getUser(id)
+    let friendsArray = friendsObj.friends
+    return friendsArray.reduce((acc, currentID)=> {
+       this.users.forEach((user)=> {
+        if(currentID === user.id) {
+          acc.push(user.name)
+        } 
+      })
+      return acc
+    },[]);
   };
 };
 

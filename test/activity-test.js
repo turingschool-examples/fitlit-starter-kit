cd5c005d0c74dfb79be30944c0c58198e2b9fedf
Trey Marshall <marshall.trey92@yahoo.com>
2023-04-01 15:22:27 -0500

    feat: Add property tests to activity-test.js

--- test/activity-test.js
+++ test/activity-test.js
@@ -17,26 +17,26 @@ describe('Activity', function() {
     expect(activity).to.be.an.instanceof(Activity);
   }); 
 
-  // it('should be able to take in a user ID', function() {
-  //   expect(activity.userID).to.equal(activityTestData[0].userID);
-  // });
+  it('should be able to take in a user ID', function() {
+    expect(activity.activityData[0].userID).to.equal(1);
+  });
 
-  // it('should be able to take in a date', function() {
-  //   expect(user1.date).to.equal('2023/03/24');
-  // });
+  it('should be able to take in a date', function() {
+    expect(activity.activityData[0].date).to.equal('2023/03/24');
+  });
 
-  // it('should be able to take in number of steps', function() {
-  //   expect(user1.numSteps).to.equal(7362);
-  // });
+  it('should be able to take in number of steps', function() {
+    expect(activity.activityData[0].numSteps).to.equal(7362);
+  });
 
-  // it('should be able to take in minutes active', function() {
-  //   expect(user1.minutesActive).to.equal(261);
-  //   expect(user2.minutesActive).to.equal(125);
-  // });
+  it('should be able to take in minutes active', function() {
+    expect(activity.activityData[0].minutesActive).to.equal(261);
+    expect(activity.activityData[1].minutesActive).to.equal(125);
+  });
 
-  // it('should be able to take in a users flights of stairs traversed', function() {
-  //   expect(user1.flightsOfStairs).to.equal(26)
-  // });
+  it('should be able to take in a users flights of stairs traversed', function() {
+    expect(activity.activityData[0].flightsOfStairs).to.equal(26)
+  });
 
   it('should be able to calculate the number of miles walked in a day', function() {
     expect(activity.returnMiles(1, '2023/03/24')).to.equal(6)

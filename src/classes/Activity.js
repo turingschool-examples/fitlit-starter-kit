8451f63fdce2d9ff947faf3fcb35e0184f47514f
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-04-01 13:32:56 -0700

    Refactor formatting for activity class

--- src/classes/Activity.js
+++ src/classes/Activity.js
@@ -1,18 +1,16 @@
 const dayjs = require("dayjs");
 
-
 class Activity {
   constructor(activityData) {
     this.data = activityData;
-  }
- 
-  calculateMilesWalked(date, user) {
+  };
 
+  calculateMilesWalked(date, user) {
     const dayActivities = this.data.find(activity => activity.date === date);
     const steps = dayActivities.numSteps;
-    const miles = Math.round((steps * user.strideLength)/5280);
-    return miles; 
-  }
+    const miles = Math.round((steps * user.strideLength) / 5280);
+    return miles;
+  };
 
   dailyMinutesActive(userID, date) {
     const activeTime = this.data.find((entry) => {
@@ -25,13 +23,12 @@ class Activity {
     const stepsToday = this.data.find((entry) => {
       return entry.date === date && user === user
     });
-    if(user.dailyStepGoal <= stepsToday.numSteps) {
+    if (user.dailyStepGoal <= stepsToday.numSteps) {
       return true;
-    }
-    else {
+    } else {
       return false;
-    }
-  }
+    };
+  };
 
   weeklyMinutes(user, date) {
     const startDate = dayjs(date);
@@ -40,9 +37,8 @@ class Activity {
       return entry.userID === user && dayjs(entry.date).isAfter(startDate.subtract(1, "day")) && dayjs(entry.date).isBefore(endDate.add(1, "day"));
     });
     return milesByWeek;
-  }
-  
+  };
 
-}
+};
 
 export default Activity;

839711e110fe0c268c0c9ea2250fce5d7ccf4e50
“Sarah <sarahcatherine311@gmail.com>
2023-04-01 13:37:00 -0600

    refactor: Refactor the hydration methods and script file

--- src/data/Activity.js
+++ src/data/Activity.js
@@ -1,13 +1,13 @@
 class Activity {
   constructor(activityData, userData) {
-    this.activityData = activityData,
-    this.userData = userData
+    this.activityData = activityData;
+    this.userData = userData;
   };
 
   returnDailySteps(userID, date) {
     const activityEntries = this.activityData.filter(activityEntry => activityEntry.userID === userID);
     const dailyEntry = activityEntries.find(entry => {
-      return entry.date === date ;
+      return entry.date === date;
     });
     return dailyEntry.numSteps
   };
@@ -15,52 +15,49 @@ class Activity {
   returnWeeklySteps(userID, startDate) {
     const activityEntries = this.activityData.filter(activityEntry => activityEntry.userID === userID);
     const indexOfCurrentDayEntry = activityEntries.indexOf(activityEntries.find(entry => {
-      return entry.date === startDate
-    }))
+      return entry.date === startDate;
+    }));
     let weeklySteps = [];
 
     for (let i = indexOfCurrentDayEntry; i > indexOfCurrentDayEntry - 7; i--) {
-      weeklySteps.push(activityEntries[i])
-    }
+      weeklySteps.push(activityEntries[i]);
+    };
 
-    const weeklyActivityData = weeklySteps.map(entry => (
-      {
+    const weeklyActivityData = weeklySteps.map(entry => ({
       date: entry.date, 
       steps: entry.numSteps + ' steps taken'
-      }
-      ))
-      return weeklyActivityData
+      }));
+      return weeklyActivityData;
   };
 
   returnMiles(userID, date) {
-    const userInfo = this.userData.find(user => user.id === userID)
+    const userInfo = this.userData.find(user => user.id === userID);
     const activityEntries = this.activityData.filter(activityEntry => activityEntry.userID === userID);
-
     const dailyEntry = activityEntries.find(entry => {
-      return entry.date === date ;
+      return entry.date === date;
     });
-    return Math.round(userInfo.strideLength * dailyEntry.numSteps / 5280)
+    return Math.round(userInfo.strideLength * dailyEntry.numSteps / 5280);
   };
 
   returnMinutesActive(userID, date) {
     const activityEntries = this.activityData.filter(activityEntry => activityEntry.userID === userID);
 
     const dailyEntry = activityEntries.find(entry => {
-      return entry.date === date ;
+      return entry.date === date;
     });
-    return dailyEntry.minutesActive
+    return dailyEntry.minutesActive;
   };
 
   returnMetStepGoal(userID, date) {
-    const userInfo = this.userData.find(user => user.id === userID)
+    const userInfo = this.userData.find(user => user.id === userID);
     const activityEntries = this.activityData.filter(activityEntry => activityEntry.userID === userID);
     const dailyEntry = activityEntries.find(entry => {
-      return entry.date === date ;
+      return entry.date === date;
     });
     if(dailyEntry.numSteps >= userInfo.dailyStepGoal) {
-      return true
+      return true;
     } else {
-      return false
+      return false;
     };
   };
 };
--- src/data/sleep.js
+++ src/data/sleep.js
@@ -1,76 +1,54 @@
-
 class Sleep{
   constructor(sleepData) {
-    this.sleepData = sleepData
-  }
+    this.sleepData = sleepData;
+  };
 
   getAvgSleep(userID) {
     const sleepEntries = this.sleepData.filter(sleepEntry => sleepEntry.userID === userID);
     const avgSleep = sleepEntries.reduce((acc, user) => {
-      return acc += user.hoursSlept
+      return acc += user.hoursSlept;
     }, 0);
     return Math.round(avgSleep/sleepEntries.length * 10) / 10;
-};
+  };
   
   getAvgQuality(userID) {
     const sleepEntries = this.sleepData.filter(entry => entry.userID === userID);
     const avgQuality = sleepEntries.reduce((acc, user) => {
-      return acc += user.sleepQuality
+      return acc += user.sleepQuality;
     }, 0);
     return Math.round(avgQuality/sleepEntries.length * 10) / 10;
-  }
+  };
 
   getHoursByDay(userID, date) {
     const sleepEntries = this.sleepData.filter(entry => entry.userID === userID);
-    const dailyEntry = sleepEntries.find(entry => entry.date === date)
-    return dailyEntry.hoursSlept
-  }
+    const dailyEntry = sleepEntries.find(entry => entry.date === date);
+    return dailyEntry.hoursSlept;
+  };
 
   getQualityByDay(userID, date) {
     const sleepEntries = this.sleepData.filter(entry => entry.userID === userID);
-    const dailyEntry = sleepEntries.find(entry => entry.date === date)
-    return dailyEntry.sleepQuality
-  }
+    const dailyEntry = sleepEntries.find(entry => entry.date === date);
+    return dailyEntry.sleepQuality;
+  };
 
   getWeekSleep(userID, startDate) {
     const sleepEntries = this.sleepData.filter(entry => entry.userID === userID);
     const indexOfCurrentDayEntry = sleepEntries.indexOf(sleepEntries.find(entry => {
-      return entry.date === startDate
+      return entry.date === startDate;
     }))
     let weeklySleep = [];
 
     for (let i = indexOfCurrentDayEntry; i > indexOfCurrentDayEntry - 7; i--) {
-      weeklySleep.push(sleepEntries[i])
-    }
+      weeklySleep.push(sleepEntries[i]);
+    };
 
-    const weeklySleepData = weeklySleep.map(entry => (
-      {
+    const weeklySleepData = weeklySleep.map(entry => ({
       date: entry.date, 
-      hoursSlept: entry.hoursSlept + ' hours slept'
-      }
-      ))
-      return weeklySleepData
-  }
-
-  getWeekSleepQuality(userID, startDate) {
-    const sleepEntries = this.sleepData.filter(entry => entry.userID === userID);
-    const indexOfCurrentDayEntry = sleepEntries.indexOf(sleepEntries.find(entry => {
-      return entry.date === startDate
-    }))
-    let weeklySleepQuality = [];
-
-    for (let i = indexOfCurrentDayEntry; i > indexOfCurrentDayEntry - 7; i--) {
-      weeklySleepQuality.push(sleepEntries[i])
-    }
-
-    const weeklySleepQualityData = weeklySleepQuality.map(entry => (
-      {
-      date: entry.date,
-      sleepQuality: entry.sleepQuality + ' sleep quality rating'
-      }
-      ))
-      return weeklySleepQualityData
-  }
+      hoursSlept: entry.hoursSlept + ' hours slept',
+      sleepQuality: ' a sleep quality rating of ' + entry.sleepQuality
+      }));
+      return weeklySleepData;
+  };
 };
 
 

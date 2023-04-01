e0c2d3f9b5a114dca65104906043494b8732509f
Winston Calhoun <superdevant@gmail.com>
2023-04-01 13:20:27 -0500

    Feat: add new method to activity class and functionaly for activity to show on DOM

--- src/data/Activity.js
+++ src/data/Activity.js
@@ -2,7 +2,15 @@ class Activity {
   constructor(activityData, userData) {
     this.activityData = activityData,
     this.userData = userData
-  }
+  };
+
+  returnDailySteps(userID, date) {
+    const activityEntries = this.activityData.filter(activityEntry => activityEntry.userID === userID);
+    const dailyEntry = activityEntries.find(entry => {
+      return entry.date === date ;
+    });
+    return dailyEntry.numSteps
+  };
 
   returnMiles(userID, date) {
     const userInfo = this.userData.find(user => user.id === userID)

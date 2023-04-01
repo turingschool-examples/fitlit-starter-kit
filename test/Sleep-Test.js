be3a3f825fd0ec7637ebd8ad1b10c8a9ec020196
Em Lindvall <elindvall22@turing.edu>
2023-04-01 00:19:38 -0500

    refac: Alter test code to include beforeEach hook

--- test/Sleep-Test.js
+++ test/Sleep-Test.js
@@ -4,52 +4,49 @@ import User from '../src/User';
 import mock from '../src/data/mock';
 
 describe('Sleep', function() {
-  let user1, user2, user3;
-  beforeEach(function() {
-      user1 = new User(mock.users[0])
-      user2 = new User(mock.users[1])
-      user3 = new User(mock.users[2])
+  let user1;
+  let sleep;
+  beforeEach(() =>  {
+    user1 = new User(mock.users[0]);
+    sleep = new Sleep(user1.id);
   })  
+
   it('should be a function', function() {
       expect(Sleep).to.be.a('function')
   });
+
   it('should be able to calculate average hours slept', function() {
     const sleep = new Sleep(user1.id);
     sleep.findAvgHours();
 
     expect(sleep.findAvgHours()).to.deep.equal("7.95");
   });
+
   it('should be able to store average sleep quality', function()  {
-    const sleep = new Sleep(user1.id);
     sleep.findAvgQuality();
 
     expect(sleep.findAvgQuality()).to.deep.equal("3.80");
   })
 
   it('should be able to find hours slept by date', function() {
-    const sleep = new Sleep(user1.id);
     sleep.findDailyHours("2023/03/24");
 
     expect(sleep.dailyHours).to.deep.equal(9.6);
   })
 
   it('should be able to find sleep quality by date', function() {
-    const sleep = new Sleep(user1.id);
     sleep.findDailyQuality("2023/03/24");
     
     expect(sleep.dailyQuality).to.deep.equal(4.3);
   })
 
   it('should be able store one week of sleep data', function()  {
-    const sleep = new Sleep(user1.id);
     sleep.findWeeklyHours("2023/03/24"); 
     
     expect(sleep.weeklyHours).to.deep.equal({ '2023/03/24': 9.6 })
   })
 
-
   it('should be able store one week of sleep quality data', function()  {
-    const sleep = new Sleep(user1.id);
     sleep.findWeeklyQuality("2023/03/24"); 
     
     expect(sleep.weeklyQuality).to.deep.equal({ '2023/03/24': 4.3 })

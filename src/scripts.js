5c463f8f2494114b5298ae1e6f2b5bfd819e6b7b
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-04-02 11:54:21 -0400

    Refactor: Add updated syntax and dry up scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -1,34 +1,31 @@
-
 // Webpack links
 import { fetchAllData } from '../src/apiCalls';
 import './css/styles.css';
-import User from '../src/User'
+import User from '../src/User';
 import Sleep from '../src/Sleep';
 import Hydration from '../src/Hydration';
 import Activity from '../src/Activity';
-// import './images/turing-logo.png';
-import userClass from '../src/apiCalls.js';
-import Chart from 'chart.js/auto'
+import Chart from 'chart.js/auto';
 
 // Queury selectors
-const firstName = document.querySelector('#userName');
-const userInfo = document.querySelector('#userInfo');
-const sleepDay = document.querySelector('#sleepBox');
-const sleepWeek = document.querySelector('#sleepBoxWeek')
-const sleepAvg = document.querySelector('#sleepBoxAvg');
-const hydrationDay = document.querySelector('#hydrationBoxDaily');
-const hydrationWeek = document.querySelector('#hydrationBoxWeek');
-const hydrationAvg = document.querySelector('#hydrationBoxAvg');
-const activityDay = document.querySelector('#activityBoxDaily');
-const activityAvg = document.querySelector('#activityBoxAvg');
-const activityWeek = document.querySelector('#activityBoxWeek');
+const firstName = document.getElementById('userName'),
+      userInfo = document.getElementById('userInfo'),
+      sleepDay = document.getElementById('sleepBox'),
+      sleepWeek = document.getElementById('sleepBoxWeek'),
+      sleepAvg = document.getElementById('sleepBoxAvg'),
+      hydrationDay = document.getElementById('hydrationBoxDaily'),
+      hydrationWeek = document.getElementById('hydrationBoxWeek'),
+      hydrationAvg = document.getElementById('hydrationBoxAvg'),
+      activityDay = document.getElementById('activityBoxDaily'),
+      activityAvg = document.getElementById('activityBoxAvg'),
+      activityWeek = document.getElementById('activityBoxWeek');
 
 // Global Variables
-let userList;
-let userObj;
-let sleepObj;
-let hydrationObj;
-let activityObj;
+let userList,
+    userObj,
+    sleepObj,
+    hydrationObj,
+    activityObj;
 
 // Event listeners
 window.addEventListener('load', () => {
@@ -69,12 +66,11 @@ const displaySleepInfo = (sleep) => {
   const pastWeekSleep = sleep.getInfoForPastWeek('hoursSlept');
   const avgQuality = sleep.getAverage('sleepQuality');
   const avgHours = sleep.getAverage('hoursSlept');
-  test(pastWeekSleep)
   sleepDay.innerHTML = `<h4>Latest Hours Slept: ${latestSleep.hoursSlept}</h4>
   <h4>Latest Quality of Sleep: ${latestSleep.sleepQuality}</h4>`;
-  // sleepWeek.innerHTML = `<h4>Last 7 Days: ${pastWeekSleep.join(', ')}</h4>`;
   sleepAvg.innerHTML = `<h4> Average Sleep Quality: ${avgQuality.toFixed(1)}</h4>
   <h4>Average Hours Slept: ${avgHours.toFixed(1)}</h4>`;
+  displaySleepChart(pastWeekSleep);
 };
 
 const displayHydration = (userId) => {
@@ -82,31 +78,29 @@ const displayHydration = (userId) => {
   let weekData = hydrationObj.findWeeklyHydration();
   let splitData = weekData.map(num => num + 'oz');
   hydrationWeek.innerHTML = `<h4>Last 7 days: ${splitData.join(', ')}</h4>`;
-  hydrationDay.innerHTML = `<h4>Fluid ounces drank today: ${hydrationObj.getHydrationSpecificDay(currentDate)}oz</h4>`;
   hydrationAvg.innerHTML = `<h4>Average daily water intake: ${hydrationObj.findAvgDailyHydration(userId)}oz</h4>`;
+  hydrationDay.innerHTML = `<h4>Fluid ounces drank today: ${hydrationObj.getHydrationSpecificDay(currentDate)}oz</h4>`;
 };
 
 const displayActivity = () => {
   let currentDate = activityObj.data[0].date;
   activityDay.innerHTML = `<h4>Latest # of Steps: ${activityObj.getStepCount(currentDate)}</h4>`;
-  activityWeek.innerHTML = `<h4>Weekly Steps: ${activityObj.getLatestWeek()}</h4>`;
   activityAvg.innerHTML = `<h4>Latest # of Minutes Active: ${activityObj.getMinutesActive(currentDate)}</h4>
     <h4>Latest Distance Walked: ${activityObj.calculateMiles(currentDate)}</h4>`;
+  activityWeek.innerHTML = `<h4>Weekly Steps: ${activityObj.getLatestWeek()}</h4>`;
 };
 
 
-//chart stuff
-
-
-let test = (sleepWeekData) => {
+//Chart Stuff
+let displaySleepChart = (sleepWeekData) => {
   const data = [
+    { day: "", sleep: 2 },
+    { day: "", sleep: 4 },
+    { day: "", sleep: 6 },
+    { day: "", sleep: 8 },
     { day: "", sleep: 10 },
-    { day: "", sleep: 20 },
-    { day: "", sleep: 15 },
-    { day: "", sleep: 25 },
-    { day: "", sleep: 22 },
-    { day: "", sleep: 30 },
-    { day: "", sleep: 28 },
+    { day: "", sleep: 12 },
+    { day: "", sleep: 14 },
   ];
 
   new Chart(

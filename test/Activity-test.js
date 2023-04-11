301520285314a608d2d3bef6abb150695c024e79
Cade Chaney <cadechaney@live.com>
2023-04-10 20:39:01 -0500

    refactor activity test to make the beforeEach cleaner

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -1,40 +1,11 @@
 import { expect } from 'chai';
 import Activity from '../src/Activity';
+import { activityLogs, testUserInfo } from './Activity-test-data';
+
 
 describe('Activity', () => {
 	let testUser;
-	let testUserInfo;
-	let activityLogs;
 	beforeEach('data creation', () => {
-		activityLogs = {
-			activityData: [
-				{ "userID": 1, "date": "2023/03/18", "numSteps": 3505, "minutesActive": 265, "flightsOfStairs": 22 },
-				{ "userID": 3, "date": "2023/03/18", "numSteps": 5327, "minutesActive": 185, "flightsOfStairs": 14 },
-				{ "userID": 1, "date": "2023/03/19", "numSteps": 3493, "minutesActive": 201, "flightsOfStairs": 15 },
-				{ "userID": 1, "date": "2023/03/20", "numSteps": 9675, "minutesActive": 501, "flightsOfStairs": 22 },
-				{ "userID": 1, "date": "2023/03/21", "numSteps": 4575, "minutesActive": 211, "flightsOfStairs": 17 },
-				{ "userID": 1, "date": "2023/03/22", "numSteps": 5505, "minutesActive": 361, "flightsOfStairs": 15 },
-				{ "userID": 1, "date": "2023/03/23", "numSteps": 6502, "minutesActive": 275, "flightsOfStairs": 23 },
-				{ "userID": 2, "date": "2023/03/23", "numSteps": 4867, "minutesActive": 155, "flightsOfStairs": 10 },
-				{ "userID": 2, "date": "2023/03/24", "numSteps": 5205, "minutesActive": 200, "flightsOfStairs": 16 },
-				{ "userID": 1, "date": "2023/03/24", "numSteps": 7362, "minutesActive": 250, "flightsOfStairs": 26 }
-			]
-		};
-
-		testUserInfo = {
-			"id": 1,
-			"name": "Trystan Gorczany",
-			"address": "9484 Lucas Flat, West Kittymouth WA 67504",
-			"email": "Taurean_Pollich31@gmail.com",
-			"strideLength": 4,
-			"dailyStepGoal": 7000,
-			"friends": [
-				5,
-				43,
-				46,
-				11
-			]
-		};
 		testUser = new Activity(testUserInfo, activityLogs)
 	});
 

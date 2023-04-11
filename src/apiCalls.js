90b3dcae32aa390356eb1686bd403ce3ce86bd6b
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-04-11 15:47:31 -0400

    Add postActivityData function that will handle the fetch request to post the data from our form for recording activity data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -12,4 +12,18 @@ const fetchAllData = () => {
   ]);
 };
 
+const postActivityData = (data) => {
+  fetch('http://localhost:3001/api/v1/activity', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(json => console.log(json))
+  .catch(err => console.log(err))
+  };
+
 export { fetchAllData }
+export { postActivityData }

2f23b3a0a67f802aac9f15766aba8ce6e5faf330
Joseph Stewart <JosephStewart1752@gmail.com>
2023-10-18 20:56:20 -0400

    created fetch functions for sleep and activity data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,19 +8,14 @@ export const fetchUserData = () => {
     .then((response) => {
       if (!response.ok) {
         throw new Error("Network response was not ok");
-      } // handles errors with the network request
+      } 
       return response.json();
     })
     .then((data) => {
-      
-       //console.log("USER DATA", userData)
        return data.users
-      // You can add additional logic to handle the data here
     })
     .catch((err) => {
       console.error("Fetch error:", err);
-      // Handle errors with the promise obect such as json parsing errors
-      // You can display an error message to the user or retry the request
     });
 };
 
@@ -29,19 +24,46 @@ export const fetchHydrationData = () => {
     .then((response) => {
       if (!response.ok) {
         throw new Error("Network response was not ok");
-      } // handles errors with the network request
+      } 
       return response.json();
     })
     .then((data) => {
-      
-      //  console.log("Hydration Data", hydrationData)
        return data.hydrationData
-      // You can add additional logic to handle the data here
-    })s
+    })
     .catch((err) => {
       console.error("Fetch error:", err);
-      // Handle errors with the promise obect such as json parsing errors
-      // You can display an error message to the user or retry the request
     });
 };
 
+
+export const fetchSleepData = () => {
+  return fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      } 
+      return response.json();
+    })
+    .then((data) => {
+       return data.sleepData
+    })
+    .catch((err) => {
+      console.error("Fetch error:", err);
+    });
+};
+
+export const fetchActivityData = () => {
+  return fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      } 
+      return response.json();
+    })
+    .then((data) => {
+       return data.activityData
+    })
+    .catch((err) => {
+      console.error("Fetch error:", err);
+    });
+};

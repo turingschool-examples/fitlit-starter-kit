d5f6092c31b7d9f4be03fd365f8fbd7d3694a9c0
Joseph Stewart <JosephStewart1752@gmail.com>
2023-10-18 20:31:17 -0400

    Add hydration fetch call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -24,3 +24,24 @@ export const fetchUserData = () => {
     });
 };
 
+export const fetchHydrationData = () => {
+  return fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      } // handles errors with the network request
+      return response.json();
+    })
+    .then((data) => {
+      
+      //  console.log("Hydration Data", hydrationData)
+       return data.hydrationData
+      // You can add additional logic to handle the data here
+    })s
+    .catch((err) => {
+      console.error("Fetch error:", err);
+      // Handle errors with the promise obect such as json parsing errors
+      // You can display an error message to the user or retry the request
+    });
+};
+

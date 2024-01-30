ee6c8dad823994143c80cecb86baa96aa42301dd
Kayla Wood <kaylaewood@gmail.com>
2024-01-30 15:50:11 -0700

    Add spaces to scripts file for readability

--- src/scripts.js
+++ src/scripts.js
@@ -5,19 +5,20 @@
 // An example of how you tell webpack to use a CSS file
 import './styles.css';
 import apiCalls from './apiCalls'
+
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png';
+
 // An example of how you tell webpack to use a JS file
 import userData from './data/users';
+
 // Below are examples of how you can import functions from either users, hydration, or domUpdates files.
 import { findUser } from './users'
 import { findAvgFluidOunces } from './hydration';
 import { displayUserInfo } from './domUpdates';
 
-
-
-
 console.log("User Data:", userData);
+
 findUser(12)
 findAvgFluidOunces(12);
 displayUserInfo();

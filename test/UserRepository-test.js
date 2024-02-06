c6b3fdc2bea211ee7f392493eedb37f91c5ba1ef
Megan Crotteau <megan.crotteau@gmail.com>
2024-02-05 21:54:23 -0700

    Find and display user data functions

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,7 +1,30 @@
 import { expect } from 'chai';
+// import { } from '../src/scripts';
+// import userData from './usersSampleData'
 
 describe('User Repository', () => {
   it('should run tests', function () {
     expect(true).to.be(true);
   });
 });
+
+// describe('display user data', () => {
+//   it.skip('should be able to find user data using a user ID', function () {
+//     const userID = 2
+//     const userData = userData
+//     expect(getUserInfo(userID).be.equal.to({
+//       "id": 2,
+//       "name": "Tyreek VonRueden",
+//       "address": "623 Koelpin Skyway, Lake Luigichester MN 77576-1678",
+//       "email": "Nicolette_Halvorson43@yahoo.com",
+//       "strideLength": 4.5,
+//       "dailyStepGoal": 9000,
+//       "friends": [
+//         13,
+//         19,
+//         3
+//       ]
+//     },))
+
+//   })
+// })

b68c8258cefe6d9df2403b437d42140a618d6f60
Edwin Chalmers <edwinchalmers@gmail.com>
2024-02-06 15:48:50 -0700

    bug fixes
    Co-authored-by Tom Coney : <tom.ce.coney@gmail.com>
    Co-authored-by Theotis McCray : <tlmccraycray@gmail.com>

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,20 +1,8 @@
 import userData from './data/users.js';
 import hydrationData from './data/hydration.js';
-// import { 
-//   generateUser, 
-//   getAverageStepGoal 
-// } from '../scripts';
-
-// const { generateUser, getAverageStepGoal } = require('./scripts.js');
-
-import { generateUser, getAverageStepGoal } from './scripts'; // Adjust the path as necessary
-import users from './data/users'
-console.log('domUpdates:', users)
 
 // ------------- * Event Listeners *
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("userObject:", userObject)
-  console.log("generateUser:", generateUser(30))
   displayWelcomeMessage(userObject)
   
   // displayUserInfo(userObject)
@@ -22,30 +10,6 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 
-var randomUserId = () => {
-  const randomIndex = Math.floor(Math.random() * users.users.length);
-  console.log(randomIndex)
-  console.log('randomUserId:', users.users[randomIndex].id)
-  // return users.users[randomIndex].id;
-  return 30;
-}
-
-
-var userObject = generateUser(30); // Note the parentheses to invoke the function
-
-//function displayUserInfo(user)
-
-function displayWelcomeMessage(user) {
-  console.log("name should be replaced, is this invoked?")
-  const firstName = user.name.split(' ')[0];
-  const welcomeMessageElement = document.getElementById('welcomeMessage')
-  welcomeMessageElement.innerText = `Welcome Back, ${firstName}!`
-}
-*/
-
-// Import userData from the data file assuming it has a default export
-// Uncomment the following line if you have the users data exported as default from the users.js file
-// import userData from './data/users.js';
 
 // DOM update functions
 function displayWelcomeMessage(user) {

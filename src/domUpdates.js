47d3f14ddea6bf93e48f4a7f14f286067b7940ba
Ethan Duvall <eduvall23@turing.edu>
2024-02-06 10:32:42 -0700

    Feature/Show-avg-steps

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,6 +1,6 @@
 //NOTE: Your DOM manipulation will occur in this file
 import usersData from "./data/users";
-import { getUserData } from "./scripts";
+import { getUserData, getAverageSteps } from "./scripts";
 const info = document.querySelector("#info");
 const friendsList = document.querySelector("#friends");
 //Here are 2 example functions just to demonstrate one way you can export/import between the two js files. You'll want to delete these once you get your own code going.
@@ -8,14 +8,15 @@ function updateUserInfo() {
   
   let randomIndex = Math.floor(Math.random() * (usersData.users.length - 1)) + 1;
   let user = getUserData(randomIndex, usersData.users);
-
+  let avgStep = getAverageSteps(usersData.users)
   info.innerHTML = `<h1>Activity Tracker</h1>
   <h2 id = "name">Welcome : ${user.name}</h2>
   <h3 id = "id">Id : ${user.id} </h3>
   <h3 id = "adress">Adress : ${user.address} </h3>
   <h3 id = "adress">Email : ${user.email} </h3>
   <h3 id = "strideLength">Stride Length : ${user.strideLength}</h3>
-  <h3 id = "stepGoal">Step Goal : ${user.dailyStepGoal}</h3>`;
+  <h3 id = "stepGoal">My Step Goal : ${user.dailyStepGoal} steps</h3>
+  <h3 id = "comparedStepGoal"> Avg Step Goal : ${avgStep} steps`;
   console.log(user.friends)
   updateFriendsList(user.friends);
 }

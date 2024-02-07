284c33a7a59e472bb4a75deff572d8192eb07d6e
Asher Spurr <asher.spurr@gmail.com>
2024-02-06 17:38:00 -0700

    chore: comment/divs change class names/imgs

--- src/scripts.js
+++ src/scripts.js
@@ -7,6 +7,18 @@ import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png';
+import '../src/images/layered-dumbell.png'
+import '../src/images/profile-image1.png'
+import '../src/images/profile-image2.png'
+import '../src/images/profile-image3.png'
+import '../src/images/profile-image4.png'
+import '../src/images/profile-image5.png'
+import '../src/images/profile-image6.png'
+import '../src/images/profile-image7.png'
+import '../src/images/profile-image8.png'
+import '../src/images/profile-image9.png'
+import '../src/images/profile-image10.png'
+
 
 // An example of how you tell webpack to use a JS file
 import userData from './data/users';

a54a2596b8822b16622b35b4cdde6668c1573dcb
Megan Crotteau <megan.crotteau@gmail.com>
2024-02-08 13:41:38 -0700

    Hydration data API and functions

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,15 @@
 // Your fetch requests will live here!
-
-
+import { initiateSleepFunctions } from "./sleepData"
 console.log('I will be a fetch request!')
 
+function getSleepData() {
+fetch('https://fitlit-api.herokuapp.com/api/v1/sleep')
+  .then(resp => resp.json())
+  .then(data => initiateSleepFunctions(data))
+}
+
+getSleepData()
+
+export {
+    getSleepData,
+}
--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,12 +1,14 @@
 //NOTE: Your DOM manipulation will occur in this file
 import { getUserInfo, calculateAverageSteps, getRandomUser } from './scripts.js';
 import userData from './data/users';
+import { getSleepData } from './apiCalls.js';
 
 const username = document.querySelector('.user-name')
 const address = document.querySelector('.address-cont')
 const strideData = document.querySelector('.stride-data')
 const stepGoal = document.querySelector('.steps-goal-data')
 const avgStepGoal = document.querySelector('.global-steps-goal-data')
+
 
 function displayUserData(userInfo) {
   username.innerText = userInfo.name
